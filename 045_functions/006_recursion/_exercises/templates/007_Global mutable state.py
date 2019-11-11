47efe24999d349e6db3ab3d4a6b8a233795b5d77
Grey <sergejyurskyj@yahoo.com>
2019-11-11 12:16:11 -0800

    Committed added exercises for recursion.

---
+++ 045_functions/006_recursion/_exercises/templates/007_Global mutable state.py
@@ -0,0 +1,19 @@
+# Global mutable state
+current_number = 1
+accumulated_sum = 0
+
+
+def sum_recursive():
+    global current_number
+    global accumulated_sum
+    # Base case
+    if current_number == 11:
+        return accumulated_sum
+    # Recursive case
+    else:
+        accumulated_sum = accumulated_sum + current_number
+        current_number = current_number + 1
+        return sum_recursive()
+
+print(sum_recursive())
+# 55
---
+++ 045_functions/006_recursion/examples/011_lru_cache.py
@@ -0,0 +1,20 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=None)
+def fibonacci_recursive(n):
+    print("Calculating F", "(", n, ")", sep="", end=", ")
+
+    # Base case
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+
+    # Recursive case
+    else:
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+print(fibonacci_recursive(5))
+# Calculating F(5), Calculating F(4), Calculating F(3), Calculating F(2), Calculating F(1), Calculating F(0),
+
+# 5
