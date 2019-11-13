7aac070878d67e5f9a0a732d2e31b881d3d1d4b0
Grey <sergejyurskyj@yahoo.com>
2019-11-12 22:30:07 -0800

    Committed update lambda exercises

---
+++ 045_functions/007_lambda/_exercises/003_Декоратор может быть применен к лямбде.py
@@ -0,0 +1,44 @@
+# ___ some_decorator f
+#     ___ wraps 0a___
+#         print |_*Calling function *|f.__n__|**|
+#         r_ _a___
+#
+#     r_ w00
+#
+#
+#
+# 0s0000000000
+# ___ decorated_function x
+#     print|_*With argument *|x|**|
+#
+# d00_f00 2
+#
+# print('#' * 52 + '  ')
+# print('#' * 52 + '  ')
+# print('#' * 52 + '  ')
+#
+# # Defining a decorator
+# ___ trace f
+#     ___ wrap 0a___ 00k__
+#         print|_*|TRACE| func; |f.__n___|; a00; |a00|; k00; |k00|*|
+#         r_ _|0a___ 00k___
+#
+#     r_ w00
+#
+# # Applying decorator to a function
+# 0t__
+# ___ add_two x
+#     r_ x + 2
+#
+# # Calling the decorated function
+# add_two 3
+#
+# # Applying decorator to a lambda
+# print t00|l_____ x; x ** 2|||3
+#
+# print('#' * 52 + '  ')
+# print('#' * 52 + '  ')
+# print('#' * 52 + '  ')
+#
+#
+# l00|m0|t00|l_____ x; x * 2|; r000|3||||
---
+++ 045_functions/007_lambda/_exercises/010_Example use with filter.py
@@ -0,0 +1,8 @@
+# # Program to filter out only the even items from a list
+#
+# my_list _ [1, 5, 4, 6, 8, 11, 3, 12]
+#
+# new_list _ l00 fi00|l_____ x; |x%2 __ 0| ; m0_00||
+#
+# # Output: [4, 6, 8, 12]
+# print n00_l0
