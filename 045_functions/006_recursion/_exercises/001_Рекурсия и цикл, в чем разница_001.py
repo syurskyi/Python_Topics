9532ad84d7876709f3e50561c90afe56ae8feaf7
Grey <sergejyurskyj@yahoo.com>
2019-11-13 22:52:24 -0800

    Committed update recursion exercises

--- 045_functions/006_recursion/_exercises/001_Рекурсия и цикл, в чем разница_001.py
+++ 045_functions/006_recursion/_exercises/001_Рекурсия и цикл, в чем разница_001.py
@@ -1,21 +1,20 @@
-# for
-# def getTotal(n):
-#     total = 0
-#     for number in list(range(n+1)):
-#         print(number)
-#         total = total + number
-#     return total
+# # for
+# ___ getTotal n
+#     total _ 0
+#     ___ number i_ l__|ra000|n+1
+#         print nu000
+#         total _ to00 + nu00
+#     r__ to00
 #
-# print(getTotal(5))
+# print ge0000 5
 #
 # # recursion
-# print()
-
-def getTotal(n, total):
-    print(n)
-    if n == 0:  # base condition
-        return total
-    else:
-        return getTotal(n-1, (total+(n)))
-
-print(getTotal(5, 0))
+#
+# ___ getTotal n total
+#     print n
+#     i_ n __ 0  # base condition
+#         r_ to00
+#     e__
+#         r_ g00|n-1 |t...+|n
+#
+# print g000 5 0
