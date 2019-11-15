052c722a965db3897b5994129b6cd4d293fa09d6
syurskyi <sergejyurskyj@yahoo.com>
2019-11-14 17:19:59 -0800

    Update 007_decorators_template_Simulating a simple Switch in Python.py

--- 045_functions/005_decorators/_exercises/templates/1/007_decorators_template_Simulating a simple Switch in Python.py
+++ 045_functions/005_decorators/_exercises/templates/1/007_decorators_template_Simulating a simple Switch in Python.py
@@ -3,25 +3,25 @@
 
 # ___ dow_switch_fn dow
 #     __ dow __ 1
-#         fn _ l_: print Monday
+#         fn _ l____: print Monday
 #     e__ dow __ 2
-#         fn _ l_: print Tuesday
+#         fn _ l____: print Tuesday
 #     e__ dow __ 3
-#         fn _ l_: print Wednesday
+#         fn _ l____: print Wednesday
 #     e__ dow __ 4:
-#         fn _ l_: print Thursday
+#         fn _ l____: print Thursday
 #     e___ dow __ 5:
-#         fn _ l_: print Friday
+#         fn _ l____: print Friday
 #     e___ dow __ 6:
-#         fn _ l_: print Saturday
+#         fn _ l____: print Saturday
 #     e___ dow == 7:
-#         fn = l_: print Sunday
+#         fn = l____: print Sunday
 #     e___
-#         fn = l_: print Invalid day of week
+#         fn = l____: print Invalid day of week
 #
 #     r_ fn()
 #
-# dow_switch_fn 1
-# dow_switch_fn 100
+# d.......... 1
+# d 100
 #
 # print()
