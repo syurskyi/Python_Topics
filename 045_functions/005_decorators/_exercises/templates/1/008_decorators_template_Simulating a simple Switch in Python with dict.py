bc82ef42e2a27a2b5c3aebdaa58c8418f2c76a5c
syurskyi <sergejyurskyj@yahoo.com>
2019-11-14 17:22:10 -0800

    Update 008_decorators_template_Simulating a simple Switch in Python with dict.py

--- 045_functions/005_decorators/_exercises/templates/1/008_decorators_template_Simulating a simple Switch in Python with dict.py
+++ 045_functions/005_decorators/_exercises/templates/1/008_decorators_template_Simulating a simple Switch in Python with dict.py
@@ -4,19 +4,19 @@
 
 # ___ dow_switch_dict dow
 #     dow_dict _ {
-#         1: l_: print Monday
-#         2: l_: print Tuesday
-#         3: l_: print Wednesday
-#         4: l_: print Thursday
-#         5: l_: print Friday
-#         6: l_: print Saturday
-#         7: l_: print Sunday
-#         'default': l_: print Invalid day of week
+#         1: l____: print Monday
+#         2: l____: print Tuesday
+#         3: l____: print Wednesday
+#         4: l____: print Thursday
+#         5: l____: print Friday
+#         6: l____: print Saturday
+#         7: l____: print Sunday
+#         'default': l____: print Invalid day of week
 #     }
 #
-#     r_ dow_dict.g_ dow dow_d_|_default_|
+#     r_ d___.g_ dow dow_d_|_default_|
 #
-# dow_switch_dict 1
-# dow_switch_dict 100
+# d..... 1
+# d..... 100
 #
 # print()
