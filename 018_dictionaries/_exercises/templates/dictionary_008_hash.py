9a4bcaeffd49227b622b08882ba0c33c9dcecf2c
Grey <sergejyurskyj@yahoo.com>
2019-11-22 20:08:03 -0800

    Committed dictionaries templates update

---
+++ 018_dictionaries/_exercises/templates/dictionary_008_hash.py
@@ -0,0 +1,17 @@
+# # -*- coding: utf-8 -*-
+#
+# # Dictionary keys must be hashable objects. Associated values on the other hand can be any object
+# # So tuples of hashable objects are themselves hashable, but lists are not, even if they only contain hashable elements.
+# # Tuples of non-hashable elements are also not hashable.
+# #
+# hash 1 2 3
+# hash([1, 2, 3])
+# hash(([1, 2], [3, 4]))
+#
+# # Interestingly, functions are hashable:
+# ___ my_func a b c
+#     print(a, b, c)
+#
+# # Which means we can use functions as keys in dictionaries:
+# d _ |my_func 10 20 30|
+#
---
+++ 018_dictionaries/_exercises/templates/dictionary_011_chain.py
@@ -0,0 +1,31 @@
+# # -*- coding: utf-8 -*-
+#
+# # how many times each character occurred in a str__:
+# #
+# # We could also do it this way, creating a categories dictionary that has all the individual characters
+# # we are interested in:
+#
+# ______ str__
+# f___ it.... ______ ch..
+#
+#
+# ___ cat_key c
+#     cat_1 _  ' ' N..
+#     cat_2 _ di__.f... str__.a.._l.. 'lower'
+#     cat_3 _ di__.f...str__.a.._u.. 'upper'
+#     categories _ di__ ch.. cat_1.it.. cat_2.it.. cat_3.it..
+#     # categories _ {**cat_1, **cat_2, **cat_3} - I'll explain this later
+#     r_ c____.ge. c 'other'
+#
+#
+# cat_key  a cat_key A cat_key ! cat_key ' '
+#
+# categories _ {}
+# ___ c i_ text
+#     key _ cat_key c
+#     i_ key
+#         c____.s..d.. ke. se.|||.ad. c
+#
+# ___ cat i_ categories
+#     print _* cat :* **.j.. c... cat
+#
