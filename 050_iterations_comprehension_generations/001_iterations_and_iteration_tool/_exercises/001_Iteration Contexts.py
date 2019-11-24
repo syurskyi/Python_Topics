0c4b785bfddd28cadcadcff6242478b0fb7f5235
Grey <sergejyurskyj@yahoo.com>
2019-11-24 12:30:27 -0800

    Committed working files for iterations

---
+++ 050_iterations_comprehension_generations/001_iterations_and_iteration_tool/_exercises/001_Iteration Contexts.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+# Iteration Contexts
+# Use file iterators
+
+___ line i_ o... script2.py
+    print l__.u... e.._
+#
+# # Iteration Contexts
+# # Use List Comprehension
+#
+# uppers _  l__.u... ___ l... i_ o.. script2.py
+# print uppers
+#
+# # Iteration Contexts
+# # Use Map
+#
+# print m.. st_.u... o... script1.py
+#
+# # Iteration Contexts
+# # Use List and Map
+#
+# print l.. m.. st_.u.. o... script2.py
+#
+# # Iteration Contexts
+# # Use Sorted
+#
+# print s... o.. script2.py
+#
+# # Iteration Contexts
+# # Use Zip
+#
+# print l... z.. o.. script2.py  o.. script2.py
+#
+# # Iteration Contexts
+# # Use Enumerate
+#
+# print l.. enu... o.. script2.py
+#
+# # Iteration Contexts
+# # Use Filter
+#
+# print l.. f.. bo.. o... script2.py
+#
+# # Iteration Contexts
+# # Use Reduce
+#
+# print f___.r.... o__.a.. o... script2.py
+#
+# # Iteration Contexts
+# # Use Sum
+#
+# print su. 3, 2, 4, 1, 5, 0
+#
+# # Iteration Contexts
+# # Use Any
+#
+# print an. spam', '', ni
+#
+# # Iteration Contexts
+# # Use All
+#
+# print al. spam '', ni
+#
+# # Iteration Contexts
+# # Use Max
+#
+# print ma. 3, 2, 5, 1, 4
+#
+# # Iteration Contexts
+# # Use Min
+#
+# print mi. 3, 2, 5, 1, 4
+#
+# # Iteration Contexts
+# # Use Max for file
+#
+# print m.. o.. script2.py
+#
+# # Iteration Contexts
+# # Use Min for file
+#
+# print m.. o.. script2.py
+#
+# # Iteration Contexts
+# # Use List for file
+# print l... o... script2.py
+#
+# # Iteration Contexts
+# # Use Tuple for file
+# print t... o... script2.py
+#
+# # Iteration Contexts
+# # Slice assignment
+# L _ 11 22 33 44   # list
+# L 1;3 _ o.. script2.py
+# print(L)
+#
+# # Iteration Contexts
+# # list.extend method
+# L = 11
+# L.ex.. o.. script2.py
+# print(L)
