7de50eaa8e9bd84e5584d7787315fd6d60065758
Grey <sergejyurskyj@yahoo.com>
2019-11-24 23:10:32 -0800

    Committed Context Manager examples updates

--- 060_context_managers/examples/104. Generators and Context Managers - Coding.py
+++ 060_context_managers/examples/104. Generators and Context Managers - Coding.py
@@ -1,3 +1,6 @@
+# Generators and Context Managers
+# Let's see how we might write something that almost behaves like a context manager, using a generator function:
+
 print('#' * 52 + '  ### Generators and Context Managers')
 
 
@@ -14,8 +17,12 @@ gen = my_gen()  # create generator
 
 lst = next(gen)  # enter context and get "as" object
 print(lst)
+# [1, 2, 3, 4, 5]
+# ######################################################################################################################
 
 # next(gen)  # exit context    # StopIteration:
+
+# As you can see, the exiting context code ran. But to make this cleaner, we'll catch the StopIteration exception:
 
 print('#' * 52 + '  As you can see, the exiting context code ran.')
 print('#' * 52 + '  But to make this cleaner, we will catch the StopIteration exception:')
@@ -27,7 +34,14 @@ try:
     next(gen)
 except StopIteration:
     pass
+# creating context and yielding object
+# [1, 2, 3, 4, 5]
+# exiting context and cleaning up
+# ######################################################################################################################
 
+
+# Now let's write a context manager that can use the type of generator we wrote so we can use it using a with statement
+# instead:
 print('#' * 52 + '  Now lets write a context manager that can use the type of generator we wrote'
                  '  so we can use it using a `with` statement instead:')
 
@@ -49,6 +63,14 @@ class GenCtxManager:
 
 with GenCtxManager(my_gen) as lst:
     print(lst)
+# creating context and yielding object
+# [1, 2, 3, 4, 5]
+# exiting context and cleaning up
+# ######################################################################################################################
+
+# Our GenCtxManager class is not very flexible - we cannot pass arguments to the generator function for example.
+# We are also not doing any exception handling...
+# We could try some of this ourselves. For example handling arguments:
 
 print('#' * 52 + '  Our `GenCtxManager` class is not very flexible -'
                  '  we cannot pass arguments to the generator function for example.')
@@ -69,6 +91,7 @@ class GenCtxManager:
             pass
         return False
 
+
 def open_file(fname, mode):
     try:
         print('opening file...')
@@ -78,12 +101,22 @@ def open_file(fname, mode):
         print('closing file...')
         f.close()
 
+
 with GenCtxManager(open_file, 'test.txt', 'w') as f:
     print('writing to file...')
     f.write('testing...')
+# opening file...
+# writing to file...
+# closing file...
+# ######################################################################################################################
 
 print('#' * 52 + '  ')
 
 with open('test.txt') as f:
     print(next(f))
+# testing...
+# ######################################################################################################################
+
+# This works, but is not very elegant, and we still are not doing much exception handling. In the next video,
+# we'll look at a decorator in the standard library that does this far more robustly and elegantly...
 
--- 060_context_managers/examples/106. The contextmanager Decorator - Coding.py
+++ 060_context_managers/examples/106. The contextmanager Decorator - Coding.py
@@ -1,4 +1,20 @@
+# Using Decorators to Create Context Managers using Generator Functions
+# In the last video we saw how we could create a generic class that could take a generator function that
+# had a specific structure and turn it into a context manager.
+# Let's see if we can do one step better, using a decorator instead.
+# Recall the basic structure our generator function needs to have:
+# def gen(args):
+#     # set up happens here, or inside try
+#     try:
+#         yield obj # whatever normally gets returned by __enter__
+#     finally:
+#         # perform clean up code here
+# First let's define a generator function to open a file, yield it, and then close it - same as the example we saw
+# in the previous video:
+
+
 print('#' * 52 + '  ### Using Decorators to Create Context Managers using Generator Functions')
+
 
 def open_file(fname, mode='r'):
     print('opening file...')
@@ -8,6 +24,9 @@ def open_file(fname, mode='r'):
     finally:
         print('closing file...')
         f.close()
+
+# Next, let's re-create the context manager wrapper we did in the last video, but this time, I'm going to pass it a
+# generator object, instead of a generator function. But basically the same idea:
 
 
 class GenContextManager:
@@ -25,20 +44,38 @@ class GenContextManager:
             pass
         return False
 
+# At this point we can use this with our generator function as follows:
+
 file_gen = open_file('test.txt', 'w')
 
 with GenContextManager(file_gen) as f:
     f.writelines('Sir Spamalot')
+# opening file...
+# calling next to perform cleanup in generator
+# closing file...
+# ######################################################################################################################
 
+# And we can read back from the file too:
 print('#' * 52 + '  And we can read back from the file too:')
 
 file_gen = open_file('test.txt')
 with GenContextManager(file_gen) as f:
     print(f.readlines())
+# opening file...
+# ['Sir Spamalot']
+# calling next to perform cleanup in generator
+# closing file...
+# ######################################################################################################################
+
+# Of course, our context manager object is not very robust - there is no exception handling for example.
+# But let's leave that "minor detail" aside for now :-)
+# We still have to create the generator object from the generator function before we can use the context manager class.
+# We can simplify things even more by using a decorator:
 
 print('#' * 52 + '  We still have to create the generator object from the generator function before'
                  '  we can use the context manager class.')
 print('#' * 52 + '  We can simplify things even more by using a decorator:')
+
 
 def context_manager_dec(gen_fn):
     def helper(*args, **kwargs):
@@ -46,6 +83,12 @@ def context_manager_dec(gen_fn):
         ctx = GenContextManager(gen)
         return ctx
     return helper
+
+# Notice what this decorator does.
+# It decorates a generator function and returns helper. When we invoke helper it will create an instance
+# of the generator, and create and return an instance of the context manager.
+# Let's try it out:
+
 
 @context_manager_dec
 def open_file(fname, mode='r'):
@@ -57,13 +100,28 @@ def open_file(fname, mode='r'):
         print('closing file...')
         f.close()
 
+
 with open_file('test.txt') as f:
     print(f.readlines())
+# opening file...
+# ['Sir Spamalot']
+# calling next to perform cleanup in generator
+# closing file...
+# ######################################################################################################################
+
+
+# So now we have an approach to using a decorator to turn any generator function (that has the structure we mentioned
+# earlier) into a context manager!
+# Our code was not very robust, either in the context manager class or in the decorator - and it would take quite
+# a bit more work to make it so.
+# Fortunately the standard library has already though of this - in fact that was one of the critical goals of Python's
+# context managers - the ability to create context managers using generator functions (see PEP 343).
 
 print('#' * 52 + '  So now we have an approach to using a decorator to turn any generator function'
                  '  (that has the structure we mentioned earlier) into a context manager!')
 
 from contextlib import contextmanager
+
 
 @contextmanager
 def open_file(fname, mode='r'):
@@ -77,10 +135,21 @@ def open_file(fname, mode='r'):
 
 with open_file('test.txt') as f:
     print(f.readlines())
+# opening file...
+# ['Sir Spamalot']
+# closing file...
+# ######################################################################################################################
+
+
+# And of course, this works for more than just opening and closing files.
+# Here are some more examples:
+# Example 1
+# Let's implement a timer
 
 print('#' * 52 + '  Lets implement a timer.')
 
 from time import perf_counter, sleep
+
 
 @contextmanager
 def timer():
@@ -94,14 +163,21 @@ def timer():
         stats['end'] = end
         stats['elapsed'] = end - start
 
+
 with timer() as stats:
     sleep(1)
 
 print(stats)
+# {'start': 5.417897319468211e-07, 'end': 1.005228270913287, 'elapsed': 1.005227729123555}
+# ######################################################################################################################
+
+# Example 2
+# In this example, let's redirect stdout.
 
 print('#' * 52 + '  In this example, lets redirect `stdout`.')
 
 import sys
+
 
 @contextmanager
 def out_to_file(fname):
@@ -114,16 +190,29 @@ def out_to_file(fname):
         file.close()
         sys.stdout = current_stdout
 
+
 with out_to_file('test.txt'):
     print('line 1')
     print('line 2')
 
 with open('test.txt') as f:
     print(f.readlines())
+# ['line 1\n', 'line 2\n']
+# ######################################################################################################################
 
+# And of course, stdout is back to "normal":
 print('#' * 52 + '  And of course, `stdout` is back to "normal":')
 
 print('line 1')
+# line 1
+# ######################################################################################################################
+
+
+# The contextlib module actually implements a stdout redirect context manager, so we technically don't
+# have to write one ourselves.
+# The difference from thge one we wrote is that it needs an open file object, not just a file name.
+# So we would have to open the file, then redirect stdout. We can do this easily by nesting two context managers
+# as follows:
 
 print('#' * 52 + '  The `contextlib` module actually implements a `stdout` redirect context manager,'
                  '  so we technically dont have to write one ourselves.')
@@ -134,5 +223,9 @@ with open('test.txt', 'w') as f:
     with redirect_stdout(f):
         print('Look on the bright side of life')
 
+# And we can check that this worked:
+
 with open('test.txt') as f:
     print(f.readlines())
+# ['Look on the bright side of life\n']
+# ######################################################################################################################
