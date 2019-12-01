d9d1a9a8680e014784450629d174da632da83e72
Grey <sergejyurskyj@yahoo.com>
2019-12-01 11:49:13 -0800

    Committed 12-Deleting_Properties

--- 070_oop/001_classes/_exercises/templates/Python_3_Deep_Dive_Part_4/Section 02 - Classes/12-Deleting_Properties.py
+++ 070_oop/001_classes/_exercises/templates/Python_3_Deep_Dive_Part_4/Section 02 - Classes/12-Deleting_Properties.py
@@ -1,123 +1,123 @@
-# %%
-'''
-### Deleting Properties
-'''
-
-# %%
-'''
-Just like we can delete an attribute from an instance object, we can also delete a property from an instance object.
-Note that this action simply runs the deleter method, but the propertu remains defined **on the class**. 
-It does not remove the property from the class, instead it is generally used to remove the property value from the 
-**instance**.
-'''
-
-# %%
-'''
-Properties, like attributes, can be deleted by using the `del` keyword, or the `delattr` function.
-'''
-
-# %%
-class Person:
-    def __init__(self, name):
-        self.name = name
-
-    def get_name(self):
-        print('getting name property value...')
-        return self._name
-    
-    def set_name(self, value):
-        print(f'setting name property to {value}...')
-        self._name = value
-    
-    def del_name(self):
-        # delete the underlying data
-        print('deleting name property value...')
-        del self._name
-        
-    name = property(fget=get_name, fset=set_name, fdel=del_name, doc='Person name.')
-
-
-# %%
-p = Person('Guido')
-
-# %%
-print(p.name)
-
-# %%
-'''
-And the underlying `_name` property is in our instance dictionary:
-'''
-
-# %%
-print(p.__dict__)
-
-# %%
-del p.name
-
-# %%
-'''
-As we can see, the underlying `_name` attribute is no longer present in the instance dictionary:
-'''
-
-# %%
-print(p.__dict__)
-
-# %%
-try:
-    print(p.name)
-except AttributeError as ex:
-    print(ex)
-
-# %%
-'''
-As you can see, the property deletion did not remove the property definition, that still exists.
-'''
-
-# %%
-'''
-Alternatively, we can use the `delattr` function as well:
-'''
-
-# %%
-p = Person('Raymond')
-
-# %%
-delattr(p, 'name')
-
-# %%
-'''
-And we can of course use the decorator syntax as well:
-'''
-
-# %%
-class Person:
-    def __init__(self, name):
-        self.name = name
-
-    @property
-    def name(self):
-        print('getting name property value...')
-        return self._name
-    
-    @name.setter
-    def name(self, value):
-        """Person name"""
-        print(f'setting name property to {value}...')
-        self._name = value
-    
-    @name.deleter
-    def name(self):
-        # delete the underlying data
-        print('deleting name property value...')
-        del self._name
-
-# %%
-p = Person('Alex')
-
-# %%
-print(p.name)
-
-# %%
-del p.name
-
-# %%
+# # %%
+# '''
+# ### Deleting Properties
+# '''
+#
+# # %%
+# '''
+# Just like we can delete an attribute from an instance object, we can also delete a property from an instance object.
+# Note that this action simply runs the deleter method, but the propertu remains defined **on the class**.
+# It does not remove the property from the class, instead it is generally used to remove the property value from the
+# **instance**.
+# '''
+#
+# # %%
+# '''
+# Properties, like attributes, can be deleted by using the `del` keyword, or the `delattr` function.
+# '''
+#
+# # %%
+# c_ Person
+#     ___ - ____ name
+#         ____.n... = n...
+#
+#     ___ get_name ____
+#         print('getting name property value...')
+#         r_ ____._na..
+#
+#     ___ set_name ____ value
+#         print _ setting name property to |v.. ...
+#         ____._name = value
+#
+#     ___ del_name ____
+#         # delete the underlying data
+#         print('deleting name property value...')
+#         de. ____._na..
+#
+#     name _ p....fg.._g.. fs.._s... fd.._d... do._'Person name.'
+#
+#
+# # %%
+# p = ? Guido
+#
+# # %%
+# print ?.n...
+#
+# # %%
+# '''
+# And the underlying `_name` property is in our instance dictionary:
+# '''
+#
+# # %%
+# print ?.-d
+#
+# # %%
+# de. ?.n...
+#
+# # %%
+# '''
+# As we can see, the underlying `_name` attribute is no longer present in the instance dictionary:
+# '''
+#
+# # %%
+# print ? -d
+#
+# # %%
+# t__
+#     print ?.n...
+# e____ A a_ ex
+#     print ?
+#
+# # %%
+# '''
+# As you can see, the property deletion did not remove the property definition, that still exists.
+# '''
+#
+# # %%
+# '''
+# Alternatively, we can use the `delattr` function as well:
+# '''
+#
+# # %%
+# p _ ? Raymond
+#
+# # %%
+# d.... ? 'n...'
+#
+# # %%
+# '''
+# And we can of course use the decorator syntax as well:
+# '''
+#
+# # %%
+# c_ Person
+#     ___ - ____ name
+#         ____.n... _ n...
+#
+#     0p....
+#     ___ n... ____
+#         print('getting name property value...')
+#         r_ ____._na..
+#
+#     0n....s..
+#     ___ name ____ value
+#         """Person name"""
+#         print _ setting name property to |v.....
+#         ____._name = value
+#
+#     0n....d..
+#     ___ n... ____
+#         # delete the underlying data
+#         print('deleting name property value...')
+#         del ____._na..
+#
+# # %%
+# p = ? Alex
+#
+# # %%
+# print ?.n...
+#
+# # %%
+# de. ?.n...
+#
+# # %%
