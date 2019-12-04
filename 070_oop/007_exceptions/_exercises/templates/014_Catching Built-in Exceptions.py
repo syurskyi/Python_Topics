18d165215e78e0994106b01538d80b494a2dfaa0
Grey <sergejyurskyj@yahoo.com>
2019-12-03 20:49:53 -0800

    Committed 004_coroutines

--- 070_oop/007_exceptions/_exercises/templates/014_Catching Built-in Exceptions.py
+++
@@ -1,9 +0,0 @@
-# Catching Built-in Exceptions
-def kaboom(x, y):
-    print(x + y)               # Trigger TypeError
-
-try:
-    kaboom([0, 1, 2], "spam")
-except TypeError:              # Catch and recover here
-    print('Hello world!')
-print('resuming here')         # Continue here if exception or not
