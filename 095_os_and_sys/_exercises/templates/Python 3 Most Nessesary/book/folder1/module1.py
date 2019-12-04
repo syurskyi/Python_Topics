b4b3f7efd1cd6cdbafbda4f6a31b4c537360c483
Grey <sergejyurskyj@yahoo.com>
2019-12-03 20:43:53 -0800

    Committed Python 3 Most Nessesary

--- 095_os_and_sys/_exercises/templates/Python 3 Most Nessesary/book/folder1/module1.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import os, sys
-def get_cwd():
-    print("%-25s%s" % ("Файл:", os.path.abspath(__file__)))
-    print("%-25s%s" % ("Текущий рабочий каталог:", os.getcwd()))
-    print("%-25s%s" % ("Каталог для импорта:", sys.path[0]))
-    print("%-25s%s" % ("Путь к файлу:", os.path.abspath("file.txt")))
