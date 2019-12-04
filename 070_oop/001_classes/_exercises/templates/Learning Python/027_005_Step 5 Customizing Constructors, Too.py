1932ddccd88bb2eb4b1f44ed2d36969cc296b979
Grey <sergejyurskyj@yahoo.com>
2019-12-03 20:34:06 -0800

    Committed Learning Python

--- 070_oop/001_classes/_exercises/templates/Learning Python/027_005_Step 5 Customizing Constructors, Too.py
+++ 070_oop/001_classes/_exercises/templates/Learning Python/027_005_Step 5 Customizing Constructors, Too.py
@@ -1,116 +1,116 @@
-# Add customization of constructor in a subclass
-
-
-class Person:
-    def __init__(self, name, job=None, pay=0):
-        self.name = name
-        self.job = job
-        self.pay = pay
-
-    def lastName(self):
-        return self.name.split()[-1]
-
-    def giveRaise(self, percent):
-        self.pay = int(self.pay * (1 + percent))
-
-    def __str__(self):
-        return '[Person: %s, %s]' % (self.name, self.pay)
-
-
-class Manager(Person):
-    def __init__(self, name, pay):  # Redefine constructor
-        Person.__init__(self, name, 'mgr', pay)  # Run original with 'mgr'
-
-    def giveRaise(self, percent, bonus=.10):
-        Person.giveRaise(self, percent + bonus)
-
-
-if __name__ == '__main__':
-    bob = Person('Bob Smith')
-    sue = Person('Sue Jones', job='dev', pay=100000)
-    print('#' * 22)
-    print('#' * 23 + ' Add customization of constructor in a subclass')
-    print(bob)
-    print(sue)
-    print(bob.lastName(), sue.lastName())
-    sue.giveRaise(.10)
-    print(sue)
-    tom = Manager('Tom Jones', 50000)  # Job name not needed:
-    tom.giveRaise(.10)  # Implied/set by class
-    print(tom.lastName())
-    print(tom)
-
-
-# Embedding-based Manager alternative
-
-class Person:
-    def __init__(self, name, job=None, pay=0):
-        self.name = name
-        self.job = job
-        self.pay = pay
-
-    def lastName(self):
-        return self.name.split()[-1]
-
-    def giveRaise(self, percent):
-        self.pay = int(self.pay * (1 + percent))
-
-    def __str__(self):
-        return '[Person: %s, %s]' % (self.name, self.pay)
-
-
-class Manager:
-    def __init__(self, name, pay):
-        self.person = Person(name, 'mgr', pay)  # Embed a Person object
-
-    def giveRaise(self, percent, bonus=.10):
-        self.person.giveRaise(percent + bonus)  # Intercept and delegate
-
-    def __getattr__(self, attr):
-        return getattr(self.person, attr)  # Delegate all other attrs
-
-    def __str__(self):
-        return str(self.person)  # Must overload again (in 3.0)
-
-if __name__ == '__main__':
-    bob = Person('Bob Smith')
-    sue = Person('Sue Jones', job='dev', pay=100000)
-    print('#' * 22)
-    print('#' * 23 + ' Embedding-based Manager alternative')
-    print(bob)
-    print(sue)
-    print(bob.lastName(), sue.lastName())
-    sue.giveRaise(.10)
-    print(sue)
-    tom = Manager('Tom Jones', pay=100000)  # Job name not needed:
-    tom.giveRaise(.10)  # Implied/set by class
-    print(tom.lastName())
-    print(tom)
-# Aggregate embedded objects into a composite
-
-
-bob = Person('Bob Smith')
-sue = Person('Sue Jones', job='dev', pay=100000)
-tom = Manager('Tom Jones', 50000)
-
-class Department:
-    def __init__(self, *args):
-        self.members = list(args)
-
-    def addMember(self, person):
-        self.members.append(person)
-
-    def giveRaises(self, percent):
-        for person in self.members:
-            person.giveRaise(percent)
-
-    def showAll(self):
-        for person in self.members:
-            print(person)
-
-
-development = Department(bob, sue)  # Embed objects in a composite
-development.addMember(tom)
-development.giveRaises(.10)  # Runs embedded objects' giveRaise
-development.showAll()  # Runs embedded objects' __str__s
-
+# # Add customization of constructor in a subclass
+#
+#
+# c_ Person:
+#     ___ -  ____ name job_N.. pay_0
+#         ____.n... _ n...
+#         ____.j.. _ j..
+#         ____.p.. _ p..
+#
+#     ___ lastName ____
+#         r_ ____.n....sp.. -1
+#
+#     ___ giveRaise ____ percent
+#         ____.p.. _ in. ____.p.. * (1 + p...
+#
+#     ___ -s ____
+#         r_ Person: /_, /_  / ____.n..., ____.p..
+#
+#
+# c_ Manager P..
+#     ___ -  ____ name pay  # Redefine constructor
+#         Person.-  ____ n... 'mgr' p..  # Run original with 'mgr'
+#
+#     ___ giveRaise ____ percent bonus_.10
+#         P___.g.. ____ p.. + b..
+#
+#
+# __ _____ __ _____
+#     bob _ P___ Bob Smith
+#     sue _ P____ 'Sue Jones', job_'dev', pay_100000
+#     print('#' * 22)
+#     print('#' * 23 + ' Add customization of constructor in a subclass')
+#     print(bob)
+#     print(sue)
+#     print(bob.lastName(), sue.lastName())
+#     sue.giveRaise(.10)
+#     print(sue)
+#     tom _ Manager('Tom Jones', 50000)  # Job name not needed:
+#     tom.giveRaise(.10)  # Implied/set by c_
+#     print(tom.lastName())
+#     print(tom)
+#
+#
+# # Embedding-based Manager alternative
+#
+# c_ Person:
+#     ___ -  ____ name job_None pay_0
+#         ____.n... _ n...
+#         ____.j.. _ j..
+#         ____.p.. _ p..
+#
+#     ___ lastName ____
+#         r_ ____.n....sp.. -1
+#
+#     ___ giveRaise ____ percent
+#         ____.p.. _ in. ____.p.. * (1 + p..
+#
+#     ___ -s  ____
+#         r_ Person: /_, /_ / ____.n... ____.p..
+#
+#
+# c_ Manager
+#     ___ -  ____ name pay
+#         ____.person _ Person(n... 'mgr' p..  # Embed a Person object
+#
+#     ___ giveRaise ____ percent, bonus_.10
+#         ____.p__.g.. p.. + b..  # Intercept and delegate
+#
+#     ___ __g ____ attr
+#         r_ g... ____.p.. a..  # Delegate all other attrs
+#
+#     ___ -s  ____
+#         r_ st. ____.p..  # Must overload again (in 3.0)
+#
+# __ _______ __ ______
+#     bob _ P.. 'Bob Smith
+#     sue _ P..('Sue Jones', job_'dev', pay_100000)
+#     print('#' * 22)
+#     print('#' * 23 + ' Embedding-based Manager alternative')
+#     print(bob)
+#     print(sue)
+#     print(bob.lastName(), sue.lastName())
+#     sue.giveRaise(.10)
+#     print(sue)
+#     tom _ Manager('Tom Jones', pay_100000)  # Job name not needed:
+#     tom.giveRaise(.10)  # Implied/set by class
+#     print(tom.lastName())
+#     print(tom)
+# # Aggregate embedded objects into a composite
+#
+#
+# bob _ Person('Bob Smith')
+# sue _ Person('Sue Jones', job_'dev', pay_100000)
+# tom _ Manager('Tom Jones', 50000)
+#
+# c_ Department
+#     ___ -  ____ 0a.
+#         ____.members _ li.. a..
+#
+#     ___ addMember ____ person
+#         ____.m__.ap.. ?
+#
+#     ___ giveRaises ____ percent
+#         ___ person i_ ____.m..
+#             ?.g.R. p..
+#
+#     ___ showAll ____
+#         ___ person i_ ____.m..
+#             print ?
+#
+#
+# development _ Department(bob, sue)  # Embed objects in a composite
+# development.addMember(tom)
+# development.giveRaises(.10)  # Runs embedded objects' giveRaise
+# development.showAll()  # Runs embedded objects' __str__s
+#
