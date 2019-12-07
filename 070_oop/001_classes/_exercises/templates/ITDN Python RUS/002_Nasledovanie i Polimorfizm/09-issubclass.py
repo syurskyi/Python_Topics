beda5732096b400456e7582902d45da35f2a2408
Grey <sergejyurskyj@yahoo.com>
2019-12-06 19:38:26 -0800

    Committed 09-issubclass

--- 070_oop/001_classes/_exercises/templates/ITDN Python RUS/002_Nasledovanie i Polimorfizm/09-issubclass.py
+++ 070_oop/001_classes/_exercises/templates/ITDN Python RUS/002_Nasledovanie i Polimorfizm/09-issubclass.py
@@ -1,21 +1,21 @@
-# -*- coding: utf-8 -*-
-
-"""
-issubclass(cls, base) проверяет, является ли класс cls наследником класса base.
-"""
-
-print(issubclass(bool, int))  # True
-print(issubclass(float, int))  # False
-print(issubclass(int, float))  # False
-print(issubclass(complex, type))  # False
-print(issubclass(complex, object))  # True, всё наследуется от object
-
-class Base(object):
-    pass
-
-class Child(object):
-    pass
-
-print(issubclass(Child, Base))  # True
-print(issubclass(Base, object))  # True
-print(issubclass(Child, object))  # True по транзитивности
+# # -*- coding: utf-8 -*-
+#
+# """
+# issubclass(cls, base) проверяет, является ли класс cls наследником класса base.
+# """
+#
+# print iss... bo.. in.  # True
+# print iss... fl.. in.  # False
+# print iss... in. fl..  # False
+# print iss... comp.... ty..  # False
+# print iss... comp.... obj..  # True, всё наследуется от object
+#
+# c_ Base o..
+#     p____
+#
+# c_ Child o...
+#     p____
+#
+# print iss... C.. B.. # True
+# print iss... B.. o...  # True
+# print iss... C.. o...  # True по транзитивности
