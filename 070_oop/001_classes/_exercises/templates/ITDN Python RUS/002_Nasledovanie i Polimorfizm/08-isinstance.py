95c8347b8acc693b6b06a7ba73a4c3c3144d5763
Grey <sergejyurskyj@yahoo.com>
2019-12-06 19:40:32 -0800

    Committed 08-isinstance

--- 070_oop/001_classes/_exercises/templates/ITDN Python RUS/002_Nasledovanie i Polimorfizm/08-isinstance.py
+++ 070_oop/001_classes/_exercises/templates/ITDN Python RUS/002_Nasledovanie i Polimorfizm/08-isinstance.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-
-"""
-isinstance(obj, cls) проверяет, является ли obj экземпляром класса cls
-или класса, который является наследником класса cls
-"""
-
-print(isinstance(8, int))  # True
-print(isinstance('str', int))  # False
-print(isinstance(True, bool))  # True
-print(isinstance(True, int))  # True, так как bool -- подкласс int
-print(isinstance('a string', object))  # True, всё является объектами
-print(isinstance(None, object))  # True, даже None
-print(isinstance(False, str))  # False
-print(isinstance(int, type))  # True, любой класс -- объект-экземпляр метакласса type
-print(isinstance(42, type))  # False, 42 -- это не тип данных
+# # -*- coding: utf-8 -*-
+#
+# """
+# isi.... obj, cls) проверяет, является ли obj экземпляром класса cls
+# или класса, который является наследником класса cls
+# """
+#
+# print isi.... 8, in.   # True
+# print isi.... 'str', in.   # False
+# print isi.... T..., bo..   # True
+# print isi.... T..., in.   # True, так как bool -- подкласс int
+# print isi.... 'a string', ob..   # True, всё является объектами
+# print isi.... N... ob..   # True, даже None
+# print isi.... F... st.  # False
+# print isi.... in., ty..   # True, любой класс -- объект-экземпляр метакласса type
+# print isi.... 42, ty..   # False, 42 -- это не тип данных
