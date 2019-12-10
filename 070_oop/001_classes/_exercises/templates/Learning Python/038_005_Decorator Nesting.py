4c9d60bfef8ed3805ff273f1e38d49107da31cae
syurskyi <sergejyurskyj@yahoo.com>
2019-12-10 09:44:50 -0800

    Update 038_005_Decorator Nesting.py

--- 070_oop/001_classes/_exercises/templates/Learning Python/038_005_Decorator Nesting.py
+++ 070_oop/001_classes/_exercises/templates/Learning Python/038_005_Decorator Nesting.py
@@ -1,54 +1,54 @@
-# @A
-# @B
-# @C
-# def f(...):
-# ...
-# runs the same as the following:
-# def f(...):
-# ...
-# f = A(B(C(f)))
-#
-# @spam
-# @eggs
-# class C:
-# ...
-# X = C()
-# is equivalent to the following:
-# class C:
-# ...
-# C = spam(eggs(C))
-# X = C()
-#
-#
-
-def d1(F):
-    return F
-
-def d2(F):
-    return F
-
-def d3(F):
-    return F
-#
-@d1
-@d2
-@d3
-def func():               # func = d1(d2(d3(func)))
-    print('spam')
-#
-func()                    # Prints "spam"
-
-
-
-
-def d1(F): return lambda: 'X' + F()
-def d2(F): return lambda: 'Y' + F()
-def d3(F): return lambda: 'Z' + F()
-
-@d1
-@d2
-@d3
-def func():               # func = d1(d2(d3(func)))
-    return 'spam'
-
-print(func())             # Prints "XYZspam"
+# # @A
+# # @B
+# # @C
+# # def f(...):
+# # ...
+# # runs the same as the following:
+# # def f(...):
+# # ...
+# # f = A(B(C(f)))
+# #
+# # @spam
+# # @eggs
+# # class C:
+# # ...
+# # X = C()
+# # is equivalent to the following:
+# # class C:
+# # ...
+# # C = spam(eggs(C))
+# # X = C()
+# #
+# #
+# 
+# ___ d1 F
+#     r_ F
+# 
+# ___ d2 F
+#     r_ F
+# 
+# ___ d3(F):
+#     r_ F
+# #
+# 1
+# 2
+# 3
+# ___ func               # func = d1(d2(d3(func)))
+#     print('spam')
+# #
+# func()                    # Prints "spam"
+# 
+# 
+# 
+# 
+# ___ _1 F r_ l____ 'X' + F
+# ___ _2 F r_ l____ 'Y' + F
+# ___ _3 F r_ l____ 'Z' + F
+# 
+# 1
+# 2
+# 3
+# ___ func               # func = d1(d2(d3(func)))
+#     r_ 'spam'
+# 
+# print(func())             # Prints "XYZspam"
