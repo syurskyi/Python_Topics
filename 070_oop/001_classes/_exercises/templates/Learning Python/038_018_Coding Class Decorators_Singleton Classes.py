a1fabd6c03f5816f0d85d22ebc205774e6f2f43d
syurskyi <sergejyurskyj@yahoo.com>
2019-12-10 11:45:33 -0800

    Update 038_018_Coding Class Decorators_Singleton Classes.py

--- 070_oop/001_classes/_exercises/templates/Learning Python/038_018_Coding Class Decorators_Singleton Classes.py
+++ 070_oop/001_classes/_exercises/templates/Learning Python/038_018_Coding Class Decorators_Singleton Classes.py
@@ -1,63 +1,63 @@
-instances = {}
-
-
-def getInstance(aClass, *args):                 # Manage global table
-    if aClass not in instances:                 # Add **kargs for keywords
-        instances[aClass] = aClass(*args)       # One dict entry per class
-    return instances[aClass]
-
-
-def singleton(aClass):                          # On @ decoration
-    def onCall(*args):                          # On instance creation
-        return getInstance(aClass, *args)
-    return onCall
-
-
-@singleton                                      # Person = singleton(Person)
-class Person:                                   # Rebinds Person to onCall
-     def __init__(self, name, hours, rate):     # onCall remembers Person
-        self.name = name
-        self.hours = hours
-        self.rate = rate
-     def pay(self):
-        return self.hours * self.rate
-
-
-@singleton                                      # Spam = singleton(Spam)
-class Spam:                                     # Rebinds Spam to onCall
-    def __init__(self, val):                    # onCall remembers Spam
-        self.attr = val
-
-
-bob = Person('Bob', 40, 10)                     # Really calls onCall
-print(bob.name, bob.pay())
-
-sue = Person('Sue', 50, 20)                     # Same, single object
-print(sue.name, sue.pay())
-
-X = Spam(42)                                    # One Person, one Spam
-Y = Spam(99)
-print(X.attr, Y.attr)
-
-
-def singleton(aClass):                          # On @ decoration
-    instance = None
-
-    def onCall(*args):                          # On instance creation
-        nonlocal instance                       # 3.0 and later nonlocal
-        if instance == None:
-            instance = aClass(*args)            # One scope per class
-        return instance
-    return onCall
-
-
-class singleton:
-    def __init__(self, aClass):                 # On @ decoration
-        self.aClass = aClass
-        self.instance = None
-
-    def __call__(self, *args):                  # On instance creation
-        if self.instance == None:
-            self.instance = self.aClass(*args)  # One instance per class
-        return self.instance
-
+# instances = {}
+#
+#
+# ___  getInstance aClass, $                 # Manage global table
+#     i_ a.. no. i_ instances                 # Add **kargs for keywords
+#         in.. |a.. _ a.. $       # One dict entry per class
+#     r_ i... a..|
+#
+#
+# ___  singleton aClass                          # On @ decoration
+#     ___  onCall $                          # On instance creation
+#         r_ g... a.. $
+#     r_ o...
+#
+#
+# _s...                                     # Person = singleton(Person)
+# c_ Person                                   # Rebinds Person to onCall
+#      ___  - ____ name hours rate     # onCall remembers Person
+#         ____.n... _ n...
+#         ____.h.. _ h..
+#         ____.r.. _ r..
+#      ___  pay ____
+#         r_ ____.h.. * ____.r..
+#
+#
+# _s...                                     # Spam = singleton(Spam)
+# c_ Spam:                                     # Rebinds Spam to onCall
+#     ___  - ____ val                    # onCall remembers Spam
+#         ____.attr = va.
+#
+#
+# bob = Person('Bob', 40, 10)                     # Really calls onCall
+# print(bob.n..., bob.pay())
+#
+# sue = Person('Sue', 50, 20)                     # Same, single object
+# print(sue.n..., sue.pay())
+#
+# X = Spam(42)                                    # One Person, one Spam
+# Y = Spam(99)
+# print(X.attr, Y.attr)
+#
+#
+# ___  singleton aClass                          # On @ decoration
+#     instance _ N..
+#
+#     ___  onCall $                          # On instance creation
+#         no.... ?                       # 3.0 and later nonlocal
+#         i_ ? __ N..
+#             ? _ a.. $            # One scope per class
+#         r_ ?
+#     r_ o...
+#
+#
+# c_ singleton
+#     ___  - ____ aClass                 # On @ decoration
+#         ____.a.. _ a..
+#         ____.? _ N..
+#
+#     ___  -c ____ $                  # On instance creation
+#         i_ ____.? __ No
+#             ____.? _ ____.a.. $  # One instance per class
+#         r_ ____.?
+#
