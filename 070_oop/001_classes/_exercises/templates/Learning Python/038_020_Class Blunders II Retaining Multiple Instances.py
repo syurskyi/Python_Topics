b946a93518e6649dde5add5cb7c95325384f3f5f
syurskyi <sergejyurskyj@yahoo.com>
2019-12-10 12:10:38 -0800

    Update 038_020_Class Blunders II Retaining Multiple Instances.py

--- 070_oop/001_classes/_exercises/templates/Learning Python/038_020_Class Blunders II Retaining Multiple Instances.py
+++ 070_oop/001_classes/_exercises/templates/Learning Python/038_020_Class Blunders II Retaining Multiple Instances.py
@@ -1,35 +1,35 @@
-class Tracer:
-    def __init__(self, aClass):               # On @decorator
-        self.aClass = aClass                  # Use instance attribute
-
-    def __call__(self, *args):                # On instance creation
-        self.wrapped = self.aClass(*args)     # ONE (LAST) INSTANCE PER CLASS!
-        return self
-
-    def __getattr__(self, attrname):
-        print('Trace: ' + attrname)
-        return getattr(self.wrapped, attrname)
-
-
-@Tracer                                       # Triggers __init__
-class Spam:                                   # Like: Spam = Tracer(Spam)
-    def display(self):
-        print('Spam!' * 8)
-
-
-food = Spam()                                 # Triggers __call__
-food.display()                                # Triggers __getattr__
-
-
-@Tracer
-class Person:                                 # Person = Tracer(Person)
-    def __init__(self, name):                 # Wrapper bound to Person
-        self.name = name
-
-
-bob = Person('Bob')                           # bob is really a Wrapper
-print(bob.name)                               # Wrapper embeds a Person
-sue = Person('Sue')
-print(sue.name)                               # sue overwrites bob
-print(bob.name)                               # OOPS: now bob's name is 'Sue'!
-
+# c_ Tracer
+#     ___ - ____ aClass               # On @decorator
+#         ____.aClass _ a...                  # Use instance attribute
+# 
+#     ___ -c ____ $                # On instance creation
+#         ____.wrapped _ ____.aClass($     # ONE (LAST) INSTANCE PER CLASS!
+#         r_ ____
+# 
+#     ___ __getattr__(____, attrname):
+#         print('Trace: ' + attrname)
+#         r_ getattr(____.wrapped, attrname)
+# 
+# 
+# _T..                                      # Triggers __init__
+# c_ Spam                                   # Like: Spam = Tracer(Spam)
+#     ___ display ____
+#         print('Spam!' * 8)
+# 
+# 
+# food = Spam()                                 # Triggers __call__
+# food.display()                                # Triggers __getattr__
+# 
+# 
+# _T..
+# c_ Person:                                 # Person = Tracer(Person)
+#     ___ - ____ name                 # Wrapper bound to Person
+#         ____.n.. _ n..
+# 
+# 
+# bob = Person('Bob')                           # bob is really a Wrapper
+# print(bob.name)                               # Wrapper embeds a Person
+# sue = Person('Sue')
+# print(sue.name)                               # sue overwrites bob
+# print(bob.name)                               # OOPS: now bob's name is 'Sue'!
+# 
