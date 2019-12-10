6e238f29653140b8bad40cc906beab67843c8884
syurskyi <sergejyurskyj@yahoo.com>
2019-12-10 12:19:09 -0800

    Update 038_022_Managing Functions and Classes Directly.py

--- 070_oop/001_classes/_exercises/templates/Learning Python/038_022_Managing Functions and Classes Directly.py
+++ 070_oop/001_classes/_exercises/templates/Learning Python/038_022_Managing Functions and Classes Directly.py
@@ -1,64 +1,62 @@
-# Registering decorated objects to an API
-
-registry = {}
-def register(obj):                          # Both class and func decorator
-    registry[obj.__name__] = obj            # Add to registry
-    return obj                              # Return obj itself, not a wrapper
-
-@register
-def spam(x):
-    return(x ** 2)                          # spam = register(spam)
-
-@register
-def ham(x):
-    return(x ** 3)
-
-@register
-class Eggs:                                 # Eggs = register(Eggs)
-    def __init__(self, x):
-        self.data = x ** 4
-    def __str__(self):
-        return str(self.data)
-
-print('Registry:')
-for name in registry:
-    print(name, '=>', registry[name], type(registry[name]))
-
-print('\nManual calls:')
-print(spam(2))                              # Invoke objects manually
-print(ham(2))                               # Later calls not intercepted
-X = Eggs(2)
-print(X)
-
-print('\nRegistry calls:')
-for name in registry:
-    print(name, '=>', registry[name](3))    # Invoke from registry
-
-
-# Augmenting decorated objects directly
-
-def decorate(func):
-    func.marked = True          # Assign function attribute for later use
-    return func
-
-@decorate
-def spam(a, b):
-    return a + b
-
-spam.marked
-# True
-
-def annotate(text):             # Same, but value is decorator argument
-    def decorate(func):
-        func.label = text
-        return func
-    return decorate
-
-@annotate('spam data')
-def spam(a, b):                 # spam = annotate(...)(spam)
-    return a + b
-
-spam(1, 2), spam.label
-# (3, 'spam data')
-
-
+# # Registering decorated objects to an API
+# 
+# registry _      # dict
+# ___ register obj                          # Both class and func decorator
+#     registry o__. -n _ o..            # Add to registry
+#     r_ o..                              # Return obj itself, not a wrapper
+# 
+# _r..
+# ___ spam x
+#     r_(x ** 2)                          # spam = register(spam)
+# 
+# _r..
+# ___ ham x
+#     r_(x ** 3)
+# 
+# _r..
+# c_ Eggs                                 # Eggs = register(Eggs)
+#     ___ - ___ x
+#         ___.data _ x ** 4
+#     ___ -s ___
+#         r_ st. ___.d..
+# 
+# print('Registry:')
+# ___ name i_ registry
+#     print n.., '=>', ? |n.. ty.. r.. n..|
+# 
+# print('\nManual calls:')
+# print(spam(2))                              # Invoke objects manually
+# print(ham(2))                               # Later calls not intercepted
+# X = Eggs(2)
+# print(X)
+# 
+# print('\nRegistry calls:')
+# ___ name i_ registry
+#     print n.. '=>' r... n.. | 3    # Invoke from registry
+# 
+# 
+# # Augmenting decorated objects directly
+# 
+# ___ decorate func
+#     f__.marked _ T..          # Assign function attribute for later use
+#     r_ f...
+# 
+# _d..
+# ___ spam(a, b
+#     r_ a + b
+# 
+# sp__.ma..
+# # True
+# 
+# ___ annotate text             # Same, but value is decorator argument
+#     ___ decorate func
+#         f___.label _ t...
+#         r_ f..
+#     r_ d...
+# 
+# _a..  'spam data'
+# ___ spam a b                 # spam = annotate(...)(spam)
+#     r_ a + b
+# 
+# spam(1, 2), spam.label
+# # (3, 'spam data')
