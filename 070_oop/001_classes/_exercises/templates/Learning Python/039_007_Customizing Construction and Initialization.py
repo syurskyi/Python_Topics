19eaae82e1e94f6b28130ee1eb84df4fcaa5cdc2
syurskyi <sergejyurskyj@yahoo.com>
2019-12-10 14:23:32 -0800

    Update 039_007_Customizing Construction and Initialization.py

--- 070_oop/001_classes/_exercises/templates/Learning Python/039_007_Customizing Construction and Initialization.py
+++ 070_oop/001_classes/_exercises/templates/Learning Python/039_007_Customizing Construction and Initialization.py
@@ -1,27 +1,27 @@
-class MetaOne(type):
-    def __new__(meta, classname, supers, classdict):
-        print('In MetaOne.new: ', classname, supers, classdict, sep='\n...')
-        return type.__new__(meta, classname, supers, classdict)
-
-    def __init__(Class, classname, supers, classdict):
-        print('In MetaOne init:', classname, supers, classdict, sep='\n...')
-        print('...init class object:', list(Class.__dict__.keys()))
-
-
-class Eggs:
-    pass
-
-
-print('making class')
-
-
-class Spam(Eggs, metaclass=MetaOne):  # Inherits from Eggs, instance of Meta
-    data = 1  # Class data attribute
-
-    def meth(self, arg):  # Class method attribute
-        pass
-
-
-print('making instance')
-X = Spam()
-print('data:', X.data)
+# c_ MetaOne ty..
+#     ___ __new__ meta classname supers classdict
+#         print('In MetaOne.new: ', c... s.. cl... s.._'\n...')
+#         r_ ty__. -n m... c.. s... c..
+#
+#     ___ - Class classname supers classdict
+#         print('In MetaOne init:', c..  s.. cl.. s.._'\n...')
+#         print('...init class object:', li.. C__. -d .k..
+#
+#
+# c_ Eggs
+#     p..
+#
+#
+# print('making class')
+#
+#
+# c_ Spam E.. m.. _ M..  # Inherits from Eggs, instance of Meta
+#     data = 1  # Class data attribute
+#
+#     ___ meth ____ arg  # Class method attribute
+#         p...
+#
+#
+# print('making instance')
+# X = S...
+# print('data:', X.data)
