9e0064b6e06955d448d08f4a7fd5ba50d9e43712
syurskyi <sergejyurskyj@yahoo.com>
2019-12-10 14:57:46 -0800

    Update 039_014_Metaclasses Versus Class Decorators Round 2.py

--- 070_oop/001_classes/_exercises/templates/Learning Python/039_014_Metaclasses Versus Class Decorators Round 2.py
+++ 070_oop/001_classes/_exercises/templates/Learning Python/039_014_Metaclasses Versus Class Decorators Round 2.py
@@ -1,33 +1,33 @@
-# Extend with a decorator: same as providing __init__ in a metaclass
-
-def eggsfunc(obj):
-    return obj.value * 4
-
-def hamfunc(obj, value):
-    return value + 'ham'
-
-def Extender(aClass):
-    aClass.eggs = eggsfunc                   # Manages class, not instance
-    aClass.ham  = hamfunc                    # Equiv to metaclass __init__
-    return aClass
-
-@Extender
-class Client1:                               # Client1 = Extender(Client1)
-    def __init__(self, value):               # Rebound at end of class stmt
-        self.value = value
-    def spam(self):
-        return self.value * 2
-
-@Extender
-class Client2:
-    value = 'ni?'
-
-X = Client1('Ni!')                           # X is a Client1 instance
-print(X.spam())
-print(X.eggs())
-print(X.ham('bacon'))
-
-Y = Client2()
-print(Y.eggs())
-print(Y.ham('bacon'))
-
+# # Extend with a decorator: same as providing __init__ in a metaclass
+# 
+# ___ eggsfunc obj
+#     r_ ?.value * 4
+# 
+# ___ hamfunc obj value
+#     r_ value + 'ham'
+# 
+# ___ Extender aClass
+#     ?.e.. _ e...                  # Manages class, not instance
+#     ?.h..  _ h...                   # Equiv to metaclass __init__
+#     r_ ?
+# 
+# _E..
+# c_ Client1                               # Client1 = Extender(Client1)
+#     ___ - ____ value               # Rebound at end of class stmt
+#         ____.v.. _ v..
+#     ___ spam ____
+#         r_ ____.v.. * 2
+# 
+# _E..
+# c_ Client2:
+#     value = 'ni?'
+# 
+# X = _1('Ni!')                           # X is a Client1 instance
+# print(?.s..
+# print(?.e..
+# print(?.h..('bacon'))
+# 
+# Y = _2
+# print(?.e..
+# print(?.h..('bacon'))
+# 
