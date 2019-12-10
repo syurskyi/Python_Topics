14d0691d00cc74425e99ebe6e0529dcfc4792482
syurskyi <sergejyurskyj@yahoo.com>
2019-12-10 15:38:54 -0800

    Update 039_019_Metaclasses Versus Class Decorators Round 3.py

--- 070_oop/001_classes/_exercises/templates/Learning Python/039_019_Metaclasses Versus Class Decorators Round 3.py
+++ 070_oop/001_classes/_exercises/templates/Learning Python/039_019_Metaclasses Versus Class Decorators Round 3.py
@@ -1,46 +1,42 @@
-# Class decorator factory: apply any decorator to all methods of a class
-
-from types import FunctionType
-from mytools2 import tracer, timer
-
-def decorateAll(decorator):
-    def DecoDecorate(aClass):
-        for attr, attrval in aClass.__dict__.items():
-            if type(attrval) is FunctionType:
-                setattr(aClass, attr, decorator(attrval))        # Not __dict__
-        return aClass
-    return DecoDecorate
-
-@decorateAll(tracer)                          # Use a class decorator
-class Person:                                 # Applies func decorator to methods
-    def __init__(self, name, pay):            # Person = decorateAll(..)(Person)
-        self.name = name                      # Person = DecoDecorate(Person)
-        self.pay  = pay
-    def giveRaise(self, percent):
-        self.pay *= (1.0 + percent)
-    def lastName(self):
-        return self.name.split()[-1]
-
-bob = Person('Bob Smith', 50000)
-sue = Person('Sue Jones', 100000)
-print(bob.name, sue.name)
-sue.giveRaise(.10)
-print(sue.pay)
-print(bob.lastName(), sue.lastName())
-
-
-
-# @decorateAll(tracer)                 # Decorate all with tracer
-
-# @decorateAll(timer())                # Decorate all with timer, defaults
-
-# @decorateAll(timer(label='@@'))      # Same but pass a decorator argument
-
-
-
-# If using timer: total time per method
-
-print('-' * 40)
-print('%.5f' % Person.__init__.alltime)
-print('%.5f' % Person.giveRaise.alltime)
-print('%.5f' % Person.lastName.alltime)
+# # Class decorator factory: apply any decorator to all methods of a class
+# 
+# f__ ty.. ______ F..T..
+# f__ mytools2 ______ tracer, timer
+# 
+# ___ decorateAll decorator
+#     ___ DecoDecorate aClass
+#         ___ attr, attrval i_ a__. -d .it..
+#             i_ ty.. a.. i_ F.T.
+#                 s... aC.. a.. d.. a..        # Not __dict__
+#         r_ a..
+#     r_ D..
+# 
+# _d.. t..                          # Use a class decorator
+# c_ Person                                 # Applies func decorator to methods
+#     ___ - ____ name pay            # Person = decorateAll(..)(Person)
+#         ____.n.. _ n..                      # Person = DecoDecorate(Person)
+#         ____.p..  _ p..
+#     ___ giveRaise ____ percent
+#         ____.pay *_ (1.0 + p..
+#     ___ lastName ____
+#         r_ ____.n__.s.. -1
+# 
+# bob = ?('Bob Smith', 50000)
+# sue = ?('Sue Jones', 100000)
+# print(bob.name, sue.name)
+# sue.giveRaise(.10)
+# print(sue.pay)
+# print(bob.lastName(), sue.lastName())
+# 
+# # @decorateAll(tracer)                 # Decorate all with tracer
+# 
+# # @decorateAll(timer())                # Decorate all with timer, defaults
+# 
+# # @decorateAll(timer(label='@@'))      # Same but pass a decorator argument
+# 
+# # If using timer: total time per method
+# 
+# print('-' * 40)
+# print('@.5_' @ P__. - .a..
+# print('@.5_' @ P__.g.R_.a.
+# print('@.5_' @ P__.l.N_.a..
