2cb26f84ec2c0ee766e6f11858710d0d8855d3cc
syurskyi <sergejyurskyj@yahoo.com>
2019-12-10 17:46:49 -0800

    Add files via upload

---
+++ 115_testing/_exercises/templates/ITVDN_Python_Advanced/007_Unit testing/example2_test_flow.py
@@ -0,0 +1,29 @@
+# ______ u__
+# ______ d_ti_
+#
+#
+# c_ UserTestCase u___.T.C.
+#
+#     ___ setUp ____
+#         print('setUp')
+#         ____.current = d_ti_.d_ti_.n..
+#
+#     _c..
+#     ___ setUpClass ___
+#         ___.current_cls _ d_ti_.d_ti_.n..
+#         print('setUpClass')
+#
+#     ___ tearDown ____
+#         print('tearDown')
+#
+#     _c.
+#     ___ tearDownClass ___
+#         print('tearDownClass')
+#
+#     ___ test_example1 ____
+#         print(____.cu..
+#         print(____.c._c.
+#
+#     ___ test_example2 ____
+#         print(____.cu..
+#         print(____.c._c.
