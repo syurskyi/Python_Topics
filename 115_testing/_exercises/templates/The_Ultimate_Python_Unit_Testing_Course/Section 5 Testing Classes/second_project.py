b7dd246e7b41aaad5276639edeff591c1d621eaa
syurskyi <sergejyurskyj@yahoo.com>
2019-12-11 12:23:11 -0800

    Add files via upload

---
+++ 115_testing/_exercises/templates/The_Ultimate_Python_Unit_Testing_Course/Section 5 Testing Classes/second_project.py
@@ -0,0 +1,24 @@
+# """
+# the Counter class is going to able users to add, delete, and reset a counter object.
+# """
+#
+#
+# c_ Counter
+#     ___ - ____
+#         ____._value = 0
+#
+#     ___ add ____
+#         ____._v... +_ 1
+#
+#     ___ remove ____
+#         i_ ____.v... <_ 0
+#             ____.v... _ 0
+#         e___
+#             ____.v... -_ 1
+#
+#     ___ clear ____
+#         ____.v... _ 0
+#
+#     ___ get_value ____
+#         r_ ____.v...
+#
---
+++ 115_testing/_exercises/templates/The_Ultimate_Python_Unit_Testing_Course/Section 9 Testing Efficiency/tests/test_fourth_project.py
@@ -0,0 +1,39 @@
+# _____ ti__
+# _____ u___
+# f___ fourth_project _____ FibonacciSequence
+#
+#
+# c_ TestEfficiency u___.T..
+#
+#     ___ setUp ____
+#         ____._fibonacci_sequence _ F..
+#         ____._efficiency_data = di..
+#
+#     ___ test_first_method ____
+#         starting_time = ti__.ti__
+#
+#         ____._fi._s_.recursive_method 20
+#
+#         ending_time = ti__.ti__
+#
+#         ____._e.. 'recursive_method' _ e.._t.. - s.._t..
+#
+#     ___ test_second_method ____
+#         starting_time _ ti__.ti__
+#
+#         ____._fi._s_.math_method 20
+#
+#         ending_time _ ti__.ti__
+#
+#         ____._e.. 'math_method' _ e.._t.. - s.._t..
+#
+#     ___ tearDown ____
+#         print(____._e._d.
+#         ____._f._s. _ N..
+#         ____._e._d_.cl..
+#
+#
+# __ _______ __ ______
+#     u___.m..
+#
+#
