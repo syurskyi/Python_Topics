f634d58daee347949a63935cfb227ca973391353
syurskyi <sergejyurskyj@yahoo.com>
2019-12-12 09:55:41 -0800

    Update example1.py

--- 070_oop/001_classes/_exercises/templates/ITVDN_Python_Advanced/006_Типизированный Python/example1.py
+++ 070_oop/001_classes/_exercises/templates/ITVDN_Python_Advanced/006_Типизированный Python/example1.py
@@ -1,19 +1,20 @@
-# untyped
-value = 10
-
-
-class User:
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
-
-
-def create_new_user(first_name, last_name):
-    # неоднозначность типов и преобразований
-    # first_name.???
-    print(first_name)
-    return User(first_name=first_name, last_name=last_name)
-
-
-# user1 = create_new_user(value, value)
-user2 = create_new_user('Test1', 'Test2')
+# # -*- coding: utf-8 -*-
+#
+# # untyped
+# value = 10
+#
+# c_ User
+#     ___ - ____, first_name, last_name
+#         ____.f.. _ f..
+#         ____.l.. _ l..
+#
+#
+# ___ create_new_user first_name, last_name
+#     # неоднозначность типов и преобразований
+#     # first_name.???
+#     print(first_name)
+#     r_  U.. f..._? l....._?
+#
+#
+# # user1 = c...(value, value)
+# user2 = c...('Test1', 'Test2')
