6ea7f3d56390ef0acb36807479756d50ca64581a
syurskyi <sergejyurskyj@yahoo.com>
2019-12-12 13:50:10 -0800

    Create challenge.py

---
+++ 115_testing/_exercises/templates/The_Ultimate_Python_Unit_Testing_Course/Section 4 Coding Challenge #1 - Testing Functions/challenge.py
@@ -0,0 +1,23 @@
+# """Coding Challenge Skeleton #1
+# This counter function purpose is to count how many english letters does your name contain.
+# After writing your tests, develop the counter function as needed to pass all your tests.
+#
+# """
+#
+#
+# ___ counter name
+#
+#     i_ n.. __ N..
+#         r... E.. You can't input None values
+#
+#     name = n___.re.. ' ', ''
+#     i_ len(name) __ 0:
+#         r... E.. Please input your name
+#
+#     i_ name.isalpha
+#         r_ le. n..
+#     e____
+#         r... E.. The string must contain english letters
+#
+# #
+# # print(counter('Sergej'))
