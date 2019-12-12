94c4156b1ec2dc3834944f6ac33243c210ed4b9c
syurskyi <sergejyurskyj@yahoo.com>
2019-12-12 10:34:21 -0800

    Update example9.py

--- 070_oop/001_classes/_exercises/templates/ITVDN_Python_Advanced/006_Типизированный Python/example9.py
+++ 070_oop/001_classes/_exercises/templates/ITVDN_Python_Advanced/006_Типизированный Python/example9.py
@@ -1,22 +1,22 @@
-from typing import TypeVar, Dict
-
-T = TypeVar('T')
-
-TemplateDict = Dict[str, T]
-IntDict = TemplateDict[int]  # Dict[str, int]
-StrDict = TemplateDict[str]  # Dict[str, str]
-
-
-def handler1(value: TemplateDict[T]) -> T:
-    return list(value.values())[0]
-
-
-def handler2(value: IntDict) -> int:
-    return list(value.values())[0]
-
-
-value1: int = handler1({'test': 10})
-value2: str = handler1({'test': 'str'})
-value3: list = handler1({'test': []})
-value4: int = handler2({'test': 10})
-# value5: int = handler2({'test': 'str'})
+# f__ ty.. _____ T.., D..
+# 
+# T _ T.. 'T'
+# 
+# TemplateDict _ D.. s.. T
+# IntDict _ Te_D i..  # Dict[str, int]
+# StrDict _ Te.D. s..  # Dict[str, str]
+# 
+# 
+# ___ handler1 value T_D_ T __ T
+#     r_ li.. v__.v.. 0
+# 
+# 
+# ___ handler2 value I_D_ __ i..
+#     r_ li.. v__.v..   0
+# 
+# 
+# value1 i.. _ _1({'test': 10})
+# value2 s.. _ _1({'test': 'str'})
+# value3 li.. _ _1({'test': []})
+# value4 i.. _ _2({'test': 10})
+# # value5: int = handler2({'test': 'str'})
