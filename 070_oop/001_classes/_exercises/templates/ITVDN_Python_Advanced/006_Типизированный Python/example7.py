32e372d2d606fc5ac5b23c1dd8a3a0eff5f3ee42
syurskyi <sergejyurskyj@yahoo.com>
2019-12-12 10:24:09 -0800

    Update example7.py

--- 070_oop/001_classes/_exercises/templates/ITVDN_Python_Advanced/006_Типизированный Python/example7.py
+++ 070_oop/001_classes/_exercises/templates/ITVDN_Python_Advanced/006_Типизированный Python/example7.py
@@ -1,18 +1,18 @@
-from typing import Callable
-
-
-def handler(name: str, coefficient: int) -> None:
-    print(name)
-    print(coefficient)
-
-
-def incorrect_handler(value: str, coefficient: int):
-    print(value)
-
-
-def executor(value1: str, value2: int, callback: Callable[[str, int], None]):
-    callback(value1, value2)
-
-
-executor("Test string", 10, handler)
-executor("Test string", 10, incorrect_handler)
+# f__ ty.. ______ Ca..
+#
+#
+# ___ handler name s.. coefficient i.. __ N..
+#     print(name)
+#     print(coefficient)
+#
+#
+# ___ incorrect_handler value s.. coefficient i..
+#     print(value)
+#
+#
+# ___ executor value1 s.. value2 i.. callback C.. s.. i.. N..
+#     callback _1 _2
+#
+#
+# e..("Test string", 10, h..)
+# e..("Test string", 10, i..)
