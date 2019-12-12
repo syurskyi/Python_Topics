870f0c59a893085a84bb867441852036899c28d0
syurskyi <sergejyurskyj@yahoo.com>
2019-12-12 10:28:58 -0800

    Update example8.py

--- 070_oop/001_classes/_exercises/templates/ITVDN_Python_Advanced/006_Типизированный Python/example8.py
+++ 070_oop/001_classes/_exercises/templates/ITVDN_Python_Advanced/006_Типизированный Python/example8.py
@@ -1,36 +1,36 @@
-from typing import Union, Tuple, Iterable
-
-
-def handler1(name: str, coefficient: Union[float, str]) -> None:
-    print(name)
-    print(coefficient)
-
-
-def handler2(name: str, coefficient: Tuple[Union[float, str], ...]) -> None:
-    print(name)
-    print(coefficient)
-
-
-def handler3(name: str, coefficient: Iterable[Union[float, str]]) -> None:
-    print(name)
-    print(coefficient)
-
-
-handler1("Test string", 10)
-handler1("Test string", "10")
-handler1("Test string", 10.2)
-# handler1("Test string", [])
-# handler("Test string", {})
-# handler("Test string", ())
-
-
-handler2("Test string", (10,))
-handler2("Test string", ("10", 12))
-handler2("Test string", (10.2, 10))
-handler2("Test string", (10.2, 12.0))
-
-handler3("Test string", (10,))
-handler3("Test string", ("10", 12))
-handler3("Test string", [10.2, 10])
-handler3("Test string", {10.2, 12.0})
-handler3("Test string", "test")
+# f___ ty.. _____ U..., T..., I...
+# 
+# 
+# ___ handler1 name s.. coefficient U... fl.. s.. __ N...
+#     print(n..)
+#     print(c..)
+# 
+# 
+# ___ handler2 name s.. coefficient T... U... fl.. s..  ... __ N...
+#     print(n..)
+#     print(c..)
+# 
+# 
+# ___ handler3 name s.. coefficient I... U... fl.. s.. __ N...
+#     print(n..)
+#     print(c..)
+# 
+# 
+# handler1("Test string", 10)
+# handler1("Test string", "10")
+# handler1("Test string", 10.2)
+# # handler1("Test string", [])
+# # handler("Test string", {})
+# # handler("Test string", ())
+# 
+# 
+# handler2("Test string", (10,))
+# handler2("Test string", ("10", 12))
+# handler2("Test string", (10.2, 10))
+# handler2("Test string", (10.2, 12.0))
+# 
+# handler3("Test string", (10,))
+# handler3("Test string", ("10", 12))
+# handler3("Test string", [10.2, 10])
+# handler3("Test string", {10.2, 12.0})
+# handler3("Test string", "test")
