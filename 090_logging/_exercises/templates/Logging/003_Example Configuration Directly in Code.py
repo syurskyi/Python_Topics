4ec8932242073e1425132deed83119faa90ce7d1
syurskyi <sergejyurskyj@yahoo.com>
2019-12-13 10:28:06 -0800

    Update 003_Example Configuration Directly in Code.py

--- 090_logging/_exercises/templates/Logging/003_Example Configuration Directly in Code.py
+++ 090_logging/_exercises/templates/Logging/003_Example Configuration Directly in Code.py
@@ -1,11 +1,11 @@
-______ l____
-
-logger = l____.getLogger()
-handler = l____.StreamHandler()
-formatter = l____.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(l____.DEBUG)
-
-logger.debug('often makes a very good meal of %s', 'visiting tourists')
+# ______ l____
+# 
+# logger _ l____.gL_
+# handler _ l____.SH_
+# formatter _ l____.F__(
+#         '_ asctime _ _ name -12s _ levelname -8s _ message s')
+# h__.sF_ f__
+# l__.aH_ h..
+# l__.sL_ l____.D..
+# 
+# l__.d.. 'often makes a very good meal of %s', 'visiting tourists'
