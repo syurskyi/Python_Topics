71cae9558639e194c644a69cb570c9debca7d1c8
syurskyi <sergejyurskyj@yahoo.com>
2019-12-13 10:21:46 -0800

    Update 001_Example Configuration via an INI File.py

--- 090_logging/_exercises/templates/Logging/001_Example Configuration via an INI File.py
+++ 090_logging/_exercises/templates/Logging/001_Example Configuration via an INI File.py
@@ -1,6 +1,6 @@
-______ l____
-from l____.config ______ fileConfig
-
-fileConfig('logging_config.ini')
-logger = l____.getLogger()
-logger.debug('often makes a very good meal of %s', 'visiting tourists')
+# ______ l____
+# from l____.config ______ fC_
+# 
+# fC_ 'logging_config.ini'
+# logger = l____.gL_
+# ?.debug 'often makes a very good meal of @', 'visiting tourists'
