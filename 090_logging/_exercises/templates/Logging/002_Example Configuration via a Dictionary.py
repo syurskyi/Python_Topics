5a6675e45813f31e58516d1209260f2766032f35
syurskyi <sergejyurskyj@yahoo.com>
2019-12-13 10:25:10 -0800

    Update 002_Example Configuration via a Dictionary.py

--- 090_logging/_exercises/templates/Logging/002_Example Configuration via a Dictionary.py
+++ 090_logging/_exercises/templates/Logging/002_Example Configuration via a Dictionary.py
@@ -1,24 +1,24 @@
-______ l____
-from l____.config ______ dictConfig
-
-logging_config = dict(
-    version = 1,
-    formatters = {
-        'f': {'format':
-              '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'}
-        },
-    handlers = {
-        'h': {'class': 'logging.StreamHandler',
-              'formatter': 'f',
-              'level': l____.DEBUG}
-        },
-    root = {
-        'handlers': ['h'],
-        'level': l____.DEBUG,
-        },
-)
-
-dictConfig(logging_config)
-
-logger = l____.getLogger()
-logger.debug('often makes a very good meal of %s', 'visiting tourists')
+# ______ l____
+# from l____.config ______ dC_
+# 
+# logging_config _ di..(
+#     ve.. _ 1,
+#     fo.. _ {
+#         '_': {'format':
+#               '_ asctime _ _ name -12s _ levelname -8s _ message _'}
+#         },
+#     han.. _ {
+#         'h': {'class': 'logging.StreamHandler',
+#               'formatter': 'f',
+#               'level': l____.D..}
+#         },
+#     ro.. _ {
+#         'handlers': ['h'],
+#         'level': l____.D..,
+#         },
+# )
+# 
+# dC_ ?
+# 
+# logger = l____.gL_
+# ?.d.. 'often makes a very good meal of @', 'visiting tourists'
