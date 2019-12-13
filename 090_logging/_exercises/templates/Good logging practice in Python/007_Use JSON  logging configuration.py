2f4636da48005ef8d1315f2f25b8cfea8368d1aa
syurskyi <sergejyurskyj@yahoo.com>
2019-12-13 10:12:37 -0800

    Update 007_Use JSON  logging configuration.py

--- 090_logging/_exercises/templates/Good logging practice in Python/007_Use JSON  logging configuration.py
+++ 090_logging/_exercises/templates/Good logging practice in Python/007_Use JSON  logging configuration.py
@@ -1,29 +1,29 @@
-# Use JSON or YAML logging configuration
-# You can configure your logging system in Python code, but then you need to modify your code whenever you want
-# to change the log configuration. Another way to do it is to use a logging configuration file. After Python 2.7,
-# you can load logging configuration from a dict. It means you can load the logging configuration from a JSON or YAML
-# file. Although you can use the old .ini style logging configuration, it is difficult to read and write.
-# Here let me show you the logging configuration examples in JSON and YAML
-
-______ os
-______ json
-______ l____.config
-
-def setup_logging(
-    default_path='l____.json',
-    default_level=l____.INFO,
-    env_key='LOG_CFG'
-):
-    """Setup l____ configuration
-
-    """
-    path = default_path
-    value = os.getenv(env_key, None)
-    if value:
-        path = value
-    if os.path.exists(path):
-        with open(path, 'rt') as f:
-            config = json.load(f)
-        l____.config.dictConfig(config)
-    else:
-        l____.basicConfig(level=default_level)
+# # Use JSON or YAML logging configuration
+# # You can configure your logging system in Python code, but then you need to modify your code whenever you want
+# # to change the log configuration. Another way to do it is to use a logging configuration file. After Python 2.7,
+# # you can load logging configuration from a dict. It means you can load the logging configuration from a JSON or YAML
+# # file. Although you can use the old .ini style logging configuration, it is difficult to read and write.
+# # Here let me show you the logging configuration examples in JSON and YAML
+#
+# ______ __
+# ______ j...
+# ______ l____.c...
+#
+# ___ setup_logging(
+#     default_path_'l____.json',
+#     default_level_l____.I..,
+#     env_key_'LOG_CFG'
+# ):
+#     """Setup logging configuration
+#
+#     """
+#     path _ d..
+#     value = __.g_e_ e.. N..
+#     i_ v...
+#         path _ v..
+#     i_ __.pa__.ex__ p..
+#         w___ o.. p.. __ a_ f
+#             config _ j___.l.. ?
+#         l____.c__.dC_ c..
+#     e__
+#         l____.bC_ le.._d..
