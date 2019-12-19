b33afebca356f100cd1ab6f0fd42fd6c1f0925cc
Grey <sergejyurskyj@yahoo.com>
2019-12-18 21:17:59 -0800

    Committed the list templates

---
+++ 012_lists/_exercises/templates/Learning Python/009_002_References Versus Copies.py
@@ -0,0 +1,34 @@
+__author__ = 'sergejyurskyj'
+
+
+X = [1, 2, 3]
+L = ['a', X, 'b']          # Embed references to X's object
+D = {'x' : X, 'y':2}
+
+print('#' * 52 + ' Changes all three references!')
+
+X[1] = 'surprise'          # Changes all three references!
+print(L)
+
+print(D)
+
+L = [1, 2, 3]
+D = {'a':1, 'b':2}
+
+A = L[:]            # Instead of A = L (or list(L))
+B = D.copy()        # Instead of B = D (ditto for sets)
+
+A[1] = 'Ni'
+B['c'] = 'spam'
+
+print(L, D)
+
+print(A, B)
+
+
+X = [1, 2, 3]
+L = ['a', X[:], 'b']     # Embed copies of X's object
+D = {'x':X[:], 'y':2}
+
+import copy
+X = copy.deepcopy(L) # Fully copy an arbitrarily nested object Y
