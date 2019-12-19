827dae258c260c6e067732252248c349b639dfad
Grey <sergejyurskyj@yahoo.com>
2019-12-18 20:13:26 -0800

    Committed 6.6. Functions and methods for working with strings templates

--- 010_strings/_exercises/templates/Python 3 Most Nessesary/6.6. Functions and methods for working with strings.py
+++ 010_strings/_exercises/templates/Python 3 Most Nessesary/6.6. Functions and methods for working with strings.py
@@ -1,123 +1,123 @@
-# -*- coding: utf-8 -*-
-
-print(str(), str([1, 2]), str((3, 4)), str({"x": 1}))
-# ('', '[1, 2]', '(3, 4)', "{'x': 1}")
-print("строка1\nстрока2")
-# строка1
-# строка2
-
-
-print(repr("Строка"), repr([1, 2, 3]), repr({"x": 5}))
-# ("'Строка'", '[1, 2, 3]', "{'x': 5}")
-print(repr("строка1\nстрока2"))
-# "'строка1\\nстрока2'"
-
-
-print(ascii([1, 2, 3]), ascii({"x": 5}))
-# ('[1, 2, 3]', "{'x': 5}")
-print(ascii("строка"))
-# "'\\u0441\\u0442\\u0440\\u043e\\u043a\\u0430'"
-
-
-print(len("Python"), len("\r\n\t"), len(r"\r\n\t"))
-# (6, 3, 6)
-print(len("строка"))
-# 6
-
-
-s1, s2 = "     str\n\r\v\t", "strstrstrokstrstrstr"
-print("'%s' — '%s'" % (s1.strip(), s2.strip("tsr")))
-# "'str' — 'ok'"
-
-
-s1, s2 = "     str     ", "strstrstrokstrstrstr"
-print("'%s' — '%s'" % (s1.lstrip(), s2.lstrip("tsr")))
-# "'str     ' — 'okstrstrstr'"
-
-s1, s2 = "     str     ", "strstrstrokstrstrstr"
-print("'%s' — '%s'" % (s1.rstrip(), s2.rstrip("tsr")))
-# "'     str' — 'strstrstrok'"
-
-
-s = "word1 word2 word3"
-print(s.split(), s.split(None, 1))
-# (['word1', 'word2', 'word3'], ['word1', 'word2 word3'])
-s = "word1\nword2\nword3"
-print(s.split("\n"))
-# ['word1', 'word2', 'word3']
-
-
-s = "word1           word2 word3     "
-print(s.split())
-# ['word1', 'word2', 'word3']
-
-
-s = ",,word1,,word2,,word3,,"
-print(s.split(","))
-# ['', '', 'word1', '', 'word2', '', 'word3', '', '']
-print("1,,2,,3".split(","))
-# ['1', '', '2', '', '3']
-
-
-print("word1 word2 word3".split("\n"))
-# ['word1 word2 word3']
-
-
-s = "word1 word2 word3"
-print(s.rsplit(), s.rsplit(None, 1))
-# (['word1', 'word2', 'word3'], ['word1 word2', 'word3'])
-print("word1\nword2\nword3".rsplit("\n"))
-# ['word1', 'word2', 'word3']
-
-
-print("word1\nword2\nword3".splitlines())
-# ['word1', 'word2', 'word3']
-print("word1\nword2\nword3".splitlines(True))
-# ['word1\n', 'word2\n', 'word3']
-print("word1\nword2\nword3".splitlines(False))
-# ['word1', 'word2', 'word3']
-print("word1 word2 word3".splitlines())
-# ['word1 word2 word3']
-
-
-print("word1 word2 word3".partition(" "))
-# ('word1', ' ', 'word2 word3')
-print("word1 word2 word3".partition("\n"))
-# ('word1 word2 word3', '', '')
-
-
-print("word1 word2 word3".rpartition(" "))
-# ('word1 word2', ' ', 'word3')
-print("word1 word2 word3".rpartition("\n"))
-# ('', '', 'word1 word2 word3')
-
-
-print(" => ".join(["word1", "word2", "word3"]))
-# 'word1 => word2 => word3'
-print(" ".join(("word1", "word2", "word3")))
-# 'word1 word2 word3'
-
-
-" ".join(("word1", "word2", 5))
-# Traceback (most recent call last):
-#   File "<pyshell#48>", line 1, in <module>
-#     " ".join(("word1", "word2", 5))
-# TypeError: sequence item 2: expected str instance, int found
-
-
-s = "Python"
-arr = list(s); arr        # Преобразуем строку в список
-# ['P', 'y', 't', 'h', 'o', 'n']
-arr[0] = "J"; arr         # Изменяем элемент по индексу
-# ['J', 'y', 't', 'h', 'o', 'n']
-s = "".join(arr); s       # Преобразуем список в строку
-# 'Jython'
-
-
-s = "Python"
-b = bytearray(s, "cp1251"); b
-# bytearray(b'Python')
-b[0] = ord("J"); b
-# bytearray(b'Jython')
-s = b.decode("cp1251"); s
-# 'Jython'
+# # -*- coding: utf-8 -*-
+#
+# print( s.. ,  s.. 1, 2,  s.. 3, 4,  s.. "x" 1
+# # ('', '[1, 2]', '(3, 4)', "{'x': 1}")
+# print("строка1\nстрока2")
+# # строка1
+# # строка2
+#
+#
+# print(r.. Строка", r.. 1, 2, 3, r.. "x" 5
+# # ("'Строка'", '[1, 2, 3]', "{'x': 5}")
+# print(r..("строка1\nстрока2"))
+# # "'строка1\\nстрока2'"
+#
+#
+# print(as.. 1, 2, 3, as.. "x" 5
+# # ('[1, 2, 3]', "{'x': 5}")
+# print(as.. строка
+# # "'\\u0441\\u0442\\u0440\\u043e\\u043a\\u0430'"
+#
+#
+# print(le. Python, le. "\r\n\t" le. r"\r\n\t"
+# # (6, 3, 6)
+# print(le. строка
+# # 6
+#
+#
+# s1, s2 = "     str\n\r\v\t", "strstrstrokstrstrstr"
+# print("'__' — '__'"  _1.st.., _2.st.."tsr"
+# # "'str' — 'ok'"
+#
+#
+# s1, s2 = "     str     ", "strstrstrokstrstrstr"
+# print("'__' — '__'"  _1.ls.. _2.ls.."tsr"
+# # "'str     ' — 'okstrstrstr'"
+#
+# s1, s2 = "     str     ", "strstrstrokstrstrstr"
+# print("'__' — '__'"  _1.rs.., _2.rs.."tsr"
+# # "'     str' — 'strstrstrok'"
+#
+#
+# s = "word1 word2 word3"
+# print ?.sp..  ?.sp.. N.. 1
+# # (['word1', 'word2', 'word3'], ['word1', 'word2 word3'])
+# s = "word1\nword2\nword3"
+# print ?.sp.. "\n"
+# # ['word1', 'word2', 'word3']
+#
+#
+# s = "word1           word2 word3     "
+# print ?.sp..
+# # ['word1', 'word2', 'word3']
+#
+#
+# s = ",,word1,,word2,,word3,,"
+# print(?.sp..","
+# # ['', '', 'word1', '', 'word2', '', 'word3', '', '']
+# print "1,,2,,3".sp.. ","
+# # ['1', '', '2', '', '3']
+#
+#
+# print("word1 word2 word3".sp.. "\n"
+# # ['word1 word2 word3']
+#
+#
+# s = "word1 word2 word3"
+# print ?.rs.. ?.rs.. N.. 1
+# # (['word1', 'word2', 'word3'], ['word1 word2', 'word3'])
+# print("word1\nword2\nword3".rs.. "\n"
+# # ['word1', 'word2', 'word3']
+#
+#
+# print("word1\nword2\nword3".s.l.
+# # ['word1', 'word2', 'word3']
+# print("word1\nword2\nword3".s.l. T..
+# # ['word1\n', 'word2\n', 'word3']
+# print("word1\nword2\nword3".s.l. F..
+# # ['word1', 'word2', 'word3']
+# print("word1 word2 word3".s.l.
+# # ['word1 word2 word3']
+#
+#
+# print("word1 word2 word3".pa.." "
+# # ('word1', ' ', 'word2 word3')
+# print("word1 word2 word3".pa.. "\n"
+# # ('word1 word2 word3', '', '')
+#
+#
+# print("word1 word2 word3".rpa.. " "
+# # ('word1 word2', ' ', 'word3')
+# print("word1 word2 word3".rpa.."\n"
+# # ('', '', 'word1 word2 word3')
+#
+#
+# print(" => ".j.. "word1" "word2" "word3"
+# # 'word1 => word2 => word3'
+# print(" ".j.. "word1" "word2" "word3"
+# # 'word1 word2 word3'
+#
+#
+# # " ".join(("word1", "word2", 5
+# # Traceback (most recent call last):
+# #   File "<pyshell#48>", line 1, in <module>
+# #     " ".join(("word1", "word2", 5))
+# # TypeError: sequence item 2: expected str instance, int found
+#
+#
+# s = "Python"
+# arr = li.. s; print ?        # Преобразуем строку в список
+# # ['P', 'y', 't', 'h', 'o', 'n']
+# ? 0 = "J"; print ?       # Изменяем элемент по индексу
+# # ['J', 'y', 't', 'h', 'o', 'n']
+# s = "".j.. ?; print ?       # Преобразуем список в строку
+# # 'Jython'
+#
+#
+# s = "Python"
+# b = b.a. ?  cp1251 ; print ?
+# # bytearray(b'Python')
+# ? 0 = o.. "J"; print ?
+# # bytearray(b'Jython')
+# s = ?.de..  cp1251 ; print ?
+# # 'Jython'
