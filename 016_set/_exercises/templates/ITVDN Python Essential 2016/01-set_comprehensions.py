5c98cebb167136cc5a4b00821929dd9f5a210ea4
Grey <sergejyurskyj@yahoo.com>
2019-12-19 20:27:07 -0800

    Committed update for the set templates

--- 016_set/_exercises/templates/ITVDN Python Essential 2016/01-set_comprehensions.py
+++ 016_set/_exercises/templates/ITVDN Python Essential 2016/01-set_comprehensions.py
@@ -1,10 +1,12 @@
-﻿"""Пример создания множеств при помощи включений множеств
-(аналогично списковым включениям).
-Эти объекты также имеют тип set
-"""
-
-number_set = {i + j for i in range(10) for j in range(5)}
-print(number_set)
-
-char_set = {x for x in 'abracadabra' if x not in 'abc'}
-print(char_set)
+﻿# # -*- coding: utf-8 -*-
+#
+# """Пример создания множеств при помощи включений множеств
+# (аналогично списковым включениям).
+# Эти объекты также имеют тип set
+# """
+#
+# number_set = i + j __ ? __ ra.. 10 __ j __ ra.. 5
+# print ?
+#
+# char_set = x ___ ? __ 'abracadabra' i_ ? ___ __ 'abc'
+# print ?
--- 016_set/_exercises/templates/ITVDN Python Essential 2016/02-set_constructor.py
+++ 016_set/_exercises/templates/ITVDN Python Essential 2016/02-set_constructor.py
@@ -1,24 +1,26 @@
-﻿"""Использование конструкторов"""
-
-# Пустое множество
-empty_set = set()
-print(empty_set)
-
-# Пустое неизменяемое множество
-empty_frozenset = frozenset()
-print(empty_frozenset)
-
-# Создание множеств из итерабельых объектов
-
-my_frozenset = frozenset([4, 1, 3, 8])
-my_set = set(my_frozenset)
-print(my_set)
-print(my_frozenset)
-
-def get_ints(n):
-    for i in range(n):
-        yield i
-        yield i + 1
-
-print(list(get_ints(10)))  # все числа, возвращаемые генератором
-print(set(get_ints(10)))   # множество содержит лишь уникальные значения
+﻿# # -*- coding: utf-8 -*-
+#
+# """Использование конструкторов"""
+#
+# # Пустое множество
+# empty_set _
+# print ?
+#
+# # Пустое неизменяемое множество
+# empty_frozenset =
+# print ?
+#
+# # Создание множеств из итерабельых объектов
+#
+# my_frozenset = f.. 4, 1, 3, 8
+# my_set = s.. ?
+# print ?
+# print my_f..
+#
+# ___ get_ints n
+#     ___ i __ ra.. n
+#         y___ ?
+#         y___ ? + 1
+#
+# print li.. ? 10  # все числа, возвращаемые генератором
+# print se. ? 10   # множество содержит лишь уникальные значения
