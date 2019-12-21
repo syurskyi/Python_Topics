742ee9eeacd094c08292caf7643bc4ae21ba350f
Grey <sergejyurskyj@yahoo.com>
2019-12-21 13:37:00 -0800

    Committed the new range exercises

---
+++ 030_control_flow/004_range/_exercises/templates/Python 3 Most Nessesary/4.4.Listing 4.10. An example of using the enumerate () function.py
@@ -0,0 +1,19 @@
+# # -*- coding: utf-8 -*-
+#
+# arr = [1, 2, 3, 4, 5, 6]
+# ___ i, elem __ en.. ?
+#     __ ? % 2 __ 0
+#         ? ? *_ 2
+# print ?                # Результат выполнения: [1, 4, 3, 8, 5, 12]
+#
+# arr = [1, 2]
+# obj _ en.. ? st.. _ 2
+# print ne.. ?
+# # (2, 1)
+# print ne.. ?
+# # (3, 2)
+# # next(obj)
+# # Traceback (most recent call last):
+# #   File "<pyshell#10>", line 1, in <module>
+# #     next(obj)
+# # StopIteration
