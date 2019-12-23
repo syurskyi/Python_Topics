51e8bf8051247809aaf885248ea09eb656c13627
syurskyi <sergejyurskyj@yahoo.com>
2019-12-23 12:42:37 -0800

    Update 007_Extended slicing The third limit and slice objects.py

--- 010_strings/_exercises/templates/Learning Python/007_Extended slicing The third limit and slice objects.py
+++ 010_strings/_exercises/templates/Learning Python/007_Extended slicing The third limit and slice objects.py
@@ -1,15 +1,15 @@
 # S = 'abcdefghijklmnop'
 # print('#' * 52 + ' Skipping items')
-# print S 1 10 2    # Skipping items
-# print S 2
+# print ? 1 10 2    # Skipping items
+# print ? 2
 #
 # S = 'hello'
 # print('#' * 52 + ' Reversing items')
-# print S -1    # Reversing items
+# print ? -1    # Reversing items
 #
 # S = 'abcedfg'
 # print('#' * 52 + ' Bounds roles differ')
-# print S 5 1-1    # Bounds roles differ
+# print ? 5 1-1    # Bounds roles differ
 #
 # print('#' * 52 + ' Slicing syntax')
 # print 'spam' 1 3    # Slicing syntax
