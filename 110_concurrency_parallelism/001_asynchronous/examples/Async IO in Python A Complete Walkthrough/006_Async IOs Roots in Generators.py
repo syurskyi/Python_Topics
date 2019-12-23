1c0e13a7d46b88a999f53fc45c46978369164789
Grey <sergejyurskyj@yahoo.com>
2019-12-22 21:21:12 -0800

    Committed the new folder Async with examples

---
+++ 110_concurrency_parallelism/001_asynchronous/examples/Async IO in Python A Complete Walkthrough/006_Async IOs Roots in Generators.py
@@ -0,0 +1,17 @@
+import asyncio
+
+@asyncio.coroutine
+def py34_coro():
+    """Generator-based coroutine"""
+    # No need to build these yourself, but be aware of what they are
+    s = yield from stuff()
+    return s
+
+async def py35_coro():
+    """Native coroutine, modern syntax"""
+    s = await stuff()
+    return s
+
+async def stuff():
+    return 0x10, 0x20, 0x30
+
