db9ea351e0c2ccc687828654ff8176b330913f3a
syurskyi <sergejyurskyj@yahoo.com>
2019-12-24 12:10:14 -0800

    Update 017_String Formatting Expressions.py

--- 010_strings/_exercises/templates/Learning Python/017_String Formatting Expressions.py
+++ 010_strings/_exercises/templates/Learning Python/017_String Formatting Expressions.py
@@ -1,4 +1,4 @@
 # print 'That is @ @ bird!'   1 'dead'  # Format expression
 # print 'The knights who say @!'  'exclamation'  # String substitution
-# print '@ @ %g you'   1, 'spam', 4.0  # Type-specific substitutions
-# print '@ -- @ -- @'   42, 3.14159, [1, 2, 3  # All types match a %s target
+# print '@ @ @ you'   1, 'spam', 4.0  # Type-specific substitutions    | - эквивалентно f или е 
+# print '@ -- @ -- @'   42, 3.14159, |1, 2, 3  # All types match a %s target  |list
