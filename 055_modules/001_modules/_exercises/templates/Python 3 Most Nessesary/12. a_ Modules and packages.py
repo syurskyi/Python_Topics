a96ab2262894bacfaa5ebd90f30eb19b46b8792c
Grey <sergejyurskyj@yahoo.com>
2019-12-25 10:52:05 -0800

    Committed the new module exercises

---
+++ 055_modules/001_modules/_exercises/templates/Python 3 Most Nessesary/12. a_ Modules and packages.py
@@ -0,0 +1,3 @@
+# # -*- coding: utf-8 -*-
+#
+# print -n                   # Выведет: __main__
---
+++ 055_modules/001_modules/_exercises/templates/Python 3 Most Nessesary/12.3.Listing 12.18. Modifying the list of module search paths.py
@@ -0,0 +1,9 @@
+# # -*- coding: utf-8 -*-
+# _______ ___
+# ___.pa__.ap.. _"C:\folder1"          # Добавляем в конец списка
+# ___.pa__.in..  0 _"C:\folder2"       # Добавляем в начало списка
+# print(___.pa__
+# input()
+#
+#
+# ___.pa__.append("C:\\folder1\\folder2\\folder3")
