38fc8ef4f050fad61eb897a59f7aaf7464f22320
syurskyi <sergejyurskyj@yahoo.com>
2019-12-26 15:36:32 -0800

    Update 023_Adding Keys, Attributes, and Offsets.py

--- 010_strings/_exercises/templates/Learning Python/023_Adding Keys, Attributes, and Offsets.py
+++ 010_strings/_exercises/templates/Learning Python/023_Adding Keys, Attributes, and Offsets.py
@@ -1,9 +1,9 @@
 # _____ ___
-# print('My _ kind runs _.platform'.f.... ___ 'kind' 'laptop'
-# print('My map kind runs ___.platform'.f...  ___ _ ___ m..  _ 'kind' 'laptop'
+# print('My _ |kind runs _.platform'.f.... ___ 'kind' 'laptop'     # dict
+# print('My |map kind runs ___.platform'.f...  ___ _ ___ m..  _ 'kind' 'laptop'
 # somelist = li.. 'SPAM'
 # print(somelist)
-# print('first _ 0 0 third _ 0 2'.f... s..
-# print('first_@ last_@'.f.. s.. 0 s.. -1 # [-1] fails in fmt
-# parts = s.. 0 s.. -1 s.. 1 3  # [1:3] fails in fmt
-# print('first_@ last_@ middle_@'.f.. @? # Or '{}' in 2.7/3.1+
+# print('first _ 0 0 third _ 0 2'.f... ?
+# print('first_@ last_@'.f.. s.. 0 ? -1 # [-1] fails in fmt
+# parts = ? 0 ? -1 ? 1 3  # [1:3] fails in fmt
+# print('first_@ last_@ middle_@'.f.. $? # Or '{}' in 2.7/3.1+
