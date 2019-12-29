76a58c3b4930d04f1d580971e42f4274fee0b983
Grey <sergejyurskyj@yahoo.com>
2019-12-29 15:46:04 -0800

    Committed while exercises

--- 030_control_flow/002_while/_exercises/ITVDN Python Starter 2016/04-break.py
+++ 030_control_flow/002_while/_exercises/ITVDN Python Starter 2016/04-break.py
@@ -1,8 +1,8 @@
-# # -*- coding: utf-8 -*-
-#
-# # более простой пример
-# _____ T..
-#     print('Введите exit, чтобы завершить цикл')
-#     response _ in.. ('> ')
-#     __ ? __ 'exit':
-#         b..
+# -*- coding: utf-8 -*-
+
+# более простой пример
+while True:
+    print('Введите exit, чтобы завершить цикл')
+    response = input('> ')
+    if response == 'exit':
+        break
