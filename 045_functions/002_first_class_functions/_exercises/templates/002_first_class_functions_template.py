482d1aac6597a0d345471654787a50aa39d42f95
syurskyi <sergejyurskyj@yahoo.com>
2019-12-31 15:25:18 -0800

    Update 002_first_class_functions_template.py

--- 045_functions/002_first_class_functions/_exercises/templates/002_first_class_functions_template.py
+++ 045_functions/002_first_class_functions/_exercises/templates/002_first_class_functions_template.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # functions_are_objects
 
 # ___ my_function
@@ -12,7 +14,7 @@
 # ___ my_function
 #     print I am a function
 #
-# test  my_function
+# test _ my_function
 # test
 #
 # # print()
@@ -21,7 +23,7 @@
 
 # ___ call_passed_function incoming
 #     print Calling!
-#     i_()
+#     ?()
 #     print Called!
 
 # call_passed_function my_function
@@ -30,7 +32,7 @@
 # ######################################################################################################################
 # You can check if something could be called
 
-# print(c_(l_), c_ 45 c_(c_))
+# print(c_(le_ , c_ 45, c_(c_
 
 # print()
 # ######################################################################################################################
@@ -40,7 +42,7 @@
 #     r_ min
 
 # test = return_min_function__
-# min_value = t_  4 5 -9 12
+# min_value = ? 4 5 -9 12
 # print Min values is_  min_value
 
 # print()
@@ -73,28 +75,28 @@
 # ######################################################################################################################
 # Functions and Methods are callable
 
-# ca_ print
-# ca_ l_
+# print ca_ print
+# print ca_ l_
 # l = [1, 2, 3]
-# ca_ l.a_
+# print ca_ l.a_
 # s = 'abc'
-# ca_ s.u_
+# print ca_ s.u_
 #
 # print()
 # ######################################################################################################################
 # Callables always return a value:
 
 # result _ print hello
-# print result
+# print ?
 #
 # l = [1, 2, 3]
-# result _ l.a_ 4
-# print result
-# print l
+# result _ ?.a_ 4
+# print ?
+# print ?
 #
 # s _ abc
-# result _ s.u_
-# print result
+# result _ ?.u_
+# print ?
 #
 # print()
 # ######################################################################################################################
@@ -103,7 +105,7 @@
 # f_ d_ i_ D_
 # ca_ D_
 # result _ D_ 10.5
-# print result
+# print ?
 #
 # print()
 # # ######################################################################################################################
@@ -114,15 +116,15 @@
 #         print initializing...'
 #         _.counter = 0
 #
-#     ___ __c_(s_ x_1
+#     ___ -c s_ x_1
 #         _.counter += x
 #         print s_.counter
 #
 # my_obj _ MyClass
-# ca_ my_obj.__i_
-# ca_(my_obj.__c_
-# my_obj()
-# my_obj()
+# ca_ my_obj. -i
+# ca_(my_obj. -c
+# my_obj
+# my_obj
 # my_obj(10)
 #
 # print()
@@ -148,8 +150,8 @@
 #     '-': sub
 # }
 #
-# print(oper___['+'](2, 3))
-# print(oper___['-'](2, 3))
+# print(?['+'](2, 3))
+# print(?['-'](2, 3))
 #
 # print()
 
@@ -159,7 +161,7 @@
 
 
 # ___ shout text
-#     r_ t_*u____
+#     r_ ? *u____
 #
 #
 # print shout__Hello__
