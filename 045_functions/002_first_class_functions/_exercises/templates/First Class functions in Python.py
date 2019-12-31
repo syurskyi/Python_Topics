a2ba08dc3d3bf1bf02fa6022d8dcfb2136c7ac22
syurskyi <sergejyurskyj@yahoo.com>
2019-12-31 14:07:54 -0800

    Create First Class functions in Python.py

---
+++ 045_functions/002_first_class_functions/_exercises/templates/First Class functions in Python.py
@@ -0,0 +1,50 @@
+# # Python program to illustrate functions
+# # can be treated as objects
+#
+#
+# ___ shout text
+#     r_ ?.up..
+#
+#
+# print ? Hello
+#
+# yell = shout
+#
+# print ? Hello
+#
+#
+# # Python program to illustrate functions
+# # can be passed as arguments to other functions
+# ___ shout text
+#     r_ ?.up..
+#
+#
+# ___ whisper text
+#     r_ ?.lo..
+#
+#
+# ___ greet func
+#     # storing the function in a variable
+#     greeting = ? "Hi, I am created by a function passed as an argument."
+#     print ?
+#
+#
+# ? s..
+# ? w..
+#
+#
+# # Python program to illustrate functions
+# # Functions can r_ another function
+#
+# ___ create_adder x
+#     ___ adder y
+#         r_ x + y
+#
+#     r_ ?
+#
+#
+# add_15 = ? 15
+#
+# print ? 10
+#
+#
