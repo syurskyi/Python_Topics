c1d883f2a53f8d08d41c8b4de7d2c6cfbf1355bc
syurskyi <sergejyurskyj@yahoo.com>
2019-12-30 16:55:13 -0800

    Update 008_009_Sorting dictionary keys in 3.X.py

--- 018_dictionaries/_exercises/templates/Learning Python/008_009_Sorting dictionary keys in 3.X.py
+++ 018_dictionaries/_exercises/templates/Learning Python/008_009_Sorting dictionary keys in 3.X.py
@@ -9,13 +9,13 @@
 # print('#' * 52 + ' Force it to be a list and then sort')
 # Ks = li.. ?                           # Force it to be a list and then sort
 # ?.so..
-# ___ k __ ? print ? ? ?
+# ___ k __ ? print ? 1? ?
 #
 # print('#' * 52 + ' Or you can use sorted() on the keys. orted() accepts any iterable , sorted() returns its result')
-# print ?
+# print 1?
 # Ks = ?.k..                           # Or you can use sorted() on the keys
-# ___ k __ so.. ? print ? ? ?     # sorted() accepts any iterable , sorted() returns its result
+# ___ k __ so.. ? print ? 1? ?     # sorted() accepts any iterable , sorted() returns its result
 #
 # print('#' * 52 + ' Better yet, sort the dict directly. dict iterators return keys')
 # print ?                              # Better yet, sort the dict directly
-# ___ k __ so.. ? print ? ? ?      # dict iterators return keys
+# ___ k __ so.. ? print ? 1? ?      # dict iterators return keys
