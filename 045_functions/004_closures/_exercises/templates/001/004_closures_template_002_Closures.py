57f2a3534370532fa73dbf19d42a0ac5bbb5fcc5
syurskyi <sergejyurskyj@yahoo.com>
2020-01-02 13:23:57 -0800

    Update and rename 004_closures_template_002_Замыкания.py to 004_closures_template_002_Closures.py

--- 045_functions/004_closures/_exercises/templates/001/004_closures_template_002_Closures.py
+++ 045_functions/004_closures/_exercises/templates/001/004_closures_template_002_Closures.py
@@ -7,11 +7,11 @@
 #     r_ ?
 #
 # # То же самое, но через безымянные функции:
-# make_adder _ l_ x| _l..... n: x + n)
+# make_adder _ l_ x |l... n x + ?)
 #
-# f _ m.. 10
-# print f 5 # 15
-# print f -1  # 9
+# f _ ? 10
+# print ? 5 # 15
+# print ? -1  # 9
 #print()
 #
 # ___ multiplier n  # multiplier возвращает функцию умножения на n
@@ -20,8 +20,8 @@
 #     r_ ?
 #
 #
-# mul3 _ m.... 3   # mul3 - функция, умножающая на 3
-# print m..3 3  m..3 5
+# mul3 _ ? 3   # mul3 - функция, умножающая на 3
+# print ? 3  ? 5
 #
 # n _ 3
 #
@@ -35,8 +35,8 @@
 # print m.. 5
 #
 # n = 10
-# mult = l____ k_ mul_n| m.. * k
-# print m.. 3
+# mult = l____ k mul_n m.. * k
+# print ? 3
 #
 #
 # ___ outer_func x
