cad0304d2df7636c078fc70a04c258dda9e6fea7
syurskyi <sergejyurskyj@yahoo.com>
2020-01-02 16:51:06 -0800

    Update 004_Thinking Recursively in Python.py

--- 045_functions/006_recursion/_exercises/templates/004_Thinking Recursively in Python.py
+++ 045_functions/006_recursion/_exercises/templates/004_Thinking Recursively in Python.py
@@ -1,9 +1,9 @@
-# houses _ |"Eric's house", "Kenny's house", "Kyle's house", "Stan's house"|
+# houses _ "Eric's house", "Kenny's house", "Kyle's house", "Stan's house"   # list
 #
 #
 # ___ deliver_presents_iteratively
-# ___ house i_ houses
-#         print Delivering presents to*; h...
+#     ___ house __ ?
+#         print Delivering presents to; h...
 #
 # deliver_presents_iteratively
 # # Delivering presents to Eric's house
@@ -16,26 +16,26 @@
 # print('#' * 52 + '  ')
 # print('#' * 52 + '  ')
 #
-# houses = |"Eric's house", "Kenny's house", "Kyle's house", "Stan's house"|
+# houses = "Eric's house", "Kenny's house", "Kyle's house", "Stan's house"  # # list
 #
 # # Each function call represents an elf doing his work
 # ___ deliver_presents_recursively houses
 #     # Worker elf doing his work
-#     i_ l00 houses __ 1
-#         hou00 _ hou000|0|
-#         print Delivering presents to*; h...
+#     __ le. ? __ 1
+#         house _ ? 0
+#         print Delivering presents to ; h...
 #
 #     # Manager elf doing his work
-#     e___
-#         mid _ l00|houses| // 2
-#         first_half _ h...|:mid|
-#         second_half _ h...|mid:|
+#     ____
+#         mid _ le. ? // 2
+#         first_half _ ? ;mid
+#         second_half _ ? mid;
 #
 #         # Divides his work among two elves
-#         d0000 first_half
-#         d0000 second_half
+#         ? first_half
+#         ? second_half
 #
-# d0000 houses
+# ? houses
 # # Delivering presents to Eric's house
 # # Delivering presents to Kenny's house
 # # Delivering presents to Kyle's house
