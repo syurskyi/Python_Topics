4acaeebe2b18c8e59307dc2512adfafdac380ad1
syurskyi <sergejyurskyj@yahoo.com>
2020-01-02 18:01:47 -0800

    Update 7.1. Regular Expression Syntax.py

--- 085_regular_expressions/_exercises/templates/Python 3 Most Nessesary/7.1. Regular Expression Syntax.py
+++ 085_regular_expressions/_exercises/templates/Python 3 Most Nessesary/7.1. Regular Expression Syntax.py
@@ -1,32 +1,33 @@
-import re
-p = re.compile(r"^[а-яе]+$", re.I | re.U)
-print("Найдено" if p.search("АБВГДЕЕ") else "Нет")
-# Найдено
-p = re.compile(r"^[а-яе]+$", re.U)
-print("Найдено" if p.search("АБВГДЕЕ") else "Нет")
-# Нет
-
-
-p = re.compile(r"^.$")
-print("Найдено" if p.search("\n") else "Нет")
-# Нет
-p = re.compile(r"^.$", re.M)
-print("Найдено" if p.search("\n") else "Нет")
-# Нет
-p = re.compile(r"^.$", re.S)
-print("Найдено" if p.search("\n") else "Нет")
-# Найдено
-
-
-p = re.compile(r"""^ # Привязка к началу строки
-              [0-9]+ # Строка должна содержать одну цифру (или более)
-              $      # Привязка к концу строки
-        """, re.X | re.S)
-print("Найдено" if p.search("1234567890") else "Нет")
-# Найдено
-print("Найдено" if p.search("abcd123") else "Нет")
-# Нет
-
-p = re.compile(r"^\w+$")
-
-p = re.compile("^\\w+$")
+# # -*- coding: utf-8 -*-
+# ____ __
+# p = __.c__ _"% а-яе + _", __.I _ __.U  # Starts with
+# print("Найдено" __ ?.se.. АБВГДЕЕ ___ "Нет")
+# # Найдено
+# p = __.c__ _"% а-яе + _", __.U    # Starts with
+# print("Найдено" __ ?.se.. АБВГДЕЕ ____ "Нет")
+# # Нет
+# 
+# 
+# p = __.c__ _"%.%"  # Starts with | Ends with
+# print("Найдено" __ p.se..("\n") ____ "Нет")
+# # Нет
+# p = __.c__ _"%.%", __.M  # Starts with | Ends with
+# print("Найдено" __ p.se..("\n") ____ "Нет")
+# # Нет
+# p = __.c__ _"%.%", __.S  # Starts with | Ends with
+# print("Найдено" __ p.se..("\n") ____ "Нет")
+# # Найдено
+# 
+# 
+# p = __.c__(r"""% # Привязка к началу строки
+#               [0-9]+ # Строка должна содержать одну цифру (или более)
+#               %      # Привязка к концу строки
+#         """, __.X | __.S)
+# print("Найдено" __ p.se.. 1234567890 ____ "Нет")
+# # Найдено
+# print("Найдено" __ p.se.. abcd123 ____ "Нет")
+# # Нет
+# 
+# p = __.c__ _ %%%+%     # Starts with | string contains any word characters | Ends with
+# 
+# p = __.c__ %%%%+%     # Starts with | string contains any word characters | Ends with
