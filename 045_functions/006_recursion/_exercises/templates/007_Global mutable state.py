ad354d69c955d78d31cc436aa965d81678897b39
syurskyi <sergejyurskyj@yahoo.com>
2020-01-02 17:03:59 -0800

    Update 007_Global mutable state.py

--- 045_functions/006_recursion/_exercises/templates/007_Global mutable state.py
+++ 045_functions/006_recursion/_exercises/templates/007_Global mutable state.py
@@ -1,19 +1,19 @@
 # # Global mutable state
-# # current_number = 1
-# # accumulated_sum = 0
-# #
-# #
-# # _ sum_recursive():
-# #     g_ c00_n00
-# #     g_ a00_s00
-# #     # Base case
-# #     i_ c00_n00 __ 11:
-# #         r_ accumulated_sum
-# #     # Recursive case
-# #     e__
-# #         accumulated_sum = accumulated_sum + c00_n00
-# #         c00_n00 = c00_n00 + 1
-# #         r_ s00_r00
-# #
-# # print s00_r00
-# # # 55
+# current_number = 1
+# accumulated_sum = 0
+#
+#
+# ___ sum_recursive
+#     g_ c..
+#     g_ a..
+#     # Base case
+#     __ c.. __ 11:
+#         r_ a..
+#     # Recursive case
+#     ____
+#         a.. = a.. + c..
+#         c.. = c.. + 1
+#         r_ ?
+#
+# print ?
+# # 55
