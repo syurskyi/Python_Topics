9b2943cd20878907dee507711ed805dc406b17dc
syurskyi <sergejyurskyj@yahoo.com>
2020-01-02 17:16:33 -0800

    Update 010_Naive Recursion is Naive.py

--- 045_functions/006_recursion/_exercises/templates/010_Naive Recursion is Naive.py
+++ 045_functions/006_recursion/_exercises/templates/010_Naive Recursion is Naive.py
@@ -1,17 +1,17 @@
 # ___ fibonacci_recursive n
-#     print *Calculating F", "(", n, ")*; s00_""; e00_", "
+#     print *Calculating F", "(", n, ")*; s.._""  e.._", "
 #
 #     # Base case
-#     i_ n __ 0
+#     __ n __ 0
 #         r_ 0
-#     e000 n __ 1
+#     ____ n __ 1
 #         r_ 1
 #
 #     # Recursive case
-#     e_
-#         r_ f00_r00|n-1| + f00_r00|n-2|
+#     ____
+#         r_ ? n-1 + ? n-2
 #
-# print|f00_r00|5||
+# print ? 5
 # # Calculating F(5), Calculating F(4), Calculating F(3), Calculating F(2), Calculating F(1),
 # # Calculating F(0), Calculating F(1), Calculating F(2), Calculating F(1), Calculating F(0),
 # # Calculating F(3), Calculating F(2), Calculating F(1), Calculating F(0), Calculating F(1),
