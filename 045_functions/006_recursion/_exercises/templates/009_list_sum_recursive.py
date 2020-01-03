9b7e02ad9b617ec8b7f0717a02089dcfe2c9f0f3
syurskyi <sergejyurskyj@yahoo.com>
2020-01-02 17:11:38 -0800

    Update 009_list_sum_recursive.py

--- 045_functions/006_recursion/_exercises/templates/009_list_sum_recursive.py
+++ 045_functions/006_recursion/_exercises/templates/009_list_sum_recursive.py
@@ -1,16 +1,16 @@
 # ___ list_sum_recursive input_list
 #     # Base case
-#     i_ input_list __ ||
+#     __ ? __     # list
 #         r_ 0
 #
 #     # Recursive case
 #     # Decompose the original problem into simpler instances of the same problem
 #     # by making use of the fact that the input is a recursive data structure
 #     # and can be deﬁned in terms of a smaller version of itself
-#     e_
-#         head _ i00_l00[0]
-#         smaller_list = i00_l00|1:|
-#         r_ head + l0_s00_r00|s00_l00|
+#     ____
+#         head _ ? 0
+#         smaller_list = ? 1;
+#         r_ h.. + ? sm..
 #
-# print l0_s00_r00 1 2 3
+# print ? 1 2 3
 # # 6
