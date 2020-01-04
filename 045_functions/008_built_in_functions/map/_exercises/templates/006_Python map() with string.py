35be493c4abe979188b313e55374c56fdf859ff9
syurskyi <sergejyurskyj@yahoo.com>
2020-01-03 17:41:06 -0800

    Update 006_Python map() with string.py

--- 045_functions/008_built_in_functions/map/_exercises/templates/006_Python map() with string.py
+++ 045_functions/008_built_in_functions/map/_exercises/templates/006_Python map() with string.py
@@ -1,15 +1,15 @@
-# ___ to_upper_case(s):
-#     return str(s).upper()
+# ___ to_upper_case s
+#     r_ st. ?|.up..
 #
-# ___ print_iterator(it):
-#     ___ x in it:
-#         print(x, end=' ')
+# ___ print_iterator it
+#     ___ x __ ?
+#         print ? e.._' ')
 #     print('')  # for new line
 #
 # # map() with string
-# map_iterator = map(to_upper_case, 'abc')
-# print(type(map_iterator))
-# print_iterator(map_iterator)
+# map_iterator _ m.. t... 'abc'
+# print ty.. ?
+# p.. ?
 #
 # # Output:
 # # <class 'map'>
@@ -18,8 +18,8 @@
 # # Python map() with tuple
 #
 # # map() with tuple
-# map_iterator = map(to_upper_case, (1, 'a', 'abc'))
-# print_iterator(map_iterator)
+# map_iterator = m.. t.. |1, 'a', 'abc'
+# p.. m...
 #
 # # Output:
 # # 1 A ABC
@@ -27,24 +27,24 @@
 # # Python map() with list
 #
 #
-# map_iterator = map(to_upper_case, ['x', 'a', 'abc'])
-# print_iterator(map_iterator)
+# map_iterator = m.. t... |'x', 'a', 'abc'
+# p.. m...
 #
 # # X A ABC
 #
 # # Converting map to list, tuple, set
 #
-# map_iterator = map(to_upper_case, ['a', 'b', 'c'])
-# my_list = list(map_iterator)
-# print(my_list)
+# map_iterator = m.. t.. |'a', 'b', 'c'
+# my_list = li.. ?
+# print ?
 #
-# map_iterator = map(to_upper_case, ['a', 'b', 'c'])
-# my_set = set(map_iterator)
-# print(my_set)
+# map_iterator = m.. t... |'a', 'b', 'c'
+# my_set = se. ?
+# print ?
 #
-# map_iterator = map(to_upper_case, ['a', 'b', 'c'])
-# my_tuple = tuple(map_iterator)
-# print(my_tuple)
+# map_iterator = m.. t... |'a', 'b', 'c'
+# my_tuple = tu.. ?
+# print ?
 #
 # # Output:
 # # ['A', 'B', 'C']
@@ -55,8 +55,8 @@
 #
 # list_numbers = [1, 2, 3, 4]
 #
-# map_iterator = map(lambda x: x * 2, list_numbers)
-# print_iterator(map_iterator)
+# map_iterator = m.. l___ x ? * 2 ?
+# p... ?
 #
 # # Output:
 # # 2 4 6 8
@@ -64,19 +64,19 @@
 # # map() with multiple iterable arguments
 # list_numbers _ 1 2 3 4
 # tuple_numbers _ 5 6 7 8
-# map_iterator _ m00 l.... x y x * y l..._n... t.._n...
-# print_iterator m..._i...
+# map_iterator _ m.. l____ x y x * y ?1 ?2
+# p... ?
 #
 # # Output: 5 12 21 32
 #
 # # map() with multiple iterable arguments of different sizes
 # list_numbers _ 1 2 3 4
 # tuple_numbers _ 5 6 7 8 9 10
-# map_iterator _ m.. l.... x y x * y l...._n... t..._n...
-# print_iterator m..
+# map_iterator _ m.. l____ x y x * y ?1 ?2
+# p... ?
 #
-# map_iterator _ m.. l..... x y x * y t... l....
-# print_iterator(map_iterator)
+# map_iterator _ m.. l___ x y x * y t... l....
+# p... ?
 #
 # # Output:
 # # 5 12 21 32
@@ -85,9 +85,9 @@
 # # Python map() with function None
 #
 # map_iterator _ m.. N... 'abc'
-# printm.._i..
-# ___ x i_ m...........
-#     print x
+# print ?
+# ___ x __ ?
+#     print ?
 #
 # # Output:
 # #
