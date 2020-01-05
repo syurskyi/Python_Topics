a17a49900ed181d2ae95afd48508dabfb85db272
Grey <sergejyurskyj@yahoo.com>
2020-01-04 19:32:47 -0800

    Committed zip exrecises

--- 045_functions/008_built_in_functions/zip/_exercises/templates/018_Function zip_!cool!.py
+++ 045_functions/008_built_in_functions/zip/_exercises/templates/018_Function zip_!cool!.py
@@ -7,7 +7,7 @@
 # # 20 b
 # # 30 c
 # # 40 d
-#
+# print()
 # ___ i __ z_ ? ?
 #     print ? ty..?
 #
@@ -15,7 +15,7 @@
 # # (20, 'b') <class 'tuple'>
 # # (30, 'c') <class 'tuple'>
 # # (40, 'd') <class 'tuple'>
-#
+# _______ it...
 # a _ 10 20 30 40              # list
 # b _ 'a' 'b' 'c' 'd' 'e'      # list
 # c _ 1.1 1.2                  # list
@@ -27,7 +27,7 @@
 # # (30, 'c', None)
 # # (40, 'd', None)
 # # (None, 'e', None)
-#
+# print()
 # ___ i __ i__.z_l.. ? ? ? f..v.._0
 #     print ?
 #
@@ -37,9 +37,11 @@
 # # (40, 'd', 0)
 # # (0, 'e', 0)
 #
+# print()
+#
 # a _ 10 20 30 40         # list
 # c _ 1.1 1.2 1.3 1.4     # list
-# ac _ z_ a c
+# ac _ z_ ? ?
 # print ty.. ?
 # # <class 'zip'>
 # ac _ li.. ?
@@ -47,19 +49,22 @@
 # # <class 'list'>
 # print ?
 # # [(10, 1.1), (20, 1.2), (30, 1.3), (40, 1.4)]
+# print()
 #
 # values _ 1.34 3.25 2.99     # list
 # coefficient _ 3 2 2         # list
-# ___ i, j __ z_ ? ?
+# ___ i j __ z_ ? ?
 #     print ? * ?
 #
 # # 4.0200000000000005
 # # 6.5
 # # 5.98
 #
+# print()
+#
 # a _                         # list
 # b _                         # list
-# ___ i, j __ z_ ra.. 10 20 ra.. 1 10
+# ___ i j __ z_ ra.. 10 20 ra.. 1 10
 #     a.ap.. i
 #
 # b.ap... j
