8f0b8d27132375b4bcc9439c217f5b7b863a4c57
syurskyi <sergejyurskyj@yahoo.com>
2020-01-06 17:04:10 -0800

    Update 018_decorators_template_Decorators with parameters.py

--- 045_functions/005_decorators/_exercises/_templates/1/018_decorators_template_Decorators with parameters.py
+++ 045_functions/005_decorators/_exercises/_templates/1/018_decorators_template_Decorators with parameters.py
@@ -2,27 +2,27 @@
 # Decorators with parameters
 # user_has_permission
 
-# i_ f_
+# _______ fu...
 #
 # user + 'username': 'jose123', 'access_level': 'user'  # dictionary
 #
 # ___ user_has_permission access_level
 #     ___ my_decorator f..
-#         func__.wr__ f..
+#         f__.w__ ?
 #         ___ secure_func panel
-#             __ user.g__('access_level') __ access_level
-#                 r_ func panel
-#         r_ s..
-#     r_ m..
+#             __ u__.g__ 'access_level'| __ access_level
+#                 r_ f.. pa.
+#         r_ ?
+#     r_ ?
 #
-# _u... 'user'
+# ? 'user'
 # ___ my_function panel
 #     """
 #     Allows us to retrieve the password for the admin panel.
 #     """
 #     r_ _'Password for |panel| panel is 1234.'
 #
-# print m._f_.__n_
-# print m.._f.. 'movies'
+# print ?. -n
+# print ? 'movies'
 #
 # print()
