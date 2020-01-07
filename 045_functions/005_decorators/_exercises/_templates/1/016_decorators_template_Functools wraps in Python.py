8fb2296641af26dde37ed332ec6470c91db37191
syurskyi <sergejyurskyj@yahoo.com>
2020-01-06 16:56:12 -0800

    Update 016_decorators_template_Functools wraps in Python.py

--- 045_functions/005_decorators/_exercises/_templates/1/016_decorators_template_Functools wraps in Python.py
+++ 045_functions/005_decorators/_exercises/_templates/1/016_decorators_template_Functools wraps in Python.py
@@ -2,29 +2,29 @@
 # Functools wraps in Python
 # user_has_permission
 
-# i_ f_
+# _______ fu...
 #
 # user _ 'username': 'jose123', 'access_level': 'guest'  # dictionary
 #
 # ___ user_has_permission func
-#     _fun___.w_ f..
+#     _f__.w_ f..
 #     ___ secure_func
-#         __ user.g_('access_level') __ 'admin'
-#             r_ f..
-#     r_ s..
+#         __ u__.g_ 'access_level'| __ 'admin'
+#             r_ ?
+#     r_ ?
 #
-# _u..
+# ??
 # ___ my_function
 #     """
 #     Allows us to retrieve the password for the admin panel.
 #     """
 #     r_ 'Password for admin panel is 1234.'
 #
-# _u...
+# ??
 # ___ another
 #     pass
 #
-# print my_fu_.__n_
-# print ano__.__n_
+# print m... -n
+# print a... -n
 #
 # print()
