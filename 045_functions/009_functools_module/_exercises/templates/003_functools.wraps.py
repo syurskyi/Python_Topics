cd8bdfbe4c7ff280118815f385ec8885467cd5a8
syurskyi <sergejyurskyj@yahoo.com>
2020-01-09 10:26:36 -0800

    Update 003_functools.wraps.py

--- 045_functions/009_functools_module/_exercises/templates/003_functools.wraps.py
+++ 045_functions/009_functools_module/_exercises/templates/003_functools.wraps.py
@@ -1,66 +1,68 @@
-def another_function(func):
-    """
-    Функция которая принимает другую функцию
-    """
-
-    def wrapper():
-        """
-        Оберточная функция
-        """
-        val = "The result of %s is %s" % (func(),
-                                          eval(func())
-                                          )
-
-        return val
-
-    return wrapper
-
-
-@another_function
-def a_function():
-    """Обычная функция"""
-    return "1+1"
-
-
-if __name__ == "__main__":
-    print(a_function.__name__)
-    print(a_function.__doc__)
-
-
-# wrapper
+# # -*- coding: utf-8 -*-
 #
-# Оберточная функция
-# Это не правильно! Если вы запустите эту программу IDLE или в интерпретаторе, станет понятно,
-# насколько это может запутать.
-
-from functools import wraps
-
-
-def another_function(func):
-    """
-    Функция которая принимает другую функцию
-    """
-
-    @wraps(func)
-    def wrapper():
-        """
-        Оберточная функция
-        """
-        val = "The result of %s is %s" % (func(), eval(func()))
-        return val
-
-    return wrapper
-
-
-@another_function
-def a_function():
-    """Обычная функция"""
-    return "1+1"
-
-
-if __name__ == "__main__":
-    # a_function()
-    print(a_function.__name__)
-
-# a_function
-# Обычная функция
+# ___ another_function func
+#     """
+#     Функция которая принимает другую функцию
+#     """
+#
+#     ___ wrapper
+#         """
+#         Оберточная функция
+#         """
+#         val _ "The result of @ is @"  |?       # string
+#                                           ev.. ?
+#                                           ?
+#
+#         r_ v..
+#
+#     r_ ?
+#
+#
+# ??
+# ___ a_function
+#     """Обычная функция"""
+#     r_ "1+1"
+#
+#
+# __ ______ __ ______
+#     print(?. -n
+#     print(?. -d
+#
+#
+# # wrapper
+# #
+# # Оберточная функция
+# # Это не правильно! Если вы запустите эту программу IDLE или в интерпретаторе, станет понятно,
+# # насколько это может запутать.
+#
+# _____ f..._______ w..
+#
+#
+# ___ another_function func
+#     """
+#     Функция которая принимает другую функцию
+#     """
+#
+#     ?? ?
+#     ___ wrapper
+#         """
+#         Оберточная функция
+#         """
+#         val _ "The result of @ is @"  ? ev.. ?
+#         r_ ?
+#
+#     r_ ?
+#
+#
+# ??
+# ___ a_function
+#     """Обычная функция"""
+#     r_ "1+1"
+#
+#
+# __ ______ __ ______
+#     # a_function()
+#     print(?. -n
+# 
+# # a_function
+# # Обычная функция
