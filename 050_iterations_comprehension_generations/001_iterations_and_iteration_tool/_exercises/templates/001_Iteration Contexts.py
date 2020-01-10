bfd6ededb0df60b275c71bdb28663a10204b460b
syurskyi <sergejyurskyj@yahoo.com>
2020-01-10 10:29:23 -0800

    Update 001_Iteration Contexts.py

--- 050_iterations_comprehension_generations/001_iterations_and_iteration_tool/_exercises/templates/001_Iteration Contexts.py
+++ 050_iterations_comprehension_generations/001_iterations_and_iteration_tool/_exercises/templates/001_Iteration Contexts.py
@@ -3,51 +3,51 @@
 # # Iteration Contexts
 # # Use file iterators
 #
-# ___ line i_ o... script2.py
-#     print l__.u... e.._
+# ___ line __ o... script2.py
+#     print ?.u... e.._
 #
 # print()
 # # Iteration Contexts
 # # Use List Comprehension
 #
-# uppers _  l__.u... ___ l... i_ o.. script2.py
-# print uppers
+# uppers _  line.u... ___ ? __ o.. script2.py
+# print ?
 #
 # print()
 # # Iteration Contexts
 # # Use Map
 #
-# print m.. st_.u... o... script1.py
+# print ? st_.u... o... script2.py
 #
 # print()
 # # Iteration Contexts
 # # Use List and Map
 #
-# print l.. m.. st_.u.. o... script2.py
+# print ? ? st_.u.. o... script2.py
 #
 # print()
 # # Iteration Contexts
 # # Use Sorted
 #
-# print s... o.. script2.py
+# print ? o.. script2.py
 #
 # print()
 # # Iteration Contexts
 # # Use Zip
 #
-# print l... z.. o.. script2.py  o.. script2.py
+# print l... ? o.. script2.py  o.. script2.py
 #
 # print()
 # # Iteration Contexts
 # # Use Enumerate
 #
 # print()
-# print l.. enu... o.. script2.py
+# print l.. ? o.. script2.py
 #
 # # Iteration Contexts
 # # Use Filter
 #
-# print l.. f.. bo.. o... script2.py
+# print l.. ? bo.. o... script2.py
 #
 # print()
 # # Iteration Contexts
@@ -55,58 +55,58 @@
 # ______ fun...
 # ______ op...
 #
-# print f___.r.... o__.a.. o... script2.py
+# print f___.? o__.a.. o... script2.py
 #
 # print()
 # # Iteration Contexts
 # # Use Sum
 #
-# print su. 3, 2, 4, 1, 5, 0   # list
+# print ? 3, 2, 4, 1, 5, 0   # list
 #
 # print()
 # # Iteration Contexts
 # # Use Any
 #
-# print an. spam', '', ni
+# print ? spam', '', ni
 #
 # print()
 # # Iteration Contexts
 # # Use All
 #
-# print al. spam '', ni
+# print ? spam '', ni
 #
 # print()
 # # Iteration Contexts
 # # Use Max
 #
-# print ma. 3, 2, 5, 1, 4
+# print ? 3, 2, 5, 1, 4
 #
 # print()
 # # Iteration Contexts
 # # Use Min
 #
-# print mi. 3, 2, 5, 1, 4
+# print ? 3, 2, 5, 1, 4
 #
 # print()
 # # Iteration Contexts
 # # Use Max for file
 #
-# print m.. o.. script2.py
+# print ? o.. script2.py
 #
 # print()
 # # Iteration Contexts
 # # Use Min for file
 #
-# print m.. o.. script2.py
+# print ? o.. script2.py
 #
 # print()
 # # Iteration Contexts
 # # Use List for file
-# print l... o... script2.py
+# print ? o... script2.py
 #
 # # Iteration Contexts
 # # Use Tuple for file
-# print t... o... script2.py
+# print ? o... script2.py
 #
 # print()
 # # Iteration Contexts
