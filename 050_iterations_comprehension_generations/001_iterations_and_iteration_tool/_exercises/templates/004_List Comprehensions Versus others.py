c6da5123f96ec279b0902ae411eb5749a5a2ce1d
syurskyi <sergejyurskyj@yahoo.com>
2020-01-10 16:50:37 -0800

    Update 004_List Comprehensions Versus others.py

--- 050_iterations_comprehension_generations/001_iterations_and_iteration_tool/_exercises/templates/004_List Comprehensions Versus others.py
+++ 050_iterations_comprehension_generations/001_iterations_and_iteration_tool/_exercises/templates/004_List Comprehensions Versus others.py
@@ -1,68 +1,65 @@
-# # List Comprehensions Versus map
-#
-# res _   # list
-# ___ x i_ spam
-#     res.ap.. or_ x  # Manual results collection
-#
-# print(res)
-#
-# res _ l___ ma. or_ spam  # Apply function to sequence
-# print(res)
-#
-# res _  or_ x ___ x i_  spam  # Apply expression to sequence
-# print(res)
-#
-# print x ** 2 ___ x i_ ra.. 10
-#
-# print l___ ma. l____ x; x ** 2; ra.. 10
-#
-# # Adding Tests and Nested Loops - filter
-# # range(5)
-#
-# print x ___ x i_ ra.. 5 i_ x % 2 __ 0
-#
-# print l___ fi.. l____ x; x % 2 __ 0 ; ra.. 5
-#
-# res _   # list
-# ___ x i_ ra.. 5
-#     i_
-# x % 2 __ 0
-# res.ap.. x
-# print r..
-#
-# # Adding Tests and Nested Loops - filter
-# # range(10)
-#
-# print x ** 2 ___ x i_ ra.. 10 i_ x % 2 __ 0
-#
-# print l___ ma. l____ x; x ** 2 ; fi.. l____ x; x % 2 __ 0 ; ra.. 10
-#
-# # Adding Tests and Nested Loops - filter
-# #  for x in
-#
-# res _ x + y ___ x i_ |0 1 2| ___ y i_ |100 200 300
-# print r..
-#
-# res _     # list
-# ___ x i_ 0 1 2
-#     ___
-# y i_ 100 200 300
-# res.ap.. x + y
-# print r..
-#
-# print x + y ___ x i_ 'spam' ___ y i_ 'SPAM'
-#
-# # Adding Tests and Nested Loops - filter
-# # for x in range(5)
-#
-# print |||x y ___ x i_ ra.. 5 i_ x % 2 __ 0 ___ y i_ ra.. 5 i_ y % 2 __ 1
-#
-# res _    # list
-# ___ x i_ ra.. 5
-#     i_
-# x % 2 __ 0
-# ___ y i_ ra.. 5
-#     i_
-# y % 2 __ 1
-# res.ap.. x y
-# print r..
+# # # List Comprehensions Versus map
+# #
+# # res _   # list
+# # ___ x __ spam
+# #     ?.ap.. or_ x  # Manual results collection
+# #
+# # print(res)
+# #
+# # res _ l___ ma. or. 'spam  # Apply function to sequence
+# # print(res)
+# #
+# # res _  or_ x ___ ? __  spam  # Apply expression to sequence
+# # print(res)
+# #
+# # print x ** 2 ___ x __ ra.. 10
+# #
+# # print l___ ma. l____ x x ** 2| ra.. 10
+# #
+# # # Adding Tests and Nested Loops - filter
+# # # range(5)
+# #
+# # print x ___ ? __ ra.. 5 __ x % 2 __ 0
+# #
+# # print l___ fi..||l____ x x % 2 __ 0| ra.. 5
+# #
+# # res _    # list
+# # ___ x _ ra.. 5
+# #     __ _ % 2 __ 0
+# #         ?.ap.. x
+# #     print ?
+# #
+# # # Adding Tests and Nested Loops - filter
+# # # range(10)
+# #
+# # print x ** 2 ___ x __ ra.. 10 __ x % 2 __ 0
+# #
+# # print l___ ma. ||l____ x ? ** 2| fi.. ||l____ x ? % 2 __ 0| ra.. 10
+# #
+# # # Adding Tests and Nested Loops - filter
+# # #  for x in
+# #
+# # res _ x + y ___ x __ |0 1 2| ___ y __ |100 200 300
+# # print r..
+# #
+# # res _     # list
+# # ___ x __ 0 1 2
+# #     ___
+# # y i_ 100 200 300
+# # res.ap.. x + y
+# # print r..
+# #
+# # print x + y ___ ? __ 'spam' ___ ? __ 'SPAM'
+# #
+# # # Adding Tests and Nested Loops - filter
+# # # for x in range(5)
+# #
+# # print |||x y ___ x __ ra.. 5 __ x % 2 __ 0 ___ y __ ra.. 5 __ y % 2 __ 1
+# #
+# res _  # list
+# ___ x __ ra.. 5
+#     __ ? % 2 __ 0
+#         ___ y __ ra.. 5
+#             __ ? % 2 __ 1
+#                 ?.ap.. x y
+#     print ?
