848bd18c14a930006eb88ff9fee314c40aabce3a
syurskyi <sergejyurskyj@yahoo.com>
2020-01-16 09:45:12 -0800

    Update 029_005_Membership __contains__, __iter__, and __getitem__.py

--- 070_oop/001_classes/_exercises/_templates/Learning Python/029_005_Membership __contains__, __iter__, and __getitem__.py
+++ 070_oop/001_classes/_exercises/_templates/Learning Python/029_005_Membership __contains__, __iter__, and __getitem__.py
@@ -1,11 +1,11 @@
 # f_ -f _______ p._f.
 # c_ Iters
 #     ___ - ____ value
-#         ____.data = v..
+#         ____.data = ?
 #
 #     ___ -g ____ i  # Fallback for iteration
-#         print('get |/_:' / i e.._'')  # Also for index, slice
-#         r_ ____.data i
+#         print('get|@:'  i e.._'')  # Also for index, slice # string
+#         r_ ____.d.. ?
 #
 #     ___ -i ____  # Preferred for iteration
 #         print('iter=> ', e.._''  # Allows only 1 active iterator
@@ -14,30 +14,31 @@
 #
 #     ___ -n ____
 #         print('next:', e.._''
-#         i_ ____.ix __ le. ____.data r.. S..
+#         __ ____.ix __ le. ____.d..
+#             r.. S..
 #         item = ____.d... ____.i_
 #         ____.i. += 1
 #         r_ ?
 #
 #     ___ -c ____ x  # Preferred for 'in'
 #         print('contains: ', e.._''
-#         r_ x i_ ____.da..
+#         r_ x i_ ____.d..
 #
 #
 # X = ? 1, 2, 3, 4, 5  # Make instance
 # print(3 in X)  # Membership
-# ___ i i_ X  # For loops
-#     print i e.._' | '
+# ___ i __ ?  # For loops
+#     print ? e.._' | '
 #
 # print()
-# print i ** 2 ___ i i_ X  # Other iteration contexts
+# print i ** 2 ___ ? __ ?  # Other iteration contexts
 # print li.. ma. bi. ?
 #
 # I = it.. ?  # Manual iteration (what other contexts do)
 # w___ T..
-#     t__
+#     ___
 #         print n.. I e.._' @ '
-#     e.... S..
+#     _.... S..
 #         b..
 #
 # X = ? 'spam'  # Indexing
