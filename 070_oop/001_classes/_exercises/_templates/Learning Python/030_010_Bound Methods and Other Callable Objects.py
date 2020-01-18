862610678dd2b09709ee74a67e69c7887203d25d
syurskyi <sergejyurskyj@yahoo.com>
2020-01-18 11:45:33 -0800

    Update 030_010_Bound Methods and Other Callable Objects.py

--- 070_oop/001_classes/_exercises/_templates/Learning Python/030_010_Bound Methods and Other Callable Objects.py
+++ 070_oop/001_classes/_exercises/_templates/Learning Python/030_010_Bound Methods and Other Callable Objects.py
@@ -1,6 +1,6 @@
-# c_ Number:
+# c_ Number
 #     ___ - ____ base
-#         ____.b.. _ b..
+#         ____.?  ?
 #
 #     ___ double ____
 #         r_ ____.b.. * 2
@@ -16,8 +16,8 @@
 #
 # print('#' * 23 + ' List of bound methods')
 # acts =  x.d... y.d... y.t.. z.d..     # List of bound methods
-# ___ act i_ ?                                    # Calls are deferred
-#     print a..                                    # Call as though functions
+# ___ act __ ?                                    # Calls are deferred
+#     print ?                                    # Call as though functions
 # #
 # bound _ x.d..
 # print ?.-s ? -f
@@ -28,57 +28,57 @@
 # print ?                             # Calls bound.__func__(bound.__self__, ...)
 # #
 # ___ square arg
-#     r_ ar. ** 2                          # Simple functions (def or lambda)
+#     r_ ? ** 2                          # Simple functions (def or lambda)
 # #
 # c_ Sum
 #     ___ - ____, val                 # Callable instances
-#         ____.v.. _ v..
+#         ____.?  ?
 #     ___ -c ____ arg
-#         r_ ____.val + arg
+#         r_ ____.val + a..
 # #
 # c_ Product
 #     ___ - ____, val                 # Bound methods
-#         ____.v.. _ v..
+#         ____.?  ?
 #     ___ method ____ arg
-#         r_ ____.v.. * ar.
+#         r_ ____.v.. * a..
 # #
 # sobject = S__ 2
 # pobject = P___ 3
 # actions _ square sobject po___.me..  # Function, instance, method
 # #
 # print('#' * 23 + ' Function, instance, method. All 3 called same way. Call any 1-arg callable')
-# ___ act i_ a..                          # All 3 called same way
+# ___ act __ a..                          # All 3 called same way
 #     print  5                            # Call any 1-arg callable
 # #
 # #
 # print('#' * 23 + ' Index, comprehensions, maps')
 # actions[-1](5)                               # Index, comprehensions, maps
 # #
-# [act 5 ___ ? i_ a...
+# [act 5 ___ ? __ a...
 #
-# li.. ma. l act: ac. (5) ac..
+# li.. ma. l act ac. (5) ac..
 #
 #
 # c_ Negate
 #     ___ - ____ val                 # Classes are callables too
-#         ____.val _ -val                      # But called for object, not work
+#         ____.?  ?                      # But called for object, not work
 #
 #     ___ -r ____                      # Instance print format
 #         r_ st. ____.v..
 #
 # print('#' * 23 + ' Call a c_ too')
 # actions _  s... so... po____.me... Ne...     # Call a c_ too
-# ___ act i_ ?
+# ___ act __ ?
 #     print ? 5
 # #
 # print('#' * 23 + ' Runs __repr__ not __str__!')
-# print act 5 ___ ? i_ ac..                    # Runs __repr__ not __str__!
+# print act 5 ___ ? __ ac..                    # Runs __repr__ not __str__!
 # #
 # #
 #
 # table =  act 5 act ___ ? i_ ac..        # 2.6/3.0 dict comprehension
 # print('#' * 23 + '  2.6/3.0 str.format')
-# ___ key value i_ t___.it..
+# ___ key value __ t___.it..
 #     print(' _:2 => _ '.f... k. v..    # 2.6/3.0 str.format
 #
 # # -5 => <c_ '__main__.Negate'>
