f2005923681f09f83216ffa8497fddcd3ed99efb
syurskyi <sergejyurskyj@yahoo.com>
2020-01-20 12:57:50 -0800

    Update and rename 031_16_Counting Instances with Class Methods.py to 031_016_Counting Instances with Class Methods.py

--- 070_oop/001_classes/_exercises/_templates/Learning Python/031_016_Counting Instances with Class Methods.py
+++ 070_oop/001_classes/_exercises/_templates/Learning Python/031_016_Counting Instances with Class Methods.py
@@ -1,29 +1,29 @@
 # c_ Spam:
 #     numInstances = 0                         # Use class method instead of static
 #     ___ - ____
-#         S_.n... += 1
+#         ?.? += 1
 #     ___ printNumInstances ___
 #         print("Number of instances:", ___.n....
-#     p... _ cl... p..
+#     ? _ cl... ?
 #
 #
-# a, b = S.. S..
+# a, b = ? ?
 # a.p...                   # Passes class to first argument
 # S__.p..                 # Also passes class to first argument
 #
 # c_ Spam
 #     numInstances = 0                         # Trace class passed in
 #     ___ -  ____
-#         S_.n.. += 1
+#         ?.? += 1
 #     ___ printNumInstances ___
 #         print("Number of instances:", ___.n... ___
-#     printNumInstances _ cl... p..
+#     ? _ cl... p..
 #
 # c_ Sub S..
 #     ___ printNumInstances ___              # Override a class method
 #         print("Extra stuff...", ___         # But call back to original
-#         S__.p...
-#     printNumInstances _ cl... p..
+#         ?.?
+#     ? _ cl... p..
 #
 # c_ Other S.. p..                      # Inherit class method verbatim
 #
