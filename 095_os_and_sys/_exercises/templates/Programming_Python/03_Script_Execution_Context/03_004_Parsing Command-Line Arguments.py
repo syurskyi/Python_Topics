9db65f562809df6d7b605d0180b1b15ce53793c9
syurskyi <sergejyurskyj@yahoo.com>
2020-01-23 12:26:45 -0800

    Update 03_004_Parsing Command-Line Arguments.py

--- 095_os_and_sys/_exercises/templates/Programming_Python/03_Script_Execution_Context/03_004_Parsing Command-Line Arguments.py
+++ 095_os_and_sys/_exercises/templates/Programming_Python/03_Script_Execution_Context/03_004_Parsing Command-Line Arguments.py
@@ -1,22 +1,22 @@
 # ___ getopts argv
-# #     opts =      # dict
-# #     w___ a..
-# #         i_ a..  0 | 0 _ '-'                  # find "-name value" pairs
-# #             ? | a.. 0 _ a.. 1            # dict key is "-name" arg
-# #             a.. _ a.. 2:
-# #         e__
-# #             a.. _ a.. 1:
-# #     r_ ?
-# #
-# # __ ______ __ _____
-# #     f.. ___ ________ a..                       # example client code
-# #     myargs = ? a..
-# #     if '-i' i_ ?
-# #         print ? '-i'
-# #     print ?
-# #
-# # # C:\...\PP4E\System> python testargv2.py
-# # # {}
-# # # C:\...\PP4E\System> python testargv2.py -i data.txt -o results.txt
-# # # data.txt
-# # # {'-o': 'results.txt', '-i': 'data.txt'}
+#     opts =      # dict
+#     w___ a..
+#         __ a..|0 0 __ '-'                  # find "-name value" pairs
+#             ?|a.. 0 _ a.. 1            # dict key is "-name" arg
+#             a.. _ a.. 2:
+#         ____
+#             a.. _ a.. 1
+#     r_ ?
+#
+# __ ______ __ _____
+#     .... ___ ________ a..                       # example client code
+#     myargs = ? a..
+#     __ '-i' __ ?
+#         print ? '-i'
+#     print ?
+#
+# # C:\...\PP4E\System> python testargv2.py
+# # {}
+# # C:\...\PP4E\System> python testargv2.py -i data.txt -o results.txt
+# # data.txt
+# # {'-o': 'results.txt', '-i': 'data.txt'}
