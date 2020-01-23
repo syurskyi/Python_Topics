f176256305687d12cb4c1208d9e4c273c154d681
syurskyi <sergejyurskyj@yahoo.com>
2020-01-22 17:06:12 -0800

    Update 06-open_for_appending.py

--- 095_os_and_sys/_exercises/templates/ITVDN Python Essential 2016/06-open_for_appending.py
+++ 095_os_and_sys/_exercises/templates/ITVDN Python Essential 2016/06-open_for_appending.py
@@ -1,4 +1,4 @@
-﻿# # -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 #
 # """Пример открытия файла для дозаписи"""
 #
@@ -6,5 +6,5 @@
 # ______ d_t_
 #
 # log_file _ __.p__.j.. data ex06_log.txt
-# w___ o.. ? _ a_ log
+# w___ o.. ? _ __ log
 #     print(d_t_.d_t_.n. file _ l..
