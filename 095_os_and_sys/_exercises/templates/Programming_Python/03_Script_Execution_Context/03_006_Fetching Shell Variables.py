34db9662fd70150e9bfb3dcd5017b391b90aa698
syurskyi <sergejyurskyj@yahoo.com>
2020-01-23 12:58:34 -0800

    Update 03_006_Fetching Shell Variables.py

--- 095_os_and_sys/_exercises/templates/Programming_Python/03_Script_Execution_Context/03_006_Fetching Shell Variables.py
+++ 095_os_and_sys/_exercises/templates/Programming_Python/03_Script_Execution_Context/03_006_Fetching Shell Variables.py
@@ -1,26 +1,26 @@
 # _______ __
-# print(__.en_____.k...
+# print(__.e__.k...
 # # KeysView(<os._Environ object at 0x013B8C70>)
 #
 # print('#' * 52)
-# print(li.. __.en_____.k...
+# print(li.. __.e__.k...
 # # ['TMP', 'COMPUTERNAME', 'USERDOMAIN', 'PSMODULEPATH', 'COMMONPROGRAMFILES',
 # #...many more deleted...
 # # 'NUMBER_OF_PROCESSORS', 'PROCESSOR_LEVEL', 'USERPROFILE', 'OS', 'PUBLIC', 'QTJAVA']
 # # ######################################################################################################################
 #
 # print('#' * 52)
-# print __.en_____ 'TEMP'
+# print __.e__ 'TEMP'
 # # 'C:\\Users\\mark\\AppData\\Local\\Temp'
 # # ######################################################################################################################
 #
 # print('#' * 52)
-# print(__.en_____ 'PYTHONPATH'
+# print(__.e___ 'PYTHONPATH'
 # # 'C:\\PP4thEd\\Examples;C:\\Users\\Mark\\temp'
 # # ######################################################################################################################
 #
 # print('#' * 52)
-# ___ srcdir i_ __.en_____ 'PYTHONPATH' .sp..__.p.s.
+# ___ srcdir __ __.e__ 'PYTHONPATH' .sp.. __.p.s.
 #     print ?
 #
 # # C:\PP4thEd\Examples
