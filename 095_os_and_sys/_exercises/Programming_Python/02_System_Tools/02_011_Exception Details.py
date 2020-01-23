ca0339eaac21fc4aa4abc833adcdf0b5c1ff4d19
Grey <sergejyurskyj@yahoo.com>
2020-01-22 20:17:45 -0800

    Committed os and sys exercises

---
+++ 095_os_and_sys/_exercises/Programming_Python/02_System_Tools/02_011_Exception Details.py
@@ -0,0 +1,25 @@
+import sys
+try:
+    raise IndexError
+except:
+    print(sys.exc_info())
+
+# (<class 'IndexError'>, IndexError(), <traceback object at 0x019B8288>)
+
+import traceback, sys
+
+def grail():
+    raise T  already got one
+
+t__
+    g.. arthur
+e____
+    e._i. = ___.e._i.
+    print(e._i. 0
+    print(e._i. 1
+    tr__.print_tb e.._i. 2
+
+# <class 'TypeError'>
+# already got one
+# File "<stdin>", line 2, in <module>
+# File "<stdin>", line 2, in grail
---
+++ 095_os_and_sys/_exercises/Programming_Python/03_Script_Execution_Context/03_011_Coding alternatives for adders and sorters.py
@@ -0,0 +1,9 @@
+# # C:\...\PP4E\System\Streams> type sorterSmall.py
+# ______ ___
+# ___ line i_ so..  ___.s.i.
+#     print ? e.._''
+# # C:\...\PP4E\System\Streams> type adderSmall.py
+# # ######################################################################################################################
+#
+# ______ ___
+# print su. in. line ___ l.. i_ ___.s.i.
