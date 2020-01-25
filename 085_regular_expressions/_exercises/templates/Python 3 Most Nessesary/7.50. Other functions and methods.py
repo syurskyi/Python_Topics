de146f6507688feb425ddf09dbf2653b4261c1de
syurskyi <sergejyurskyj@yahoo.com>
2020-01-24 17:09:20 -0800

    Update 7.50. Other functions and methods.py

--- 085_regular_expressions/_exercises/templates/Python 3 Most Nessesary/7.50. Other functions and methods.py
+++ 085_regular_expressions/_exercises/templates/Python 3 Most Nessesary/7.50. Other functions and methods.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
-import re
-p = re.compile(r"[\s,.]+")
-p.split("word1, word2\nword3\r\nword4.word5")
+_______ __
+p _ __.c..(r"[\s,.]+")
+p.s..("word1, word2\nword3\r\nword4.word5")
 # ['word1', 'word2', 'word3', 'word4', 'word5']
-p.split("word1, word2\nword3\r\nword4.word5", 2)
+p.s..("word1, word2\nword3\r\nword4.word5", 2)
 # ['word1', 'word2', 'word3\r\nword4.word5']
 
 
-p = re.compile(r"[0-9]+")
-p.split("word, word\nword")
+p _ __.c..(r"[0-9]+")
+p.s..("word, word\nword")
 # ['word, word\nword']
 
 
-p = re.compile(r"[\s,.]+")
-re.split(p, "word1, word2\nword3")
+p _ __.c..(r"[\s,.]+")
+__.s..(p, "word1, word2\nword3")
 # ['word1', 'word2', 'word3']
-re.split(r"[\s,.]+", "word1, word2\nword3")
+__.s..(r"[\s,.]+", "word1, word2\nword3")
 # ['word1', 'word2', 'word3']
 
-print(re.escape(r"[]().*"))
+print(__.escape(r"[]().*"))
 # \[\]\(\)\.\*
 
-re.purge()
+__.purge()
