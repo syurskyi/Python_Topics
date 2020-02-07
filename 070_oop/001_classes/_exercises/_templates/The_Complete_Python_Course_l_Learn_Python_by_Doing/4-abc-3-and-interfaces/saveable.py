fe701122c34d6a105fb96491d8d43507609e32a7
syurskyi <sergejyurskyj@yahoo.com>
2020-02-07 14:30:20 -0800

    Update saveable.py

--- 070_oop/001_classes/_exercises/_templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/4-abc-3-and-interfaces/saveable.py
+++ 070_oop/001_classes/_exercises/_templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/4-abc-3-and-interfaces/saveable.py
@@ -1,12 +1,12 @@
-from abc import ABCMeta, abstractmethod
-
-from database import Database
-
-class Saveable(metaclass=ABCMeta):
-    def save(self):
-        Database.insert(self.to_dict())
-    
-    # @classmethod (or @staticmethod, or @property)
-    @abstractmethod  # @abstractmethod must always be the innermost decorator if used in conjunction with other decorators.
-    def to_dict():
-        pass
+# ____ ___ ______ A.. a..
+#
+# ____ database ______ Database
+#
+# c_ Saveable m.._
+#     ___ save ____
+#         D__.i.. ____.to_d..
+#
+#     # @classmethod (or @staticmethod, or @property)
+#     ??  # @abstractmethod must always be the innermost decorator if used in conjunction with other decorators.
+#     ___ to_dict
+#         p..
