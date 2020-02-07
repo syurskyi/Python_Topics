6f86246ecaf3870bab7d1edf0cf918fab9578cea
syurskyi <sergejyurskyj@yahoo.com>
2020-02-07 14:30:24 -0800

    Update database.py

--- 070_oop/001_classes/_exercises/_templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/4-abc-3-and-interfaces/database.py
+++ 070_oop/001_classes/_exercises/_templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/4-abc-3-and-interfaces/database.py
@@ -1,14 +1,14 @@
-class Database:
-    content = {'users': []}
-
-    @classmethod
-    def insert(cls, data):
-        cls.content['users'].append(data)
-    
-    @classmethod
-    def remove(cls, finder):
-        cls.content['users'] = [user for user in cls.content['users'] if not finder(user)]
-    
-    @classmethod
-    def find(cls, finder):
-        return [user for user in cls.content['users'] if finder(user)]
+# c_ Database
+#     content  _  'users':    # list
+#
+#     ??
+#     ___ insert ___ data
+#         ___.? 'users'|.ap.. ?
+#
+#     ??
+#     ___ remove ___ finder
+#         ___.? 'users'| _ user ___ ? __ ___.? 'users'| __ no. f.. ?
+#
+#     ??
+#     ___ find ___ finder
+#         r_ user ___ ? __ ___.? 'users'| __ f.. ?
