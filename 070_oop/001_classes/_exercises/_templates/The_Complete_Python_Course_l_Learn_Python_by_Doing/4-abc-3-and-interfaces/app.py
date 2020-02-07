dd9c1ff4821680b5e380692f9f141ca14c3b532a
syurskyi <sergejyurskyj@yahoo.com>
2020-02-07 14:29:50 -0800

    Update app.py

--- 070_oop/001_classes/_exercises/_templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/4-abc-3-and-interfaces/app.py
+++ 070_oop/001_classes/_exercises/_templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/4-abc-3-and-interfaces/app.py
@@ -1,22 +1,22 @@
-from database import Database
-from admin import Admin
-from user import User
-from saveable import Saveable
-
-a = Admin('paco', 'perez', 2)
-b = Admin('rolf', 'smith', 1)
-
-a.save()
-b.save()
-
-user = Database.find(lambda x: x['username'] == 'paco')[0]
-user_obj = Admin(**user)
-print(user_obj.username)
-
-print(isinstance(user_obj, Saveable))  # This is True because it's a subclass
-
-# You can do things like these without worry:
-
-users_to_save = [a, b, User('jose', '1234')]
-for u in users_to_save:
-    u.save()  # This is fine, because all users (Admin and User) implement the Saveable interface so we know they have a .save() method
+# ____ d... ______ D__
+# ____ a.. ______ A..
+# ____ u.. ______ U..
+# ____ s.. ______ S..
+#
+# a = A..('paco', 'perez', 2)
+# b = A..('rolf', 'smith', 1)
+#
+# a.s..
+# b.s..
+#
+# user = D__.f.. l____ x ? 'username'| __ 'paco' 0
+# user_obj = A.. $$?
+# print ?.u..
+#
+# print isi..  ? S..  # This is True because it's a subclass
+#
+# # You can do things like these without worry:
+#
+# users_to_save = [a, b, U..('jose', '1234')]
+# ___ u __ ?
+#     ?.s..  # This is fine, because all users (Admin and User) implement the Saveable interface so we know they have a .save() method
