f754fb437fc29fd7ac6c12ca1c89293dbd6e0523
syurskyi <sergejyurskyj@yahoo.com>
2020-02-11 13:01:50 -0800

    Create 001.py

---
+++ 070_oop/002_special_method/_exercises/templates/__getattribute__/001.py
@@ -0,0 +1,22 @@
+# c_ Foo o..
+#
+#     ___ -g ____ name
+#         print "getting attribute @"  ?
+#         r_ o__.-g____ ?
+#
+#     ___ -s ____ name, val
+#         print "setting attribute @ to @"  ?  ?
+#         r_ o__.-s ____ ?  ?
+#
+#
+#
+# foo = ?
+# ?.var = 100
+# # setting attribute var to 100
+# ?.name = 'stuart'
+# # setting attribute name to 'stuart'
+# bar = foo.var
+# # getting attribute var
+# foo.var = foo.name
+# # getting attribute name
+# # setting attribute var to 'stuart'
