58d02b793779a596e3db0af2e33e4fab519c1771
syurskyi <sergejyurskyj@yahoo.com>
2020-02-11 13:02:36 -0800

    Create 003.py

---
+++ 070_oop/002_special_method/_exercises/templates/__getattribute__/003.py
@@ -0,0 +1,16 @@
+# # Override access to one variable in a class, but return all others normally
+# # http://stackoverflow.com/a/371833/18852
+#
+# c_ D o..
+#     ___  -
+#         ____.test _ 20
+#         ____.test2 _ 40
+#     ___ -g ____ name
+#         __ ? __ 'test'
+#             r_ 0
+#         ____
+#             r_ o__.-g ____ ?
+#
+# obj1 = ?
+# print ?.test
+# print ?.test2
