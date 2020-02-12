b07c12c04ac9b93ef09a182ad30aed134712a1ef
syurskyi <sergejyurskyj@yahoo.com>
2020-02-12 10:12:43 -0800

    Update and rename 030_006_OOP and Delegation Wrapper Objects.py to 030_006_OOP and Delegation Wrapper Objects_!cool!.py

--- 070_oop/001_classes/_exercises/_templates/Learning Python/030_006_OOP and Delegation Wrapper Objects_!cool!.py
+++ 070_oop/001_classes/_exercises/_templates/Learning Python/030_006_OOP and Delegation Wrapper Objects_!cool!.py
@@ -1,17 +1,17 @@
 # c_ Wrapper
-#     ___ - ____, object
+#     ___ - ____ object
 #         ____.wrapped _ ?                # Save object
 #
 #     ___ -g ____ attrname
 #         print('Trace:', ?                # Trace fetch
-#         r_ g_a. ____.w.. ?   # Delegate fetch
+#         r_ g_a.. ____.w.. ?   # Delegate fetch
 #
 # # NOTE: in the following, use list(x.keys()) for Python 3.X
 # # (list() was not used in the first printing of the book
 #
 # # from trace import Wrapper
 # x = ? 1, 2, 3                         # Wrap a list
-# ?.a.. 4                                 # Delegate to list method
+# ?.ap.. 4                                 # Delegate to list method
 # #
 # print ?.w..                                  # Print my member
 # #
