5d251e880f90974d9274100c63f6bc17ab414b22
Grey <sergejyurskyj@yahoo.com>
2020-02-11 22:30:09 -0800

    Committed oop templates update

--- 070_oop/001_classes/_exercises/The_Modern_Python_3_Bootcamp/235. Creating Classes and Instances.py
+++
@@ -1,7 +0,0 @@
-class Person:
-    pass
-
-
-p = Person()
-print(type(p))
-
--- 070_oop/001_classes/_exercises/_templates/The_Modern_Python_3_Bootcamp/240. Adding Instance Methods.py
+++ 070_oop/001_classes/_exercises/_templates/The_Modern_Python_3_Bootcamp/240. Adding Instance Methods.py
@@ -1,37 +1,37 @@
-# A User class with both instance attributes and instance methods
-class User:
-    def __init__(self, first, last, age):
-        self.first = first
-        self.last = last
-        self.age = age
-
-    def full_name(self):
-        return f"{self.first} {self.last}"
-
-    def initials(self):
-        return f"{self.first[0]}.{self.last[0]}."
-
-    def likes(self, thing):
-        return f"{self.first} likes {thing}"
-
-    def is_senior(self):
-        return self.age >= 65
-
-    def birthday(self):
-        self.age += 1
-        return f"Happy {self.age}th, {self.first}"
-
-
-user1 = User("Joe", "Smith", 68)
-user2 = User("Blanca", "Lopez", 41)
-
-print(user1.likes("Ice Cream"))
-print(user2.likes("Chips"))
-
-print(user2.initials())
-print(user1.initials())
-
-print(user2.is_senior())
-print(user1.age)  # Print age before we update it
-print(user1.birthday())  # updates age
-print(user1.age)  # Print new value of age
+# # A User class with both instance attributes and instance methods
+# c_ User
+#     ___ - ____ first last age
+#         ____.?  ?
+#         ____.?  ?
+#         ____.?  ?
+#
+#     ___ full_name ____
+#         r_ _* ____.f.. ____.l..
+#
+#     ___ initials ____
+#         r_ _* ____.f.. 0 . ____.l.. 0 .
+#
+#     ___ likes ____ thing
+#         r_ _* ____.f.. likes t..
+#
+#     ___ is_senior ____
+#         r_ ____.a.. >_ 65
+#
+#     ___ birthday ____
+#         ____.a.. +_ 1
+#         r_ _*Happy ____.a.. th, ____.f..
+#
+#
+# user1 = ?("Joe", "Smith", 68)
+# user2 = ?("Blanca", "Lopez", 41)
+#
+# print(user1.likes("Ice Cream"))
+# print(user2.likes("Chips"))
+#
+# print(user2.initials())
+# print(user1.initials())
+#
+# print(user2.is_senior())
+# print(user1.age)  # Print age before we update it
+# print(user1.birthday())  # updates age
+# print(user1.age)  # Print new value of age
