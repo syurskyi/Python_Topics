a48c5ff5709c6520c51546391c28e2a2a95b0f6a
syurskyi <sergejyurskyj@yahoo.com>
2020-02-13 20:55:38 -0800

    Update 1 - Dog - Getters and Setters.py

--- 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 7/Properties-Code/1 - Dog - Getters and Setters.py
+++ 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 7/Properties-Code/1 - Dog - Getters and Setters.py
@@ -1,13 +1,13 @@
-class Dog:
-    
-    def __init__(self, name):
-        self._name = name
-
-    def get_name(self):
-        return self._name
-
-    def set_name(self, name):
-        if isinstance(name, str):
-            self._name = name
-        else:
-            print("Please enter a valid name")
+# c_ Dog
+#
+#     ___ - ____ name
+#         ____._?  ?
+#
+#     ___ get_name ____
+#         r_ ____._n..
+#
+#     ___ set_name ____ name
+#         __ isi.. ? st.
+#             ____._n.. _ ?
+#         ____
+#             print("Please enter a valid name")
