136c016ec19f30aa6d7197a0888bd7ba056b23d6
syurskyi <sergejyurskyj@yahoo.com>
2020-02-13 20:55:09 -0800

    Update 1-Book-Example.py

--- 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 6/1-Book-Example.py
+++ 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 6/1-Book-Example.py
@@ -1,8 +1,8 @@
-class Book:
-
-	def __init__(self, title, author, num_pages, ISBN, publisher):
-		self.title = title
-		self.author = author
-		self.num_pages = num_pages
-		self.ISBN = ISBN
-		self.publisher = publisher
+# c_ Book
+# 
+# 	___ - ____ title author num_pages ISBN publisher
+# 		____.?  ?
+# 		____.?  ?
+# 		____.?  ?
+# 		____.?  ?
+# 		____.?  ?
