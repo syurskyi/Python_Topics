212aa750b2d3eeef7fc830b8be1211d2c7bba202
syurskyi <sergejyurskyj@yahoo.com>
2020-02-13 21:02:06 -0800

    Update 001_Standart_dialog_QMessageBox_QInputDialog.py

--- 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/008_Class_widget/001_Standart_dialog_QMessageBox_QInputDialog.py
+++ 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/008_Class_widget/001_Standart_dialog_QMessageBox_QInputDialog.py
@@ -1,42 +1,42 @@
-from PySide.QtCore import *
-from PySide.QtGui import *
-
-class simpleWindow(QWidget):
-    def __init__(self):
-        super(simpleWindow, self).__init__()
-        ly = QVBoxLayout(self)
-        self.btn = QPushButton('Open')
-        ly.addWidget(self.btn)
-        self.resize(300, 200)
-        self.btn.clicked.connect(self.showMessage)
-
-    def showMessage2(self):
-        i = QInputDialog.getItem(self, 'Enter text', 'Name:',  # Tak kak eto staticheskij metod, mu mozem vuzvat' ne sozdavaja ekzempljar klassa
-                                                               # getItem eto staticheskaja fynkcija, mu mozem vuzvat' ne sozdavaja ekzempljar klassa
-                                                               # Esli smotret' na Qt documentation to mozno videt
-                                                               # QWidget *parent, const QString &title, const QString &label, const QStringList &items
-                                                               # self,            title,                   label,               elementu iz kotoruh mozno vuberat' i eto objazatel'no dolznu but' stroki
-                                 [str(x) for x in range(10)])
-        print i
-
-    def showMessage(self):
-        msgBox = QMessageBox()      # sozdali ekzempljar klassa
-        print msgBox, type(msgBox)
-        msgBox.setText("The document has been modified.")
-        msgBox.setInformativeText("Do you want to save your changes?")
-        msgBox.setStandardButtons(QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
-        msgBox.setDefaultButton(QMessageBox.Save)
-        msgBox.setDetailedText('Detail text')
-        ret = msgBox.exec_()                    # Kogda mu vuzuvaem exec_, eta fynkcija dolzna nam vernyt' kakoe to
-                                                # znachenie, a kakoe znachenie zavisit ot togo kakyjy knopky mu nazmjom
-                                                # Posle togo kak mu sdelali vuzov nashego Message boxa nam nado
-                                                # proverit', shto ypalo nam v peremenyjy i potom vzavisimosti ot etogo
-                                                # prodolzat'  kakie to dejstvija
-        print ret
-        print ret == QMessageBox.Save
-
-if __name__ == '__main__':
-    app = QApplication([])
-    w = simpleWindow()
-    w.show()
-    app.exec_()
+# ____ ?.? ______ _
+# ____ ?.? ______ _
+#
+# c_ simpleWindow QW..
+#     ___ - ____
+#         s__ ? ____. -
+#         ly = QVL.. ____
+#         ____.btn = QPB.. 'Open'
+#         ?.aW.. ?
+#         ____.re.. 300, 200
+#         ____.b__.cl__.co.. ____.sM..
+#
+#     ___ showMessage2 ____
+#         i = QID___.gI.. ____, 'Enter text', 'Name:',  # Tak kak eto staticheskij metod, mu mozem vuzvat' ne sozdavaja ekzempljar klassa
+#                                                                # getItem eto staticheskaja fynkcija, mu mozem vuzvat' ne sozdavaja ekzempljar klassa
+#                                                                # Esli smotret' na Qt documentation to mozno videt
+#                                                                # QWidget *parent, const QString &title, const QString &label, const QStringList &items
+#                                                                # ____,            title,                   label,               elementu iz kotoruh mozno vuberat' i eto objazatel'no dolznu but' stroki
+#                                  st..x ___ ? __ ra.. 10
+#         print ?
+#
+#     ___ showMessage ____
+#         msgBox = QMB..     # sozdali ekzempljar klassa
+#         print ? ty.. ?
+#         ?.sT.. "The document has been modified."
+#         ?.sIT.. "Do you want to save your changes?"
+#         ?.sSB.. QMB__.S.. _ QMB__.D.. _ QMB__.C..
+#         ?.sDB.. QMB__.S..
+#         ?.sDT__ 'Detail text'
+#         ret = ?.e..                    # Kogda mu vuzuvaem exec_, eta fynkcija dolzna nam vernyt' kakoe to
+#                                                 # znachenie, a kakoe znachenie zavisit ot togo kakyjy knopky mu nazmjom
+#                                                 # Posle togo kak mu sdelali vuzov nashego Message boxa nam nado
+#                                                 # proverit', shto ypalo nam v peremenyjy i potom vzavisimosti ot etogo
+#                                                 # prodolzat'  kakie to dejstvija
+#         print ?
+#         print ? __ QMB__.S..
+#
+# __ ______ __ ______
+#     app = QA..
+#     w = ?
+#     ?.s..
+#     ?.?
