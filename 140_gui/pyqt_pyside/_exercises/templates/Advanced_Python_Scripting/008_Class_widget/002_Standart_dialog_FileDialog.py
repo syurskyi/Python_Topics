4d3ab29c36ca317226dc2e95c6df92023fd150ab
syurskyi <sergejyurskyj@yahoo.com>
2020-02-13 21:02:32 -0800

    Update 002_Standart_dialog_FileDialog.py

--- 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/008_Class_widget/002_Standart_dialog_FileDialog.py
+++ 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/008_Class_widget/002_Standart_dialog_FileDialog.py
@@ -1,29 +1,29 @@
-from PySide.QtGui import *
-# from PyQt4.QtGui import *
-
-class simpleWindow(QWidget):
-    def __init__(self):
-        super(simpleWindow, self).__init__()
-        ly = QVBoxLayout(self)
-        self.btn  = QPushButton('Open')
-        ly.addWidget(self.btn)
-        self.resize(300,200)
-        self.btn.clicked.connect(self.showMessage)
-
-    def showMessage(self):
-        # _filter = '*.py'
-        # _filter = '*.py;;*.txt'
-        # _filter = '*.py *.txt'
-        # _filter = '*.py *.txt;;cmd.exe'
-        # _filter = 'Python File(*.py);; All(*.*)'
-        d = QFileDialog.getExistingDirectory(self, 'Set folder', 'c:/Users') # staticheskaja peremenaja getExistingDirectory
-                                                                             # vozvrachaet pyt' k direktorii
-        # d = QFileDialog.getOpenFileName(self, 'Set folder', 'c:/tmp', _filter)
-        # d = QFileDialog.getOpenFileNames(self, 'Set folder', 'c:/Users', _filter)
-        print d, type(d)
-
-if __name__ == '__main__':
-    app = QApplication([])
-    w = simpleWindow()
-    w.show()
-    app.exec_()
+# ____ ?.? ______ _
+# ____ ?.? ______ _
+#
+# c_ simpleWindow QW..
+#     ___ - ___
+#         s__ ? ___ . -
+#         ly = QVBL.. ___
+#         ___.btn  _ QPB.. 'Open
+#         ?.aW.. ?
+#         ___.re.. 300,200
+#         ___.b__.cl__.c.. ___.?
+#
+#     ___ showMessage ___
+#         # _filter = '*.py'
+#         # _filter = '*.py;;*.txt'
+#         # _filter = '*.py *.txt'
+#         # _filter = '*.py *.txt;;cmd.exe'
+#         # _filter = 'Python File(*.py);; All(*.*)'
+#         d = QFD__.gED.. ___ 'Set folder', 'c:/Users' # staticheskaja peremenaja getExistingDirectory
+#                                                                              # vozvrachaet pyt' k direktorii
+#         # d = QFileDialog.getOpenFileName(___, 'Set folder', 'c:/tmp', _filter)
+#         # d = QFileDialog.getOpenFileNames(___, 'Set folder', 'c:/Users', _filter)
+#         print ? ty.. ?
+#
+# __ ______ __ ______
+#     app _ QA..
+#     w = ?
+#     ?.s..
+#     ?.e..
