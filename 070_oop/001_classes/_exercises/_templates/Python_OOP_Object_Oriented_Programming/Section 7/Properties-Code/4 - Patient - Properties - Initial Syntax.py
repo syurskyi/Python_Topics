5466c35070e90cfdaaa6402fe70effa02f04c2d8
syurskyi <sergejyurskyj@yahoo.com>
2020-02-13 20:57:30 -0800

    Update 4 - Patient - Properties - Initial Syntax.py

--- 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 7/Properties-Code/4 - Patient - Properties - Initial Syntax.py
+++ 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 7/Properties-Code/4 - Patient - Properties - Initial Syntax.py
@@ -1,34 +1,34 @@
-class Patient:
-    
-    def __init__(self, name, age, id_num, num_children=0):
-        self.name = name
-        self.age = age
-        self._id_num = id_num
-        self._num_children = num_children
-
-    def get_id_num(self):
-        print("Getter")
-        return self._id_num
-
-    def set_id_num(self, new_id):
-        print("Setter")
-        if isinstance(new_id, str):
-            self._id_num = new_id
-        else:
-            print("Please enter a valid id")
-
-    id_num = property(get_id_num, set_id_num)
-
-    def get_num_children(self):
-        return self._num_children
-
-    def set_num_children(self, num_children):
-        if isinstance(num_children, int) and 0 < num_children < 70:
-            self._num_children = num_children
-        else:
-            print("Please enter a valid number of children")
-
-    num_children = property(get_num_children, set_num_children)
-        
-
-        
+# c_ Patient
+#
+#     ___ - ____ name age id_num num_children_0
+#         ____.?  ?
+#         ____.?  ?
+#         ____._?  ?
+#         ____._?  ?
+#
+#     ___ get_id_num ____
+#         print("Getter")
+#         r_ ____._i..
+#
+#     ___ set_id_num ____ new_id
+#         print("Setter")
+#         __ isi.. ? st.
+#             ____._i.. _ ?
+#         ____
+#             print("Please enter a valid id")
+#
+#     id_num _ ? g.. s..
+#
+#     ___ get_num_children ____
+#         r_ ____._n..
+#
+#     ___ set_num_children ____ num_children
+#         __ isi.. ? in. an. 0 < ? < 70
+#             ____._? = ?
+#         ____
+#             print("Please enter a valid number of children")
+#
+#     num_children = ? g..  s..
+#
+#
+#
