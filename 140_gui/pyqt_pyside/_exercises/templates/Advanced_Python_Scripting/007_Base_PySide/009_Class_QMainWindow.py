2a0bd85e702c3eed29fc89882aaced4d1c0e3b31
syurskyi <sergejyurskyj@yahoo.com>
2020-02-13 21:01:43 -0800

    Update 009_Class_QMainWindow.py

--- 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/007_Base_PySide/009_Class_QMainWindow.py
+++ 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/007_Base_PySide/009_Class_QMainWindow.py
@@ -1,26 +1,26 @@
-from PySide.QtCore import *
-from PySide.QtGui import *
-
-
-class windowClass(QMainWindow):
-    def __init__(self):
-        super(windowClass, self).__init__()
-        self.w = QWidget()
-        self.setCentralWidget(self.w)  # etot metod est' tol'ko y QMainWindow
-
-        self.menuBar = QMenuBar()      # shto bu programno sozdat' menubar ili statusbar, nyzno snachalo sozdat'
-                                       # etot widget kak objekt
-        self.setMenuBar(self.menuBar)
-
-        self.menu = QMenu('File')
-        self.menuBar.addMenu(self.menu)
-
-        self.act1 = QAction('Open', self)
-        self.menu.addAction(self.act1)
-
-
-if __name__ == '__main__':
-    app = QApplication([])
-    w = windowClass()
-    w.show()
-    app.exec_()
+# ____ ?.? ______ _
+# ____ ?.? ______ _
+#
+#
+# c_ windowClass(QMW..
+#     ___ - ____
+#         s__ ? ____ . -
+#         ____.w = QW..
+#         ____.sCW.. ____.?  # etot metod est' tol'ko y QMainWindow
+#
+#         ____.menuBar _ QMB..     # shto bu programno sozdat' menubar ili statusbar, nyzno snachalo sozdat'
+#                                        # etot widget kak objekt
+#         ____.sMB.. ____.?
+#
+#         ____.menu = QM.. 'File
+#         ____.mB__.aM.. ____.?
+#
+#         ____.act1 _ QA.. 'Open' ____
+#         ____.m__.aA.. ____.?
+#
+#
+# __ _______ __ ____
+#     app = QA..
+#     w = ?
+#     ?.s..
+#     ?.e..
