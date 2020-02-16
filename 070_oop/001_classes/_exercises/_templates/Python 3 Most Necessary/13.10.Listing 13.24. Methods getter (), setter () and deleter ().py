7f96e223199bab0ebfacd93ffdf838ffd7bd73a2
Grey <sergejyurskyj@yahoo.com>
2020-02-16 11:14:21 -0800

    Committed Methods getter (), setter () and deleter () template

--- 070_oop/001_classes/_exercises/_templates/Python 3 Most Necessary/13.10.Listing 13.24. Methods getter (), setter () and deleter ().py
+++ 070_oop/001_classes/_exercises/_templates/Python 3 Most Necessary/13.10.Listing 13.24. Methods getter (), setter () and deleter ().py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-
-class MyClass:
-    def __init__(self, value):
-        self.__var = value
-    @property
-    def v(self):                       # Чтение
-        return self.__var
-    @v.setter
-    def v(self, value):                # Запись
-        self.__var = value
-    @v.deleter
-    def v(self):                       # Удаление
-        del self.__var
-c = MyClass(5)
-c.v = 35                               # Запись
-print(c.v)                             # Чтение
-del c.v                                # Удаление
+# # -*- coding: utf-8 -*-
+#
+# c_ MyClass
+#     ___ -  value
+#         ?__var  ?
+#     ??
+#     ___ v ?                       # Чтение
+#         r_ ??
+#     ??.?
+#     ___ v ? value                # Запись
+#         ?__var _ ?
+#     ??.?
+#     ___ v ?                      # Удаление
+#         del ??
+# c = ? 5
+# c.v = 35                               # Запись
+# print(c.v)                             # Чтение
+# del c.v                                # Удаление
