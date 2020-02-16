59508eb742055feaf9c821b5e0394d3807e0e922
Grey <sergejyurskyj@yahoo.com>
2020-02-16 11:38:18 -0800

    Committed Python_OOP_Object_Oriented_Programming template

--- 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 16/1-Docstrings.py
+++ 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 16/1-Docstrings.py
@@ -1,43 +1,43 @@
-class Flight:
-        
-	def __init__(self, number, origin, destination, num_passengers, total_weight, pilot, crew):
-		self.number = number
-		self.origin = origin
-		self.destination = destination
-		self.num_passengers = num_passengers
-		self._total_weight = total_weight
-		self._pilot = pilot 
-		self._crew = crew
-
-	@property
-	def total_weight(self):
-		return self._total_weight
-
-	@total_weight.setter
-	def total_weight(self, weight):
-		if weight > 0 and isinstance(weight, float):
-			self._total_weight = weight	
-
-	@property
-	def pilot(self):
-		return self._pilot
-
-	@pilot.setter
-	def pilot(self, new_pilot):
-		self._pilot = pilot
-
-	@property
-	def crew(self):
-		return self._crew
-	
-	@crew.setter
-	def crew(self, new_crew):
-		self._crew = new_crew
-
-	def display_flight_data(self):
-		print("== Flight ==")
-		print("Number:", self.number)
-		print("Number of Passengers:", self.num_passengers)
-		print("Weight:", self.weight)
-		print("Pilot:", self._pilot)
-		print("Crew:", self._crew)
+# c_ Flight
+#
+# 	___ -  number origin destination num_passengers total_weight pilot crew
+# 		?  ?
+# 		?  ?
+# 		?  ?
+# 		?  ?
+# 		?_?  ?
+# 		?_?  ?
+# 		?_?  ?
+#
+# 	??
+# 	___ total_weight ?
+# 		r_ ?_t..
+#
+# 	??.?
+# 	___ total_weight  weight
+# 		_ ? > 0 an. isi.. ? fl..
+# 			?_t..  ?
+#
+# 	??
+# 	___ pilot ?
+# 		r_ ?_?
+#
+# 	??.?
+# 	___ pilot  new_pilot
+# 		?_?  ?
+#
+# 	??
+# 	___ crew ?
+# 		r_ ?_?
+#
+# 	??.?
+# 	___ crew  new_crew
+# 		?_c.. _ ?
+#
+# 	___ display_flight_data ?
+# 		print("== Flight ==")
+# 		print("Number:", ??
+# 		print("Number of Passengers:", ??
+# 		print("Weight:", ??
+# 		print("Pilot:", ??
+# 		print("Crew:", ??
--- 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 16/Docstrings-Code/4 - Example - Documenting class and method.py
+++ 070_oop/001_classes/_exercises/_templates/Python_OOP_Object_Oriented_Programming/Section 16/Docstrings-Code/4 - Example - Documenting class and method.py
@@ -1,57 +1,57 @@
-class Calculator:
-
-    def __init__(self, model, year, price, rating=None):
-        self._model = model
-        self._year = year
-        self._price = price
-        self._rating = rating
-
-    # Properties
-
-    @property
-    def model(self):
-        return self._model
-
-    @property
-    def year(self):
-        return self._year
-
-    @property
-    def price(self):
-        return self._price
-
-    @price.setter
-    def price(self, new_price):        
-        if isinstance(new_price, float)and price > 0:
-            self._price = new_price
-        else:
-            print("Please enter a valid price")
-
-    @property
-    def rating(self):
-        return self._rating
-
-    @rating.setter
-    def rating(self, new_rating):
-        if isinstance(new_rating, float)and  0.0 <= new_rating <= 5.0:
-            self._rating = new_rating
-        else:
-            print("Please enter a valid rating")
-
-    # Methods
-
-    def add(self, a, b):
-        return a + b
-
-    def subtract(self, a, b):
-        return a - b
-
-    def multiply(self, a, b):
-        return a * b
-
-    def divide(self, a, b):
-        if b == 0:
-            raise ValueError("b cannot be 0")
-        return a / b
-        
-        
+# c_ Calculator
+#
+#     ___ -  model, year, price, rating_N..
+#         ?_?  ?
+#         ?_?  ?
+#         ?_?  ?
+#         ?_?  ?
+#
+#     # Properties
+#
+#     ??
+#     ___ model ?
+#         r_ ?_?
+#
+#     ??
+#     ___ year ?
+#         r_ ?_?
+#
+#     ??
+#     ___ price ?
+#         r_ ?_?
+#
+#     ??.?
+#     ___ price  new_price
+#         __ isi.. ? fl.. an. price > 0
+#             ?_p.. _ ?
+#         ____
+#             print("Please enter a valid price")
+#
+#     ??
+#     ___ rating ?
+#         r_ ?_?
+#
+#     ??.?
+#     ___ rating  new_rating
+#         __ isi.. ? fl.. an.  0.0 <_ ? <_ 5.0
+#             ?_r..  ?
+#         _____
+#             print("Please enter a valid rating")
+#
+#     # Methods
+#
+#     ___ add  a b
+#         r_ a + b
+#
+#     ___ subtract  a b
+#         r_ a - b
+#
+#     ___ multiply  a b
+#         r_ a * b
+#
+#     ___ divide  a b
+#         __ b __ 0
+#             r_ V.."b cannot be 0")
+#         r_ a / b
+#
+#
