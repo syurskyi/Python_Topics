b362e36f15b021e1eaae2af6ee98b11d9a94fcba
Grey <sergejyurskyj@yahoo.com>
2020-02-16 11:17:24 -0800

    Committed Defining an abstract property template

--- 070_oop/001_classes/_exercises/_templates/Python 3 Most Necessary/13.10.Listing 13.25. Defining an abstract property.py
+++ 070_oop/001_classes/_exercises/_templates/Python 3 Most Necessary/13.10.Listing 13.25. Defining an abstract property.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-
-from abc import ABCMeta, abstractmethod
-class MyClass1(metaclass=ABCMeta):
-    def __init__(self, value):
-        self.__var = value
-    @property
-    @abctractmethod
-    def v(self):                       # Чтение
-        return self.__var
-    @v.setter
-    @abctractmethod
-    def v(self, value):                # Запись
-        self.__var = value
-    @v.deleter
-    @abctractmethod
-    def v(self):                       # Удаление
-        del self.__var
+# # -*- coding: utf-8 -*-
+#
+# ____ a.. ______ A.. a...
+# c_ MyClass1 m..
+#     ___ -  value
+#         ?__var ?
+#     ?p..
+#     ?a..
+#     ___ v ?                       # Чтение
+#         r_ ??
+#     ??.?
+#     ?a..
+#     ___ v ? value               # Запись
+#         ?__var ?
+#     ??.?
+#     ?a..
+#     ___ v ?                       # Удаление
+#         del ??
