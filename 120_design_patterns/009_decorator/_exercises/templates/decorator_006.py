eb3ccaf09eedbdeabb3134faf4c0473c730bbd8f
Grey <sergejyurskyj@yahoo.com>
2020-02-17 13:18:08 -0800

    Committed decorator templates

--- 120_design_patterns/009_decorator/_exercises/templates/decorator_006.py
+++ 120_design_patterns/009_decorator/_exercises/templates/decorator_006.py
@@ -1,151 +1,151 @@
-#!/usr/bin/python
-# Authoer: Spencer.Luo
-# Date: 11/26/2017
-
-# Version 1.0
-#=======================================================================================================================
-from abc import ABCMeta, abstractmethod
-# Introduce ABCMeta and abstractmethod to define abstract classes and abstract methods
-
-class Person(metaclass=ABCMeta):
-    """people"""
-
-    def __init__(self, name):
-        self._name = name
-
-    @abstractmethod
-    def wear(self):
-        print("Dress code")
-
-
-class Engineer(Person):
-    """engineer"""
-
-    def __init__(self, name, skill):
-        super().__init__(name)
-        self.__skill = skill
-
-    def getSkill(self):
-        return self.__skill
-
-    def wear(self):
-        print("I'm " + self.getSkill() + "engineer " + self._name, end="， ")
-        super().wear()
-
-class Teacher(Person):
-    "teacher"
-
-    def __init__(self, name, title):
-        super().__init__(name)
-        self.__title = title
-
-    def getTitle(self):
-        return self.__title
-
-    def wear(self):
-        print("I'm " + self._name + self.getTitle(), end="， ")
-        super().wear()
-
-class ClothingDecorator(Person):
-    """Base class for clothing decorators"""
-
-    def __init__(self, person):
-        self._decorated = person
-
-    def wear(self):
-        self._decorated.wear()
-        self.decorate()
-
-    @abstractmethod
-    def decorate(self):
-        pass
-
-
-class CasualPantDecorator(ClothingDecorator):
-    """Casual pants decorator"""
-
-    def __init__(self, person):
-        super().__init__(person)
-
-    def decorate(self):
-        print("One pair of khaki slacks")
-
-
-class BeltDecorator(ClothingDecorator):
-    """Belt decorator"""
-
-    def __init__(self, person):
-        super().__init__(person)
-
-    def decorate(self):
-        print("Black belt with a silver pin buckle")
-
-class LeatherShoesDecorator(ClothingDecorator):
-    """Leather shoe decorator"""
-
-    def __init__(self, person):
-        super().__init__(person)
-
-    def decorate(self):
-        print("A pair of dark casual leather shoes")
-
-class KnittedSweaterDecorator(ClothingDecorator):
-    """Knitted sweater decorator"""
-
-    def __init__(self, person):
-        super().__init__(person)
-
-    def decorate(self):
-        print("A fuchsia knitted sweater")
-
-
-class WhiteShirtDecorator(ClothingDecorator):
-    """White shirt decorator"""
-
-    def __init__(self, person):
-        super().__init__(person)
-
-    def decorate(self):
-        print("A white shirt")
-
-
-class GlassesDecorator(ClothingDecorator):
-    """Glasses decorator"""
-
-    def __init__(self, person):
-        super().__init__(person)
-
-    def decorate(self):
-        print("A pair of square black frame glasses")
-
-
-
-
-# Test
-#=======================================================================================================================
-def mainDecorator():
-    tony = Engineer("Tony", "Client")
-    pant = CasualPantDecorator(tony)
-    belt = BeltDecorator(pant)
-    shoes = LeatherShoesDecorator(belt)
-    shirt = WhiteShirtDecorator(shoes)
-    sweater = KnittedSweaterDecorator(shirt)
-    glasses = GlassesDecorator(sweater)
-    glasses.wear()
-
-    print()
-    decorateTeacher = GlassesDecorator(WhiteShirtDecorator(LeatherShoesDecorator(Teacher("wells", "professor"))))
-    decorateTeacher.wear()
-
-
-def mainDecorator2():
-    tony = Engineer("Tony", "Client")
-    sweater = KnittedSweaterDecorator(tony)
-    shirt = WhiteShirtDecorator(sweater)
-    glasses = GlassesDecorator(shirt)
-    glasses.wear()
-
-mainDecorator()
-print()
-print('#' * 50)
-mainDecorator2()
+# #!/usr/bin/python
+# # Authoer: Spencer.Luo
+# # Date: 11/26/2017
+#
+# # Version 1.0
+# #=======================================================================================================================
+# ____ a.. ______ A... a...
+# # Introduce ABCMeta and abstractmethod to define abstract classes and abstract methods
+#
+# c_ Person m..
+#     """people"""
+#
+#     ___ - name
+#         _?  ?
+#
+#     ??
+#     ___ wear
+#         print("Dress code")
+#
+#
+# c_ Engineer P..
+#     """engineer"""
+#
+#     ___ - name skill
+#         s___. - n..
+#         __?  ?
+#
+#     ___ getSkill
+#         r_ __s..
+#
+#     ___ wear
+#         print("I'm " + ?g.. + "engineer " + _n.. en._"， ")
+#         s__.w..
+#
+# c_ Teacher P..
+#     "teacher"
+#
+#     ___  - name title
+#         s___. - ?
+#         __?  ?
+#
+#     ___ getTitle
+#         r_ __t..
+#
+#     ___ wear
+#         print("I'm " + _n.. + ?g.. en._"， ")
+#         s__.w..
+#
+# c_ ClothingDecorator P..
+#     """Base c_ for clothing decorators"""
+#
+#     ___ - person
+#         _dec.. _ ?
+#
+#     ___ wear
+#         _d___.w..
+#         d..
+#
+#     ??
+#     ___ decorate
+#         p..
+#
+#
+# c_ CasualPantDecorator(CD..
+#     """Casual pants decorator"""
+#
+#     ___ - person
+#         s__. - ?
+#
+#     ___ decorate
+#         print("One pair of khaki slacks")
+#
+#
+# c_ BeltDecorator CD..
+#     """Belt decorator"""
+#
+#     ___ - person
+#         s__. - ?
+#
+#     ___ decorate
+#         print("Black belt with a silver pin buckle")
+#
+# c_ LeatherShoesDecorator CD..
+#     """Leather shoe decorator"""
+#
+#     ___ - person
+#         s__. - ?
+#
+#     ___ decorate
+#         print("A pair of dark casual leather shoes")
+#
+# c_ KnittedSweaterDecorator CD..
+#     """Knitted sweater decorator"""
+#
+#     ___ - person
+#         s__. - ?
+#
+#     ___ decorate
+#         print("A fuchsia knitted sweater")
+#
+#
+# c_ WhiteShirtDecorator CD..
+#     """White shirt decorator"""
+#
+#     ___ - person
+#         s__. - ?
+#
+#     ___ decorate
+#         print("A white shirt")
+#
+#
+# c_ GlassesDecorator CD...
+#     """Glasses decorator"""
+#
+#     ___ - person
+#         s__. - ?
+#
+#     ___ decorate
+#         print("A pair of square black frame glasses")
+#
+#
+#
+#
+# # Test
+# #=======================================================================================================================
+# ___ mainDecorator
+#     tony = E..("Tony", "Client")
+#     pant = CPD.. ?
+#     belt = BD.. ?
+#     shoes = LSD.. ?
+#     shirt = WSD.. ?
+#     sweater = KSD.. ?
+#     glasses = GD.. ?
+#     ?.w..
+#
+#     print()
+#     decorateTeacher = GD.. WSD.. LSD.. T..."wells", "professor"
+#     ?.w..
+#
+#
+# ___ mainDecorator2
+#     tony = E..("Tony", "Client")
+#     sweater = KSD.. ?
+#     shirt = WSD.. ?
+#     glasses = GD.. ?
+#     ?.w..
+#
+# mainDecorator()
+# print()
+# print('#' * 50)
+# mainDecorator2()
