aeedb58f768e645ef13becfa7a0f744568b31e40
syurskyi <sergejyurskyj@yahoo.com>
2020-02-19 19:43:19 -0800

    Create a_005_Abstract Properties.py

---
+++ 070_oop/008_metaprogramming/_exercises/templates/Abstract Classes in Python/a_005_Abstract Properties.py
@@ -0,0 +1,40 @@
+# # Abstract Properties :
+# # Abstract classes includes attributes in addition to methods, you can require the attributes in concrete classes
+# # by defining them with @abstractproperty.
+#
+# # Python program showing
+# # abstract properties
+#
+# _____ a..
+# ____ a.. _____ A.. a..
+#
+#
+# c_ parent A..
+#     ??.?
+#     ___ geeks
+#         r_ "parent class"
+#
+#
+# c_ child ?
+#
+#     ??
+#     ___ geeks
+#         r_ "child class"
+#
+#
+# ___
+#     r = p..
+#     print ?.g..
+# _______ E.. __ err
+#     print ?
+#
+# r = c..
+# print ?.g..
+#
+#
+# # Output:
+# #
+# # Can't instantiate abstract class parent with abstract methods geeks
+# # child class
+# # In the above example, the Base class cannot be instantiated because it has only an abstract version
+# # of the property getter method.
