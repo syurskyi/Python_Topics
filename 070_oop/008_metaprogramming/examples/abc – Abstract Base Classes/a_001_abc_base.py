5ce808620b5fee89dda7c585a0a739b6fb4345f9
syurskyi <sergejyurskyj@yahoo.com>
2020-02-19 19:24:49 -0800

    Update a_001_abc_base.py

--- 070_oop/008_metaprogramming/examples/abc – Abstract Base Classes/a_001_abc_base.py
+++ 070_oop/008_metaprogramming/examples/abc – Abstract Base Classes/a_001_abc_base.py
@@ -31,3 +31,21 @@ class PluginBase(metaclass=abc.ABCMeta):
     def save(self, output, data):
         """Save the data object to the output."""
 
+
+# python 2
+
+# import abc
+#
+#
+# class PluginBase(object):
+#     __metaclass__ = abc.ABCMeta
+#
+#     @abc.abstractmethod
+#     def load(self, input):
+#         """Retrieve data from the input source and return an object."""
+#         return
+#
+#     @abc.abstractmethod
+#     def save(self, output, data):
+#         """Save the data object to the output."""
+#         return
