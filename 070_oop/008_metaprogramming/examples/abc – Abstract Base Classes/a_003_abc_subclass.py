e6428af4d31c6f2ff851c53449885720028ce075
syurskyi <sergejyurskyj@yahoo.com>
2020-02-19 19:28:57 -0800

    Update a_003_abc_subclass.py

--- 070_oop/008_metaprogramming/examples/abc – Abstract Base Classes/a_003_abc_subclass.py
+++ 070_oop/008_metaprogramming/examples/abc – Abstract Base Classes/a_003_abc_subclass.py
@@ -32,3 +32,22 @@ if __name__ == '__main__':
 # In this case, normal Python class management features are used to recognize SubclassImplementation as implementing
 # the abstract PluginBase.
 
+
+# python 2
+
+# import abc
+# from abc_base import PluginBase
+#
+#
+# class SubclassImplementation(PluginBase):
+#
+#     def load(self, input):
+#         return input.read()
+#
+#     def save(self, output, data):
+#         return output.write(data)
+#
+#
+# if __name__ == '__main__':
+#     print 'Subclass:', issubclass(SubclassImplementation, PluginBase)
+#     print 'Instance:', isinstance(SubclassImplementation(), PluginBase)
