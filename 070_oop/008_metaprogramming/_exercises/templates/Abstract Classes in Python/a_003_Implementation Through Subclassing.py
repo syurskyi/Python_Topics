4087108cf031865ea3343a96cf44f00d93a6b4dc
syurskyi <sergejyurskyj@yahoo.com>
2020-02-19 19:42:25 -0800

    Create a_003_Implementation Through Subclassing.py

---
+++ 070_oop/008_metaprogramming/_exercises/templates/Abstract Classes in Python/a_003_Implementation Through Subclassing.py
@@ -0,0 +1,27 @@
+# # Python program showing
+# # implementation of abstract
+# # class through subclassing
+#
+# _____ a..
+#
+#
+# c_ parent
+#     ___ geeks
+#         p..
+#
+#
+# c_ child ?
+#     ___ geeks
+#         print("child class")
+#
+#     # Driver code
+#
+#
+# print(iss.. c.. p..
+# print(isi.. c.. p..
+#
+#
+# # Output:
+#
+# # True
+# # True
