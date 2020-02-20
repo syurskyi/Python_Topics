0bd8d7b27f03baf4ce7979361b53ac8fc3a814cd
syurskyi <sergejyurskyj@yahoo.com>
2020-02-19 19:28:46 -0800

    Update a_007_abc_concrete_method.py

--- 070_oop/008_metaprogramming/examples/abc – Abstract Base Classes/a_007_abc_concrete_method.py
+++ 070_oop/008_metaprogramming/examples/abc – Abstract Base Classes/a_007_abc_concrete_method.py
@@ -46,3 +46,33 @@ print()
 # base class reading data
 # subclass sorting data
 # ['line one', 'line three', 'line two']
+
+# python 2
+
+# import abc
+# from cStringIO import StringIO
+#
+# class ABCWithConcreteImplementation(object):
+#     __metaclass__ = abc.ABCMeta
+#
+#     @abc.abstractmethod
+#     def retrieve_values(self, input):
+#         print 'base class reading data'
+#         return input.read()
+#
+# class ConcreteOverride(ABCWithConcreteImplementation):
+#
+#     def retrieve_values(self, input):
+#         base_data = super(ConcreteOverride, self).retrieve_values(input)
+#         print 'subclass sorting data'
+#         response = sorted(base_data.splitlines())
+#         return response
+#
+# input = StringIO("""line one
+# line two
+# line three
+# """)
+#
+# reader = ConcreteOverride()
+# print reader.retrieve_values(input)
+# print
