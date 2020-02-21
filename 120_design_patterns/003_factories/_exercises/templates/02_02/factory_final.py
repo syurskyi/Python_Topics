42544106a7d97007c9db9478599c093774a27787
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 10:30:08 -0800

    Update factory_final.py

--- 120_design_patterns/003_factories/_exercises/templates/02_02/factory_final.py
+++ 120_design_patterns/003_factories/_exercises/templates/02_02/factory_final.py
@@ -1,41 +1,36 @@
-class Dog:
-
-	"""A simple dog class"""
-
-	def __init__(self, name):
-		self._name = name
-
-	def speak(self):
-		return "Woof!"
-
-class Cat:
-
-	"""A simple dog class"""
-
-	def __init__(self, name):
-		self._name = name
-
-	def speak(self):
-		return "Meow!"
-
-def get_pet(pet="dog"):
-
-	"""The factory method"""
-
-	pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
-
-	return pets[pet]
-
-d = get_pet("dog")
-
-print(d.speak())
-
-c = get_pet("cat")
-
-print(c.speak())
-
-
-
-
-
+# c_ Dog
+#
+# 	"""A simple dog class"""
+#
+# 	___ - name
+# 		_?  ?
+#
+# 	___ speak
+# 		r_ "Woof!"
+#
+# c_ Cat
+#
+# 	"""A simple dog class"""
+#
+# 	___ - name
+# 		_?  ?
+#
+# 	___ speak
+# 		r_ "Meow!"
+#
+# ___ get_pet pet_"dog")
+#
+# 	"""The factory method"""
+#
+# 	pets = di.. dog_?("Hope") cat_?("Peace"))
+#
+# 	r_ ? p..
+#
+# d = g..("dog")
+#
+# print(?.s..
+#
+# c = g_("cat")
+#
+# print(?.s..
 
