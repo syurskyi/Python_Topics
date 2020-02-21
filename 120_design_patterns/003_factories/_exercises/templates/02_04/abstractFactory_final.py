1bb97133b412df1f1543b46780d3860401a33ddd
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 10:33:55 -0800

    Update abstractFactory_final.py

--- 120_design_patterns/003_factories/_exercises/templates/02_04/abstractFactory_final.py
+++ 120_design_patterns/003_factories/_exercises/templates/02_04/abstractFactory_final.py
@@ -1,51 +1,51 @@
-class Dog:
-	"""One of the objects to be returned"""
-
-	def speak(self):
-		return "Woof!"
-
-	def __str__(self):
-		return "Dog"
-
-
-class DogFactory:
-	"""Concrete Factory"""
-
-	def get_pet(self):
-		"""Returns a Dog object"""
-		return Dog()
-
-	def get_food(self):
-		"""Returns a Dog Food object"""
-		return "Dog Food!"
-
-
-class PetStore:
-	""" PetStore houses our Abstract Factory """
-
-	def __init__(self, pet_factory=None):
-		""" pet_factory is our Abstract Factory """
-
-		self._pet_factory = pet_factory
-
-
-	def show_pet(self):
-		""" Utility method to display the details of the objects retured by the DogFactory """
-
-		pet = self._pet_factory.get_pet()
-		pet_food = self._pet_factory.get_food()
-
-		print("Our pet is '{}'!".format(pet))
-		print("Our pet says hello by '{}'".format(pet.speak()))
-		print("Its food is '{}'!".format(pet_food))
-
-
-#Create a Concrete Factory
-factory = DogFactory()
-
-#Create a pet store housing our Abstract Factory
-shop = PetStore(factory)
-
-#Invoke the utility method to show the details of our pet
-shop.show_pet()
-
+# c_ Dog
+# 	"""One of the objects to be returned"""
+#
+# 	___ speak
+# 		r_ "Woof!"
+#
+# 	___ -s
+# 		r_ "Dog"
+#
+#
+# c_ DogFactory
+# 	"""Concrete Factory"""
+#
+# 	___ get_pet
+# 		"""Returns a Dog object"""
+# 		r_ D..
+#
+# 	___ get_food
+# 		"""Returns a Dog Food object"""
+# 		r_ "Dog Food!"
+#
+#
+# c_ PetStore
+# 	""" PetStore houses our Abstract Factory """
+#
+# 	___ - pet_factory_N..
+# 		""" pet_factory is our Abstract Factory """
+#
+# 		_?  ?
+#
+#
+# 	___ show_pet
+# 		""" Utility method to display the details of the objects retured by the DogFactory """
+#
+# 		pet = _p__.g..
+# 		pet_food = ._p__.g_f_
+#
+# 		print("Our pet is '@'!".f.. ?
+# 		print("Our pet says hello by '@'".f.. ?.s..
+# 		print("Its food is '@'!".f.. ?
+#
+#
+# #Create a Concrete Factory
+# factory = D..
+#
+# #Create a pet store housing our Abstract Factory
+# shop = P.. ?
+#
+# #Invoke the utility method to show the details of our pet
+# ?.s..
+# 
