35d643d2d87ee0477791c34e747da1c3b09222c9
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 17:49:56 -0800

    Update ford.py

--- 120_design_patterns/003_factories/_exercises/templates/8-Abstract Factory Pattern/BeforeAbstractFactory/factories/ford.py
+++ 120_design_patterns/003_factories/_exercises/templates/8-Abstract Factory Pattern/BeforeAbstractFactory/factories/ford.py
@@ -1,17 +1,17 @@
-class FordFiesta:
-    def start(self):
-        print('Ford Fiesta running cheaply.')
-    def stop(self):
-        print('Ford Fiesta shutting down.')
-
-class FordMustang:
-    def start(self):
-        print("Ford Mustang roaring and ready to go!")
-    def stop(self):
-        print('Ford Mustang shutting down.')
-
-class LincolnMKS:
-    def start(self):
-        print('Lincoln MKS running smoothly.')
-    def stop(self):
-        print('Lincoln MKS shutting down.')
+# c_ FordFiesta
+#     ___ start
+#         print('Ford Fiesta running cheaply.')
+#     ___ stop
+#         print('Ford Fiesta shutting down.')
+#
+# c_ FordMustang
+#     ___ start
+#         print("Ford Mustang roaring and ready to go!")
+#     ___ stop
+#         print('Ford Mustang shutting down.')
+#
+# c_ LincolnMKS
+#     ___ start
+#         print('Lincoln MKS running smoothly.')
+#     ___ stop
+#         print('Lincoln MKS shutting down.')
