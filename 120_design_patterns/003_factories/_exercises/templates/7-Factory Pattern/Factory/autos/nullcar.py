90d362caaf72466e930168cc1a24a37b0f0fa829
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 17:35:29 -0800

    Update nullcar.py

--- 120_design_patterns/003_factories/_exercises/templates/7-Factory Pattern/Factory/autos/nullcar.py
+++ 120_design_patterns/003_factories/_exercises/templates/7-Factory Pattern/Factory/autos/nullcar.py
@@ -1,9 +1,9 @@
-from .abs_auto import AbsAuto
-
-class NullCar(AbsAuto):
-
-	def start(self):
-		print ('Unknown car "%s".' % self.name)
-
-	def stop(self):
-		pass
+# ____ .a.. _______ A..
+#
+# c_ NullCar A..
+#
+# 	___ start
+# 		print ('Unknown car "@".'  n..
+#
+# 	___ stop
+# 		p..
