228903826b2fcfcade3a21fc70c9073e7015a4cc
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 17:35:01 -0800

    Update fordfocus.py

--- 120_design_patterns/003_factories/_exercises/templates/7-Factory Pattern/Factory/autos/fordfocus.py
+++ 120_design_patterns/003_factories/_exercises/templates/7-Factory Pattern/Factory/autos/fordfocus.py
@@ -1,9 +1,9 @@
-from .abs_auto import AbsAuto
-
-class FordFocus(AbsAuto):
-
-	def start(self):
-		print('%s running cooly!' % self.name)
-
-	def stop(self):
-		print('%s shutting down.' % self.name)
+# ____ .a.. _______ A..
+#
+# c_ FordFocus A..
+#
+# 	___ start
+# 		print('@ running cooly!'  sn..
+#
+# 	___ stop(self):
+# 		print('@ shutting down.'  n..
