c96e1dd87b28e8de8dd0653947cbd2d9bf7c47d1
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 17:35:13 -0800

    Update jeepsahara.py

--- 120_design_patterns/003_factories/_exercises/templates/7-Factory Pattern/Factory/autos/jeepsahara.py
+++ 120_design_patterns/003_factories/_exercises/templates/7-Factory Pattern/Factory/autos/jeepsahara.py
@@ -1,12 +1,12 @@
-from .abs_auto import AbsAuto
-
-class JeepSahara(AbsAuto):
-
-	def __init__(self, name):
-		self._name = name
-
-	def start(self):
-		print('%s running ruggedly.' % self.name)
-
-	def stop(self):
-		print('%s shutting down.' % self.name)
+# ____ .a.. _______ A..
+#
+# c_ JeepSahara A..
+#
+# 	___ -  name
+# 		_?  ?
+#
+# 	___ start
+# 		print('@ running ruggedly.'  n..
+#
+# 	___ stop(self):
+# 		print('@ shutting down.' n..
