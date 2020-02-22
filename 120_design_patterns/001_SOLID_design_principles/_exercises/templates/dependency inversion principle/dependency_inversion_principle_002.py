f34942525f9c8d6959b7d05608120af0dbfb065f
Grey <sergejyurskyj@yahoo.com>
2020-02-22 15:06:45 -0800

    Committed dependency inversion principle template

--- 120_design_patterns/001_SOLID_design_principles/_exercises/templates/dependency inversion principle/dependency_inversion_principle_002.py
+++ 120_design_patterns/001_SOLID_design_principles/_exercises/templates/dependency inversion principle/dependency_inversion_principle_002.py
@@ -1,56 +1,56 @@
-# Dependency Inversion Principle (DIP)
-
-# High-level objects should not depend on low-level implementation. Both should depend on abstractions.
-from abc import abstractmethod, ABC
-from time import sleep
-
-
-class TurnableOnObject(ABC):
-    def __init__(self):
-        self._on = False
-
-    @property
-    def on(self) -> bool:
-        return self._on
-
-    @abstractmethod
-    def turn_on(self) -> None:
-        pass
-
-
-class Lamp(TurnableOnObject):
-    def turn_on(self):
-        self._on = True
-
-
-class TimedLamp(Lamp):
-    def turn_on(self):
-        self._on = True
-        print("Waiting for 1 second before turning the lamp off.")
-        sleep(1)
-        self._on = False
-
-
-class OnButton:
-    def __init__(self, lamp: Lamp):
-        self.lamp = lamp
-
-    def press(self):
-        if not self.lamp.on:
-            self.lamp.turn_on()
-            print("The lamp has been turned on.")
-        else:
-            print("The lamp was already on.")
-
-
-if __name__ == "__main__":
-    lamp = Lamp()
-    on_button = OnButton(lamp)
-    on_button.press()
-    on_button.press()
-    on_button.press()
-
-    timed_lamp = TimedLamp()
-    on_button_2 = OnButton(timed_lamp)
-    # on_button_2.press()
-    # on_button_2.press()
+# # Dependency Inversion Principle (DIP)
+#
+# # High-level objects should not depend on low-level implementation. Both should depend on abstractions.
+# ____ a.. ______ a.. A..
+# ____ ti.. ______ sl..
+#
+#
+# c_ TurnableOnObject A..
+#     ___ -
+#         _on _ F..
+#
+#     ?p...
+#     ___ on __ b..
+#         r_ _o.
+#
+#     ?a..
+#     ___ turn_on __ N..
+#         p..
+#
+#
+# c_ Lamp T..
+#     ___ turn_on(
+#         _on _ T..
+#
+#
+# c_ TimedLamp L..
+#     ___ turn_on
+#         _o. _ T..
+#         print("Waiting for 1 second before turning the lamp off.")
+#         sl.. 1
+#         _o. _ F..
+#
+#
+# c_ OnButton
+#     ___ - lamp L..
+#         ??  ?
+#
+#     ___ press
+#         __ no. ?l__.o.
+#             ?l__.t...
+#             print("The lamp has been turned on.")
+#         ____
+#             print("The lamp was already on.")
+#
+#
+# __ _______ __ _____
+#     lamp = L..
+#     on_button = O.. ?
+#     ?.p..
+#     ?.p..
+#     ?.p..
+#
+#     timed_lamp = T..
+#     on_button_2 = O.. ?
+#     # on_button_2.press()
+#     # on_button_2.press()
