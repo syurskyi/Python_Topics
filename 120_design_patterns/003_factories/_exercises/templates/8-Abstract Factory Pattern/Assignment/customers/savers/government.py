d327ebea23646a07b29b8b18a5c46e7f54495723
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 17:47:33 -0800

    Update government.py

--- 120_design_patterns/003_factories/_exercises/templates/8-Abstract Factory Pattern/Assignment/customers/savers/government.py
+++ 120_design_patterns/003_factories/_exercises/templates/8-Abstract Factory Pattern/Assignment/customers/savers/government.py
@@ -1,6 +1,6 @@
-from ..abs_cust import AbsCust
-
-
-class Government(AbsCust):
-    def report_type(self):
-        print('"%s" is a government saver.' % self.name)
+# ____ ..a.. ______ A..
+#
+#
+# c_ Government A..
+#     ___ report_type
+#         print('"@" is a government saver.' n..
