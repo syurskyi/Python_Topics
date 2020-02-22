8071a9adc393878f3bf7291293bb37d4e97864b2
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 17:50:39 -0800

    Update __main__.py

--- 120_design_patterns/003_factories/_exercises/templates/8-Abstract Factory Pattern/BeforeAbstractFactory/__main__.py
+++ 120_design_patterns/003_factories/_exercises/templates/8-Abstract Factory Pattern/BeforeAbstractFactory/__main__.py
@@ -1,32 +1,32 @@
-from factories.gm import ChevySpark, ChevyCamaro, CadillacCTS
-from factories.ford import FordFiesta, FordMustang, LincolnMKS
-from random import randint
-
-makers = ('gm', 'ford')
-editions = ('Econonmy', 'Sport', 'Luxury')
-maker = makers[randint(0, 1)]
-edition = editions[randint(0, 2)]
-
-if maker == 'gm':
-    if edition == 'Econonmy':
-        car = ChevySpark()
-    elif edition == 'Sport':
-        car = ChevyCamaro()
-    elif edition == 'Luxury':
-        car = CadillacCTS()
-    else:
-        raise ValueError("Unknown car")
-elif maker == 'ford':
-    if edition == 'Econonmy':
-        car = FordFiesta()
-    elif edition == 'Sport':
-        car = FordMustang()
-    elif edition == 'Luxury':
-        car = LincolnMKS()
-    else:
-        raise ValueError("Unknown car.")
-else:
-    raise ValueError("Unknown maker.")
-
-car.start()
-car.stop()
+# ____ f__.g. ______ CS. CC.. C_CTS
+# ____ f__.f.. ______ FF.. FM. LMKS
+# ____ ra.. ______ r_i_
+#
+# makers _ ('gm', 'ford')
+# editions _ ('Econonmy', 'Sport', 'Luxury')
+# maker _ m.. r_i_ 0 1
+# edition _ e.. r_i_(0 2
+#
+# __ maker __ 'gm':
+#     __ edition __ 'Econonmy':
+#         car _ ChevySpark()
+#     ____ edition __ 'Sport':
+#         car _ ChevyCamaro()
+#     ____ edition __ 'Luxury':
+#         car _ CadillacCTS()
+#     ____
+#         r_ V..("Unknown car")
+# ____ maker __ 'ford':
+#     __ edition __ 'Econonmy':
+#         car _ FordFiesta()
+#     ____ edition __ 'Sport':
+#         car _ FordMustang()
+#     ____ edition __ 'Luxury':
+#         car _ LincolnMKS()
+#     ____
+#         r_ V..("Unknown car.")
+# ____
+#     r_ V..("Unknown maker.")
+#
+# c__.s..
+# c__.s..
