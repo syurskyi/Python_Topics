395e92355fa7b2c52cc2428278b1b53302358648
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 17:51:03 -0800

    Update abstract_factory.py

--- 120_design_patterns/003_factories/_exercises/templates/9324OS_01_code/abstract_factory.py
+++ 120_design_patterns/003_factories/_exercises/templates/9324OS_01_code/abstract_factory.py
@@ -1,86 +1,86 @@
-class Frog:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-    def interact_with(self, obstacle):
-        print('{} the Frog encounters {} and {}!'.format(self, obstacle, obstacle.action()))
-
-class Bug:
-    def __str__(self):
-        return 'a bug'
-
-    def action(self):
-        return 'eats it'
-
-class FrogWorld:
-    def __init__(self, name):
-        print(self)
-        self.player_name = name
-    
-    def __str__(self):
-        return '\n\n\t------ Frog World -------'
-
-    def make_character(self):
-        return Frog(self.player_name)
-
-    def make_obstacle(self):
-        return Bug()
-
-class Wizard:
-    def __init__(self, name):
-        self.name = name
-
-    def __str__(self):
-        return self.name
-
-    def interact_with(self, obstacle):
-        print('{} the Wizard battles against {} and {}!'.format(self, obstacle, obstacle.action()))
-
-class Ork:
-    def __str__(self):
-        return 'an evil ork'
-
-    def action(self):
-        return 'kills it'
-
-class WizardWorld:
-    def __init__(self, name):
-        print(self)
-        self.player_name = name
-
-    def __str__(self):
-        return '\n\n\t------ Wizard World -------'
-
-    def make_character(self):
-        return Wizard(self.player_name)
-
-    def make_obstacle(self):
-        return Ork()
-
-class GameEnvironment:
-    def __init__(self, factory):
-        self.hero = factory.make_character()
-        self.obstacle = factory.make_obstacle()
-
-    def play(self):
-        self.hero.interact_with(self.obstacle)
-
-def main():
-    name = input("Hello. What's your name? ")
-    valid_input = False
-    while not valid_input:
-        try:
-            age = input('Welcome {}. How old are you? '.format(name))
-            age = int(age)
-            valid_input = True
-            game = FrogWorld if age < 18 else WizardWorld
-            environment = GameEnvironment(game(name))
-            environment.play()
-        except ValueError as err:
-            print("Age {} is invalid, please try again...".format(age))
-
-if __name__ == '__main__':
-    main()
+# c_ Frog
+#     ___ - name
+#         ?  ?
+#
+#     ___ -s
+#         r_ n..
+#
+#     ___ interact_with obstacle
+#         print('@ the Frog encounters @ and @!'.f..  ? ?.a..
+#
+# c_ Bug
+#     ___ -s
+#         r_ 'a bug'
+#
+#     ___ action
+#         r_ 'eats it'
+#
+# c_ FrogWorld
+#     ___ - name
+#         print
+#         player_name _ ?
+#
+#     ___ -s
+#         r_ '\n\n\t------ Frog World -------'
+#
+#     ___ make_character
+#         r_ F.. p_n..
+#
+#     ___ make_obstacle
+#         r_ B..
+#
+# c_ Wizard
+#     ___ - name
+#         ?  ?
+#
+#     ___ -s
+#         r_ n..
+#
+#     ___ interact_with obstacle
+#         print('@ the Wizard battles against @ and @!'.f..  ?  ?.a..
+#
+# c_ Ork
+#     ___ -s
+#         r_ 'an evil ork'
+#
+#     ___ action
+#         r_ 'kills it'
+#
+# c_ WizardWorld
+#     ___ - name
+#         print
+#         player_name _ ?
+#
+#     ___ -s
+#         r_ '\n\n\t------ Wizard World -------'
+#
+#     ___ make_character
+#         r_ W.. p_n..
+#
+#     ___ make_obstacle
+#         r_ O..
+#
+# c_ GameEnvironment
+#     ___ - factory
+#         ?hero _ ?.m_c..
+#         ?obstacle _ ?.m_o..
+#
+#     ___ play
+#         ?h__.i_w.. ?o..
+#
+# ___ main
+#     name = i.. "Hello. What's your name? "
+#     valid_input = F..
+#     w___ no. ?
+#         ___
+#             age = in.. ('Welcome @. How old are you? '.f.. n..
+#             age = in. ?
+#             valid_input = T..
+#             game = FW.. __ a.. < 18 ____ WiW..
+#             environment = GE.. g.. n..
+#             ?.p..
+#         _______ V... __ err:
+#             print("Age @ is invalid, please try again...".f.. a..
+#
+# __ _______ __ _____
+#     ?
