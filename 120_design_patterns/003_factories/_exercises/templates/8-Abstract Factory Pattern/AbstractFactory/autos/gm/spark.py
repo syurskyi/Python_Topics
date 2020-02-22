d580459a09038f798237d2f83f4ffd7d10af5f65
syurskyi <sergejyurskyj@yahoo.com>
2020-02-21 17:43:57 -0800

    Update spark.py

--- 120_design_patterns/003_factories/_exercises/templates/8-Abstract Factory Pattern/AbstractFactory/autos/gm/spark.py
+++ 120_design_patterns/003_factories/_exercises/templates/8-Abstract Factory Pattern/AbstractFactory/autos/gm/spark.py
@@ -1,7 +1,7 @@
-from autos.abs_auto import AbsAuto
-
-class ChevySpark(AbsAuto):
-    def start(self):
-        print('Chevy Spark running efficiently.')
-    def stop(self):
-        print('Chevy Spark shutting down.')
+# ____ a__.a.. ______ A..
+#
+# class ChevySpark A..
+#     ___ start
+#         print('Chevy Spark running efficiently.')
+#     ___ stop
+#         print('Chevy Spark shutting down.')
