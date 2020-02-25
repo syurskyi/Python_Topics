f7819fef5d35de45a651b27c08e24a240e8b2140
syurskyi <sergejyurskyj@yahoo.com>
2020-02-24 21:00:26 -0800

    Create a_006_Using Subclass Detection With Registration.py

---
+++ 070_oop/008_metaprogramming/examples/Interface/Implementing an Interface in Python/a_006_Using Subclass Detection With Registration.py
@@ -0,0 +1,46 @@
+# Using Subclass Detection With Registration
+# You must be careful when you're combining .__subclasshook__() with .register(), as .__subclasshook__() takes
+# precedence over virtual subclass registration. To ensure that the registered virtual subclasses are taken into
+# consideration, you must add NotImplemented to the .__subclasshook__() dunder method. The FormalParserInterface
+# would be updated to the following:
+
+import abc
+
+class FormalParserInterface(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'load_data_source') and
+                callable(subclass.load_data_source) and
+                hasattr(subclass, 'extract_text') and
+                callable(subclass.extract_text) or
+                NotImplemented)
+
+class PdfParserNew:
+    """Extract text from a PDF."""
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Overrides FormalParserInterface.load_data_source()"""
+        pass
+
+    def extract_text(self, full_file_path: str) -> dict:
+        """Overrides FormalParserInterface.extract_text()"""
+        pass
+
+@FormalParserInterface.register
+class EmlParserNew:
+    """Extract text from an email."""
+    def load_data_source(self, path: str, file_name: str) -> str:
+        """Overrides FormalParserInterface.load_data_source()"""
+        pass
+
+    def extract_text_from_email(self, full_file_path: str) -> dict:
+        """A method defined only in EmlParser.
+        Does not override FormalParserInterface.extract_text()
+        """
+        pass
+
+print(issubclass(PdfParserNew, FormalParserInterface))  # True
+print(issubclass(EmlParserNew, FormalParserInterface))  # True
+
+# Since you've used registration, you can see that EmlParserNew is considered a virtual subclass of your
+# FormalParserInterface interface. This is not what you wanted since EmlParserNew doesn't override .extract_text().
+# Please use caution with virtual subclass registration!
