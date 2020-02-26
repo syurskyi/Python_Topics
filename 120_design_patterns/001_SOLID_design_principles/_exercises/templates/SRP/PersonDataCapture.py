bc1136c05d627d64a3803cfd4a4b4edcc3832bb8
syurskyi <sergejyurskyj@yahoo.com>
2020-02-25 20:09:47 -0800

    Create PersonDataCapture.py

---
+++ 120_design_patterns/001_SOLID_design_principles/_exercises/templates/SRP/PersonDataCapture.py
@@ -0,0 +1,16 @@
+# ____ ?.P.. ______ P..
+#
+#
+# c_ PersonDataCapture
+#
+# 	?s..
+# 	___ capture __ P..
+# 		output _ P..
+#
+# 		print('Enter your first name: ')
+# 		?.f.. _ in...
+#
+# 		print('Enter your last name: ')
+# 		?.l.. _ in..
+#
+# 		r_ ?
