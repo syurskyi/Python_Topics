c85b645604bece896552a37e8a709f4fb50b04d9
syurskyi <sergejyurskyj@yahoo.com>
2020-02-27 12:12:58 -0800

    Update liskov_substitution_principle_002.py

--- 120_design_patterns/001_SOLID_design_principles/_exercises/templates/lsp/liskov_substitution_principle_002.py
+++ 120_design_patterns/001_SOLID_design_principles/_exercises/templates/lsp/liskov_substitution_principle_002.py
@@ -1,59 +1,59 @@
-# Liskov Substitution Principle (LSP)
-
-# Functions that use references to base classes must be able to use any object of derived classes without knowing it.
-
-# Consequence: Sometimes something "is a" in the real world but should not be an "is a" in the code
-
-
-class Rectangle:
-    def __init__(self, name: str, width: float, height: float):
-        self.name = name
-        self._height = height
-        self._width = width
-
-    def __str__(self):
-        return f"{self.name}: {self._width}x{self._height}"
-
-    def double_height(self):
-        self._height *= 2
-
-    def double_width(self):
-        self._width *= 2
-
-    @property
-    def is_square(self) -> bool:
-        return self._width == self._height
-
-
-class Square(Rectangle):
-    def __init__(self, name: str, side: float):
-        super().__init__(name, side, side)
-
-    def double_height(self):
-        self._height *= 2
-        self._width *= 2
-
-    def double_width(self):
-        self._width *= 2
-        self._height *= 2
-
-
-def double_size(rectangle: Rectangle) -> None:
-    rectangle.double_height()
-    rectangle.double_width()
-
-
-if __name__ == "__main__":
-    rectangle = Rectangle("My Rectangle", 3, 2)
-    print(rectangle)
-
-    # rectangle.double_height()
-    double_size(rectangle)
-    print(rectangle)
-
-    square = Square("My Square", 3)
-    print(square)
-
-    # square.double_height()
-    double_size(square)
-    print(square)
+# # Liskov Substitution Principle (LSP)
+#
+# # Functions that use references to base classes must be able to use any object of derived classes without knowing it.
+#
+# # Consequence: Sometimes something "is a" in the real world but should not be an "is a" in the code
+#
+#
+# c_ Rectangle:
+#     ___ -  name ? width: f.. height f..
+#         ??  ?
+#         _?  ?
+#         _?  ?
+#
+#     ___ -s
+#         r_ _*|n.. : |_w.. x|_h.. "
+#
+#     ___ double_height
+#         _h.. *_ 2
+#
+#     ___ double_width
+#         _w.. *_ 2
+#
+#     ?p..
+#     ___ is_square __ b..
+#         r_ _w.. __ _h...
+#
+#
+# c_ Square R..
+#     ___ -  name ? side f..
+#         s___.- ? ? side
+#
+#     ___ double_height
+#         _h.. *_ 2
+#         _w.. *_ 2
+#
+#     ___ double_width
+#         _w.. *_ 2
+#         _h.. *_ 2
+#
+#
+# ___ double_size rectangle R.. __ N..
+#     ?.d_h..
+#     ?.d_w..
+#
+#
+# __ _______ __ _______
+#     rectangle = R.. "My Rectangle", 3, 2
+#     print ?
+#
+#     # rectangle.double_height()
+#     d_s.. ?
+#     print ?
+#
+#     square = S.. "My Square", 3
+#     print ?
+#
+#     # square.double_height()
+#     d_s.. ?
+#     print ?
