b9b9757a03c47d2ec0404b90f045a6f636c90cf7
syurskyi <sergejyurskyj@yahoo.com>
2020-02-26 20:18:26 -0800

    Update main.py

--- 120_design_patterns/001_SOLID_design_principles/_exercises/templates/lsp/liskovSubstitutionPrinciple/correct/main.py
+++ 120_design_patterns/001_SOLID_design_principles/_exercises/templates/lsp/liskovSubstitutionPrinciple/correct/main.py
@@ -1,70 +1,71 @@
-# -*- coding: utf-8 -*-
-from abc import ABCMeta, abstractmethod
-
-# Base Interface's
-class SavinAccount(object):
-    """Abstract class
-    """
-    __metaclass__ = ABCMeta
-
-class SavinAccountWithWithdrawal(SavinAccount):
-    """Abstract class
-    """
-    __metaclass__ = ABCMeta
-
-    @abstractmethod
-    def withdrawal(self, ammount): raise NotImplementedError
-    
-class SavinAccountWithoutWithdrawal(SavinAccount):
-    """Abstract class
-    """
-    __metaclass__ = ABCMeta
-
-
-# Child Class
-
-class RegularSavingAccount(SavinAccountWithWithdrawal):
-
-    def withdrawal(self, ammount):
-        return ammount
-
-class SalarySavingAccount(SavinAccountWithWithdrawal):
-
-    def withdrawal(self, ammount):
-        return ammount
-
-class FixDepositeSavingAccount(SavinAccountWithoutWithdrawal):
-    def __init__(self):
-        pass
-
-
-# Decorator
-
-def verifiy_withdrawal(func):
-    def inner(*argv):
-        if not issubclass(type(argv[1]), SavinAccountWithWithdrawal) : print("%s is a type %s, the function need a SavinAccountWithWithdrawal type" % (argv[1], type(argv[1]))); exit(0)
-        return func(*argv)
-    return inner
-
-    
-# Context Manager
-
-class AccountManager(object):
-
-    @verifiy_withdrawal
-    def withdrawFromAccount(self, account, ammount):
-        print(account.withdrawal(ammount))
-
-
-if __name__ == '__main__':
-    regular_saving_account = RegularSavingAccount()
-    salary_saving_account = SalarySavingAccount()
-    fix_deposite_saving_account = FixDepositeSavingAccount()
-    ammount = 100
-    
-    account_manager = AccountManager()
-    account_manager.withdrawFromAccount(regular_saving_account, ammount)
-    account_manager.withdrawFromAccount(salary_saving_account, ammount)
-    account_manager.withdrawFromAccount(fix_deposite_saving_account, ammount)
-
-
+# # -*- coding: utf-8 -*-
+# ____ a.. _______ A... a..
+#
+# # Base Interface's
+# c_ SavinAccount o..
+#     """Abstract class
+#     """
+#     m...
+#
+# c_ SavinAccountWithWithdrawal S...
+#     """Abstract class
+#     """
+#     m...
+#
+#     ??
+#     ___ withdrawal ammount
+#         r_ N..
+#
+# c_ SavinAccountWithoutWithdrawal S..
+#     """Abstract class
+#     """
+#     m..
+#
+#
+# # Child Class
+#
+# c_ RegularSavingAccount S..
+#
+#     ___ withdrawal  ammount
+#         r_ ?
+#
+# c_ SalarySavingAccount S..
+#
+#     ___ withdrawal  ammount
+#         r_ ?
+#
+# c_ FixDepositeSavingAccount S..
+#     ___ -
+#         p..
+#
+#
+# # Decorator
+#
+# ___ verifiy_withdrawal func
+#     ___ inner $
+#         __ no. iss.. ty.. ar.. 1 , S.. : print("@ is a type @, the function need a SavinAccountWithWithdrawal type"  ar.. 1 ty.. ar.. 1||||; exit(0)
+#         r_ f.. $
+#     r_ i..
+#
+#
+# # Context Manager
+#
+# c_ AccountManager o..
+#
+#     ?v..
+#     ___ withdrawFromAccount account ammount
+#         print a__.w.. am..
+#
+#
+# __ ______ __ ______
+#     regular_saving_account _ R...
+#     salary_saving_account _ S...
+#     fix_deposite_saving_account _ F..
+#     ammount _ 100
+#
+#     account_manager _ A..
+#     ?.w____ r___ a...
+#     ?.w____ s____ a...
+#     ?.w____ f____ a...
+#
+#
