480d22b8155ad8ecd459263d5251a62ac259afd2
syurskyi <sergejyurskyj@yahoo.com>
2020-02-26 20:25:04 -0800

    Update L.py

--- 120_design_patterns/001_SOLID_design_principles/_exercises/templates/lsp/L.py
+++ 120_design_patterns/001_SOLID_design_principles/_exercises/templates/lsp/L.py
@@ -1,45 +1,43 @@
-"""
-Liskov Substitution Principle
-
-
-
-bad
-"""
-
-
-def animal_leg_count(animals: list):
-    for animal in animals:
-        if isinstance(animal, Lion):
-            print(lion_leg_count(animal))
-        elif isinstance(animal, Mouse):
-            print(mouse_leg_count(animal))
-        elif isinstance(animal, Pigeon):
-            print(pigeon_leg_count(animal))
-
-
-animal_leg_count(animals)
-
-"""
-better
-if we follow LSP, there is no need for above if and else statement. As a sub-class must be substitutable for its super-class
-"""
-
-
-def animal_leg_count(animals: list):
-    for animal in animals:
-        print(animal.leg_count())
-
-
-animal_leg_count(animals)
-
-
-
-class Animal:
-    def leg_count(self):
-        pass
-
-
-class Lion(Animal):
-    def leg_count(self):
-
-
+# """
+# Liskov Substitution Principle
+#
+# bad
+# """
+#
+#
+# def animal_leg_count(animals: list):
+#     for animal in animals:
+#         if isinstance(animal, Lion):
+#             print(lion_leg_count(animal))
+#         elif isinstance(animal, Mouse):
+#             print(mouse_leg_count(animal))
+#         elif isinstance(animal, Pigeon):
+#             print(pigeon_leg_count(animal))
+#
+#
+# animal_leg_count(animals)
+#
+# """
+# better
+# if we follow LSP, there is no need for above if and else statement. As a sub-class must be substitutable for its super-class
+# """
+#
+#
+# ___ animal_leg_count animals li..
+#     ___ animal __ a..
+#         print ?.l_c..
+#
+#
+# ? a..
+#
+#
+#
+# c_ Animal
+#     ___ leg_count
+#         p..
+#
+#
+# c_ Lion A..
+#     ___ leg_count
+#
+#
