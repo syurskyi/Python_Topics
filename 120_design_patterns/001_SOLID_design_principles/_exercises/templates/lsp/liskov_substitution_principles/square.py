0a39b26f52b09384deb33b6e80290d3194649485
syurskyi <sergejyurskyj@yahoo.com>
2020-02-26 20:11:01 -0800

    Update square.py

--- 120_design_patterns/001_SOLID_design_principles/_exercises/templates/lsp/liskov_substitution_principles/square.py
+++ 120_design_patterns/001_SOLID_design_principles/_exercises/templates/lsp/liskov_substitution_principles/square.py
@@ -1,32 +1,32 @@
-from src.liskov_substitution_principles.rectangle import Rectangle
-
-
-class Square(Rectangle):
-    ''' This example violates LSP, because even though Square follows
-    Rectangle contract, the method's body does not.
-    '''
-
-    def __init__(self, widthHeight):
-        self.width = widthHeight
-        self.height = widthHeight
-
-    @property
-    def width(self):
-        return self.__width
-
-    @property
-    def height(self):
-        return self.__height
-
-    @width.setter
-    def width(self, width):
-        self.__width = width
-        self.__height = width
-
-    @height.setter
-    def height(self, height):
-        self.__height = height
-        self.__width = height
-
-    def get_area(self):
-        return self.width * self.height
+# ___ ?.r.. _______ R..
+#
+#
+# c_ Square R..
+#     ''' This example violates LSP, because even though Square follows
+#     Rectangle contract, the method's body does not.
+#     '''
+#
+#     ___ - widthHeight)
+#         width _ ?
+#         height _ ?
+#
+#     ?p..
+#     ___ width
+#         r_ __?
+#
+#     ?p..
+#     ___ height
+#         r_ __?
+#
+#     ??.?
+#     ___ width width
+#         __?  ?
+#         __h.. _ ?
+#
+#     ??.?
+#     ___ height height
+#         __?   ?
+#         __w.. _ ?
+#
+#     ___ get_area
+#         r_ ?  * ?
