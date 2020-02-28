cbdafd8b6c06426a5e7f307766b5f2ebb627c8a9
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 15:29:24 -0800

    Update another_singleton.py

--- 120_design_patterns/005_singleton/_exercises/templates/Chapter02/another_singleton.py
+++ 120_design_patterns/005_singleton/_exercises/templates/Chapter02/another_singleton.py
@@ -1,13 +1,11 @@
-__author__ = 'Chetan'
-
-class Singleton(object):
-    cls_attr = None
-    def __new__(cls, *args, **kwargs):
-        if Singleton.cls_attr:
-            raise Exception
-        Singleton.cls_attr = 1
-        return super(Singleton, cls).__new__(cls, *args, **kwargs)
-
-s = Singleton()
-print("Object created", s) ## Object got created
-s1 = Singleton() ## Exception thrown
+# c_ Singleton o..
+#     cls_attr _ N..
+#     ___ -n ___ $ $$
+#         __ S__.c_a..
+#             r_ E..
+#         S___.c_a.. _ 1
+#         r_ s.. S.. ___ . -n ___ $ $$
+#
+# s = ?
+# print("Object created", s) ## Object got created
+# s1 = Singleton() ## Exception thrown
