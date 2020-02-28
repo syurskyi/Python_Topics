c900e1e125c84d80d337f19b918e22ab55ebee32
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 15:22:14 -0800

    Update singleton_classic.py

--- 120_design_patterns/005_singleton/_exercises/templates/5-Singleton Pattern/singleton_classic.py
+++ 120_design_patterns/005_singleton/_exercises/templates/5-Singleton Pattern/singleton_classic.py
@@ -1,19 +1,19 @@
-class Singleton(object):
-    ans = None
-
-    @staticmethod
-    def instance():
-        if  '_instance' not in Singleton.__dict__:
-             Singleton._instance = Singleton()
-        return Singleton._instance
-
-s1 = Singleton.instance()
-s2 = Singleton.instance()
-
-assert s1 is s2
-
-s1.ans = 42
-
-assert s2.ans == s1.ans
-print('Assertions passed.')
-     
+# c_ Singleton o..
+#     ans _ N..
+# 
+#     ?s...
+#     ___ instance
+#         __  '_i..' no. __ S___. -d
+#              S___._i... _ S..
+#         r_ S___._i..
+# 
+# s1 _ ?.i..
+# s2 _ ?.i..
+# 
+# assert s1 is s2
+# 
+# s1.ans _ 42
+# 
+# assert s2.ans == s1.ans
+# print('Assertions passed.')
+#      
