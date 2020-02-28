8f6dc64a74c0541ee6d38919c1d9261a4fad8217
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 13:19:06 -0800

    Update 005_prototype.py

--- 120_design_patterns/004_prototype/_exercises/templates/005_prototype.py
+++ 120_design_patterns/004_prototype/_exercises/templates/005_prototype.py
@@ -1,57 +1,57 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-*TL;DR80
-Creates new object instances by cloning prototype.
-"""
-
-
-class Prototype(object):
-
-    value = 'default'
-
-    def clone(self, **attrs):
-        """Clone a prototype and update inner attributes dictionary"""
-        # Python in Practice, Mark Summerfield
-        obj = self.__class__()
-        obj.__dict__.update(attrs)
-        return obj
-
-
-class PrototypeDispatcher(object):
-
-    def __init__(self):
-        self._objects = {}
-
-    def get_objects(self):
-        """Get all objects"""
-        return self._objects
-
-    def register_object(self, name, obj):
-        """Register an object"""
-        self._objects[name] = obj
-
-    def unregister_object(self, name):
-        """Unregister an object"""
-        del self._objects[name]
-
-
-def main():
-    dispatcher = PrototypeDispatcher()
-    prototype = Prototype()
-
-    d = prototype.clone()
-    a = prototype.clone(value='a-value', category='a')
-    b = prototype.clone(value='b-value', is_checked=True)
-    dispatcher.register_object('objecta', a)
-    dispatcher.register_object('objectb', b)
-    dispatcher.register_object('default', d)
-    print([{n: p.value} for n, p in dispatcher.get_objects().items()])
-
-
-if __name__ == '__main__':
-    main()
-
-### OUTPUT ###
-# [{'objectb': 'b-value'}, {'default': 'default'}, {'objecta': 'a-value'}]
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+# 
+# """
+# *TL;DR80
+# Creates new object instances by cloning prototype.
+# """
+# 
+# 
+# c_ Prototype o..
+# 
+#     value _ 'default'
+# 
+#     ___ clone $$attrs
+#         """Clone a prototype and update inner attributes dictionary"""
+#         # Python in Practice, Mark Summerfield
+#         obj _ ?. -c
+#         ?. -d .up.. a..
+#         r_ ?
+# 
+# 
+# c_ PrototypeDispatcher o..
+# 
+#     ___ -
+#         _o.. _     # dict
+# 
+#     ___ get_objects
+#         """Get all objects"""
+#         r_ _o..
+# 
+#     ___ register_object name obj
+#         """Register an object"""
+#         _o..|n.. _ ?
+# 
+#     ___ unregister_object name
+#         """Unregister an object"""
+#         de. _o..|n..
+# 
+# 
+# ___ main
+#     dispatcher _ PD..
+#     prototype _ P..
+# 
+#     d _ p___.cl..
+#     a _ p___.cl.. value_'a-value', category_'a')
+#     b _ p___.cl.. value_'b-value', is_checked_True)
+#     d___.r_o.. 'objecta', a
+#     d___.r_o.. 'objectb', b
+#     d___.r_o.. 'default', d
+#     print n p.v... ___ ? ? __ d___.g_o___.i..
+# 
+# 
+# __ ______ __ ______
+#     ?
+# 
+# ### OUTPUT ###
+# # [{'objectb': 'b-value'}, {'default': 'default'}, {'objecta': 'a-value'}]
