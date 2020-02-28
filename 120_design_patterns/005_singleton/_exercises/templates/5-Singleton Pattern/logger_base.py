1ee451d472fb576147f78a9922f09bd2ec3381d7
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 15:03:45 -0800

    Update logger_base.py

--- 120_design_patterns/005_singleton/_exercises/templates/5-Singleton Pattern/logger_base.py
+++ 120_design_patterns/005_singleton/_exercises/templates/5-Singleton Pattern/logger_base.py
@@ -1,18 +1,18 @@
-import datetime
-from singleton_meta import Singleton
-
-class Logger(metaclass=Singleton):
-    log_file = None
-
-    def __init__(self, path):
-        if self.log_file is None:
-            self.log_file = open(path,mode='w')
-
-    def write_log(self, log_record):
-        now = str(datetime.datetime.now())
-        record = '%s: %s\n' % (now, log_record)
-        self.log_file.write(record)
-
-    def close_log(self):
-        self.log_file.close() 
-        self.log_file = None       
+# _______ d_t_
+# ____ singleton_meta _______ S..
+# 
+# c_ Logger m..._S..
+#     log_file _ N..
+# 
+#     ___ - path
+#         __ .l.. __ N..
+#             .l.. _ o.. ? mode_ _
+# 
+#     ___ write_log  log_record
+#         now = st. d_t_.d_t_.no.
+#         record = '@: @\n'  ? ?
+#         .l___.w.. ?
+# 
+#     ___ close_log
+#         .l__.cl..
+#         .l__ _ N.. 
