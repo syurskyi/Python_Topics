b98e520bfa88751a9790634649301e5426930a46
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 15:33:17 -0800

    Update borg_singleton_2.py

--- 120_design_patterns/005_singleton/_exercises/templates/Chapter02/borg_singleton_2.py
+++ 120_design_patterns/005_singleton/_exercises/templates/Chapter02/borg_singleton_2.py
@@ -1,8 +1,6 @@
-__author__ = 'Chetan'
-
-class Borg(object):
-     _shared_state = {}
-     def __new__(cls, *args, **kwargs):
-       obj = super(Borg, cls).__new__(cls, *args, **kwargs)
-       obj.__dict__ = cls._shared_state
-       return obj
+# c_ Borg o..
+#      _shared_state   # dict
+#      ___ -n ___, $ $$
+#        obj = s.. B.. ___ . n ___ $ $$
+#        ?. -d _ ___._?
+#        r_ ?
