1ac1d4f2369bad02ba9c84116cc36ee19f0a6300
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 15:55:26 -0800

    Update real_world_scenario_1.py

--- 120_design_patterns/005_singleton/_exercises/templates/Chapter02/real_world_scenario_1.py
+++ 120_design_patterns/005_singleton/_exercises/templates/Chapter02/real_world_scenario_1.py
@@ -1,27 +1,25 @@
-__author__ = 'Chetan'
-
-import sqlite3
-
-class MetaSingleton(type):
-    
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-
-class Database(metaclass=MetaSingleton):
-    connection = None
-    def connect(self):
-        if self.connection is None:
-            self.connection = sqlite3.connect("db.sqlite3")
-            self.cursorobj = self.connection.cursor()
-        return self.cursorobj
-
-db1 = Database().connect()
-db2 = Database().connect()
-
-print ("Database Objects DB1", db1)
-print ("Database Objects DB2", db2)
-
+# ______ sq_3
+# 
+# c_ MetaSingleton ty..
+#     
+#     _instances    # dict
+#     ___ -c ___ $ $$
+#         __ ___ no. __ ___._?
+#             ___._i..|___ _ s.. M... ___ . -c $ $$
+#         r_ ___._?|___
+# 
+# 
+# c_ Database(m.._M..
+#     connection _ N..
+#     ___ connect
+#         __ .c.. __ N..
+#             .c.. _ sq_3.c.. "db.sqlite3"
+#             .cursorobj _ .co__.cu..
+#         r_ ?
+# 
+# db1 = ? .c..
+# db2 = ? .c..
+# 
+# print ("Database Objects DB1" _1
+# print ("Database Objects DB2" _2
+# 
