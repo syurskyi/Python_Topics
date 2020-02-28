05e5d70d686ddef517f38f393bf7dfd688094335
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 15:15:45 -0800

    Update logger_mono.py

--- 120_design_patterns/005_singleton/_exercises/templates/5-Singleton Pattern/logger_mono.py
+++ 120_design_patterns/005_singleton/_exercises/templates/5-Singleton Pattern/logger_mono.py
@@ -1,18 +1,18 @@
-import datetime
-from monostate import MonoState
-
-class Logger(MonoState):
-    log_file = None
-
-    def __init__(self, path):
-        if self.log_file is None:
-            self.log_file = open(path,mode='w')
-
-    def write_log(self, log_record):
-        now = str(datetime.datetime.now())
-        record = '%s: %s\n' % (now, log_record)
-        self.log_file.write(record)
-
-    def close_log(self):
-        self.log_file.close() 
-        self.log_file = None       
+# _______ d_t_
+# _____ monostate _______ M..
+# 
+# c_ Logger M..
+#     log_file _ N..
+# 
+#     ___ - path
+#         __ .l... __ N..
+#             .l... _ o.. ? mode_ _
+# 
+#     ___ write_log log_record
+#         now _ st. d_t_.d_t_.n..
+#         record _ '@: @\n' ? l..
+#         .l....w.. ?
+# 
+#     ___ close_log 
+#         .l....cl..
+#         .l... _ N..       
