a3b588c3a06f17bd15890005a602361a06125806
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 15:50:01 -0800

    Update metaclass_singleton.py

--- 120_design_patterns/005_singleton/_exercises/templates/Chapter02/metaclass_singleton.py
+++ 120_design_patterns/005_singleton/_exercises/templates/Chapter02/metaclass_singleton.py
@@ -1,16 +1,14 @@
-__author__ = 'Chetan'
-
-class MetaSingleton(type):
-    
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(MetaSingleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-class Logger(metaclass=MetaSingleton):
-    pass
-
-logger1 = Logger()
-logger2 = Logger()
-print(logger1, logger2)
+# c_ MetaSingleton ty..
+#     
+#     _instances    # dict
+#     ___ -c ___ $ $$
+#         __ ___ no. __ ___._?
+#             ___._?|___ _ s.. M.. ___ . -c $ $$
+#         r_ ___._?|___
+# 
+# c_ Logger m.._M..
+#     p..
+# 
+# logger1 = ?
+# logger2 = ?
+# print(logger1, logger2)
