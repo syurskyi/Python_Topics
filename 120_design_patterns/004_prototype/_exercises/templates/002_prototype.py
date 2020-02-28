05d28f794a8f67d60063eb89c9e6d395dfa35b3f
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 13:06:48 -0800

    Update 002_prototype.py

--- 120_design_patterns/004_prototype/_exercises/templates/002_prototype.py
+++ 120_design_patterns/004_prototype/_exercises/templates/002_prototype.py
@@ -1,92 +1,92 @@
-import copy
-
-
-class Prototype:
-    _type = None
-    _value = None
-
-    def clone(self):
-        pass
-
-    def getType(self):
-        return self._type
-
-    def getValue(self):
-        return self._value
-
-
-class Type1(Prototype):
-
-    def __init__(self, number):
-        self._type = "Type1"
-        self._value = number
-
-    def clone(self):
-        return copy.copy(self)
-
-
-class Type2(Prototype):
-    """ Concrete prototype. """
-
-    def __init__(self, number):
-        self._type = "Type2"
-        self._value = number
-
-    def clone(self):
-        return copy.copy(self)
-
-
-class ObjectFactory:
-    """ Manages prototypes.
-    Static factory, that encapsulates prototype
-    initialization and then allows instatiation
-    of the classes from these prototypes.
-    """
-
-    __type1Value1 = None
-    __type1Value2 = None
-    __type2Value1 = None
-    __type2Value2 = None
-
-    @staticmethod
-    def initialize():
-        ObjectFactory.__type1Value1 = Type1(1)
-        ObjectFactory.__type1Value2 = Type1(2)
-        ObjectFactory.__type2Value1 = Type2(1)
-        ObjectFactory.__type2Value2 = Type2(2)
-
-    @staticmethod
-    def getType1Value1():
-        return ObjectFactory.__type1Value1.clone()
-
-    @staticmethod
-    def getType1Value2():
-        return ObjectFactory.__type1Value2.clone()
-
-    @staticmethod
-    def getType2Value1():
-        return ObjectFactory.__type2Value1.clone()
-
-    @staticmethod
-    def getType2Value2():
-        return ObjectFactory.__type2Value2.clone()
-
-
-def main():
-    ObjectFactory.initialize()
-
-    instance = ObjectFactory.getType1Value1()
-    print("%s: %s" % (instance.getType(), instance.getValue()))
-
-    instance = ObjectFactory.getType1Value2()
-    print("%s: %s" % (instance.getType(), instance.getValue()))
-
-    instance = ObjectFactory.getType2Value1()
-    print("%s: %s" % (instance.getType(), instance.getValue()))
-
-    instance = ObjectFactory.getType2Value2()
-    print("%s: %s" % (instance.getType(), instance.getValue()))
-
-
-if __name__ == "__main__":
-    main()
+# ______ c__
+# 
+# 
+# c_ Prototype:
+#     _type _ N..
+#     _value _ N..
+# 
+#     ___ clone
+#         p..
+# 
+#     ___ getType
+#         r_ _t..
+# 
+#     ___ getValue
+#         r_ _v..
+# 
+# 
+# c_ Type1 P..
+# 
+#     ___ - number
+#         _t.. _ "Type1"
+#         _v.. _ ?
+# 
+#     ___ clone
+#         r_ c____.c___ ?
+# 
+# 
+# c_ Type2 P..
+#     """ Concrete prototype. """
+# 
+#     ___ - number
+#         _t.. _ "Type2"
+#         _v.. _ ?
+# 
+#     ___ clone
+#         r_ c____.c___ ?
+# 
+# 
+# c_ ObjectFactory
+#     """ Manages prototypes.
+#     Static factory, that encapsulates prototype
+#     initialization and then allows instatiation
+#     of the classes from these prototypes.
+#     """
+# 
+#     __type1Value1 _ N..
+#     __type1Value2 _ N..
+#     __type2Value1 _ N..
+#     __type2Value2 _ N..
+# 
+#     ??
+#     ___ initialize
+#         ?.__1 _ T1 1
+#         ?.__2 _ T1 2
+#         ?.__2_1 _ T2 1
+#         ?.__2_2 _ T2 2
+# 
+#     ??
+#     ___ getType1Value1
+#         r_ ?.__1.cl..
+# 
+#     ??
+#     ___ getType1Value2
+#         r_ ?.__2.cl..
+# 
+#     ??
+#     ___ getType2Value1
+#         r_ ?.__2_1.cl..
+# 
+#     ??
+#     ___ getType2Value2():
+#         r_ ?.__2_2.clone()
+# 
+# 
+# ___ main
+#     ?.i...
+# 
+#     instance _ ?.ge_1
+#     print("@: @"  ?.gT.. ?.gV..
+# 
+#     instance _ ?.ge_2
+#     print("@: @"  ?.gT.. ?.gV..
+# 
+#     instance _ ?.ge_2__1
+#     print("@: @"  ?.gT.. ?.gV..
+# 
+#     instance _ ?.ge_2_2
+#     print("@: @"  ?.gT.., ?.gV..
+# 
+# 
+# __ ______ __ ______
+#     ?
