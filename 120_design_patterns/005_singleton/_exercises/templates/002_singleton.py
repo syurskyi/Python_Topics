5f8adb461bb7e65e60dc35d3d85df2eb8020ff2d
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 16:12:40 -0800

    Update 002_singleton.py

--- 120_design_patterns/005_singleton/_exercises/templates/002_singleton.py
+++ 120_design_patterns/005_singleton/_exercises/templates/002_singleton.py
@@ -1,22 +1,22 @@
-class Singleton:
-   __instance = None
-   @staticmethod
-   def getInstance():
-      """ Static access method. """
-      if Singleton.__instance == None:
-         Singleton()
-      return Singleton.__instance
-   def __init__(self):
-      """ Virtually private constructor. """
-      if Singleton.__instance != None:
-         raise Exception("This class is a singleton!")
-      else:
-         Singleton.__instance = self
-s = Singleton()
-print(s)
-
-s = Singleton.getInstance()
-print(s)
-
-s = Singleton.getInstance()
-print(s)
+# c_ Singleton
+#    __instance _ N..
+#    ?s..
+#    ___ getInstance
+#       """ Static access method. """
+#       __ S__.__? __ N..
+#          S__
+#       r_ S__.__?
+#    ___ -
+#       """ Virtually private constructor. """
+#       __ S__.__? !_ N..
+#          r_ E...("This class is a singleton!")
+#       ____
+#          S__.__? _ ____
+# s = S__
+# print(s)
+# 
+# s = S__.gI..
+# print ?
+# 
+# s = S__.gI..
+# print ?
