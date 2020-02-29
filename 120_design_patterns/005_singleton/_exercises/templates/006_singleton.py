e5a09ae7b5410e163f7054fd9ef17bb0924b6d0c
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 16:34:41 -0800

    Update 006_singleton.py

--- 120_design_patterns/005_singleton/_exercises/templates/006_singleton.py
+++ 120_design_patterns/005_singleton/_exercises/templates/006_singleton.py
@@ -1,72 +1,72 @@
-#!/usr/bin/env python
-# Written by: DGC
-
-import abc
-
-#==============================================================================
-class Singleton(object):
-    """ A generic base class to derive any singleton class from. """
-    __metaclass__ = abc.ABCMeta
-    __instance = None
-
-    def __new__(new_singleton, *arguments, **keyword_arguments):
-        """Override the __new__ method so that it is a singleton."""
-        if new_singleton.__instance is None:
-            new_singleton.__instance = object.__new__(new_singleton)
-            new_singleton.__instance.init(*arguments, **keyword_arguments)
-        return new_singleton.__instance
-
-    @abc.abstractmethod
-    def init(self, *arguments, **keyword_arguments):
-        """ 
-        as __init__ will be called on every new instance of a base class of 
-        Singleton we need a function for initialisation. This will only be 
-        called once regardless of how many instances of Singleton are made.
-        """
-        raise
-
-#==============================================================================
-class GlobalState(Singleton):
-
-    def init(self):
-        self.value = 0
-        print("init() called once")
-        print("")
-
-    def __init__(self):
-        print("__init__() always called")
-        print("")
-
-class DerivedGlobalState(GlobalState):
-    
-    def __init__(self):
-        print("derived made")
-        super(DerivedGlobalState, self).__init__()
-
-    def thing(self):
-        print(self.value)
-
-#==============================================================================
-if (__name__ == "__main__"):
-    d = DerivedGlobalState()
-    print(type(d))
-    d.thing()
-    d.value = -20
-    e = DerivedGlobalState()
-    e.thing()
-    f = DerivedGlobalState()
-    f.thing()
-    
-    a = GlobalState()
-    # value is default, 0
-    print("Expecting 0, value = %i" %(a.value))
-    print("")
-
-    # set the value to 5
-    a.value = 5
-
-    # make a new object, the value will still be 5
-    b = GlobalState()
-    print("Expecting 5, value = %i" %(b.value))
-    print("")
-    print("Is a == b? " + str(a == b))
+# #!/usr/bin/env python
+# # Written by: DGC
+# 
+# ______ a..
+# 
+# #==============================================================================
+# c_ Singleton o..
+#     """ A generic base class to derive any singleton class from. """
+#     m..
+#     __instance _ N..
+# 
+#     ___  -n new_singleton, $arguments $$keyword_arguments
+#         """Override the __new__ method so that it is a singleton."""
+#         __ n___.__? __ N..
+#             n___.__? _ ob__. -n  n___
+#             n___.__?.in.. $a.. $$k..
+#         r_ n___.__?
+# 
+#     ??.?
+#     ___ init  $arguments $$keyword_arguments
+#         """ 
+#         as __init__ will be called on every new instance of a base class of 
+#         Singleton we need a function for initialisation. This will only be 
+#         called once regardless of how many instances of Singleton are made.
+#         """
+#         r_
+# 
+# #==============================================================================
+# c_ GlobalState S..
+# 
+#     ___ init
+#         ?value = 0
+#         print("init() called once")
+#         print("")
+# 
+#     ___ -
+#         print("__init__() always called")
+#         print("")
+# 
+# c_ DerivedGlobalState G..
+#     
+#     ___ -
+#         print("derived made")
+#         s__D.. ____ . -
+# 
+#     ___ thing 
+#         print .v..
+# 
+# #==============================================================================
+# __ _____ __ _____
+#     d = D..
+#     print(t.. ?
+#     ?.t..
+#     ?.v.. = -20
+#     e = D..
+#     ?.t..
+#     f = D..
+#     ?.t...
+#     
+#     a = G..
+#     # value is default, 0
+#     print("Expecting 0, value = %i"  ?.v..
+#     print("")
+# 
+#     # set the value to 5
+#     ?.v.. = 5
+# 
+#     # make a new object, the value will still be 5
+#     b = G..
+#     print("Expecting 5, value = %i"  ?.v..
+#     print("")
+#     print("Is a == b? " + str(a == b))
