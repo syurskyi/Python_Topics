81db91cd537d0ae88346b0a46e2696b3dae8418a
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 16:42:38 -0800

    Update 008_singleton.py

--- 120_design_patterns/005_singleton/_exercises/templates/008_singleton.py
+++ 120_design_patterns/005_singleton/_exercises/templates/008_singleton.py
@@ -1,39 +1,39 @@
-# coding: utf-8
-
-"""
-Одиночка (Singleton) - паттерн, порождающий объекты.
-Гарантирует, что у класса есть только один экземпляр, и предоставляет к нему глобальную точку доступа.
-С помощью паттерна одиночка могут быть реализованы многие паттерны (абстрактная фабрика, строитель, прототип).
-"""
-
-
-class SingletonMeta(type):
-    def __init__(cls, *args, **kwargs):
-        cls._instance = None
-        # глобальная точка доступа `Singleton.get_instance()`
-        cls.get_instance = classmethod(lambda c: c._instance)
-        super(SingletonMeta, cls).__init__(*args, **kwargs)
-
-    def __call__(cls, *args, **kwargs):
-        if not cls._instance:
-            cls._instance = super(SingletonMeta, cls).__call__(*args, **kwargs)
-        return cls._instance
-
-
-class Singleton(object):
-    __metaclass__ = SingletonMeta
-
-    def __init__(self, name):
-        self._name = name
-
-    def get_name(self):
-        return self._name
-
-
-obj1 = Singleton('MyInstance 1')
-print(obj1.get_name())  # MyInstance 1
-
-obj2 = Singleton('MyInstance 2')
-print(obj2.get_name())  # MyInstance 1
-
-print(obj1 is obj2 is Singleton.get_instance())  # True
+# # coding: utf-8
+# 
+# """
+# Одиночка (Singleton) - паттерн, порождающий объекты.
+# Гарантирует, что у класса есть только один экземпляр, и предоставляет к нему глобальную точку доступа.
+# С помощью паттерна одиночка могут быть реализованы многие паттерны (абстрактная фабрика, строитель, прототип).
+# """
+# 
+# 
+# c_ SingletonMeta ty..
+#     ___ - ___ $ $$
+#         ___._instance _ N..
+#         # глобальная точка доступа `Singleton.get_instance()`
+#         ___.g_in.. _ c..m.. l____ c ?._?
+#         s___ S.. ___ . - $ $$
+# 
+#     ___ -c $ $$
+#         __ no. ___._?
+#             ___._? _ s__ S.. ___ . -c $ $$
+#         r_ ___._?
+# 
+# 
+# c_ Singleton o..
+#     m...
+# 
+#     ___ - name
+#         _?  ?
+# 
+#     ___ get_name
+#         r_ _?
+# 
+# 
+# obj1 _ S.. 'MyInstance 1'
+# print ?.g..  # MyInstance 1
+# 
+# obj2 _ S..'MyInstance 2'
+# print ?.g..  # MyInstance 1
+# 
+# print(_1 i. _2 i. S___.g.. # True
