1192c04267d9d83a2a86b1466a08930077ea03a8
syurskyi <sergejyurskyj@yahoo.com>
2020-02-28 16:49:09 -0800

    Update 009_singleton.py

--- 120_design_patterns/005_singleton/_exercises/templates/009_singleton.py
+++ 120_design_patterns/005_singleton/_exercises/templates/009_singleton.py
@@ -1,88 +1,88 @@
-##!/usr/bin/python
-
-# Version 1.0
-########################################################################################################################
-
-class MyBeautifulGril(object):
-    """My pretty goddess"""
-    __instance = None
-    __isFirstInit = False
-
-    def __new__(cls, name):
-        if not cls.__instance:
-            MyBeautifulGril.__instance = super().__new__(cls)
-        return cls.__instance
-
-    def __init__(self, name):
-        if not self.__isFirstInit:
-            self.__name = name
-            print("meet" + name + "，I fell in love at first sight！")
-            MyBeautifulGril.__isFirstInit = True
-        else:
-            print("meet" + name + ", I turned a deaf ear")
-
-    def showMyHeart(self):
-        print(self.__name + "The only one in my heart！")
-
-
-
-#========================================================================================
-def singletonDecorator(cls, *args, **kwargs):
-    """Define a singleton decorator"""
-    instance = {}
-
-    def wrapperSingleton(*args, **kwargs):
-        if cls not in instance:
-            instance[cls] = cls(*args, **kwargs)
-        return instance[cls]
-
-    return wrapperSingleton
-
-
-@singletonDecorator
-class Singleton3:
-    """Decorate a class with a singleton decorator"""
-
-    def __init__(self, name):
-        self.__name = name
-
-    def getName(self):
-        return self.__name
-
-
-# tony = Singleton3("Tony")
-# karry = Singleton3("Karry")
-# print(tony.getName(), karry.getName())
-# print("id(tony):", id(tony), "id(karry):", id(karry))
-# print("tony == karry:", tony == karry)
-
-
-# Version 2.0
-########################################################################################################################
-@singletonDecorator
-class MyBeautifulGril(object):
-    """My pretty goddess"""
-
-    def __init__(self, name):
-        self.__name = name
-        if self.__name == name:
-            print("meet" + name + "，I fell in love at first sight！")
-        else:
-            print("meet" + name + ", I turned a deaf ear")
-
-    def showMyHeart(self):
-        print(self.__name + "The only one in my heart! ")
-
-
-# Test
-########################################################################################################################
-def TestLove():
-    jenny = MyBeautifulGril("Jenny")
-    jenny.showMyHeart()
-    kimi = MyBeautifulGril("Kimi")
-    kimi.showMyHeart()
-    print("id(jenny):", id(jenny), " id(kimi):", id(kimi))
-
-
-TestLove()
-
+# ##!/usr/bin/python
+#
+# # Version 1.0
+# ########################################################################################################################
+#
+# c_ MyBeautifulGril o..
+#     """My pretty goddess"""
+#     __instance _ N..
+#     __isFirstInit _ F..
+#
+#     ___ -n ___ name
+#         __ no. ___.__?
+#             M___.__? _ s___ . -n  ___
+#         r_ ___.__?
+#
+#     ___ - name
+#         __ no. __is..
+#             __?  ?
+#             print("meet" + ? + "，I fell in love at first sight！")
+#             M___.__is.. _ T..
+#         ____
+#             print("meet" + ? + ", I turned a deaf ear")
+#
+#     ___ showMyHeart
+#         print(__? + "The only one in my heart！")
+#
+#
+#
+# #========================================================================================
+# ___ singletonDecorator(___, $  $$
+#     """Define a singleton decorator"""
+#     instance _    # dict
+#
+#     ___ wrapperSingleton $ $$
+#         __ ___ no. __ ?
+#             ?|___ _ ___ $ $$
+#         r_ ?|___
+#
+#     r_ ?
+#
+#
+# ??
+# c_ Singleton3
+#     """Decorate a class with a singleton decorator"""
+#
+#     ___ - name
+#         __?  ?
+#
+#     ___ getName
+#         r_ __?
+#
+#
+# # tony _ Singleton3("Tony")
+# # karry _ Singleton3("Karry")
+# # print(tony.getName(), karry.getName())
+# # print("id(tony):", id(tony), "id(karry):", id(karry))
+# # print("tony == karry:", tony == karry)
+#
+#
+# # Version 2.0
+# ########################################################################################################################
+# ??
+# c_ MyBeautifulGril o..
+#     """My pretty goddess"""
+#
+#     ___ - name
+#         __?  ?
+#         __ __? __ ?
+#             print("meet" + ? + "，I fell in love at first sight！")
+#         ____:
+#             print("meet" + ? + ", I turned a deaf ear")
+#
+#     ___ showMyHeart
+#         print __? + "The only one in my heart! ")
+#
+#
+# # Test
+# ########################################################################################################################
+# ___ TestLove
+#     jenny _ M___ "Jenny"
+#     ?.sh..
+#     kimi _ M___("Kimi")
+#     ?.sh..
+#     print("id(jenny):", i. j.. " id(kimi):", i. k..
+#
+#
+# TestLove()
+#
