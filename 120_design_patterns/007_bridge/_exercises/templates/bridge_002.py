8997dfe570d22e67bd66a6543209e33fd34ccca4
Grey <sergejyurskyj@yahoo.com>
2020-02-29 20:45:20 -0800

    Committed design patterns bridge template

--- 120_design_patterns/007_bridge/_exercises/templates/bridge_002.py
+++ 120_design_patterns/007_bridge/_exercises/templates/bridge_002.py
@@ -1,58 +1,58 @@
-# Implementor
-class DrawingAPI:
-    def drawCircle(x, y, radius):
-        pass
-
-
-# ConcreteImplementor 1/2
-class DrawingAPI1(DrawingAPI):
-    def drawCircle(self, x, y, radius):
-            print "API1.circle at %f:%f radius %f" % (x, y, radius)
-
-
-# ConcreteImplementor 2/2
-class DrawingAPI2(DrawingAPI):
-    def drawCircle(self, x, y, radius):
-            print "API2.circle at %f:%f radius %f" % (x, y, radius)
-
-
-# Abstraction
-class Shape:
-    # Low-level
-    def draw(self):
-        pass
-
-    # High-level
-    def resizeByPercentage(self, pct):
-        pass
-
-
-# Refined Abstraction
-class CircleShape(Shape):
-    def __init__(self, x, y, radius, drawingAPI):
-        self.__x = x
-        self.__y = y
-        self.__radius = radius
-        self.__drawingAPI = drawingAPI
-
-    # low-level i.e. Implementation specific
-    def draw(self):
-        self.__drawingAPI.drawCircle(self.__x, self.__y, self.__radius)
-
-    # high-level i.e. Abstraction specific
-    def resizeByPercentage(self, pct):
-        self.__radius *= pct
-
-
-def main():
-    shapes = [
-        CircleShape(1, 2, 3, DrawingAPI1()),
-        CircleShape(5, 7, 11, DrawingAPI2())
-    ]
-
-    for shape in shapes:
-        shape.resizeByPercentage(2.5)
-        shape.draw()
-
-if __name__ == "__main__":
-    main()
+# # Implementor
+# c_ DrawingAPI
+#     ___ drawCircle x y radius
+#         p..
+#
+#
+# # ConcreteImplementor 1/2
+# c_ DrawingAPI1 D..
+#     ___ drawCircle x y radius
+#             print "API1.circle at @:@ radius @"  ?  ?  ?
+#
+#
+# # ConcreteImplementor 2/2
+# c_ DrawingAPI2 D..
+#     ___ drawCircle self x, y radius)
+#             print "API2.circle at @:@ radius @"  ?  ?  ?
+#
+#
+# # Abstraction
+# c_ Shape
+#     # Low-level
+#     ___ draw
+#         p..
+#
+#     # High-level
+#     ___ resizeByPercentage pct
+#         p..
+#
+#
+# # Refined Abstraction
+# c_ CircleShape S..
+#     ___ - x y radius drawingAPI
+#         __?  ?
+#         __?  ?
+#         __?  ?
+#         __?  ?
+#
+#     # low-level i.e. Implementation specific
+#     ___ draw
+#         __dAP_.dC.. __x __y __r..
+#
+#     # high-level i.e. Abstraction specific
+#     ___ resizeByPercentage pct
+#         __r.. *_ ?
+#
+#
+# ___ main
+#     shapes = |
+#         CS..(1, 2, 3, D_1,
+#         CS..(5, 7, 11, D_2
+#     |
+#
+#     ___ shape __ ?
+#         ?.rBP..(2.5)
+#         ?.d..
+#
+# __ ______ __ ______
+#     ?
