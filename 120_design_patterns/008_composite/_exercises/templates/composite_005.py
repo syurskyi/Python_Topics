87ad8cafc237a81fede725563b85806b132277bd
Grey <sergejyurskyj@yahoo.com>
2020-03-01 14:08:11 -0800

    Committed Design Patterns Composite Template

--- 120_design_patterns/008_composite/_exercises/templates/composite_005.py
+++ 120_design_patterns/008_composite/_exercises/templates/composite_005.py
@@ -1,74 +1,74 @@
-# coding: utf-8
-
-"""
-Компоновщик (Composite) - паттерн, структурирующий объекты.
-
-Компонует объекты в древовидные структуры для представления иерархий часть-целое.
-Позволяет клиентам единообразно трактовать индивидуальные и составные объекты.
-"""
-
-
-# Класс представляющий одновременно примитивы и контейнеры
-class Graphic(object):
-    def draw(self):
-        raise NotImplementedError()
-
-    def add(self, obj):
-        raise NotImplementedError()
-
-    def remove(self, obj):
-        raise NotImplementedError()
-
-    def get_child(self, index):
-        raise NotImplementedError()
-
-
-class Line(Graphic):
-    def draw(self):
-        print 'Линия'
-
-
-class Rectangle(Graphic):
-    def draw(self):
-        print 'Прямоугольник'
-
-
-class Text(Graphic):
-    def draw(self):
-        print 'Текст'
-
-
-class Picture(Graphic):
-    def __init__(self):
-        self._children = []
-
-    def draw(self):
-        print 'Изображение'
-        # вызываем отрисовку у вложенных объектов
-        for obj in self._children:
-            obj.draw()
-
-    def add(self, obj):
-        if isinstance(obj, Graphic) and not obj in self._children:
-            self._children.append(obj)
-
-    def remove(self, obj):
-        index = self._children.index(obj)
-        del self._children[index]
-
-    def get_child(self, index):
-        return self._children[index]
-
-
-pic = Picture()
-pic.add(Line())
-pic.add(Rectangle())
-pic.add(Text())
-pic.draw()
-# Изображение
-# Линия
-# Прямоугольник
-# Текст
-
-line = pic.get_child(0)
-line.draw()  # Линия
+# # coding: utf-8
+#
+# """
+# Компоновщик (Composite) - паттерн, структурирующий объекты.
+#
+# Компонует объекты в древовидные структуры для представления иерархий часть-целое.
+# Позволяет клиентам единообразно трактовать индивидуальные и составные объекты.
+# """
+#
+#
+# # Класс представляющий одновременно примитивы и контейнеры
+# c_ Graphic o..
+#     ___ draw
+#         r_ N..
+#
+#     ___ add obj
+#         r_ N..
+#
+#     ___ remove obj
+#         r_ N..
+#
+#     ___ get_child index
+#         r_ N..
+#
+#
+# c_ Line G..
+#     ___ draw
+#         print 'Линия'
+#
+#
+# c_ Rectangle G..
+#     ___ draw
+#         print 'Прямоугольник'
+#
+#
+# c_ Text G..
+#     ___ draw
+#         print 'Текст'
+#
+#
+# c_ Picture G..
+#     ___ -
+#         _children _   # list
+#
+#     ___ draw
+#         print 'Изображение'
+#         # вызываем отрисовку у вложенных объектов
+#         ___ obj __ _c..
+#             ?.d..
+#
+#     ___ add, obj
+#         __ isi.. ? G... an. no. ? __ _c..
+#             _c___.ap.. ?
+#
+#     ___ remove obj
+#         index = _c___.in.. ?
+#         de. _c..|?
+#
+#     ___ get_child index
+#         R_ _c..|?
+#
+#
+# pic = P...
+# ?.a.. L..
+# ?.a.. R..
+# ?.a.. T..
+# ?.d..
+# # Изображение
+# # Линия
+# # Прямоугольник
+# # Текст
+#
+# line = ?.get_child(0)
+# ?.d..  # Линия
