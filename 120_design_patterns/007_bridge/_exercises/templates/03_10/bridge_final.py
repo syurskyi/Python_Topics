3bd684c54878f3a210e771531c06ed6ccb73b1c9
Grey <sergejyurskyj@yahoo.com>
2020-02-29 23:27:36 -0800

    Committed design patterns bridge template

--- 120_design_patterns/007_bridge/_exercises/templates/03_10/bridge_final.py
+++ 120_design_patterns/007_bridge/_exercises/templates/03_10/bridge_final.py
@@ -1,41 +1,41 @@
-class DrawingAPIOne(object):
-	"""Implementation-specific abstraction: concrete class one"""
-	def draw_circle(self, x, y, radius):
-		print("API 1 drawing a circle at ({}, {} with radius {}!)".format(x, y, radius))
-
-
-class DrawingAPITwo(object):
-	"""Implementation-specific abstraction: concrete class two"""
-	def draw_circle(self, x, y, radius):
-		print("API 2 drawing a circle at ({}, {} with radius {}!)".format(x, y, radius))
-
-class Circle(object):
-	"""Implementation-independent abstraction: for example, there could be a rectangle class!"""
-
-	def __init__(self, x, y, radius, drawing_api):
-		"""Initialize the necessary attributes"""
-		self._x = x
-		self._y = y
-		self._radius = radius
-		self._drawing_api = drawing_api
-
-	def draw(self):
-		"""Implementation-specific abstraction taken care of by another class: DrawingAPI"""
-		self._drawing_api.draw_circle(self._x, self._y, self._radius)
-
-	def scale(self, percent):
-		"""Implementation-independent"""
-		self._radius *= percent
-
-
-#Build the first Circle object using API One
-circle1 = Circle(1, 2, 3, DrawingAPIOne())
-#Draw a circle
-circle1.draw()
-
-#Build the second Circle object using API Two
-circle2 = Circle(2, 3, 4, DrawingAPITwo())
-#Draw a circle
-circle2.draw()
-
-
+# c_ DrawingAPIOne o..
+# 	"""Implementation-specific abstraction: concrete class one"""
+# 	___ draw_circle x y radius
+# 		print("API 1 drawing a circle at (@, @ with radius @!)".f.. ?  ?  ?
+#
+#
+# c_ DrawingAPITwo o...
+# 	"""Implementation-specific abstraction: concrete class two"""
+# 	___ draw_circle x y radius
+# 		print("API 2 drawing a circle at (@, @ with radius @!)".f.. ?  ?  ?
+#
+# c_ Circle o..
+# 	"""Implementation-independent abstraction: for example, there could be a rectangle class!"""
+#
+# 	___ - x y radius drawing_api
+# 		"""Initialize the necessary attributes"""
+# 		_?  ?
+# 		_?  ?
+# 		_?  ?
+# 		_?  ?
+#
+# 	___ draw
+# 		"""Implementation-specific abstraction taken care of by another class: DrawingAPI"""
+# 		_drawing_api.draw_circle _x _y, _r..
+#
+# 	___ scale percent
+# 		"""Implementation-independent"""
+# 		_r.. *_ ?
+#
+#
+# #Build the first Circle object using API One
+# circle1 = C___(1, 2, 3, D__O.
+# #Draw a circle
+# ?.d..
+#
+# #Build the second Circle object using API Two
+# circle2 = C___(2, 3, 4, D__T
+# #Draw a circle
+# ?.d..
+#
+#
