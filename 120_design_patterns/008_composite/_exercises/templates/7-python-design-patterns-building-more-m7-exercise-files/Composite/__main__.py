d97eff5dc88de21504dbe440ca2a24a1a5534a43
Grey <sergejyurskyj@yahoo.com>
2020-03-01 14:36:23 -0800

    Committed Design Patterns Composite Template

--- 120_design_patterns/008_composite/_exercises/templates/7-python-design-patterns-building-more-m7-exercise-files/Composite/__main__.py
+++ 120_design_patterns/008_composite/_exercises/templates/7-python-design-patterns-building-more-m7-exercise-files/Composite/__main__.py
@@ -1,31 +1,31 @@
-____ datetime ______ date
-____ person ______ Person
-____ tree ______ Tree
-
-def main():
-    hitchhikers = Tree([
-        Person('Trillian', date(1970, 3, 14)),
-        Person('Arthur', date(1965, 7, 4)),
-        Person('Ford', date(1995, 2, 2)),
-        Person('Zaphod', date(1997, 5, 1)),
-        Person('Douglas', date(1999, 4, 2))
-    ])
-
-    singles = Tree([
-        Person('Marvin', date(1991, 1, 1)),
-        Person('Slarti', date(1993, 9, 9))
-    ])
-
-    loner = Person('Dirk', date(1990, 6, 6))
-
-    tree1 = Tree([hitchhikers])
-    tree2 = Tree([singles, loner])
-    tree3 = Tree([tree1, tree2])
-
-    for tree in tree1, tree2, tree3:
-        oldest = tree.get_oldest()
-        max_age = (date.today() - oldest.birthdate).days / 365.2425
-        print('Oldest person: {}; Age: {:6.2f}'.format(oldest.name, max_age))
-
-if __name__ == '__main__':
-    main()
+# ____ d_t_ ______ d...
+# ____ p.. ______ P...
+# ____ t.. ______ T..
+#
+# ___ main
+#     hitchhikers _ T..||
+#         P..('Trillian', d..(1970, 3, 14
+#         P..('Arthur', d..(1965, 7, 4
+#         P..('Ford', d..(1995, 2, 2
+#         P..('Zaphod', d..(1997, 5, 1
+#         P..('Douglas', d..(1999, 4, 2
+#     ||
+#
+#     singles _ T..||
+#         P..('Marvin', d..(1991, 1, 1
+#         P..('Slarti', d..(1993, 9, 9
+#     ??
+#
+#     loner _ P..('Dirk', d..(1990, 6, 6))
+#
+#     tree1 _ T.. h...
+#     tree2 _ T.. s.. l..
+#     tree3 _ T.. _1 _2
+#
+#     ___ tree __ _1, _2 _3
+#         oldest _ ?.g_o..
+#         max_age _ d...t.. - ?.b___ .da.. / 365.2425
+#         print('Oldest person: @; Age: :6.2f'.f.. o____.n.. m_a..
+#
+# __ _______ __ ______
+#     ?
