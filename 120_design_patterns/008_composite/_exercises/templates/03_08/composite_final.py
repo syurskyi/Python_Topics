9c227b5ecf8a71862888d4935a7a053dbb359e39
Grey <sergejyurskyj@yahoo.com>
2020-03-01 14:18:08 -0800

    Committed Design Patterns Composite Template

--- 120_design_patterns/008_composite/_exercises/templates/03_08/composite_final.py
+++ 120_design_patterns/008_composite/_exercises/templates/03_08/composite_final.py
@@ -1,79 +1,79 @@
-class Component(object):
-	"""Abstract class"""
-
-	def __init__(self, *args, **kwargs):
-		pass
-
-	def component_function(self):
-		pass
-
-class Child(Component): #Inherits from the abstract class, Component
-	"""Concrete class"""
-
-	def __init__(self, *args, **kwargs):
-		Component.__init__(self, *args, **kwargs)
-
-		#This is where we store the name of your child item!
-		self.name = args[0]
-
-	def component_function(self):
-		#Print the name of your child item here!
-		print("{}".format(self.name))
-
-class Composite(Component): #Inherits from the abstract class, Component
-	"""Concrete class and maintains the tree recursive structure"""
-
-	def __init__(self, *args, **kwargs):
-		Component.__init__(self, *args, **kwargs)
-
-		#This is where we store the name of the composite object
-		self.name = args[0]
-
-		#This is where we keep our child items
-		self.children = []
-
-	def append_child(self, child):
-		"""Method to add a new child item"""
-		self.children.append(child)
-
-	def remove_child(self, child):
-		"""Method to remove a child item"""
-		self.children.remove(child)
-
-	def component_function(self):
-
-		#Print the name of the composite object
-		print("{}".format(self.name))
-
-		#Iterate through the child objects and invoke their component function printing their names
-		for i in self.children:
-			i.component_function()
-
-#Build a composite submenu 1
-sub1 = Composite("submenu1")
-
-#Create a new child sub_submenu 11
-sub11 = Child("sub_submenu 11")
-#Create a new Child sub_submenu 12
-sub12 = Child("sub_submenu 12")
-
-#Add the sub_submenu 11 to submenu 1
-sub1.append_child(sub11)
-#Add the sub_submenu 12 to submenu 1
-sub1.append_child(sub12)
-
-#Build a top-level composite menu
-top = Composite("top_menu")
-
-#Build a submenu 2 that is not a composite
-sub2 = Child("submenu2")
-
-#Add the composite submenu 1 to the top-level composite menu
-top.append_child(sub1)
-
-#Add the plain submenu 2 to the top-level composite menu
-top.append_child(sub2)
-
-#Let's test if our Composite pattern works!
-top.component_function()
-
+# c_ Component o..
+# 	"""Abstract class"""
+#
+# 	___  -  $  $$
+# 		pass
+#
+# 	___ component_function(
+# 		pass
+#
+# c_ Child C.. #Inherits from the abstract class, Component
+# 	"""Concrete class"""
+#
+# 	___  -  $  $$
+# 		C___. -  $  $$
+#
+# 		#This is where we store the name of your child item!
+# 		name _ ar.. 0
+#
+# 	___ component_function
+# 		#Print the name of your child item here!
+# 		print("@".f... n..
+#
+# c_ Composite C___ #Inherits from the abstract class, Component
+# 	"""Concrete class and maintains the tree recursive structure"""
+#
+# 	___  -  $  $$
+# 		C___. -  $ $$
+#
+# 		#This is where we store the name of the composite object
+# 		self.name _ ar.. 0
+#
+# 		#This is where we keep our child items
+# 		self.children _    # list
+#
+# 	___ append_child child
+# 		"""Method to add a new child item"""
+# 		c___.ap... ?
+#
+# 	___ remove_child child
+# 		"""Method to remove a child item"""
+# 		c___.r.. ?
+#
+# 	___ component_function
+#
+# 		#Print the name of the composite object
+# 		print("@".f.. n..
+#
+# 		#Iterate through the child objects and invoke their component function printing their names
+# 		___ i __ c___
+# 			?.c_f..
+#
+# #Build a composite submenu 1
+# sub1 _ C.. "submenu1"
+#
+# #Create a new child sub_submenu 11
+# sub11 _ Ch.. "sub_submenu 11"
+# #Create a new Child sub_submenu 12
+# sub12 _ Ch.. "sub_submenu 12"
+#
+# #Add the sub_submenu 11 to submenu 1
+# sub1.a_c.. sub11
+# #Add the sub_submenu 12 to submenu 1
+# sub1.a_c.. sub12
+#
+# #Build a top-level composite menu
+# top _ C.. "top_menu"
+#
+# #Build a submenu 2 that is not a composite
+# sub2 _ Ch.. "submenu2"
+#
+# #Add the composite submenu 1 to the top-level composite menu
+# top.a_c.. sub1
+#
+# #Add the plain submenu 2 to the top-level composite menu
+# top.a_c..sub2
+#
+# #Let's test if our Composite pattern works!
+# top.c_f_
+#
