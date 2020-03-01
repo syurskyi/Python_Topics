a177873eabd2d0654f27a07ced513d5745ae7a98
Grey <sergejyurskyj@yahoo.com>
2020-02-29 21:00:06 -0800

    Committed design patterns bridge template

--- 120_design_patterns/007_bridge/_exercises/templates/bridge_004.py
+++ 120_design_patterns/007_bridge/_exercises/templates/bridge_004.py
@@ -1,61 +1,61 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-*References:
-http://en.wikibooks.org/wiki/Computer_Science_Design_Patterns/Bridge_Pattern#Python
-
-*TL;DR80
-Decouples an abstraction from its implementation.
-"""
-
-
-# ConcreteImplementor 1/2
-class DrawingAPI1(object):
-
-    def draw_circle(self, x, y, radius):
-        print('API1.circle at {}:{} radius {}'.format(x, y, radius))
-
-
-# ConcreteImplementor 2/2
-class DrawingAPI2(object):
-
-    def draw_circle(self, x, y, radius):
-        print('API2.circle at {}:{} radius {}'.format(x, y, radius))
-
-
-# Refined Abstraction
-class CircleShape(object):
-
-    def __init__(self, x, y, radius, drawing_api):
-        self._x = x
-        self._y = y
-        self._radius = radius
-        self._drawing_api = drawing_api
-
-    # low-level i.e. Implementation specific
-    def draw(self):
-        self._drawing_api.draw_circle(self._x, self._y, self._radius)
-
-    # high-level i.e. Abstraction specific
-    def scale(self, pct):
-        self._radius *= pct
-
-
-def main():
-    shapes = (
-        CircleShape(1, 2, 3, DrawingAPI1()),
-        CircleShape(5, 7, 11, DrawingAPI2())
-    )
-
-    for shape in shapes:
-        shape.scale(2.5)
-        shape.draw()
-
-
-if __name__ == '__main__':
-    main()
-
-### OUTPUT ###
-# API1.circle at 1:2 radius 7.5
-# API2.circle at 5:7 radius 27.5
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+#
+# """
+# *References:
+# http://en.wikibooks.org/wiki/Computer_Science_Design_Patterns/Bridge_Pattern#Python
+#
+# *TL;DR80
+# Decouples an abstraction from its implementation.
+# """
+#
+#
+# # ConcreteImplementor 1/2
+# c_ DrawingAPI1 o..
+#
+#     ___ draw_circle x y radius
+#         print('API1.circle at @:@ radius @'.f.. ?  ?  ?
+#
+#
+# # ConcreteImplementor 2/2
+# c_ DrawingAPI2 o..
+#
+#     ___ draw_circle x y radius
+#         print('API2.circle at @:@ radius @'.f.. ?  ?  ?
+#
+#
+# # Refined Abstraction
+# c_ CircleShape o..
+#
+#     ___ - x y radius drawing_api
+#         _?  ?
+#         _?  ?
+#         _?  ?
+#         _?  ?
+#
+#     # low-level i.e. Implementation specific
+#     ___ draw
+#         _d_a_.d_c.. _x _y _r..
+#
+#     # high-level i.e. Abstraction specific
+#     ___ scale pct
+#         _radius *_ ?
+#
+#
+# ___ main
+#     shapes = |
+#         CS_(1, 2, 3, D_1
+#         CS_(5, 7, 11, D_2
+#     )
+#
+#     ___ shape in ?
+#         ?.s.. 2.5
+#         ?.d..
+#
+#
+# __ ______ __ ______
+#     ?
+#
+# ### OUTPUT ###
+# # API1.circle at 1:2 radius 7.5
+# # API2.circle at 5:7 radius 27.5
