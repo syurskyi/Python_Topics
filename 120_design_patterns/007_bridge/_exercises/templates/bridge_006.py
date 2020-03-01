1d314945bf686ffc93161dd7ba1d2bcb8ebf8386
Grey <sergejyurskyj@yahoo.com>
2020-02-29 23:23:34 -0800

    Committed design patterns bridge template

--- 120_design_patterns/007_bridge/_exercises/templates/bridge_006.py
+++ 120_design_patterns/007_bridge/_exercises/templates/bridge_006.py
@@ -1,65 +1,65 @@
-# coding: utf-8
-
-"""
-Мост (Bridge) - паттерн, структурирующий объекты.
-
-Основная задача - отделить абстракцию от её реализации так,
-чтобы то и другое можно было изменять независимо.
-"""
-
-
-class TVBase(object):
-    """Абстрактный телевизор"""
-    def tune_channel(self, channel):
-        raise NotImplementedError()
-
-
-class SonyTV(TVBase):
-    """Телевизор Sony"""
-    def tune_channel(self, channel):
-        print('Sony TV: выбран %d канал' % channel)
-
-
-class SharpTV(TVBase):
-    """Телевизор Sharp"""
-    def tune_channel(self, channel):
-        print('Sharp TV: выбран %d канал' % channel)
-
-
-class RemoteControlBase(object):
-    """Абстрактный пульт управления"""
-    def __init__(self):
-        self._tv = self.get_tv()
-
-    def get_tv(self):
-        raise NotImplementedError()
-
-    def tune_channel(self, channel):
-        self._tv.tune_channel(channel)
-
-
-class RemoteControl(RemoteControlBase):
-    """Пульт управления"""
-    def __init__(self):
-        super(RemoteControl, self).__init__()
-        self._channel = 0  # текущий канал
-
-    def get_tv(self):
-        return SharpTV()
-
-    def tune_channel(self, channel):
-        super(RemoteControl, self).tune_channel(channel)
-        self._channel = channel
-
-    def next_channel(self):
-        self._channel += 1
-        self.tune_channel(self._channel)
-
-    def previous_channel(self):
-        self._channel -= 1
-        self.tune_channel(self._channel)
-
-
-remote_control = RemoteControl()
-remote_control.tune_channel(5)  # Sharp TV: выбран 5 канал
-remote_control.next_channel()  # Sharp TV: выбран 6 канал
+# # coding: utf-8
+#
+# """
+# Мост (Bridge) - паттерн, структурирующий объекты.
+#
+# Основная задача - отделить абстракцию от её реализации так,
+# чтобы то и другое можно было изменять независимо.
+# """
+#
+#
+# c_ TVBase o..
+#     """Абстрактный телевизор"""
+#     ___ tune_channel channel
+#         r_ N...
+#
+#
+# c_ SonyTV T..
+#     """Телевизор Sony"""
+#     ___ tune_channel channel
+#         print('Sony TV: выбран @ канал'  ?
+#
+#
+# c_ SharpTV T
+#     """Телевизор Sharp"""
+#     ___ tune_channel channel
+#         print('Sharp TV: выбран @ канал'  ?
+#
+#
+# c_ RemoteControlBase o..
+#     """Абстрактный пульт управления"""
+#     ___ -
+#         _tv _ .g_t.
+#
+#     ___ get_tv
+#         r_ N...
+#
+#     ___ tune_channel channel
+#         _t_.t_c.. ?
+#
+#
+# c_ RemoteControl R..
+#     """Пульт управления"""
+#     ___ -
+#         s___ ? ____. -
+#         _c.. _ 0  # текущий канал
+#
+#     ___ get_tv(
+#         r_ S..
+#
+#     ___ tune_channel channel
+#         s___ ? ____ .t_c.. ?
+#         _?  ?
+#
+#     ___ next_channel
+#         _c.. +_ 1
+#         t_c.. _c..
+#
+#     ___ previous_channel
+#         _c.. -_ 1
+#         t_c.. _c..
+#
+#
+# remote_control _ R...
+# ?.t_c.. 5  # Sharp TV: выбран 5 канал
+# ?.n_c..  # Sharp TV: выбран 6 канал
