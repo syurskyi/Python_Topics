19838e44c4fa28a0a96d4909c4393995df61f50b
Grey <sergejyurskyj@yahoo.com>
2020-02-29 18:28:35 -0800

    Committed design patterns adapter template

--- 120_design_patterns/006_adapter/_exercises/templates/03_06/adapter.py
+++ 120_design_patterns/006_adapter/_exercises/templates/03_06/adapter.py
@@ -1,49 +1,49 @@
-class Korean:
-	"""Korean speaker"""
-	def __init__(self):
-		self.name = "Korean"
-
-	def speak_korean(self):
-		return "An-neyong?"
-
-class British:
-	"""English speaker"""
-	def __init__(self):
-		self.name = "British"	
-
-	#Note the different method name here!
-	def speak_english(self):
-		return "Hello!"	
-
-class Adapter:
-	"""This changes the generic method name to individualized method names"""
-
-	def __init__(self, object, **adapted_method):
-		"""Change the name of the method"""
-		self._object = object
-
-		#Add a new dictionary item that establishes the mapping between the generic method name: speak() and the concrete method
-		#For example, speak() will be translated to speak_korean() if the mapping says so
-		self.__dict__.update(adapted_method)
-
-	def __getattr__(self, attr):
-		"""Simply return the rest of attributes!"""
-		return getattr(self._object, attr)
-		
-#List to store speaker objects
-objects = []
-
-#Create a Korean object
-korean = Korean()
-
-#Create a British object
-british =British()
-
-#Append the objects to the objects list
-objects.append(Adapter(korean, speak=korean.speak_korean))
-objects.append(Adapter(british, speak=british.speak_english))
-
-
-for obj in objects:
-	print("{} says '{}'\n".format(obj.name, obj.speak()))
-
+# c_ Korean
+# 	"""Korean speaker"""
+# 	___  -
+# 		name _ "Korean"
+#
+# 	___ speak_korean
+# 		r_ "An-neyong?"
+#
+# c_ British
+# 	"""English speaker"""
+# 	___  -
+# 		name _ "British"
+#
+# 	#Note the different method name here!
+# 	___ speak_english
+# 		r_ "Hello!"
+#
+# c_ Adapter
+# 	"""This changes the generic method name to individualized method names"""
+#
+# 	___  -  object $$adapted_method
+# 		"""Change the name of the method"""
+# 		_?  ?
+#
+# 		#Add a new dictionary item that establishes the mapping between the generic method name: speak() and the concrete method
+# 		#For example, speak() will be translated to speak_korean() if the mapping says so
+# 		. - .update a...
+#
+# 	___ -g attr
+# 		"""Simply r_ the rest of attributes!"""
+# 		r_ ge.. _o.. ?
+#
+# #List to store speaker objects
+# objects _     # list
+#
+# #Create a Korean object
+# korean _ K..
+#
+# #Create a British object
+# british _ B...
+#
+# #Append the objects to the objects list
+# o___.ap..  A.. k.. s_k_.s_k..
+# o___.ap..  A.. b.. s_b_.s_e..
+#
+#
+# ___ obj in o___
+# 	print("@ says '@'\n".f.. ?.n.. ?.s..
+#
