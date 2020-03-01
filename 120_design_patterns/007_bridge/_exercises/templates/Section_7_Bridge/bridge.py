01a22ace82e4009e1e36a6bc1b30b120c9f03315
Grey <sergejyurskyj@yahoo.com>
2020-02-29 23:31:44 -0800

    Committed design patterns bridge template

--- 120_design_patterns/007_bridge/_exercises/templates/Section_7_Bridge/bridge.py
+++ 120_design_patterns/007_bridge/_exercises/templates/Section_7_Bridge/bridge.py
@@ -1,45 +1,45 @@
-# Circles and squares
-# Each can be rendered in vector or raster form
-
-class Renderer():
-    def render_circle(self, radius):
-        pass
-
-
-class VectorRenderer(Renderer):
-    def render_circle(self, radius):
-        print(f'Drawing a circle of radius {radius}')
-
-
-class RasterRenderer(Renderer):
-    def render_circle(self, radius):
-        print(f'Drawing pixels for circle of radius {radius}')
-
-
-class Shape:
-    def __init__(self, renderer):
-        self.renderer = renderer
-
-    def draw(self): pass
-    def resize(self, factor): pass
-
-
-class Circle(Shape):
-    def __init__(self, renderer, radius):
-        super().__init__(renderer)
-        self.radius = radius
-
-    def draw(self):
-        self.renderer.render_circle(self.radius)
-
-    def resize(self, factor):
-        self.radius *= factor
-
-
-if __name__ == '__main__':
-    raster = RasterRenderer()
-    vector = VectorRenderer()
-    circle = Circle(vector, 5)
-    circle.draw()
-    circle.resize(2)
-    circle.draw()
+# # Circles and squares
+# # Each can be rendered in vector or raster form
+#
+# c_ Renderer
+#     ___ render_circle radius
+#         p..
+#
+#
+# c_ VectorRenderer R..
+#     ___ render_circle radius
+#         print _*Drawing a circle of radius |?')
+#
+#
+# c_ RasterRenderer R..
+#     ___ render_circle radius
+#         print _*Drawing pixels for circle of radius |?')
+#
+#
+# c_ Shape
+#     ___ - renderer
+#         ?  ?
+#
+#     ___ draw p..
+#     ___ resize factor p..
+#
+#
+# c_ Circle S..
+#     ___ - renderer radius
+#         s__. - ?
+#         ?  ?
+#
+#     ___ draw
+#         r___.r_c.. r..
+#
+#     ___ resize factor
+#         r.. *_ ?
+#
+#
+# __ _______ __ ______k
+#     raster = ?
+#     vector = ?
+#     circle = ? vector, 5
+#     c___.d..
+#     c___.r.. 2
+#     c___.d..
