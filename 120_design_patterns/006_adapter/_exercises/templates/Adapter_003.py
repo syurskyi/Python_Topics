32950b1eaf354bb7955d2e3a74b992d77c7cc19f
Grey <sergejyurskyj@yahoo.com>
2020-02-29 17:33:23 -0800

    Committed design patterns adapter template

--- 120_design_patterns/006_adapter/_exercises/templates/Adapter_003.py
+++ 120_design_patterns/006_adapter/_exercises/templates/Adapter_003.py
@@ -1,51 +1,51 @@
-#!/usr/bin/env python
-# Written by: DGC
-
-#==============================================================================
-c_ RCCar o..
-
-    ___ -
-        .speed _ 0
-
-    ___ change_speed speed
-        ?   ?
-        print("RC car is moving at " + st. ??
-
-#==============================================================================
-c_ RCAdapter o..
-
-    ___ -
-        ?car _ R..
-
-    ___ move_forwards
-        c__.c__ 10
-
-    ___ move_backwards
-        c__.c...-10
-
-    ___ stop(self):
-        self.car.change_speed(0)
-
-#==============================================================================
-c_ RemoteControl o..
-
-    ___ __init__(self):
-        self.adapter _ RCAdapter()
-
-    ___ stick_up(self):
-        self.adapter.move_forwards()
-
-    ___ stick_down(self):
-        self.adapter.move_backwards()
-
-    ___ stick_middle(self):
-        self.adapter.stop()
-
-#==============================================================================
-if (__name__ == "__main__"):
-    controller _ RemoteControl()
-    controller.stick_up()
-    controller.stick_middle()
-    controller.stick_down()
-    controller.stick_middle()
-    
+# #!/usr/bin/env python
+# # Written by: DGC
+#
+# #==============================================================================
+# c_ RCCar o..
+#
+#     ___ -
+#         .speed _ 0
+#
+#     ___ change_speed speed
+#         ?   ?
+#         print("RC car is moving at " + st. ??
+#
+# #==============================================================================
+# c_ RCAdapter o..
+#
+#     ___ -
+#         ?car _ R..
+#
+#     ___ move_forwards
+#         c__.c__ 10
+#
+#     ___ move_backwards
+#         c__.c...-10
+#
+#     ___ stop
+#         c__.c.. 0
+#
+# #==============================================================================
+# c_ RemoteControl o..
+#
+#     ___ -
+#         adapter _ RCA..
+#
+#     ___ stick_up
+#         a____.m_f..
+#
+#     ___ stick_down
+#         a___.m_b..
+#
+#     ___ stick_middle
+#         a__.st..
+#
+# #==============================================================================
+# __ ______ __ _______
+#     controller _ R..
+#     ?.s_u...
+#     ?.s_m..
+#     ?.s_d..
+#     ?.s_m..
+#
