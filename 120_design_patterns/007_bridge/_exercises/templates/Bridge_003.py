2e28c0da0f810843326f18d8b49d66b92fdc825f
Grey <sergejyurskyj@yahoo.com>
2020-02-29 20:51:51 -0800

    Committed design patterns bridge template

--- 120_design_patterns/007_bridge/_exercises/templates/Bridge_003.py
+++ 120_design_patterns/007_bridge/_exercises/templates/Bridge_003.py
@@ -1,63 +1,63 @@
-#!/usr/bin/env python
-# Written by: DGC
-
-______ abc
-
-#==============================================================================
-class Shape(object):
-    __metaclass__ = abc.ABCMeta
-
-    @abc.abstractmethod
-    def __init__(self):
-        pass
-
-    def area(self):
-        """ 
-        Returns the area of the shape calculated using the shape specific 
-        implementation. 
-        """
-        assert self.calculator != None, "self.calculator not defined."
-        return self.calculator(self)
-
-#==============================================================================
-class Rectangle(Shape):
-    
-    def __init__(self, x, y):
-        self.calculator = rectangular_area_calculator
-        self.x = x
-        self.y = y
-
-#==============================================================================
-def rectangular_area_calculator(rectangle):
-    return rectangle.x * rectangle.y
- 
-#==============================================================================
-class Triangle(Shape):
-
-    def __init__(self, base, height):
-        self.calculator = triangular_area_calculator
-        self.base = base
-        self.height = height
-
-#==============================================================================
-def triangular_area_calculator(triangle):
-    return 0.5 * triangle.base * triangle.height
-       
-#==============================================================================
-if (__name__ == "__main__"):
-    x = 4
-    y = 5
-    rect = Rectangle(x, y)
-    print(str(x) + " x " + str(y) + " Rectangle area: " + str(rect.area()))
-  
-    base = 4
-    height = 5
-    tri = Triangle(base, height);
-    print(
-        "Base " +
-        str(base) +
-        ", Height " +
-        str(height) + 
-        " Triangle area: " +
-        str(tri.area())
-        )
+# #!/usr/bin/env python
+# # Written by: DGC
+#
+# ______ a..
+#
+# #==============================================================================
+# c_ Shape o...
+#     m...
+#
+#     ??.?
+#     ___ -
+#         p..
+#
+#     ___ area
+#         """
+#         Returns the area of the shape calculated using the shape specific
+#         implementation.
+#         """
+#         ass.. ca... !_ N.. "self.calculator not defined."
+#         r_ ca...
+#
+# #==============================================================================
+# c_ Rectangle S..
+#
+#     ___ - x y
+#         calculator _ r..
+#         ?  ?
+#         ?  ?
+#
+# #==============================================================================
+# ___ rectangular_area_calculator rectangle
+#     r_ ?.x * ?.y
+#
+# #==============================================================================
+# c_ Triangle S...
+#
+#     ___ - base height
+#         calculator _ t...
+#         ?  ?
+#         ?  ?
+#
+# #==============================================================================
+# ___ triangular_area_calculator triangle
+#     r_ 0.5 * ?.b.. * ?.h..
+#
+# #==============================================================================
+# __ ______ __ ______
+#     x _ 4
+#     y _ 5
+#     rect _ R..e x y
+#     print(s..(x) + " x " + s..(y) + " Rectangle area: " + s..(re__.a..
+#
+#     base _ 4
+#     height _ 5
+#     tri _ T..  b.. h..
+#     print(
+#         "Base " +
+#         s..(b.. +
+#         ", Height " +
+#         s..(h.. +
+#         " Triangle area: " +
+#         s..(t__.a..
+#         )
