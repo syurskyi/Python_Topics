21b2a2a2835d07833db3f329e9c0a5c7f0bd6779
syurskyi <sergejyurskyj@yahoo.com>
2020-03-03 17:20:06 -0800

    Create abs_transport.py

---
+++ 120_design_patterns/022_template_method/_exercises/templates/5-python-design-patterns-building-more-m5-exercise-files/Template/abs_transport.py
@@ -0,0 +1,30 @@
+# ______ a..
+#
+# c_ AbsTransport m...
+#
+#     ___ - destination
+#         _?  ?
+#
+#     ___ take_trip
+#         ?
+#         ?
+#         ?
+#         t..
+#         a..
+#
+#     ??.?
+#     ___ start_engine
+#         p..
+#
+#     ___ leave_terminal
+#         print('Leaving terminal')
+#
+#     ??.?
+#     ___ travel_to_destination
+#         print('Travelling...')
+#
+#     ___ entertainment
+#         p..
+#
+#     ___ arrive_at_destination
+#         print('Arriving at ' + _?
