89be04b413949a48ec1e90010f929fcd0b4c62ae
syurskyi <sergejyurskyj@yahoo.com>
2020-03-04 18:20:36 -0800

    Update metaclasses.py

--- 070_oop/008_metaprogramming/_exercises/templates/Expert Python Programming/metaclasses.py
+++ 070_oop/008_metaprogramming/_exercises/templates/Expert Python Programming/metaclasses.py
@@ -1,44 +1,44 @@
-"""
-"Metaclasses" section example of metaclass that reveal order of all its
-methods called.
-
-"""
-
-print(" >>> defining RevealingMeta(type)")
-
-
-class RevealingMeta(type):
-    def __new__(mcs, name, bases, namespace, **kwargs):
-        print(mcs, "__new__ called")
-        return super().__new__(mcs, name, bases, namespace)
-
-    @classmethod
-    def __prepare__(mcs, name, bases, **kwargs):
-        print(mcs, "__prepare__ called")
-        return super().__prepare__(name, bases, **kwargs)
-
-    def __init__(cls, name, bases, namespace, **kwargs):
-        print(cls, "__init__ called")
-        super().__init__(name, bases, namespace)
-
-    def __call__(cls, *args, **kwargs):
-        print(cls, "__call__ called")
-        return super().__call__(*args, **kwargs)
-
-
-print(" >>> defining RevealingClass(metaclass=RevealingMeta)")
-
-
-class RevealingClass(metaclass=RevealingMeta):
-    def __new__(cls):
-        print(cls, "__new__ called")
-        return super().__new__(cls)
-
-    def __init__(self):
-        print(self, "__init__ called")
-        super().__init__()
-
-
-if __name__ == "__main__":
-    print(" >>> Creating RevealingClass()")
-    instance = RevealingClass()
+# """
+# "Metaclasses" section example of metaclass that reveal order of all its
+# methods called.
+# 
+# """
+# 
+# print(" >>> defining RevealingMeta(type)")
+# 
+# 
+# c_ RevealingMeta ty..
+#     ___ -n mcs name bases namespace $$
+#         print m.. "__new__ called")
+#         r_ s___. -n ?  ?  ?  ?
+# 
+#     ??
+#     ___ -p mcs name bases $$
+#         print(m.. "__prepare__ called")
+#         r_ s___ . -p n... b... $$
+# 
+#     ___ - cls name bases namespace $$
+#         print c.. "__init__ called")
+#         s___ . - n.. b.. n..
+# 
+#     ___ -c ___ $ $$
+#         print ___ "__call__ called")
+#         r_ s___ . -c $ $$
+# 
+# 
+# print(" >>> defining RevealingClass(metaclass=RevealingMeta)")
+# 
+# 
+# c_ RevealingClass m..
+#     ___ -n cls
+#         print(? "__new__ called")
+#         r_ s___ . -n ?
+# 
+#     ___ -
+#         print ? "__init__ called")
+#         s___ . -
+# 
+# 
+# __ _______ __ _______
+#     print(" >>> Creating RevealingClass()")
+#     instance _ R..
