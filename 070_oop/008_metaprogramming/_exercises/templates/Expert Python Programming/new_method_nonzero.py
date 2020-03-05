bfb2b38331f810d759dde471de2af0c4f313c759
syurskyi <sergejyurskyj@yahoo.com>
2020-03-04 19:09:07 -0800

    Update new_method_nonzero.py

--- 070_oop/008_metaprogramming/_exercises/templates/Expert Python Programming/new_method_nonzero.py
+++ 070_oop/008_metaprogramming/_exercises/templates/Expert Python Programming/new_method_nonzero.py
@@ -1,24 +1,24 @@
-"""
-"Using the __new__() method to override instance creation process" section
-example of overriding `__new__()` method when subclassing non-mutable
-Python built-in types.
-
-"""
-
-
-class NonZero(int):
-    def __new__(cls, value):
-        return super().__new__(cls, value) if value != 0 else None
-
-    def __init__(self, skipped_value):
-        # implementation of __init__ could be skipped in this case
-        # but it is left to present how it may be not called
-        print("__init__() called")
-        super().__init__()
-
-
-if __name__ == "__main__":
-    print("NonZero(-12)    =", NonZero(-12))
-    print("NonZero(-3.123) =", NonZero(-3.123))
-    print("NonZero(0)      =", NonZero(0))
-
+# """
+# "Using the __new__() method to override instance creation process" section
+# example of overriding `__new__()` method when subclassing non-mutable
+# Python built-in types.
+#
+# """
+#
+#
+# c_ NonZero int
+#     ___ -n ___ value
+#         r_ s___ . -n ___ ? __ ? !_ 0 ____ N...
+#
+#     ___ - skipped_value
+#         # implementation of __init__ could be skipped in this case
+#         # but it is left to present how it may be not called
+#         print("__init__() called")
+#         s___ . -
+#
+#
+# __ _______ __ _______
+#     print("NonZero(-12)    =" ? -12
+#     print("NonZero(-3.123) =" ? -3.123
+#     print("NonZero(0)      =" ? 0
+#
