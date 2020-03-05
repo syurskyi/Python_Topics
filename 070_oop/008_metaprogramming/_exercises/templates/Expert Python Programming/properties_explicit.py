4df90ad0f6f7655dcb9506abe13f3551a6b8527b
syurskyi <sergejyurskyj@yahoo.com>
2020-03-04 20:16:39 -0800

    Update properties_explicit.py

--- 070_oop/008_metaprogramming/_exercises/templates/Expert Python Programming/properties_explicit.py
+++ 070_oop/008_metaprogramming/_exercises/templates/Expert Python Programming/properties_explicit.py
@@ -1,53 +1,53 @@
-"""
-"Properties" section example of writing properties as separate
-access methods with class level call to `property()` function.
-
-"""
-
-
-class Rectangle:
-    def __init__(self, x1, y1, x2, y2):
-        self.x1, self.y1 = x1, y1
-        self.x2, self.y2 = x2, y2
-
-    def _width_get(self):
-        return self.x2 - self.x1
-
-    def _width_set(self, value):
-        self.x2 = self.x1 + value
-
-    def _height_get(self):
-        return self.y2 - self.y1
-
-    def _height_set(self, value):
-        self.y2 = self.y1 + value
-
-    width = property(
-        _width_get, _width_set,
-        doc="rectangle width measured from left"
-    )
-    height = property(
-        _height_get, _height_set,
-        doc="rectangle height measured from top"
-    )
-
-    def __repr__(self):
-        return "{}({}, {}, {}, {})".format(
-            self.__class__.__name__,
-            self.x1, self.y1, self.x2, self.y2
-        )
-
-
-if __name__ == "__main__":
-    rectangle = Rectangle(0, 0, 10, 10)
-    print(
-        "At start we have {} with size of {} x {}"
-        "".format(rectangle, rectangle.width, rectangle.height)
-    )
-
-    rectangle.width = 2
-    rectangle.height = 8
-    print(
-        "After resizing we have {} with size of {} x {}"
-        "".format(rectangle, rectangle.width, rectangle.height)
-    )
+# """
+# "Properties" section example of writing properties as separate
+# access methods with class level call to `property()` function.
+#
+# """
+#
+#
+# c_ Rectangle
+#     ___ -  x1 y1 x2 y2
+#         x1 y1 _ x1 y1
+#         x2 y2 _ x2 y2
+#
+#     ___ _width_get
+#         r_ x2 - x1
+#
+#     ___ _width_set value
+#         x2 _ x1 + ?
+#
+#     ___ _height_get
+#         r_ y2 - y1
+#
+#     ___ _height_set value
+#         y2 _ y1 + ?
+#
+#     width _ p... |
+#         _? _?
+#         doc_"rectangle width measured from left"
+#     |
+#     height _ p... |
+#         _? _?
+#         doc_"rectangle height measured from top"
+#     |
+#
+#     ___ -r
+#         r_ "@(@ @ @ @)".f..|
+#             -c. -n
+#             x1 y1 x2 y2
+#         |
+#
+#
+# __ _______ __ _______
+#     rectangle _ R...(0 0 10 10)
+#     print|
+#         "At start we have @ with size of @ x @"
+#         "".f.. ? ?.w.. ?.h..
+#     |
+#
+#     ?.w.. _ 2
+#     ?.h.. _ 8
+#     print(
+#         "After resizing we have @ with size of @ x @"
+#         "".f.. ? ?.w.. ?.h..
+#     ?
