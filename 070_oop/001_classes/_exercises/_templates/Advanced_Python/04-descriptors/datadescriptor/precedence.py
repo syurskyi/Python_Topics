1b627815431c90b1c812982011bb568cc39cb4e7
syurskyi <sergejyurskyj@yahoo.com>
2020-03-05 18:53:06 -0800

    Update precedence.py

--- 070_oop/001_classes/_exercises/_templates/Advanced_Python/04-descriptors/datadescriptor/precedence.py
+++ 070_oop/001_classes/_exercises/_templates/Advanced_Python/04-descriptors/datadescriptor/precedence.py
@@ -1,24 +1,23 @@
-
-class DataDescriptor:
-
-    def __get__(self, instance, owner):
-        print("DataDescriptor.__get__({!r}, {!r}, {!r})"
-              .format(self, instance, owner))
-
-    def __set__(self, instance, value):
-        print("DataDescriptor.__set__({!r}, {!r}, {!r})"
-              .format(self, instance, value))
-
-
-class NonDataDescriptor:
-
-    def __get__(self, instance, owner):
-        print("NonDataDescriptor.__get__({!r}, {!r}, {!r})"
-              .format(self, instance, owner))
-
-
-class Owner:
-
-    a = DataDescriptor()
-    b = NonDataDescriptor()
-
+#
+# c_ DataDescriptor
+#
+#     ___ -g instance owner
+#         print("DataDescriptor.__get__( _r ,  _r ,  _r )"
+#               .f.. ? ? ?
+#
+#     ___ -s instance value
+#         print("DataDescriptor.__set__( _r ,  _r ,  _r )"
+#               .f.. ?  ?  ?
+#
+#
+# c_ NonDataDescriptor
+#
+#     ___ -g instance owner
+#         print("NonDataDescriptor.__get__( _r ,  _r ,  _r )"
+#               .f.. ?  ?  ?
+#
+#
+# c_ Owner
+#
+#     a = D..
+#     b = N..
