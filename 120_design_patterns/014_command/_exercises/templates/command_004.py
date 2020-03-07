8d62cffa6a3996a3300ae902cfdce291bb55d13c
Grey <sergejyurskyj@yahoo.com>
2020-03-06 20:18:47 -0800

    Committed Design Patterns Command template

--- 120_design_patterns/014_command/_exercises/templates/command_004.py
+++ 120_design_patterns/014_command/_exercises/templates/command_004.py
@@ -1,108 +1,108 @@
-"""Command pattern
-
-The Command pattern is handy when we want to isolate the portion of the code
-that executes an action, from the one that requests the execution. Command can
-be useful when we want to create a batch of operations and execute them later.
-"""
-import datetime
-from copy import deepcopy
-
-
-def rename_command(x, y, *args, **kwargs):
-    undo = kwargs.get("undo", False)
-    if not undo:
-        print("rename {} into {}".format(x, y))
-    else:
-        print("rename {} into {}".format(y, x))
-
-
-def move_command(x, source, dest, *args, **kwargs):
-    undo = kwargs.get("undo", False)
-    if not undo:
-        print("move {} from {} to {}".format(x, source, dest))
-    else:
-        print("move {} from {} to {}".format(x, dest, source))
-
-
-class Queue(object):
-
-    def __init__(self):
-        self._commands = list()
-        self._history = list()
-
-    def add_command(self, func, *args, **kwargs):
-        timestamp = datetime.datetime.now().isoformat()
-        self._commands.append(
-            {"timestamp": timestamp, "func": func, "args": args, "kwargs": kwargs}
-        )
-
-    def execute(self, commands=None):
-        if commands is None:
-            commands = self._commands
-        for cmd in commands:
-            func = cmd["func"]
-            args, kwargs = cmd["args"], cmd["kwargs"]
-            func(*args, **kwargs)
-        self.update_history(commands)
-        self.clear_queue()
-
-    def redo(self):
-        commands = self._history[-1]
-        self.execute(commands)
-
-    def undo(self):
-        original_commands = self._history[-1]
-        commands = deepcopy(original_commands)
-        for cmd in commands:
-            func = cmd["func"]
-            args, kwargs = cmd["args"], cmd["kwargs"]
-            # we need to store the "undo" within the command (for history)
-            kwargs.update({"undo": True})
-            func(*args, **kwargs)
-        self.update_history(commands)
-
-    def clear_queue(self):
-        self._commands = list()
-
-    def update_history(self, commands):
-        self._history.append(commands)
-
-    def history(self):
-        for i, commands in enumerate(self._history):
-            print("Set of commands {}".format(i))
-            for cmd in commands:
-                t = cmd["timestamp"]
-                f = cmd["func"].__name__
-                ar, kw = cmd["args"], cmd["kwargs"]
-                print(" {} - f: {} args: {} kwargs: {}".format(t, f, ar, kw))
-
-
-def main():
-    queue = Queue()
-
-    queue.add_command(rename_command, "test.py", "hello.py")
-    queue.add_command(move_command, "hello.py", source="/lib", dest="/home")
-    queue.add_command(rename_command, x="readme.txt", y="README.txt")
-
-    print("Execute all commands as a single operation")
-    queue.execute()
-
-    print("\nRedo last operation")
-    queue.redo()
-
-    print("\nUndo last operation")
-    queue.undo()
-
-    print("\nExecute a single command")
-    queue.add_command(move_command, "hello.py", source="/lib", dest="/home")
-    queue.execute()
-
-    print("\nUndo last operation")
-    queue.undo()
-
-    print("\nShow history")
-    queue.history()
-
-
-if __name__ == "__main__":
-    main()
+# """Command pattern
+#
+# The Command pattern is handy when we want to isolate the portion of the code
+# that executes an action, from the one that requests the execution. Command can
+# be useful when we want to create a batch of operations and execute them later.
+# """
+# ______ d_t_
+# ____ co.. ______ d_c_
+#
+#
+# ___ rename_command x y $ $$
+#     undo _ kw__.ge. *u.., F..
+#     __ no. ?
+#         print("rename @ into @".f.. x, y
+#     ____
+#         print("rename @ into @".f.. y x
+#
+#
+# ___ move_command x source dest $ $$
+#     undo _ kw__.ge. *u.. F..
+#     __ no. ?
+#         print("move @ from @ to @".f.. ?  ?  ?
+#     ____
+#         print("move @ from @ to @".f.. ? d.. s..
+#
+#
+# c_ Queue o..
+#
+#     ___ -
+#         _commands _ li..
+#         _history _ li..
+#
+#     ___ add_command func $ $$
+#         timestamp _ d_t_.d_t_.n__.i_f..
+#         _c___.ap..|
+#             |*timestamp t.. *func ? *args ? *kwargs ?
+#         |
+#
+#     ___ execute commands_N..
+#         __ commands __ N..
+#             ? _ _c..
+#         ___ cmd __ ?
+#             func _ cmd["func"]
+#             args, kwargs _ cmd|*args, cmd|*kwargs
+#             func $ $$
+#         u_h.. c..
+#         c_q..
+#
+#     ___ redo
+#         commands _ _h..|-1
+#         execute ?
+#
+#     ___ undo
+#         original_commands _ _h..|-1
+#         commands _ d_c_ o_c..
+#         ___ cmd __ ?
+#             func _ ?|*func
+#             args, kwargs _ c..|*args , cmd|*kwargs
+#             # we need to store the "undo" within the command (for history)
+#             kw__.up..||*undo T..
+#             func $ $$
+#         u_h.. c..
+#
+#     ___ clear_queue
+#         _commands _ li..
+#
+#     ___ update_history commands
+#         _h___.ap.. ?
+#
+#     ___ history
+#         ___ i, commands __ en... _h..
+#             print("Set of commands @".f.. i
+#             ___ cmd in c...
+#                 t _ ?|*timestamp
+#                 f _ ?|*func . -n
+#                 ar, kw _ ?|*args,  ?|*kwargs
+#                 print(" @ - f: @ args: @ kwargs: @".f.. ?  ?  ?  ?
+#
+#
+# ___ main
+#     queue _ Q..
+#
+#     ?.a... r_c.. "test.py", "hello.py")
+#     ?.a... m_c.. "hello.py", source_"/lib", dest_"/home")
+#     ?.a... r_c.. x_"readme.txt", y_"README.txt")
+#
+#     print("Execute all commands as a single operation")
+#     ?.ex..
+#
+#     print("\nRedo last operation")
+#     ?.r..
+#
+#     print("\nUndo last operation")
+#     ?.u..
+#
+#     print("\nExecute a single command")
+#     ?.a... m_c.. "hello.py", source_"/lib", dest_"/home")
+#     ?.ex..
+#
+#     print("\nUndo last operation")
+#     ?.u..
+#
+#     print("\nShow history")
+#     ?.h..
+#
+#
+# __ _______ __ ______
+#     ?
