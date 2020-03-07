27af517ddf3281d734a9d40c5f64fadcde30ad76
Grey <sergejyurskyj@yahoo.com>
2020-03-06 20:30:08 -0800

    Committed Design Patterns Command template

--- 120_design_patterns/014_command/_exercises/templates/command_006.py
+++ 120_design_patterns/014_command/_exercises/templates/command_006.py
@@ -1,63 +1,63 @@
-"""
-*TL;DR
-Encapsulates all information needed to perform an action or trigger an event.
-
-*Examples in Python ecosystem:
-Django HttpRequest (without `execute` method):
- https://docs.djangoproject.com/en/2.1/ref/request-response/#httprequest-objects
-"""
-
-import os
-
-
-class MoveFileCommand:
-    def __init__(self, src, dest):
-        self.src = src
-        self.dest = dest
-
-    def execute(self):
-        self.rename(self.src, self.dest)
-
-    def undo(self):
-        self.rename(self.dest, self.src)
-
-    def rename(self, src, dest):
-        print("renaming {} to {}".format(src, dest))
-        os.rename(src, dest)
-
-
-def main():
-
-    from os.path import lexists
-
-    command_stack = [
-        MoveFileCommand('foo.txt', 'bar.txt'),
-        MoveFileCommand('bar.txt', 'baz.txt')
-    ]
-
-    # Verify that none of the target files exist
-    assert not lexists("foo.txt")
-    assert not lexists("bar.txt")
-    assert not lexists("baz.txt")
-
-    # Create empty file
-    open("foo.txt", "w").close()
-
-    # Commands can be executed later on
-    for cmd in command_stack:
-        cmd.execute()
-    # renaming foo.txt to bar.txt
-    # renaming bar.txt to baz.txt
-
-    # And can also be undone at will
-    for cmd in reversed(command_stack):
-        cmd.undo()
-    # renaming baz.txt to bar.txt
-    # renaming bar.txt to foo.txt
-
-    os.unlink("foo.txt")
-
-
-
-if __name__ == "__main__":
-    main()
+# """
+# *TL;DR
+# Encapsulates all information needed to perform an action or trigger an event.
+#
+# *Examples in Python ecosystem:
+# Django HttpRequest (without `execute` method):
+#  https://docs.djangoproject.com/en/2.1/ref/request-response/#httprequest-objects
+# """
+#
+# _____ __
+#
+#
+# c_ MoveFileCommand
+#     ___ - src dest
+#         ?  ?
+#         ?  ?
+#
+#     ___ execute
+#         re.. s.. d..
+#
+#     ___ undo
+#         re.. d.. s..
+#
+#     ___ rename src dest
+#         print("renaming @ to @".f.. ? ?
+#         __.re.. ? ?
+#
+#
+# ___ main
+#
+#     ____ __.pa.. _____ lex..
+#
+#     command_stack _ |
+#         M.. 'foo.txt', 'bar.txt'
+#         M.. 'bar.txt', 'baz.txt'
+#     |
+#
+#     # Verify that none of the target files exist
+#     ass.. no. lex... "foo.txt"
+#     ass.. no. lex... "bar.txt"
+#     ass.. no. lex... "baz.txt"
+#
+#     # Create empty file
+#     o.. foo.txt _ .cl..
+#
+#     # Commands can be executed later on
+#     ___ cmd __ c_s..
+#         ?.ex..
+#     # renaming foo.txt to bar.txt
+#     # renaming bar.txt to baz.txt
+#
+#     # And can also be undone at will
+#     ___ cmd __ re.. c_s..
+#         ?.u..
+#     # renaming baz.txt to bar.txt
+#     # renaming bar.txt to foo.txt
+#
+#     __.un.. foo.txt
+#
+#
+#
+# __ _______ __ ______
+#     ?
