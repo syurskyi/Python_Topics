f138eac1e1cd336e4ec5450f462410b48b8613ab
Grey <sergejyurskyj@yahoo.com>
2020-03-07 20:51:50 -0800

    Committed Design Patterns Iterator template

--- 120_design_patterns/016_iterator/_exercises/templates/iterator_005.py
+++ 120_design_patterns/016_iterator/_exercises/templates/iterator_005.py
@@ -1,72 +1,72 @@
-"""Iterator pattern
-"""
-
-
-class MyIterator(object):
-
-    def __init__(self, *args):
-        self.data = args
-        self.index = 0
-
-    def __iter__(self):
-        generator = self.generator_function()
-        return generator
-
-    def __next__(self):
-        if len(self.data) > self.index:
-            obj = self.data[self.index]
-            self.index += 1
-        else:
-            raise StopIteration("No more elements!")
-
-        return obj
-
-    def __getitem__(self, index):
-        return self.data[index]
-
-    def __len__(self):
-        return len(self.data[self.index:])
-
-    def generator_function(self):
-        for d in self.data[self.index:]:
-            self.index += 1
-            yield "Item: {} (type: {})".format(d, type(d))
-
-
-def some_function(a, b, c=123):
-    print(a, b, c)
-
-
-def main():
-    iterator = MyIterator(1, 3.0, None, "hello", some_function)
-    print("Initial length")
-    print(len(iterator))
-
-    index = 3
-    print("\nGet item at index {}".format(index))
-    print(iterator[index])
-
-    print("\n__next__ calls")
-    print(next(iterator))
-    print(next(iterator))
-
-    print("\nLength after __next__ calls")
-    print(len(iterator))
-
-    print("\nFor loop with the remaining items")
-    for item in iterator:
-        print(item)
-
-    print("\nStopIteration")
-    try:
-        next(iterator)
-    except StopIteration as e:
-        print(e)
-
-    print("But in Python we don't really need any of the above...")
-    for item in (1, 3.0, None, "hello", some_function):
-        print(item)
-
-
-if __name__ == "__main__":
-    main()
+# """Iterator pattern
+# """
+#
+#
+# c_ MyIterator o..
+#
+#     ___ - $
+#         data _ args
+#         index _ 0
+#
+#     ___ -i
+#         generator _ g..
+#         r_ g?
+#
+#     ___ -n
+#         __ le. d.. > in..
+#             obj _ d..|in..
+#             in.. +_ 1
+#         ____
+#              S.. "No more elements!"
+#
+#         r_ ?
+#
+#     ___ -g index
+#         r_ da..|in..
+#
+#     ___ -l
+#         r_ le. da..|in..;
+#
+#     ___ generator_function
+#         ___ d __ da..|in..;
+#             in.. +_ 1
+#             y.. "Item: @ (type: @)".f.. ? ty.. ?
+#
+#
+# ___ some_function a b c_123
+#     print ?  ?  ?
+#
+#
+# ___ main
+#     iterator _ M.. (1, 3.0, N..., "hello", some_function)
+#     print("Initial length")
+#     print(le. it..
+#
+#     index _ 3
+#     print("\nGet item at index @".f.. ?
+#     print(it..|?
+#
+#     print("\n__next__ calls")
+#     print(ne.. it..
+#     print(ne.. it..
+#
+#     print("\nLength after __next__ calls")
+#     print(le. it..
+#
+#     print("\nFor loop with the remaining items")
+#     ___ item __ it..
+#         print ?
+#
+#     print("\nStopIteration")
+#     ___
+#         ne.. it..
+#     _______ S.. __ e
+#         print
+#
+#     print("But in Python we don't really need any of the above...")
+#     ___ item __ (1, 3.0, N.. "hello", some_function):
+#         print ?
+#
+#
+# __ _______ __ ______
+#     ?
