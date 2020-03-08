961af702206997b41611cd422e7880fc97301993
Grey <sergejyurskyj@yahoo.com>
2020-03-08 11:34:17 -0700

    Committed Design Patterns - Pattern Memento template

--- 120_design_patterns/018_memento/_exercises/templates/Section_18_Memento/memento.py
+++ 120_design_patterns/018_memento/_exercises/templates/Section_18_Memento/memento.py
@@ -1,34 +1,34 @@
-class Memento:
-    def __init__(self, balance):
-        self.balance = balance
-
-
-class BankAccount:
-    def __init__(self, balance=0):
-        self.balance = balance
-
-
-    def deposit(self, amount):
-        self.balance += amount
-        return Memento(self.balance)
-
-    def restore(self, memento):
-        self.balance = memento.balance
-
-    def __str__(self):
-        return f'Balance = {self.balance}'
-
-
-if __name__ == '__main__':
-    ba = BankAccount(100)
-    m1 = ba.deposit(50)
-    m2 = ba.deposit(25)
-    print(ba)
-
-    # restore to m1
-    ba.restore(m1)
-    print(ba)
-
-    # restore to m2
-    ba.restore(m2)
-    print(ba)
+# c_ Memento
+#     ___ - balance
+#         ?  ?
+#
+#
+# c_ BankAccount
+#     ___ - balance_0
+#         ?  ?
+#
+#
+#     ___ deposit amount
+#         balance +_ ?
+#         r_ M.. b..
+#
+#     ___ restore memento
+#         balance _ ?.b...
+#
+#     ___ -s
+#         r_ _*Balance _ |b..'
+#
+#
+# __ _______ __ ______
+#     ba _ B.. 100
+#     m1 _ ?.de.. 50
+#     m2 _ ?.de.. 25
+#     print ?
+#
+#     # restore to m1
+#     ?.re.. m1
+#     print ?
+#
+#     # restore to m2
+#     ?.re.. m2
+#     print ?
