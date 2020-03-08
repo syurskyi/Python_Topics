600754c7d7f87ba40d2c5b2265f33d02aee8b178
Grey <sergejyurskyj@yahoo.com>
2020-03-08 11:21:50 -0700

    Committed Design Patterns - Pattern Memento examples

--- 120_design_patterns/018_memento/_exercises/templates/memento_006.py
+++ 120_design_patterns/018_memento/_exercises/templates/memento_006.py
@@ -1,61 +1,61 @@
-# coding: utf-8
-
-"""
-Хранитель (Memento) - паттерн поведения объектов.
-
-Не нарушая инкапсуляции, фиксирует и выносит за пределы объекта его внутреннее состояние так,
-чтобы позднее можно было восстановить в нем объект.
-"""
-
-
-class Memento(object):
-    """Хранитель"""
-    def __init__(self, state):
-        self._state = state
-
-    def get_state(self):
-        return self._state
-
-
-class Caretaker(object):
-    """Опекун"""
-    def __init__(self):
-        self._memento = None
-
-    def get_memento(self):
-        return self._memento
-
-    def set_memento(self, memento):
-        self._memento = memento
-
-
-class Originator(object):
-    """Создатель"""
-    def __init__(self):
-        self._state = None
-
-    def set_state(self, state):
-        self._state = state
-
-    def get_state(self):
-        return self._state
-
-    def save_state(self):
-        return Memento(self._state)
-
-    def restore_state(self, memento):
-        self._state = memento.get_state()
-
-
-originator = Originator()
-caretaker = Caretaker()
-
-originator.set_state('on')
-print 'Originator state:', originator.get_state()  # Originator state: on
-caretaker.set_memento(originator.save_state())
-
-originator.set_state('off')
-print 'Originator change state:', originator.get_state()  # Originator change state: off
-
-originator.restore_state(caretaker.get_memento())
-print 'Originator restore state:', originator.get_state()  # Originator restore state: on
+# # coding: utf-8
+#
+# """
+# Хранитель (Memento) - паттерн поведения объектов.
+#
+# Не нарушая инкапсуляции, фиксирует и выносит за пределы объекта его внутреннее состояние так,
+# чтобы позднее можно было восстановить в нем объект.
+# """
+#
+#
+# c_ Memento o..
+#     """Хранитель"""
+#     ___ - state
+#         _?  ?
+#
+#     ___ get_state
+#         r_ _state
+#
+#
+# c_ Caretaker o..
+#     """Опекун"""
+#     ___ -
+#         _memento _ N..
+#
+#     ___ get_memento
+#         r_ _?
+#
+#     ___ set_memento memento
+#         _?  ?
+#
+#
+# c_ Originator o..
+#     """Создатель"""
+#     ___ -
+#         _state _ N..
+#
+#     ___ set_state state
+#         _?  ?
+#
+#     ___ get_state
+#         r_ _?
+#
+#     ___ save_state
+#         r_ M.. _s..
+#
+#     ___ restore_state memento
+#         _state _ m___.g_s..
+#
+#
+# originator _ O..
+# caretaker _ C..
+#
+# o___.s_s.. 'on'
+# print 'Originator state:', o___.g_s..  # Originator state: on
+# c___.s_m... o__.s_s..
+#
+# o___.s_s.. 'off'
+# print 'Originator change state:', o___.g_s..  # Originator change state: off
+#
+# o____.r_s.. c___.g_m..
+# print 'Originator restore state:', o_____.g_s..    # Originator restore state: on
