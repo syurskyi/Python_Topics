d2ef16bf8c2f0af165a285061c685cd12d8df0b0
Grey <sergejyurskyj@yahoo.com>
2020-03-08 00:11:05 -0800

    Committed Design Patterns Iterator template

--- 120_design_patterns/017_mediator/examples/mediator_002.py
+++ 120_design_patterns/017_mediator/examples/mediator_002.py
@@ -1,112 +1,112 @@
-"""Mediator pattern
-"""
-import random
-import time
-
-
-class ControlTower(object):
-
-    def __init__(self):
-        self.available_runways = list()
-        self.engaged_runways = list()
-
-    def authorize_landing(self):
-        if not self.available_runways:
-            print("Request denied. No available runways")
-            return False
-
-        else:
-            runway = self.available_runways.pop()
-            self.engaged_runways.append(runway)
-            print("Request granted. Please land on runway {}".format(runway))
-            self.status()
-            return True
-
-    def authorize_takeoff(self):
-        # for simplicity, all takeoff requests are granted
-        time.sleep(random.randint(0, 2))
-        runway = self.engaged_runways.pop()
-        self.available_runways.append(runway)
-        self.status()
-
-    def status(self):
-        print(
-            "The control tower has {} available runway/s".format(
-                len(self.available_runways)
-            )
-        )
-
-
-class Airplane(object):
-
-    def __init__(self):
-        self.control_tower = None
-
-    @property
-    def registered(self):
-        return True if self.control_tower is not None else False
-
-    def register(self, control_tower):
-        self.control_tower = control_tower
-        print("An airplane registers with the control tower")
-
-    def request_landing(self):
-        is_authorized = self.control_tower.authorize_landing()
-        if is_authorized:
-            self.land()
-
-    def land(self):
-        print("The airplane {} lands".format(self))
-
-    def takeoff(self):
-        print("The airplane {} takes off".format(self))
-        self.control_tower.authorize_takeoff()
-
-
-class Runway(object):
-
-    def register(self, control_tower):
-        print("A runway has been registered with the control tower")
-        control_tower.available_runways.append(self)
-        control_tower.status()
-
-
-def main():
-    print("There is an airport with 2 runways and a control tower\n")
-    r1 = Runway()
-    r2 = Runway()
-    ct = ControlTower()
-    r1.register(ct)
-    r2.register(ct)
-
-    print("\n3 airplanes approach the airport and register with the tower")
-    a1 = Airplane()
-    a2 = Airplane()
-    a3 = Airplane()
-    a1.register(ct)
-    a2.register(ct)
-    a3.register(ct)
-
-    print(
-        "\nTwo airplanes request for landing. There are enough runways, so "
-        "the requests are granted"
-    )
-    a1.request_landing()
-    a2.request_landing()
-
-    print(
-        "\nThe third airplane also makes a request for landing. There are no"
-        " runways available, so the request is denied"
-    )
-    a3.request_landing()
-
-    print(
-        "\nAfter a while, the first airplane takes off, so now the third "
-        "airplane can land"
-    )
-    a1.takeoff()
-    a3.request_landing()
-
-
-if __name__ == "__main__":
-    main()
+# """Mediator pattern
+# """
+# ______ ra__
+# ______ ti__
+#
+#
+# c_ ControlTower o...
+#
+#     ___ -
+#         available_runways _ l..
+#         engaged_runways _ l..
+#
+#     ___ authorize_landing
+#         __ no. a_r..:
+#             print("Request denied. No available runways")
+#             r_ F...
+#
+#         ____
+#             runway _ a_r...po.
+#             e_r__.ap.. ?
+#             print("Request granted. Please land on runway @".f.. ?
+#             s..
+#             r_ T..
+#
+#     ___ authorize_takeoff
+#         # for simplicity, all takeoff requests are granted
+#         ti__.sl.  ra__.r_i.. 0 2
+#         runway _ e_r__.po.
+#         a_r...ap.. ?
+#         s..
+#
+#     ___ status
+#         print(
+#             "The control tower has @ available runway/s".f..|
+#                 le. a_r..
+#             |
+#         |
+#
+#
+# c_ Airplane o...
+#
+#     ___ -
+#         control_tower _ N..
+#
+#     ??
+#     ___ registered
+#         r_ T.. __ c_t.. __ no. N.. ____ F...
+#
+#     ___ register control_tower
+#         ?  ?
+#         print("An airplane registers with the control tower")
+#
+#     ___ request_landing
+#         is_authorized _ c_t___.a_l..
+#         __ ?
+#             la..
+#
+#     ___ land
+#         print("The airplane @ lands".f.. ?
+#
+#     ___ takeoff
+#         print("The airplane @ takes off".f.. ?
+#         c_t___.a_t..
+#
+#
+# c_ Runway o...
+#
+#     ___ register control_tower
+#         print("A runway has been registered with the control tower")
+#         c_t___.a_r...ap.. ?
+#         c_t___.st..
+#
+#
+# ___ main
+#     print("There is an airport with 2 runways and a control tower\n")
+#     r1 _ R..
+#     r2 _ R..
+#     ct _ C..
+#     r1.r__(ct)
+#     r2.r___(ct)
+#
+#     print("\n3 airplanes approach the airport and register with the tower")
+#     a1 _ A...
+#     a2 _ A...
+#     a3 _ A...
+#     a1.r___(ct)
+#     a2.r___(ct)
+#     a3.r___(ct)
+#
+#     print(
+#         "\nTwo airplanes request for landing. There are enough runways, so "
+#         "the requests are granted"
+#     )
+#     a1.request_landing()
+#     a2.request_landing()
+#
+#     print(
+#         "\nThe third airplane also makes a request for landing. There are no"
+#         " runways available, so the request is denied"
+#     )
+#     a3.request_landing()
+#
+#     print(
+#         "\nAfter a while, the first airplane takes off, so now the third "
+#         "airplane can land"
+#     )
+#     a1.takeoff()
+#     a3.request_landing()
+#
+#
+# __ _______ __ ______
+#     ?
