1af6c270750784ffe4807e6d25863e624c756f14
Grey <sergejyurskyj@yahoo.com>
2020-03-08 11:02:05 -0700

    Committed Design Patterns - Pattern Memento examples

--- 120_design_patterns/018_memento/_exercises/templates/Memento_003.py
+++ 120_design_patterns/018_memento/_exercises/templates/Memento_003.py
@@ -1,57 +1,57 @@
-#!/usr/bin/env python
-# Written by: DGC
-
-import copy
-
-#==============================================================================
-class Memento(object):
-
-    def __init__(self, data):
-        # make a deep copy of every variable in the given class
-        for attribute in vars(data):
-            # mechanism for using properties without knowing their names
-            setattr(self, attribute, copy.deepcopy(getattr(data, attribute)))
-
-#==============================================================================
-class Undo(object):
-
-    def __init__(self):
-        # each instance keeps the latest saved copy so that there is only one 
-        # copy of each in memory
-        self.__last = None
-
-    def save(self):
-        self.__last = Memento(self)
-
-    def undo(self):
-        for attribute in vars(self):
-            # mechanism for using properties without knowing their names
-            setattr(self, attribute, getattr(self.__last, attribute))
-
-#==============================================================================
-class Data(Undo):
-
-    def __init__(self):
-        super(Data, self).__init__()
-        self.numbers = []
-
-#==============================================================================
-if (__name__ == "__main__"):
-    d = Data()
-    repeats = 10
-    # add a number to the list in data repeat times
-    print("Adding.")
-    for i in range(repeats):
-        print("0" + str(i) + " times: " + str(d.numbers))
-        d.save()
-        d.numbers.append(i)
-    print("10 times: " + str(d.numbers))
-    d.save()
-    print("")
-    
-    # now undo repeat times
-    print("Using Undo.")
-    for i in range(repeats):
-        print("0" + str(i) + " times: " + str(d.numbers))
-        d.undo()
-    print("10 times: " + str(d.numbers))
+# #!/usr/bin/env python
+# # Written by: DGC
+#
+# ______ co..
+#
+# #==============================================================================
+# c_ Memento o..
+#
+#     ___ - data
+#         # make a deep copy of every variable in the given class
+#         ___ attribute __ vars ?
+#             # mechanism for using properties without knowing their names
+#             s.. ? a.. co__.d_c_ ge.. ? a..
+#
+# #==============================================================================
+# c_ Undo o..
+#
+#     ___ -
+#         # each instance keeps the latest saved copy so that there is only one
+#         # copy of each in memory
+#         __last _ N..
+#
+#     ___ save
+#         __last _ M.. ?
+#
+#     ___ undo
+#         ___ attribute __ vars
+#             # mechanism for using properties without knowing their names
+#             s..  ? a... ge.. __l.. a...
+#
+# #==============================================================================
+# c_ Data U..
+#
+#     ___ -
+#         s__ D.. ?. -
+#         numbers _    # list
+#
+# #==============================================================================
+# __ _______ __ ______
+#     d _ D..
+#     repeats _ 10
+#     # add a number to the list in data repeat times
+#     print("Adding.")
+#     ___ i __ ra.. r..
+#         print("0" + st. ? + " times: " + st. d.n..
+#         d.s..
+#         d.n____.ap.. ?
+#     print("10 times: " + st. d.n..
+#     d.s..
+#     print("")
+#
+#     # now undo repeat times
+#     print("Using Undo.")
+#     ___ i __ ra.. r..
+#         print("0" + st. ? + " times: " + st. d.n..
+#         d.u..
+#     print("10 times: " + st. d.n..
