583cea4739ed920ece5104d44847888d4aeba13b
Grey <sergejyurskyj@yahoo.com>
2020-03-07 20:39:31 -0800

    Committed Design Patterns Iterator template

--- 120_design_patterns/016_iterator/_exercises/templates/iterator_001.py
+++ 120_design_patterns/016_iterator/_exercises/templates/iterator_001.py
@@ -1,41 +1,41 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
-Implementation of the iterator pattern with a generator
-
-*TL;DR80
-Traverses a container and accesses the container's elements.
-"""
-
-from __future__ import print_function
-
-
-def count_to(count):
-    """Counts by word numbers, up to a maximum of five"""
-    numbers = ["one", "two", "three", "four", "five"]
-    for number in numbers[:count]:
-        yield number
-
-# Test the generator
-count_to_two = lambda: count_to(2)
-count_to_five = lambda: count_to(5)
-
-print('Counting to two...')
-for number in count_to_two():
-    print(number, end=' ')
-
-print()
-
-print('Counting to five...')
-for number in count_to_five():
-    print(number, end=' ')
-
-print()
-
-### OUTPUT ###
-# Counting to two...
-# one two
-# Counting to five...
-# one two three four five
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+#
+# """
+# http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
+# Implementation of the iterator pattern with a generator
+#
+# *TL;DR80
+# Traverses a container and accesses the container's elements.
+# """
+#
+# ____ -f ______ p..
+#
+#
+# ___ count_to count
+#     """Counts by word numbers, up to a maximum of five"""
+#     numbers _ "one", "two", "three", "four", "five"
+#     ___ ? __ ?|;c..
+#         y.. ?
+#
+# # Test the generator
+# count_to_two _ l____ c... 2
+# count_to_five _ l____ c.. 5
+#
+# print('Counting to two...')
+# ___ number __ c_t_t..
+#     print ? e.._' ')
+#
+# print()
+#
+# print('Counting to five...')
+# ___ number __ c_t_f..
+#     print ? e.._' ')
+#
+# print()
+#
+# ### OUTPUT ###
+# # Counting to two...
+# # one two
+# # Counting to five...
+# # one two three four five
