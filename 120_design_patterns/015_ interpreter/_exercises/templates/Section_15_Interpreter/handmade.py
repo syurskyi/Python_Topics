49749db84ea3ebbda94f72f4f29afccbbf3775f3
Grey <sergejyurskyj@yahoo.com>
2020-03-07 16:55:09 -0800

    Committed Design Patterns Interpreter template

--- 120_design_patterns/015_ interpreter/_exercises/templates/Section_15_Interpreter/handmade.py
+++ 120_design_patterns/015_ interpreter/_exercises/templates/Section_15_Interpreter/handmade.py
@@ -1,123 +1,123 @@
-from enum import Enum
-
-
-class Token:
-    class Type(Enum):
-        INTEGER = 0
-        PLUS = 1
-        MINUS = 2
-        LPAREN = 3
-        RPAREN = 4
-
-    def __init__(self, type, text):
-        self.type = type
-        self.text = text
-
-    def __str__(self):
-        return f'`{self.text}`'
-
-
-def lex(input):
-    result = []
-
-    i = 0
-    while i < len(input):
-        if input[i] == '+':
-            result.append(Token(Token.Type.PLUS, '+'))
-        elif input[i] == '-':
-            result.append(Token(Token.Type.MINUS, '-'))
-        elif input[i] == '(':
-            result.append(Token(Token.Type.LPAREN, '('))
-        elif input[i] == ')':
-            result.append(Token(Token.Type.RPAREN, ')'))
-        else:  # must be a number
-            digits = [input[i]]
-            for j in range(i + 1, len(input)):
-                if input[j].isdigit():
-                    digits.append(input[j])
-                    i += 1
-                else:
-                    result.append(Token(Token.Type.INTEGER,
-                                        ''.join(digits)))
-                    break
-        i += 1
-
-    return result
-
-
-# ↑↑↑ lexing ↑↑↑
-
-# ↓↓↓ parsing ↓↓↓
-
-class Integer:
-    def __init__(self, value):
-        self.value = value
-
-
-class BinaryOperation:
-    class Type(Enum):
-        ADDITION = 0
-        SUBTRACTION = 1
-
-    def __init__(self):
-        self.type = None
-        self.left = None
-        self.right = None
-
-    @property
-    def value(self):
-        if self.type == self.Type.ADDITION:
-            return self.left.value + self.right.value
-        elif self.type == self.Type.SUBTRACTION:
-            return self.left.value - self.right.value
-
-
-def parse(tokens):
-    result = BinaryOperation()
-    have_lhs = False
-    i = 0
-    while i < len(tokens):
-        token = tokens[i]
-
-        if token.type == Token.Type.INTEGER:
-            integer = Integer(int(token.text))
-            if not have_lhs:
-                result.left = integer
-                have_lhs = True
-            else:
-                result.right = integer
-        elif token.type == Token.Type.PLUS:
-            result.type = BinaryOperation.Type.ADDITION
-        elif token.type == Token.Type.MINUS:
-            result.type = BinaryOperation.Type.SUBTRACTION
-        elif token.type == Token.Type.LPAREN:  # note: no if for RPAREN
-            j = i
-            while j < len(tokens):
-                if tokens[j].type == Token.Type.RPAREN:
-                    break
-                j += 1
-            # preprocess subexpression
-            subexpression = tokens[i + 1:j]
-            element = parse(subexpression)
-            if not have_lhs:
-                result.left = element
-                have_lhs = True
-            else:
-                result.right = element
-            i = j  # advance
-        i += 1
-    return result
-
-def eval(input):
-    tokens = lex(input)
-    print(' '.join(map(str, tokens)))
-
-    parsed = parse(tokens)
-    print(f'{input} = {parsed.value}')
-
-if __name__ == '__main__':
-    eval('(13+4)-(12+1)')
-    eval('1+(3-4)')
-
-    # this won't work
-    eval('1+2+(3-4)')
+# ____ e.. ______ E..
+#
+#
+# c_ Token
+#     c_ Ty__ E..
+#         INTEGER _ 0
+#         PLUS _ 1
+#         MINUS _ 2
+#         LPAREN _ 3
+#         RPAREN _ 4
+#
+#     ___ - type text
+#         ?  ?
+#         ?  ?
+#
+#     ___ -S
+#         r_ _*`|?`'
+#
+#
+# ___ lex input
+#     result _    # list
+#
+#     i _ 0
+#     w____ ? < le. ?
+#         __ ?|? __ '+'
+#             r___.ap.. T__ T__.Ty__.P.. '+'
+#         ____ ?|? __ '-'
+#             r___.ap.. T__(T__.Ty__.M.. '-'
+#         ____ ?|? __ '('
+#             r___.ap.. T__ T__.Ty__.LP.. '(')
+#         ____ ?|? __ ')'
+#             r___.ap..  T__ T__.Ty__.RP.. ')'
+#         ____  # must be a number
+#             digits _ [?|?
+#             ___ j __ ra.. ? + 1, le. ?
+#                 __ ?|?.isd..
+#                     di__.ap.. ?|?
+#                     ? +_ 1
+#                 ____
+#                     r___.ap.. T__T__.Ty__.IN..
+#                                         ''.jo.. di..
+#                     b..
+#         ? +_ 1
+#
+#     r_ r___
+#
+#
+# # ↑↑↑ lexing ↑↑↑
+#
+# # ↓↓↓ parsing ↓↓↓
+#
+# c_ Integer
+#     ___ -  value
+#         ?
+#
+#
+# c_ BinaryOperation
+#     c_ Ty__ E..
+#         ADDITION _ 0
+#         SUBTRACTION _ 1
+#
+#     ___ -
+#         type _ N..
+#         left _ N..
+#         right _ N..
+#
+#     ??
+#     ___ value
+#         __ type __ Ty__.A..
+#             r_ l___.v... + r____.v...
+#         ____ type __ Ty__.S..
+#             r_ l___.v... - r____.v...
+#
+#
+# ___ parse tokens
+#     result _ B..
+#     have_lhs _ F..
+#     i _ 0
+#     w____ i < le. t..
+#         token _ t...|?
+#
+#         __ ?.ty.. __ T__.Ty__.IN..
+#             integer _ In.. in. ?.t..
+#             __ no. h_l..
+#                 r___.l___ _ int..
+#                 h_l.. _ T..
+#             ____
+#                 r___.r____ _ integer
+#         ____ ?.ty... __ T__.Ty__.P..
+#             r___.ty... _ BO__.Ty__.A...
+#         ____ ?.ty... __ T__.Ty__.M..
+#             r___.ty... _ BO__.Ty__.S...
+#         ____ ?.ty... __ T__.Ty__.LP..  # note: no __ ___ RPAREN
+#             j _ i
+#             w____ j < le.  t..
+#                 __ ?|?.ty... __ T__.Ty__.RP..
+#                     b..
+#                 ? +_ 1
+#             # preprocess subexpression
+#             subexpression _ ?|i + 1:j
+#             element _ pa..|sub..
+#             __ no. h_l..
+#                 r___.l___ _ el..
+#                 h_l.. _ T..
+#             ____
+#                 r___.r____ _ el..
+#             i _ j  # advance
+#         i +_ 1
+#     r_ r___
+#
+# ___ evalinput
+#     tokens _ lex in..
+#     print(' '.jo.. ma. st. ?
+#
+#     parsed _ parse tokens
+#     print(_*|input _ |parsed.v...
+#
+# __ _______ __ ______
+#     eval('(13+4)-(12+1)')
+#     eval('1+(3-4)')
+#
+#     # this won't work
+#     eval('1+2+(3-4)')
