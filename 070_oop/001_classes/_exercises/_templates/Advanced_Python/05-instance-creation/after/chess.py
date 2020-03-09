8b0960ba386749bfda11250ae41f67d2bc46209f
Grey <sergejyurskyj@yahoo.com>
2020-03-08 18:24:07 -0700

    Committed oop template

--- 070_oop/001_classes/_exercises/_templates/Advanced_Python/05-instance-creation/after/chess.py
+++ 070_oop/001_classes/_exercises/_templates/Advanced_Python/05-instance-creation/after/chess.py
@@ -1,87 +1,87 @@
-
-class ChessCoordinate:
-
-    _interned = {}
-
-    def __new__(cls, file, rank):
-
-        if len(file) != 1:
-            raise ValueError("{} component file {!r} does not have a length of one."
-                             .format(cls.__name__, file))
-
-        if file not in 'abcdefgh':
-            raise ValueError("{} component file {!r} is out of range."
-                             .format(cls.__name__, file))
-
-        if rank not in range(1, 9):
-            raise ValueError("{} component rank {!r} is out of range."
-                             .format(cls.__name__, rank))
-
-        key = (file, rank)
-        if key not in cls._interned:
-            obj = super().__new__(cls)
-            obj._file = file
-            obj._rank = rank
-            cls._interned[key] = obj
-
-        return cls._interned[key]
-
-
-    @property
-    def file(self):
-        return self._file
-
-    @property
-    def rank(self):
-        return self._rank
-
-    def __repr__(self):
-        return "{}(file={}, rank={})".format(self.__class__.__name__, self.file, self.rank)
-
-    def __str__(self):
-        return '{}{}'.format(self.file, self.rank)
-
-
-def starting_board():
-    return {'♕♖': ChessCoordinate('a', 1),
-            '♕♘': ChessCoordinate('b', 1),
-            '♕♗': ChessCoordinate('c', 1),
-            '♕♕': ChessCoordinate('d', 1),
-            '♔♔': ChessCoordinate('e', 1),
-            '♔♗': ChessCoordinate('f', 1),
-            '♔♘': ChessCoordinate('g', 1),
-            '♔♖': ChessCoordinate('h', 1),
-            '♕♖♙': ChessCoordinate('a', 2),
-            '♕♘♙': ChessCoordinate('b', 2),
-            '♕♗♙': ChessCoordinate('c', 2),
-            '♕♕♙': ChessCoordinate('d', 2),
-            '♔♔♙': ChessCoordinate('e', 2),
-            '♔♗♙': ChessCoordinate('f', 2),
-            '♔♘♙': ChessCoordinate('g', 2),
-            '♔♖♙': ChessCoordinate('h', 2),
-            '♛♜': ChessCoordinate('a', 8),
-            '♛♞': ChessCoordinate('b', 8),
-            '♛♝': ChessCoordinate('c', 8),
-            '♛♛': ChessCoordinate('d', 8),
-            '♚♚': ChessCoordinate('e', 8),
-            '♚♝': ChessCoordinate('f', 8),
-            '♚♞': ChessCoordinate('g', 8),
-            '♚♜': ChessCoordinate('h', 8),
-            '♛♜♟': ChessCoordinate('a', 7),
-            '♛♞♟': ChessCoordinate('b', 7),
-            '♛♝♟': ChessCoordinate('c', 7),
-            '♛♛♟': ChessCoordinate('d', 7),
-            '♚♚♟': ChessCoordinate('e', 7),
-            '♚♝♟': ChessCoordinate('f', 7),
-            '♚♞♟': ChessCoordinate('g', 7),
-            '♚♜♟': ChessCoordinate('h', 7),
-        }
-
-
-def main():
-    boards = [starting_board() for _ in range(10000)]
-    pass
-
-
-if __name__ == '__main__':
-    main()
+#
+# c_ ?
+#
+#     _interned _     # dict
+#
+#     ___ -n ___ file rank
+#
+#         __ le. f... !_ 1
+#             r_ V... "@ component file {!r} does not have a length of one."
+#                              .f.. ___. -c  f...
+#
+#         __ f... not in 'abcdefgh':
+#             r_ V... "@ component file {!r} is out of range."
+#                              .f.. ___. -c , f...
+#
+#         __ rank no. __ ra.. 1, 9
+#             r_ V...("@ component rank {!r} is out of range."
+#                              .f.. ___. -c , ?
+#
+#         key _ f... r..
+#         __ ? no. __ ___._i..
+#             obj _ s___ . -n ___
+#             ?._f.. _ f...
+#             ?._r.. _ r..
+#             ___._i..|k.. _ o..
+#
+#         r_ ___._?|k..
+#
+#
+#     ??
+#     ___ file
+#         r_ _?
+#
+#     ??
+#     ___ rank
+#         r_ _?
+#
+#     ___ -r
+#         r_ "@(file_@, rank_@)".f..  -c . -c , f... r..
+#
+#     ___ -s
+#         r_ '@@'.f..(f... r..
+#
+#
+# ___ starting_board
+#     r_ |'♕♖': ?('a', 1),
+#             '♕♘': ?('b', 1),
+#             '♕♗': ?('c', 1),
+#             '♕♕': ?('d', 1),
+#             '♔♔': ?('e', 1),
+#             '♔♗': ?('f', 1),
+#             '♔♘': ?('g', 1),
+#             '♔♖': ?('h', 1),
+#             '♕♖♙': ?('a', 2),
+#             '♕♘♙': ?('b', 2),
+#             '♕♗♙': ?('c', 2),
+#             '♕♕♙': ?('d', 2),
+#             '♔♔♙': ?('e', 2),
+#             '♔♗♙': ?('f', 2),
+#             '♔♘♙': ?('g', 2),
+#             '♔♖♙': ?('h', 2),
+#             '♛♜': ?('a', 8),
+#             '♛♞': ?('b', 8),
+#             '♛♝': ?('c', 8),
+#             '♛♛': ?('d', 8),
+#             '♚♚': ?('e', 8),
+#             '♚♝': ?('f', 8),
+#             '♚♞': ?('g', 8),
+#             '♚♜': ?('h', 8),
+#             '♛♜♟': ?('a', 7),
+#             '♛♞♟': ?('b', 7),
+#             '♛♝♟': ?('c', 7),
+#             '♛♛♟': ?('d', 7),
+#             '♚♚♟': ?('e', 7),
+#             '♚♝♟': ?('f', 7),
+#             '♚♞♟': ?('g', 7),
+#             '♚♜♟': ?('h', 7),
+#         |
+#
+#
+# ___ main
+#     boards _ |s_b.. ___ _ __ ra.. 10000
+#     p..
+#
+#
+# __ _______ __ ______
+#     ?
