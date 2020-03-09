3cca11b1bd03458348cc416035e60069f217ec6a
Grey <sergejyurskyj@yahoo.com>
2020-03-08 18:30:19 -0700

    Committed oop metaclasses template

--- 070_oop/001_classes/_exercises/_templates/Advanced_Python/06-metaclasses/entries.py
+++ 070_oop/001_classes/_exercises/_templates/Advanced_Python/06-metaclasses/entries.py
@@ -1,39 +1,39 @@
-
-class EntriesMeta(type):
-
-    @classmethod
-    def __prepare__(mcs, name, bases, **kwargs):
-        print("Entries.__prepare__(name, bases, **kwargs)")
-        print("  mcs =", mcs)
-        print("  name =", name)
-        print("  bases =", bases)
-        print("  kwargs =", kwargs)
-        namespace = super().__prepare__(name, bases, **kwargs)
-        print("<-- namespace =", namespace)
-        print()
-        return namespace
-
-    def __new__(mcs, name, bases, namespace, num_entries, **kwargs):
-        print("Entries.__new__(mcs, name, bases, namespace, **kwargs)")
-        print("  mcs =", mcs)
-        print("  name =", name)
-        print("  bases =", bases)
-        print("  namespace =", namespace)
-        print("  kwargs =", kwargs)
-        print("  num_entries =", num_entries)
-        namespace.update({chr(i): i for i in range(ord('a'), ord('a')+num_entries)})
-        cls = super().__new__(mcs, name, bases, namespace)
-        print("<-- cls =", cls)
-        print()
-        return cls
-
-    def __init__(cls, name, bases, namespace, **kwargs):
-        print("Entries.__init__(cls, name, bases, namespace, **kwargs)")
-        print("  cls =", cls)
-        print("  name =", name)
-        print("  bases =", bases)
-        print("  namespace =", namespace)
-        print("  kwargs =", kwargs)
-        super().__init__(name, bases, namespace)
-        print()
-
+#
+# c_ EntriesMeta ty..
+#
+#     ??
+#     ___ -p___ name bases $$
+#         print("Entries.__prepare__(name, bases, **kwargs)")
+#         print("  mcs =" ?
+#         print("  name =" ?
+#         print("  bases =" ?
+#         print("  kwargs =" ?
+#         namespace = s___ . -p n.. b.. $$
+#         print("<-- namespace =" ?
+#         print()
+#         r_ n_s_
+#
+#     ___ -n ___ name bases namespace num_entries $$
+#         print("Entries.__new__(mcs, name, bases, namespace, **kwargs)")
+#         print("  mcs =" ?
+#         print("  name =" ?
+#         print("  bases =" ?
+#         print("  namespace =" ?
+#         print("  kwargs =" ?
+#         print("  num_entries =" ?
+#         n__.up..||ch.(i): ? ___ ? __ ra.. or.('a'), or.('a')+n.._e..
+#         cls = s__ . -n ___ n.. b.. n..
+#         print("<-- cls =", ___
+#         print()
+#         r_ ___
+#
+#     ___ - ___ name bases namespace $$
+#         print("Entries.__init__(cls, name, bases, namespace, **kwargs)")
+#         print("  cls =" ?
+#         print("  name =" ?
+#         print("  bases =" ?
+#         print("  namespace =" ?
+#         print("  kwargs =" ?
+#         s___ . -  n.. b.. n..
+#         print()
+#
