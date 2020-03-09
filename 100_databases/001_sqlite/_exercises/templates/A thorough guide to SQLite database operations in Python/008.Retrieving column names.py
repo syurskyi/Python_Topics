ab226b7a23ecbff718e94939ca0249f73505724f
syurskyi <sergejyurskyj@yahoo.com>
2020-03-09 14:30:25 -0700

    Update 008.Retrieving column names.py

--- 100_databases/001_sqlite/_exercises/templates/A thorough guide to SQLite database operations in Python/008.Retrieving column names.py
+++ 100_databases/001_sqlite/_exercises/templates/A thorough guide to SQLite database operations in Python/008.Retrieving column names.py
@@ -1,21 +1,21 @@
-import sqlite3
-
-sqlite_file = 'my_first_db.sqlite'
-table_name = 'my_table_3'
-
-# Connecting to the database file
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
-
-# Retrieve column information
-# Every column will be represented by a tuple with the following attributes:
-# (id, name, type, notnull, default_value, primary_key)
-c.execute('PRAGMA TABLE_INFO({})'.format(table_name))
-
-# collect names in a list
-names = [tup[1] for tup in c.fetchall()]
-print(names)
-# e.g., ['id', 'date', 'time', 'date_time']
-
-# Closing the connection to the database file
-conn.close()
+# _____ _3
+#
+# sqlite_file _ 'my_first_db.sqlite'
+# table_name _ 'my_table_3'
+#
+# # Connecting to the database file
+# conn _ _3.co.. s_f..
+# c _ ?.cu..
+#
+# # Retrieve column information
+# # Every column will be represented by a tuple with the following attributes:
+# # (id, name, type, notnull, default_value, primary_key)
+# c.ex..('PRAGMA TABLE_INFO(@)'.f.. t_n..
+#
+# # collect names in a list
+# names _ |tup|1 ___ ? __ ?.f..
+# print ?
+# # e.g., ['id', 'date', 'time', 'date_time']
+#
+# # Closing the connection to the database file
+# ?.cl..
