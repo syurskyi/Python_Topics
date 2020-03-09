023c4bec0792522264366984da75103c99c110f9
syurskyi <sergejyurskyj@yahoo.com>
2020-03-09 14:30:13 -0700

    Update 007.Date and time operations.py

--- 100_databases/001_sqlite/_exercises/templates/A thorough guide to SQLite database operations in Python/007.Date and time operations.py
+++ 100_databases/001_sqlite/_exercises/templates/A thorough guide to SQLite database operations in Python/007.Date and time operations.py
@@ -1,64 +1,64 @@
-import sqlite3
-
-sqlite_file = 'my_first_db.sqlite'    # name of the sqlite database file
-table_name = 'my_table_3'   # name of the table to be created
-id_field = 'id' # name of the ID column
-date_col = 'date' # name of the date column
-time_col = 'time'# name of the time column
-date_time_col = 'date_time' # name of the date & time column
-field_type = 'TEXT'  # column data type
-
-# Connecting to the database file
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
-
-# Creating a new SQLite table with 1 column
-c.execute('CREATE TABLE {tn} ({fn} {ft} PRIMARY KEY)'\
-        .format(tn=table_name, fn=id_field, ft=field_type))
-
-# A) Adding a new column to save date insert a row with the current date
-# in the following format: YYYY-MM-DD
-# e.g., 2014-03-06
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}'"\
-         .format(tn=table_name, cn=date_col))
-# insert a new row with the current date and time, e.g., 2014-03-06
-c.execute("INSERT INTO {tn} ({idf}, {cn}) VALUES('some_id1', DATE('now'))"\
-         .format(tn=table_name, idf=id_field, cn=date_col))
-
-# B) Adding a new column to save date and time and update with the current time
-# in the following format: HH:MM:SS
-# e.g., 16:26:37
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}'"\
-         .format(tn=table_name, cn=time_col))
-# update row for the new current date and time column, e.g., 2014-03-06 16:26:37
-c.execute("UPDATE {tn} SET {cn}=TIME('now') WHERE {idf}='some_id1'"\
-         .format(tn=table_name, idf=id_field, cn=time_col))
-
-# C) Adding a new column to save date and time and update with current date-time
-# in the following format: YYYY-MM-DD HH:MM:SS
-# e.g., 2014-03-06 16:26:37
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}'"\
-         .format(tn=table_name, cn=date_time_col))
-# update row for the new current date and time column, e.g., 2014-03-06 16:26:37
-c.execute("UPDATE {tn} SET {cn}=(CURRENT_TIMESTAMP) WHERE {idf}='some_id1'"\
-         .format(tn=table_name, idf=id_field, cn=date_time_col))
-
-# The database should now look like this:
-# id         date           time        date_time
-# "some_id1" "2014-03-06"   "16:42:30"  "2014-03-06 16:42:30"
-
-# 4) Retrieve all IDs of entries between 2 date_times
-c.execute("SELECT {idf} FROM {tn} WHERE {cn} BETWEEN '2013-03-06 10:10:10' AND '2015-03-06 10:10:10'".\
-    format(idf=id_field, tn=table_name, cn=date_time_col))
-all_date_times = c.fetchall()
-print('4) all entries between ~2013 - 2015:', all_date_times)
-
-# 5) Retrieve all IDs of entries between that are older than 1 day and 12 hrs
-c.execute("SELECT {idf} FROM {tn} WHERE DATE('now') - {dc} >= 1 AND DATE('now') - {tc} >= 12".\
-    format(idf=id_field, tn=table_name, dc=date_col, tc=time_col))
-all_1day12hrs_entries = c.fetchall()
-print('5) entries older than 1 day:', all_1day12hrs_entries)
-
-# Committing changes and closing the connection to the database file
-conn.commit()
-conn.close()
+# _____ _3
+#
+# sqlite_file _ 'my_first_db.sqlite'    # name of the sqlite database file
+# table_name _ 'my_table_3'   # name of the table to be created
+# id_field _ 'id' # name of the ID column
+# date_col _ 'date' # name of the date column
+# time_col _ 'time'# name of the time column
+# date_time_col _ 'date_time' # name of the date & time column
+# field_type _ 'TEXT'  # column data type
+#
+# # Connecting to the database file
+# conn _ _3.co.. s_f..
+# c _ ?.cu..
+#
+# # Creating a new SQLite table with 1 column
+# ?.ex.. 'CREATE TABLE @ (@ @ PRIMARY KEY)'\
+#         .f..(tn_? fn_i.f.. ft_?
+#
+# # A) Adding a new column to save date insert a row with the current date
+# # in the following format: YYYY-MM-DD
+# # e.g., 2014-03-06
+# ?.ex.. "ALTER TABLE @ ADD COLUMN '@'"\
+#          .f.. tn_? cn_d.c..
+# # insert a new row with the current date and time, e.g., 2014-03-06
+# ?.ex.. "INSERT INTO @ (@, @) VALUES('some_id1', DATE('now'))"\
+#          .f.. tn_? idf_? cn_d_c..
+#
+# # B) Adding a new column to save date and time and update with the current time
+# # in the following format: HH:MM:SS
+# # e.g., 16:26:37
+# ?.ex.. "ALTER TABLE @ ADD COLUMN '@'"\
+#          .f.. tn_? cn_t_c..
+# # update row for the new current date and time column, e.g., 2014-03-06 16:26:37
+# ?.ex.. "UPDATE @ SET @_TIME('now') WHERE @_'some_id1'"\
+#          .f.. tn_? idf_? cn_t_c..
+#
+# # C) Adding a new column to save date and time and update with current date-time
+# # in the following format: YYYY-MM-DD HH:MM:SS
+# # e.g., 2014-03-06 16:26:37
+# ?.ex.. "ALTER TABLE @ ADD COLUMN '@'"\
+#          .f.. tn_? cn_d_t_c..
+# # update row for the new current date and time column, e.g., 2014-03-06 16:26:37
+# ?.ex.. "UPDATE @ SET @_(CURRENT_TIMESTAMP) WHERE @_'some_id1'"\
+#          .f.. tn_? idf_? cn_d_t_c..
+#
+# # The database should now look like this:
+# # id         date           time        date_time
+# # "some_id1" "2014-03-06"   "16:42:30"  "2014-03-06 16:42:30"
+#
+# # 4) Retrieve all IDs of entries between 2 date_times
+# ?.ex.. "SELECT @ FROM @ WHERE @ BETWEEN '2013-03-06 10:10:10' AND '2015-03-06 10:10:10'".\
+#     f.. idf_? tn_? cn_d_t_c..
+# all_date_times _ ?.f..
+# print('4) all entries between ~2013 - 2015:' ?
+#
+# # 5) Retrieve all IDs of entries between that are older than 1 day and 12 hrs
+# ?.ex.. "SELECT @ FROM @ WHERE DATE('now') - @ >_ 1 AND DATE('now') - @ >_ 12".\
+#     f.. idf_? tn_? dc_? tc_?
+# all_1day12hrs_entries _ ?.f..
+# print('5) entries older than 1 day:' ?
+#
+# # Committing changes and closing the connection to the database file
+# ?.co..
+# ?.cl..
