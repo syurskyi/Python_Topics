605e45294e25ea8a5365b737a4cda94dce91eb26
syurskyi <sergejyurskyj@yahoo.com>
2020-03-09 14:29:57 -0700

    Update 006.Querying the database - Selecting rows.py

--- 100_databases/001_sqlite/_exercises/templates/A thorough guide to SQLite database operations in Python/006.Querying the database - Selecting rows.py
+++ 100_databases/001_sqlite/_exercises/templates/A thorough guide to SQLite database operations in Python/006.Querying the database - Selecting rows.py
@@ -1,50 +1,50 @@
 # Selecting rows from an existing SQLite database
-
-import sqlite3
-
-sqlite_file = 'my_first_db.sqlite'    # name of the sqlite database file
-table_name = 'my_table_2'	# name of the table to be queried
-id_column = 'my_1st_column'
-some_id = 123456
-column_2 = 'my_2nd_column'
-column_3 = 'my_3rd_column'
-
-# Connecting to the database file
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
-
-# 1) Contents of all columns for row that match a certain value in 1 column
-c.execute('SELECT * FROM {tn} WHERE {cn}="Hi World"'.\
-        format(tn=table_name, cn=column_2))
-all_rows = c.fetchall()
-print('1):', all_rows)
-
-# 2) Value of a particular column for rows that match a certain value in column_1
-c.execute('SELECT ({coi}) FROM {tn} WHERE {cn}="Hi World"'.\
-        format(coi=column_2, tn=table_name, cn=column_2))
-all_rows = c.fetchall()
-print('2):', all_rows)
-
-# 3) Value of 2 particular columns for rows that match a certain value in 1 column
-c.execute('SELECT {coi1},{coi2} FROM {tn} WHERE {coi1}="Hi World"'.\
-        format(coi1=column_2, coi2=column_3, tn=table_name, cn=column_2))
-all_rows = c.fetchall()
-print('3):', all_rows)
-
-# 4) Selecting only up to 10 rows that match a certain value in 1 column
-c.execute('SELECT * FROM {tn} WHERE {cn}="Hi World" LIMIT 10'.\
-        format(tn=table_name, cn=column_2))
-ten_rows = c.fetchall()
-print('4):', ten_rows)
-
-# 5) Check if a certain ID exists and print its column contents
-c.execute("SELECT * FROM {tn} WHERE {idf}=?".\
-        format(tn=table_name, cn=column_2, idf=id_column), (123456,))
-id_exists = c.fetchone()
-if id_exists:
-    print('5): {}'.format(id_exists))
-else:
-    print('5): {} does not exist'.format(some_id))
-
-# Closing the connection to the database file
-conn.close()
+#
+# _____ _3
+#
+# sqlite_file _ 'my_first_db.sqlite'    # name of the sqlite database file
+# table_name _ 'my_table_2'	# name of the table to be queried
+# id_column _ 'my_1st_column'
+# some_id _ 123456
+# column_2 _ 'my_2nd_column'
+# column_3 _ 'my_3rd_column'
+#
+# # Connecting to the database file
+# conn _ _3.co.. s_f..
+# c _ ?.cu..
+#
+# # 1) Contents of all columns for row that match a certain value in 1 column
+# ?.ex.. 'SELECT * FROM @ WHERE @_"Hi World"'.\
+#         f.. tn_? cn_c_2
+# all_rows _ ?.f..
+# print('1):' ?
+#
+# # 2) Value of a particular column for rows that match a certain value in column_1
+# ?.ex..('SELECT (@) FROM @ WHERE @_"Hi World"'.\
+#         f.. coi_c_2 tn_? cn_c_2
+# all_rows _ ?.f..
+# print('2):'?
+#
+# # 3) Value of 2 particular columns for rows that match a certain value in 1 column
+# ?.ex..('SELECT @,@ FROM @ WHERE @_"Hi World"'.\
+#         f.. coi1_c_2, coi2_c_3 tn_? cn_c_2
+# all_rows _ ?.f..
+# print('3):' ?
+#
+# # 4) Selecting only up to 10 rows that match a certain value in 1 column
+# ?.ex..('SELECT * FROM @ WHERE @_"Hi World" LIMIT 10'.\
+#         f.. tn_? cn_c_2
+# ten_rows _ ?.f..
+# print('4):' ?
+#
+# # 5) Check if a certain ID exists and print its column contents
+# ?.ex..("SELECT * FROM @ WHERE @_?".\
+#         f.. tn_?, cn_c_2, idf_i_c.. (123456,
+# id_exists _ ?.f..
+# __ ?
+#     print('5): @'.f.. ?
+# ____
+#     print('5): @ does not exist'.f.. s_i.
+#
+# # Closing the connection to the database file
+# ?.cl..
