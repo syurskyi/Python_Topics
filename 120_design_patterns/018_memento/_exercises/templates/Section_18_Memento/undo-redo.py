1c91832f30b8fd27b1bf802bf3d607e2f8ac3d8b
Grey <sergejyurskyj@yahoo.com>
2020-03-08 17:49:11 -0700

    Committed Design Patterns - Pattern Memento template

--- 120_design_patterns/018_memento/_exercises/templates/Section_18_Memento/undo-redo.py
+++ 120_design_patterns/018_memento/_exercises/templates/Section_18_Memento/undo-redo.py
@@ -1,57 +1,57 @@
-class Memento:
-    def __init__(self, balance):
-        self.balance = balance
-
-
-class BankAccount:
-    def __init__(self, balance=0):
-        self.balance = balance
-        self.changes = [Memento(self.balance)]
-        self.current = 0
-
-
-    def deposit(self, amount):
-        self.balance += amount
-        m = Memento(self.balance)
-        self.changes.append(m)
-        self.current += 1
-        return m
-
-    def restore(self, memento):
-        if memento:
-            self.balance = memento.balance
-            self.changes.append(memento)
-            self.current = len(self.changes)-1
-
-    def undo(self):
-        if self.current > 0:
-            self.current -= 1
-            m = self.changes[self.current]
-            self.balance = m.balance
-            return m
-        return None
-
-    def redo(self):
-        if self.current + 1 < len(self.changes):
-            self.current += 1
-            m = self.changes[self.current]
-            self.balance = m.balance
-            return m
-        return None
-
-    def __str__(self):
-        return f'Balance = {self.balance}'
-
-
-if __name__ == '__main__':
-    ba = BankAccount(100)
-    ba.deposit(50)
-    ba.deposit(25)
-    print(ba)
-
-    ba.undo()
-    print(f'Undo 1: {ba}')
-    ba.undo()
-    print(f'Undo 2: {ba}')
-    ba.redo()
-    print(f'Redo 1: {ba}')
+# c_ Memento
+#     ___ - balance
+#         ?  ?
+#
+#
+# c_ BankAccount
+#     ___ - balance_0
+#         ?  ?
+#         changes _ |M.. ?
+#         current _ 0
+#
+#
+#     ___ deposit amount
+#         b.. +_ ?
+#         m _ M.. b..
+#         ch__.ap.. ?
+#         cu__ +_ 1
+#         r_ m
+#
+#     ___ restore memento
+#         __ ?
+#             balance _ ?.b..
+#             ch__.ap.. ?
+#             current _ le. ch.. -1
+#
+#     ___ undo
+#         __ current > 0
+#             ? -_ 1
+#             m _ ch__|?
+#             balance _ m.b..
+#             r_ m
+#         r_ N..
+#
+#     ___ redo
+#         __ current + 1 < le. ch..
+#             ? +_ 1
+#             m _ ch__|?
+#             balance _ m.b..
+#             r_ m
+#         r_ N..
+#
+#     ___ -s
+#         r_ f'Balance _ |b..
+#
+#
+# __ _______ __ ______
+#     ba _ BA.. 100
+#     ?.d.. 50
+#     ?.d.. 25
+#     print(?)
+#
+#     ?.u..
+#     print _*Undo 1: |?
+#     ?.u..
+#     print _*Undo 2: |?
+#     ?.r..
+#     print _*Redo 1: |?
