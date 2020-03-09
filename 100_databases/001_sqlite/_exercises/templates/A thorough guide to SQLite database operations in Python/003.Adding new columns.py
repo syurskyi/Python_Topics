669c28444fbac0935ac8f52955e756d49249f723
syurskyi <sergejyurskyj@yahoo.com>
2020-03-09 14:28:59 -0700

    Update 003.Adding new columns.py

--- 100_databases/001_sqlite/_exercises/templates/A thorough guide to SQLite database operations in Python/003.Adding new columns.py
+++ 100_databases/001_sqlite/_exercises/templates/A thorough guide to SQLite database operations in Python/003.Adding new columns.py
@@ -1,25 +1,25 @@
-import sqlite3
-
-sqlite_file = 'my_first_db.sqlite'    # name of the sqlite database file
-table_name = 'my_table_2'   # name of the table to be created
-id_column = 'my_1st_column' # name of the PRIMARY KEY column
-new_column1 = 'my_2nd_column'  # name of the new column
-new_column2 = 'my_3nd_column'  # name of the new column
-column_type = 'TEXT' # E.g., INTEGER, TEXT, NULL, REAL, BLOB
-default_val = 'Hello World' # a default value for the new column rows
-
-# Connecting to the database file
-conn = sqlite3.connect(sqlite_file)
-c = conn.cursor()
-
-# A) Adding a new column without a row value
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct}"\
-        .format(tn=table_name, cn=new_column1, ct=column_type))
-
-# B) Adding a new column with a default row value
-c.execute("ALTER TABLE {tn} ADD COLUMN '{cn}' {ct} DEFAULT '{df}'"\
-        .format(tn=table_name, cn=new_column2, ct=column_type, df=default_val))
-
-# Committing changes and closing the connection to the database file
-conn.commit()
-conn.close()
+# _____ _3
+#
+# sqlite_file _ 'my_first_db.sqlite'    # name of the sqlite database file
+# table_name _ 'my_table_2'   # name of the table to be created
+# id_column _ 'my_1st_column' # name of the PRIMARY KEY column
+# new_column1 _ 'my_2nd_column'  # name of the new column
+# new_column2 _ 'my_3nd_column'  # name of the new column
+# column_type _ 'TEXT' # E.g., INTEGER, TEXT, NULL, REAL, BLOB
+# default_val _ 'Hello World' # a default value for the new column rows
+#
+# # Connecting to the database file
+# conn _ _3.co.. s_f..
+# c _ ?.cu..
+#
+# # A) Adding a new column without a row value
+# ?.ex..("ALTER TABLE @ ADD COLUMN '@' @"\
+#         .f..(tn_? cn_? ct_?
+#
+# # B) Adding a new column with a default row value
+# ?.ex..("ALTER TABLE @ ADD COLUMN '@' @ DEFAULT '@'"\
+#         .f..(tn_? cn_? ct_? df_?
+#
+# # Committing changes and closing the connection to the database file
+# ?.co..
+# ?.cl..
