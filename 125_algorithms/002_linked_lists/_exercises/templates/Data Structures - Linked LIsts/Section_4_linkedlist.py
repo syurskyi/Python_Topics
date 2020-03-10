a4d6a9140c47fbf8d60613d6892fe86ee6173708
syurskyi <sergejyurskyj@yahoo.com>
2020-03-09 18:10:06 -0700

    Update Section_4_linkedlist.py

--- 125_algorithms/002_linked_lists/_exercises/templates/Data Structures - Linked LIsts/Section_4_linkedlist.py
+++ 125_algorithms/002_linked_lists/_exercises/templates/Data Structures - Linked LIsts/Section_4_linkedlist.py
@@ -1,96 +1,96 @@
-c_ Node o..
-
-    ___  - ____ data
-        ____.?  ?
-        ____.nextNode _ N..
-
-
-c_ LinkedList o..
-
-    ___  - ____
-        ____.head _ N..
-        ____.size _ 0
-
-    # O(1) !!!
-    ___ insertStart ____ data
-
-        ____.size _ ____.? + 1
-        newNode _ N.. ?
- 
-        __ no. ____.h..
-            ____.h.. _ n..
-        ____
-            n__.nextNode _ ____.h..
-            ____.h.. _ n..
-
-    ___ remove ____ data
-
-        __ ____.h.. i. N..
-            r_
-
-        ____.s.. _ ____.? - 1
-
-        currentNode _ ____.h..
-        previousNode _ N..
-
-        while currentNode.data !_ data:
-            previousNode _ currentNode
-            currentNode _ currentNode.nextNode
-
-        __ previousNode is None:
-            ____.head _ currentNode.nextNode
-        ____:
-            previousNode.nextNode _ currentNode.nextNode
-
-    # O(1)
-    ___ size1(____):
-        r_ ____.size
-
-    # O(N) no. good !!!
-    ___ size2(____):
-
-        actualNode _ ____.head
-        size _ 0
-
-        while actualNode is no. None:
-            size +_ 1
-            actualNode _ actualNode.nextNode
-
-        r_ size
-
-    # O(N)
-    ___ insertEnd(____, data):
-
-        ____.size _ ____.size + 1
-        newNode _ Node(data)
-        actualNode _ ____.head
-
-        while actualNode.nextNode is no. None:
-            actualNode _ actualNode.nextNode
-
-        actualNode.nextNode _ newNode
-
-    ___ traverseList(____):
-
-        actualNode _ ____.head
-
-        while actualNode is no. None:
-            print("%d " % actualNode.data)
-            actualNode _ actualNode.nextNode
-
-
-linkedlist _ LinkedList()
-
-linkedlist.insertStart(12)
-linkedlist.insertStart(122)
-linkedlist.insertStart(3)
-linkedlist.insertEnd(31)
-
-linkedlist.traverseList()
-
-linkedlist.remove(3)
-linkedlist.remove(12)
-linkedlist.remove(122)
-linkedlist.remove(31)
-
-print(linkedlist.size1())
+# c_ Node o..
+#
+#     ___  -  data
+#         ?  ?
+#         nextNode _ N..
+#
+#
+# c_ LinkedList o..
+#
+#     ___  -
+#         head _ N..
+#         size _ 0
+#
+#     # O(1) !!!
+#     ___ insertStart  data
+#
+#         size _ s.. + 1
+#         newNode _ N.. ?
+#
+#         __ no. h..
+#             h.. _ nN..
+#         ____
+#             nN.nextNode _ h..
+#             h.. _ nN..
+#
+#     ___ remove data
+#
+#         __ h.. i. N..
+#             r_
+#
+#         s.. _ s.. - 1
+#
+#         currentNode _ h..
+#         previousNode _ N..
+#
+#         w____ cN__.d.. !_ d...
+#             pN__ _ cN__
+#             cN__ _ cN__.nN..
+#
+#         __ pN__ __ N..
+#             head _ cN__.nN..
+#         ____:
+#             pN__.nN.. _ cN__.nN..
+#
+#     # O(1)
+#     ___ size1 ____
+#         r_ s..
+#
+#     # O(N) no. good !!!
+#     ___ size2
+#
+#         actualNode _ h..
+#         size _ 0
+#
+#         w____ aN.. __ no. N..
+#             s.. +_ 1
+#             aN.. _ aN__.nN..
+#
+#         r_ ?
+#
+#     # O(N)
+#     ___ insertEnd data
+#
+#         s.. _ s.. + 1
+#         newNode _ N.. ?
+#         actualNode _ h..
+#
+#         w____ aN__.nN.. __ no. N..
+#             aN__ _ aN__.nN..
+#
+#         aN__.nN.. _ newNode
+#
+#     ___ traverseList ____
+#
+#         actualNode _ head
+#
+#         w___ aN__ __ no. N..
+#             print("@ "  aN__.d..
+#             aN__ _ aN__.nN..
+#
+#
+# linkedlist _ L..
+#
+# ?.iS.. 12
+# ?.iS.. 122
+# ?.iS.. 3
+# ?.iE.. 31
+#
+# ?.t..
+#
+# ?.r.. 3
+# ?.r.. 12
+# ?.r.. 122
+# ?.r.. 31
+#
+# print ?.size1
