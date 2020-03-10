032c14b958aa919a7de78e0a02ec00c122af2efe
syurskyi <sergejyurskyj@yahoo.com>
2020-03-10 12:46:45 -0700

    Update settings.py

--- 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/009_Project_Manager/projectManager/settings.py
+++ 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/009_Project_Manager/projectManager/settings.py
@@ -1,22 +1,22 @@
-import os, json
-settingsFileName = 'projectManagerFileName.json'
-
-class settingsClass(object):
-    def __init__(self):
-        self.path = os.path.join(os.path.expanduser('~'), settingsFileName)
-        if not os.path.exists(self.path):
-            self.makeDefault(self.path)
-
-    def makeDefault(self, path):
-        defData = dict(
-            path=''
-        )
-        with open(path, 'w') as f:
-            json.dump(defData, f, indent=4)
-
-    def load(self):
-        return json.load(open(self.path))
-
-    def save(self, data):
-        with open(self.path, 'w') as f:
-            json.dump(data, f, indent=4)
+# ______ __ j..
+# settingsFileName _ 'projectManagerFileName.json'
+#
+# c_ settingsClass o..
+#     ___ -
+#         path _ __.pa__.jo.. __.pa__.e..u.. ~ sFN..
+#         __ no. __.pa__.ex.. ?
+#             mD.. ?
+#
+#     ___ makeDefault path
+#         defData _ di..|
+#             ?_''
+#         |
+#         w___ o.. ? _ __ f
+#             j___.d.. dD.. ? ind.._4
+#
+#     ___ load
+#         r___ j__.l.. o.. ?
+#
+#     ___ save data
+#         w___ o.. ?, _ __ f
+#             j__.d.. ? ? ind.._4
