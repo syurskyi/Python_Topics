56080802d0cd082158c0258bf99d95229f806712
syurskyi <sergejyurskyj@yahoo.com>
2020-03-10 12:46:15 -0700

    Update projectManager.py

--- 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/009_Project_Manager/projectManager/projectManager.py
+++ 140_gui/pyqt_pyside/_exercises/templates/Advanced_Python_Scripting/009_Project_Manager/projectManager/projectManager.py
@@ -1,77 +1,72 @@
-from PySide.QtCore import *
-from PySide.QtGui import *
-from widgets import projectManager_UIs as ui
-from widgets import projectListWidget
-import createProjectDialog, settingsDialog, templateEditor, settings, createProject
-import webbrowser
-
-class projectManagerClass(QMainWindow, ui.Ui_projectManager):
-    def __init__(self):
-        super(projectManagerClass, self).__init__()
-        self.setupUi(self)
-
-        # widgets
-
-        self.projectList_lwd = projectListWidget.projectListClass()
-        self.projectList_ly.addWidget(self.projectList_lwd)
-
-        # connects
-
-        self.create_btn.clicked.connect(self.createProject)
-        self.settings_btn.clicked.connect(self.openSettingsDialog)
-        self.templateEditor_btn.clicked.connect(self.openTemplateEditorDialog)
-        self.projectList_lwd.itemClicked.connect(self.showInfo)
-        self.projectList_lwd.itemDoubleClicked.connect(self.openProject)
-
-        # start
-
-        self.info_lb.setText('')
-        self.updateList()
-
-    def updateList(self):
-
-        if not self.projectList_lwd.updateProjectList():
-            self.create_btn.setEnabled(0)
-        else:
-            self.create_btn.setEnabled(1)
-
-    def openSettingsDialog(self):
-        self.dial = settingsDialog.settingsDialogClass(self)
-        if self.dial.exec_():
-            data = self.dial.getTableData()
-            settings.settingsClass().save(data)
-        self.updateList()
-
-    def openTemplateEditorDialog(self):
-        self.dial = templateEditor.templateEditorClass()
-        self.dial.show()
-
-    def createProject(self):
-        self.dial = createProjectDialog.createProjectDialogClass(self)
-        if self.dial.exec_():
-            data = self.dial.getDialogData()
-            createProject.createProject(data)
-            self.updateList()
-
-    def showInfo(self, item):
-        info = createProject.getProjectInfo(item.data(32))
-        if info:
-            text = '''Name:
-%s
-
-Comment:
-%s
-''' % (info['name'], info['comment'])
-        else:
-            text = ''
-        self.info_lb.setText(text)
-
-    def openProject(self, item):
-        path = item.data(32)
-        webbrowser.open(path)
-
-if __name__ == '__main__':
-    app = QApplication([])
-    w = projectManagerClass()
-    w.show()
-    app.exec_()
+# ____ __.________ _
+# ____ __.__ ______ _
+# ____ w.. ______ pM_U.. __ ui
+# ____ w.. ______ pLW..
+# ______ cPD.. sD... tE... s... cP..
+# ______ web...
+#
+# c_ projectManagerClass QM.. __.U_pM..
+#     ___ -
+#         s___ ?  ? . -
+#         sU. ?
+#
+#         # widgets
+#
+#         projectList_lwd _ pLW__.pLC..
+#         pL.._l_.aW.. pL_l..
+#
+#         # connects
+#
+#         c_b__.c__.c__ cP..
+#         s_b__.c__.c__ oSD..
+#         tE_b__.c__.c__ oTED..
+#         pL_l__.iC__.c__ sI..
+#         pL_l__.iDC__.c__ oP..
+#
+#         # start
+#
+#         i_l_.sT.. ''
+#         uL..
+#
+#     ___ updateList
+#
+#         __ no. pL_l__.uPL..
+#             c_b__.sE.. 0
+#         ____
+#             c_b__.sE.. 1
+#
+#     ___ openSettingsDialog
+#         dial _ sD__.sDC..
+#         __ ?.ex.._
+#             data _ ?.gTD..
+#             s___.sC__.s.. d..
+#         uL..
+#
+#     ___ openTemplateEditorDialog
+#         dial _ tE__.tEC..
+#         ?.sh..
+#
+#     ___ cP..
+#         dial _ cPD___.cPDC..
+#         __ ?.ex.._
+#             data _ ?.gDD..
+#             cP...cP.. ?
+#             uL..
+#
+#     ___ showInfo item
+#         info _ cP...gPI.. it__.da.. 32
+#         __ ?
+#             text _ "Name: @ Comment: @ "  |?|*n.. , ?|*c..
+#         ____
+#             text _ ''
+#         i_l_.sT.. ?
+#
+#     ___ openProject item
+#         path _ ?.d.. 32
+#         w___.o.. ?
+#
+# __ _____ __ ______
+#     app _ ?
+#     w _ ?
+#     ?.s..
+#     ?.e..
