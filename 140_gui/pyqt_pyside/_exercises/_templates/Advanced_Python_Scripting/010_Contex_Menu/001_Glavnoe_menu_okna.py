2a7c908c025f6b359b77ab1f895497a1e0bd72f6
Grey <sergejyurskyj@yahoo.com>
2020-03-10 21:35:29 -0700

    Committed Advanced Python Scripting - Context Menu template

--- 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/010_Contex_Menu/001_Glavnoe_menu_okna.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/010_Contex_Menu/001_Glavnoe_menu_okna.py
@@ -1,51 +1,51 @@
-from PySide.QtCore import *
-from PySide.QtGui import *
-
-# ne vsegda menjy buvaet statichnoe. Sostav menju zavisit ot kakih to yslovij. Poetomy chasto menu prohoditsja generirovat'
-# v moment sozdanija
-
-class MainWindowClass(QMainWindow):
-    def __init__(self):
-        super(MainWindowClass, self).__init__()
-        # widget
-        self.widget = QWidget(self)
-        self.setCentralWidget(self.widget)
-        # menu bar
-        self.menu_bar = QMenuBar()
-        self.setMenuBar(self.menu_bar)
-        # menu
-        self.menu = QMenu('File')
-        self.menu.setTearOffEnabled(1)                                     # dobavljaet v menu  divide line pri nazatii na kototyjy mozno otsoedinit' menu i ono stanet otdel;num vidgetpm
-        self.menu_bar.addMenu(self.menu)
-        # actions first version
-        self.act1 = QAction('Open', self)    # objazatel'no ykazuvat' parent
-        self.act1.setCheckable(1)
-        self.act1.triggered.connect(self.action)
-        self.menu.addAction(self.act1)
-
-        # actions second version                                            # raznica mezdy pervum i vtorum sposobom
-                                                                            # mu ne imeem prjmogo dostypa neposredstvenno k action (self.act1)
-                                                                            # potomy shto vo vtorom variante on sozdajotsja dinamicheski
-                                                                            # i y nas net peremennoj, kotoraja ssulaetsja na etot objekt
-
-        self.menu.addAction(QAction('Save', self, triggered=self.action))
-        # self.menu.actions()[1]
-
-        # submenu
-        self.sMenu = QMenu('Sub')
-        self.menu.addMenu(self.sMenu)
-
-        for i in range(10):
-            self.sMenu.addAction(QAction('Item %s' % i, self))
-
-    def action(self):
-        if self.act1.isCheckable():
-            print 'ACTION'
-        else:
-            print 'STOP'
-
-if __name__ == '__main__':
-    app = QApplication([])
-    w = MainWindowClass()
-    w.show()
-    app.exec_()
+# ____ __.__ ______ _
+# ____ __.__ ______ _
+#
+# # ne vsegda menjy buvaet statichnoe. Sostav menju zavisit ot kakih to yslovij. Poetomy chasto menu prohoditsja generirovat'
+# # v moment sozdanija
+#
+# c_ MainWindowClass QMa..
+#     ___ -
+#         s___ ?  ? -
+#         # widget
+#         widget _ QW.. ?
+#         sCW.. ?
+#         # menu bar
+#         menu_bar _ QM..
+#         sMB.. ?
+#         # menu
+#         menu _ QM.. File
+#         ?.sTOE.. 1                                    # dobavljaet v menu  divide line pri nazatii na kototyjy mozno otsoedinit' menu i ono stanet otdel;num vidgetpm
+#         m_b_.aM.. ?
+#         # actions first version
+#         act1 _ QAc.. Open ?    # objazatel'no ykazuvat' parent
+#         ?.sC.. 1
+#         ?.tr__.c.. a..
+#         m__.aA.. ?
+#
+#         # actions second version                                            # raznica mezdy pervum i vtorum sposobom
+#                                                                             # mu ne imeem prjmogo dostypa neposredstvenno k action (act1)
+#                                                                             # potomy shto vo vtorom variante on sozdajotsja dinamicheski
+#                                                                             # i y nas net peremennoj, kotoraja ssulaetsja na etot objekt
+#
+#         m___.aA.. QAc.. Save ? t_a..
+#         # menu.actions()[1]
+#
+#         # submenu
+#         sMenu _ QMe.. Sub
+#         m___.aM.. ?
+#
+#         ___ i __ ra.. 10
+#             sM__.aA.. QAc.. 'Item @'  ? ?
+#
+#     ___ action
+#         __ _1.iCh..
+#             print 'ACTION'
+#         ____
+#             print 'STOP'
+#
+# __ ______ __ ______
+#     app _ ?
+#     w _ ?
+#     ?.s..
+#     ?.e..
