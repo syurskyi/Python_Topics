c959d6a5b4ac991ac53d658bfbf9ec19cf67731d
syurskyi <sergejyurskyj@yahoo.com>
2020-03-10 17:46:45 -0700

    Update 0207.Primary Key ID.py

--- 100_databases/001_sqlite/_exercises/templates/Introduction_to_SQLite_Databases_for_Python_Programming/02.Using SQLite With Python/0207.Primary Key ID.py
+++ 100_databases/001_sqlite/_exercises/templates/Introduction_to_SQLite_Databases_for_Python_Programming/02.Using SQLite With Python/0207.Primary Key ID.py
@@ -1,21 +1,21 @@
-import sqlite3
-
-# connect to database
-# conn = sqlite3.connect(':memory:')
-conn = sqlite3.connect('customer.db')
-
-# Create a cursor
-c = conn.cursor()
-
-# Query The Database
-c.execute("SELECT rowid, * FROM customers")
-items = c.fetchall()
-
-for item in items:
-    print(item)
-
-# Commit our command
-conn.commit()
-
-# Close our connection
-conn.close()
+# ______ _3
+#
+# # connect to database
+# # conn = sqlite3.connect(':memory:')
+# conn _ _3.c.. customer.db
+#
+# # Create a cursor
+# c _ ?.c..
+#
+# # Query The Database
+# ?.execute("S.. r.., _ F.. customers")
+# items _ ?.f..
+#
+# ___ item __ ?
+#     print ?
+#
+# # Commit our command
+# ?.c..
+#
+# # Close our connection
+# ?.c..
