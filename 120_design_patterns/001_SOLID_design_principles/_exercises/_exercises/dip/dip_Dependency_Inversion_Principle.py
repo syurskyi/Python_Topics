6c1565e2ab1d3e23f942db825f400a766687ef52
Grey <sergejyurskyj@yahoo.com>
2020-03-10 20:48:39 -0700

    Committed dependency_inversion_principle.py exercise

--- 120_design_patterns/001_SOLID_design_principles/_exercises/_exercises/dip/dip_Dependency_Inversion_Principle.py
+++ 120_design_patterns/001_SOLID_design_principles/_exercises/_exercises/dip/dip_Dependency_Inversion_Principle.py
@@ -1,59 +1,60 @@
-# ____ a.. ______ a..
-# ____ e.. ______ E..
-#
-#
-# c_ Relationship E..
-#     PARENT = 0
-#     CHILD = 1
-#     SIBLING = 2
-#
-#
-# c_ Person
-#     ___ - name
-#         ??  ?
-#
-#
-# c_ RelationshipBrowser
-#     ??
-#     ___ find_all_children_of name p..
-#
-#
-# c_ Relationships(RB..  # low-level
-#     relations _     # list
-#
-#     ___ add_parent_and_child  parent child
-#         ?r___.ap.. p.. R__.P... c..
-#         ?r___.ap.. c.. R__.P... p..
-#
-#     ___ find_all_children_of  name
-#         ___ r __ ?r..
-#             __ ? 0 .? __ ? an. ? 1 __ R__.P..
-#                 y.. ? 2 .n..
-#
-#
-# c_ Research
-#     # dependency on a low-level module directly
-#     # bad because strongly dependent on e.g. storage type
-#
-#     # ___ __init__(self, relationships):
-#     #     # high-level: find all of john's children
-#     #     relations = relationships.relations
-#     #     for r in relations:
-#     #         if r[0].name == 'John' and r[1] == Relationship.PARENT:
-#     #             print(f'John has a child called {r[2].name}.')
-#
-#     ___ - browser
-#         ___ p __ ?.find_all_children_of "John"
-#             print _*John has a child called ?')
-#
-#
-# parent = P.. ('John')
-# child1 = P.. ('Chris')
-# child2 = P.. ('Matt')
-#
-# # low-level module
-# relationships = R..
-# ?.a.. p.. c_1
-# ?.a.. p.. c_2
-#
-# R.. ?
+from abc import abstractmethod
+from enum import Enum
+
+
+class Relationship(Enum):
+    PARENT = 0
+    CHILD = 1
+    SIBLING = 2
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+class RelationshipBrowser:
+    @abstractmethod
+    def find_all_children_of(self, name):
+        pass
+
+
+class Relationships(RelationshipBrowser):
+    relations = []
+
+    def add_parent_and_child(self, parent, child):
+        self.relations.append((parent, Relationship.PARENT, child))
+        self.relations.append((child, Relationship.PARENT, parent))
+
+    def find_all_children_of(self, name):
+        for r in self.relations:
+            if r[0].name == name and r[1] == Relationship.PARENT:
+                yield r[2].name
+
+
+class Research:
+    # dependency on a low-level module directly
+    # bad because strongly dependent on e.g. storage type
+
+    # ___ __init__(self, relationships):
+    #     # high-level: find all of john's children
+    #     relations = relationships.relations
+    #     for r in relations:
+    #         if r[0].name == 'John' and r[1] == Relationship.PARENT:
+    #             print(f'John has a child called {r[2].name}.')
+
+    def __init__(self, browser):
+        for p in browser.find_all_children_of("John"):
+            print(f'John has a child called ?')
+
+
+parent = Person('John')
+child1 = Person('Chris')
+child2 = Person('Matt')
+
+# low-level module
+relationships = Relationship()
+relationships.add_parent_and_child(parent, child1)
+relationships.add_parent_and_child(parent, child2)
+
+Research(relationships)
