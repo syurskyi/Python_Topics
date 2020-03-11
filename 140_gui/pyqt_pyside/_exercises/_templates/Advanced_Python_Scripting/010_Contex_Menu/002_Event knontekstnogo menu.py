e742aaf170a3304dab3022f1c28628137102ff6d
Grey <sergejyurskyj@yahoo.com>
2020-03-10 21:35:41 -0700

    Committed Advanced Python Scripting template

--- 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/010_Contex_Menu/002_Event knontekstnogo menu.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/010_Contex_Menu/002_Event knontekstnogo menu.py
@@ -1,38 +1,38 @@
-from PySide.QtCore import *
-from PySide.QtGui import *
-
-textArray = 'Click', 'Press', 'Enter'
-
-
-class WidgetMenuClass(QWidget):
-    def __init__(self):
-        super(WidgetMenuClass, self).__init__()
-        ly = QVBoxLayout(self)
-        self.btn = QPushButton('Click')
-        ly.addWidget(self.btn)
-        self.line = QLineEdit()
-        ly.addWidget(self.line)
-
-        self.btn.setContextMenuPolicy(Qt.CustomContextMenu)        # shto bu contekstnoe menu pojavilos', nado etoj knopke skazat' shto bu ona reagirovala na etot signal
-        self.btn.customContextMenuRequested.connect(self.openMenu) # etot signal generitsja kogda mu zaprashuvaem
-                                                                   #  konteksnoe menu a eto y nas nazatie pravoj knopkoj mushi
-
-    def openMenu(self, pos):
-        pos = self.btn.mapToGlobal(pos)                           # y lybogo widgeta est' metodu kotorue delajyt remap koordinat
-        menu = QMenu()                                            # esli net pozicii gde bul proizvedjon klik mozno ykazat' koordinatu kyrsora
-
-        for i in textArray:
-            menu.addAction(QAction(i, self))
-        # menu.exec_(pos)                                         # lokalnue koordinatu klika na preobrazovat' v global'nue koordinatu ekrana
-        a = menu.exec_(QCursor().pos())                           # funkcija obrachaetsja k tekychemy kyrsory i vozvrachaet ego global'nue koordinatu
-        # print a                                                 # menu.exec_ vozvrachaet nam action, kotoruj bul vubran i mu mozem ego zabrat'
-        # print a.text()
-        if a:
-            self.btn.setText(a.text())
-
-
-if __name__ == '__main__':
-    app = QApplication([])
-    w = WidgetMenuClass()
-    w.show()
-    app.exec_()
+# ____ __.__ ______ _
+# ____ __.__ ______ _
+#
+# textArray _ 'Click', 'Press', 'Enter'
+#
+#
+# c_ WidgetMenuClass QW..
+#     ___ -
+#         s____ ?  ?. -
+#         ly _ QVBL.. ?
+#        btn _ QPB.. *Cl..
+#         ?.aW.. ?
+#        line _ QE..
+#         l_.aW.. ?
+#
+#        ?.sCMP.. __.CCM..       # shto bu contekstnoe menu pojavilos', nado etoj knopke skazat' shto bu ona reagirovala na etot signal
+#        ?.cCMR__.c.. o.. # etot signal generitsja kogda mu zaprashuvaem
+#                                                                    #  konteksnoe menu a eto y nas nazatie pravoj knopkoj mushi
+#
+#     ___ openMenu pos
+#         pos _b__.mTG.. ?                        # y lybogo widgeta est' metodu kotorue delajyt remap koordinat
+#         menu _ QM..                                           # esli net pozicii gde bul proizvedjon klik mozno ykazat' koordinatu kyrsora
+#
+#         ___ i __ tA..
+#             m__.aA.. QA.. ? ?
+#         # menu.exec_(pos)                                         # lokalnue koordinatu klika na preobrazovat' v global'nue koordinatu ekrana
+#         a _ m__.ex.._ QCu...p..                         # funkcija obrachaetsja k tekychemy kyrsory i vozvrachaet ego global'nue koordinatu
+#         # print a                                                 # menu.exec_ vozvrachaet nam action, kotoruj bul vubran i mu mozem ego zabrat'
+#         # print a.text()
+#         __ a
+#            b__.sT.. ?.t..
+#
+#
+# __ ______ __ ______
+#     app _ ?
+#     w _ ?
+#     ?.s..
+#     ?.e..
