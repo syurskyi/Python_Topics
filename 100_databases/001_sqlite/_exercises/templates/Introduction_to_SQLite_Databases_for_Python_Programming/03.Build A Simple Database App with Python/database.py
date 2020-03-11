1f3e5a447db6157b6cf1903db904057921a1063d
syurskyi <sergejyurskyj@yahoo.com>
2020-03-10 17:48:56 -0700

    Update database.py

--- 100_databases/001_sqlite/_exercises/templates/Introduction_to_SQLite_Databases_for_Python_Programming/03.Build A Simple Database App with Python/database.py
+++ 100_databases/001_sqlite/_exercises/templates/Introduction_to_SQLite_Databases_for_Python_Programming/03.Build A Simple Database App with Python/database.py
@@ -1,64 +1,64 @@
-import sqlite3
-
-
-# Query The DB and Return All Records
-
-def show_all():
-    # connect to database
-    conn = sqlite3.connect('customer.db')
-
-    # Create a cursor
-    c = conn.cursor()
-
-    # Query The Database
-    c.execute("SELECT rowid, * FROM customers")
-
-    items = c.fetchall()
-
-    for item in items:
-        print(item)
-
-    # Commit our command
-    conn.commit()
-
-    # Close our connection
-    conn.close()
-
-
-# Add a New Record to the Table
-def add_one(first, last, email):
-    conn = sqlite3.connect('customer.db')
-    c = conn.cursor()
-    c.execute("INSERT INTO customers VALUES (?, ?, ?)", (first, last, email))
-    conn.commit()
-    conn.close()
-
-
-# Add Many Records to Table
-def add_many(list):
-    conn = sqlite3.connect('customer.db')
-    c = conn.cursor()
-    c.executemany("INSERT INTO customers VALUES (?, ?, ?)", (list))
-    conn.commit()
-    conn.close()
-
-
-# Delete Record from the table
-def delete_one(id):
-    conn = sqlite3.connect('customer.db')
-    c = conn.cursor()
-    c.execute("DELETE from customers WHERE rowid = (?)", id)
-    conn.commit()
-    conn.close()
-
-
-# Lookup with Where
-def email_lookup(email):
-    conn = sqlite3.connect('customer.db')
-    c = conn.cursor()
-    c.execute('SELECT * from customers WHERE email = (?)', (email,))
-    items = c.fetchall()
-    for item in items:
-        print(item)
-    conn.commit()
-    conn.close()
+# ______ _3
+#
+#
+# # Query The DB and Return All Records
+#
+# ___ show_all
+#     # connect to database
+#     conn _ _3.c.. customer.db
+#
+#     # Create a cursor
+#     c _ ?.c..
+#
+#     # Query The Database
+#     ?.e.. S.. r.., _ F.. customers
+#
+#     items _ c.f..
+#
+#     ___ item __ ?
+#         print ?
+#
+#     # Commit our command
+#     ?.c..
+#
+#     # Close our connection
+#     ?.c..
+#
+#
+# # Add a New Record to the Table
+# ___ add_one first last email
+#     conn _ _3.c.. customer.db
+#     c _ ?.c..
+#     c.e.. I.. I.. customers V... |?, ?, ? *, ?  ?  ?
+#     ?.c..
+#     ?.c..
+#
+#
+# # Add Many Records to Table
+# ___ add_many li..
+#     conn _ _3.c.. customer.db
+#     c _ ?.c..
+#     c.e..m.. I.. I.. customers V.. |?, ?, ?|  ?
+#     ?.c..
+#     ?.c..
+#
+#
+# # Delete Record from the table
+# ___ delete_one id
+#     conn _ _3.c.. customer.db
+#     c _ ?.c..
+#     c.e..  D.. f.. customers W.. r.. _ ? , ?
+#     ?.c..
+#     ?.c..
+#
+#
+# # Lookup with Where
+# ___ email_lookup email
+#     conn _ _3.c.. customer.db
+#     c _ ?.c..
+#     c.e.. S.. _ from customers W.. email _ ? , ?
+#     items _ c.f..
+#     ___ item __ ?
+#         print ?
+#     ?.c..
+#     ?.c..
