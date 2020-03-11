b42da3dde497a1fc13799cde17514566739e2346
syurskyi <sergejyurskyj@yahoo.com>
2020-03-10 17:45:10 -0700

    Update 0201.Create SQLite Connection.py

--- 100_databases/001_sqlite/_exercises/templates/Introduction_to_SQLite_Databases_for_Python_Programming/02.Using SQLite With Python/0201.Create SQLite Connection.py
+++ 100_databases/001_sqlite/_exercises/templates/Introduction_to_SQLite_Databases_for_Python_Programming/02.Using SQLite With Python/0201.Create SQLite Connection.py
@@ -1,5 +1,5 @@
-import sqlite3
-
-# connect to database
-# conn = sqlite3.connect(':memory:')
-conn = sqlite3.connect('customer.db')
+# ______ _3
+#
+# # connect to database
+# # conn = _3.connect(':memory:')
+# conn _ _3.c.. customer.db
