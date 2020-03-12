b982dcc509f3e7ae09d6375042c87e1f8aac5ab1
syurskyi <sergejyurskyj@yahoo.com>
2020-03-11 17:55:55 -0700

    Update 001.SQLite Python Creating a New Database.py

--- 100_databases/001_sqlite/_exercises/templates/SQLite Python sqlitetutorial/001.SQLite Python Creating a New Database.py
+++ 100_databases/001_sqlite/_exercises/templates/SQLite Python sqlitetutorial/001.SQLite Python Creating a New Database.py
@@ -1,19 +1,19 @@
-import sqlite3
-from sqlite3 import Error
-
-
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-
-
-if __name__ == '__main__':
-    create_connection(r"C:\sqlite\db\pythonsqlite.db")
+# ______ _3
+# ____ _3 ______ E..
+#
+#
+# ___ create_connection db_file
+#     """ create a database connection to a SQLite database """
+#     conn _ N..
+#     ___
+#         ? _ _3.c.. ?
+#         print _3.v..
+#     ______ E.. __ e
+#         print ?
+#     ________
+#         __ ?
+#             ?.c..
+#
+#
+# __ _______ __ _____
+#     ? _ C:\sqlite\db\pythonsqlite.db
