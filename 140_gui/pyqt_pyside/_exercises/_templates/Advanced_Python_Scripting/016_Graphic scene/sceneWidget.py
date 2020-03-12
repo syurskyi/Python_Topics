bf0559132ec71108b614ef3852f8892cf1c588eb
syurskyi <sergejyurskyj@yahoo.com>
2020-03-12 16:55:08 -0700

    Update sceneWidget.py

--- 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/016_Graphic scene/sceneWidget.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/016_Graphic scene/sceneWidget.py
@@ -1,25 +1,25 @@
-from PySide.QtCore import *
-from PySide.QtGui import *
-import exampleItem
-
-class exampleSceneClass(QGraphicsScene):
-    def __init__(self):
-        super(exampleSceneClass, self).__init__()
-        self.setSceneRect(-200, -200, 400, 400)
-        self.fillScene()
-
-    def fillScene(self):
-        item = exampleItem.exampleItemClass()
-        self.addItem(item)
-
-    def drawBackground(self, painter, rec):
-        painter.setPen(QPen(Qt.red, 3))
-        painter.drawRect(self.sceneRect())
-        painter.setPen(QPen(Qt.black))
-        painter.drawText(0, 0, '0')
-        painter.drawText(-200, -200, '-200')
-        painter.drawText(200, 200, '200')
-
-        painter.setPen(QPen(Qt.black, 1))
-        painter.drawLine(0, -300, 0, 300)
-        painter.drawLine(-300, 0, 300, 0)
+# ____ __.__ ______ _
+# ____ __.__ ______ _
+# ______ eI..
+#
+# c_ exampleSceneClass QG..
+#     ___ -
+#         s___? ?. -
+#         sSR.. -200, -200, 400, 400
+#         fS..
+#
+#     ___ fillScene
+#         item _ eI__.eIC..
+#         aI.. ?
+#
+#     ___ drawBackground painter rec
+#         ?.sP.. QP.. __.re. 3
+#         ?.dR.. sR..
+#         ?.sP.. QP.. __.bl..
+#         ?.dT..0, 0, '0')
+#         ?.dT.. -200, -200, '-200'
+#         ?.dT.. 200, 200, '200'
+#
+#         ?.sP.. QP.. __.bl.. 1
+#         ?.dL.. 0 -300 0 300
+#         ?.dL.. -300 0 300 0
