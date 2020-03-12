4c540e8622cfd25153edd443a7fcc1b8074ba323
syurskyi <sergejyurskyj@yahoo.com>
2020-03-12 15:07:50 -0700

    Update 002_Risovanie_primitivov.py

--- 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/015_Custom_Widget/002_Risovanie_primitivov.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/015_Custom_Widget/002_Risovanie_primitivov.py
@@ -1,35 +1,34 @@
-from PySide.QtCore import *
-from PySide.QtGui import *
-
-
-class DrawCustomWidget(QWidget):
-    def __init__(self):
-        super(DrawCustomWidget, self).__init__()
-        self.resize(300, 200)
-        self.setWindowTitle('Custom Widget')
-
-    def paintEvent(self, event):
-        rec = event.rect()
-        if False:
-            rec = QRect()
-        painter = QPainter()
-        painter.begin(self)
-        painter.setRenderHint(QPainter.Antialiasing)
-        painter.setRenderHint(QPainter.HighQualityAntialiasing)
-        ###
-        painter.fillRect(rec, Qt.black)
-        painter.setPen(QPen(QBrush(Qt.red), 3))
-        painter.setBrush(QBrush(Qt.black))
-        painter.drawRect(30, 30, rec.width() - 60, rec.height() - 60)
-
-        painter.setPen(QPen(QBrush(Qt.green), 8))
-        painter.drawLine(100, 0, 150, rec.height())
-
-        painter.end()
-
-
-if __name__ == '__main__':
-    app = QApplication([])
-    w = DrawCustomWidget()
-    w.show()
-    app.exec_()
+# ____ __.__ ______ _
+# ____ __.__ ______ _
+# 
+# c_ DrawCustomWidget QW..
+#     ___ -
+#         s__ ? ?. -
+#         r.. 300, 200
+#         sWT.. *Custom Widget
+# 
+#     ___ paintEvent event
+#         rec _ ?.re..
+#         __ F..
+#             rec _ QR..
+#         painter _ QP..
+#         ?.b.. ?
+#         ?.sRH..(QP__.An..
+#         ?.sRH..(QP__.HQAn..
+#         ###
+#         ?.fR.. ? __.bl..
+#         ?.sP.. QP.. QB.. __.re. 3
+#         ?.sB.. QB.. __.bl..
+#         ?.dR.. 30, 30, ?.w.. - 60 ?.h.. - 60
+# 
+#         ?.sP.. QP.. QB.. __.gr.. 8
+#         ?.dL.. 100, 0, 150, ?.h..
+# 
+#         ?.en.
+# 
+# 
+# __ ______ __ ______
+#     app _ ?
+#     w _ ?
+#     ?.s..
+#     ?.e..
