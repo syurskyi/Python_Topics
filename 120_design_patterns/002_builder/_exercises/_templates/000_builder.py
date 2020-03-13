27eb083302d674a18396c0cff54b91e5e757970f
Grey <sergejyurskyj@yahoo.com>
2020-03-12 19:49:24 -0700

    Committed design patterns exercise

--- 120_design_patterns/002_builder/_exercises/_templates/000_builder.py
+++ 120_design_patterns/002_builder/_exercises/_templates/000_builder.py
@@ -1,5 +1,41 @@
 # # -*- coding : utf-8 -*-
 #
+
+# abc.ABC basically just an extra layer over metaclass=abc.ABCMeta. i.e abc.ABC implicitly defines the metaclass for us.
+# The only difference is that in the former case you need a simple inheritance and in the latter you need
+# to specify the metaclass.
+# New class ABC has ABCMeta as its meta class. Using ABC as a base class has essentially the same effect as specifying
+# metaclass=abc.ABCMeta, but is simpler to type and easier to read.
+
+#
+# class abc.ABC
+#     A helper class that has ABCMeta as its metaclass. With this class, an abstract base class can be created by
+#     simply deriving from ABC avoiding sometimes confusing metaclass usage, for example:
+#     from abc import ABC
+#
+#     class MyABC(ABC):
+#         pass
+#     Note that the type of ABC is still ABCMeta, therefore inheriting from ABC requires the usual precautions
+#     regarding metaclass usage, as multiple inheritance may lead to metaclass conflicts. One may also define
+#     an abstract base class by passing the metaclass keyword and using ABCMeta directly, for example:
+#
+#     from abc import ABCMeta
+#
+#     class MyABC(metaclass=ABCMeta):
+#         pass
+#
+#     New in version 3.4.
+#
+# class abc.ABCMeta
+#
+#     Metaclass for defining Abstract Base Classes (ABCs).
+#     Use this metaclass to create an ABC. An ABC can be subclassed directly, and then acts as a mix-in class.
+#     You can also register unrelated concrete classes (even built-in classes) and unrelated ABCs
+#     as “virtual subclasses” – these and their descendants will be considered subclasses of the registering ABC
+#     by the built-in issubclass() function, but the registering ABC won’t show up in their
+#     MRO (Method Resolution Order) nor will method implementations defined by the registering ABC be callable
+#     (not even via super()). 1
+
 # ____ -f ______ an...
 # ____ a.. ______ A.. a..m.. a..p..
 # ____ ty.. ______ A..
