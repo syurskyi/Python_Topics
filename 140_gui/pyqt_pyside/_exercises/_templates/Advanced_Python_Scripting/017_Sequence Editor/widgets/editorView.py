12ac88c2a6ce86bdc7db15855e9463952cb91197
syurskyi <sergejyurskyj@yahoo.com>
2020-03-13 11:47:54 -0700

    Update editorView.py

--- 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/017_Sequence Editor/widgets/editorView.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/017_Sequence Editor/widgets/editorView.py
@@ -1,10 +1,44 @@
-from PySide.QtCore import *
-from PySide.QtGui import *
-import editorScene
-
-
-class editorViewClass(QGraphicsView):
-    def __init__(self):
-        super(editorViewClass, self).__init__()
-        self.s = editorScene.editorSceneClass()
-        self.setScene(self.s)
+# ____ __.__ ______ _
+# ____ __.__ ______ _
+# ______ eS..
+#
+#
+# c_ editorViewClass QG..
+#     ___ -
+#         s__ ? ?. -
+#         sDM.. QGV__.RBD..
+#         sHSBP.. __.SBAO..
+#         sVSBP... __.SBAO..
+#
+#         s _ eS__.eSC..
+#         sS.. ?
+#
+#         pan _ F..
+#         lastX _ 0
+#         lasty _ 0
+#
+#     ___ mousePressEvent event
+#         __ ?.b.. __ __.MiB..
+#             pan _ T..
+#             lastX _ ?.x
+#             lastY _ ?.y
+#         ____
+#             s__ ? ?.mPE.. ?
+#
+#     ___ mouseMoveEvent event
+#         __ pan
+#             sI.. F..
+#             valX _ hSB__.v.. - (?.x - _X
+#             hSB__.sV.. _X
+#             lastX _ ?.x
+#
+#             valY _ vSB__.v.. - (?.y - _Y
+#             vSB__.sV.. ?
+#             lastY _ ?.y
+#             sI.. T..
+#         ____
+#             s__ ? ?.mME.. ?
+#
+#     ___ mouseReleaseEvent event
+#         s__ ? ?.mRE.. ?
+#         pan _ F..
