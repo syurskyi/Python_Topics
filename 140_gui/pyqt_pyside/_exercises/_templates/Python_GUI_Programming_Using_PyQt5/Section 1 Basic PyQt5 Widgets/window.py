fdad79afd5201063afeba2be3eda2eea0b621a5c
Grey <sergejyurskyj@yahoo.com>
2020-03-12 20:39:44 -0700

    Committed pyqt window template

--- 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 1 Basic PyQt5 Widgets/window.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 1 Basic PyQt5 Widgets/window.py
@@ -1,17 +1,17 @@
-import sys
-from PyQt5.QtWidgets import *
-
-
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setGeometry(150, 250, 550, 250)
-        self.setWindowTitle("This our Window's Title")
-
-        self.show()
-
-
-App = QApplication(sys.argv)
-window = Window()
-sys.exit(App.exec_())
-
+# ______ ___
+# ____ _5._W.. ______ _
+#
+#
+# c_ Window QW..
+#     ___ -
+#         s_. -
+#         sG..  150, 250, 550, 250
+#         sWT.. "This our Window's Title"
+#
+#         s..
+#
+#
+# App _ ?
+# window _ ?
+# ?.e.. ?.e._
+#
