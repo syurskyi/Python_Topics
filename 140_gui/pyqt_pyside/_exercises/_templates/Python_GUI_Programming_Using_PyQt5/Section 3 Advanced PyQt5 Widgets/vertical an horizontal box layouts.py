4cd5288476c7d6ecd714922c83076a7671f74a48
syurskyi <sergejyurskyj@yahoo.com>
2020-03-13 17:02:18 -0700

    Update vertical an horizontal box layouts.py

--- 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/vertical an horizontal box layouts.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/vertical an horizontal box layouts.py
@@ -1,45 +1,44 @@
-______ ___
-____ _5._W.. ______ _
-
-
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Vertical and horizontal Box Layouts")
-        self.setGeometry(350, 150, 400, 400)
-        self.ui()
-
-    def ui(self):
-        main_layout = QVBoxLayout()
-        top_layout = QHBoxLayout()
-        bottom_layout = QHBoxLayout()
-        main_layout.addLayout(top_layout)
-        main_layout.addLayout(bottom_layout)
-
-        cbox = QCheckBox()
-        rbtn = QRadioButton()
-        combo = QComboBox()
-        btn1 = QPushButton()
-        btn2 = QPushButton()
-        top_layout.setContentsMargins(150, 10, 20, 20)  # left,top,right,bottom
-        top_layout.addWidget(cbox)
-        top_layout.addWidget(rbtn)
-        top_layout.addWidget(combo)
-        bottom_layout.setContentsMargins(150,10,150,10)
-        bottom_layout.addWidget(btn1)
-        bottom_layout.addWidget(btn2)
-
-        self.setLayout(main_layout)
-
-        self.show()
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = Window()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
-
+# ______ ___
+# ____ _5._W.. ______ _
+#
+#
+# c_ Window QW..
+#     ___ -
+#         s__. -
+#         sWT.. *Vertical and horizontal Box Layouts
+#         sG.. 350, 150, 400, 400
+#         ?
+#
+#     ___ ui
+#         main_layout _ QVBL..
+#         top_layout _ QHBL..
+#         bottom_layout _ QHBL..
+#         m_l_.aL.. t_l..
+#         m_l_.aL.. b_l..
+#
+#         cbox _ QCB..
+#         rbtn _ QRB..
+#         combo _ QCB..
+#         btn1 _ QPB..
+#         btn2 _ QPB..
+#         t_l_.sCM.. 150, 10, 20, 20  # left,top,right,bottom
+#         t_l_.aW.. c..
+#         t_l_.aW.. r..
+#         t_l_.aW.. co..
+#         b_l_.sCM.. 150,10,150,10
+#         b_l_.aW.. _1
+#         b_l_.aW.. _2
+#
+#         sL.. m_l_
+#
+#         s..
+#
+#
+# ___ main
+#     App _ ?
+#     window _ ?
+#     ___.e.. ?.e..
+#
+# __ _____ __ ______
+#     ?
+#
