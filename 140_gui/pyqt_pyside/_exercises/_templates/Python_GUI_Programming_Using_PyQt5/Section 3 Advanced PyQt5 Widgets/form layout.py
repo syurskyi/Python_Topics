a8c653bd152b95ad679825eac579556370b6959a
syurskyi <sergejyurskyj@yahoo.com>
2020-03-13 17:00:38 -0700

    Update form layout.py

--- 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/form layout.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/form layout.py
@@ -1,46 +1,45 @@
-______ ___
-____ _5._W.. ______ _
-
-
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Form Layout")
-        self.setGeometry(350, 150, 400, 400)
-        self.ui()
-
-    def ui(self):
-        form_layout = QFormLayout()
-        # formLayout.setRowWrapPolicy(QFormLayout.WrapAllRows)
-        name_txt = QLabel("Name: ")
-        name_input = QLineEdit()
-        pass_txt = QLabel("Password :")
-        pass_input = QLineEdit()
-        hbox = QHBoxLayout()
-        hbox.addStretch()
-        hbox.addWidget(QPushButton("Enter"))
-        hbox.addWidget(QPushButton("Exit"))
-
-        hbox1 = QHBoxLayout()
-        hbox1.addWidget(QLineEdit())
-        hbox1.addWidget(QLineEdit())
-
-        form_layout.addRow(name_txt, hbox1)
-        form_layout.addRow(pass_txt, pass_input)
-        form_layout.addRow(QLabel("Country :"), QComboBox())
-        # formLayout.addRow(QPushButton("Enter"), QPushButton("Exit"))
-        form_layout.addRow(hbox)
-
-        self.setLayout(form_layout)
-
-        self.show()
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = Window()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+# ______ ___
+# ____ _5._W.. ______ _
+#
+#
+# c_ Window QW..
+#     ___ -
+#         s__. -
+#         sWT.. *Form Layout
+#         sG.. 350, 150, 400, 400
+#         ?
+#
+#     ___ ui
+#         form_layout  QFL
+#         # formLayout.setRowWrapPolicy(QFormLayout.WrapAllRows)
+#         name_txt  QL.. *Name:
+#         name_input  QLE..
+#         pass_txt  QL.. *Password :
+#         pass_input  QLE..
+#         hbox  QHBL..
+#         ?.aS..
+#         ?.aW.. QPB.. *Enter
+#         ?.aW.. QPB.. *Exit
+#
+#         hbox1  QHBL..
+#         ?.aW.. QLE..
+#         ?.aW.. QLE..
+#
+#         f_l__.aR.. n_t.. _1
+#         f_l__.aR.. p_t.. p_i..
+#         f_l__.aR..(QL..("Country :" QCB..
+#         # formLayout.aR..(QPushButton("Enter"), QPushButton("Exit"))
+#         f_l__.aR.. hb..
+#
+#         sL.. f_l..
+#
+#         s..
+#
+#
+# ___ main
+#     App _ ?
+#     window _ ?
+#     ___.e.. ?.e..
+#
+# __ _____ __ ______
+#     ?
