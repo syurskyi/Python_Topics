f8c3749bcfe12dca24d65506e8e61ac8da1a0dc7
syurskyi <sergejyurskyj@yahoo.com>
2020-03-13 17:01:33 -0700

    Update progressBar.py

--- 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/progressBar.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/progressBar.py
@@ -1,55 +1,54 @@
-______ ___
-____ _5._W.. ______ _
-____ _5._C.. ______ QT..
-
-count = 0
-
-
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("ProgressBar widget")
-        self.setGeometry(350, 150, 500, 500)
-        self.ui()
-
-    def ui(self):
-        vbox = QVBoxLayout()
-        hbox = QHBoxLayout()
-        self.progress_bar = QProgressBar()
-        btn_start = QPushButton("Start")
-        btn_start.clicked.connect(self.timer_start)
-        btn_stop = QPushButton("Stop")
-        btn_stop.clicked.connect(self.timer_stop)
-        self.timer = QTimer()
-        self.timer.setInterval(1000)
-        self.timer.timeout.connect(self.run_progress_bar)
-        vbox.addWidget(self.progress_bar)
-        vbox.addLayout(hbox)
-        hbox.addWidget(btn_start)
-        hbox.addWidget(btn_stop)
-        self.setLayout(vbox)
-        self.show()
-
-    def run_progress_bar(self):
-        global count
-        count += 1
-        print(count)
-        self.progress_bar.setValue(count)
-        if count == 100:
-            self.timer.stop()
-
-    def timer_start(self):
-        self.timer.start()
-
-    def timer_stop(self):
-        self.timer.stop()
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = Window()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+# ______ ___
+# ____ _5._W.. ______ _
+# ____ _5._C.. ______ QT..
+#
+# count _ 0
+#
+#
+# c_ Window QW..
+#     ___ -
+#         s__. -
+#         sWT.. *ProgressBar widget
+#         sG.. 50, 150, 500, 500
+#         ?
+#
+#     ___ ui
+#         vbox _ QVBL..
+#         hbox _ QHL..
+#         progress_bar _ QPB..
+#         btn_start _ QPB.. *Start
+#         ?.c__.c.. t___
+#         btn_stop _ QPB.. *Stop
+#         ?.c__.c.. t___
+#         timer _ QT..
+#         ?.sI.. 1000
+#         ?.ti___.c.. r__
+#         v__.aW.. p_b..
+#         v__.aL.. h..
+#         h__.aW.. b_s..
+#         h_.aW.. b_s..
+#         sL.. v..
+#         s..
+#
+#     ___ run_progress_bar
+#         gl.. count
+#         ? +_ 1
+#         print ?
+#         pr_b_.sV.. ?
+#         __ ? __ 100
+#             ti__.s..
+#
+#     ___ timer_start
+#         t__.?
+#
+#     ___ timer_stop
+#         t___.?
+#
+#
+# ___ main
+#     App _ ?
+#     window _ ?
+#     ___.e.. ?.e..
+#
+# __ _____ __ ______
+#     ?
