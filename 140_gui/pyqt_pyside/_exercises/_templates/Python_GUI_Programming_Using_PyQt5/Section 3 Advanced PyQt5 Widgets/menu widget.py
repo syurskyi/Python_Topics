e9035551aa053490cb855d0bbb5c3d234883242c
syurskyi <sergejyurskyj@yahoo.com>
2020-03-13 17:01:15 -0700

    Update menu widget.py

--- 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/menu widget.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/menu widget.py
@@ -1,72 +1,32 @@
-______ ___
-____ _5._W.. ______ _
-____ _5._G.. ______ QI..
-____ _5._C.. ______ Qt
-
-
-class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Menu Widget")
-        self.setGeometry(350, 150, 600, 600)
-        self.ui()
-
-    def ui(self):
-        # #########################Main Menu###############
-        menubar = self.menuBar()
-        file = menubar.addMenu("File")
-        edit = menubar.addMenu("Edit")
-        code = menubar.addMenu("Code")
-        helpMenu = menubar.addMenu("Help")
-
-        # ##########################Sub Menu Items################
-        new = QAction("New Project", self)
-        new.setShortcut("Ctrl+O")
-        file.addAction(new)
-        open = QAction("Open", self)
-        file.addAction(open)
-        exit = QAction("Exit", self)
-        exit.setIcon((QIcon("icons/exit.png")))
-        exit.triggered.connect(self.exit_func)
-        file.addAction(exit)
-        # ###############ToolBar###################
-        tb = self.addToolBar("My Toolbar")
-        tb.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
-        new_tb = QAction(QIcon('icons/folder.png'), "New", self)
-        tb.addAction(new_tb)
-        open_tb = QAction(QIcon('icons/empty.png'), "Open", self)
-        tb.addAction(open_tb)
-        save_tb = QAction(QIcon('icons/save.png'), "Save", self)
-        tb.addAction(save_tb)
-        exit_tb = QAction(QIcon('icons/exit.png'), "Exit", self)
-        exit_tb.triggered.connect(self.exit_func)
-        tb.addAction(exit_tb)
-        tb.actionTriggered.connect(self.btn_func)
-        self.combo = QComboBox()
-        self.combo.addItems(["Spiderman", "Superman", "Batman"])
-        tb.addWidget(self.combo)
-        self.show()
-
-    def exit_func(self):
-        mbox = QMessageBox.information(self, "Warning", "Are you sure to exit?", QMessageBox.Yes | QMessageBox.No,
-                                       QMessageBox.No)
-        if mbox == QMessageBox.Yes:
-            sys.exit()
-
-    def btn_func(self, btn):
-        if btn.text() == "New":
-            print("You clicked new button")
-        elif btn.text() =="Open":
-            print("You clicked open button")
-        else:
-            print("You clicked save button")
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = Window()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+# ______ ___
+# ____ _5._W.. ______ _
+#
+#
+# c_ Window QW..
+#     ___ __. -
+#         s__. -
+#         sWT.. *Horizontal Box Layout
+#         sG..350, 150, 400, 400)
+#         ?
+#
+#     ___ ui
+#         hbox  QHBL..
+#         button1  QPB.. *Button1 ?
+#         button2  QPB.. *Button2
+#         button3  QPB.. *Button3
+#         ?.aS..
+#         ?.aW.. _1
+#         ?.aW.. _2
+#         ?.aW.. _3
+#         ?.aS..
+#         sL.. ?
+#         s..
+#
+#
+# ___ main
+#     App _ ?
+#     window _ ?
+#     ___.e.. ?.e..
+#
+# __ _____ __ ______
+#     ?
