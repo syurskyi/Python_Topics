151b66c95e3bc983f0b52f33612f3c2209566079
syurskyi <sergejyurskyj@yahoo.com>
2020-03-13 17:01:24 -0700

    Update open file dialog.py

--- 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/open file dialog.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/open file dialog.py
@@ -1,60 +1,59 @@
-______ ___
-____ _5._W.. ______ _
-
-
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("File Dialogs")
-        self.setGeometry(350, 150, 400, 400)
-        self.ui()
-
-    def ui(self):
-        vbox = QVBoxLayout()
-        hbox = QHBoxLayout()
-        self.editor = QTextEdit()
-        file_button = QPushButton("Open File")
-        file_button.clicked.connect(self.open_file)
-        font_button = QPushButton("Change Font")
-        font_button.clicked.connect(self.change_font)
-        color_button = QPushButton("Change Color")
-        color_button.clicked.connect(self.change_color)
-        vbox.addWidget(self.editor)
-        vbox.addLayout(hbox)
-        hbox.addStretch()
-        hbox.addWidget(file_button)
-        hbox.addWidget(font_button)
-        hbox.addWidget(color_button)
-        hbox.addStretch()
-        self.setLayout(vbox)
-
-        self.show()
-
-    def open_file(self):
-        url = QFileDialog.getOpenFileName(self, "Open a file", "", "All Files(*);;*txt")
-        print(url)
-        file_url = url[0]
-        print(file_url)
-        file = open(file_url, 'r')
-        content = file.read()
-        self.editor.setText(content)
-
-    def change_font(self):
-        font, ok = QFontDialog.getFont()
-        if ok:
-            self.editor.setCurrentFont(font)
-
-    def change_color(self):
-        color = QColorDialog.getColor()
-        self.editor.setTextColor(color)
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = Window()
-    sys.exit(app.exec_())
-
-
-if __name__ =='__main__':
-    main()
-
+# ______ ___
+# ____ _5._W.. ______ _
+#
+#
+# c_ Window QW..
+#     ___ -
+#         s__. -
+#         sWT.. *File Dialogs
+#         sG.. 350, 150, 400, 400
+#         ?
+#
+#     ___ ui
+#         vbox _ QVBL..
+#         hbox _ QHBL..
+#         editor _ QTE..
+#         file_button _ QPB.. *Open File
+#         ?.c__.c.. o_f..
+#         font_button _ QPB.. *Change Font
+#         ?.c__.c.. c_f..
+#         color_button _ QPB.. Change Color
+#         ?.c__.c.. c_c..
+#         v__.aW.. e..
+#         v__.aL.. h..
+#         h__.aS..
+#         h__.aW.. f_b..
+#         h__.aW.. f_b..
+#         h__.aW.. c_b..
+#         h__.aS..
+#         sL.. v..
+#
+#         s..
+#
+#     ___ open_file
+#         url _ QFD__.gOFN.. ? "Open a file", "", "All Files(*);;*txt")
+#         print ?
+#         file_url _ ? 0
+#         print ?
+#         file _ o.. ? _
+#         content _ ?.r..
+#         e__.sT... ?
+#
+#     ___ change_font
+#         font, ok _ QFD__.gF..
+#         __ o.
+#             e__.sCF.. f..
+#
+#     ___ change_color
+#         color _ QCD__.gC..
+#         e__.tTC.. ?
+#
+#
+# ___ main
+#     App _ ?
+#     window _ ?
+#     ___.e.. ?.e..
+#
+# __ _____ __ ______
+#     ?
+#
