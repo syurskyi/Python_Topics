5e1f60ce93e4914ca79b7569811ec7798bfce971
syurskyi <sergejyurskyj@yahoo.com>
2020-03-13 17:00:47 -0700

    Update grid layout.py

--- 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/grid layout.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Python_GUI_Programming_Using_PyQt5/Section 3 Advanced PyQt5 Widgets/grid layout.py
@@ -1,49 +1,48 @@
-______ ___
-____ _5._W.. ______ _
-
-
-class Window(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Grid Layout")
-        self.setGeometry(350, 150, 600, 600)
-        self.ui()
-
-    def ui(self):
-        self.grid_layout = QGridLayout()
-        # btn1=QPushButton("Button1")
-        # btn2=QPushButton("Button2")
-        # btn3=QPushButton("Button3")
-        # btn4=QPushButton("Button4")
-        # self.gridLayout.addWidget(btn1,0,0)
-        # self.gridLayout.addWidget(btn2,0,1)
-        # self.gridLayout.addWidget(btn3,1,0)
-        # self.gridLayout.addWidget(btn4,1,1)
-        for i in range(0, 3):
-            for j in range(0, 3):
-                btn = QPushButton("Button{}{}".format(i, j))
-                self.grid_layout.addWidget(btn, i, j)
-                btn.clicked.connect(self.click_me)
-
-        self.setLayout(self.grid_layout)
-        self.show()
-
-    def click_me(self):
-        button_id = self.sender().text()
-        if button_id == "Button00":
-            print("Button1 was clicked")
-        elif button_id == "Button01":
-            print("Button2 was clicked")
-        elif button_id == "Button02":
-            print("Button3 was clicked")
-
-
-def main():
-    app = QApplication(sys.argv)
-    window = Window()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
-
+# ______ ___
+# ____ _5._W.. ______ _
+#
+#
+# c_ Window QW..
+#     ___ -
+#         s__. -
+#         sWT.. *Grid Layout
+#         sG.. 350, 150, 600, 600
+#         ?
+#
+#     ___ ui
+#         grid_layout  QGL..
+#         # btn1QPushButton("Button1")
+#         # btn2QPushButton("Button2")
+#         # btn3QPushButton("Button3")
+#         # btn4QPushButton("Button4")
+#         # gridLayout.addWidget(btn1,0,0)
+#         # gridLayout.addWidget(btn2,0,1)
+#         # gridLayout.addWidget(btn3,1,0)
+#         # gridLayout.addWidget(btn4,1,1)
+#         ___ i __ ra.. 0 3
+#             ___ j __ ra.. 0 3
+#                 btn  QPB.. *Button@ @".f.. ? ?
+#                 g_l__.aW.. b.. ? ?
+#                 b__.c___.c.. ?
+#
+#         sL.. g_l..
+#         s..
+#
+#     ___ click_me
+#         button_id  s___.t..
+#         __ ? __ *Button00
+#             print *Button1 was clicked
+#         ____ ? __ *Button01
+#             print("Button2 was clicked")
+#         ____ ? __ *Button02
+#             print("Button3 was clicked")
+#
+#
+# ___ main
+#     App _ ?
+#     window _ ?
+#     ___.e.. ?.e..
+#
+# __ _____ __ ______
+#     ?
+#
