8c0f2137ae44e224fab4d05a5483d18f21630a5a
Grey <sergejyurskyj@yahoo.com>
2020-03-14 18:13:04 -0700

    Committed design pattern builder exercise

--- 120_design_patterns/002_builder/_exercises/exercises/000a_builder.py
+++ 120_design_patterns/002_builder/_exercises/exercises/000a_builder.py
@@ -1,107 +1,107 @@
-# c_ Director
-#     __builder _ N..
-#
-#     ___ setBuilder  builder
-#         __?  ?
-#
-#     ___ getCar
-#         car = C..
-#
-#         # First goes the body
-#         body _ __b__.g..
-#         c__.s.. ?
-#
-#         # Then engine
-#         engine _ __b__.g..
-#         c__.s.. ?
-#
-#         # And four wheels
-#         i = 0
-#         w____ ? < 4
-#             wheel = __b__.gW..
-#             car.aW.. ?
-#             ? +_ 1
-#
-#         r_ c..
-#
-#
-# # The whole product
-# c_ Car
-#     ___ -
-#         __wheels = li..
-#         __engine = N..
-#         __body = N..
-#
-#     ___ setBody  body
-#         __?  ?
-#
-#     ___ attachWheel  wheel
-#         __w__.ap.. ?
-#
-#     ___ setEngine engine
-#         __?  ?
-#
-#     ___ specification
-#         print("body: @"  __b__.s..      # string
-#         print("engine horsepower: @"  __e__.h..   # digit
-#         print("tire size: @\'"  __w.. 0 .s..    # digit
-#
-#
-# c_ Builder
-#     ___ getWheel
-#         p..
-#
-#     ___ getEngine
-#         p..
-#
-#     ___ getBody
-#         p..
-#
-#
-# c_ JeepBuilder B..
-#
-#     ___ getWheel
-#         wheel = W..
-#         ?.s.. _ 22
-#         r_ ?
-#
-#     ___ getEngine
-#         engine = E..
-#         ?.h.. _ 400
-#         r_ ?
-#
-#     ___ getBody
-#         body = B..
-#         ?.s.. = "SUV"
-#         r_ body
-#
-#
-# # Car parts
-# c_ Wheel
-#     size _ N..
-#
-#
-# c_ Engine
-#     horsepower _ N..
-#
-#
-# c_ Body
-#     shape = N..
-#
-#
-# ___ main
-#     jeepBuilder = J..  # initializing the class
-#
-#     director = D..
-#
-#     # Build Jeep
-#     print("Jeep")
-#
-#     ?.sB... j..
-#     jeep = ?.gC..
-#     ?.s..
-#     print("")
-#
-#
-# __ _______ __ ______
-#     ?
+class Director:
+    __builder = None
+
+    def setBuilder(self, builder):
+        self.__builder = builder
+
+    def getCar(self):
+        car = Car()
+
+        # First goes the body
+        body = self.__builder.getBody()
+        car.setBody(body)
+
+        # Then engine
+        engine = self.__builder.getEngine()
+        car.setEngine(engine)
+
+        # And four wheels
+        i = 0
+        while i < 4:
+            wheel = self.__builder.getWheel()
+            car.attachWheel(wheel)
+            i += 1
+
+        return car
+
+
+# The whole product
+class Car:
+    def __init__(self):
+        self.__wheels = list()
+        self.__engine = None
+        self.__body = None
+
+    def setBody(self, body):
+        self.__body = body
+
+    def attachWheel(self, wheel):
+        self.__wheels.append(wheel)
+
+    def setEngine(self, engine):
+        self.__engine = engine
+
+    def specification(self):
+        print("body: %s" % self.__body.shape)      # string
+        print("engine horsepower: %d" % self.__engine.housepower)   # digit
+        print("tire size: %d\'" % self.__wheels[0].size)    # digit
+
+
+class Builder:
+    def getWheel(self):
+        pass
+
+    def getEngine(self):
+        pass
+
+    def getBody(self):
+        pass
+
+
+class JeepBuilder(Builder):
+
+    def getWheel(self):
+        wheel = Wheel()
+        wheel.size = 22
+        return wheel
+
+    def getEngine(self):
+        engine = Engine()
+        engine.housepower = 400
+        return engine
+
+    def getBody(self):
+        body = Body()
+        body.shape = "SUV"
+        return body
+
+
+# Car parts
+class Wheel:
+    size = None
+
+
+class Engine:
+    horsepower = None
+
+
+class Body:
+    shape = None
+
+
+def main():
+    jeepBuilder = JeepBuilder  # initializing the class
+
+    director = Director
+
+    # Build Jeep
+    print("Jeep")
+
+    director.setBuilder(jeepBuilder)
+    jeep = director.getCar()
+    jeep.specification()
+    print("")
+
+
+if __name__ == "__main__":
+    main()
