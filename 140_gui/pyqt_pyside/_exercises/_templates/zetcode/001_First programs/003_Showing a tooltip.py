47eaf2b4b7559a260197f714748c9603e11b30d1
Grey <sergejyurskyj@yahoo.com>
2020-03-16 21:09:05 -0700

    Committed PySide2 templates

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/001_First programs/003_Showing a tooltip.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/001_First programs/003_Showing a tooltip.py
@@ -1,40 +1,40 @@
-import sys
-from PySide2.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication)
-from PySide2.QtGui import QFont
-
-
-class Example(QWidget):
-    def __init__(self):
-        super(Example, self).__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        QToolTip.setFont(QFont('SansSerif', 10))
-
-        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        btn = QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushButton</b> widget')
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)
-
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Tooltips')
-        self.show()
-
-
-if __name__ == '__main__':
-    import sys
-
-    app = None
-    try:
-        import nuke
-    except ImportError:
-        app = QApplication(sys.argv)
-    main = Example()
-    main.show()
-
-    if app is not None:
-        app.exec_()
+# ______ ___
+# ____ _2._W.. ______ (QW.. QTT..
+#     QPB.. QAp..
+# ____ _2._G.. ______ QF..
+#
+#
+# c_ Example QW..
+#     ___ -
+#            s__. -
+#
+#         ?
+#
+#     ___ initUI
+#         QTT__.sF.. QF.. *SansSerif 10
+#
+#         sTT.. *This is a <b>QWidget</b> widget
+#
+#         btn _ QPB..  *Button ?
+#         ?.sTT.. *This is a <b>QPushButton</b> widget
+#         ?.r.. ?.sH..
+#         ?.m.. 50, 50
+#
+#         sG.. 300, 300, 250, 150
+#         sWT.. *Tooltips
+#         ?
+#
+#
+# __ ______ __ ______
+#     ______ ___
+#
+#     app _ N..
+#     ___
+#         ______ n..
+#     ______ I..
+#         ? _ ?
+#     main _ ?
+#     ?*.s..
+#
+#     __ ? __ no. N..
+#         ?.e.._
