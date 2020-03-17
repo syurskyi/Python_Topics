5d514a034f301e5a4b083bb4cbf85c0b182d8b6a
Grey <sergejyurskyj@yahoo.com>
2020-03-16 21:47:41 -0700

    Committed PySide2 templates

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/002_Menus and toolbars/007_mainwindow.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/002_Menus and toolbars/007_mainwindow.py
@@ -1,39 +1,48 @@
-import sys
-from PyQt5.QtWidgets import QMainWindow, QTextEdit, QAction, QApplication
-from PyQt5.QtGui import QIcon
-
-
-class Example(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        textEdit = QTextEdit()
-        self.setCentralWidget(textEdit)
-
-        exitAct = QAction(QIcon('exit24.png'), 'Exit', self)
-        exitAct.setShortcut('Ctrl+Q')
-        exitAct.setStatusTip('Exit application')
-        exitAct.triggered.connect(self.close)
-
-        self.statusBar()
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
-        fileMenu.addAction(exitAct)
-
-        toolbar = self.addToolBar('Exit')
-        toolbar.addAction(exitAct)
-
-        self.setGeometry(300, 300, 350, 250)
-        self.setWindowTitle('Main window')
-        self.show()
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+# ______ ___
+# ____ _2._W.. ______ QMW.. QTE.. QA.. QA..
+# ____ _2._G.. ______  QI..
+#
+#
+# c_ Example QM..
+#
+#     ___ -
+#         s__. -
+#
+#         ?
+#
+#     ___ initUI
+#         textEdit _ QTE..
+#         sCW.. ?
+#
+#         exitAct _ QA.. QI.. *exit24.png *Exit ?
+#         ?.sS.. *Ctrl+Q
+#         ?.sST.. *Exit application
+#         ?.t___.c.. cl..
+#
+#         sB..
+#
+#         menubar _ mB..
+#         fileMenu _ ?.aM.. *&File
+#         ?.aA.. eA..
+#
+#         toolbar _ aTB.. *Exi
+#         ?.aA.. eA..
+#
+#         sG.. 300, 300, 350, 250
+#         sWT.. *Main window
+#         ?
+#
+#
+# __ ______ __ ______
+#     ______ ___
+#
+#     app _ N..
+#     ___
+#         ______ n..
+#     ______ I..
+#         ? _ ?
+#     main _ ?
+#     ?*.s..
+#
+#     __ ? __ no. N..
+#         ?.e.._
