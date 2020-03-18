403051fd504d9f0c4b5b97e9401b2533e2adc9af
syurskyi <sergejyurskyj@yahoo.com>
2020-03-18 12:55:44 -0700

    Update Section_5_stack.py

--- 125_algorithms/003_stacks_and_queues/_exercises/templates/5 Data Structures - Stacks & Queues/Section_5_stack.py
+++ 125_algorithms/003_stacks_and_queues/_exercises/templates/5 Data Structures - Stacks & Queues/Section_5_stack.py
@@ -1,33 +1,33 @@
-class Stack:
-
-    def __init__(self):
-        self.stack = []
-
-    def isEmpty(self):
-        return self.stack == []
-
-    def push(self, data):
-        self.stack.append(data)
-
-    def pop(self):
-        data = self.stack[-1]
-        del self.stack[-1]
-        return data
-
-    def peek(self):
-        return self.stack[-1]
-
-    def sizeStack(self):
-        return len(self.stack)
-
-
-stack = Stack()
-stack.push(1)
-stack.push(2)
-stack.push(3)
-print(stack.sizeStack())
-print("Popped: ", stack.pop())
-print("Popped: ", stack.pop())
-print(stack.sizeStack())
-print("Peek:", stack.peek())
-print(stack.sizeStack())
+# c_ Stack
+#
+#     ___ -
+#         stack _    # list
+#
+#     ___ isEmpty
+#         r_ ? __    # list
+#
+#     ___ push data
+#         ?.ap.. ?
+#
+#     ___ pop
+#         data _ ? -1
+#         de. ? -1
+#         r_ ?
+#
+#     ___ peek
+#         r_ ? -1
+#
+#     ___ sizeStack
+#         r_ le. ?
+#
+#
+# stack _ ?
+# ?.p.. 1
+# ?.p.. 2
+# ?.p.. 3
+# print(?.sS..
+# print("Popped: ", ?.p..
+# print("Popped: ", ?.p..
+# print(?.sS..
+# print("Peek:", ?.pe..
+# print(?.sS..
