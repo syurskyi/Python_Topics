c87e784433ec044a7b7b3e4b523e55c72f43cfe7
syurskyi <sergejyurskyj@yahoo.com>
2020-03-18 19:17:41 -0700

    Update Balanced Parentheses Check .py

--- 125_algorithms/003_stacks_and_queues/_exercises/templates/13 Stacks Queues and Deques/Stacks, Queues, and Deques Interview Problems/PRACTICE/Balanced Parentheses Check .py
+++ 125_algorithms/003_stacks_and_queues/_exercises/templates/13 Stacks Queues and Deques/Stacks, Queues, and Deques Interview Problems/PRACTICE/Balanced Parentheses Check .py
@@ -1,54 +1,54 @@
-# Balanced Parentheses Check
-# Problem Statement
+# # Balanced Parentheses Check
+# # Problem Statement
+# #
+# # Given a string of opening and closing parentheses, check whether its balanced. We have 3 types of parentheses:
+# # round brackets: (), square brackets: [], and curly brackets: {}. Assume that the string doesnt contain any other
+# # character than these, no spaces words or numbers. As a reminder, balanced parentheses require every opening
+# # parenthesis to be closed in the reverse order opened. For example ([]) is balanced but ([)] is not.
+# #
+# # You can assume the input string has no spaces.
+# # Solution
+# #
+# # Fill out your solution below:
 #
-# Given a string of opening and closing parentheses, check whether its balanced. We have 3 types of parentheses:
-# round brackets: (), square brackets: [], and curly brackets: {}. Assume that the string doesnt contain any other
-# character than these, no spaces words or numbers. As a reminder, balanced parentheses require every opening
-# parenthesis to be closed in the reverse order opened. For example ([]) is balanced but ([)] is not.
+# ____ balance_check s
+#     chars      # list
+#     matches  {')':'(',']':'[','}':'{'}
+#     ___ c __ s
+#         __ c __ ?
+#             __ ?.p.. ! ?|?
+#                 r_ F..
+#         ____
+#             ?.ap.. ?
+#     r_ ? __      # list
+#     p..
 #
-# You can assume the input string has no spaces.
-# Solution
+# ?('[]')
+# # True
 #
-# Fill out your solution below:
-
-def balance_check(s):
-    chars = []
-    matches = {')':'(',']':'[','}':'{'}
-    for c in s:
-        if c in matches:
-            if chars.pop() != matches[c]:
-                return False
-        else:
-            chars.append(c)
-    return chars == []
-    pass
-
-balance_check('[]')
-# True
-
-balance_check('[](){([[[]]])}')
-# True
-
-balance_check('()(){]}')
-# False
-
-# Test Your Solution
-
-"""
-RUN THIS CELL TO TEST YOUR SOLUTION
-"""
-from nose.tools import assert_equal
-
-class TestBalanceCheck(object):
-
-    def test(self, sol):
-        assert_equal(sol('[](){([[[]]])}('), False)
-        assert_equal(sol('[{{{(())}}}]((()))'), True)
-        assert_equal(sol('[[[]])]'), False)
-        print ('ALL TEST CASES PASSED')
-
-
-# Run Tests
-
-t = TestBalanceCheck()
-t.test(balance_check)
+# ?('[](){([[[]]])}')
+# # True
+#
+# ?('()(){]}')
+# # False
+#
+# # Test Your Solution
+#
+# """
+# RUN THIS CELL TO TEST YOUR SOLUTION
+# """
+# from nose.tools import assert_equal
+#
+# class TestBalanceCheck(object):
+#
+#     def test(self, sol):
+#         assert_equal(sol('[](){([[[]]])}('), False)
+#         assert_equal(sol('[{{{(())}}}]((()))'), True)
+#         assert_equal(sol('[[[]])]'), False)
+#         print ('ALL TEST CASES PASSED')
+#
+#
+# # Run Tests
+#
+# t  TestBalanceCheck()
+# t.test(balance_check)
