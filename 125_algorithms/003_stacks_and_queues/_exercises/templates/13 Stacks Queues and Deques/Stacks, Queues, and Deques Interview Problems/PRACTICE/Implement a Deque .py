7eb329785a6390173282f9f92c3f210abeeea2c8
syurskyi <sergejyurskyj@yahoo.com>
2020-03-18 19:17:55 -0700

    Update Implement a Deque .py

--- 125_algorithms/003_stacks_and_queues/_exercises/templates/13 Stacks Queues and Deques/Stacks, Queues, and Deques Interview Problems/PRACTICE/Implement a Deque .py
+++ 125_algorithms/003_stacks_and_queues/_exercises/templates/13 Stacks Queues and Deques/Stacks, Queues, and Deques Interview Problems/PRACTICE/Implement a Deque .py
@@ -1,56 +1,56 @@
-# %%
-'''
-# Implement a Deque
-Finally, implement a Deque class! It should be able to do the following:
-* Check if its empty
-* Add to both front and rear
-* Remove from Front and Rear
-* Check the Size
-'''
-
-# %%
-class Deque(object):
-    def __init__(self):
-        self.items = []
-    def is_empty(self):
-        return self.items == []
-    def add_front(self, e):
-        self.items.insert(0, e)
-    def add_rear(self, e):
-        self.items.append(e)
-    def remove_front(self):
-        return self.items.pop(0)
-    def remove_rear(self):
-        return self.items.pop()
-    def size(self):
-        return len(self.items)
-    pass
-
-# %%
-deq = Deque()
-
-# %%
-deq.size()
-
-# %%
-deq.add_front(2)
-
-# %%
-deq.add_front(1)
-
-# %%
-deq.add_rear(3)
-
-# %%
-deq.remove_front()
-
-# %%
-deq.remove_rear()
-
-# %%
-deq.remove_rear()
-
-# %%
-deq.is_empty()
-
-# %%
+# # %%
+# '''
+# # Implement a Deque
+# Finally, implement a Deque class! It should be able to do the following:
+# * Check if its empty
+# * Add to both front and rear
+# * Remove from Front and Rear
+# * Check the Size
+# '''
+#
+# # %%
+# c_ Deque o..
+#     ___ -
+#         items _     # list
+#     ___ is_empty
+#         r_ items __    # list
+#     ___ add_front  e
+#         ?.i.. 0 ?
+#     ___ add_rear  e)
+#         ?.ap..?
+#     ___ remove_front
+#         r_ ?.p.. 0
+#     ___ remove_rear
+#         r_ ?.p..
+#     ___ size
+#         r_ le. ?
+#     p..
+#
+# # %%
+# deq = ?
+#
+# # %%
+# ?.s..
+#
+# # %%
+# ?.a_f.. 2
+#
+# # %%
+# ?.a_f.. 1
+#
+# # %%
+# ?.a_r.. 3
+#
+# # %%
+# ?.r_f..
+#
+# # %%
+# ?.r_r..
+#
+# # %%
+# ?.r_r..
+#
+# # %%
+# ?.i_e..
+#
+# # %%
