af7781a95039149bb5f36e95e8948f9c16d684a0
Grey <sergejyurskyj@yahoo.com>
2020-03-19 20:49:01 -0700

    Committed 3_matrix_expo template

--- 125_algorithms/003_Recursion/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 3 General Recursive Algorithms/2_expressions.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 3 General Recursive Algorithms/2_expressions.py
@@ -1,33 +1,33 @@
-def expressions(expr):
-    if expr[0] == '(':
-        expr[:] = expr[1:]
-        result = terms(expr)
-        expr[:] = expr[1:]
-        return result
-    result = expr[0]
-    expr[:] = expr[1:]
-    return float(result)
-
-def factors(expr):
-    result = expressions(expr)
-    while expr[0] in '*/':
-        if expr[0] == '*':
-            expr[:] = expr[1:]
-            result *= expressions(expr)
-        else:
-            expr[:] = expr[1:]
-            result /= expressions(expr)
-    return result
-
-def terms(expr):
-    result = factors(expr)
-    while expr[0] in '+-':
-        if expr[0] == '+':
-            expr[:] = expr[1:]
-            result += factors(expr)
-        else:
-            expr[:] = expr[1:]
-            result -= factors(expr)
-    return result
-
-print(terms(list('4+2*(3-1).')))
+# ___ expressions expr
+#     __ ? 0 __ '('
+#         ? : _ ? 1:
+#         result _ terms ?
+#         ? : _ ? 1:
+#         r_ ?
+#     result _ ? 0
+#     ? : _ ? 1:
+#     r_ fl. ?
+#
+# ___ factors expr
+#     result _ ? ?
+#     w___ ? 0 __ '*/'
+#         __ ? 0 __ '*'
+#             ? : _ ? 1:
+#             r.. *_ e.. ?
+#         ____
+#             ? : _ ? 1:
+#             r.. /_ e.. ?
+#     r_ ?
+#
+# ___ terms expr
+#     result _ ? ?
+#     w___ ? 0 __ '+-'
+#         __ ? 0 __ '+'
+#             ? : _ ? 1:
+#             r... +_ f.. ?
+#         ____
+#             ? : _ ? 1:
+#             r.. -_ f.. ?
+#     r_ ?
+#
+# print(terms(list('4+2*(3-1).')))
