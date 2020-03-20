15bfdc92b819d7c044164159d2c9b61a3e786927
Grey <sergejyurskyj@yahoo.com>
2020-03-19 20:52:38 -0700

    Committed 4_powers_sum template

--- 125_algorithms/003_Recursion/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 3 General Recursive Algorithms/4_powers_sum.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 3 General Recursive Algorithms/4_powers_sum.py
@@ -1,38 +1,38 @@
-import numpy as np
-
-def naive(A, n):
-    result = np.zeros_like(A)
-    power = A
-    for i in range(1, n+1):
-        result += power
-        power = np.matmul(power, A) % 666013
-        result %= 666013
-    return result
-
-def optimal(A, n):
-    # n = 2k    => S = (I+A^k)(A+...+A^k)
-    # n = 2k+1  => S = (I+A^k)(A+...+A^k)+A^(2k+1)
-    # n = 1 =>  => S = A
-    eye = np.eye(A.shape[0])
-    def inner(A, n):
-        if n == 1:
-            return A, A
-
-        A_pow_n_div2, S_n_div2 = inner(A, n // 2)
-        A_pow_n = np.matmul(A_pow_n_div2, A_pow_n_div2) % 666013
-        S_n = np.matmul(eye + A_pow_n_div2, S_n_div2)
-        if n % 2 == 0:
-            return A_pow_n % 666013, S_n % 666013
-        A_pow_n = np.matmul(A_pow_n, A) % 666013
-        return A_pow_n % 666013, (S_n + A_pow_n) % 666013
-    return inner(A, n)[1]
-
-
-
-n = 1000
-arr = np.array([[1,2],[3,4]], dtype=np.int64)
-print(naive(arr, n))
-print(optimal(arr, n))
-
-for i in range(1, 1000):
-    assert np.array_equal(naive(arr, i), optimal(arr, i))
+# ______ numpy __ np
+#
+# ___ naive A, n
+#     result _ ?.zeros_like ?
+#     power _ A
+#     ___ i __ ra.. 1 ?+1
+#         r.. +_ p..
+#         p.. _ ?.matmul p.. ? % 666013
+#         r.. %_ 666013
+#     r_ ?
+#
+# ___ optimal A n
+#     # n _ 2k    _> S _ (I+A^k)(A+...+A^k)
+#     # n _ 2k+1  _> S _ (I+A^k)(A+...+A^k)+A^(2k+1)
+#     # n _ 1 _>  _> S _ A
+#     eye _ ?.e.. ?.shape 0
+#     ___ inner A n
+#         __ n __ 1
+#             r_ A, A
+#
+#         A_pow_n_div2, S_n_div2 _ inner(A, n // 2)
+#         A_pow_n _ ?.matmul(A_pow_n_div2, A_pow_n_div2) % 666013
+#         S_n _ ?.matmul(eye + A_pow_n_div2, S_n_div2)
+#         if n % 2 == 0:
+#             r_ A_pow_n % 666013, S_n % 666013
+#         A_pow_n _ ?.matmul(A_pow_n, A) % 666013
+#         r_ A_pow_n % 666013, (S_n + A_pow_n) % 666013
+#     r_ inner(A, n)[1]
+#
+#
+#
+# n _ 1000
+# arr _ ?.array([[1,2],[3,4]], dtype_?.int64)
+# print(naive(arr, n))
+# print(optimal(arr, n))
+#
+# ___ i __ ra..(1, 1000):
+#     assert ?.array_equal(naive(arr, i), optimal(arr, i))
