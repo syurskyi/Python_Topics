a2fbd0e6d9b3821be2b08e3af90816669a1e0245
Grey <sergejyurskyj@yahoo.com>
2020-03-22 11:50:11 -0700

    Committed count_derangements_rec template

--- 125_algorithms/003_Recursion/_exercises/templates/Section 3 Counting Derangements/count_derangements_rec.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Section 3 Counting Derangements/count_derangements_rec.py
@@ -1,19 +1,19 @@
-class CountDerangementsRec:
-    def __init__(self, set_size):
-        self.set_size = set_size
-
-    def count_derangements(self):
-        return self.count_derangements_rec(self.set_size)
-
-    def count_derangements_rec(self, n):
-        if n == 1:
-            return 0
-        if n == 2:
-            return 1
-        return (n - 1) * (self.count_derangements_rec(n - 1) + self.count_derangements_rec(n - 2))
-
-
-for i in range(1, 64):
-    n = CountDerangementsRec(i).count_derangements()
-    print("Derangements in set size {} -> {}".format(i, n))
-
+# c_ CountDerangementsRec
+#     ___ - set_size
+#         ?  ?
+#
+#     ___ count_derangements
+#         r_ _r.. ?
+#
+#     ___ count_derangements_rec n
+#         __ ? __ 1
+#             r_ 0
+#         __ ? __ 2
+#             r_ 1
+#         r_ ? - 1) * ?|? - 1 + ?|? - 2
+#
+#
+# ___ i in ra.. 1 64
+#     n = ?|?.c..
+#     print("Derangements in set size @ -> @".f.. ?  ?
+#
