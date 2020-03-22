0f3d5cdf89b64a1fa2a3624939dd25f33ad2f069
Grey <sergejyurskyj@yahoo.com>
2020-03-22 10:15:05 -0700

    Committed 61. Recursion mini-project 2 - Factorial template

--- 125_algorithms/003_Recursion/_exercises/templates/Python 3 Project-based Python, Algorithms, Data Structures/61. Recursion mini-project 2 - Factorial.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Python 3 Project-based Python, Algorithms, Data Structures/61. Recursion mini-project 2 - Factorial.py
@@ -1,12 +1,12 @@
-def factorial_recur(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial_recur(n-1)
+___ factorial_recur( 
+    __ n __ 0
+        r_ 1
+    ____
+        r_ ? * ? ?-1
 
 z = 0 # expecting 1
-print(f"The value of {z}! is {factorial_recur(z)}")
+print _*The value of ?! is |?|?
 z = 1 # expecting 1
-print(f"The value of {z}! is {factorial_recur(z)}")
+print _*The value of ?! is |?|?
 z = 5 # expecting 120
-print(f"The value of {z}! is {factorial_recur(z)}")
+print _*The value of ?! is |?|?
