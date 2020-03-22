119345c04576e3ada627861085a513feaac75225
Grey <sergejyurskyj@yahoo.com>
2020-03-21 22:46:57 -0700

    Committed 04-Check-subset templates

--- 125_algorithms/003_stacks_and_queues/_exercises/templates/Section 2 Coding Interview Questions 1 - 10/04-Check-subset.py
+++ 125_algorithms/003_stacks_and_queues/_exercises/templates/Section 2 Coding Interview Questions 1 - 10/04-Check-subset.py
@@ -1,10 +1,10 @@
-# [1, 2, 4, 7, "a"] [" b", "a", 1, 2, 7, 3, 4] = true
-
-def is_subset(L1, L2):
-    temp = dict([(e, 1) for e in L2])
-    for e in L1: #n 
-        if not e in temp: # n n  
-            return False
-    return True
-
-print(is_subset([1, 2, 4, 7, "c"], [" b", "a", 1, 2, 7, 3, 4]))
+# # [1, 2, 4, 7, "a"] [" b", "a", 1, 2, 7, 3, 4] = true
+#
+# ___ is_subset L1 L2
+#     temp _ di.. |||e 1| ___ ? __ _2
+#     ___ ? __ _1 #n
+#         __ no. ? __ t.. # n n
+#             r_ F..
+#     r_ T..
+#
+# print(?([1, 2, 4, 7, "c"], [" b", "a", 1, 2, 7, 3, 4]))
