6ad5ac9362da530f25eb89971a08abab9353f5aa
Grey <sergejyurskyj@yahoo.com>
2020-03-22 12:07:22 -0700

    Committed aircraft_spacing_bottom_up template

--- 125_algorithms/003_Recursion/_exercises/templates/Section 4 Aircraft Spacing/aircraft_spacing_bottom_up.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Section 4 Aircraft Spacing/aircraft_spacing_bottom_up.py
@@ -1,18 +1,18 @@
-class AircraftSpacingBottomUp:
-    def __init__(self, passengers):
-        self.passengers = passengers
-        self.sub_solutions = [-1] * len(passengers)
-        for i in range(len(passengers) - 1, -1, -1):
-            choosing_first = self.passengers[i] + (self.sub_solutions[i + 2] if i + 2 < len(self.sub_solutions) else 0)
-            not_choosing_first = self.sub_solutions[i + 1] if i + 1 < len(self.sub_solutions) else 0
-            self.sub_solutions[i] = max(choosing_first, not_choosing_first)
-
-    def max_passengers(self):
-        return self.sub_solutions[0]
-
-
-# spacing = AircraftSpacingBottomUp([155, 55, 2, 96, 67, 203, 3, 66, 32, 65, 29, 8, 299, 323, 77, 3, 28,
-#                              128, 19, 523, 372, 2, 3, 66, 124, 38, 34, 12,88, 23 ,74,65, 87, 434,
-#                              14, 7, 49, 38, 27, 641, 61, 58, 14, 57, 71, 11, 82, 178, 93, 191, 4])
-spacing = AircraftSpacingBottomUp([155, 55, 2, 96, 67, 203, 3])
-print(spacing.max_passengers())
+# c_ AircraftSpacingBottomUp
+#     ___ - passengers
+#         ?  ?
+#         sub_solutions _ |-1 * le. ?
+#         ___ i __ ra.. le. ? - 1, -1, -1
+#             choosing_first _ ?|? + |?|? + 2 __ ? + 2 < le. |? ____ 0
+#             not_choosing_first _ ?|? + 1 __ ? + 1 < le. |? ____ 0
+#             s_s..|? _ ma. c.. n..
+#
+#     ___ max_passengers
+#         rer_turn ? 0
+#
+#
+# # spacing _ AircraftSpacingBottomUp([155, 55, 2, 96, 67, 203, 3, 66, 32, 65, 29, 8, 299, 323, 77, 3, 28,
+# #                              128, 19, 523, 372, 2, 3, 66, 124, 38, 34, 12,88, 23 ,74,65, 87, 434,
+# #                              14, 7, 49, 38, 27, 641, 61, 58, 14, 57, 71, 11, 82, 178, 93, 191, 4])
+# spacing _ ? 155, 55, 2, 96, 67, 203, 3
+# print ?.m..
