aeb087399bc654018bc0110019d9500d461e1ba4
Grey <sergejyurskyj@yahoo.com>
2020-03-22 12:02:44 -0700

    Committed count_derangements_top_down template

--- 125_algorithms/003_Recursion/_exercises/templates/Section 3 Counting Derangements/count_derangements_top_down.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Section 3 Counting Derangements/count_derangements_top_down.py
@@ -1,24 +1,25 @@
-class CountDerangementsTopDown:
-    def __init__(self, set_size):
-        self.set_size = set_size
-        self.sub_solutions = [-1] * (set_size + 1)
-
-    def count_derangements(self):
-        return self.count_derangements_rec(self.set_size)
-
-    def count_derangements_rec(self, n):
-        if self.sub_solutions[n] != -1:
-            return self.sub_solutions[n]
-        if n == 1:
-            return 0
-        if n == 2:
-            return 1
-        result = (n - 1) * (self.count_derangements_rec(n - 1) + self.count_derangements_rec(n - 2))
-        self.sub_solutions[n] = result
-        return result
-
-
-# for i in range(1,64):
-for i in range(1,11):
-    n = CountDerangementsTopDown(i).count_derangements()
-    print("Derangments in set size {} -> {}".format(i,n))
+# c_ CountDerangementsTopDown
+#     ___ - set_size
+#         ?  ?
+#         sub_solutions _ |-1 * |? + 1
+#
+#     ___ count_derangements
+#         r_ ?|?
+#
+#     ___ count_derangements_rec n
+#         __ ?|? !_ -1
+#             r_ ?|?
+#         __ ? __ 1
+#             r_ 0
+#         __ ? __ 2
+#             r_ 1
+#         result _ ? - 1 * ?|? - 1 + ?|? - 2
+#
+#         ?|? _ ?
+#         r_ ?
+#
+#
+# # for i in range(1,64):
+# ___ i __ ra.. 1 11
+#     n _ ?|?.c..
+#     print("Derangments in set size @ -> @".f.. ? ?
