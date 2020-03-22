6396db1fd5800913c11fb76398531075ba44ed15
Grey <sergejyurskyj@yahoo.com>
2020-03-21 22:16:21 -0700

    Committed 6_math templates

--- 125_algorithms/001_arrays/_exercises/_templates/Advanced Data Structures and Algorithms in Python/Section 1 Doing a Lot with Very Little/6_math.py
+++ 125_algorithms/001_arrays/_exercises/_templates/Advanced Data Structures and Algorithms in Python/Section 1 Doing a Lot with Very Little/6_math.py
@@ -1,37 +1,37 @@
-def square_free(n):
-
-    count = 0
-    for i in range(1, n+1):
-        ok = True
-        for j in range(2, i):
-            if i % (j*j) == 0:
-                ok = False
-                break
-        if ok:
-            count += 1
-
-    return count
-
-def inclusion_exclusion(n):
-    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-
-    def count(k, primes_used):
-        if k == len(primes):
-            product = 1
-            for p in primes_used:
-                product *= p * p
-            if len(primes_used) % 2 == 0:
-                return n // product
-            return -(n // product)
-
-        return count(k + 1, primes_used) + \
-               count(k + 1, primes_used + [primes[k]])
-
-    return count(0, [])
-
-for i in range(1, 10000):
-    sf = square_free(i)
-    ie = inclusion_exclusion(i)
-    #print(i, sf, ie)
-    # supposed to fail at some point, depending on how many primes you have
-    assert sf == ie, 'i={}, sf={}, ie={}'.format(i, sf, ie)
+# ___ square_free n
+#
+#     count _ 0
+#     ___ i __ ra.. 1, ?+1
+#         ok _ T..
+#         ___ j __ ra.. 2 ?
+#             __ i % (j*j) __ 0
+#                 ok _ F..
+#                 b..
+#         __ o.
+#             c.. +_ 1
+#
+#     r_ ?
+#
+# ___ inclusion_exclusion n
+#     primes _ [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+#
+#     ___ count k primes_used
+#         __ k __ le. p..
+#             product _ 1
+#             ___ p __ p_u..
+#                 ? *_ p * p
+#             __ le. p_u.. % 2 __ 0
+#                 r_ ? // p..
+#             r_ - ? // p..
+#
+#         r_ co.. k + 1 p_u.. + \
+#                co.. k + 1 p_u.. + |p..|k
+#
+#     r_ ? 0 ||   # list
+#
+# ___ i __ ra.. 1 10000
+#     sf _ s_f.. ?
+#     ie _ i_e.. ?
+#     #print(i, sf, ie)
+#     # supposed to fail at some point, depending on how many primes you have
+#     ass.. sf __ ie, 'i_@, sf_@, ie_@'.f.. ? ? ?
