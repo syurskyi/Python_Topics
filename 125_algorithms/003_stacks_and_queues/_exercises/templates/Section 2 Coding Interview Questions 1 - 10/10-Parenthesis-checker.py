b1979ce71398258f499702ab573b412f0e5b533c
Grey <sergejyurskyj@yahoo.com>
2020-03-21 23:00:44 -0700

    Committed 10-Parenthesis-checker template

--- 125_algorithms/003_stacks_and_queues/_exercises/templates/Section 2 Coding Interview Questions 1 - 10/10-Parenthesis-checker.py
+++ 125_algorithms/003_stacks_and_queues/_exercises/templates/Section 2 Coding Interview Questions 1 - 10/10-Parenthesis-checker.py
@@ -1,19 +1,19 @@
-# ((()())()) => True
-# )())) = False
-# (() => False
-# )( => False
-
-def check(s):
-    left = 0
-    for c in s:
-        if c == '(':
-            left += 1
-        else:
-            if left == 0:
-                return False
-            left -= 1
-    return left == 0
-
-print(check("((()())())"))
-print(check("(()()))"))
-print(check(")("))
+# # ((()())()) => True
+# # )())) = False
+# # (() => False
+# # )( => False
+#
+# ___ check s
+#     left _ 0
+#     ___ c __ ?
+#         __ ? __ '('
+#             l.. +_ 1
+#         ____
+#             __ l.. __ 0
+#                 r_ F..
+#             l.. -_ 1
+#     r_ l.. __ 0
+#
+# print(check("((()())())"))
+# print(check("(()()))"))
+# print(check(")("))
