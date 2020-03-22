7bd9597e718b3cc3f38854f78860490706c7f965
Grey <sergejyurskyj@yahoo.com>
2020-03-21 22:59:12 -0700

    Committed 09-Missing-number-in-array template

--- 125_algorithms/003_stacks_and_queues/_exercises/templates/Section 2 Coding Interview Questions 1 - 10/09-Missing-number-in-array.py
+++ 125_algorithms/003_stacks_and_queues/_exercises/templates/Section 2 Coding Interview Questions 1 - 10/09-Missing-number-in-array.py
@@ -1,17 +1,17 @@
-# [1,2,3,5,6] => 4
-def missing_number(l):
-    for i in range(1, len(l) + 1): #n
-        if i != l[i - 1]:
-            return i
-    return -1
-
-def missing_number_r(l, start, end): #l, 3, 5
-    if start == end:
-        return start + 1
-    index_middle = int((start + end) / 2) # 3 + 5 / 2 = 4 
-    if l[index_middle] == index_middle + 1:
-        missing_number_r(l,index_middle + 1,end)
-    else:
-        missing_number_r(l,start,index_middle - 1)
-
-print(missing_number_r([1,2,3,5,6],0,5))
+# # [1,2,3,5,6] => 4
+# ___ missing_number l
+#     ___ i __ ra.. 1 le. ? + 1 #n
+#         __ i !_ ?|? - 1
+#             r_ ?
+#     r_ -1
+#
+# ___ missing_number_r l start end    #l, 3, 5
+#     __ ? __ ?
+#         r_ ? + 1
+#     index_middle _ in. ? + ?| / 2 # 3 + 5 / 2 = 4
+#     __ ?|? __ ? + 1
+#         _r ? ? + 1 e..
+#     ____
+#         _r ? s.. i.. - 1
+#
+# print(?([1,2,3,5,6],0,5))
