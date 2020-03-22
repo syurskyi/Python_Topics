9fb6af70ca994e4cc55a164f86f51796276f36a8
Grey <sergejyurskyj@yahoo.com>
2020-03-21 22:11:16 -0700

    Committed 5_modulos templates

--- 125_algorithms/001_arrays/_exercises/_templates/Advanced Data Structures and Algorithms in Python/Section 1 Doing a Lot with Very Little/5_modulos.py
+++ 125_algorithms/001_arrays/_exercises/_templates/Advanced Data Structures and Algorithms in Python/Section 1 Doing a Lot with Very Little/5_modulos.py
@@ -1,40 +1,40 @@
-from random import randint
-
-def in_n_cubed(array, k):
-    n = len(array)
-    min_len = n + 1
-    for i in range(n):
-        for j in range(i, n):
-            if sum(array[i:j+1]) % k == 0 and j - i + 1 < min_len:
-                min_len = j - i + 1
-
-    return min_len
-
-def efficient(array, k):
-    n = len(array)
-    min_len = n + 1
-    last_prefix_idx = {0: -1}
-    current_sum = 0
-    for i in range(n):
-        current_sum = (current_sum + array[i]) % k
-        if current_sum in last_prefix_idx:
-            length = i - last_prefix_idx[current_sum]
-            if length < min_len:
-                min_len = length
-        last_prefix_idx[current_sum] = i
-
-    return min_len
-for tests in range(100):
-    array = []
-    for i in range(200):
-        array.append(randint(1, 100))
-    k = randint(1, 10)
-    n_cubed_result = in_n_cubed(array, k)
-    efficient_result = efficient(array, k)
-    assert n_cubed_result == efficient_result, \
-        'n_cubed={}, efficient={}, array={}, k={}'.format(
-            n_cubed_result,
-            efficient_result,
-            array,
-            k
-        )
+# ____ ra.. _______ r..i..
+#
+# ___ in_n_cubed array k
+#     n _ le. ?
+#     min_len _ ? + 1
+#     ___ i __ ra.. ?
+#         ___ j __ ra.. i n
+#             __ su. ?|i:j+1 ) % k __ 0 an. j - i + 1 < m_l..
+#                 m_l.. _ j - i + 1
+#
+#     r_ ?
+#
+# ___ efficient array k
+#     n _ le. ?
+#     min_len _ ? + 1
+#     last_prefix_idx _ |0: -1
+#     current_sum _ 0
+#     ___ i __ ra.. ?
+#         current_sum _ c_s.. + ?|? % k
+#         __ ? __ l..
+#             length _ ? - l_p_i..|?
+#             __ ? < m_l..
+#                 m_l.. _ ?
+#         l..|c.. _ i
+#
+#     r_ min_len
+# ___ tests __ ra.. 100
+#     array _    # list
+#     ___ i __ ra..(200):
+#         ?.ap.. r..i.. 1 100
+#     k _ r..i.. 1, 10
+#     n_cubed_result _ i.. ? k
+#     efficient_result _ e.. ? ?
+#     ass.. n_ __ e_r.., \
+#         'n_cubed_@, efficient_@, array_@, k_@'.f..(
+#             ?
+#             ?
+#             ?
+#             ?
+#         )
