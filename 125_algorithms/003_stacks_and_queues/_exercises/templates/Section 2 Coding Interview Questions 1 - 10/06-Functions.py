73385bc50fa17efb9a62fd9b3552c4fde9506c3b
Grey <sergejyurskyj@yahoo.com>
2020-03-21 22:50:59 -0700

    Committed 06-Functions templates

--- 125_algorithms/003_stacks_and_queues/_exercises/templates/Section 2 Coding Interview Questions 1 - 10/06-Functions.py
+++ 125_algorithms/003_stacks_and_queues/_exercises/templates/Section 2 Coding Interview Questions 1 - 10/06-Functions.py
@@ -1,12 +1,12 @@
-
-def d(start):
-    def inner():
-        nonlocal start
-        start += 1
-        print(start)
-    return inner
-
-f = d(10)
-
-f() # 11 will be printed
-f() # 12 will be printed
+#
+# ___ d start
+#     ___ inner
+#         nonl.. ?
+#         ? += 1
+#         print ?
+#     r_ ?
+#
+# f = d(10)
+#
+# f() # 11 will be printed
+# f() # 12 will be printed
