390c9ba4ec5881f5c28214a8e3068621f49eda4d
Grey <sergejyurskyj@yahoo.com>
2020-03-22 10:12:57 -0700

    Committed 60. Recursion mini-project 1 - Countdown timer template

--- 125_algorithms/003_Recursion/_exercises/templates/Python 3 Project-based Python, Algorithms, Data Structures/60. Recursion mini-project 1 - Countdown timer.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Python 3 Project-based Python, Algorithms, Data Structures/60. Recursion mini-project 1 - Countdown timer.py
@@ -1,21 +1,21 @@
-import time
-
-def recur_countdown_timer(n):
-    if n == 0:
-        return n
-    else:
-        print(n)
-        time.sleep(1)
-        return recur_countdown_timer(n-1)
-
-def iter_countdown_timer(n):
-    while n > 0:
-        print(n)
-        time.sleep(1)
-        n -= 1
-    print(n)
-
-z = 5
-print(f"Counting down from {z}:")
-# iter_countdown_timer(z)
-print(recur_countdown_timer(z))
+# ______ ti..
+#
+# ___ recur_countdown_timer n
+#     __ ? __ 0
+#         r_ ?
+#     ____
+#         print ?
+#         ti__.sl.. 1
+#         r_ ? ?-1
+#
+# ___ iter_countdown_timer n
+#     w__ ? > 0
+#         print ?
+#         ti__.sl.. 1
+#         ? -= 1
+#     print ?
+#
+# z = 5
+# print _*Counting down from ? :")
+# # iter_countdown_timer(z)
+# print(? ?
