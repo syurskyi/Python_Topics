69204490e1b29342282d1ca502a9275c1d717007
Grey <sergejyurskyj@yahoo.com>
2020-03-22 19:25:51 -0700

    Committed max_sub_array_rec template

--- 125_algorithms/003_Recursion/_exercises/templates/Section 5 Maximum Sub Array/max_sub_array_rec.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Section 5 Maximum Sub Array/max_sub_array_rec.py
@@ -1,24 +1,24 @@
-c_ MaxSubArrayRec:
-    def __init__(self, prices):
-        self.prices = prices
-
-    def max_sub_array(self):
-        max_value = 0
-        for j in range(len(self.prices)):
-            max_value = max(max_value, self.max_sub_array_ending_at(j))
-        return max_value
-
-    def max_sub_array_ending_at(self, i):
-        if i == 0:
-            return self.prices[0]
-        return max(self.prices[i], self.max_sub_array_ending_at(i - 1) + self.prices[i])
-
-
-msa = MaxSubArrayRec([5, -4, 8, -10, -2, 4, -3, 2, 7, -8, 3, -5, 3])
-print(msa.max_sub_array())
-
-# input = [1] * 900
-# for i in range(10):
-#     msa = MaxSubArrayRec(input)
-#     print(msa.max_sub_array())
-
+# c_ MaxSubArrayRec
+#     ___ - prices
+#         ? ?
+#
+#     ___ max_sub_array
+#         max_value _ 0
+#         ___ j __ ra.. le. ?
+#             max_value _ ma. ? ?|?
+#         r_ ?
+#
+#     ___ max_sub_array_ending_at i
+#         __ i __ 0
+#             r_ ? 0
+#         r_ ma. ?|? ?|?- 1 + p..|?
+#
+#
+# msa _ ? 5, -4, 8, -10, -2, 4, -3, 2, 7, -8, 3, -5, 3
+# print(msa.max_sub_array())
+#
+# # input _ [1] * 900
+# # for i in range(10):
+# #     msa _ MaxSubArrayRec(input)
+# #     print(msa.max_sub_array())
+#
