e8fbdbd7272ba6764424d04a23541adefbbc8407
Grey <sergejyurskyj@yahoo.com>
2020-03-23 20:27:52 -0700

    Committed text_justify_opt template

--- 125_algorithms/003_Recursion/_exercises/templates/Section 6 Text Justification/text_justify_opt.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Section 6 Text Justification/text_justify_opt.py
@@ -1,47 +1,47 @@
-_____ sys
-
-class TextJustifyOpt:
-    def __init__(self, txt, line_length):
-        self.txt = txt
-        self.line_length = line_length
-        self.word_length = [[]] * len(txt)
-        for i in range(len(txt)):
-            self.word_length[i] = [0] * len(txt)
-            self.word_length[i][i] = len(txt[i])
-            for j in range(i + 1, len(txt)):
-                self.word_length[i][j] = self.word_length[i][j - 1] + 1 + len(txt[j])
-        for arr in self.word_length:
-            print(arr)
-
-    def ugly_score(self, txt_length):
-        if txt_length <= self.line_length:
-            return (self.line_length - txt_length) ** 2
-        else:
-            return sys.maxsize
-
-    def format_txt(self):
-        scores = [0] * (len(self.txt) + 1)
-        ptrs = [0] * len(self.txt)
-
-        for i in range(len(self.txt) - 1, -1, -1):
-            score = sys.maxsize
-            for j in range(i + 1, len(self.txt) + 1):
-                curr_score = self.ugly_score(self.word_length[i][j - 1]) + scores[j]
-                if curr_score < score:
-                    score = curr_score
-                    ptrs[i] = j
-            scores[i] = score
-
-        self.print_txt(ptrs)
-        return scores[0]
-
-    def print_txt(self, ptrs):
-        i = 0
-        while i < len(ptrs):
-            line = self.txt[i:ptrs[i]]
-            print(" ".join(line))
-            i = ptrs[i]
-
-
-justify = TextJustifyOpt("Isabel sat on the step".split(), 10)
-print(justify.format_txt())
+# _____ ___
+#
+# c_ TextJustifyOpt
+#     ___ -  txt, line_length
+#         ?  ?
+#         ?  ?
+#         word_length _ |||| * le. ?
+#         ___ i __ ra.. le. t..
+#             w__|? _ |0 * le. t..
+#             w__|?|? _ le. t..|?
+#             ___ j __ ra.. ? + 1, le. t..
+#                 w__|?|? _ w__|?|? - 1| + 1 + le. t..|?
+#         ___ arr __ w__
+#             print ?
+#
+#     ___ ugly_score txt_length
+#         __ ? <_ l..
+#             r_ l.. - t.. ** 2
+#         ____
+#             r_ ___.ma..
+#
+#     ___ format_txt
+#         scores _ |0 * (le. t.. + 1
+#         ptrs _ [0] * le. t..
+#
+#         ___ i __ ra.. le. t.. - 1, -1, -1
+#             score _ ___.ma..
+#             ___ j __ ra.. ? + 1 le. t.. + 1
+#                 curr_score _ u..|w__|?|? - 1|| + scores|?
+#                 __ ? < s..
+#                     s.._ c..
+#                     p..|? _ ?
+#             scores|? _ s..
+#
+#         print_txt ptrs
+#         r_ scores|0
+#
+#     ___ print_txt ptrs
+#         i _ 0
+#         w___ ? < le. p..
+#             line _ t..|i;p..|?
+#             print(" ".j.. ?
+#             ? _ p..|?
+#
+#
+# justify _ ?("Isabel sat on the step".s.. 10)
+# print ?.f..
