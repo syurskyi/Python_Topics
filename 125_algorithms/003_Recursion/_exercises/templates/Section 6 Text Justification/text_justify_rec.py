af711ac89f78c2301a0092c861883e0b8ddb2c08
Grey <sergejyurskyj@yahoo.com>
2020-03-23 20:32:28 -0700

    Committed text_justify_rec template

--- 125_algorithms/003_Recursion/_exercises/templates/Section 6 Text Justification/text_justify_rec.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Section 6 Text Justification/text_justify_rec.py
@@ -1,35 +1,35 @@
-_____ sys
-
-class TextJustifyRec:
-    def __init__(self, txt, line_length):
-        self.txt = txt
-        self.line_length = line_length
-
-    def ugly_score(self, txt_length):
-        if txt_length <= self.line_length:
-            return (self.line_length - txt_length) ** 2
-        else:
-            return sys.maxsize
-
-    def count_chars(self, fr, to):
-        total_chars = 0
-        for i in range(fr, to):
-            total_chars += len(self.txt[i])
-            if i < to - 1:
-                total_chars += 1
-        return total_chars
-
-    def format_txt(self, i):
-        if i == len(self.txt):
-            return 0
-        score = sys.maxsize
-        for x in range(i + 1, len(self.txt) + 1):
-            line_len = self.count_chars(i, x)
-            curr_score = self.ugly_score(line_len)
-            curr_score += self.format_txt(x)
-            score = min(curr_score, score)
-        return score
-
-
-justify = TextJustifyRec("Isabel sat on the step".split(), 10)
-print(justify.format_txt(0))
+# _____ ___
+#
+# c_ TextJustifyRec
+#     ___ - txt line_length
+#         ? ?
+#         ? ?
+#
+#     ___ ugly_score txt_length
+#         __ ? <_ l..
+#             r_ l.. - ? ** 2
+#         ____
+#             r_ ___.ma..
+#
+#     ___ count_chars fr, to
+#         total_chars _ 0
+#         ___ i __ ra.. ? ?
+#             t.. +_ le. t..|?
+#             __ ? < t. - 1
+#                 t.. +_ 1
+#         r_ ?
+#
+#     ___ format_txt i
+#         __ i __ le. ?
+#             r_ 0
+#         score _ ___.ma..
+#         ___ x __ ra..  ? + 1, le. t.. + 1
+#             line_len _ c.. ? ?
+#             curr_score _ u.. l..
+#             c.. +_ f.. ?
+#             score _ mi. c.. s..
+#         r_ ?
+#
+#
+# justify _ ?("Isabel sat on the step".sp.. 10
+# print ?.f.. 0
