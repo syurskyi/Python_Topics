e163b67fc805f8d0b4dbb5452b4c61b36dc92cbe
syurskyi <sergejyurskyj@yahoo.com>
2020-03-25 12:10:37 -0700

    Update Linked List Nth to Last Node.py

--- 125_algorithms/002_linked_lists/_exercises/templates/Linked Lists/Linked Lists Interview Problems/PRACTICE/Linked List Nth to Last Node.py
+++ 125_algorithms/002_linked_lists/_exercises/templates/Linked Lists/Linked Lists Interview Problems/PRACTICE/Linked List Nth to Last Node.py
@@ -1,82 +1,82 @@
-# Linked List Nth to Last Node
-# Problem Statement
+# # Linked List Nth to Last Node
+# # Problem Statement
+# #
+# # Write a function that takes a head node and an integer value n and then returns the nth to last node in the linked list. For example, given:
 #
-# Write a function that takes a head node and an integer value n and then returns the nth to last node in the linked list. For example, given:
-
-class Node:
-
-    def __init__(self, value):
-        self.value = value
-        self.nextnode  = None
-
-# Example Input and Output:
-
-a = Node(1)
-b = Node(2)
-c = Node(3)
-d = Node(4)
-e = Node(5)
-
-a.nextnode = b
-b.nextnode = c
-c.nextnode = d
-d.nextnode = e
-
-# # This would return the node d with a value of 4, because its the 2nd to last node.
-target_node = nth_to_last_node(2, a)
-
-target_node.value#
-# 4
+# c_ Node
 #
-# Solution
+#     ___ - value
+#         ? ?
+#         nextnode  _ N..
 #
-# Fill out your solution below:
+# # Example Input and Output:
 #
-def nth_to_last_node(n, head):
-    slow, fast = head, head
-    while n > 0 and fast:
-        fast=fast.nextnode
-        n-=1
-    if not fast:
-        return head
-    while fast:
-        fast=fast.nextnode
-        slow=slow.nextnode
-    return slow
-    pass
+# a = Node(1)
+# b = Node(2)
+# c = Node(3)
+# d = Node(4)
+# e = Node(5)
 #
-# Test Your Solution
+# a.nextnode = b
+# b.nextnode = c
+# c.nextnode = d
+# d.nextnode = e
 #
-# """
-# RUN THIS CELL TO TEST YOUR SOLUTION AGAINST A TEST CASE
+# # # This would return the node d with a value of 4, because its the 2nd to last node.
+# target_node = nth_to_last_node(2, a)
 #
-# PLEASE NOTE THIS IS JUST ONE CASE
-# """
+# target_node.value#
+# # 4
+# #
+# # Solution
+# #
+# # Fill out your solution below:
+# #
+# ___ nth_to_last_node n head
+#     slow, fast _ head head
+#     w___ n > 0 an. f..
+#         f.. _ ?.n..
+#         ? -= 1
+#     __ no. f..
+#         r_ h..
+#     w___ ?
+#         f.. _ ?.n..
+#         s.. _ ?.n..
+#     r_ ?
+#     p..
+# #
+# # Test Your Solution
+# #
+# # """
+# # RUN THIS CELL TO TEST YOUR SOLUTION AGAINST A TEST CASE
+# #
+# # PLEASE NOTE THIS IS JUST ONE CASE
+# # """
+# #
+# from nose.tools import assert_equal
 #
-from nose.tools import assert_equal
-
-a = Node(1)
-b = Node(2)
-c = Node(3)
-d = Node(4)
-e = Node(5)
-
-a.nextnode = b
-b.nextnode = c
-c.nextnode = d
-d.nextnode = e
-
-####
-
-class TestNLast(object):
-
-    def test(self,sol):
-
-        assert_equal(sol(2,a),d)
-        print ('ALL TEST CASES PASSED')
-
-# Run tests
-t = TestNLast()
-t.test(nth_to_last_node)
-
-# ALL TEST CASES PASSED
+# a = Node(1)
+# b = Node(2)
+# c = Node(3)
+# d = Node(4)
+# e = Node(5)
+#
+# a.nextnode = b
+# b.nextnode = c
+# c.nextnode = d
+# d.nextnode = e
+#
+# ####
+#
+# c_ TestNLast(object):
+#
+#     def test(self,sol):
+#
+#         assert_equal(sol(2,a),d)
+#         print ('ALL TEST CASES PASSED')
+#
+# # Run tests
+# t = TestNLast()
+# t.test(nth_to_last_node)
+#
+# # ALL TEST CASES PASSED
