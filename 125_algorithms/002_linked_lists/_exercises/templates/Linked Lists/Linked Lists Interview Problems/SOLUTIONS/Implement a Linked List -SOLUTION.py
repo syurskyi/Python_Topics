c6070b086dcc2d6abbcd35078295b7e0d799d51c
Grey <sergejyurskyj@yahoo.com>
2020-03-25 20:16:35 -0700

    Committed Implement a Linked List -SOLUTION template

--- 125_algorithms/002_linked_lists/_exercises/templates/Linked Lists/Linked Lists Interview Problems/SOLUTIONS/Implement a Linked List -SOLUTION.py
+++ 125_algorithms/002_linked_lists/_exercises/templates/Linked Lists/Linked Lists Interview Problems/SOLUTIONS/Implement a Linked List -SOLUTION.py
@@ -1,46 +1,45 @@
-# Implement a Linked List - SOLUTION
-# Problem Statement
-# Implement a Linked List by using a Node class object. Show how you would implement a Singly Linked List and
-# a Doubly Linked List!
-# Solution
-# Since this is asking the same thing as the implementation lectures, please refer to those video lectures and notes
-# for a full explanation. The code from those lectures is displayed below:
-# Singly Linked List
+# # Implement a Linked List - SOLUTION
+# # Problem Statement
+# # Implement a Linked List by using a Node class object. Show how you would implement a Singly Linked List and
+# # a Doubly Linked List!
+# # Solution
+# # Since this is asking the same thing as the implementation lectures, please refer to those video lectures and notes
+# # for a full explanation. The code from those lectures is displayed below:
+# # Singly Linked List
+# #
+# c_ LinkedListNode o..
+#     ___ - value
 #
-class LinkedListNode(object):
-
-    def __init__(self,value):
-
-        self.value = value
-        self.nextnode = None
-
-a = LinkedListNode(1)
-b = LinkedListNode(2)
-c = LinkedListNode(3)
-
-a.nextnode = b
-b.nextnode = c
-
-# Doubly Linked List
+#         ? ?
+#         nextnode _ N..
 #
-class DoublyLinkedListNode(object):
-
-    def __init__(self,value):
-
-        self.value = value
-        self.next_node = None
-        self.prev_node = None
-
-a = DoublyLinkedListNode(1)
-b = DoublyLinkedListNode(2)
-c = DoublyLinkedListNode(3)
-
-# Setting b after a
-b.prev_node = a
-a.next_node = b
-
-# Setting c after a
-b.next_node = c
-c.prev_node = b
-
-# Good Job!
+# a = ? 1
+# b = ? 2
+# c = ? 3
+#
+# a.nextnode = b
+# b.nextnode = c
+#
+# # Doubly Linked List
+# #
+# c_ DoublyLinkedListNode o..
+#
+#     ___ - value
+#
+#         ? ?
+#         next_node _ N..
+#         prev_node _ N..
+#
+# a = ? 1
+# b = ? 2
+# c = ? 3
+#
+# # Setting b after a
+# b.prev_node = a
+# a.next_node = b
+#
+# # Setting c after a
+# b.next_node = c
+# c.prev_node = b
+#
+# # Good Job!
