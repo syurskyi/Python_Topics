30922824bfdadd7806aa996e2789433810603922
Grey <sergejyurskyj@yahoo.com>
2020-03-28 16:32:56 -0700

    Committed Arrays exercises

---
+++ 125_algorithms/001_arrays/_exercises/exercises/Array Sequences/Array Sequences Interview Questions/SOLUTIONS/Unique Characters in String - SOLUTION.py
@@ -0,0 +1,44 @@
+# # Unique Characters in String
+# # Problem
+# #
+# # Given a string,determine if it is compreised of all unique characters. For example, the string 'abcde' has all
+# # unique characters and should return True. The string 'aabcde' contains duplicate characters and should return false.
+# # Solution
+# #
+# # We'll show two possible solutions, one using a built-in data structure and a built in function, and another using
+# # a built-in data structure but using a look-up method to check if the characters are unique
+#
+# ___ uni_char s
+#     r_ le. se. ? __ le. ?
+#
+# ___ uni_char2 s
+#     chars = se.
+#     ___ let __ ?
+#         # Check if in set
+#         __ ? __ c..
+#             r_ F..
+#         e..
+#             #Add it to the set
+#             c__.a.. l..
+#     r_ T..
+#
+# # Test Your Solution
+# # """
+# # RUN THIS CELL TO TEST YOUR CODE>
+# # """
+# # from nose.tools import assert_equal
+# #
+# #
+# # class TestUnique(object):
+# #
+# #     ___ test(self, sol):
+# #         assert_equal(sol(''), True)
+# #         assert_equal(sol('goo'), False)
+# #         assert_equal(sol('abcdefg'), True)
+# #         print('ALL TEST CASES PASSED')
+# #
+# #
+# # # Run Tests
+# # t = TestUnique()
+# # t.test(uni_char)
+#
