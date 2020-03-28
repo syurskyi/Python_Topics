9472851a132876f99eb65d8d15e6319203129f49
Grey <sergejyurskyj@yahoo.com>
2020-03-28 11:23:41 -0700

    Committed 3.sort template

--- 125_algorithms/002_linked_lists/_exercises/templates/Section 5 Singly Linked List/3.sort.py
+++ 125_algorithms/002_linked_lists/_exercises/templates/Section 5 Singly Linked List/3.sort.py
@@ -1,30 +1,30 @@
-from singlyLinkedList import Node, LinkedList
+____ singlyLinkedList ______ Node, LinkedList
 
-def swapNext(linkedList, previousNode, largestNode, nextNode):
+___ swapNext linkedList previousNode largestNode nextNode
     # 4->3->5->1 || 3->4->5->1 || 4->|1->5->None
-    largestNode.next = nextNode.next
-    nextNode.next = largestNode
-    if largestNode is linkedList.head:
-        linkedList.head = nextNode
-        return
-    previousNode.next =  nextNode
+    lN__.n.. _ nN__.n..
+    nN__.n.. _ lN__
+    __ lN__ __ lL__.h..
+        lL__.head _ nN__
+        r_
+    pN__.n.. _  nN__
 
-def sort(linkedList):
-    # number of iterations = number of nodes - 1
-    numberOfIterations = linkedList.listLength() - 1 # 3
-    while numberOfIterations != 0: # 3, 2, 1
-        largestNode = linkedList.head
-        previousNode = None
-        numberOfComparisons = numberOfIterations
-        while numberOfComparisons != 0:
-            if largestNode.data > largestNode.next.data:
-                swapNext(linkedList, previousNode, largestNode, largestNode.next)
-            else:
-                previousNode = largestNode
-                largestNode = largestNode.next
-            numberOfComparisons -= 1
+____ sort lL__
+    # number of iterations _ number of nodes - 1
+    numberOfIterations _ lL__.lL.. - 1 # 3
+    w___ nOI.. !_ 0 # 3, 2, 1
+        largestNode _ lL__.h..
+        pN__ _ N..
+        nOC.. _ nOI..
+        w__ nOC.. !_ 0
+            __ lN__.data > lN__.n...da..
+                sN.. lL__ pN__ lN__ lN__.n..
+            ____
+                pN__ _ lN__
+                lN__ _ lN__.n..
+            nOC.. -_ 1
 
-        numberOfIterations -= 1
+        nOI.. -_ 1
 
 nodeOne = Node(4)
 nodeTwo = Node(3)
