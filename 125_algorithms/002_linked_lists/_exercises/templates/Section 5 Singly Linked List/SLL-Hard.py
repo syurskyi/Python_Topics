08e77ece48f91c51e0e660dff5ba5d2f317bb173
Grey <sergejyurskyj@yahoo.com>
2020-03-28 11:58:25 -0700

    Committed SLL-Hard template

--- 125_algorithms/002_linked_lists/_exercises/templates/Section 5 Singly Linked List/SLL-Hard.py
+++ 125_algorithms/002_linked_lists/_exercises/templates/Section 5 Singly Linked List/SLL-Hard.py
@@ -1,51 +1,51 @@
 # Delete nodes that have a greater value on the right side
 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+c_ Node
+    ___ - data
+        ?  ?
+        next _ N..
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+c_ LinkedList
+    ___ -
+        head _ N..
 
-    def insertEnd(self, newNode):
-        if self.head is None:
-            self.head = newNode
-            return
-        lastNode = self.head
-        while lastNode.next is not None:
-            lastNode = lastNode.next
-        lastNode.next = newNode
+    ___ insertEnd newNode
+        __ h.. __ N..
+            h.. _ ?
+            r_
+        lastNode _ h..
+        w__ ?.n.. __ no. N..
+            ? _ ?.n..
+        ?.n.. _ nN..
 
-    def deleteRight(self):
-        currentNode = self.head
-        while currentNode.next is not None:
+    ___ deleteRight
+        currentNode _ h..
+        w__ ?.n.. __ no. N..
             # Check if node on the right has a greater value
-            if currentNode.next.data > currentNode.data:
+            __ ?.n__.d.. > ?.d..
                 # If currentNode is the head node, delete it and make the new node as the head node
-                if currentNode is self.head:
-                    self.head = currentNode.next
-                    currentNode.next = None
-                    currentNode = self.head
-                    continue
+                __ cN__ __ h..
+                    h.. _ cN__.n..
+                    cN__.n.. _ N..
+                    cN__ _ h..
+                    c____
                 # If currentNode is not head node, make the previous node point to next node and remove this node
-                previousNode.next = currentNode.next
-                currentNode.next = None
-                currentNode = self.head
-            else:
+                pN__.n.. _ cN__.n..
+                cN__.n.. _ N..
+                cN__ _ h..
+            ____
                 # Advance to next node if the data on the right side is not greater than currentNode
-                previousNode = currentNode
-                currentNode = currentNode.next
+                pN.. _ cN__
+                cN__ _ cN__.n..
 
-    def printList(self):
-        if self.head is None:
+    ___ printList
+        __ h.. __ N..
             print("Empty List")
-            return
-        currentNode = self.head
-        while currentNode is not None:
-            print(currentNode.data)
-            currentNode = currentNode.next
+            r_
+        cN__ _ h..
+        w__ cN__ __ no. N..
+            print(cN__.data)
+            cN__ _ cN__.n..
 
 nodeOne = Node(10)
 nodeTwo = Node(5)
