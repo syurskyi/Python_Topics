1a8e8393d2aeeaf68a136c7129305c3920a4bb0a
Grey <sergejyurskyj@yahoo.com>
2020-03-28 15:46:34 -0700

    Committed string_distance_opt template

--- 125_algorithms/003_Recursion/_exercises/templates/Section 7 String Distance/string_distance_opt.py
+++ 125_algorithms/003_Recursion/_exercises/templates/Section 7 String Distance/string_distance_opt.py
@@ -1,29 +1,29 @@
-class StringDistanceOpt:
-    def __init__(self, str_A, str_B):
-        self.str_A = str_A
-        self.str_B = str_B
-        self.dist_read = [-1] * (len(str_B) + 1)
-        self.dist_write = [-1] * (len(str_B) + 1)
-
-        for a in range(len(str_A) + 1):
-            for b in range(len(str_B) + 1):
-                if a == 0:
-                    self.dist_write[b] = b
-                elif b == 0:
-                    self.dist_write[b] = a
-                else:
-                    replace_cost = 0 if self.str_A[a - 1] == self.str_B[b - 1] else 1
-                    cost_delete = self.dist_read[b] + 1
-                    cost_insert = self.dist_write[b - 1] + 1
-                    cost_replace = self.dist_read[b - 1] + replace_cost
-                    self.dist_write[b] = min(cost_delete, cost_insert, cost_replace)
-            (self.dist_read, self.dist_write) = (self.dist_write, self.dist_read)
-            print(self.dist_read)
-
-    def distance(self):
-        return self.dist_read[len(self.str_B)]
-
-
-#dist = StringDistanceOpt("TodayIsSaturday", "TomorrowIsSunday")
-dist = StringDistanceOpt("Saturday", "Sundays")
-print(dist.distance())
+# c_ StringDistanceOpt
+#     ___ - str_A str_B
+#         ? ?
+#         ? ?
+#         dist_read _ |-1 * le. _B + 1
+#         dist_write _ |-1 * le. _B + 1
+#
+#         ___ a __ ra.. le. _A + 1
+#             for b __ ra.. le. _B + 1
+#                 __ a __ 0
+#                     dist_write|b _ b
+#                 ____ b __ 0
+#                     dist_write|b _ a
+#                 ____
+#                     replace_cost _ 0 __ _A|a - 1 __ _B|b - 1 ____ 1
+#                     cost_delete _ d_r..|b + 1
+#                     cost_insert _ d_w..|b - 1 + 1
+#                     cost_replace _ d_r..|b - 1 + r_c..
+#                     d_w..|b _ mi. c_d.. c_i.. c_r..
+#             d_r.., d_w.. _ d_w.., d_r..
+#             print d_r..
+#
+#     ___ distance
+#         r_ d_r..|le. _B
+#
+#
+# #dist _ StringDistanceOpt("TodayIsSaturday", "TomorrowIsSunday")
+# dist _ ?("Saturday", "Sundays")
+# print(?.d..
