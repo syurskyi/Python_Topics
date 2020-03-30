d787d5c126d82889adc641ffe93ffb4ba3d2c10b
Grey <sergejyurskyj@yahoo.com>
2020-03-29 21:45:57 -0700

    Committed max_counterstemplate

--- 125_algorithms/_exercises/templates/Beat the Codility Coding Interview in Python/Section 4 Counting Elements/countingelements/max_counters.py
+++ 125_algorithms/_exercises/templates/Beat the Codility Coding Interview in Python/Section 4 Counting Elements/countingelements/max_counters.py
@@ -1,24 +1,24 @@
-# This is the solution for CountingElements > MaxCounters
+# # This is the solution for CountingElements > MaxCounters
+# #
+# # This is marked as RESPECTABLE difficulty
 #
-# This is marked as RESPECTABLE difficulty
-
-def solution(N, A):
-    counters = [0] * N
-    start_line = 0
-    current_max = 0
-    for i in A:
-        x = i - 1
-        if i > N:
-            start_line = current_max
-        elif counters[x] < start_line:
-            counters[x] = start_line + 1
-        else:
-            counters[x] += 1
-        if i <= N and counters[x] > current_max:
-            current_max = counters[x]
-    for i in range(0, len(counters)):
-        if counters[i] < start_line:
-            counters[i] = start_line
-    return counters
-
-print (solution(5, [3, 4, 4, 6, 1, 4, 4]))
+# ___ solution N, A
+#     counters _ [0] * N
+#     start_line _ 0
+#     current_max _ 0
+#     ___ i __ A
+#         x _ ? - 1
+#         __ ? > N
+#             s.. _ _m
+#         ____ c..|x < s..
+#             c..|x] _ s.. + 1
+#         ____
+#             c..|x +_ 1
+#         __ ? <_ N an. c..|x > _m..
+#             _m.. _ c..|x
+#     ___ ? __ ra.. 0 le. c..
+#         __ c..|? < s..
+#             c..|? _ s..
+#     r_ c..
+#
+# print (solution(5, [3, 4, 4, 6, 1, 4, 4]))
