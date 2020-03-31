6c18d4f2ea22d7c618adadfe5047470d0504e7a9
Grey <sergejyurskyj@yahoo.com>
2020-03-30 22:10:52 -0700

    Committed NQueens template

--- 125_algorithms/012_backtracking/_exercises/templates/Algorithmic Problems in Python/NQueens.py
+++ 125_algorithms/012_backtracking/_exercises/templates/Algorithmic Problems in Python/NQueens.py
@@ -1,84 +1,83 @@
-
-class QueensProblem:
-	
-	def __init__(self, numOfQueens):
-		self.numOfQueens = numOfQueens
-		self.chessTable = [[None for i in range(numOfQueens)] for j in range(numOfQueens)]
-		
-	def solveQueensProblem(self):
-		
-		if self.solve(0):
-			self.printQueens()
-		else:
-			print('There is no solution...')
-	
-	def solve(self, colIndex):
-		
-		if colIndex == self.numOfQueens:
-			return True
-		
-		for rowIndex in range(self.numOfQueens):
-		
-			if self.isPlaceValid(rowIndex, colIndex):
-				
-				self.chessTable[rowIndex][colIndex] =  1
-				
-				if self.solve(colIndex+1):
-					return True
-
-				# BACKTRACK
-				self.chessTable[rowIndex][colIndex] = 0
-				
-		return False
-			
-	def isPlaceValid(self, rowIndex, columnIndex):
-		
-		#same row
-		for i in range(columnIndex):
-			if self.chessTable[rowIndex][i] == 1:
-				return False
-				
-		#from top left to bottom right
-		j = columnIndex
-		for i in range(rowIndex,-1,-1):
-			
-			if j < 0:
-				break
-				
-			if self.chessTable[i][j] == 1:
-				return False
-			
-			j = j -1
-			
-		#from bottom left to top right
-		j = columnIndex
-		for i in range(rowIndex,len(self.chessTable)):
-			
-			if j < 0:
-				break
-				
-			if self.chessTable[i][j] == 1:
-				return False
-			
-			j = j -1
-			
-		return True	
-		
-	def printQueens(self):
-
-		for i in range(self.numOfQueens):
-			for j in range(self.numOfQueens):
-			
-				if self.chessTable[i][j] == 1:
-					print(' * ', end=""),
-				else:
-					print(' - ', end=""),
-				
-			print('\n')
-		
-if __name__ == "__main__":
-
-	queensProblem = QueensProblem(100)
-	queensProblem.solveQueensProblem()
-		
-
+# c_ QueensProblem
+#
+# 	___ - numOfQueens
+# 		? ?
+# 		chessTable _ ||None ___ i __ ra.. ?|| ___ j __ ra.. ?
+#
+# 	___ solveQueensProblem
+#
+# 		__ solve 0
+# 			pQ..
+# 		____
+# 			print('There is no solution...')
+#
+# 	___ solve colIndex
+#
+# 		__ ? __ nOQ..
+# 			r_ T..
+#
+# 		___ rowIndex __ ra.. nOQ..
+#
+# 			__ iPV.. rI.. cI..
+#
+# 				cT..|rI.. |cI.. _  1
+#
+# 				__ s.. cI..+1
+# 					r_ T..
+#
+# 				# BACKTRACK
+# 				cT..|rI.. |cI.. _ 0
+#
+# 		r_ F..
+#
+# 	___ isPlaceValid rowIndex columnIndex
+#
+# 		#same row
+# 		___ i __ ra.. cI..
+# 			__ cT..|rI.. |? __ 1
+# 				r_ F..
+#
+# 		#from top left to bottom right
+# 		j _ cI..
+# 		___ i __ ra.. rI..,-1,-1
+#
+# 			__ j < 0
+# 				b..
+#
+# 			__ cT..|? |? __ 1
+# 				r_ F..
+#
+# 			j _ j -1
+#
+# 		#from bottom left to top right
+# 		j _ columnIndex
+# 		___ i __ ra.. rI.. le. cT..
+#
+# 			__ j < 0
+# 				b..
+#
+# 			__ cT..|? |? __ 1
+# 				r_ F..
+#
+# 			j _ j - 1
+#
+# 		r_ T..
+#
+# 	___ printQueens
+#
+# 		___ i __ ra.. nOQ..
+# 			___ j __ ra.. nOQ..
+#
+# 				__ cT..|? |? __ 1
+# 					print(' * ', e.._""
+# 				____
+# 					print(' - ', e.._""
+#
+# 			print('\n')
+#
+# __ _______ __ ______
+#
+# 	queensProblem _ ? 100
+# 	?.sQP..
+#
+#
