e91ad39d8c59a8abcebd84a7f720db576d43344d
Grey <sergejyurskyj@yahoo.com>
2020-03-30 21:03:24 -0700

    Committed Section_09_Heaps template

--- 125_algorithms/006_heaps/_exercises/templates/9 Data Structures - Heaps/Section_09_Heaps.py
+++ 125_algorithms/006_heaps/_exercises/templates/9 Data Structures - Heaps/Section_09_Heaps.py
@@ -1,87 +1,87 @@
-class Heap(object):
-    HEAP_SIZE = 10
-
-    def __init__(self):
-        self.heap = [0] * Heap.HEAP_SIZE
-        self.currentPosition = -1
-
-    def insert(self, item):
-
-        if self.isFull():
-            print("Heap is full..");
-            return
-
-        self.currentPosition = self.currentPosition + 1
-        self.heap[self.currentPosition] = item
-        self.fixUp(self.currentPosition)
-
-    def fixUp(self, index):
-
-        parentIndex = int((index - 1) / 2)
-
-        while parentIndex >= 0 and self.heap[parentIndex] < self.heap[index]:
-            temp = self.heap[index]
-            self.heap[index] = self.heap[parentIndex]
-            self.heap[parentIndex] = temp
-            index = parentIndex
-            parentIndex = (int)((index - 1) / 2)
-
-    def heapsort(self):
-
-        for i in range(0, self.currentPosition + 1):
-            temp = self.heap[0]
-            print("%d " % temp)
-            self.heap[0] = self.heap[self.currentPosition - i]
-            self.heap[self.currentPosition - i] = temp
-            self.fixDown(0, self.currentPosition - i - 1)
-
-    def fixDown(self, index, upto):
-
-        while index <= upto:
-
-            leftChild = 2 * index + 1
-            rightChild = 2 * index + 2
-
-            if leftChild < upto:
-                childToSwap = None
-
-                if rightChild > upto:
-                    childToSwap = leftChild
-                else:
-                    if self.heap[leftChild] > self.heap[rightChild]:
-                        childToSwap = leftChild
-                    else:
-                        childToSwap = rightChild
-
-                if self.heap[index] < self.heap[childToSwap]:
-                    temp = self.heap[index]
-                    self.heap[index] = self.heap[childToSwap]
-                    self.heap[childToSwap] = temp
-                else:
-                    break
-
-                index = childToSwap
-            else:
-                break;
-
-    def isFull(self):
-        if self.currentPosition == Heap.HEAP_SIZE:
-            return True
-        else:
-            return False
-
-
-if __name__ == "__main__":
-    heap = Heap()
-    heap.insert(10)
-    heap.insert(-20)
-    heap.insert(0)
-    heap.insert(2)
-    heap.insert(4)
-    heap.insert(5)
-    heap.insert(6)
-    heap.insert(7)
-    heap.insert(20)
-    heap.insert(15)
-
-    heap.heapsort()
+# c_ Heap o..
+#     HEAP_SIZE _ 10
+#
+#     ___ -
+#         heap _ |0 * H__.H..
+#         currentPosition _ -1
+#
+#     ___ insert item
+#
+#         __ isFull
+#             print("Heap is full..")
+#             r_
+#
+#         currentPosition _ ? + 1
+#         h..|? _ i..
+#         fU. ?
+#
+#     ___ fixUp index
+#
+#         parentIndex _ in. ? - 1) / 2
+#
+#         w___ ? >_ 0 an. h..|? < h..|i..
+#             temp _ h..|i..
+#             h..|i.. _ h..|?
+#             h..|? _ temp
+#             i.. _ ?
+#             ? _ in. i... - 1) / 2
+#
+#     ___ heapsort
+#
+#         ___ i __ RA.. 0 c.. + 1
+#             temp _ h..|0
+#             print("@ "  ?   # digit
+#             h..|0 _ h..[c.. - i]
+#             h..[c.. - i] _ ?
+#             fD.. 0 c.. - i - 1
+#
+#     ___ fixDown index upto
+#
+#         w__ ? <_ ?
+#
+#             leftChild _ 2 * i.. + 1
+#             rightChild _ 2 * i.. + 2
+#
+#             __ l.. < u..
+#                 cTS.. _ N..
+#
+#                 __ r.. > u..
+#                     cTS.. _ l..
+#                 ____
+#                     __ h..|l.. > h..|r..
+#                         cTS.. _ l..
+#                     ____
+#                         cTS.. _ r..
+#
+#                 __ h..|i.. < h..|cTS..
+#                     temp _ h..|i..
+#                     h..|i.. _ h..|cTS..
+#                     h..|cTS.. _ t..
+#                 ____
+#                     b..
+#
+#                 i.. _ cTS..
+#             ____
+#                 b..
+#
+#     ___ isFull
+#         __ cP.. __ H__.H..
+#             r_ T..
+#         ____
+#             r_ F..
+#
+#
+# __ _______ __ _____
+#     heap _ ?
+#     heap.insert(10)
+#     heap.insert(-20)
+#     heap.insert(0)
+#     heap.insert(2)
+#     heap.insert(4)
+#     heap.insert(5)
+#     heap.insert(6)
+#     heap.insert(7)
+#     heap.insert(20)
+#     heap.insert(15)
+#
+#     heap.heapsort()
