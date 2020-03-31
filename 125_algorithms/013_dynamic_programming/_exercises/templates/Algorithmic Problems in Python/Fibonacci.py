2aa83bd063db16d96662931dfb5e578f905e9f59
syurskyi <sergejyurskyj@yahoo.com>
2020-03-31 10:28:11 -0700

    Update Fibonacci.py

--- 125_algorithms/013_dynamic_programming/_exercises/templates/Algorithmic Problems in Python/Fibonacci.py
+++ 125_algorithms/013_dynamic_programming/_exercises/templates/Algorithmic Problems in Python/Fibonacci.py
@@ -1,37 +1,34 @@
+# c_ Fibonacci
+#
+# 	___ -
+# 		memoizeTable _    # dict  # O(1)
+# 		?|0 _ 0
+# 		?|1 _ 1
+#
+# 	___ fibonacciMemoize n
+#
+# 		__ ? __ m..
+# 			r_ m..|?
+#
+# 		m..|?-1 _ f.. ?-1
+# 		m..|?-2 _ f.. ?-2
+#
+# 		calculatedNumber _ m..|?-1] + m..|?-2] # O(1)
+# 		m..|?] _ ?
+#
+# 		r_ c..
+#
+# 	___ naiveApproach n
+#
+# 		# f(n) _ f(n-1) + f(n-2) where f(0) _ 0 and f(1) _ 1
+#
+# 		__ ? __ 0: r_ 0
+# 		__ ? __ 1: r_ 1
+#
+# 		r_ ? ?-1 + ? ?-2
+#
+# __ _______ __ ______
+#
+# 	fibonacci _ ?
+# 	print( ?.n.. 100
 
-class Fibonacci:
-	
-	def __init__(self):
-		self.memoizeTable = {}  # O(1)
-		self.memoizeTable[0] = 0
-		self.memoizeTable[1] = 1
-	
-	def fibonacciMemoize(self, n):
-	
-		if n in self.memoizeTable:
-			return self.memoizeTable[n]
-			
-		self.memoizeTable[n-1] = self.fibonacciMemoize(n-1)
-		self.memoizeTable[n-2] = self.fibonacciMemoize(n-2)
-		
-		calculatedNumber = self.memoizeTable[n-1] + self.memoizeTable[n-2] # O(1)
-		self.memoizeTable[n] = calculatedNumber
-	
-		return calculatedNumber
-	
-	def naiveApproach(self, n):
-		
-		# f(n) = f(n-1) + f(n-2) where f(0) = 0 and f(1) = 1
-		
-		if n == 0: return 0
-		if n == 1: return 1
-		
-		return self.naiveApproach(n-1) + self.naiveApproach(n-2)
-		
-if __name__ == "__main__":
-
-	fibonacci = Fibonacci()
-	print( fibonacci.naiveApproach(100) )
-	
-		
-	
