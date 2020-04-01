e13e69aa9d58b481e6a07ce17612afeaf317e485
Grey <sergejyurskyj@yahoo.com>
2020-03-31 21:59:09 -0700

    Committed phone-numtemplates

--- 125_algorithms/012_backtracking/_exercises/templates/Cracking Coding Interviews - Mastering Algorithms/phone-num.py
+++ 125_algorithms/012_backtracking/_exercises/templates/Cracking Coding Interviews - Mastering Algorithms/phone-num.py
@@ -1,43 +1,43 @@
-# Given a string containing digits from 2-9 inclusive,
-# return all possible letter combinations that the number could represent.
-
-# Example:
-
-# Input: "23"
-# Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
-
-mapping = {
-	"2": "abc",
-	"3": "def",
-	"4": "ghi",
-	"5": "jkl",
-	"6": "mno",
-	"7": "pqrs",
-	"8": "tuv",
-	"9": "wxyz",
-}
-
-def combos(digits):
-	if len(digits) == 0:
-		return []
-
-	output = []
-
-	cur_digit = digits[0]
-	letters = mapping[cur_digit]
-
-	remaining_combos = combos(digits[1:])
-
-	for i in range(len(letters)):
-		letter = letters[i]
-
-		for word in remaining_combos:
-			output.append(letter + word)
-
-		if len(remaining_combos) == 0:
-			output.append(letter)
-
-	return output
-
-print(combos("23"))
-print(combos("2389"))
+# # Given a string containing digits from 2-9 inclusive,
+# # return all possible letter combinations that the number could represent.
+#
+# # Example:
+#
+# # Input: "23"
+# # Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+#
+# mapping _ |
+# 	"2" "abc"
+# 	"3" "def"
+# 	"4" "ghi"
+# 	"5" "jkl"
+# 	"6" "mno"
+# 	"7" "pqrs"
+# 	"8" "tuv"
+# 	"9" "wxyz"
+#
+#
+# ___ combos digits
+# 	__ le. ? __ 0
+# 		return []
+#
+# 	output _     # list
+#
+# 	cur_digit _ ?|0
+# 	letters _ m..|?
+#
+# 	remaining_combos _ c.. d..|1:
+#
+# 	___ i __ ra.. le. l..
+# 		l.. _ l..|?
+#
+# 		___ word __ r..
+# 			o__.ap.. l.. + w..
+#
+# 		__ le. r.. __ 0
+# 			o__.ap.. l..
+#
+# 	r_ o..
+#
+# print(?("23"))
+# print(?("2389"))
