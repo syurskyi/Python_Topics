0604bc1cd64984be9e468980df6f84012a182fd6
Grey <sergejyurskyj@yahoo.com>
2020-04-01 15:08:35 -0700

    Committed 2_matrix_min_sum templates

--- 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 4 Dynamic Programming/2_matrix_min_sum.py
+++ 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 4 Dynamic Programming/2_matrix_min_sum.py
@@ -1,88 +1,63 @@
-import numpy as np
-
-
-def run_recursive(array):
-    def inner(line, col):
-        if line < 0 or col < 0:
-            return 10**20
-        if line == 0 and col == 0:
-            return array[line, col]
-        return array[line, col] + min(inner(line - 1, col),
-                                      inner(line, col - 1))
-
-    n = array.shape[0]
-    return inner(n - 1, n - 1)
-
-
-def print_path(dp, array, line, col):
-    prev_dp = dp[line, col] - array[line, col]
-    if col - 1 >= 0 and prev_dp == dp[line, col - 1]:
-        print_path(dp, array, line, col - 1)
-    elif line - 1 >= 0 and prev_dp == dp[line - 1, col]:
-        print_path(dp, array, line - 1, col)
-    print(line, col)
-
-
-def run_dp(array):
-    dp = np.zeros_like(array)
-    n = dp.shape[0]
-    dp[0, 0] = array[0, 0]
-    for i in range(1, n):
-        dp[0, i] = dp[0, i - 1] + array[0, i]
-        dp[i, 0] = dp[i - 1, 0] + array[i, 0]
-    for i in range(1, n):
-        for j in range(1, n):
-            dp[i, j] = array[i, j] + min(dp[i - 1, j], dp[i, j - 1])
-    print_path(dp, array, n - 1, n - 1)
-    return dp[n - 1, n - 1]
-
-
-def run_dp_memory_optimized(array):
-    dp1 = np.zeros_like(array[0])
-    dp2 = np.zeros_like(array[0])
-    n = dp1.shape[0]
-    dp1[0] = array[0, 0]
-    for i in range(1, n):
-        dp1[i] = dp1[i - 1] + array[0, i]
-    for i in range(1, n):
-        dp2[0] = dp1[0] + array[i, 0]
-        for j in range(1, n):
-            dp2[j] = array[i, j] + min(dp1[j], dp2[j - 1])
-        dp1[:] = dp2
-    return dp1[n - 1]
-
-
-array = np.array([
-    [4, 3, 4, 31],
-    [1, 15, 9, 11],
-    [71, 13, 10, 6],
-    [21, 41, 51, 2]
-])
-
-print('Recursive:', run_recursive(array))
-print('DP:', run_dp(array))
-print('Memory optimized DP:', run_dp_memory_optimized(array))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# ______ n.. __ np
+#
+# ___ run_recursive array
+#     ___ inner line col
+#         __ l.. < 0 o. c.. < 0
+#             r_ 10**20
+#         __ l.. __ 0 an. co. __ 0
+#             r_ a..|?
+#         r_ a..|l.. c..| + mi. i.. l.. - 1 c..
+#                                       i.. l.. c.. - 1
+#
+#     n _ a__.s..|0
+#     r_ i.. ? - 1 ? - 1
+#
+#
+# ___ print_path dp array line col
+#     prev_dp _ d.|l.. c.. - a..|l.. c..
+#     __ c.. - 1 >_ 0 an. p_d. __ d.|l.. c.. - 1
+#         p_p.. d. a.. l c.. - 1
+#     ____ l.. - 1 >_ 0 an. p_d. __ d.|l.. - 1 c..
+#         p_p.. d. a.., l.. - 1 c..
+#     print l.. c..
+#
+#
+# ___ run_dp array
+#     dp _ __.z_l.. ?
+#     n _ ?.s..|0
+#     d.|0, 0 _ ?|0, 0
+#     ___ i __ ra.. 1 n
+#         d.|0 ? _ d.|0 ? - 1 + ?|0 ?
+#         d.|? 0 _ d.|? - 1 0 + ?|? 0
+#     ___ i __ ra.. 1 n
+#         ___ j __ ra.. 1 n
+#             d.|? ? _ ?|? ? + mi. d.|? - 1 ?| d.|? ? - 1
+#     print_path d. ? n - 1 n - 1
+#     r_ d.|n - 1, n - 1
+#
+#
+# ___ run_dp_memory_optimized array
+#     dp1 _ __.z_l.. ?|0
+#     dp2 _ __.z_l..?|0
+#     n _ __1.s..|0
+#     __1|0 _ ?|0 0
+#     ___ i in range(1, n):
+#         __1|? _ __1|? - 1 + ?|0 ?
+#     ___ i __ ra.. 1 n
+#         __2|0 _ __1|0 + ?|? 0
+#         ___ j __ ra.. 1 n
+#             __2|? _ ?|? ? + mi. __1|? __2|? - 1
+#         __1|; _ __2
+#     r_ __1|n - 1
+#
+#
+# array _ __.a..||
+#     [4, 3, 4, 31],
+#     [1, 15, 9, 11],
+#     [71, 13, 10, 6],
+#     [21, 41, 51, 2]
+# ||
+#
+# print('Recursive:', r_r.. ?
+# print('DP:', r_d. ?
+# print('Memory optimized DP:', r_d_m_o.. ?
