07e4f13cd18d4d11fd97e1405cf778a5c8040746
Grey <sergejyurskyj@yahoo.com>
2020-04-01 13:26:46 -0700

    Committed hashing templates

--- 125_algorithms/011_hashing/_exercises/templates/80. Hash project 1.py
+++ 125_algorithms/011_hashing/_exercises/templates/80. Hash project 1.py
@@ -1,20 +1,20 @@
-class AlgoHashTable:
-
-    def __init__(self, size):
-        self.size = size
-        self.hash_table = [[] for _ in range(self.size)]
-
-    def create_buckets(self):
-        pass
-
-    def set_val(self, key, value):
-        pass
-
-    def get_val(self, key):
-        pass
-
-    def __str__(self):
-        return "".join(str(item) for item in self.hash_table)
-
-hash_table = AlgoHashTable(256)
-print(hash_table)
+# c_ AlgoHashTable
+#
+#     ___ - size
+#         ?  ?
+#         hash_table = || ___ _ __ ra.. ?    # _ original symbol
+#
+#     ___ create_buckets
+#         p..
+#
+#     ___ set_val key value
+#         p..
+#
+#     ___ get_val key
+#         p..
+#
+#     ___ -s
+#         r_ "".jo.. st. i.. ___ i.. __ h_t..
+#
+# hash_table = ?(256)
+# print(hash_table)
