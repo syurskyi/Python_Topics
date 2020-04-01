b7553de054a93511945640bf37b09b8a01cafa52
Grey <sergejyurskyj@yahoo.com>
2020-03-31 22:11:42 -0700

    Committed unique-path-bottom-up templates

--- 125_algorithms/013_dynamic_programming/examples/Cracking Coding Interviews - Mastering Algorithms/unique-path-bottom-up.py
+++ 125_algorithms/013_dynamic_programming/examples/Cracking Coding Interviews - Mastering Algorithms/unique-path-bottom-up.py
@@ -1,36 +1,36 @@
-# Example 1:
-
-# Input: m = 3, n = 2
-# Output: 3
-# Explanation:
-# From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
-# 1. Right -> Right -> Down
-# 2. Right -> Down -> Right
-# 3. Down -> Right -> Right
-
-
-
-# Example 2:
-
-# Input: m = 7, n = 3
-# Output: 28
-
-# [
-# 	[1, 1, 1],
-# 	[1, 2, 3],
-# ]
-
-def unique_paths(m, n):
-	arr = [[0 for j in range(m)] for i in range(n)]
-
-	for i in range(n):
-		for j in range(m):
-			if i == 0 or j == 0:
-				arr[i][j] = 1
-			else:
-				arr[i][j] = arr[i - 1][j] + arr[i][j - 1]
-
-	return arr[n - 1][m - 1]
-
-print(unique_paths(3, 2))
-print(unique_paths(7, 3))
+# # Example 1:
+#
+# # Input: m = 3, n = 2
+# # Output: 3
+# # Explanation:
+# # From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+# # 1. Right -> Right -> Down
+# # 2. Right -> Down -> Right
+# # 3. Down -> Right -> Right
+#
+#
+#
+# # Example 2:
+#
+# # Input: m = 7, n = 3
+# # Output: 28
+#
+# # [
+# # 	[1, 1, 1],
+# # 	[1, 2, 3],
+# # ]
+#
+# ___ unique_paths m n
+# 	arr _ ||0 ___ j __ ra.. ?| ___ i __ ra.. ?
+#
+# 	___ i __ ra.. ?2
+# 		___ j __ ra.. ?1
+# 			__ ? __ 0 o. ? __ 0
+# 				a..|? |? _ 1
+# 			____
+# 				a..|? |? _ a..|? - 1 |? + a..|? |? - 1
+#
+# 	r_ a..|?2 - 1 |?1 - 1
+#
+# print(? 3, 2
+# print(? 7, 3
