2684c662696702a7fe4b362b42e3e14f85f9a0ab
Grey <sergejyurskyj@yahoo.com>
2020-04-01 14:09:38 -0700

    Committed 83. Hash project 4 Build search method templates

--- 125_algorithms/011_hashing/_exercises/templates/83. Hash project 4 Build search method.py
+++ 125_algorithms/011_hashing/_exercises/templates/83. Hash project 4 Build search method.py
@@ -1,46 +1,46 @@
-class AlgoHashTable:
-
-    def __init__(self, size):
-        self.size = size
-        self.hash_table = self.create_buckets()
-
-    def create_buckets(self):
-        return [[] for _ in range(self.size)]
-
-    def set_val(self, key, value):
-        hashed_key = hash(key)%self.size
-        bucket = self.hash_table[hashed_key]
-        found_key = False
-        for index, record in enumerate(bucket):
-            record_key, record_value = record
-            if record_key == key:
-                found_key = True
-                break
-        if found_key:
-            bucket[index] = (key, value)
-        else:
-            bucket.append((key, value))
-
-    def get_val(self, key):
-        hashed_key = hash(key)%self.size
-        bucket = self.hash_table[hashed_key]
-        found_key = False
-        for index, record in enumerate(bucket):
-            record_key, record_value = record
-            if record_key == key:
-                found_key = True
-                break
-        if found_key:
-            return record_value
-        else:
-            return "No record found with that email address"
-
-    def __str__(self):
-        return "".join(str(item) for item in self.hash_table)
-
-hash_table = AlgoHashTable(256)
-hash_table.set_val('mashrur@example.com',{'first_name':'Mashrur','last_name':'Hossain'})
-hash_table.set_val('evgeny@example.com',{'first_name':'Evgeny','last_name':'CoderElite'})
-hash_table.set_val('tyrion@example.com',{'first_name':'Tyrion','last_name':'BadAdvice'})
-print(hash_table)
-print(hash_table.get_val('tyrion@example.com'))
+# c_ AlgoHashTable
+#
+#     ___ - size
+#         ? ?
+#         hash_table _ c..
+#
+#     ___ create_buckets
+#         r_ || ___ _ __ ra.. s..    # _original symbol
+#
+#     ___ set_val key value
+#         hashed_key _ ha.. k.. % s..
+#         bucket _ h_t..|?
+#         found_key _ F..
+#         ___ index record __ en.. b..
+#             record_key record_value _ r..
+#             __ r_k.. __ key
+#                 found_key _ T..
+#                 b..
+#         __ f_k..
+#             b..|i.. _ ? ?
+#         ____
+#             b__.ap.. ? ?
+#
+#     ___ get_val key
+#         hashed_key _ ha.. k.. % s..
+#         bucket _ h_t..|?
+#         found_key _ F..
+#         ___ index record __ en.. b..
+#             record_key, record_value _ r..
+#             __ r_k.. __ k..
+#                 f_k.. _ T..
+#                 b..
+#         __ f_k..
+#             r_ ?
+#         ____
+#             r_ "No record found with that email address"
+#
+#     ___ -s
+#         r_ "".jo.. st. i.. ___ i.. __ h_t..
+#
+# hash_table _ ? 256
+# ?.s..('mashrur@example.com',{'first_name':'Mashrur','last_name':'Hossain'})
+# ?.s..('evgeny@example.com',{'first_name':'Evgeny','last_name':'CoderElite'})
+# ?.s..('tyrion@example.com',{'first_name':'Tyrion','last_name':'BadAdvice'})
+# print(?)
+# print(?.g.. ('tyrion@example.com'))
