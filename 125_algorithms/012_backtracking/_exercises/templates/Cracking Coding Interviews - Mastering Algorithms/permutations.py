536abad4e2fcb0ddb7d3d13e72fd212a3173d9e1
Grey <sergejyurskyj@yahoo.com>
2020-03-31 21:53:37 -0700

    Committed permutations templates

--- 125_algorithms/012_backtracking/_exercises/templates/Cracking Coding Interviews - Mastering Algorithms/permutations.py
+++ 125_algorithms/012_backtracking/_exercises/templates/Cracking Coding Interviews - Mastering Algorithms/permutations.py
@@ -1,35 +1,35 @@
-import copy
-# Given a collection of distinct integers, return all possible permutations.
-
-# Example:
-
-# Input: [1,2,3]
-# Output:
-# [
-#   [1,2,3],
-#   [1,3,2],
-#   [2,1,3],
-#   [2,3,1],
-#   [3,1,2],
-#   [3,2,1]
-# ]
-
-def permutation_recursive(nums, path):
-	if len(nums) == 0:
-		return [path]
-
-	output = []
-
-	for i in range(len(nums)):
-		copy_path = copy.deepcopy(path)
-		copy_path.append(nums[i])
-		permutations = permutation_recursive(nums[:i] + nums[i + 1:], copy_path)
-
-		output += permutations
-
-	return output
-
-def permute(nums):
-	return permutation_recursive(nums, [])
-
-print(permute([1, 2, 3, 4, 5, 6]))
+# ______ co..
+# # Given a collection of distinct integers, return all possible permutations.
+#
+# # Example:
+#
+# # Input: [1,2,3]
+# # Output:
+# # [
+# #   [1,2,3],
+# #   [1,3,2],
+# #   [2,1,3],
+# #   [2,3,1],
+# #   [3,1,2],
+# #   [3,2,1]
+# # ]
+#
+# ___ permutation_recursive nums path
+# 	__ le. ? __ 0
+# 		r_ |?
+#
+# 	output _    # list
+#
+# 	___ i __ ra.. le. ?
+# 		copy_path _ co__.d_c.. ?
+# 		?.ap.. n..|?
+# 		permutations _ ? n..|:? + n..|? + 1: ?
+#
+# 		o.. +_ ?
+#
+# 	r_ ?
+#
+# ___ permute nums
+# 	r_ ? ? ||
+#
+# print(?([1, 2, 3, 4, 5, 6]))
