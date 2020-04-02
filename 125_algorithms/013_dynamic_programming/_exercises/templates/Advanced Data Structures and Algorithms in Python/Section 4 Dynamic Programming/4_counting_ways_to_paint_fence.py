2436e8aa94122e9833614baff64944cde09ed734
Grey <sergejyurskyj@yahoo.com>
2020-04-01 18:11:45 -0700

    Committed 4_counting_ways_to_paint_fence templates

--- 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 4 Dynamic Programming/4_counting_ways_to_paint_fence.py
+++ 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 4 Dynamic Programming/4_counting_ways_to_paint_fence.py
@@ -1,30 +1,30 @@
-import numpy as np
-
-
-def dp_2_states(n, k):
-    dp = np.zeros((n, k))
-    dp[0, :] = 1
-    dp[1, :] = k
-    for i in range(2, n):
-        for c in range(k):
-            s = 0
-            for c1 in range(k):
-                if c1 != c:
-                    s += dp[i - 2, c1] + dp[i - 1, c1]
-            dp[i, c] = s
-    return sum(dp[n - 1, :])
-
-
-def dp_1_state(n, k):
-    dp = np.zeros(n)
-    dp[0] = k
-    dp[1] = k*k
-    for i in range(2, n):
-        dp[i] = (k - 1) * (dp[i - 1] + dp[i - 2])
-    return dp[n - 1]
-
-n = 15
-k = 6
-print(dp_2_states(n, k))
-print(dp_1_state(n, k))
-
+# ______ n.. __ np
+#
+#
+# ___ dp_2_states n k
+#     dp _ __.z.. ? ?
+#     ?|0 ; _ 1
+#     ?|1 ; _ k
+#     ___ i __ ra.. 2 n
+#         ___ c __ ra.. k
+#             s _ 0
+#             ___ c1 __ ra.. k
+#                 __ c1 !_ c
+#                     s +_ d.|? - 2, _1 + d.|? - 1 _1
+#             d. ? c _ s
+#     r_ su. d.|n - 1 ;
+#
+#
+# ___ dp_1_state n k
+#     dp _ __.z.. ?
+#     ?|0 _ ?
+#     ?|1 _ k*k
+#     ___ i __ ra.. 2 n
+#         d.|? _ |k - 1| * |d.|? - 1| + d.|? - 2
+#     r_ d.|? - 1
+#
+# n _ 15
+# k _ 6
+# print _2_ ?
+# print _1_ ?
+#
