b2a2493b29d0edb4875087fee5734ec19c592ed6
Grey <sergejyurskyj@yahoo.com>
2020-04-01 20:59:56 -0700

    Committed 1_counting_digital_sums templates

--- 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 5 Advanced Dynamic Programming/1_counting_digital_sums.py
+++ 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 5 Advanced Dynamic Programming/1_counting_digital_sums.py
@@ -1,85 +1,85 @@
-from random import randint
-import numpy as np
-
-MOD = 666013
-
-def naive(n, d):
-
-    def inner(number):
-        if len(number) == n:
-            digital_sum = sum(int(digit) for digit in number)
-            count = 0
-            if digital_sum % d == 0:
-                count += 1
-            return count
-        count = 0
-        for digit in range(10):
-            count += inner(number + str(digit))
-        return count
-
-    return inner('')
-
-
-def dp_naive(n, d):
-    dp = np.zeros((n, d))
-    for i in range(10):
-        dp[0, i % d] += 1  # 0 = 1 digit numbers
-
-    for i in range(1, n):
-        for j in range(d):
-            for k in range(10):
-                dp[i, j] += dp[i - 1, (j - k) % d]
-            dp[i, j] %= MOD
-
-    return dp[n - 1, 0]
-
-
-def dp_efficient(n, d):
-    log2n = int(np.log2(n))
-    dp = np.zeros((log2n + 1, d))
-    for i in range(10):
-        dp[0, i % d] += 1
-    for i in range(1, log2n + 1):
-        for k1 in range(d):
-            for k2 in range(d):
-                h = (k1 + k2) % d
-                dp[i, h] += dp[i - 1, k1] * dp[i - 1, k2]
-                dp[i, h] %= MOD
-
-    if 2 ** log2n == n:
-        return dp[log2n, 0]
-    binary = '{0:b}'.format(n)[::-1]
-    results = None
-    for i, b in enumerate(binary):
-        if b == '1' and results is None:
-            results = dp[i, :]
-        elif b == '1':
-            new_results = np.zeros_like(results)
-            for k1 in range(d):
-                for k2 in range(d):
-                    h = (k1 + k2) % d
-                    new_results[h] += dp[i, k1] * results[k2]
-                    new_results[h] %= MOD
-            results = new_results
-    return results[0]
-
-
-print(naive(6, 3))
-print(dp_naive(6, 3))
-print(dp_efficient(6, 3))
-print(dp_efficient(10 ** 18, 100))
-
-for tests in range(100):
-    array = []
-    n = randint(1, 1000)
-    d = randint(1, 100)
-    with_naive_dp = dp_naive(n, d)
-    with_efficient_dp = dp_efficient(n, d)
-    assert with_naive_dp == with_efficient_dp, \
-        'naive dp={}, efficient dp={},\nn={},\nd={}'.format(
-            with_naive_dp,
-            with_efficient_dp,
-            n,
-            d
-        )
-
+# ____ ra.. ______ r_i..
+# ______ n. __ np
+#
+# MOD _ 666013
+#
+# ___ naive n d
+#
+#     ___ inner number
+#         __ le. ? __ n
+#             digital_sum _ su. __. digit ___ ? __ n..
+#             count _ 0
+#             __ d_s. % d __ 0
+#                 c.. +_ 1
+#             r_ co..
+#         co.. _ 0
+#         ___ digit __ ra.. 10
+#             c.. +_ i.. n.. + st. d..
+#         r_ ?
+#
+#     r_ ? ''
+#
+#
+# ___ dp_naive n d
+#     dp _ __.z.. ? ?
+#     ___ i __ ra.. 10
+#         d.|0 ? % d +_ 1  # 0 _ 1 digit numbers
+#
+#     ___ i __ ra.. 1  n
+#         ___ j __ ra.. d
+#             ___ k __ ra.. 10
+#                 d.|? ? +_ d.|? - 1 ? - ? % d
+#             d.|? ? %_ M..
+#
+#     r_ ?|? - 1 0
+#
+#
+# ___ dp_efficient n d
+#     log2n _ in. __.__2 n
+#     dp _ __.z.. __2_ + 1 d
+#     ___ i __ ra.. 10
+#         d.|0 i % d +_ 1
+#     ___ i __ ra.. 1 __2_ + 1
+#         ___ k1 __ ra.. d
+#             ___ k2 __ ra.. d
+#                 h _ _1 + _2 % d
+#                 d.|i h +_ d.|i - 1 _1 * d.|i - 1 _2
+#                 d.|i h %_ M..
+#
+#     __ 2 ** __2_ __ n
+#         r_ d.|__2_ 0
+#     binary _ ' 0:b'.f.. ?|;;-1
+#     results _ N..
+#     ___ i, b __ en.. b..
+#         __ b __ '1' an. r.. __ N..
+#             r.. _ d.|? ;
+#         ____ b __ '1'
+#             new_results _ __.z_l.. r...
+#             ___ k1 __ ra.. d
+#                 ___ k2 __ ra.. d
+#                     h _ _1 + _2 % d
+#                     n_r..|h +_ d.|i _1 * r..|_2
+#                     n_r..|h %_ M..
+#             r.. _ n_r..
+#     r_ ?|0
+#
+#
+# print(n.. 6, 3
+# print(d_n.. 6, 3
+# print(d_e.. 6, 3
+# print(d_e.. 10 ** 18, 100
+#
+# ___ tests __ ra.. 100
+#     array _     # list
+#     n _ r_i.. 1 1000
+#     d _ r_i.. 1 100
+#     with_naive_dp _ d_n.. ? ?
+#     with_efficient_dp _ d_e.. ? ?
+#     as.. w.._n_d. __ w_e_d. \
+#         'naive dp_@, efficient dp_@,\nn_@,\nd_@'.f..(
+#             w_n_d.
+#             w_e_d
+#             n
+#             d
+#         )
+#
