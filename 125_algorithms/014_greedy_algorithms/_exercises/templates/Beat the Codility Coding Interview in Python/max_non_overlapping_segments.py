d117f4fa963eb01b70635261a1c2058241bd34e9
Grey <sergejyurskyj@yahoo.com>
2020-04-02 19:56:59 -0700

    Committed max_non_overlapping_segments template

--- 125_algorithms/014_greedy_algorithms/_exercises/templates/Beat the Codility Coding Interview in Python/max_non_overlapping_segments.py
+++ 125_algorithms/014_greedy_algorithms/_exercises/templates/Beat the Codility Coding Interview in Python/max_non_overlapping_segments.py
@@ -1,22 +1,22 @@
-# This is the solution for Greedy algorithms > MaxNonoverlappingSegments
-# The problem is equivalent to the Activity Selection Problem,
-# where you have to choose the maximum non overlapping tasks.
+# # This is the solution for Greedy algorithms > MaxNonoverlappingSegments
+# # The problem is equivalent to the Activity Selection Problem,
+# # where you have to choose the maximum non overlapping tasks.
+# #
+# # This is marked as PAINLESS difficulty
 #
-# This is marked as PAINLESS difficulty
-
-def solution(A, B):
-    last_end_segment = -1
-    chosen_count = 0
-    for i in range(len(A)):
-        if A[i] > last_end_segment:
-            chosen_count += 1
-            last_end_segment = B[i]
-    return chosen_count
-
-
-
-print(solution([1, 3, 7, 9, 1], [5, 6, 8, 9, 10]))
-
-print(solution([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]))
-
-print(solution([], []))
+# ___ solution A B
+#     last_end_segment _ -1
+#     chosen_count _ 0
+#     ___ i __ ra..  le.. ?
+#         __ ?|? > l...
+#             c.. +_ 1
+#             l.. _ ?|?
+#     r_ c..
+#
+#
+#
+# print(? 1, 3, 7, 9, 1], [5, 6, 8, 9, 10
+#
+# print(? 1, 3, 5, 7, 9], [2, 4, 6, 8, 10
+#
+# print(?([], []))
