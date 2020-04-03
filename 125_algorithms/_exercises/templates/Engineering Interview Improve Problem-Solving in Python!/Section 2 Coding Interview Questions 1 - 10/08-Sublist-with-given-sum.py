7cd8e84324bf5937c8c5b9d858b55f68a6f038a7
Grey <sergejyurskyj@yahoo.com>
2020-04-02 20:56:49 -0700

    Committed 08-Sublist-with-given-sum template

--- 125_algorithms/_exercises/templates/Engineering Interview Improve Problem-Solving in Python!/Section 2 Coding Interview Questions 1 - 10/08-Sublist-with-given-sum.py
+++ 125_algorithms/_exercises/templates/Engineering Interview Improve Problem-Solving in Python!/Section 2 Coding Interview Questions 1 - 10/08-Sublist-with-given-sum.py
@@ -1,19 +1,19 @@
-# [1,2,5,4,5], 5 => 1 3
-
-def sublist_given_sum(l, s):
-    for i in range(len(l)):
-        interval_sum = l[i]
-        if interval_sum == s:
-            print(i,i)
-            return True
-        for j in range(i + 1, len(l)):
-            interval_sum += l[j]
-            if interval_sum == s:
-                print(i,j)
-                return True
-            elif s < interval_sum:
-                break
-    print(-1)
-    return False
-
-sublist_given_sum([1,2,5,4,5], 5)
+# # [1,2,5,4,5], 5 => 1 3
+#
+# ___ sublist_given_sum l s
+#     ___ i __ ra.. le. ?
+#         interval_sum _ ?|?
+#         __ ? __ ?
+#             print ? ?
+#             r_ T..
+#         ___ j __ ra.. ? + 1 le. ?
+#             i.. +_ ?|?
+#             __ i.. __ ?
+#                 print ? ?
+#                 r_ T..
+#             ____ ? < i..
+#                 b..
+#     print -1
+#     r_ F..
+#
+# ? ([1,2,5,4,5], 5)
