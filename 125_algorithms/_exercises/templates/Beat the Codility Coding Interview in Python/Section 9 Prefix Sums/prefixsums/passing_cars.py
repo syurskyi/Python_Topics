956590286b960212f078e75505b6791137c02d5c
Grey <sergejyurskyj@yahoo.com>
2020-04-02 20:08:31 -0700

    Committed passing_cars template

--- 125_algorithms/_exercises/templates/Beat the Codility Coding Interview in Python/Section 9 Prefix Sums/prefixsums/passing_cars.py
+++ 125_algorithms/_exercises/templates/Beat the Codility Coding Interview in Python/Section 9 Prefix Sums/prefixsums/passing_cars.py
@@ -1,20 +1,20 @@
-# This is the solution for Prefix Sums > Passing Cars
+# # This is the solution for Prefix Sums > Passing Cars
+# #
+# # This is marked as PAINLESS difficulty
 #
-# This is marked as PAINLESS difficulty
-
-def solution(A):
-    suffix_sum = [0] * (len(A) + 1)
-    for i in range(len(A) - 1, -1, -1):
-        suffix_sum[i] = A[i] + suffix_sum[i + 1]
-
-    count = 0
-    for i in range(len(A)):
-        if A[i] == 0:
-            count += suffix_sum[i]
-        if count > 1000000000:
-            return -1
-
-    return count
-
-
-print(solution([0, 1, 0, 1, 1]))
+# ___ solution A
+#     suffix_sum _ |0 * le. ? + 1
+#     ___ i __ ra.. le. ? - 1 -1 -1
+#         s..|? = ?|? + s..|? + 1
+#
+#     count _ 0
+#     ___ i __ ra.. le. ?
+#         __ ?|? __ 0
+#             c.. +_ s..|?
+#         __ c.. > 1000000000:
+#             r_ -1
+#
+#     r_ ?
+#
+#
+# print(? 0, 1, 0, 1, 1
