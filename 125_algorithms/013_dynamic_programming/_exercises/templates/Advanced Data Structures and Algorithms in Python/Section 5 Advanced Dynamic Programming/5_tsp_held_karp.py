22314a7bc6484907666352d4cb12e89a640dbb91
Grey <sergejyurskyj@yahoo.com>
2020-04-02 18:32:30 -0700

    Committed 5_tsp_held_karp template

--- 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 5 Advanced Dynamic Programming/5_tsp_held_karp.py
+++ 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 5 Advanced Dynamic Programming/5_tsp_held_karp.py
@@ -1,65 +1,65 @@
-______ numpy as np
-
-inf = 10 ** 18
-
-
-def generate_d(n):
-    d = np.zeros((n, n), dtype=np.int64)
-    for i in range(n):
-        for j in range(i+1, n):
-            d[i, j] = np.random.randint(1, 10)
-            if d[i, j] == 0:
-                d[i, j] = inf
-            d[j, i] = d[i, j]
-    return d
-
-
-def brute_force(n, d):
-
-    def inner(current_node, current_length, visited=set()):
-        if len(visited) == n - 1:
-            return current_length + d[current_node, 0]
-
-        best = inf
-        for i in range(1, n):
-            if i not in visited:
-                best = min(best, inner(i,
-                                       current_length + d[current_node, i],
-                                       visited | {i}))
-        return best
-
-    return inner(0, 0)
-
-
-def run_dp(n, d):
-    dp = np.ones((2 ** n, n)) * inf
-    # dp[S, c] = sum of the minimum sum tour that visits all cities
-    #            corresponding to the indexes of the set bits in S [...]
-    # S = 10011 => 0, 1, 4
-    for c in range(1, n):
-        dp[1 << c, c] = d[0, c] # 1 << 3 => 1000
-
-    for S in range(2**n - 1): # 111...1110
-        for c in range(1, n):
-            if S & (1 << c) != 0:
-                S_no_c = S - (1 << c)
-                for c1 in range(1, n):
-                    if S & (1 << c1) != 0:
-                        dp[S, c] = min(dp[S, c], dp[S_no_c, c1] + d[c1, c])
-
-    final_state = 2**n - 2
-    best = inf
-    for c in range(1, n):
-        best = min(best, dp[final_state, c] + d[c, 0])
-    return best
-
-
-
-for i in range(10):
-    n = np.random.randint(2, 5)
-    d = generate_d(n)
-
-    print(d)
-    print('brute force search :', brute_force(n, d))
-    print('dynamic programming:', run_dp(n, d))
-    print('-' * 20)
+# ______ n.. __ __
+#
+# inf _ 10 ** 18
+#
+#
+# ___ generate_d n
+#     d _ __.z.. ?2 ?2 dtype_np.int64
+#     ___ i __ ra.. ?2
+#         ___ j __ ra.. ?+1, ?2
+#             ?|? ? _ __.ra...r_i.. 1 1.
+#             __ ?|? ? __ 0
+#                 ?|? ? _ inf
+#             ?|? ? _ d|? ?
+#     r_ ?
+#
+#
+# ___ brute_force n d
+#
+#     ___ inner current_node current_length visited_set
+#         __ le. v.. __ ? - 1
+#             r_ c_l.. + ?|c_n.. 0
+#
+#         best _ inf
+#         ___ i __ ra.. 1 ?
+#             __ i no. __ v..
+#                 best _ mi. b.. i.. ?
+#                                        c_l.. + ?|c_n.. ?
+#                                        v.. | |?
+#         r_ ?
+#
+#     r_ ? 0 0
+#
+#
+# ___ run_dp n d
+#     dp _ __.o.. 2 ** ? ? * inf
+#     # dp[S, c] _ sum of the minimum sum tour that visits all cities
+#     #            corresponding to the indexes of the set bits __ S [...]
+#     # S _ 10011 _> 0, 1, 4
+#     ___ c __ ra.. 1 n
+#         ?|1 << ? ? _ ?|0 ? # 1 << 3 _> 1000
+#
+#     ___ S __ ra.. 2**? - 1 # 111...1110
+#         ___ c __ ra.. 1 ?
+#             __ S & |1 << ? !_ 0
+#                 S_no_c _ S - |1 << ?
+#                 ___ c1 __ ra.. 1 ?
+#                     __ S & 1 << _1 !_ 0
+#                         d.|S c _ mi. d.|S c d.[S_no_c _1 + ?|_1 c
+#
+#     final_state _ 2**? - 2
+#     best _ in.
+#     ___ c __ ra.. 1 ?
+#         best _ mi. b.. ?|f.._s.. ?| + ?|? 0
+#     r_ ?
+#
+#
+#
+# ___ i __ ra.. 10
+#     n _ __.ra__.r__i.. 2 5
+#     d _ g_d ?
+#
+#     print(d)
+#     print('brute force search :', b.. n d
+#     print('dynamic programming:', r_d. n d
+#     print('-' * 20)
