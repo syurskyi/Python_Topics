96e68baef56800800ef032d10b081c19d9d3a361
Grey <sergejyurskyj@yahoo.com>
2020-04-02 21:06:10 -0700

    Committed 11-Stock-buy-and-sell template

--- 125_algorithms/_exercises/templates/Engineering Interview Improve Problem-Solving in Python!/Section 3 Coding Interview Questions 10 - 18/13. Stock buy and sell/11-Stock-buy-and-sell.py
+++ 125_algorithms/_exercises/templates/Engineering Interview Improve Problem-Solving in Python!/Section 3 Coding Interview Questions 10 - 18/13. Stock buy and sell/11-Stock-buy-and-sell.py
@@ -1,19 +1,19 @@
-# [23,18,15,12,34,55,22,44] => (1 3) (4 5)
-# Day when price drops (price < previous day) => sell the previous day
-# BUY when price > previous day => BUY the previous day
-
-def buy_sell(prices):
-    buy = 0
-    previous_price = 10000 #max int
-    for index, price in enumerate(prices):
-        if price < previous_price:
-            if buy:
-                print(buy, index - 1)
-                buy = 0
-        elif previous_price < price:
-            if not buy:
-                buy = index - 1
-        previous_price = price
-    if buy:
-        print(buy, len(prices) - 1)
-buy_sell([23,18,15,12,34,55,22,44])
+# # [23,18,15,12,34,55,22,44] => (1 3) (4 5)
+# # Day when price drops (price < previous day) => sell the previous day
+# # BUY when price > previous day => BUY the previous day
+#
+# ___ buy_sell prices
+#     buy _ 0
+#     previous_price _ 10000 #max int
+#     ___ index, price __ en.. ?
+#         __ ? < ?
+#             __ ?
+#                 print ? ? - 1
+#                 b.. _ 0
+#         ___ ? < ?
+#             __ no. ?
+#                 b.. _ ? - 1
+#         ? _ ?
+#     __ ?
+#         print(? le. ?| - 1)
+# ?([23,18,15,12,34,55,22,44])
