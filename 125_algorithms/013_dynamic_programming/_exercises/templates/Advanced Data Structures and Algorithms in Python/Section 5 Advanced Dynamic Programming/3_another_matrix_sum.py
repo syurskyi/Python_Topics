c825c5b55281ac4115f04e7978f45ba01049b771
Grey <sergejyurskyj@yahoo.com>
2020-04-02 18:22:03 -0700

    Committed 3_another_matrix_sum template

--- 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 5 Advanced Dynamic Programming/3_another_matrix_sum.py
+++ 125_algorithms/013_dynamic_programming/_exercises/templates/Advanced Data Structures and Algorithms in Python/Section 5 Advanced Dynamic Programming/3_another_matrix_sum.py
@@ -1,78 +1,77 @@
-______ numpy as np
-
-MOD = 666013
-
-
-def run_classic_dp(n, m):
-    dp = np.zeros((m+1, n, n))
-    dp[0, 0, 0] = 1
-    for k in range(1, m+1):
-        for i in range(n):
-            for j in range(n):
-                n1 = dp[k-1, i-1, j] if i-1 >= 0 else 0
-                n2 = dp[k-1, i, j-1] if j-1 >= 0 else 0
-                n3 = dp[k-1, i+1, j] if i+1 < n else 0
-                n4 = dp[k-1, i, j+1] if j+1 < n else 0
-                dp[k, i, j] = n1 + n2 + n3 + n4
-                dp[k, i, j] %= MOD
-    return dp[m, n-1, n-1]
-
-
-def run_new_dp(n, m):
-    log2m = int(np.log2(m))
-    dp = np.zeros((log2m+1, n, n, n, n))
-    for i in range(n):
-        for j in range(n):
-            if i + 1 < n:
-                dp[0, i, j, i + 1, j] = 1
-            if i - 1 >= 0:
-                dp[0, i, j, i - 1, j] = 1
-            if j + 1 < n:
-                dp[0, i, j, i, j + 1] = 1
-            if j - 1 >= 0:
-                dp[0, i, j, i, j - 1] = 1
-
-    for k in range(1, log2m + 1):
-        for i1 in range(n):
-            for j1 in range(n):
-                for i2 in range(n):
-                    for j2 in range(n):
-                        for p in range(n):
-                            for q in range(n):
-                                dp[k, i1, j1, i2, j2] += dp[k-1, i1, j1, p, q] *\
-                                                         dp[k-1, p, q, i2, j2]
-                                dp[k, i1, j1, i2, j2] %= MOD
-
-    if 2 ** log2m == m:
-        return dp[log2m, 0, 0, n - 1, n - 1]
-    binary = '{0:b}'.format(m)[::-1]
-    results = None
-    for i, b in enumerate(binary):
-        if b == '1' and results is None:
-            results = dp[i, 0, 0, :, :]
-        elif b == '1':
-            new_results = np.zeros_like(results)
-            for k1 in range(n):
-                for k2 in range(n):
-                    for p in range(n):
-                        for q in range(n):
-                            new_results[k1, k2] += results[p, q] *\
-                                                   dp[i, p, q, k1, k2]
-                            new_results[k1, k2] %= MOD
-            results = new_results
-    return results[n - 1, n - 1]
-
-print(run_classic_dp(6, 100))
-print(run_new_dp(6, 100))
-print(run_new_dp(5, 10 ** 18))
-
-for n in range(2, 5):
-    for m in range(2, 100):
-        classic = run_classic_dp(n, m)
-        new = run_new_dp(n, m)
-        assert classic == new, 'classic={}, new={}, n={}, m={}'.format(
-            classic,
-            new,
-            n,
-            m
-        )
+# ______ n.. __ np
+#
+# MOD _ 666013
+#
+# ___ run_classic_dp n m
+#     dp _ __.z.. ?2+1, ?1, ?1))
+#     ?[0, 0, 0] _ 1
+#     ___ k __ ra..(1, ?2+1):
+#         ___ i __ ra..(?1):
+#             ___ j __ ra.. ?1
+#                 n1 _ ?|?-1, ?-1, ? __ ?2-1 >_ 0 ____ 0
+#                 n2 _ ?|?-1, ?, ?-1 __ ?3-1 >_ 0 ____ 0
+#                 n3 _ ?|?-1, ?+1, ? __ ?+1 < ?1 ____ 0
+#                 n4 _ ?|?-1, ?, ?+1 __ ?3+1 < ?1 ____ 0
+#                 ?|? ? ? _ ? + ? + ? + ?
+#                 ?|? ? ? %_ M..
+#     r_ ?|?2, ?1-1, ?1-1
+#
+#
+# ___ run_new_dp n m
+#     log2m _ in. __.__2 ?2
+#     dp _ __.z.. __2_+1, ?1 ?1 ?1 ?1
+#     ___ i __ ra.. ?1
+#         ___ j __ ra.. ?1
+#             __ i + 1 < ?1
+#                 ?|0 ? ? ?1 + 1 ?2| _ 1
+#             __ ?1 - 1 >_ 0
+#                 ?|0, ? ? ?1 - 1 ?2| _ 1
+#             __ ?2 + 1 < ?1
+#                 ?|0 ?1 ?2 ?1 ?2 + 1| _ 1
+#             __ ?2 - 1 >_ 0:
+#                 ?|0 ? ? ?1 ?2 - 1| _ 1
+#
+#     ___ k __ ra.. 1 __2_ + 1
+#         ___ i1 __ ra.. ?1
+#             ___ j1 __ ra.. ?1
+#                 ___ i2 __ ra.. ?1
+#                     ___ j2 __ ra.. ?1
+#                         ___ p __ ra.. ?1
+#                             ___ q __ ra.. ?1
+#                                 ?|? ? ? ? ? +_ ?|?1-1 ?2, ?3 p q| *\
+#                                                          ?|?1-1 p q i2 j2
+#                                 ?[k, i1, j1, i2, j2] %_ M..
+#
+#     __ 2 ** __2_ __ ?2
+#         r_ ?|__2_ 0 0 ?1 - 1, ?1 - 1
+#     binary _ '{0:b}'.f.. ?2 |;;-1
+#     results _ N..
+#     ___ i b __ en..  b..
+#         __ b __ '1' an. r.. __ N..
+#             r.. _ ?|? 0 0 : :
+#         ____ b __ '1'
+#             new_results _ __.z_l.. r..
+#             ___ k1 __ ra.. ?1
+#                 ___ k2 __ ra.. ?1
+#                     ___ p __ ra.. ?1
+#                         ___ q __ ra.. ?1
+#                             n..__1 _2 +_ r..|? ? *\
+#                                                    ?|i, p q k1 k2
+#                             n..|_1 _2 %_ M..
+#             r.. _ n..
+#     r_ ?|?1 - 1 ?1 - 1
+#
+# print(r_c_d.. 6, 100
+# print(r_n_d.. 6, 100
+# print(r_n_d.(5, 10 ** 18))
+#
+# ___ ?1 __ ra.. 2  5
+#     ___ ?2 __ ra.. 2 100
+#         classic _ r_c_d0 ?1 ?2
+#         new _ r_n_d. ?1 ?2
+#         as.. cl.. __ n.. 'classic_@, new_@, n_@, m_@'.f..(
+#             c..
+#             n..0
+#             ?1
+#             m
+#         )
