afd42c9a4099aeb94edc3614f7935b17fec93f6f
Grey <sergejyurskyj@yahoo.com>
2020-04-02 20:26:47 -0700

    Committed count_distinct_slices template

--- 125_algorithms/_exercises/templates/Beat the Codility Coding Interview in Python/Section 11 Caterpillar method/caterpillarmethod/count_distinct_slices.py
+++ 125_algorithms/_exercises/templates/Beat the Codility Coding Interview in Python/Section 11 Caterpillar method/caterpillarmethod/count_distinct_slices.py
@@ -1,22 +1,22 @@
-# This is the solution for Caterpillar Method > CountDistinctSlices
+# # This is the solution for Caterpillar Method > CountDistinctSlices
+# #
+# # This is marked as PAINLESS difficulty
 #
-# This is marked as PAINLESS difficulty
-
-def solution(M, A):
-    total_slices = 0
-    in_current_slice = [False] * (M + 1)
-    head = 0
-    for tail in range(0,len(A)):
-        while head < len(A) and (not in_current_slice[A[head]]):
-            in_current_slice[A[head]] = True
-            total_slices += (head - tail) + 1
-            head += 1
-            total_slices = 1000000000 if total_slices > 1000000000 else total_slices
-        in_current_slice[A[tail]] = False
-    return total_slices
-
-
-print(solution(9, [2, 4, 1, 7, 4, 9, 7, 3, 5, 5, 8, 7, 1]))
-
-print(solution(6, [3, 4, 5, 5, 2]))
-
+# ___ solution M A
+#     total_slices _ 0
+#     in_current_slice _ |F.. * ? + 1
+#     head _ 0
+#     ___ tail __ ra.. 0 le. ?
+#         w__ h.. < le. ? an. |no. i..|?|h..
+#             i..|?|h.. _ T..
+#             t.. +_ h.. - t.. + 1
+#             h.. +_ 1
+#             t.. _ 1000000000 __ t.. > 1000000000 ____ t..
+#         i..|?|t.. _ F..
+#     r_ ?
+#
+#
+# print(?(9, [2, 4, 1, 7, 4, 9, 7, 3, 5, 5, 8, 7, 1
+#
+# print(?(6, [3, 4, 5, 5, 2
+#
