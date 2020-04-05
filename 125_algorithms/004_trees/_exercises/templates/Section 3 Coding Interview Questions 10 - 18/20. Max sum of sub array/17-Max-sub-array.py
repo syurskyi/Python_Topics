974d8adfbda421152d9f0305382ec1a91c14a3a5
Grey <sergejyurskyj@yahoo.com>
2020-04-05 15:15:57 -0700

    Committed 17-Max-sub-array templates

--- 125_algorithms/004_trees/_exercises/templates/Section 3 Coding Interview Questions 10 - 18/20. Max sum of sub array/17-Max-sub-array.py
+++ 125_algorithms/004_trees/_exercises/templates/Section 3 Coding Interview Questions 10 - 18/20. Max sum of sub array/17-Max-sub-array.py
@@ -1,40 +1,40 @@
-# [1, 3, 2, -2, 4, 5] => [1, 3, 2] [4, 5] = 9 => 4, 5
-# [] = - 1                  6
-# [-1, -3] = -1
-# [2] = 0 0
-# [-1, 2] = 1 1
-
-# break it down
-
-def max_sub_array(l):
-    index_start = 0
-    index_end = 0
-    sum_of_sub_array = 0
-    new_sub_array_start = 0
-    sum_of_new_sub_array = 0
-    for index,val in enumerate(l):
-        if val < 0:
-            # figure out the sum
-            if sum_of_sub_array < sum_of_new_sub_array:
-                #swap subarray indexes
-                index_start = new_sub_array_start
-                index_end = index - 1
-                sum_of_sub_array = sum_of_new_sub_array
-                sum_of_new_sub_array = 0
-            new_sub_array_start = index + 1
-        else:
-            sum_of_new_sub_array += val
-    
-    if sum_of_sub_array < sum_of_new_sub_array:
-        #swap subarray indexes
-        index_start = new_sub_array_start
-        index_end = len(l) - 1
-        sum_of_sub_array = sum_of_new_sub_array
-    
-    if sum_of_sub_array == 0:
-        print(-1)
-    else:
-        print(index_start, index_end)
-
-max_sub_array([2])
-    
+# # [1, 3, 2, -2, 4, 5] => [1, 3, 2] [4, 5] = 9 => 4, 5
+# # [] = - 1                  6
+# # [-1, -3] = -1
+# # [2] = 0 0
+# # [-1, 2] = 1 1
+#
+# # break it down
+#
+# ___ max_sub_array l
+#     index_start _ 0
+#     index_end _ 0
+#     sum_of_sub_array _ 0
+#     new_sub_array_start _ 0
+#     sum_of_new_sub_array _ 0
+#     ___ index val __ en.. ?
+#         __ v.. < 0:
+#             # figure out the sum
+#             __ s.. < s..
+#                 #swap subarray indexes
+#                 i.. _ n..
+#                 i.. _ i.. - 1
+#                 s.. _ s..
+#                 s.. _ 0
+#             n.. _ i.. + 1
+#         ____
+#             s.. +_ v..
+#
+#     __ s.. < s..
+#         #swap subarray indexes
+#         i.. _ n...
+#         i.. _ le. ? - 1
+#         s.. _ s..
+#
+#     __ s.. __ 0
+#         print(-1)
+#     ____
+#         print i.. i..
+#
+# ?([2])
+#
