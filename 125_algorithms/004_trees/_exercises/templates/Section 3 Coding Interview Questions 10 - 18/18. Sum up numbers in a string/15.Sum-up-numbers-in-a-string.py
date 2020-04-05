babfe073d5190f1950e6901da333cd7eba7b2b75
Grey <sergejyurskyj@yahoo.com>
2020-04-05 15:09:35 -0700

    Committed 15.Sum-up-numbers-in-a-string templates

--- 125_algorithms/004_trees/_exercises/templates/Section 3 Coding Interview Questions 10 - 18/18. Sum up numbers in a string/15.Sum-up-numbers-in-a-string.py
+++ 125_algorithms/004_trees/_exercises/templates/Section 3 Coding Interview Questions 10 - 18/18. Sum up numbers in a string/15.Sum-up-numbers-in-a-string.py
@@ -1,16 +1,16 @@
-# "asddas22asda3fdf4aaaaa444" => 22  3 + 4 = 29
-
-def count_number(s):
-    temp = 0
-    total = 0
-    for c in s:
-        if '0' <= c and c <= '9': # only <<<<<<<<<<<<<
-            temp *= 10
-            temp += int(c)
-        else:
-            total += temp
-            temp = 0
-    total += temp
-    return total
-
-print(count_number("asddas22asda3fdf4"))
+# # "asddas22asda3fdf4aaaaa444" => 22  3 + 4 = 29
+#
+# ___ count_number s
+#     temp _ 0
+#     total _ 0
+#     ___ c __ ?
+#         __ '0' <_ ? an. ? <_ '9' # only <<<<<<<<<<<<<
+#             ? *_ 10
+#             ? +_ in. ?
+#         ____
+#             ? +_ ?
+#             ? _ 0
+#     ? +_ ?
+#     r_ ?
+#
+# print(?("asddas22asda3fdf4"))
