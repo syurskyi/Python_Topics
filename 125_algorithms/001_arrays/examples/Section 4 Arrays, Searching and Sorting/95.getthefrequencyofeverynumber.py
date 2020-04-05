0af5dfad8ea2509502754416525542b3f24075bf
Grey <sergejyurskyj@yahoo.com>
2020-04-05 14:06:30 -0700

    Committed Section 4 Arrays, Searching and Sorting fixed some grammating issues

--- 125_algorithms/001_arrays/examples/Section 4 Arrays, Searching and Sorting/95.getthefrequencyofeverynumber.py
+++ 125_algorithms/001_arrays/examples/Section 4 Arrays, Searching and Sorting/95.getthefrequencyofeverynumber.py
@@ -1,8 +1,7 @@
-
 def getthefrequencyofeverynumber(myarray):
-        print "Given array:",myarray
-	hashTable = {}  
-        #print "created hash:",hashTable
+	print("Given array:",myarray)
+	hashTable = {}
+	# print "created hash:",hashTable
 	for ele in myarray:
 		if ele in hashTable:
 			hashTable[ele] += 1
@@ -10,12 +9,12 @@ def getthefrequencyofeverynumber(myarray):
 			hashTable[ele] = 1
 		else:
 			hashTable[ele] = 0
-                #print ">> start loop: Array:",myarray
-                #print "               hashTable:", hashTable
-                #print "<<end loop"
+		# print ">> start loop: Array:",myarray
+	# print "               hashTable:", hashTable
+	# print "<<end loop"
 
 	for ele in myarray:
-		print ele, "frequency", hashTable[ele]
+		print(ele, "frequency", hashTable[ele])
  
 myarray = [100, 25, 25, 120, 100] 
 getthefrequencyofeverynumber(myarray)
