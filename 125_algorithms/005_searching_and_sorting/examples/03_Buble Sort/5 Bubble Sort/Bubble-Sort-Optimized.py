632dc1ed48fb907117009499bae0c30a6412d2c9
Grey <sergejyurskyj@yahoo.com>
2020-04-06 14:25:09 -0700

    Committed Bubble-Sort-Optimized template

--- 125_algorithms/005_searching_and_sorting/examples/03_Buble Sort/5 Bubble Sort/Bubble-Sort-Optimized.py
+++ 125_algorithms/005_searching_and_sorting/examples/03_Buble Sort/5 Bubble Sort/Bubble-Sort-Optimized.py
@@ -1,26 +1,26 @@
-def bubble_sort(lst):
+___ bubble_sort lst
     # Number of items in the list
-    n = len(lst)
+    n _ le. ?
     
     # Traverse the list
-    for i in range(n):
+    ___ i __ ra.. ?
         # If the iteration causes a swap or not.
         # By default, it's False, but if a swap
         # occurs, it changes to True.
-        swapped = False
+        swapped _ F..
             
         # For every unsorted element in the list
         # (the last i elements are already sorted)
-        for j in range(0, n-i-1):
+        ___ j __ ra.. 0 ? - ? - 1
             # If the current element is greater than
             # the element to its right, swap them
-            if lst[j] > lst[j+1]:
+            __ ?|? > ?|?+1
                 # Swapping...
-                lst[j], lst[j+1] = lst[j+1], lst[j]
+                ?|? ?|?+1 _ ?|?+1 ?|?
                 # A swap occured, update the variable
-                swapped = True
+                swapped _ T..
                 
         # If the inner loop did not cause any swaps,
         # the list is sorted, so the loop can stop.
-        if not swapped:
-            break
+        __ no. ?
+            b..
