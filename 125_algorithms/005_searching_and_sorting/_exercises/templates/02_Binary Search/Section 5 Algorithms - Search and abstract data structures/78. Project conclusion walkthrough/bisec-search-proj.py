58604455d223a2f1c8763057a5e850bdad4bc388
Grey <sergejyurskyj@yahoo.com>
2020-04-06 09:57:13 -0700

    Committed Section 5 Algorithms - Search and abstract data structures templates

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/02_Binary Search/Section 5 Algorithms - Search and abstract data structures/78. Project conclusion walkthrough/bisec-search-proj.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/02_Binary Search/Section 5 Algorithms - Search and abstract data structures/78. Project conclusion walkthrough/bisec-search-proj.py
@@ -1,27 +1,27 @@
-from demos import bisection_iter, analyze_func, generate_emails
-
-# Add domains to list below for additional email extensions
-list_of_domains = ['yaexample.com','goexample.com','example.com']
-
-# Generate emails
-emails = generate_emails(10,list_of_domains,1000000)
-
-# Test email to add to list of emails, followed by append to list
-email = 'mashrur@example.com'
-emails.append(email)
-
-# Sort list of generated emails
-sorted_emails = sorted(emails)
-
-# Run bisection search to find test email in sorted list of emails
-index, found = bisection_iter(email, sorted_emails)
-
-# Print result from function
-print(found)
-
-# Print index returned by function used on the list of sorted emails
-if index:   print(f"element at index: {index} is {sorted_emails[index]}")
-
-# Run analysis of functions
-analyze_func(bisection_iter, email, sorted_emails)
-analyze_func(generate_emails, 10, list_of_domains, 1000000)
+# ____ d.. ________ bisection_iter, analyze_func, generate_emails
+#
+# # Add domains to list below for additional email extensions
+# list_of_domains = ['yaexample.com','goexample.com','example.com']
+#
+# # Generate emails
+# emails = g_e.. 10 l_o_d.. 1000000
+#
+# # Test email to add to list of emails, followed by append to list
+# email = 'mashrur@example.com'
+# ?.ap.. ?
+#
+# # Sort list of generated emails
+# sorted_emails = so.. ?
+#
+# # Run bisection search to find test email in sorted list of emails
+# index found _ b_i.. ? ?
+#
+# # Print result from function
+# print f..
+#
+# # Print index returned by function used on the list of sorted emails
+# __ index   print _*element at index: |i.. is |s_e..|i..
+#
+# # Run analysis of functions
+# a.. b_i.. e.. s_e..
+# a.. g_e.. 10 l_o_d.. 1000000
