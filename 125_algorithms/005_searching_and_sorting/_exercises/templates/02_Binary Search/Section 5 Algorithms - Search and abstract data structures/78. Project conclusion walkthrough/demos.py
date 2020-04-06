4babb79d683f2732febb374b307da1a74b3c923b
Grey <sergejyurskyj@yahoo.com>
2020-04-06 09:54:01 -0700

    Committed Section 5 Algorithms - Search and abstract data structures templates

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/02_Binary Search/Section 5 Algorithms - Search and abstract data structures/78. Project conclusion walkthrough/demos.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/02_Binary Search/Section 5 Algorithms - Search and abstract data structures/78. Project conclusion walkthrough/demos.py
@@ -1,36 +1,36 @@
-import time
-from random import choice
-from string import ascii_lowercase as letters
-
-def bisection_iter(n, arr):
-    start = 0
-    stop = len(arr)-1
-    while start <= stop:
-        mid = (start + stop)//2
-        if n == arr[mid]:
-            return mid, f"{n} found at index: {mid}" # return tuple
-        elif n > arr[mid]:
-            start = mid + 1
-        else:
-            stop = mid - 1
-    return None, f"{n} not found in list"
-
-def analyze_func(func_name, *args):
-    tic = time.time()
-    func_name(*args) # accept variable list of arguments
-    toc = time.time()
-    seconds = toc-tic
-    print(f"{func_name.__name__.capitalize()}\t-> Elapsed time: {seconds:.5f}")
-
-###### functions to generate lists of random emails ######
-def generate_name(length_of_name):
-    return ''.join(choice(letters) for i in range(length_of_name))
-
-def get_domain(list_of_domains):
-    return choice(list_of_domains)
-
-def generate_emails(length_of_name, list_of_domains, total_emails):
-    emails = []
-    for num in range(total_emails):
-        emails.append(generate_name(length_of_name)+"@"+get_domain(list_of_domains))
-    return emails
+# ______ t..
+# ____ ra.. ______ ch..
+# ____ str.. ______ a_l.. __ letters
+#
+# ___ bisection_iter n arr
+#     start _ 0
+#     stop _ le. a..-1
+#     w___ ? <_ ?
+#         mid _ (? + ?)//2
+#         __ n == a..|?
+#             r_ mid, _*|? found at index: |?" # return tuple
+#         ____ n > a..|m..
+#             s.. _ m.. + 1
+#         ____
+#             s.. _ m.. - 1
+#     r_ N.. _*|? not found in list"
+#
+# ___ analyze_func func_name $
+#     tic _ t__.t..
+#     ? $ # accept variable list of arguments
+#     toc _ t__.t..
+#     seconds _ toc-tic
+#     print(f"{func_name.__name__.capitalize()}\t-> Elapsed time: {seconds:.5f}")
+#
+# ###### functions to generate lists of random emails ######
+# ___ generate_name length_of_name
+#     r_ ''.jo.. ch.. l.. ___ i __ ra.. ?
+#
+# ___ get_domain list_of_domains
+#     r_ ch.. ?
+#
+# ___ generate_emails length_of_name list_of_domains total_emails
+#     emails _    # list
+#     ___ num __ ra.. t_e..
+#         e__.ap.. g_n.. l_o_n.. +"@"+g_d.. l..
+#     r_ ?
