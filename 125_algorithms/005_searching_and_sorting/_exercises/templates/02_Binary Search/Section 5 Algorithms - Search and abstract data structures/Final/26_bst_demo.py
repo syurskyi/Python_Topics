afe5fbeecde1896fac59e600fada98ecf604b4bc
Grey <sergejyurskyj@yahoo.com>
2020-04-06 12:02:48 -0700

    Committed 26_bst_demo templates

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/02_Binary Search/Section 5 Algorithms - Search and abstract data structures/Final/26_bst_demo.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/02_Binary Search/Section 5 Algorithms - Search and abstract data structures/Final/26_bst_demo.py
@@ -1,160 +1,160 @@
-# Final implementation
-
-class Node:
-    def __init__(self, key):
-        self.data = key
-        self.left_child = None
-        self.right_child = None
-
-class BSTDemo:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, key):
-        if not isinstance(key, Node):
-            key = Node(key)
-        if self.root == None:
-            self.root = key
-        else:
-            self._insert(self.root, key)
-
-    def _insert(self, curr, key):
-        if key.data > curr.data:
-            if curr.right_child == None:
-                curr.right_child = key
-            else:
-                self._insert(curr.right_child, key)
-        elif key.data < curr.data:
-            if curr.left_child == None:
-                curr.left_child = key
-            else:
-                self._insert(curr.left_child, key)
-
-    def in_order(self):
-        self._in_order(self.root)
-        print("")
-
-    def _in_order(self, curr):
-        if curr:
-            self._in_order(curr.left_child)
-            print(curr.data, end=" ")
-            self._in_order(curr.right_child)
-
-    def pre_order(self):
-        '''root, left, right'''
-        pass
-
-    def _pre_order(self, curr):
-        pass
-
-    def post_order(self):
-        '''left, right, root'''
-        pass
-
-    def _post_order(self, curr):
-        pass
-
-    def find_val(self, key):
-        return self._find_val(self.root, key)
-
-    def _find_val(self, curr, key):
-        if curr:
-            if key == curr.data:
-                return "Value found in tree"
-            elif key < curr.data:
-                return self._find_val(curr.left_child, key)
-            else:
-                return self._find_val(curr.right_child, key)
-        return "Value not found in tree"
-
-    def min_right_subtree(self, curr):
-        if curr.left_child == None:
-            return curr
-        else:
-            return self.min_right_subtree(curr.left_child)
-
-    def delete_val(self, key):
-        self._delete_val(self.root, None, None, key)
-
-    def _delete_val(self, curr, prev, is_left, key):
-        if curr:
-            if key == curr.data:
-                if curr.left_child and curr.right_child:
-                    min_child = self.min_right_subtree(curr.right_child)
-                    curr.data = min_child.data
-                    self._delete_val(curr.right_child, curr, False, min_child.data)
-                elif curr.left_child == None and curr.right_child == None:
-                    if prev:
-                        if is_left:
-                            prev.left_child = None
-                        else:
-                            prev.right_child = None
-                    else:
-                        self.root = None
-                elif curr.left_child == None:
-                    if prev:
-                        if is_left:
-                            prev.left_child = curr.right_child
-                        else:
-                            prev.right_child = curr.right_child
-                    else:
-                        self.root = curr.right_child
-                else:
-                    if prev:
-                        if is_left:
-                            prev.left_child = curr.left_child
-                        else:
-                            prev.right_child = curr.left_child
-                    else:
-                        self.root = curr.left_child
-            elif key < curr.data:
-                self._delete_val(curr.left_child, curr, True, key)
-            elif key > curr.data:
-                self._delete_val(curr.right_child, curr, False, key)
-        else:
-            print(f"{key} not found in Tree")
-
-tree = BSTDemo()
-tree.insert("F")
-tree.insert("C")
-print("Test deleting leaf node which is left child of parent")
-tree.in_order()
-tree.delete_val("C")
-tree.in_order()
-tree.insert("G")
-print("Test deleting leaf node which is right child of parent")
-tree.in_order()
-tree.delete_val("G")
-tree.in_order()
-tree.insert("A")
-print("Test deleting parent/root node which has one child")
-tree.in_order()
-tree.delete_val("F")
-tree.in_order()
-print("Test deleting root node which has no children")
-tree.in_order()
-tree.delete_val("A")
-tree.in_order()
-tree.insert("F")
-tree.insert("C")
-tree.insert("G")
-tree.insert("A")
-tree.insert("B")
-tree.insert("K")
-tree.insert("E")
-tree.insert("H")
-tree.insert("D")
-tree.insert("I")
-tree.insert("M")
-tree.insert("J")
-tree.insert("L")
-tree.in_order()
-tree.delete_val("F")
-tree.in_order()
-tree.in_order()
-tree.delete_val("K")
-tree.in_order()
-tree.in_order()
-tree.delete_val("C")
-tree.in_order()
-tree.delete_val("Z")
+# # Final implementation
+#
+# c_ Node
+#     ___ - key
+#         data _ ?
+#         left_child _ N..
+#         right_child _ N..
+#
+# c_ BSTDemo
+#     ___ -
+#         root _ N..
+#
+#     ___ insert key
+#         __ no. isi.. ? ?
+#             ? _ ? ?
+#         __ r.. __ N..
+#             r.. _ ?
+#         ____
+#             _? r.. ?
+#
+#     ___ _insert curr key
+#         __ ?.d.. > ?.d..
+#             __ ?.r.. __ N..
+#                 ?.r.. _ ?
+#             ____
+#                 _? ?.r.. ?
+#         ____ ?.d.. < ?.d..
+#             __ ?.l.. __ N..
+#                 ?.l.. _ ?
+#             ____
+#                 _? ?.l..  ?
+#
+#     ___ in_order
+#         _? r..
+#         print("")
+#
+#     ___ _in_order curr
+#         __ ?
+#             _? ?.l..
+#             print ?.d.. e.._" ")
+#             _? ?.r..
+#
+#     ___ pre_order
+#         '''root, left, right'''
+#         p..
+#
+#     ___ _pre_order curr
+#         p..
+#
+#     ___ post_order
+#         '''left, right, root'''
+#         p..
+#
+#     ___ _post_order curr
+#         p..
+#
+#     ___ find_val ?
+#         r_ _? r.. ?
+#
+#     ___ _find_val curr, key
+#         __ ?
+#             __ ? __ ?.d..
+#                 r_ "Value found in tree"
+#             ____ ? < ?.d..
+#                 r_ _? ?.l.. ?
+#             ____
+#                 r_ _? ?.r.. ?
+#         r_ "Value no. found in tree"
+#
+#     ___ min_right_subtree curr
+#         __ ?.l.. __ N..
+#             r_ ?
+#         ____
+#             r_ ? ?.l..
+#
+#     ___ delete_val key
+#         _? r.. N.. N.. ?
+#
+#     ___ _delete_val curr prev is_left key
+#         __ ?
+#             __ ? __ ?.d..
+#                 __ ?.l.. an. ?.r..
+#                     min_child _ m.. ?.r..
+#                     ?.d.. _ ?.d..
+#                     _d.. ?.r.. ? F.. m__.d..
+#                 ____ ?.l.. __ N.. an. ?.r.. __ N..
+#                     __ p..
+#                         __ i..
+#                             p__.l.. _ N..
+#                         ____
+#                             p__.r.. _ N..
+#                     ____
+#                         r.. _ N..
+#                 ____ ?.l.. __ N..
+#                     __ p__:
+#                         __ is_left
+#                             p__.l.. _ ?.r..
+#                         ____
+#                             p__.r.. _ ?.r..
+#                     ____
+#                         r.. _ ?.r..
+#                 ____
+#                     __ p__
+#                         __ i..
+#                             p__.l.. _ ?.l..
+#                         ____
+#                             p__.r.. _ ?.l..
+#                     ____
+#                         r.. _ ?.l..
+#             ____ ? < ?.d..
+#                 _d.. ?.l.. ? T.. ?
+#             ____ ? > ?.d..
+#                 _d.. ?.r.. ? F.. ?
+#         ____
+#             print _*|? no. found in Tree")
+#
+# tree _ ?
+# tree.insert("F")
+# tree.insert("C")
+# print("Test deleting leaf node which is left child of parent")
+# tree.in_order()
+# tree.delete_val("C")
+# tree.in_order()
+# tree.insert("G")
+# print("Test deleting leaf node which is right child of parent")
+# tree.in_order()
+# tree.delete_val("G")
+# tree.in_order()
+# tree.insert("A")
+# print("Test deleting parent/root node which has one child")
+# tree.in_order()
+# tree.delete_val("F")
+# tree.in_order()
+# print("Test deleting root node which has no children")
+# tree.in_order()
+# tree.delete_val("A")
+# tree.in_order()
+# tree.insert("F")
+# tree.insert("C")
+# tree.insert("G")
+# tree.insert("A")
+# tree.insert("B")
+# tree.insert("K")
+# tree.insert("E")
+# tree.insert("H")
+# tree.insert("D")
+# tree.insert("I")
+# tree.insert("M")
+# tree.insert("J")
+# tree.insert("L")
+# tree.in_order()
+# tree.delete_val("F")
+# tree.in_order()
+# tree.in_order()
+# tree.delete_val("K")
+# tree.in_order()
+# tree.in_order()
+# tree.delete_val("C")
+# tree.in_order()
+# tree.delete_val("Z")
