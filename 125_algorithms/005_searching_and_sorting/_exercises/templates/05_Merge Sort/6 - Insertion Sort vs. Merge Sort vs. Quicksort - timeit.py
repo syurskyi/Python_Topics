9e7c59b0f5bda10bf9f76bef6447a6e7b3020cd8
Grey <sergejyurskyj@yahoo.com>
2020-04-07 15:11:23 -0700

    Committed 6 - Insertion Sort vs. Merge Sort vs. Quicksort - timeit templates

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/05_Merge Sort/6 - Insertion Sort vs. Merge Sort vs. Quicksort - timeit.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/05_Merge Sort/6 - Insertion Sort vs. Merge Sort vs. Quicksort - timeit.py
@@ -1,132 +1,132 @@
-import timeit
-
-NUM_REPETITIONS = 100
-
-SETUP_CODE = "from random import shuffle"
-
-# First test case - Insertion Sort
-
-def insertion_sort(lst):
-    for i in range(1, len(lst)):
-        elem_selected = lst[i]
-
-        while i > 0 and elem_selected < lst[i-1]:
-            lst[i] = lst[i-1]
-            i -= 1
-
-        lst[i] = elem_selected
-
-
-test_code_1 = '''
-
-a = [i for i in range(1000)]
-
-shuffle(a)
-
-insertion_sort(a)
-
-'''
-
-print("\n==> Testing Insertion Sort")
-
-time = timeit.timeit(test_code_1, setup=SETUP_CODE, number=NUM_REPETITIONS, globals=globals())
-
-print("Total time to sort the list:", time)
-print("Average time per repetition:", time/NUM_REPETITIONS)
-
-
-# Second test case - Merge Sort
-
-def merge_sort(lst):
-    
-    if len(lst) == 0 or len(lst) == 1:
-        return lst
-    else:
-        middle_index = len(lst)//2
-
-        left = merge_sort(lst[:middle_index])
-        right = merge_sort(lst[middle_index:])
-
-        return merge(left, right)
-
-
-def merge(left_half, right_half):
-
-    if not left_half or not right_half:
-        return left_half or right_half
-    
-    result = []
-    i, j = 0, 0
-
-    while True:
-        
-        if left_half[i] < right_half[j]:
-            result.append(left_half[i])
-            i += 1
-        else:
-            result.append(right_half[j])
-            j += 1
-
-        if i == len(left_half) or j == len(right_half):
-            result.extend(left_half[i:] or right_half[j:])
-            break
-
-    return result
-
-
-test_code_2 = '''
-
-b = [i for i in range(1000)]
-
-shuffle(b)
-
-merge_sort(b)
-
-'''
-
-print("\n==> Testing Merge Sort")
-
-time = timeit.timeit(test_code_2, setup=SETUP_CODE, number=NUM_REPETITIONS, globals=globals())
-
-print("Total time to sort the list:", time)
-print("Average time per repetition:", time/NUM_REPETITIONS)
-
-
-# Third test case - Quicksort
-
-def quicksort(lst, low, high):
-    if low < high: 
-        pivot_index = partition(lst, low, high) 
-   
-        quicksort(lst, low, pivot_index-1) 
-        quicksort(lst, pivot_index+1, high)
-
-def partition(lst, low, high):
-    pivot = lst[high]
-
-    i = low - 1     
-  
-    for j in range(low, high): 
-        if lst[j] <= pivot: 
-            i += 1
-            lst[i], lst[j] = lst[j], lst[i]
-             
-    lst[i+1], lst[high] = lst[high], lst[i+1] 
-    return i+1
-
-test_code_3 = '''
-
-c = [i for i in range(1000)]
-
-shuffle(c)
-
-quicksort(c, 0, len(c)-1)
-
-'''
-
-print("\n==> Testing Quicksort")
-
-time = timeit.timeit(test_code_3, setup=SETUP_CODE, number=NUM_REPETITIONS, globals=globals())
-
-print("Total time to sort the list:", time)
-print("Average time per repetition:", time/NUM_REPETITIONS)
+# ______ t..
+#
+# NUM_REPETITIONS _ 100
+#
+# SETUP_CODE _ "from random import shuffle"
+#
+# # First test case - Insertion Sort
+#
+# ___ insertion_sort lst
+#     ___ i __ ra.. 1 le. ?
+#         elem_selected _ ?|?
+#
+#         w__ i > 0 an. ? < ?|?-1
+#             ?|? _ ?|?-1
+#             ? -_ 1
+#
+#         ?|? _ ?
+#
+#
+# test_code_1 _ '''
+#
+# a _ [i for i in range(1000)]
+#
+# shuffle(a)
+#
+# insertion_sort(a)
+#
+# '''
+#
+# print("\n==> Testing Insertion Sort")
+#
+# time _ t__.t.. _1 s.. n.. g..
+#
+# print("Total time to sort the list:" ?
+# print("Average time per repetition:"  t../N..
+#
+#
+# # Second test case - Merge Sort
+#
+# ___ merge_sort lst
+#
+#     __ le.(?) __ 0 o. le.(?) __ 1
+#         r_ ?
+#     else:
+#         middle_index _ le.(?)//2
+#
+#         left _ ? ?|;?
+#         right _ ? ?|?;
+#
+#         r_ ? ? ?
+#
+#
+# ___ merge left_half right_half
+#
+#     __ no. ? o. no. ?
+#         r_ ? o. ?
+#
+#     result _    # list
+#     i j _ 0 0
+#
+#     w__ T..
+#
+#         __ ?|? < right_half|?
+#             ?.ap.. ?|?
+#             i +_ 1
+#         ____
+#             ?.ap..?|?
+#             j +_ 1
+#
+#         __ i __ le. ? o. j __ le. ?
+#             ?.ex.. ?|?; o. ?|?;
+#             b..
+#
+#     r_ ?
+#
+#
+# test_code_2 _ '''
+#
+# b _ [i for i in range(1000)]
+#
+# shuffle(b)
+#
+# merge_sort(b)
+#
+# '''
+#
+# print("\n==> Testing Merge Sort")
+#
+# time _ t__.t.. _2, s.. n.. g..
+#
+# print("Total time to sort the list:", time)
+# print("Average time per repetition:", time/NUM_REPETITIONS)
+#
+#
+# # Third test case - Quicksort
+#
+# ___ quicksort lst low high
+#     __ ? < ?
+#         pivot_index _ ?|? ? ?
+#
+#         ? ? ? ?-1
+#         ? ? ?+1 ?
+#
+# ___ partition lst low high
+#     pivot _ ?|?
+#
+#     i _ ? - 1
+#
+#     ___ j __ ra.. ? ?
+#         __ ?|? <_ ?
+#             ? +_ 1
+#             ?|? ?|? _ ?|? ?|?
+#
+#     ?+1 ?|? _ ?|? ?|?+1
+#     r_ ?+1
+#
+# test_code_3 _ '''
+#
+# c _ [i for i in range(1000)]
+#
+# shuffle(c)
+#
+# quicksort(c, 0, len(c)-1)
+#
+# '''
+#
+# print("\n==> Testing Quicksort")
+#
+# time _ t__.t.. _3, s.. n.. g..
+#
+# print("Total time to sort the list:" ?
+# print("Average time per repetition:" t../N..
