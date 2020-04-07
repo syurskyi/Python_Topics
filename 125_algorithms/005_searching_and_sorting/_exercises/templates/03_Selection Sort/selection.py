a7e4f19546791f772753dc2b803ea847ab781df1
Grey <sergejyurskyj@yahoo.com>
2020-04-06 20:46:30 -0700

    Committed selection template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/03_Selection Sort/selection.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/03_Selection Sort/selection.py
@@ -1,27 +1,27 @@
-
-def selection_sort(nums):
-    
-	for i in range(len(nums)-1):
-		
-		index = i
-		
-		for j in range(i+1,len(nums),1):
-			if nums[j] < nums[index]:
-				index = j
-			
-		if index != i:
-			swap(nums,index,i)
-			
-	
-	return nums
-	
-def swap(nums, i, j):
-	temp = nums[i]
-	nums[i] = nums[j]
-	nums[j] = temp
- 
-if __name__ == "__main__":
-   
-   nums = [-1,4,5,2,-2,0,1,2,3,2,1,0,1,2,3]
-   print(selection_sort(nums))
-  
+#
+# ___ selection_sort nums
+#
+# 	___ i __ ra.. le. ? -1
+#
+# 		index _ ?
+#
+# 		___ j __ ra.. ?+1 le. ? 1
+# 			__ ?|? < n..|i..
+# 				i.. _ ?
+#
+# 		__ i.. !_ ?
+# 			s.. n.. i.. ?
+#
+#
+# 	r_ ?
+#
+# ___ swap nums i j
+# 	temp _ ?|?
+# 	?|? _ ?|?
+# 	?|? _ t..0
+#
+# __ ______ __ _____
+#
+#    nums _ [-1,4,5,2,-2,0,1,2,3,2,1,0,1,2,3]
+#    print ? ?
+#
