ce7fa2ab06609b27d048953281748675e4cd0348
Grey <sergejyurskyj@yahoo.com>
2020-04-07 09:19:44 -0700

    Committed Merge-Sort template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/05_Merge Sort/8 Merge Sort/Merge-Sort.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/05_Merge Sort/8 Merge Sort/Merge-Sort.py
@@ -1,54 +1,54 @@
-def merge_sort(lst):
-    
-    # If the list is empty or it only contains one element
-    if len(lst) == 0 or len(lst) == 1:
-        return lst
-    else:
-        # Find the middle index
-        middle_index = len(lst)//2
-
-        # Call the function recursively
-        # passing the left half and the right half
-        # of the list in separate recursive calls
-        left = merge_sort(lst[:middle_index])
-        right = merge_sort(lst[middle_index:])
-
-        # Returned the merged version of the
-        # two halves already sorted in ascending order
-        return merge(left, right)
-
-# Merge the two halves
-def merge(left_half, right_half):
-
-    if not left_half or not right_half:
-        return left_half or right_half
-    
-    result = []
-    i, j = 0, 0
-
-    while True:
-        # If the element in the left half is smaller
-        # than the element in the right half,
-        # append that element to the result list 
-        # at the current index.
-        if left_half[i] < right_half[j]:
-            result.append(left_half[i])
-            i += 1
-        # Else, if the element in the right half is smaller,
-        # add that element to the list at the current index.
-        else:
-            result.append(right_half[j])
-            j += 1
-
-        # To check if there were any elements left
-        # because the two halves may differ in length.
-        # Insert the remaining elements to the list result.
-        if i == len(left_half) or j == len(right_half):
-            result.extend(left_half[i:] or right_half[j:])
-            break
-
-    return result
-
-    
-    
-    
+# ___ merge_sort lst
+#
+#     # If the list is empty or it only contains one element
+#     __ le. ? __ 0 o. le. ? __ 1
+#         r_ ?
+#     ____
+#         # Find the middle index
+#         middle_index _ le. ? //2
+#
+#         # Call the function recursively
+#         # passing the left half and the right half
+#         # of the list in separate recursive calls
+#         left _ ?|?|;?
+#         right _ ?|?|?;
+#
+#         # Returned the merged version of the
+#         # two halves already sorted in ascending order
+#         r_ ?|? ?
+#
+# # Merge the two halves
+# ___ merge left_half right_half
+#
+#     __ no. ? o. no. ?
+#         r_ ? o. ?
+#
+#     result _    # list
+#     i, j _ 0, 0
+#
+#     w__ T..
+#         # If the element in the left half is smaller
+#         # than the element in the right half,
+#         # append that element to the result list
+#         # at the current index.
+#         __ ?|? < ?|?
+#             r__.ap.. l..|?
+#             ? +_ 1
+#         # Else, __ the element in the right half is smaller,
+#         # add that element to the list at the current index.
+#         ____
+#             r__.ap.. r..|?
+#             j +_ 1
+#
+#         # To check __ there were any elements left
+#         # because the two halves may differ in length.
+#         # Insert the remaining elements to the list result.
+#         __ i __ le. ? o. j __ le. ?
+#             r__.ex.. ?|i; o. ?|?;
+#             b..
+#
+#     r_ ?
+#
+#
+#
+#
