51aeaa131c3aecf35ce0983e019e8731cae24abf
Grey <sergejyurskyj@yahoo.com>
2020-04-07 13:04:52 -0700

    Committed 59. In-depth look at execution context templates

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/05_Merge Sort/59. In-depth look at execution context.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/05_Merge Sort/59. In-depth look at execution context.py
@@ -1,15 +1,15 @@
-def divide_arr(arr):
-    if len(arr) < 2:
-        print(f"Base condition reached with {arr}")
-        return arr[:]
-    else:
-        middle = len(arr)//2
-        print("Current list to work with:", arr)
-        print("Left split:", arr[:middle])
-        print("Right split:", arr[middle:])
-        l1 = divide_arr(arr[:middle])
-        l2 = divide_arr(arr[middle:])
-        # implied return None
-
-l = [8, 6, 2, 5]
-divide_arr(l)
+# ___ divide_arr arr
+#     __ le. ? < 2
+#         print _* Base condition reached with |?
+#         r_ a?|;
+#     ____
+#         middle _ le. ?//2
+#         print("Current list to work with:" ?
+#         print("Left split:" ?|;?
+#         print("Right split:" ?|?;
+#         l1 = ? ?|;?
+#         l2 = ? ?|?;
+#         # implied return None
+#
+# l = [8, 6, 2, 5]
+# ? ?
