0a9a9dd826a1b8781b99a19c1f8ae04e706293b5
Grey <sergejyurskyj@yahoo.com>
2020-04-06 18:37:40 -0700

    Committed 98.SelectionSort template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/03_Selection Sort/98.SelectionSort.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/03_Selection Sort/98.SelectionSort.py
@@ -1,29 +1,29 @@
-
-def mySelectionSort(myarray):
-  print "myarray:",myarray
-  for i in range(len(myarray)):
-    least = i
-    #print "outerloop:",myarray
-    #print "I and Least element in outerloop:",i
-    for k in range(i + 1 , len(myarray)):
-      #print "innerloop:",myarray
-      #print "K:",k
-      #print "comparing myarray[",k,"] with element at least pos myarray[",least,"]",myarray[k],myarray[least]
-      if myarray[k] < myarray[least]:
-        #print "if",myarray[k],"<",myarray[least],"then least is changed from",least,"to",k
-        least = k
-
-    #print "once we are out of inner for loop"
-    #print "myarray[least] is swapped with myarray[i] i.e myarray[",least,"] is swapped with myarray[",i,"]",myarray[least],myarray[i]
-    swap(myarray, least, i)
- 
- 
-def swap(myarray, x, y):
-  temp = myarray[x]
-  myarray[x] = myarray[y]
-  myarray[y] = temp
-	      
-      
-myarray=[500,400,300,200,100]
-mySelectionSort(myarray)
-print(myarray) 
+#
+# ___ mySelectionSort myarray
+#   print "myarray:" ?
+#   ___ i __ ra.. le. ?
+#     least _ ?
+#     #print "outerloop:",myarray
+#     #print "I and Least element in outerloop:",i
+#     ___ k __ ra.. ? + 1  le. ?
+#       #print "innerloop:",myarray
+#       #print "K:",k
+#       #print "comparing myarray[",k,"] with element at least pos myarray[",least,"]",myarray[k],myarray[least]
+#       __ ?|? < ?|l..
+#         #print "if",myarray[k],"<",myarray[least],"then least is changed from",least,"to",k
+#         l.. _ ?
+#
+#     #print "once we are out of inner for loop"
+#     #print "myarray[least] is swapped with myarray[i] i.e myarray[",least,"] is swapped with myarray[",i,"]",myarray[least],myarray[i]
+#     s.. ? ? ?
+#
+#
+# ___ swap myarray x y
+#   temp _ ?|?
+#   ?|? _ ?|?
+#   ?|? _ t..
+#
+#
+# myarray=[500,400,300,200,100]
+# ? ?
+# print ?
