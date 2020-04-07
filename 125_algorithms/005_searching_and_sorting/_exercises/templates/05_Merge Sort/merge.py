8e05ae113c73b5425e8b3f53ab6250949f8a82e4
Grey <sergejyurskyj@yahoo.com>
2020-04-07 13:30:43 -0700

    Committed merge templates

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/05_Merge Sort/merge.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/05_Merge Sort/merge.py
@@ -1,39 +1,39 @@
-
-def merge_sort(nums):
-    
-	if len(nums) == 1:                                  
-		return
-		
-	middle_index = len(nums) // 2
-		
-	left_half = nums[:middle_index]
-	right_half = nums[middle_index:]
-	
-	merge_sort(left_half)
-	merge_sort(right_half)
-	
-	i = 0
-	j = 0
-	k = 0
-	
-	while i<len(left_half) and j<len(right_half):
-		if left_half[i] < right_half[j]:
-			nums[k] = left_half[i]
-			i = i + 1
-		else:
-			nums[k] = right_half[j]
-			j = j + 1
-			
-		k = k + 1
-		
-	while i<len(left_half):
-		nums[k] = left_half[i]
-		k = k + 1
-		i = i + 1		
-	
-if __name__ == "__main__":
-   
-   nums = [-3,-2,-1,1,2,1,0,-1,-2,-3]
-   merge_sort(nums)
-   print(nums)
-  
+#
+# ___ merge_sort ?
+#
+# 	__ le. ? __ 1
+# 		r_
+#
+# 	middle_index _ le. ? // 2
+#
+# 	left_half _ ?|;?
+# 	right_half _ ?|?;
+#
+# 	? ?
+# 	? ?
+#
+# 	i _ 0
+# 	j _ 0
+# 	k _ 0
+#
+# 	w__ i<le. ? an. j<le. ?
+# 		__ ?|? < ?|?
+# 			?|k _ ?|?
+# 			i _ ? + 1
+# 		____
+# 			?|? _ ?|?
+# 			j _ ? + 1
+#
+# 		k _ ? + 1
+#
+# 	w__ i<le. ?
+# 		?|k _ ?|?
+# 		k _ ? + 1
+# 		i _ ? + 1
+#
+# __ ______ __ _______
+#
+#    ? _ [-3,-2,-1,1,2,1,0,-1,-2,-3]
+#    ? ?
+#    print(?)
+#
