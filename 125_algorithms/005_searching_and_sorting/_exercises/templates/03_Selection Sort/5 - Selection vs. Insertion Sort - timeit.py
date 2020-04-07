30ed437a2426d54ec027a9d194e90c4583fc6e72
Grey <sergejyurskyj@yahoo.com>
2020-04-06 20:53:00 -0700

    Committed 5 - Selection vs. Insertion Sort - timeit template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/03_Selection Sort/5 - Selection vs. Insertion Sort - timeit.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/03_Selection Sort/5 - Selection vs. Insertion Sort - timeit.py
@@ -1,62 +1,62 @@
-import timeit
-
-NUM_REPETITIONS = 100
-
-SETUP_CODE = "from random import shuffle"
-
-def selection_sort(lst):
-    for i in range(len(lst)):
-        min_index = i
-        for curr_index in range(i+1, len(lst)):
-            if lst[min_index] > lst[curr_index]:
-                min_index = curr_index
-        lst[i], lst[min_index] = lst[min_index], lst[i]
-
-def insertion_sort(lst):
-    for i in range(1, len(lst)):
-        elem_selected = lst[i]
-
-        while i > 0 and elem_selected < lst[i-1]:
-            lst[i] = lst[i-1]
-            i -= 1
-
-        lst[i] = elem_selected
-
-# First test case - Selection Sort
-
-test_code_1 = '''
-
-a = [i for i in range(1000)]
-
-shuffle(a)
-
-selection_sort(a)
-
-'''
-
-print("\n==> Testing Selection Sort")
-
-time = timeit.timeit(test_code_1, setup=SETUP_CODE, number=NUM_REPETITIONS, globals=globals())
-
-print("Total time to sort the list:", time)
-print("Average time per repetition:", time/NUM_REPETITIONS)
-
-
-# Second test case - Insertion Sort
-
-test_code_2 = '''
-
-b = [i for i in range(1000)]
-
-shuffle(b)
-
-insertion_sort(b)
-
-'''
-
-print("\n==> Testing Insertion Sort")
-
-time = timeit.timeit(test_code_2, setup=SETUP_CODE, number=NUM_REPETITIONS, globals=globals())
-
-print("Total time to sort the list:", time)
-print("Average time per repetition:", time/NUM_REPETITIONS)
+# ______ ti..
+#
+# NUM_REPETITIONS _ 100
+#
+# SETUP_CODE _ "from random import shuffle"
+#
+# ___ selection_sort lst
+#     ___ i __ ra.. le. ?
+#         min_index _ i
+#         ___ curr_index __ ra.. ?+1 le. ?
+#             __ ?|m.. > ?|?
+#                 m.. _ ?
+#         ?|? ?|m.. _ ?|m.. ?|?
+#
+# ___ insertion_sort lst
+#     ___ i __ ra.. 1 le. ?
+#         elem_selected _ ?|?
+#
+#         w___ ? > 0 an. ? < ?|?-1
+#             ?|? _ ?|?-1
+#             i -_ 1
+#
+#         ?|? _ ?
+#
+# # First test case - Selection Sort
+#
+# test_code_1 = '''
+#
+# a = [i for i in range(1000)]
+#
+# shuffle(a)
+#
+# selection_sort(a)
+#
+# '''
+#
+# print("\n==> Testing Selection Sort")
+#
+# time = t__.t.. _1 s.. n.. g..
+#
+# print("Total time to sort the list:" ?
+# print("Average time per repetition:", t..
+#
+#
+# # Second test case - Insertion Sort
+#
+# test_code_2 = '''
+#
+# b = [i for i in range(1000)]
+#
+# shuffle(b)
+#
+# insertion_sort(b)
+#
+# '''
+#
+# print("\n==> Testing Insertion Sort")
+#
+# time = t__.t.. _2 s.. n.. g..
+#
+# print("Total time to sort the list:" ?
+# print("Average time per repetition:", t../N..
