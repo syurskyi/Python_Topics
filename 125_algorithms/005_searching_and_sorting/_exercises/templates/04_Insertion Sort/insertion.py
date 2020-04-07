d6fd8da5f8e6472ed0f78818e2fec6b555aca10a
Grey <sergejyurskyj@yahoo.com>
2020-04-06 21:09:48 -0700

    Committed Implementation of Insertion Sort template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/04_Insertion Sort/insertion.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/04_Insertion Sort/insertion.py
@@ -1,23 +1,23 @@
-
-def insertion_sort(nums):
-    
-	for i in range(len(nums)):
-		
-		j = i
-		
-		while j>0 and nums[j-1] > nums[j]:
-			swap(nums,j,j-1)
-			j = j - 1
-	
-	return nums
-	
-def swap(nums, i, j):
-	temp = nums[i]
-	nums[i] = nums[j]
-	nums[j] = temp
- 
-if __name__ == "__main__":
-   
-   nums = [5,4,3,2,1]
-   print(insertion_sort(nums))
-  
+#
+# __ insertion_sort nums
+#
+# 	___ i __ ra.. le. ?
+#
+# 		j _ ?
+#
+# 		w__ ? > 0 an. ?|?-1 > ?|?
+# 			s.. ? ? ? - 1
+# 			j _ ? - 1
+#
+# 	r_ ?
+#
+# __ swap nums i j
+# 	temp _ ?|?
+# 	?|? _ ?|?
+# 	?|? _ t..
+#
+# __ ______ __ ______
+#
+#    nums = [5,4,3,2,1]
+#    print ? ?
+#
