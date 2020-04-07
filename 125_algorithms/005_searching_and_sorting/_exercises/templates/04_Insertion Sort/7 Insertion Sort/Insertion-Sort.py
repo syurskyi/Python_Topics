b446aaa64c835acf1898f39e4701e01de274d805
Grey <sergejyurskyj@yahoo.com>
2020-04-06 20:55:22 -0700

    Committed Insertion-Sort template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/04_Insertion Sort/7 Insertion Sort/Insertion-Sort.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/04_Insertion Sort/7 Insertion Sort/Insertion-Sort.py
@@ -1,17 +1,15 @@
-def insertion_sort(lst):
-    # For every element in the list (except the first one).
-    for i in range(1, len(lst)):
-        # Select the first element in the unsorted portion of the list.
-        elem_selected = lst[i]
-
-        # Check the elements in the unsorted portion
-        # and move them one index to the right if they
-        # are greater than the element selected.
-        while i > 0 and elem_selected < lst[i-1]:
-            lst[i] = lst[i-1]
-            i -= 1
-
-        # Insert the element where it belongs.
-        lst[i] = elem_selected
-            
-        
+# ___ insertion_sort lst
+#     # For every element in the list (except the first one).
+#     ___ i __ ra.. 1 le. ?
+#         # Select the first element in the unsorted portion of the list.
+#         elem_selected = ?|?
+#
+#         # Check the elements in the unsorted portion
+#         # and move them one index to the right if they
+#         # are greater than the element selected.
+#         w___ ? > 0 an. ? < ?|?-1
+#             ?|? _ ?|?-1
+#             ? -_ 1
+#
+#         # Insert the element where it belongs.
+#         ?|? _ ?
