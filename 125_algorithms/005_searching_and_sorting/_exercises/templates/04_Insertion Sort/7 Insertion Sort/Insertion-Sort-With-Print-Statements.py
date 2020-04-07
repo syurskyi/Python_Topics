34629ec98e45924df62b5819f9bff7453a09cc93
Grey <sergejyurskyj@yahoo.com>
2020-04-06 21:01:35 -0700

    Committed Insertion-Sort-With-Print-Statements template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/04_Insertion Sort/7 Insertion Sort/Insertion-Sort-With-Print-Statements.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/04_Insertion Sort/7 Insertion Sort/Insertion-Sort-With-Print-Statements.py
@@ -1,49 +1,49 @@
-def insertion_sort(lst):
-
-    print("\n=========> Starting Insertion Sort")
-
-    for i in range(1, len(lst)):
-
-        print(f"\n---> Outer loop. Iteration #{i} (i = {i})")
-
-        if i <= len(lst) - 1:
-            print("Sorted portion:", lst[:i])
-            print("Unsorted portion:", lst[i:])
-        else:
-            print("The list is completely sorted!")
-            print(lst)
-  
-        elem_selected = lst[i]
-
-        print(f"\nWe need to find the correct spot for: {elem_selected}.")
-        print(f"{elem_selected} is the first element in the unsorted portion.")
-        print(f"Now let's compare {elem_selected} with the elements of the sorted portion.")
-        print("Let's find where it belongs...")
-
-        # Move all the elements from the sorted portion of the list
-        # one index to the right if they are greater
-        # than the element selected.
-        
-        while i > 0 and elem_selected < lst[i-1]:
-            print("\n-> Inner loop")
-            print(f"Is the element selected {elem_selected} smaller than {lst[i-1]}?")
-            print(f"Yes, it is! So we need to move {lst[i-1]} to the right to make room for {elem_selected}")
-            print(f"Moving {lst[i-1]} from index {i-1} to index {i} (see below)")
-            print("Old list:", lst)
-            lst[i] = lst[i-1]
-            print("New list:", lst)
-            print(f"See how {lst[i-1]} is now at index {i}")
-            i -= 1
-
-        if i > 0 and elem_selected >= lst[i-1]:
-            print(f"\nIs the element selected ({elem_selected}) smaller than {lst[i-1]}?")
-            print(f"No, it isn't! We need to stay where we are, at index {i}.")
-            print(f"The element {elem_selected} should be there.")
-
-        # Place the element where it belongs
-        print("\nBingo!")
-        print(f"We've found the right location for {elem_selected}: index {i}")
-        lst[i] = elem_selected
-        print("The list is now:", lst)
-
-    print("The list is now sorted!")
+# ___ insertion_sort lst
+#
+#     print("\n=========> Starting Insertion Sort")
+#
+#     ___ i __ ra.. 1 le. ?
+#
+#         print _*\n---> Outer loop. Iteration |? |? = |?
+#
+#         __ i <_ le. ? - 1
+#             print("Sorted portion:", ?|;?
+#             print("Unsorted portion:" ?|i;
+#         ____
+#             print("The list is completely sorted!")
+#             print(?)
+#
+#         elem_selected _ ?|?
+#
+#         print _*\nWe need to find the correct spot for: |?.")
+#         print _*|? is the first element in the unsorted portion.")
+#         print _*Now let's compare |? with the elements of the sorted portion.")
+#         print("Let's find where it belongs...")
+#
+#         # Move all the elements from the sorted portion of the list
+#         # one index to the right if they are greater
+#         # than the element selected.
+#
+#         w___ ? > 0 an. ? < ?|?-1
+#             print("\n-> Inner loop")
+#             print _*Is the element selected |? smaller than |?|?-1?
+#             print _*Yes, it is! So we need to move {?|?-1 to the right to make room for |?")
+#             print _*Moving |?|?-1 from index |?-1 to index |? (see below)")
+#             print("Old list:" ?)
+#             ?|? = ?|?-1
+#             print("New list:", ?)
+#             print _*See how |?|?-1 is now at index |?")
+#             i -_ 1
+#
+#         __ i > 0 an. ? >_ ?|?-1
+#             print _*\nIs the element selected (|?) smaller than |?|?-1 ?")
+#             print _*No, it isn't! We need to stay where we are, at index |?.
+#             print _*The element |? should be there.")
+#
+#         # Place the element where it belongs
+#         print("\nBingo!")
+#         print _*We've found the right location for |?: index |?")
+#         ?|? = elem_selected
+#         print("The list is now:" ?
+#
+#     print("The list is now sorted!")
