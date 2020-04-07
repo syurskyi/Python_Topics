998d4b3b0822fda01a4c7a7e6f29f05701866940
Grey <sergejyurskyj@yahoo.com>
2020-04-06 17:40:56 -0700

    Committed bubble template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/03_Buble Sort/bubble.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/03_Buble Sort/bubble.py
@@ -1,20 +1,19 @@
-
-def bubble_sort(nums):
-    
-	for i in range(len(nums)-1):
-		for j in range(0,len(nums)-1-i,1):
-			if nums[j] > nums[j+1]:
-				swap(nums, j, j+1)
-	
-	return nums
-	
-def swap(nums, i, j):
-	temp = nums[i]
-	nums[i] = nums[j]
-	nums[j] = temp
- 
-if __name__ == "__main__":
-   
-   a = [0,0,0,-1,-0,1,2,3,2,1]
-   print(bubble_sort(a))
-  
+# ___ bubble_sort nums
+#
+# 	___ i __ ra.. le. ?-1
+# 		___ j __ ra.. 0 le. ?-1-? 1
+# 			__ ?|? > ?|?+1
+# 				? ? ? ?+1
+#
+# 	r_ ?
+#
+# ___ swap nums i j
+# 	temp _ ?|?
+# 	?|? _ ?|?
+# 	?|? _ t..
+#
+# __ _______ __ ______
+#
+#    a _ [0,0,0,-1,-0,1,2,3,2,1]
+#    print(? ?
+#
