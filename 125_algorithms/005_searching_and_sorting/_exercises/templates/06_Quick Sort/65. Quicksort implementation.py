fb2331878d753d707648b07e9b96fc793c53d52b
Grey <sergejyurskyj@yahoo.com>
2020-04-07 21:24:30 -0700

    Committed 65. Quicksort implementation templates

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/06_Quick Sort/65. Quicksort implementation.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/06_Quick Sort/65. Quicksort implementation.py
@@ -1,23 +1,23 @@
-def quicksort(arr):
-    """
-    Returns sorted list of integers using Quicksort
-    Input: Unsorted list of integers
-    Note: This is not an in-place implementation
-    """
-    if len(arr) < 2:
-        return arr
-    else:
-        pivot = arr[-1]
-        smaller, equal, larger = [], [], []
-        for num in arr:
-            if num < pivot:
-                smaller.append(num)
-            elif num == pivot:
-                equal.append(num)
-            else:
-                larger.append(num)
-        return quicksort(smaller) + equal + quicksort(larger)
-
-# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
-print(quicksort(l))
+# ___ quicksort arr
+#     """
+#     Returns sorted list of integers using Quicksort
+#     Input: Unsorted list of integers
+#     Note: This is not an in-place implementation
+#     """
+#     __ le. ? < 2
+#         r_ ?
+#     ____
+#         pivot _ ?|-1
+#         smaller, equal, larger _   # lists
+#         ___ num __ ?
+#             __ ? < p..
+#                 s__.ap.. ?
+#             ____ ? __ p..
+#                 e__.ap.. ?
+#             ____
+#                 l___.ap.. ?
+#         r_ ? ? + ? + ? ?
+#
+# # l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# l = [6, 8, 1, 4, 10, 7, 8, 9, 3, 2, 5]
+# print ? ?
