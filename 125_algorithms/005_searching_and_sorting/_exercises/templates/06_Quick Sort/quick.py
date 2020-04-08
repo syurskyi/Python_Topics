5105194c94fcff0bf7aa875c0197842ae7ea09ea
Grey <sergejyurskyj@yahoo.com>
2020-04-07 21:29:30 -0700

    Committed quick templates

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/06_Quick Sort/quick.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/06_Quick Sort/quick.py
@@ -1,39 +1,39 @@
-
-def quick_sort(nums,low,high):
-    
-	if low >= high:
-		return
-		
-	pivot_index = partition(nums,low,high)
-	quick_sort(nums,low, pivot_index-1)
-	quick_sort(nums, pivot_index+1,high)
-	
-	
-def partition(nums,low,high):
-
-	pivot_index = (low+high)//2
-	swap(nums,pivot_index,high)
-	
-	i = low
-	
-	for j in range(low,high,1):
-		if nums[j] <= nums[high]:
-			swap(nums,i,j)
-			i = i + 1
-			
-	swap(nums,i,high)
-	
-	return i
-	
-def swap(nums,i,j):
-	temp = nums[i]
-	nums[i] = nums[j]
-	nums[j] = temp
-	
-	
-if __name__ == "__main__":
-   
-   nums = [-2,-1,0,1,0,-1,-2]
-   quick_sort(nums,0,len(nums)-1)
-   print(nums)
-  
+#
+# ___ quick_sort nums low high
+#
+# 	__ ? >_ ?
+# 		r_
+#
+# 	pivot_index _ ? ? ? ?
+# 	? ? ? ?-1
+# 	? ? ?+1 ?
+#
+#
+# ___ partition nums low high
+#
+# 	pivot_index _ ?+? //2
+# 	s.. ? p.. ?
+#
+# 	i _ l..
+#
+# 	___ j __ ra.. ? ? 1
+# 		__ ?|? <_ ?|?
+# 			? ? i j
+# 			i _ ? + 1
+#
+# 	? ? i h..
+#
+# 	r_ ?
+#
+# ___ swap nums i j
+# 	temp _ ?|?
+# 	?|? _ ?|?
+# 	?|? _ t..
+#
+#
+# __ _______ __ ______
+#
+#    nums _ [-2,-1,0,1,0,-1,-2]
+#    ? ? 0 le. ?)-1)
+#    print ?
+#
