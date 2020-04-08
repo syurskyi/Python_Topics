034615e962bc4c54569d6fe17a3d1c290f47e5a8
Grey <sergejyurskyj@yahoo.com>
2020-04-08 11:05:18 -0700

    Committed analyzer-d template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/06_Quick Sort/71. Project phase 4/analyzer-d.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/06_Quick Sort/71. Project phase 4/analyzer-d.py
@@ -1,30 +1,30 @@
-import time
-import random
-from demos import quicksort, mergesort, bubblesort
-
-def create_random_list(size, max_val):
-    ran_list = []
-    for num in range(size):
-        ran_list.append(random.randint(1,max_val))
-    return ran_list
-
-# For those of you who are familiar with list comprehension covered
-# in section 3, the code in the function above can be written as below:
-
-# def create_random_list(size, max_val):
-#     return [random.randint(1,max_val) for num in range(size)]
-
-def analyze_func(func_name, arr):
-    tic = time.time()
-    func_name(arr)
-    toc = time.time()
-    seconds = toc-tic
-    print(f"{func_name.__name__.capitalize()}\t-> Elapsed time: {seconds}")
-
-size = int(input("What size list do you want to create? "))
-max = int(input("What is the max value of the range? "))
-
-l = create_random_list(size,max)
-analyze_func(bubblesort, l.copy())
-analyze_func(quicksort, l)
-analyze_func(mergesort, l)
+# ______ t..
+# ______ ra..
+# ____ d.. ______ q.. m.. b..
+#
+# ___ create_random_list size max_val
+#     ran_list   # list
+#     ___ num __ ra.. ?
+#         ?.ap.. ra__.r_i.. 1 ?
+#     r_ ?
+#
+# # For those of you who are familiar with list comprehension covered
+# # in section 3, the code in the function above can be written as below:
+#
+# # ___ create_random_list(size, max_val):
+# #     return [random.randint(1,max_val) for num in range(size)]
+#
+# ___ analyze_func func_name arr
+#     tic _ t__.t__
+#     ? ?
+#     toc _ t__.t__
+#     seconds _ to. - t..
+#     print _*|?.-n .ca.. \t-> Elapsed t__: |?
+#
+# size _ in. in..("What size list do you want to create? "))
+# max _ in. in..("What is the max value of the range? "))
+#
+# l _ c.. ? ?
+# ? b.. ?.co..
+# ? q.. ?
+# ? m.. ?
