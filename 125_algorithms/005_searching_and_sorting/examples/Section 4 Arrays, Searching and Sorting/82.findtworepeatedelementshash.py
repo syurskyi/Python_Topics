bbe34e9d751e3db84374541b9fca6ae2e2a91579
Grey <sergejyurskyj@yahoo.com>
2020-04-07 22:01:41 -0700

    Committed 82.findtworepeatedelementshash template

--- 125_algorithms/005_searching_and_sorting/examples/Section 4 Arrays, Searching and Sorting/82.findtworepeatedelementshash.py
+++ 125_algorithms/005_searching_and_sorting/examples/Section 4 Arrays, Searching and Sorting/82.findtworepeatedelementshash.py
@@ -1,8 +1,7 @@
-
 def findtworepeatedelesusinghash(myarray):
-        print "Given array:", myarray
+	print("Given array:", myarray)
 	tab = {}  # hash
-        #print "Created tab:", tab
+	# print "Created tab:", tab
 	for ele in myarray:
 		if ele in tab:
 			tab[ele] += 1
@@ -10,12 +9,14 @@ def findtworepeatedelesusinghash(myarray):
 			tab[ele] = 1
 		else:
 			tab[ele] = 0
-                #print "in loop: tab",tab,"for ele '",ele,"' in array ",myarray
+	# print "in loop: tab",tab,"for ele '",ele,"' in array ",myarray
 
 	for ele in myarray:
-                #print "repeated eles are:"
+		# print "repeated eles are:"
 		if tab[ele] == 2:
-			print ele
-			
+			print(ele)
+
+
 myarray = [3, 5, 7, 18, 12, 18, 12, 1, 19]
 findtworepeatedelesusinghash(myarray)
+
