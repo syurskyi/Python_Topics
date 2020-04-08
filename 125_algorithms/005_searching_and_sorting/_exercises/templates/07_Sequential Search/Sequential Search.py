14e76a490e2fde9c887f1308affc5c6e2da31fa0
Grey <sergejyurskyj@yahoo.com>
2020-04-08 12:04:34 -0700

    Committed Sequential Search template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/07_Sequential Search/Sequential Search.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/07_Sequential Search/Sequential Search.py
@@ -1,82 +1,82 @@
-# Sequential Search
-# Check out the video lecture for a full breakdown, in this Notebook all we do is implement Sequential Search for
-# an Unordered List and an Ordered List.
-# Sequential Search
-
-def seq_search(arr,ele):
-    """
-    General Sequential Search. Works on Unordered lists.
-    """
-
-    # Start at position 0
-    pos = 0
-    # Target becomes true if ele is in the list
-    found = False
-
-    # go until end of list
-    while pos < len(arr) and not found:
-
-        # If match
-        if arr[pos] == ele:
-            found = True
-
-        # Else move one down
-        else:
-            pos  = pos+1
-
-    return found
-
-arr = [1,9,2,8,3,4,7,5,6]
-
-print seq_search(arr,1)
-# True
+# # Sequential Search
+# # Check out the video lecture for a full breakdown, in this Notebook all we do is implement Sequential Search for
+# # an Unordered List and an Ordered List.
+# # Sequential Search
 #
-print seq_search(arr,10)
-# False
+# ___ seq_search arr ele
+#     """
+#     General Sequential Search. Works on Unordered lists.
+#     """
 #
-# Ordered List
+#     # Start at position 0
+#     pos _ 0
+#     # Target becomes true if ele is in the list
+#     found _ F..
 #
-# If we know the list is ordered than, we only have to check until we have found the element or an element greater
-# than it.
-
-def ordered_seq_search(arr,ele):
-    """
-    Sequential search for an Ordered list
-    """
-    # Start at position 0
-    pos = 0
-
-    # Target becomes true if ele is in the list
-    found = False
-
-    # Stop marker
-    stopped = False
-
-    # go until end of list
-    while pos < len(arr) and not found and not stopped:
-
-        # If match
-        if arr[pos] == ele:
-            found = True
-
-        else:
-
-            # Check if element is greater
-            if arr[pos] > ele:
-                stopped = True
-
-            # Otherwise move on
-            else:
-                pos  = pos+1
-
-    return found
-
-arr.sort()
-
-ordered_seq_search(arr,3)
-# True
+#     # go until end of list
+#     w__ p.. < le. ? an. no. ?
 #
-ordered_seq_search(arr,1.5)
-# False
+#         # If match
+#         __ ?|? __ e..
+#             f.. _ T..
 #
-# Good Job!
+#         # Else move one down
+#         ____
+#             ?  _ p.. + 1
+#
+#     r_ ?
+#
+# arr _ [1,9,2,8,3,4,7,5,6]
+#
+# print ? ? 1
+# # True
+# #
+# print ? ? 10
+# # F..
+# #
+# # Ordered List
+# #
+# # If we know the list is ordered than, we only have to check until we have found the element or an element greater
+# # than it.
+#
+# ___ ordered_seq_search arr ele
+#     """
+#     Sequential search for an Ordered list
+#     """
+#     # Start at position 0
+#     pos _ 0
+#
+#     # Target becomes true if ele is in the list
+#     found _ F..
+#
+#     # Stop marker
+#     stopped _ F..
+#
+#     # go until end of list
+#     w__ p.. < le. ? an. no. ? an. no. ?
+#
+#         # If match
+#         __ ?|p.. __ e..
+#             f.. _ T..
+#
+#         ____
+#
+#             # Check if element is greater
+#             __ ?|? > e..
+#                 s.. _ T..
+#
+#             # Otherwise move on
+#             ____
+#                 p..  _ ?+1
+#
+#     r_ ?
+#
+# ?.so..
+#
+# ? ? 3
+# # True
+# #
+# ? ? 1.5
+# # F..
+# #
+# # Good Job!
