2f6d320116609fc94980346dde5b61acaaf371e2
Grey <sergejyurskyj@yahoo.com>
2020-04-08 20:51:53 -0700

    Committed 83.twoelementswhichformasuminthearray template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/Section 4 Arrays, Searching and Sorting/83.twoelementswhichformasuminthearray.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/Section 4 Arrays, Searching and Sorting/83.twoelementswhichformasuminthearray.py
@@ -1,24 +1,24 @@
-def gettwoeleswhichformasum(myarray, Sum):
-	print("Find the sum",Sum,"in the Given array:", myarray)
-	tab = {}  # hash
-	# print "Created tab:",tab
-	# print "In the first loop"
-	for ele in myarray:
-		if ele in tab:
-			tab[ele] += 1
-		elif ele != " ":
-			tab[ele] = 1
-		else:
-			tab[ele] = 0
-		# print "in loop: tab",tab,"for ele '",ele,"' in array ",myarray
-
-        # print "###################################"
-	# print "In the next loop"
-	for ele in myarray:
-		# print "Is",Sum,"-",ele,"found in",tab
-		if Sum - ele in tab:
-			print(ele, "+", Sum - ele, " = ", Sum)
-	
-myarray = [10, 4, 45, 16, 12, 7]
-myarray.sort()
-gettwoeleswhichformasum(myarray, 26)
+# ___ gettwoeleswhichformasum myarray Sum
+# 	print("Find the sum" ? "in the Given array:" ?
+# 	tab _   # dict  # hash
+# 	# print "Created tab:",tab
+# 	# print "In the first loop"
+# 	___ ele __ ?
+# 		__ ? __ t..
+# 			?|? +_ 1
+# 		____ ? !_ " "
+# 			?|? _ 1
+# 		____
+# 			?|? _ 0
+# 		# print "in loop: tab",tab,"for ele '",ele,"' in array ",myarray
+#
+#         # print "###################################"
+# 	# print "In the next loop"
+# 	___ ele __ ?
+# 		# print "Is",Sum,"-",ele,"found in",tab
+# 		__ S.. - e.. __ t..
+# 			print(e.. "+", S.. - e.. " = " S..
+#
+# myarray = [10, 4, 45, 16, 12, 7]
+# ?.so..
+# ? ? 26
