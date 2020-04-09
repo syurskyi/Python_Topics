1a5a03355f0bfc6e9cacfe0a1df79b2468090ba9
Grey <sergejyurskyj@yahoo.com>
2020-04-08 21:49:04 -0700

    Committed Section_10_Associative_Arrays_Hashable_linearprobing template

--- 125_algorithms/007_associate_arrays_dictionary/_exercises/templates/10 Data Structures - Associative Arrays/Section_10_Associative_Arrays_Hashable_linearprobing.py
+++ 125_algorithms/007_associate_arrays_dictionary/_exercises/templates/10 Data Structures - Associative Arrays/Section_10_Associative_Arrays_Hashable_linearprobing.py
@@ -1,54 +1,54 @@
-class HashTable(object):
-
-    def __init__(self):
-        self.size = 10
-        self.keys = [None] * self.size
-        self.values = [None] * self.size
-
-    def put(self, key, data):
-
-        index = self.hashfunction(key)
-
-        # not None -> it is a collision !!!
-        while self.keys[index] is not None:
-            if self.keys[index] == key:
-                self.values[index] = data  # update
-                return
-
-            # rehash try to find another slot
-            index = (index + 1) % self.size
-
-        # insert
-        self.keys[index] = key
-        self.values[index] = data
-
-    def get(self, key):
-
-        index = self.hashfunction(key)
-
-        while self.keys[index] is not None:
-            if self.keys[index] == key:
-                return self.values[index]
-
-            index = (index + 1) % self.size
-
-        # it means the key is not present in the associative array
-        return None
-
-    def hashfunction(self, key):
-        sum = 0
-        for pos in range(len(key)):
-            sum = sum + ord(key[pos])
-
-        return sum % self.size
-
-
-if __name__ == "__main__":
-    table = HashTable()
-
-    table.put("apple", 10)
-    table.put("orange", 20)
-    table.put("car", 30)
-    table.put("table", 40)
-
-    print(table.get("cara"))
+# c_ HashTable o..
+#
+#     ___ -
+#         size _ 10
+#         keys _ |N.. * s..
+#         values _ |N.. * s..
+#
+#     ___ put key data
+#
+#         index _ h.. ?
+#
+#         # not None -> it is a collision !!!
+#         w___ k..|? __ no. N..
+#             __ k..|i.. __ k..
+#                 v..|i.. _ d.  # update
+#                 r_
+#
+#             # rehash try to find another slot
+#             i.. _ ? + 1) % s..
+#
+#         # insert
+#         k..|? _ k..
+#         v..|i.. _ d..
+#
+#     ___ get key
+#
+#         index _ h.. ?
+#
+#         w___ k..|? i. no. N..
+#             __ k..|i.. __ k..
+#                 r_ v..|i..
+#
+#             i.. _ ? + 1) % s..
+#
+#         # it means the key is not present in the associative array
+#         r_ N..
+#
+#     ___ hashfunction key
+#         sum _ 0
+#         ___ pos __ ra.. le. ?
+#             s.. _ ? + or.|k..|?
+#
+#         r_ ? % ?
+#
+#
+# __ ______ __ _____
+#     table _ ?
+#
+#     ?.p.. "apple", 10
+#     ?.p.. "orange", 20
+#     ?.p.. "car", 30
+#     ?.p.. "?", 40
+#
+#     print(?.g.. "cara"
