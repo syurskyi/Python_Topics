abd8a654a144b0579143d51ca107fd89a9c0be51
Grey <sergejyurskyj@yahoo.com>
2020-04-08 20:07:13 -0700

    Committed numer_of_disc_intersections template

--- 125_algorithms/005_searching_and_sorting/_exercises/templates/Beat the Codility Coding Interview in Python/numer_of_disc_intersections.py
+++ 125_algorithms/005_searching_and_sorting/_exercises/templates/Beat the Codility Coding Interview in Python/numer_of_disc_intersections.py
@@ -1,29 +1,29 @@
-# This is the solution for Sorting > NumberOfDiscIntersections
+# # This is the solution for Sorting > NumberOfDiscIntersections
+# #
+# # This is marked as RESPECTABLE difficulty
 #
-# This is marked as RESPECTABLE difficulty
-
-class DiscLog():
-    def __init__(self, x, start_end):
-        self.x = x
-        self.start_end = start_end
-
-def solution(A):
-    discHistory = []
-    for i in range(len(A)):
-        discHistory.append(DiscLog(i - A[i], 1))
-        discHistory.append(DiscLog(i + A[i], -1))
-    discHistory.sort(key=lambda d: (d.x, -d.start_end))
-    intersections = 0
-    active_intersections = 0
-    for log in discHistory:
-        active_intersections += log.start_end
-        if log.start_end > 0:
-            intersections += active_intersections - 1
-        if intersections > 10000000:
-            return -1
-    return intersections
-
-print(solution([1, 5, 2, 1, 4, 0]))
-
-print(solution([0] * 100000))
-
+# c_ DiscLog
+#     ___ - x start_end
+#         ? ?
+#         ? ?
+#
+# ___ solution A
+#     discHistory   # list
+#     ___ i __ ra.. le. ?
+#         ?.ap.. ? ? - ?|? 1
+#         ?.ap.. ? ? + ?|? -1
+#     ?.so.. key_l____ d |?.x -?.s..
+#     intersections _ 0
+#     active_intersections _ 0
+#     ___ log __ d..
+#         a.. +_ ?.s..
+#         __ ?.s > 0
+#             i.. +_ a.. - 1
+#         __ i.. > 10000000:
+#             r_ -1
+#     r_ ?
+#
+# print(solution([1, 5, 2, 1, 4, 0]))
+#
+# print(solution([0] * 100000))
+#
