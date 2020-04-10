ef127abc5025c3f7f2c1aed47d73344a1bec17b8
Grey <sergejyurskyj@yahoo.com>
2020-04-09 20:45:20 -0700

    Committed big-o-python-master template

--- 125_algorithms/010_big_O_time_comlexity/_exercises/templates/big-o-python-master/main.py
+++ 125_algorithms/010_big_O_time_comlexity/_exercises/templates/big-o-python-master/main.py
@@ -1,22 +1,22 @@
-____ one ______ One
-____ log_n ______ LogN
-____ n ______ N
-____ n_square ______ NSquare
-____ random
-
-algorithms = [ One(), LogN(), N(), NSquare() ]
-elements = [10, 100, 250, 500, 1000, 2000, 2500, 3000]
-
-print("-" * 50)
-print("Running algorithms...")
-print("-" * 50)
-
-for e in elements:
-    print("Elements: %d" % e)
-    for alg in algorithms:
-        alg.run(e)
-        alg.print_results()
-    print()
-
-print("-" * 50)
-print("-" * 50)
+# ____ o.. ______ O..
+# ____ l.. ______ L..
+# ____ n ______ N
+# ____ n_s.. ______ NS..
+# ____ ra..
+#
+# algorithms _ | O.. L.. N, NS..
+# elements = [10, 100, 250, 500, 1000, 2000, 2500, 3000]
+#
+# print("-" * 50)
+# print("Running algorithms...")
+# print("-" * 50)
+#
+# ___ e __ e..
+#     print("Elements: @" % e)   # digital
+#     ___ alg __ a..
+#         ?.r.. ?
+#         ?.p..
+#     print()
+#
+# print("-" * 50)
+# print("-" * 50)
