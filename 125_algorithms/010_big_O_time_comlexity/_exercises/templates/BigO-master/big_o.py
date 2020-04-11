add7b395d27bcca57583d70e6a16577d1bd7fcad
Grey <sergejyurskyj@yahoo.com>
2020-04-10 21:11:25 -0700

    Committed big-o- template

--- 125_algorithms/010_big_O_time_comlexity/_exercises/templates/BigO-master/big_o.py
+++ 125_algorithms/010_big_O_time_comlexity/_exercises/templates/BigO-master/big_o.py
@@ -1,15 +1,15 @@
-class BigO_of_1(object):
+c_ BigO_of_1(object):
     def check_index_0_is_int(self, value_list):
         if value_list[0] == int(value_list[0]):
            return True
 
-class BigO_of_N(object):
+c_ BigO_of_N(object):
     def double_values(self, value_list):
         for i in range(0, len(value_list)):
             value_list[i] *= 2
         return value_list
 
-class BigO_of_N_Squared(object):
+c_ BigO_of_N_Squared(object):
     def create_spam_field(self, value_list):
         for i in range(0, len(value_list)):
             value_list[i] = []
@@ -17,7 +17,7 @@ class BigO_of_N_Squared(object):
                 value_list[i].append('spam')
         return value_list
 
-class BigO_of_N_Cubed(object):
+c_ BigO_of_N_Cubed(object):
     def create_spam_space(self, value_list):
         for i in range(0, len(value_list)):
             value_list[i] = []
@@ -27,7 +27,7 @@ class BigO_of_N_Cubed(object):
                     value_list[i][j].append('spam')
         return value_list
 
-class BigO_of_N_to_the_Fourth(object):
+c_ BigO_of_N_to_the_Fourth(object):
     def create_spam_hyperspace(self, value_list):
         for i in range(0, len(value_list)):
             value_list[i] = []
@@ -39,7 +39,7 @@ class BigO_of_N_to_the_Fourth(object):
                         value_list[i][j][k].append('spam')
         return value_list
 
-class BigO_of_2_to_the_N(object):
+c_ BigO_of_2_to_the_N(object):
     def get_factorial(self, value):
         final_number = 0
         if value > 1:
@@ -48,6 +48,6 @@ class BigO_of_2_to_the_N(object):
         else:
             return 1
 
-class BigO_of_N_log_N(object):
+c_ BigO_of_N_log_N(object):
     def sort_list(self, value_list):
         return sorted(value_list)
