091aeedbc45603632d4161fe725f9df37c1affd5
Grey <sergejyurskyj@yahoo.com>
2020-04-11 21:11:27 -0700

    Committed num-islands template

--- 125_algorithms/008_graph_algorithms/_exercises/templates/Cracking Coding Interviews - Mastering Algorithms/num-islands.py
+++ 125_algorithms/008_graph_algorithms/_exercises/templates/Cracking Coding Interviews - Mastering Algorithms/num-islands.py
@@ -1,65 +1,65 @@
-# Example 1:
-
-# Input:
-# 11110
-# 11010
-# 11000
-# 00000
-
-# Output: 1
-
-
-# Example 2:
-
-# Input:
-# 11000
-# 11000
-# 00100
-# 00011
-
-# Output: 3
-
-def mark_island(grid, i, j):
-	if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
-		return
-
-	grid[i][j] = 0
-
-	dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-
-	for d in dirs:
-		mark_island(grid, i + d[0], j + d[1])
-
-def num_islands(grid):
-	if len(grid) == 0 or len(grid[0]) == 0:
-		return 0
-
-	count = 0
-
-	for i in range(len(grid)):
-		for j in range(len(grid[0])):
-			if grid[i][j] == 1:
-				mark_island(grid, i, j)
-				count += 1
-
-	return count
-
-
-grid1 = [
-	[1,1,1,1,0],
-	[1,1,0,1,0],
-	[1,1,0,0,0],
-	[0,0,0,0,0],
-]
-
-grid2 = [
-	[1,1,0,0,0],
-	[1,1,0,0,0],
-	[0,0,1,0,0],
-	[0,0,0,1,1],
-]
-
-print(num_islands(grid1))
-print(num_islands(grid2))
-
-
+# # Example 1:
+#
+# # Input:
+# # 11110
+# # 11010
+# # 11000
+# # 00000
+#
+# # Output: 1
+#
+#
+# # Example 2:
+#
+# # Input:
+# # 11000
+# # 11000
+# # 00100
+# # 00011
+#
+# # Output: 3
+#
+# ___ mark_island grid i j
+# 	__ i < 0 __ ? >_ le. ? __ j < 0 __ ? >_ le. ?|0 __ ?|? |? __ 0
+# 		r_
+#
+# 	?|?|? _ 0
+#
+# 	dirs _ [[1, 0], [-1, 0], [0, 1], [0, -1]]
+#
+# 	__ d __ dirs
+# 		? g.. i + ?|0 j + ?|1
+#
+# ___ num_islands grid
+# 	__ le. ? __ 0 __ le. ?|0 __ 0
+# 		r_ 0
+#
+# 	count _ 0
+#
+# 	___ i __ ra.. le. g..
+# 		___ j __ ra.. le. g..|0
+# 			__ g..|? |? __ 1
+# 				? g.. ? ?
+# 				? +_ 1
+#
+# 	r_ ?
+#
+#
+# grid1 = [
+# 	[1,1,1,1,0],
+# 	[1,1,0,1,0],
+# 	[1,1,0,0,0],
+# 	[0,0,0,0,0],
+# ]
+#
+# grid2 = [
+# 	[1,1,0,0,0],
+# 	[1,1,0,0,0],
+# 	[0,0,1,0,0],
+# 	[0,0,0,1,1],
+# ]
+#
+# print(?(grid1))
+# print(?(grid2))
+#
+#
