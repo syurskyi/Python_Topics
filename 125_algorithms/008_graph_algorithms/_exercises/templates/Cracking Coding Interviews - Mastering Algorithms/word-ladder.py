caebb73ccfa520f5d203eac3892fc59dec993dfc
Grey <sergejyurskyj@yahoo.com>
2020-04-11 21:18:24 -0700

    Committed word-ladder template

--- 125_algorithms/008_graph_algorithms/_exercises/templates/Cracking Coding Interviews - Mastering Algorithms/word-ladder.py
+++ 125_algorithms/008_graph_algorithms/_exercises/templates/Cracking Coding Interviews - Mastering Algorithms/word-ladder.py
@@ -1,75 +1,62 @@
-# Example 1:
-
-# Input:
-# beginWord = "hit",
-# endWord = "cog",
-# wordList = ["hot","dot","dog","lot","log","cog"]
-
-# Output: 5
-
-# Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
-# return its length 5.
-
-
-# Example 2:
-
-# Input:
-# beginWord = "hit"
-# endWord = "cog"
-# wordList = ["hot","dot","dog","lot","log"]
-
-# Output: 0
-
-# Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
-
-from string import ascii_lowercase
-import copy
-
-class Node:
-	def __init__(self, word, path):
-		self.word = word
-		self.path = path
-
-def ladder(begin, end, word_list):
-	queue = []
-
-	words = set(word_list)
-
-	queue.append(Node(begin, [begin]))
-
-	while len(queue) > 0:
-		cur = queue.pop(0)
-
-		cur_word = cur.word
-		path = cur.path
-
-		if cur_word == end:
-			return path
-
-		for i in range(len(cur_word)):
-			for c in ascii_lowercase:
-				potential_word = cur_word[:i] + c + cur_word[i+1:]
-
-				if potential_word in words:
-					copy_path = copy.deepcopy(path)
-					copy_path.append(potential_word)
-					queue.append(Node(potential_word, copy_path))
-					words.remove(potential_word)
-
-	return []
-
-print(ladder("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
-print(ladder("hit", "cog", ["hot","dot","dog","lot","log"]))
-
-
-
-
-
-
-
-
-
-
-
-
-
+# # Example 1:
+#
+# # Input:
+# # beginWord = "hit",
+# # endWord = "cog",
+# # wordList = ["hot","dot","dog","lot","log","cog"]
+#
+# # Output: 5
+#
+# # Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+# # return its length 5.
+#
+#
+# # Example 2:
+#
+# # Input:
+# # beginWord = "hit"
+# # endWord = "cog"
+# # wordList = ["hot","dot","dog","lot","log"]
+#
+# # Output: 0
+#
+# # Explanation: The endWord "cog" is not in wordList, therefore no possible transformation.
+#
+# ____ stR.. _______ a_l..
+# _______ co..
+#
+# C_ Node
+# 	___ - word path
+# 		? ?
+# 		? ?
+#
+# ___ ladder begin end word_list
+# 	queue _    # list
+#
+# 	words _ se. w..
+#
+# 	q__.ap.. ? ? |?
+#
+# 	w__ le. q.. > 0
+# 		cur _ q__.po. 0
+#
+# 		cur_word _ c__.w..
+# 		path _ c__.p..
+#
+# 		__ c.. __ end
+# 			r_ p..
+#
+# 		___ i __ ra.. le. c..
+# 			___ c __ a_l..
+# 				potential_word _ c..|;? + ? + c..|?+1;
+#
+# 				__ potential_word __ words
+# 					copy_path _ co__.d_c.. ?
+# 					?.ap.. ?
+# 					q__.ap.. ? p.. c..
+# 					w__.r.. ?
+#
+# 	r_    # list
+#
+# print(ladder("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
+# print(ladder("hit", "cog", ["hot","dot","dog","lot","log"]))
