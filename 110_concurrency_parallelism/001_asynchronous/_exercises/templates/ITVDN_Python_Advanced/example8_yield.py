df3a561d52fdf9a8e24058022f69cf3a69de17bc
Grey <sergejyurskyj@yahoo.com>
2020-04-12 19:47:33 -0700

    Committed example8_yield template

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/ITVDN_Python_Advanced/example8_yield.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/ITVDN_Python_Advanced/example8_yield.py
@@ -1,21 +1,21 @@
-______ asyncio
-
-
-@asyncio.coroutine
-def test_cor(cor_index, number):
-    result = 0
-    for i in range(number):
-        result += i
-        yield from asyncio.sleep(0)
-        # yield
-        print('Index: {} -> {}'.format(cor_index, i))
-    return result
-
-
-event_loop = asyncio.get_event_loop()
-tasks = asyncio.wait([
-    event_loop.create_task(test_cor(1, 3)),
-    event_loop.create_task(test_cor(2, 4))
-])
-event_loop.run_until_complete(tasks)
-event_loop.close()
+# ______ a..
+#
+#
+# ??.?
+# ___ test_cor cor_index number
+#     result _ 0
+#     ___ i __ ra.. ?
+#         ? +_ ?
+#         ? ? ?.s.. 0
+#         # yield
+#         print('Index: @ -> @'.f.. ? ?
+#     r_ ?
+#
+#
+# event_loop _ ?.g_e_l..
+# tasks _ ?.w..||
+#     ?.c... t.. 1 3
+#     ?.c... t.. 2 4
+# ||
+# ?.r_u_c.. ?
+# ?.c..
