dadada6a515e3bc709e587982bdfd8531f9fa4fe
Grey <sergejyurskyj@yahoo.com>
2020-04-12 19:23:58 -0700

    Committed example3_sync_async template

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/ITVDN_Python_Advanced/example3_sync_async.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/ITVDN_Python_Advanced/example3_sync_async.py
@@ -1,34 +1,34 @@
-______ asyncio
-from asyncio.coroutines ______ iscoroutine
-______ time
-
-
-def sync_worker(number, divider):
-    print('Sync Worker started with values: {} / {}'.format(number, divider))
-    time.sleep(1)
-    print(number / divider)
-
-
-@asyncio.coroutine
-def async_worker(number, divider):
-    print('Async Worker started with values: {} / {}'.format(number, divider))
-    yield from asyncio.sleep(3)
-    print(number / divider)
-
-
-# sync
-sync_worker(30, 10)
-sync_worker(30, 10)
-
-print(iscoroutine(sync_worker))
-print(iscoroutine(async_worker(10, 2)))
-
-# async
-event_loop = asyncio.get_event_loop()
-task_list = [
-    event_loop.create_task(async_worker(30, 10)),
-    event_loop.create_task(async_worker(50, 25)),
-]
-tasks = asyncio.wait(task_list)
-event_loop.run_until_complete(tasks)
-event_loop.close()
+# ______ a..
+# ____ a__.c.. ______ is..
+# ______ ti..
+#
+#
+# ___ sync_worker number divider
+#     print('Sync Worker started with values: @ / @'.f.. ? ?
+#     t__.s.. 1
+#     print ? / ?
+#
+#
+# ??.?
+# ___ async_worker number divider
+#     print('Async Worker started with values: @ / @'.f.. ? ?
+#     y.. f.. a__.s.. 3
+#     print ? / ?
+#
+#
+# # sync
+# s.. 30 10
+# s.. 30 10
+#
+# print i.. s..
+# print(i.. a.. 10 2
+#
+# # async
+# event_loop _ a__.g_e_l..
+# task_list _ |
+#     ?.c.. a.. 30 10
+#     ?.c.. a.. 50 25
+# |
+# tasks _ a__.w.. ?
+# e__.r_u_c.. ?
+# e__.c..
