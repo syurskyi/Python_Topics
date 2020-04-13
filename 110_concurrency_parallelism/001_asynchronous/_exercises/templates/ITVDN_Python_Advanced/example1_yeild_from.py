4d3442fe49e6e6f19539e058f5ee418e372d4a09
Grey <sergejyurskyj@yahoo.com>
2020-04-12 19:13:52 -0700

    Committed example1_yeild_from template

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/ITVDN_Python_Advanced/example1_yeild_from.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/ITVDN_Python_Advanced/example1_yeild_from.py
@@ -1,27 +1,27 @@
-def concat_sequence_v1(s1, s2):
-    for elem in s1:
-        yield elem
-    for elem in s2:
-        yield elem
-
-
-def concat_sequence_v2(s1, s2):
-    yield from s1
-    yield from s2
-
-
-seq1 = range(10)
-seq2 = range(10, 20)
-result = concat_sequence_v1(seq1, seq2)
-
-print('Seq 1')
-for i in result:
-    print(i)
-
-seq1 = range(10)
-seq2 = range(10, 20)
-result = concat_sequence_v2(seq1, seq2)
-
-print('Seq 2')
-for i in result:
-    print(i)
+# ___ concat_sequence_v1 s1 s2
+#     ___ elem __ s1
+#         y___ ?
+#     ___ ? __ s2
+#         y__ ?
+#
+#
+# ___ concat_sequence_v2 s1 s2
+#     y___ f.. ?
+#     y__ f.. ?
+#
+#
+# seq1 _ ra.. 10
+# seq2 _ ra.. 10, 20
+# result _ concat_sequence_v1 ? ?
+#
+# print('Seq 1')
+# ___ i __ r..
+#     print ?
+#
+# seq1 _ ra..(10)
+# seq2 _ ra..(10, 20)
+# result _ _v2 ?
+#
+# print('Seq 2')
+# ___ i __ r..
+#     print ?
