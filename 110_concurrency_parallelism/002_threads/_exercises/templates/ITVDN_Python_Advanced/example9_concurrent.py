05dc43c3291bf4f02d4481d8d718aaa9536f3a93
Grey <sergejyurskyj@yahoo.com>
2020-04-13 18:38:25 -0700

    Committed example9_concurrent template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/ITVDN_Python_Advanced/example9_concurrent.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/ITVDN_Python_Advanced/example9_concurrent.py
@@ -1,49 +1,49 @@
-____ c__.f.. ______ TPE.. PPE..
-______ t..
-
-
-def handler(started=0, finished=0):
-    # print('VALUES', started, finished)
-    result = 0
-    for i in range(started, finished):
-        result += i
-    return result
-
-
-def run_by_executor(executor_class, max_workers=4):
-    executor = executor_class(max_workers=max_workers)
-    started = time.time()
-    future1 = executor.submit(handler, started=0, finished=2 ** 26)
-    future2 = executor.submit(handler, started=2 ** 26, finished=2 ** 28)
-
-    result = future2.result() + future1.result()
-    print('Result: {result}. Time for {executor}: {spent_time}'.format(
-        result=result,
-        executor=executor_class.__name__,
-        spent_time=time.time() - started
-    ))
-
-
-def run_by_executor_map(executor_class, max_workers=4):
-    executor = executor_class(max_workers=max_workers)
-    started = time.time()
-    params = [
-        [0, 2 ** 26],
-        [2 ** 26, 2 ** 28]
-    ]
-    result = sum(executor.map(handler, *params))
-
-    print('Result: {result}. Time for {executor}: {spent_time}'.format(
-        result=result,
-        executor=executor_class.__name__,
-        spent_time=time.time() - started
-    ))
-
-
-print('Execute using map...')
-run_by_executor_map(ThreadPoolExecutor)
-run_by_executor_map(ProcessPoolExecutor)
-
-print('Execute using submit...')
-run_by_executor(ThreadPoolExecutor)
-run_by_executor(ProcessPoolExecutor)
+# ____ c__.f.. ______ TPE.. PPE..
+# ______ t..
+#
+#
+# ___ handler started_0 finished_0
+#     # print('VALUES', started, finished)
+#     result _ 0
+#     ___ i __ ra.. ? ?
+#         ? +_ ?
+#     r_ ?
+#
+#
+# ___ run_by_executor executor_class max_workers_4
+#     executor _ ? m.._m..
+#     started _ t__.t..
+#     future1 _ ?.s.. ? .._0, f.._2 ** 26)
+#     future2 _ ?.s.. ? .._2 ** 26, f.._2 ** 28)
+#
+#     result _ _2.r.. + _1.r..
+#     print('Result: |r... Time for |e..: |s..'.f..(
+#         r.._?
+#         e.._e_c__. -n
+#         spent_time_t__.t.. - s..
+#     ||
+#
+#
+# ___ run_by_executor_map executor_class max_workers_4
+#     executor _ e_c.. m.._m..
+#     started _ t__.t..
+#     params _ [
+#         [0, 2 ** 26],
+#         [2 ** 26, 2 ** 28]
+#     ]
+#     result _ su. ?.ma. handler $params
+#
+#     print('Result: |r__ . Time for |e.. : |s.. '.f..(
+#         r.._r..
+#         e.._e_c__. -n
+#         spent_time_t__.t.. - s..
+#     ))
+#
+#
+# print('Execute using map...')
+# ?_m.. TPE..
+# ?_m.. PPE..
+#
+# print('Execute using submit...')
+# ?_e.. TPE..
+# ?_e.. PPE..
