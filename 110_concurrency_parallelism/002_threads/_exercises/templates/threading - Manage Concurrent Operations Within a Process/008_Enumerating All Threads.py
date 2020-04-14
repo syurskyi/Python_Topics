93ade74af8c173494fe7c027d05496562f1dcb50
Grey <sergejyurskyj@yahoo.com>
2020-04-14 13:31:06 -0700

    Committed 008_Enumerating All Threads template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/008_Enumerating All Threads.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/008_Enumerating All Threads.py
@@ -1,44 +1,44 @@
-# threading_enumerate.py
-
-import random
-import threading
-import time
-import logging
-
-
-def worker():
-    """thread worker function"""
-    pause = random.randint(1, 5) / 10
-    logging.debug('sleeping %0.2f', pause)
-    time.sleep(pause)
-    logging.debug('ending')
-
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='(%(threadName)-10s) %(message)s',
-)
-
-for i in range(3):
-    t = threading.Thread(target=worker, daemon=True)
-    t.start()
-
-main_thread = threading.main_thread()
-for t in threading.enumerate():
-    if t is main_thread:
-        continue
-    logging.debug('joining %s', t.getName())
-    t.join()
-
-# $ python3 threading_enumerate.py
+# # threading_enumerate.py
 #
-# (Thread-1  ) sleeping 0.20
-# (Thread-2  ) sleeping 0.30
-# (Thread-3  ) sleeping 0.40
-# (MainThread) joining Thread-1
-# (Thread-1  ) ending
-# (MainThread) joining Thread-3
-# (Thread-2  ) ending
-# (Thread-3  ) ending
-# (MainThread) joining Thread-2
-
+# ______ ra..
+# ______ th..
+# ______ ti..
+# ______ l..
+#
+#
+# ___ worker
+#     """thread worker function"""
+#     pause _ ra__.r_i..(1, 5) / 10
+#     l__.d.. 'sleeping _0.2_' ?
+#     t__.s.. ?
+#     l__.d..('ending')
+#
+#
+# l__.b..|
+#     l.._l__.D..
+#     f.._'|_|tN..|-10_| _|m..|_',
+# )
+#
+# ___ i __ ra.. 3
+#     t _ ?.T.. t.._? d.._T..
+#     ?.s..
+#
+# main_thread _ ?.m_t..
+# ___ t __ ?.en..
+#     __ t __ ?
+#         c..
+#     l__.debug('joining @' ?.gN..
+#     ?.j..
+#
+# # $ python3 threading_enumerate.py
+# #
+# # (Thread-1  ) sleeping 0.20
+# # (Thread-2  ) sleeping 0.30
+# # (Thread-3  ) sleeping 0.40
+# # (MainThread) joining Thread-1
+# # (Thread-1  ) ending
+# # (MainThread) joining Thread-3
+# # (Thread-2  ) ending
+# # (Thread-3  ) ending
+# # (MainThread) joining Thread-2
+#
