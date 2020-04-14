811b14d93dc5ebdbd7c5417980b16d590e1b832f
Grey <sergejyurskyj@yahoo.com>
2020-04-14 09:22:54 -0700

    Committed 11_receiving_through_yield template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/The Complete Python Course/11_receiving_through_yield.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/The Complete Python Course/11_receiving_through_yield.py
@@ -1,21 +1,21 @@
-from collections import deque
-
-friends = deque(('Rolf', 'Jose', 'Charlie', 'Jen', 'Anna'))
-
-
-def friend_upper():
-    while friends:
-        friend = friends.popleft().upper()
-        greeting = yield
-        print(f'{greeting} {friend}')
-
-
-def greet(g):
-    yield from g
-
-
-greeter = greet(friend_upper())
-greeter.send(None)
-greeter.send('Hello')
-print('Hello, world! Multitasking...')
-greeter.send('How are you,')
+# ____ col.. _______ d..
+#
+# friends _ d.. 'Rolf', 'Jose', 'Charlie', 'Jen', 'Anna'
+#
+#
+# ___ friend_upper
+#     w__ ?
+#         friend _ ?.p.l_.up..
+#         greeting _ y..
+#         print _*|g.. |f..
+#
+#
+# ___ greet g
+#     ? ? ?
+#
+#
+# greeter _ ? _u
+# ?.s.. N..
+# ?.s.. 'Hello'
+# print('Hello, world! Multitasking...')
+# ?.s.. 'How are you,'
