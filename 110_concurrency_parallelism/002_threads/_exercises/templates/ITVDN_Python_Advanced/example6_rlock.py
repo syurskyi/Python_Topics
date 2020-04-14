2b9ba0762899fcaa3479cd2d31b397b243939ce1
Grey <sergejyurskyj@yahoo.com>
2020-04-13 18:24:26 -0700

    Committed example6_rlock template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/ITVDN_Python_Advanced/example6_rlock.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/ITVDN_Python_Advanced/example6_rlock.py
@@ -1,22 +1,22 @@
-# DEAD LOCK
-______ t..
-
-
-def producer():
-    print('Set locking')
-    with lock:
-        with lock:
-            print("It's great")
-    print('Locking release!')
-
-
-lock = threading.RLock()
-
-task1 = threading.Thread(target=producer)
-task2 = threading.Thread(target=producer)
-
-task1.start()
-task2.start()
-
-task1.join()
-task2.join()
+# # DEAD LOCK
+# ______ t..
+#
+#
+# ___ produce
+#     print('Set locking')
+#     w__ ?
+#         w__ ?
+#             print("It's great")
+#     print('Locking release!')
+#
+#
+# lock _ ?.R..
+#
+# task1 _ ?.T... t.._ ?
+# task2 _ ?.T... t.._ ?
+#
+# _1.s..
+# _2.s..
+#
+# _1.j..
+# _2.j..
