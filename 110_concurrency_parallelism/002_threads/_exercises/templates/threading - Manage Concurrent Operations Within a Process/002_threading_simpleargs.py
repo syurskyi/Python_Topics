e948062d5671d4bb50e469d3945f4fa049305800
Grey <sergejyurskyj@yahoo.com>
2020-04-14 10:23:02 -0700

    Committed 002_threading_simpleargs template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/002_threading_simpleargs.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/002_threading_simpleargs.py
@@ -1,26 +1,26 @@
-# threading_simpleargs.py
-
-import threading
-
-
-def worker(num):
-    """thread worker function"""
-    print('Worker: %s' % num)
-
-
-threads = []
-for i in range(5):
-    t = threading.Thread(target=worker, args=(i,))
-    threads.append(t)
-    t.start()
-
-# $ python3 threading_simpleargs.py
+# # threading_simpleargs.py
 #
-# Worker: 0
-# Worker: 1
-# Worker: 2
-# Worker: 3
-# Worker: 4
-
-
-
+# ______ t..
+#
+#
+# ___ worker num
+#     """thread worker function"""
+#     print('Worker: @'  ?
+#
+#
+# threads _  #LIST
+# ___ i __ ra.. 5
+#     t = ?.T.. t.._? args_ ?,
+#     ?.ap.. ?
+#     ?.s..
+#
+# # $ python3 threading_simpleargs.py
+# #
+# # Worker: 0
+# # Worker: 1
+# # Worker: 2
+# # Worker: 3
+# # Worker: 4
+#
+#
+#
