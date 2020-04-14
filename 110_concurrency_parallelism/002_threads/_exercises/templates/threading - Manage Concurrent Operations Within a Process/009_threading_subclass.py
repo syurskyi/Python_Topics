2374db990dfe3e52c563d48fc564a305dab4ba6d
Grey <sergejyurskyj@yahoo.com>
2020-04-14 13:34:09 -0700

    Committed 009_threading_subclass template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/009_threading_subclass.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/009_threading_subclass.py
@@ -1,29 +1,29 @@
-# threading_subclass.py
-
-import threading
-import logging
-
-
-class MyThread(threading.Thread):
-
-    def run(self):
-        logging.debug('running')
-
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='(%(threadName)-10s) %(message)s',
-)
-
-for i in range(5):
-    t = MyThread()
-    t.start()
-
-# $ python3 threading_subclass.py
+# # threading_subclass.py
 #
-# (Thread-1  ) running
-# (Thread-2  ) running
-# (Thread-3  ) running
-# (Thread-4  ) running
-# (Thread-5  ) running
-
+# ______ th..
+# ______ l..
+#
+#
+# c_ MyThread ?.T..
+#
+#     ___ run
+#         l__.d.. 'running'
+#
+#
+# l__.b..|
+#     l.._l___.D..
+#     f.._'|_|tN..|-10_| _|m..|_',
+# )
+#
+# ___ i __ ra.. 5
+#     t _ ?
+#     ?.s..
+#
+# # $ python3 threading_subclass.py
+# #
+# # (Thread-1  ) running
+# # (Thread-2  ) running
+# # (Thread-3  ) running
+# # (Thread-4  ) running
+# # (Thread-5  ) running
+#
