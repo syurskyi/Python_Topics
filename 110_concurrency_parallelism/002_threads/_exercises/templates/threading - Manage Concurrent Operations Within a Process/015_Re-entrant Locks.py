fad9e7ae94cdd0044b45c3e016cf9b838adc39ee
Grey <sergejyurskyj@yahoo.com>
2020-04-15 09:46:12 -0700

    Committed 015_Re-entrant Locks template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/015_Re-entrant Locks.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/015_Re-entrant Locks.py
@@ -1,14 +1,14 @@
-# threading_lock_reacquire.py
-
-import threading
-
-lock = threading.Lock()
-
-print('First try :', lock.acquire())
-print('Second try:', lock.acquire(0))
-
-# $ python3 threading_lock_reacquire.py
+# # threading_lock_reacquire.py
 #
-# First try : True
-# Second try: False
-
+# ______ t..
+#
+# lock _ ?.L..
+#
+# print('First try :' ?.a..
+# print('Second try:' ?.a.. 0
+#
+# # $ python3 threading_lock_reacquire.py
+# #
+# # First try : True
+# # Second try: False
+#
