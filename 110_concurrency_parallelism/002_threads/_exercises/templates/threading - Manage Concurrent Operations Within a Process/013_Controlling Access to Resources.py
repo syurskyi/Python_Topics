4ab6494588f43602cf93c94ee43bd27756fc5db6
Grey <sergejyurskyj@yahoo.com>
2020-04-15 09:35:20 -0700

    Committed 013_Controlling Access to Resources template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/013_Controlling Access to Resources.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/013_Controlling Access to Resources.py
@@ -1,68 +1,68 @@
-# threading_lock.py
-
-import logging
-import random
-import threading
-import time
-
-
-class Counter:
-
-    def __init__(self, start=0):
-        self.lock = threading.Lock()
-        self.value = start
-
-    def increment(self):
-        logging.debug('Waiting for lock')
-        self.lock.acquire()
-        try:
-            logging.debug('Acquired lock')
-            self.value = self.value + 1
-        finally:
-            self.lock.release()
-
-
-def worker(c):
-    for i in range(2):
-        pause = random.random()
-        logging.debug('Sleeping %0.02f', pause)
-        time.sleep(pause)
-        c.increment()
-    logging.debug('Done')
-
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='(%(threadName)-10s) %(message)s',
-)
-
-counter = Counter()
-for i in range(2):
-    t = threading.Thread(target=worker, args=(counter,))
-    t.start()
-
-logging.debug('Waiting for worker threads')
-main_thread = threading.main_thread()
-for t in threading.enumerate():
-    if t is not main_thread:
-        t.join()
-logging.debug('Counter: %d', counter.value)
-
-# $ python3 threading_lock.py
+# # threading_lock.py
 #
-# (Thread-1  ) Sleeping 0.18
-# (Thread-2  ) Sleeping 0.93
-# (MainThread) Waiting for worker threads
-# (Thread-1  ) Waiting for lock
-# (Thread-1  ) Acquired lock
-# (Thread-1  ) Sleeping 0.11
-# (Thread-1  ) Waiting for lock
-# (Thread-1  ) Acquired lock
-# (Thread-1  ) Done
-# (Thread-2  ) Waiting for lock
-# (Thread-2  ) Acquired lock
-# (Thread-2  ) Sleeping 0.81
-# (Thread-2  ) Waiting for lock
-# (Thread-2  ) Acquired lock
-# (Thread-2  ) Done
-# (MainThread) Counter: 4
+# ______ l..
+# ______ r..
+# ______ th..
+# ______ ti..
+#
+#
+# c_ Counter
+#
+#     ___ - start_0
+#         lock _ ?.L..
+#         value _ ?
+#
+#     ___ increment
+#         ?.d.. 'Waiting for lock'
+#         l__.a..
+#         ___
+#             ?.d.. 'Acquired lock'
+#             v.. _ v.. + 1
+#         f..
+#             l__.r..
+#
+#
+# ___ worker c
+#     ___ i __ ra.. 2
+#         pause _ ra__.ra..
+#         ?.d.. 'Sleeping _0.02_' ?
+#         t__.s.. ?
+#         c.i..
+#     ?.d.. 'Done'
+#
+#
+# ?.b..|
+#     l..l__.D..
+#     f.._'|_|tN..|-10_| _|m..|_'
+# |
+#
+# counter _ C..
+# ___ i __ ra.. 2
+#     t _ ?.T.. t.._w.. a.._ ?
+#     ?.s..
+#
+# ?.d.. 'Waiting for worker threads'
+# main_thread _ ?.m..
+# ___ t __ ?.en..
+#     __ t __ no. ?
+#         ?.j..
+# ?.d.. 'Counter: @', c__.v..  # digital
+#
+# # $ python3 threading_lock.py
+# #
+# # (Thread-1  ) Sleeping 0.18
+# # (Thread-2  ) Sleeping 0.93
+# # (MainThread) Waiting for worker threads
+# # (Thread-1  ) Waiting for lock
+# # (Thread-1  ) Acquired lock
+# # (Thread-1  ) Sleeping 0.11
+# # (Thread-1  ) Waiting for lock
+# # (Thread-1  ) Acquired lock
+# # (Thread-1  ) Done
+# # (Thread-2  ) Waiting for lock
+# # (Thread-2  ) Acquired lock
+# # (Thread-2  ) Sleeping 0.81
+# # (Thread-2  ) Waiting for lock
+# # (Thread-2  ) Acquired lock
+# # (Thread-2  ) Done
+# # (MainThread) Counter: 4
