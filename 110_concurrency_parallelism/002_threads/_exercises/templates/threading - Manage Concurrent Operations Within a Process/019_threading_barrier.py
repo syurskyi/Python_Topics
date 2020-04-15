39cb0481bf749bebd80e80dc38eacf4034f0c735
Grey <sergejyurskyj@yahoo.com>
2020-04-15 10:02:31 -0700

    Committed 019_threading_barrier template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/019_threading_barrier.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/019_threading_barrier.py
@@ -1,47 +1,47 @@
-# threading_barrier.py
-
-import threading
-import time
-
-
-def worker(barrier):
-    print(threading.current_thread().name,
-          'waiting for barrier with {} others'.format(
-              barrier.n_waiting))
-    worker_id = barrier.wait()
-    print(threading.current_thread().name, 'after barrier',
-          worker_id)
-
-
-NUM_THREADS = 3
-
-barrier = threading.Barrier(NUM_THREADS)
-
-threads = [
-    threading.Thread(
-        name='worker-%s' % i,
-        target=worker,
-        args=(barrier,),
-    )
-    for i in range(NUM_THREADS)
-]
-
-for t in threads:
-    print(t.name, 'starting')
-    t.start()
-    time.sleep(0.1)
-
-for t in threads:
-    t.join()
-
-# $ python3 threading_barrier.py
+# # threading_barrier.py
 #
-# worker-0 starting
-# worker-0 waiting for barrier with 0 others
-# worker-1 starting
-# worker-1 waiting for barrier with 1 others
-# worker-2 starting
-# worker-2 waiting for barrier with 2 others
-# worker-2 after barrier 2
-# worker-0 after barrier 0
-# worker-1 after barrier 1
+# ______ t..
+# ______ t..
+#
+#
+# ___ worker barrier
+#     print(?.c_t.. .na..
+#           'waiting for barrier with @ others'.f..
+#               ?.n_w..
+#     worker_id _ ?.w..
+#     print ?.c_t.. .n.. 'after barrier'
+#           ?
+#
+#
+# NUM_THREADS _ 3
+#
+# barrier _ ?.B.. ?
+#
+# threads _ |
+#     ?.T..|
+#         n.._'worker-_s' _ i,
+#         t.._?
+#         a.._ b..
+#     |
+#     ___ i __ ra.. ?
+# ]
+#
+# ___ t __ ?
+#     print ?.n.. 'starting'
+#     ?.s..
+#     t__.s.. 0.1
+#
+# ___ t __ ?
+#     ?.j..
+#
+# # $ python3 threading_barrier.py
+# #
+# # worker-0 starting
+# # worker-0 waiting for barrier with 0 others
+# # worker-1 starting
+# # worker-1 waiting for barrier with 1 others
+# # worker-2 starting
+# # worker-2 waiting for barrier with 2 others
+# # worker-2 after barrier 2
+# # worker-0 after barrier 0
+# # worker-1 after barrier 1
