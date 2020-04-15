e487035b380172d2077d19c60d62122e5166042c
Grey <sergejyurskyj@yahoo.com>
2020-04-15 09:51:32 -0700

    Committed 017_Locks as Context Managers template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/017_Locks as Context Managers.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/threading - Manage Concurrent Operations Within a Process/017_Locks as Context Managers.py
@@ -1,38 +1,35 @@
-# threading_lock_with.py
-
-import threading
-import logging
-
-
-def worker_with(lock):
-    with lock:
-        logging.debug('Lock acquired via with')
-
-
-def worker_no_with(lock):
-    lock.acquire()
-    try:
-        logging.debug('Lock acquired directly')
-    finally:
-        lock.release()
-
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='(%(threadName)-10s) %(message)s',
-)
-
-lock = threading.Lock()
-w = threading.Thread(target=worker_with, args=(lock,))
-nw = threading.Thread(target=worker_no_with, args=(lock,))
-
-w.start()
-nw.start()
-
-# $ python3 threading_lock_with.py
+# # threading_lock_with.py
 #
-# (Thread-1  ) Lock acquired via with
-# (Thread-2  ) Lock acquired directly
-
-
-
+# ______ t..
+# ______ l..
+#
+#
+# ___ worker_with lock
+#     w__ ?
+#         l__.d.. 'Lock acquired via with'
+#
+#
+# ___ worker_no_with lock
+#     ?.a..
+#     ___
+#         l__.d.. 'Lock acquired directly'
+#     f..
+#         ?.r..
+#
+#
+# l__.b..|
+#     l.._l__.D..
+#     f.._'|_|tN..|-10_| _|m..|_'
+# )
+#
+# lock _ ?.L..
+# w _ ?.T.. t.._w.. a.._ l..
+# nw _ ?.T.. t.._w.. a.._ l..
+#
+# w.s..
+# nw.s..
+#
+# # $ python3 threading_lock_with.py
+# #
+# # (Thread-1  ) Lock acquired via with
+# # (Thread-2  ) Lock acquired directly
