9df14cec6aa5bd6c63fb5a3549062452b4ae5f32
Grey <sergejyurskyj@yahoo.com>
2020-04-16 10:19:15 -0700

    Committed compute_threaded template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/Async Techniques and Examples in Python/05-threads/cpu_attempt/compute_threaded.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/Async Techniques and Examples in Python/05-threads/cpu_attempt/compute_threaded.py
@@ -1,40 +1,40 @@
-import datetime
-import math
-from threading import Thread
-import multiprocessing
-
-
-def main():
-    do_math(1)
-
-    t0 = datetime.datetime.now()
-
-    # do_math(num=30000000)
-    print("Doing math on {:,} processors.".format(multiprocessing.cpu_count()))
-
-    processor_count = multiprocessing.cpu_count()
-    threads = []
-    for n in range(1, processor_count + 1):
-        threads.append(Thread(target=do_math,
-                              args=(30_000_000 * (n - 1) / processor_count,
-                                    30_000_000 * n / processor_count),
-                              daemon=True)
-                       )
-
-    [t.start() for t in threads]
-    [t.join() for t in threads]
-
-    dt = datetime.datetime.now() - t0
-    print("Done in {:,.2f} sec.".format(dt.total_seconds()))
-
-
-def do_math(start=0, num=10):
-    pos = start
-    k_sq = 1000 * 1000
-    while pos < num:
-        pos += 1
-        math.sqrt((pos - k_sq) * (pos - k_sq))
-
-
-if __name__ == '__main__':
-    main()
+# ? d_t_
+# ? m..
+# ___ t.. ? T..
+# ? mu..
+#
+#
+# ___ main
+#     ? 1
+#
+#     t0 _ d_t_.d_t_.n..
+#
+#     # do_math(num=30000000)
+#     print("Doing math on |;,| processors.".f.. m__.cpu_c..
+#
+#     processor_count _ m...c_c..
+#     threads _  # list
+#     ___ n __ ra..  1 ? + 1
+#         ?.ap.. T.. t.._?
+#                               a.._(30_000_000 * |? - 1) / ?
+#                                     30_000_000 * n / ?
+#                               d.._T..
+#                        )
+#
+#     t.s.. ___ ? __ ?
+#     t.j.. ___ ? __ ?
+#
+#     dt _ d_t_.d_t_.now - t0
+#     print("Done in |;,.2_ sec.".f.. ?.t_s..
+#
+#
+# ___ do_math start_0 num_10
+#     pos _ ?
+#     k_sq _ 1000 * 1000
+#     w___ ? < ?
+#         ? +_ 1
+#         ma__.sqrt p.. - k..| * |p.. - k_s..
+#
+#
+# __ _____ __ ______
+#     ?
