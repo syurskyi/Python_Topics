9a396ef96309379991b8e067ad6711a36c9ed34e
Grey <sergejyurskyj@yahoo.com>
2020-04-16 10:05:25 -0700

    Committed Async Techniques and Examples in Python template

--- 110_concurrency_parallelism/002_threads/_exercises/templates/Async Techniques and Examples in Python/05-threads/hello_threads/hello.py
+++ 110_concurrency_parallelism/002_threads/_exercises/templates/Async Techniques and Examples in Python/05-threads/hello_threads/hello.py
@@ -1,30 +1,30 @@
-import time
-import threading
-
-
-def main():
-    threads = [
-        threading.Thread(target=greeter, args=("Michael", 10), daemon=True),
-        threading.Thread(target=greeter, args=("Sarah", 5), daemon=True),
-        threading.Thread(target=greeter, args=("Zoe", 2), daemon=True),
-        threading.Thread(target=greeter, args=("Mark", 11), daemon=True),
-    ]
-
-    [t.start() for t in threads]
-
-    print("This is other work.")
-    print(2 * 2)
-
-    [t.join(timeout=1) for t in threads]
-
-    print("Done.")
-
-
-def greeter(name: str, times: int):
-    for n in range(0, times):
-        print("{}. Hello there {}".format(n, name))
-        time.sleep(1)
-
-
-if __name__ == '__main__':
-    main()
+# ______ t..
+# ______ t..
+#
+#
+# ___ main
+#     threads _ |
+#         ?.T.. t.._? a.._("Michael", 10) d.._T..
+#         ?.T.. t.._? a.._("Sarah", 5) d.._T..
+#         ?.T.. t.._? a.._("Zoe", 2) d.._T..
+#         ?.T.. t.._? a.._("Mark", 11) d.._T..
+#     |
+#
+#     |t.s.. ___ ? __ ?
+#
+#     print("This is other work.")
+#     print(2 * 2)
+#
+#     |t.j.. timeout_1 ___ ? __ ?
+#
+#     print("Done.")
+#
+#
+# ___ greeter(name ? times ?      # annotation
+#     ___ n __ ra.. 0 t..
+#         print("@. Hello there @".f.. ? n..
+#         t__.s.. 1
+#
+#
+# __ _____ __ ______
+#     ?
