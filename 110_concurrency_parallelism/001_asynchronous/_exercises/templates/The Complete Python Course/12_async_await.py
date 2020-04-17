6302162b3b916804fb3ee31025e7c1393b496cf0
Grey <sergejyurskyj@yahoo.com>
2020-04-17 09:31:34 -0700

    Committed 12_async_await template

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/The Complete Python Course/12_async_await.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/The Complete Python Course/12_async_await.py
@@ -1,29 +1,29 @@
-from collections ______ deque
-from types ______ coroutine
-
-friends = deque(('Rolf', 'Jose', 'Charlie', 'Jen', 'Anna'))
-
-
-@coroutine
-def friend_upper():
-    while friends:
-        friend = friends.popleft().upper()
-        greeting = yield
-        print(f'{greeting} {friend}')
-
-
-async def greet(g):
-    print('Starting...')
-    await g
-    print('Ending...')
-
-
-greeter = greet(friend_upper())
-greeter.send(None)
-greeter.send('Hello')
-
-greeting = input('Enter a greeting: ')
-greeter.send(greeting)
-
-greeting = input('Enter a greeting: ')
-greeter.send(greeting)
+# ___ col.. ______ d..
+# ___ ty.. ______ c..
+#
+# friends _ d.. 'Rolf', 'Jose', 'Charlie', 'Jen', 'Anna'
+#
+#
+# ??
+# ___ friend_upper
+#     w__ ?:
+#         friend _ ?.p_l__.u..
+#         greeting _ y_
+#         print(_*|g.. |f..
+#
+#
+# a.. ___ greet g
+#     print('Starting...')
+#     a.. ?
+#     print('Ending...')
+#
+#
+# greeter _ g.. ?
+# ?.s.. N..
+# ?.s.. 'Hello'
+#
+# greeting _ i.. 'Enter a greeting: '
+# ?.s.. ?
+#
+# greeting _ i.. 'Enter a greeting: '
+# ?.s.. ?
