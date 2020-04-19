e05795320552bbb4422ea0aad83582e2fb7df4ac
Grey <sergejyurskyj@yahoo.com>
2020-04-19 11:25:46 -0700

    Committed 006_Async IOs Roots in Generators templates

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async IO in Python A Complete Walkthrough/006_Async IOs Roots in Generators.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async IO in Python A Complete Walkthrough/006_Async IOs Roots in Generators.py
@@ -1,17 +1,17 @@
-______ asyncio
-
-@asyncio.coroutine
-def py34_coro():
-    """Generator-based coroutine"""
-    # No need to build these yourself, but be aware of what they are
-    s = yield from stuff()
-    return s
-
-async def py35_coro():
-    """Native coroutine, modern syntax"""
-    s = await stuff()
-    return s
-
-async def stuff():
-    return 0x10, 0x20, 0x30
-
+# ______ a..
+#
+# ??.?
+# ___ py34_coro
+#     """Generator-based coroutine"""
+#     # No need to build these yourself, but be aware of what they are
+#     s _ y__ ? s..
+#     r_ ?
+#
+# ? ___ py35_coro
+#     """Native coroutine, modern syntax"""
+#     s _ ? s..
+#     r_ ?
+#
+# ? ___ stuff
+#     r_ 0x10, 0x20, 0x30
+#
