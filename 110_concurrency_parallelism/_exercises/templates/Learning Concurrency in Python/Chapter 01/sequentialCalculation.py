a3b27428ec739ff6b413464dd0e0faa870da8ae3
Grey <sergejyurskyj@yahoo.com>
2020-04-19 13:43:26 -0700

    Committed sequentialCalculation template

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 01/sequentialCalculation.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 01/sequentialCalculation.py
@@ -1,32 +1,32 @@
-import time
-import random
-
-# This does all of our prime factorization on a given number 'n'
-def calculatePrimeFactors(n):
-  primfac = []
-  d = 2
-  while d*d <= n:
-    while (n % d) == 0:
-      primfac.append(d)  # supposing you want multiple factors repeated
-      n //= d
-    d += 1
-  if n > 1:
-    primfac.append(n)
-  return primfac
-
-def main():
-  print("Starting number crunching")
-  t0 = time.time()
-  
-  for i in range(10000):
-    rand = random.randint(20000, 100000000)
-    print(calculatePrimeFactors(rand))
-  
-  t1 = time.time()
-  totalTime = t1 - t0
-
-  print("Execution Time: {}".format(totalTime))
-
-
-if __name__ == '__main__':
-  main()
+# _____ ti..
+# _____ ra..
+#
+# # This does all of our prime factorization on a given number 'n'
+# ___ calculatePrimeFactors n
+#   primfac _   # list
+#   d _ 2
+#   w__ d*d <_ n
+#     w__ (n % d) __ 0
+#       p__.ap.. ?  # supposing you want multiple factors repeated
+#       n //_ d
+#     d +_ 1
+#   __ n > 1
+#     p__.ap.. ?
+#   r_ p..
+#
+# ___ main
+#   print("Starting number crunching")
+#   t0 _ t__.t..
+#
+#   ___ i __ ra.. 10000
+#     rand _ ra__.r_i.. 20000, 100000000)
+#     print ? ?
+#
+#   t1 _ t__.t__
+#   totalTime _ t1 - t0
+#
+#   print("Execution Time: @".f.. ?
+#
+#
+# __ _________ __ ________
+#   ?
