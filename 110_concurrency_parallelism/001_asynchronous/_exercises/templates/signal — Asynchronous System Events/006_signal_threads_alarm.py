36631b25b97fbadc9d60e5a722424f97a18261c9
Grey <sergejyurskyj@yahoo.com>
2020-04-18 20:06:17 -0700

    Committed 006_signal_threads_alarm template

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/signal — Asynchronous System Events/006_signal_threads_alarm.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/signal — Asynchronous System Events/006_signal_threads_alarm.py
@@ -1,47 +1,47 @@
-# signal_threads_alarm.py
-
-______ signal
-______ time
-______ threading
-
-
-def signal_handler(num, stack):
-    print(time.ctime(), 'Alarm in',
-          threading.currentThread().name)
-
-
-signal.signal(signal.SIGALRM, signal_handler)
-
-
-def use_alarm():
-    t_name = threading.currentThread().name
-    print(time.ctime(), 'Setting alarm in', t_name)
-    signal.alarm(1)
-    print(time.ctime(), 'Sleeping in', t_name)
-    time.sleep(3)
-    print(time.ctime(), 'Done with sleep in', t_name)
-
-
-# Start a thread that will not receive the signal
-alarm_thread = threading.Thread(
-    target=use_alarm,
-    name='alarm_thread',
-)
-alarm_thread.start()
-time.sleep(0.1)
-
-# Wait for the thread to see the signal (not going to happen!)
-print(time.ctime(), 'Waiting for', alarm_thread.name)
-alarm_thread.join()
-
-print(time.ctime(), 'Exiting normally')
-
-# $ python3 signal_threads_alarm.py
+# # signal_threads_alarm.py
 #
-# Sat Apr 22 14:49:01 2017 Setting alarm in alarm_thread
-# Sat Apr 22 14:49:01 2017 Sleeping in alarm_thread
-# Sat Apr 22 14:49:01 2017 Waiting for alarm_thread
-# Sat Apr 22 14:49:02 2017 Alarm in MainThread
-# Sat Apr 22 14:49:04 2017 Done with sleep in alarm_thread
-# Sat Apr 22 14:49:04 2017 Exiting normally
-
+# ______ si..
+# ______ ti..
+# ______ th..
+#
+#
+# ___ signal_handler num stack
+#     print(t__.ct.. 'Alarm in',
+#           ?.cT.. .n..
+#
+#
+# si__.si__ si__.S.. ?
+#
+#
+# ___ use_alarm
+#     t_name _ ?.cT.. .n..
+#     print t__.ct.. 'Setting alarm in' ?
+#     si__.al.. 1
+#     print t__.ct.. 'Sleeping in' ?
+#     t__.sl.. 3
+#     print t__.ct.. 'Done with sleep in' ?
+#
+#
+# # Start a thread that will not receive the signal
+# alarm_thread _ ?.T..|
+#     target_u..
+#     name_'alarm_thread',
+# |
+# ?.st..
+# t__.sl.. 0.1
+#
+# # Wait for the thread to see the signal (not going to happen!)
+# print(t__.ct.. 'Waiting for', ?.n..
+# ?.j..
+#
+# print(t__.ct.. 'Exiting normally')
+#
+# # $ python3 signal_threads_alarm.py
+# #
+# # Sat Apr 22 14:49:01 2017 Setting alarm in alarm_thread
+# # Sat Apr 22 14:49:01 2017 Sleeping in alarm_thread
+# # Sat Apr 22 14:49:01 2017 Waiting for alarm_thread
+# # Sat Apr 22 14:49:02 2017 Alarm in MainThread
+# # Sat Apr 22 14:49:04 2017 Done with sleep in alarm_thread
+# # Sat Apr 22 14:49:04 2017 Exiting normally
+#
