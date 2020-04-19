f91a8c51afd4652a4c6f8342f88af6f15842af48
Grey <sergejyurskyj@yahoo.com>
2020-04-19 11:23:59 -0700

    Committed 005_Using a Queue templates

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async IO in Python A Complete Walkthrough/005_Using a Queue.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async IO in Python A Complete Walkthrough/005_Using a Queue.py
@@ -1,56 +1,56 @@
-#!/usr/bin/env python3
-# asyncq.py
-
-______ asyncio
-______ itertools as it
-______ os
-______ random
-______ time
-
-async def makeitem(size: int = 5) -> str:
-    return os.urandom(size).hex()
-
-async def randsleep(a: int = 1, b: int = 5, caller=None) -> None:
-    i = random.randint(0, 10)
-    if caller:
-        print(f"{caller} sleeping for {i} seconds.")
-    await asyncio.sleep(i)
-
-async def produce(name: int, q: asyncio.Queue) -> None:
-    n = random.randint(0, 10)
-    for _ in it.repeat(None, n):  # Synchronous loop for each single producer
-        await randsleep(caller=f"Producer {name}")
-        i = await makeitem()
-        t = time.perf_counter()
-        await q.put((i, t))
-        print(f"Producer {name} added <{i}> to queue.")
-
-async def consume(name: int, q: asyncio.Queue) -> None:
-    while True:
-        await randsleep(caller=f"Consumer {name}")
-        i, t = await q.get()
-        now = time.perf_counter()
-        print(f"Consumer {name} got element <{i}>"
-              f" in {now-t:0.5f} seconds.")
-        q.task_done()
-
-async def main(nprod: int, ncon: int):
-    q = asyncio.Queue()
-    producers = [asyncio.create_task(produce(n, q)) for n in range(nprod)]
-    consumers = [asyncio.create_task(consume(n, q)) for n in range(ncon)]
-    await asyncio.gather(*producers)
-    await q.join()  # Implicitly awaits consumers, too
-    for c in consumers:
-        c.cancel()
-
-if __name__ == "__main__":
-    ______ argparse
-    random.seed(444)
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--nprod", type=int, default=5)
-    parser.add_argument("-c", "--ncon", type=int, default=10)
-    ns = parser.parse_args()
-    start = time.perf_counter()
-    asyncio.run(main(**ns.__dict__))
-    elapsed = time.perf_counter() - start
-    print(f"Program completed in {elapsed:0.5f} seconds.")
+# #!/usr/bin/env python3
+# # asyncq.py
+#
+# ______ a..
+# ______ it.. __ it
+# ______ __
+# ______ ra..
+# ______ ti..
+#
+# ? ___ makeitem size ? _ 5  s..
+#     r_ __.ur.. ?.he.
+#
+# ? ___ randsleep a i.. _ 1 b i.. _ 5 caller_N..  N..
+#     i _ ra__.r_i_ 0 10
+#     __ caller
+#         print _*|?| sleeping for |? seconds.
+#     ? ?.sl.. ?
+#
+# ? ___ produce name in. q ?.Q..  N..
+#     n _ ra__.r_i_ 0 10
+#     ___ _ __ i_.re.. N.. ?  # Synchronous loop for each single producer
+#         ? r_sl.. caller_f"Producer |n..
+#         i _ ? m..
+#         t _ t__.p_c..
+#         ? q.pu. ? ?
+#         print _*Producer |n..| added <|? > to queue.
+#
+# ? ___ consume name i.. q ?.Q.. N..
+#     w__ T..
+#         ? r_sl.. caller_f"Consumer |n..
+#         i, t _ ? q.ge.
+#         now _ t__.p_c..
+#         print(_*Consumer |n.. got element <|? >"
+#               _* in |no.-t;0.5_ seconds.")
+#         q.t_d..
+#
+# ? ___ main nprod i.. ncon i..
+#     q _ ?.Q..
+#     producers _ ?.c_t.. p.. n, q|| ___ ? __ ra.. ?
+#     consumers _ ?.c_t.. c.. n, q|| ___ ? __ ra.. ?
+#     ? ?.g.. $p..
+#     ? q.j..  # Implicitly awaits consumers, too
+#     ___ c __ c..
+#         ?.c..
+#
+# __ _________ __ ________
+#     ______ arg...
+#     ra__.se.. 444
+#     parser _ a__.A..
+#     ?.a_a.. "-p" "--nprod", t.._i.. d.._5
+#     ?.a_a.. "-c" "--ncon", t.._i.. d.._10
+#     ns _ p__.p_a..
+#     start _ t__.p_c..
+#     ?.ru. ? @@ns. -d
+#     elapsed _ t__.p.._c.. - s..
+#     print(_*Program completed in |?;0.5_ seconds.")
