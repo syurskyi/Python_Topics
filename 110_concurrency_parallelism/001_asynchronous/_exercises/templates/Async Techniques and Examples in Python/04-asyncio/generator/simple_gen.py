fbf13239dd80da1f1fe0db52c84c504fa0334701
Grey <sergejyurskyj@yahoo.com>
2020-04-19 11:27:57 -0700

    Committed simple_gen templates

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async Techniques and Examples in Python/04-asyncio/generator/simple_gen.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async Techniques and Examples in Python/04-asyncio/generator/simple_gen.py
@@ -1,28 +1,28 @@
-from typing ______ List
-
-
-# def fib(n: int) -> List[int]:
-#     numbers = []
-#     current, nxt = 0, 1
-#     while len(numbers) < n:
-#         current, nxt = nxt, current + nxt
-#         numbers.append(current)
+# ____ ty.. ______ L..
 #
-#     return numbers
-
-def fib():
-    current, nxt = 0, 1
-    while True:
-        current, nxt = nxt, current + nxt
-        yield current
-
-
-result = fib()
-
-for n in result:
-    print(n, end=', ')
-    if n > 10000:
-        break
-
-print()
-print("Done")
+#
+# # def fib(n: int) -> List[int]:
+# #     numbers = []
+# #     current, nxt = 0, 1
+# #     while len(numbers) < n:
+# #         current, nxt = nxt, current + nxt
+# #         numbers.append(current)
+# #
+# #     return numbers
+#
+# ___ fib
+#     current, nxt = 0, 1
+#     w__ T..
+#         ? ? _ n.. c.. + n..
+#         y___ c..
+#
+#
+# result _ ?
+#
+# ____ n __ ?
+#     print ? e.._', ')
+#     __ ? > 10000
+#         b..
+#
+# print()
+# print("Done")
