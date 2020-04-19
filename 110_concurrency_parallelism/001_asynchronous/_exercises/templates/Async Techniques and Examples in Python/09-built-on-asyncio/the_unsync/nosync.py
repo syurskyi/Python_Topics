27830786655563e93309e58e1a64238cdcc9fd70
Grey <sergejyurskyj@yahoo.com>
2020-04-19 12:48:38 -0700

    Committed nosync templates

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async Techniques and Examples in Python/09-built-on-asyncio/the_unsync/nosync.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async Techniques and Examples in Python/09-built-on-asyncio/the_unsync/nosync.py
@@ -1,61 +1,61 @@
-______ datetime
-______ math
-______ time
-______ requests
-
-
-def main():
-    t0 = datetime.datetime.now()
-
-    compute_some()
-    compute_some()
-    compute_some()
-    download_some()
-    download_some()
-    download_some_more()
-    download_some_more()
-    wait_some()
-    wait_some()
-    wait_some()
-    wait_some()
-
-    dt = datetime.datetime.now() - t0
-    print("Synchronous version done in {:,.2f} seconds.".format(dt.total_seconds()))
-
-
-def compute_some():
-    print("Computing...")
-    for _ in range(1, 10_000_000):
-        math.sqrt(25 ** 25 + .01)
-
-
-def download_some():
-    print("Downloading...")
-    url = 'https://talkpython.fm/episodes/show/174/coming-into-python-from-another-industry-part-2'
-    resp = requests.get(url)
-    resp.raise_for_status()
-
-    text = resp.text
-
-    print("Downloaded (more) {:,} characters.".format(len(text)))
-
-
-def download_some_more():
-    print("Downloading more ...")
-    url = 'https://pythonbytes.fm/episodes/show/92/will-your-python-be-compiled'
-    resp = requests.get(url)
-    resp.raise_for_status()
-
-    text = resp.text
-
-    print("Downloaded {:,} characters.".format(len(text)))
-
-
-def wait_some():
-    print("Waiting...")
-    for _ in range(1, 1000):
-        time.sleep(.001)
-
-
-if __name__ == '__main__':
-    main()
+# ______ da..
+# ______ ma..
+# ______ ti..
+# ______ re..
+#
+#
+# ___ main
+#     t0 _ d_t_.d_t_.n..
+#
+#     c..
+#     c..
+#     c..
+#     d..
+#     d..
+#     d_s_m..
+#     d_s_m..
+#     w..
+#     w..
+#     w..
+#     w..
+#
+#     dt _ d_t_.d_t_.n.. - t0
+#     print("Synchronous version done in |:,.2_ seconds.".f.. ?.t_s..
+#
+#
+# ___ compute_some
+#     print("Computing...")
+#     ___ _ __ ra.. 1 10_000_000
+#         ma__.sq..(25 ** 25 + .01)
+#
+#
+# ___ download_some
+#     print("Downloading...")
+#     url _ 'https://talkpython.fm/episodes/show/174/coming-into-python-from-another-industry-part-2'
+#     resp _ re__.g.. ?
+#     ?.r_f_s..
+#
+#     text _ ?.t..
+#
+#     print("Downloaded (more) |;, characters.".f.. le. ?
+#
+#
+# ___ download_some_more
+#     print("Downloading more ...")
+#     url _ 'https://pythonbytes.fm/episodes/show/92/will-your-python-be-compiled'
+#     resp _ re__.g.. ?
+#     ?.r_f_s..
+#
+#     text _ ?.t..
+#
+#     print Downloaded |:, characters. .f.. le.. ?
+#
+#
+# ___ wait_some
+#     print("Waiting...")
+#     ___ _ __ ra.. 1 1000
+#         t__.s.. .001
+#
+#
+# __ _________ __ ________
+#     ?
