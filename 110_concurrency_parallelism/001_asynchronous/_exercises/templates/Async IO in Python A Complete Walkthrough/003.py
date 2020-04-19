058dcf3f1e9066798b94f600d1cca0d93b63fc02
Grey <sergejyurskyj@yahoo.com>
2020-04-18 20:35:19 -0700

    Committed Async IO in Python A Complete Walkthrough 003 template

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async IO in Python A Complete Walkthrough/003.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async IO in Python A Complete Walkthrough/003.py
@@ -1,33 +1,33 @@
-#!/usr/bin/env python3
-# rand.py
-
-______ asyncio
-______ random
-
-# ANSI colors
-c = (
-    "\033[0m",   # End of color
-    "\033[36m",  # Cyan
-    "\033[91m",  # Red
-    "\033[35m",  # Magenta
-)
-
-async def makerandom(idx: int, threshold: int = 6) -> int:
-    print(c[idx + 1] + f"Initiated makerandom({idx}).")
-    i = random.randint(0, 10)
-    while i <= threshold:
-        print(c[idx + 1] + f"makerandom({idx}) == {i} too low; retrying.")
-        await asyncio.sleep(idx + 1)
-        i = random.randint(0, 10)
-    print(c[idx + 1] + f"---> Finished: makerandom({idx}) == {i}" + c[0])
-    return i
-
-async def main():
-    res = await asyncio.gather(*(makerandom(i, 10 - i - 1) for i in range(3)))
-    return res
-
-if __name__ == "__main__":
-    random.seed(444)
-    r1, r2, r3 = asyncio.run(main())
-    print()
-    print(f"r1: {r1}, r2: {r2}, r3: {r3}")
+# #!/usr/bin/env python3
+# # rand.py
+#
+# ______ a..
+# ______ ra..
+#
+# # ANSI colors
+# c _ |
+#     "\033[0m",   # End of color
+#     "\033[36m",  # Cyan
+#     "\033[91m",  # Red
+#     "\033[35m",  # Magenta
+# |
+#
+# a.. ___ makerandom idx ? threshold ? _ 6)  in.
+#     print ?|? + 1| + _*Initiated makerandom||i.. .
+#     i _ ra__.r_i.. 0 10
+#     w__ ? <_ t..
+#         print ?|i.. + 1| + _*makerandom||i.. __ |?| too low; retrying.")
+#         a.. ?.sl.. i.. + 1
+#         i _ ra__.r_i.. 0 10
+#     print ?|i.. + 1| + _*---> Finished: makerandom||i.. ++ |?|" + c[0])
+#     r_ ?
+#
+# a.. ___ main
+#     res _ a.. ?.g..(*(m..|i, 10 - i - 1) ___ i __ ra.. 3
+#     r_ ?
+#
+# __ ______ __ ______
+#     ra__.se.. 444
+#     r1, r2, r3 _ ?.r.. ?
+#     print()
+#     print _*r1: {r1}, r2: {r2}, r3: {r3}")
