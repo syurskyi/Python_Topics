730f27ea048e332158520b25aa96a60b82ed7ab4
Grey <sergejyurskyj@yahoo.com>
2020-04-18 19:34:18 -0700

    Committed 001_Receiving Signals template

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/signal — Asynchronous System Events/001_Receiving Signals.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/signal — Asynchronous System Events/001_Receiving Signals.py
@@ -1,40 +1,40 @@
-# signal_signal.py
-
-______ signal
-______ os
-______ time
-
-
-def receive_signal(signum, stack):
-    print('Received:', signum)
-
-
-# Register signal handlers
-signal.signal(signal.SIGUSR1, receive_signal)
-signal.signal(signal.SIGUSR2, receive_signal)
-
-# Print the process ID so it can be used with 'kill'
-# to send this program signals.
-print('My PID is:', os.getpid())
-
-while True:
-    print('Waiting...')
-    time.sleep(3)
-
-# $ python3 signal_signal.py
+# # signal_signal.py
 #
-# My PID is: 71387
-# Waiting...
-# Waiting...
-# Waiting...
-# Received: 30
-# Waiting...
-# Waiting...
-# Received: 31
-# Waiting...
-# Waiting...
-# Traceback (most recent call last):
-#   File "signal_signal.py", line 28, in <module>
-#     time.sleep(3)
-# KeyboardInterrupt
-
+# ______ si..
+# ______ __
+# ______ ti..
+#
+#
+# ___ receive_signal signum stack
+#     print('Received:' ?
+#
+#
+# # Register signal handlers
+# si__.si__(si__.S..1 ?
+# si__.si__(si__.S..2 ?
+#
+# # Print the process ID so it can be used with 'kill'
+# # to send this program signals.
+# print('My PID is:' __.g_p..
+#
+# w__ T..
+#     print('Waiting...')
+#     ti__.sl..3
+#
+# # $ python3 signal_signal.py
+# #
+# # My PID is: 71387
+# # Waiting...
+# # Waiting...
+# # Waiting...
+# # Received: 30
+# # Waiting...
+# # Waiting...
+# # Received: 31
+# # Waiting...
+# # Waiting...
+# # Traceback (most recent call last):
+# #   File "signal_signal.py", line 28, in <module>
+# #     time.sleep(3)
+# # KeyboardInterrupt
+#
