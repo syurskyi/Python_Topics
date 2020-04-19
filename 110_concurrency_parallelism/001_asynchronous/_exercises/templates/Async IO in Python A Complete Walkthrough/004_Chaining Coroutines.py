028862d6ba576c0deb814599694bf3a27ea73369
Grey <sergejyurskyj@yahoo.com>
2020-04-18 21:45:06 -0700

    Committed 004_Chaining Coroutines templates

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async IO in Python A Complete Walkthrough/004_Chaining Coroutines.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Async IO in Python A Complete Walkthrough/004_Chaining Coroutines.py
@@ -1,41 +1,41 @@
-#!/usr/bin/env python3
-# chained.py
-
-______ asyncio
-______ random
-______ time
-
-async def part1(n: int) -> str:
-    i = random.randint(0, 10)
-    print(f"part1({n}) sleeping for {i} seconds.")
-    await asyncio.sleep(i)
-    result = f"result{n}-1"
-    print(f"Returning part1({n}) == {result}.")
-    return result
-
-async def part2(n: int, arg: str) -> str:
-    i = random.randint(0, 10)
-    print(f"part2{n, arg} sleeping for {i} seconds.")
-    await asyncio.sleep(i)
-    result = f"result{n}-2 derived from {arg}"
-    print(f"Returning part2{n, arg} == {result}.")
-    return result
-
-async def chain(n: int) -> None:
-    start = time.perf_counter()
-    p1 = await part1(n)
-    p2 = await part2(n, p1)
-    end = time.perf_counter() - start
-    print(f"-->Chained result{n} => {p2} (took {end:0.2f} seconds).")
-
-async def main(*args):
-    await asyncio.gather(*(chain(n) for n in args))
-
-if __name__ == "__main__":
-    ______ sys
-    random.seed(444)
-    args = [1, 2, 3] if len(sys.argv) == 1 else map(int, sys.argv[1:])
-    start = time.perf_counter()
-    asyncio.run(main(*args))
-    end = time.perf_counter() - start
-    print(f"Program finished in {end:0.2f} seconds.")
+# #!/usr/bin/env python3
+# # chained.py
+#
+# ______ a..
+# ______ ra..
+# ______ ti..
+#
+# a.. ___ part1 n i..  st.
+#     i _ ra__.r_i.. 0, 10
+#     print(_*part1(|?|) sleeping for |?| seconds.
+#     a.. ?.s.. ?
+#     result _ _*r..|?|-1"
+#     print(_*Returning part1 ||? __ |r..| .")
+#     return result
+#
+# a.. ___ part2 n ? arg ?  st.
+#     i _ ra__.r_i.. 0 10
+#     print(_*part2{n, arg} sleeping for {i} seconds.")
+#     a.. ?.s.. ?
+#     result _ _*result|?|-2 derived from {arg}"
+#     print(_*Returning part2{n, arg} == {result}.")
+#     r_ ?
+#
+# a.. ___ chain n ?  N..
+#     start _ t__.p_c..
+#     p1 _ a.. _1 ?
+#     p2 _ a.. _2 n _1
+#     end _ t__.p_c.. - s..
+#     print(_*-->Chained result|?| _> |_2| (took |e..;0.2_| seconds).")
+#
+# a.. ___ main $
+#     a.. ?.g..(*(chain(n) for n in args))
+#
+# __ ______ __ ______
+#     ______ ___
+#     ra__.seed(444)
+#     args _ [1, 2, 3] __ le. ___.a.. __ 1 ____ ma. in. ___.a..|1;
+#     start _ t__.p_c..
+#     ?.ru. ? $
+#     end _ t__.p_c.. - s..
+#     print(_*Program finished in |e..;0.2_| seconds.")
