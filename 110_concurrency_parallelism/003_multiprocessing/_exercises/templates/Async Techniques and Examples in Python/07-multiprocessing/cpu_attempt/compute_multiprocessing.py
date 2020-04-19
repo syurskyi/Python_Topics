6391b78dbd6333aa3e9cc9da74b65e9483358b46
Grey <sergejyurskyj@yahoo.com>
2020-04-19 13:14:20 -0700

    Committed compute_multiprocessing template

--- 110_concurrency_parallelism/003_multiprocessing/_exercises/templates/Async Techniques and Examples in Python/07-multiprocessing/cpu_attempt/compute_multiprocessing.py
+++ 110_concurrency_parallelism/003_multiprocessing/_exercises/templates/Async Techniques and Examples in Python/07-multiprocessing/cpu_attempt/compute_multiprocessing.py
@@ -1,44 +1,44 @@
-import datetime
-import math
-import multiprocessing
-
-
-def main():
-    do_math(1)
-
-    t0 = datetime.datetime.now()
-
-    print("Doing math on {:,} processors.".format(multiprocessing.cpu_count()))
-
-    pool = multiprocessing.Pool()
-    processor_count = multiprocessing.cpu_count()
-    tasks = []
-    for n in range(1, processor_count + 1):
-        task = pool.apply_async(do_math, (30_000_000 * (n - 1) / processor_count,
-                                          30_000_000 * n / processor_count))
-        tasks.append(task)
-
-    pool.close()
-    pool.join()
-
-    dt = datetime.datetime.now() - t0
-    print("Done in {:,.2f} sec.".format(dt.total_seconds()))
-    print("Our results: ")
-    for t in tasks:
-        print(t.get())
-
-
-def do_math(start=0, num=10):
-    pos = start
-    k_sq = 1000 * 1000
-    ave = 0
-    while pos < num:
-        pos += 1
-        val = math.sqrt((pos - k_sq) * (pos - k_sq))
-        ave += val / num
-
-    return int(ave)
-
-
-if __name__ == '__main__':
-    main()
+# _____ da..
+# _____ ma..
+# _____ mu..
+#
+#
+# ___ main
+#     ? 1
+#
+#     t0 _ d_t_.d_t_.n..
+#
+#     print *Doing math on |;, processors. .f.. ?.c_c..
+#
+#     pool _ ?.P..
+#     processor_count _ ?.c_c..
+#     tasks _   @ list
+#     ___ n __ ra.. 1 p_c.. + 1
+#         task _ p__.a_a.. ? (30_000_000 * |? - 1| / p_c..
+#                                           30_000_000 * ? / p_c..
+#         ?.ap.. ?
+#
+#     p__.cl..
+#     p__.j..
+#
+#     dt _ d_t_.d_t_.n.. - t0
+#     print *Done __ |;,.2_ sec. .f.. ?.t_s..
+#     print("Our results: ")
+#     ___ t __ tasks
+#         print ?.g..
+#
+#
+# ___ do_math start_0 num_10
+#     pos _ s..
+#     k_sq _ 1000 * 1000
+#     ave _ 0
+#     w__ p.. < n..
+#         p.. +_ 1
+#         val _ ma__.sq..||p.. - k_|| * |p.. - k_
+#         a.. +_ v.. / n..
+#
+#     r_ i.. a..
+#
+#
+# __ _________ __ ________
+#     ?
