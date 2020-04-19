a0d1f161b9061a69adf1bc9f6d4cd3cbcda5c2a6
Grey <sergejyurskyj@yahoo.com>
2020-04-19 13:08:23 -0700

    Committed cpu_attempt template

--- 110_concurrency_parallelism/003_multiprocessing/_exercises/templates/Async Techniques and Examples in Python/07-multiprocessing/cpu_attempt/compute_it.py
+++ 110_concurrency_parallelism/003_multiprocessing/_exercises/templates/Async Techniques and Examples in Python/07-multiprocessing/cpu_attempt/compute_it.py
@@ -1,25 +1,25 @@
-import datetime
-import math
-
-
-def main():
-    do_math(1)
-
-    t0 = datetime.datetime.now()
-
-    do_math(num=30000000)
-
-    dt = datetime.datetime.now() - t0
-    print("Done in {:,.2f} sec.".format(dt.total_seconds()))
-
-
-def do_math(start=0, num=10):
-    pos = start
-    k_sq = 1000 * 1000
-    while pos < num:
-        pos += 1
-        dist = math.sqrt((pos - k_sq)*(pos - k_sq))
-
-
-if __name__ == '__main__':
-    main()
+# _____ da..
+# _____ ma..
+#
+#
+# ___ main
+#     ? 1
+#
+#     t0 _ d_t_.d_t_.n..
+#
+#     ? num_30000000)
+#
+#     dt _ d_t_.d_t_.n.. - t0
+#     print *Done in |;,.2_ sec. .f.. ?.t_s..
+#
+#
+# ___ do_math start_0, num_10
+#     pos _ s..
+#     k_sq _ 1000 * 1000
+#     w__ p.. < n..
+#         p.. +_ 1
+#         dist _ ma__.sq.. p.. - k_s.|*|p.. - k_s.
+#
+#
+# __ _________ __ ________
+#     ?
