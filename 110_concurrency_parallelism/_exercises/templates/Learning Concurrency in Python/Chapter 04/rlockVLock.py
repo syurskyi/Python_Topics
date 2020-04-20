11c362dd8ea0eb657ff0847f980917fb4d47dbd0
Grey <sergejyurskyj@yahoo.com>
2020-04-20 09:01:10 -0700

    Committed rlockVLock templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/rlockVLock.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/rlockVLock.py
@@ -1,34 +1,34 @@
-______ threading
-______ time
-
-class myWorker():
-
-  def __init__(self):
-    self.a = 1
-    self.b = 2
-    self.lock = threading.Lock()
-  
-  def modifyA(self)
-    print("Modifying A : RLock Acquired: {}".format(self.lock._is_owned()))
-    print("{}".format(self.lock))
-    self.a = self.a + 1
-    time.sleep(5)
-
-  def modifyB(self):
-    print("Modifying B : Lock Acquired: {}".format(self.lock._is_owned()))
-    print("{}".format(self.lock))
-    self.b = self.b - 1
-    time.sleep(5)
-
-  def modifyBoth(self):
-    with self.lock:
-      print("lock acquired, modifying A and B")
-      print("{}".format(self.lock))
-      self.modifyA()
-      print("{}".format(self.lock))
-      self.modifyB()
-    print("{}".format(self.lock))
-
-
-workerA = myWorker()
-workerA.modifyBoth()
+# ______ th..
+# ______ ti..
+#
+# c_ myWorker
+#
+#   ___ -
+#     a _ 1
+#     b _ 2
+#     lock _ ?.L..
+#
+#   ___ modifyA
+#     print("Modifying A : RLock Acquired: @".f.. l__._i_o..
+#     print("@".f.. l..
+#     a _ a + 1
+#     t__.s.. 5
+#
+#   ___ modifyB
+#     print("Modifying B : Lock Acquired: @".f.. l__._i_o..
+#     print("@".f.. l..
+#     b _ b - 1
+#     t__.s.. 5
+#
+#   ___ modifyBoth
+#     w__ l..
+#       print("lock acquired, modifying A and B")
+#       print("@".f.. ?
+#       ?
+#       print("@".f.. ?
+#       ?
+#     print("@".f.. ?
+#
+#
+# workerA _ ?
+# ?._B...
