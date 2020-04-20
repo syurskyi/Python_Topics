29d95173b08282ddbe5edd3762733fdd86bc0366
Grey <sergejyurskyj@yahoo.com>
2020-04-19 18:20:01 -0700

    Committed events templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/events.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/events.py
@@ -1,20 +1,20 @@
-______ threading
-______ time
-
-def myThread(myEvent):
-  while not myEvent.is_set():
-    print("Waiting for Event to be set")
-    time.sleep(1)
-  print("myEvent has been set")
-
-def main():
-  myEvent = threading.Event()
-  thread1 = threading.Thread(target=myThread, args=(myEvent,))
-  thread1.start()
-
-  time.sleep(10)
-  myEvent.set()
-
-
-if __name__ == '__main__':
-  main()
+# ______ th..
+# ______ ti..
+#
+# ___ myThread myEvent
+#   w__ no. ?.i_s..
+#     print("Waiting for Event to be set")
+#     t__.s.. 1
+#   print("myEvent has been set")
+#
+# ___ main
+#   myEvent _ ?.E..
+#   thread1 _ ?.T.. t.._? a.._ mE..,
+#   ?.s..
+#
+#   t__.s.. 10
+#   mE__.s..
+#
+#
+# __ _________ __ ________
+#   ?
