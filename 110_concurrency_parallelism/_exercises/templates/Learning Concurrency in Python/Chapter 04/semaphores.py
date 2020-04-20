8375e5317ba548b70bbd8043bedc170d93498e80
Grey <sergejyurskyj@yahoo.com>
2020-04-20 11:54:37 -0700

    Committed semaphores templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/semaphores.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/semaphores.py
@@ -1,47 +1,47 @@
-______ threading
-______ time
-______ random
+______ th..
+______ ti..
+______ ra..
 
-class TicketSeller(threading.Thread):
-  ticketsSold = 0
+c_ TicketSeller(?.T..
+  ticketsSold _ 0
 
-  def __init__(self, semaphore):
-    threading.Thread.__init__(self);
-    self.sem = semaphore
+  ___ -  semaphore
+    ?.T__. -
+    sem _ ?
     print("Ticket Seller Started Work")
 
-  def run(self):
-    global ticketsAvailable
-    running = True
-    while running:
-      self.randomDelay()
+  ___ run
+    g.. ticketsAvailable
+    running _ T..
+    w__ ?
+      r..
       
-      self.sem.acquire()
-      if(ticketsAvailable <= 0):
-        running = False
-      else:
-        self.ticketsSold = self.ticketsSold + 1
-        ticketsAvailable = ticketsAvailable - 1
-        print("{} Sold One ({} left)".format(self.getName(), ticketsAvailable))
-      self.sem.release()
-    print("Ticket Seller {} Sold {} tickets in total".format(self.getName(), self.ticketsSold))
+      s__.a..
+      __ t.. <_ 0
+        running _ F..
+      ____
+        tS.. _ tS.. + 1
+        tA.. _ tA.. - 1
+        print("@ Sold One (@ left)".f.. gN.. tA..
+      s__.r..
+    print("Ticket Seller @ Sold @ tickets in total".f.. gN.. tS..
 
-  def randomDelay(self):
-    time.sleep(random.randint(0,4)/4)
+  ___ randomDelay
+    t__.s.. ra__.r_i..(0,4)/4)
 
 
 # our sempahore primitive
-semaphore = threading.Semaphore()
+semaphore _ ?.S..
 # Our Ticket Allocation
-ticketsAvailable = 200
+ticketsAvailable _ 200
 
 # our array of sellers
-sellers = []
-for i in range(4):
-  seller = TicketSeller(semaphore)
-  seller.start()
-  sellers.append(seller)
+sellers _   # list
+___ i __ ra.. 4
+  seller _ ? s..
+  ?.s..
+  ?.ap.. ?
 
 # joining all our sellers
-for seller in sellers:
-  seller.join()
+___ s.. __ ?s
+  ?.j..
