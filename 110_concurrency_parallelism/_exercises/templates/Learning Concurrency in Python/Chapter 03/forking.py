a6e1261d384dbdf7cb53054e6226e8d75c946c4a
Grey <sergejyurskyj@yahoo.com>
2020-04-19 17:55:14 -0700

    Committed forking templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 03/forking.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 03/forking.py
@@ -1,14 +1,14 @@
-______ os 
-
-def child(): 
-  print("We are in the child process with PID= %d"%os.getpid()) 
-  
-def parent(): 
-  print("We are in the parent process with PID= %d"%os.getpid())
-  newRef=os.fork() 
-  if newRef==0: 
-    child() 
-  else: 
-    print("We are in the parent process and our child process has PID= %d"%newRef)
-    
-parent()
+# ______ __
+#
+# ___ child
+#   print("We are in the child process with PID= @"  __.g_p..
+#
+# ___ parent
+#   print("We are in the parent process with PID= %d"%os.getpid())
+#   newRef _ __.f..
+#   __ ? __ 0
+#     c..
+#   ____
+#     print("We are in the parent process and our child process has PID= @" ?
+#
+# p..
