54b8730d881d316143a134bbf1f7b78bd6620e56
Grey <sergejyurskyj@yahoo.com>
2020-04-19 18:10:59 -0700

    Committed startingMulti templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 03/startingMulti.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 03/startingMulti.py
@@ -1,11 +1,11 @@
-______ threading
-______ time
-
-def myWorker():
-  pass
-
-def main():
-  
-
-if __name__ == '__main__':
-  main()
+# ______ th..
+# ______ ti..
+#
+# ___ myWorker
+#   p..
+#
+# ___ main
+#
+#
+# __ _________ __ ________
+#   ?
