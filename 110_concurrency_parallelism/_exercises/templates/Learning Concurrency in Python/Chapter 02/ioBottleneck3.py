9616484b3424af8096bd6b3dec531df05843156e
Grey <sergejyurskyj@yahoo.com>
2020-04-19 17:32:14 -0700

    Committed ioBottleneck3 templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 02/ioBottleneck3.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 02/ioBottleneck3.py
@@ -1,16 +1,16 @@
-______ urllib.request
-______ time
-from bs4 ______ BeautifulSoup
-
-t0 = time.time()
-req = urllib.request.urlopen('http://www.example.com')
-t1 = time.time()
-print("Total Time To Fetch Page: {} Seconds".format(t1-t0))
-soup = BeautifulSoup(req.read(), "html.parser")
-
-for link in soup.find_all('a'):
-  print(link.get('href'))
-
-
-t2 = time.time()
-print("Total Execeution Time: {} Seconds".format)
+# ______ u_l_.re..
+# ______ t..
+# ____ __4 ______ BS..
+#
+# t0 _ t__.t__
+# req _ u_l_.re__.u_o.. http://www.example.com
+# t1 _ t__.t__
+# print("Total Time To Fetch Page: @ Seconds".f.. t1-t0
+# soup _ BS.. ?.r.. "html.parser")
+#
+# ___ link __ ?.f_a.. 'a'
+#   print ?.g.. 'href'
+#
+#
+# t2 _ t__.t__
+# print("Total Execeution Time: @ Seconds".f..
