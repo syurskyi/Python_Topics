46e9740dbe6b1277811d7f5044f79f1c99c08bac
Grey <sergejyurskyj@yahoo.com>
2020-04-19 20:27:45 -0700

    Committed lockExample templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/lockExample.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/lockExample.py
@@ -1,47 +1,47 @@
-______ threading
-______ time
-______ random
-
-counter = 1
-lock = threading.Lock()
-
-def workerA():
-  global counter
-  lock.acquire()
-  try:
-    while counter < 1000:
-      counter += 1
-      print("Worker A is incrementing counter to {}".format(counter))
-      
-  finally:
-    lock.release()
-
-def workerB():
-  global counter
-  lock.acquire()
-  try:
-    while counter > -1000:
-      counter -= 1
-      print("Worker B is decrementing counter to {}".format(counter))
-      
-  finally:
-    lock.release()
-
-def main():
-  t0 = time.time()
-  thread1 = threading.Thread(target=workerA)
-  thread2 = threading.Thread(target=workerB)
-
-  thread1.start()
-  thread2.start()
-
-  thread1.join()
-  thread2.join()
-
-  t1 = time.time()
-
-  print("Execution Time {}".format(t1-t0))
-
-if __name__ == '__main__':
-  main()
-
+# ______ th..
+# ______ ti..
+# ______ ra..
+#
+# counter _ 1
+# lock _ ?.L..
+#
+# ___ workerA
+#   g.. c..
+#   l__.a..
+#   ___
+#     w__ c.. < 1000
+#       ? +_ 1
+#       print("Worker A is incrementing counter to {}".f.. ?
+#
+#   f..
+#     l__.r..
+#
+# ___ workerB
+#   g.. c..
+#   l__.a..
+#   ___
+#     w__ c.. > -1000
+#       ? -_ 1
+#       print("Worker B is decrementing counter to @".f.. ?
+#
+#   f..
+#     l__.r..
+#
+# ___ main
+#   t0 _ t__.t..
+#   thread1 _ ?.T.. _A
+#   thread2 _ ?.T.. _B
+#
+#   _1.s..
+#   _2.s..
+#
+#   _1.j..
+#   _2.j..
+#
+#   t1 _ t__.t..
+#
+#   print("Execution Time @".f.. t1-t0
+#
+# __ _________ __ ________
+#   ?
+#
