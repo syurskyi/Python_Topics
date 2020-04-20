1e18c066d66b526e09c5e0031974fa5cebebad19
Grey <sergejyurskyj@yahoo.com>
2020-04-19 18:01:19 -0700

    Committed gettingCurrentThread templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 03/gettingCurrentThread.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 03/gettingCurrentThread.py
@@ -1,16 +1,16 @@
-______ threading
-______ time
-
-def threadTarget():
-  print("Current Thread: {}".format(threading.current_thread()))
-
-threads = []
-
-for i in range(10):
-  thread = threading.Thread(target=threadTarget)
-  thread.start()
-  threads.append(thread)
-
-for thread in threads:
-  thread.join()
-
+# ______ th..
+# ______ ti..
+#
+# ___ threadTarget
+#   print("Current Thread: @".f.. ?.c_t..
+#
+# threads _  # list
+#
+# ___ i __ ra.. 10
+#   thread _ ?.T.. t.._?
+#   ?.s..
+#   ?s.ap.. ?
+#
+# ___ ? __ ?s
+#   ?.j..
+#
