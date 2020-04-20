521903b4ae641e6cf5c965291d1c87de91abf043
Grey <sergejyurskyj@yahoo.com>
2020-04-19 20:23:15 -0700

    Committed diningPhilosophers templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/diningPhilosophers.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/diningPhilosophers.py
@@ -1,55 +1,55 @@
-______ threading
-______ time
-______ random
-
-class Philosopher(threading.Thread):
-
-  def __init__(self, name, leftFork, rightFork):
-    print("{} Has Sat Down At the Table".format(name))
-    threading.Thread.__init__(self, name=name)
-    self.leftFork = leftFork
-    self.rightFork = rightFork
-
-  def run(self):
-    print("{} has started thinking".format(threading.currentThread().getName()))
-    while True:
-      time.sleep(random.randint(1,5))
-      print("{} has finished thinking".format(threading.currentThread().getName()))
-      self.leftFork.acquire()
-      time.sleep(random.randint(1,5))
-      try:
-        print("{} has acquired the left fork".format(threading.currentThread().getName()))
-
-        self.rightFork.acquire()
-        try:
-          print("{} has attained both forks, currently eating".format(threading.currentThread().getName()))
-        finally:
-          self.rightFork.release()   
-          print("{} has released the right fork".format(threading.currentThread().getName()))
-      finally:
-        self.leftFork.release()
-        print("{} has released the left fork".format(threading.currentThread().getName()))
-
-fork1 = threading.RLock()
-fork2 = threading.RLock()
-fork3 = threading.RLock()
-fork4 = threading.RLock()
-fork5 = threading.RLock()
-
-philosopher1 = Philosopher("Kant", fork1, fork2)
-philosopher2 = Philosopher("Aristotle", fork2, fork3)
-philosopher3 = Philosopher("Spinoza", fork3, fork4)
-philosopher4 = Philosopher("Marx", fork4, fork5)
-philosopher5 = Philosopher("Russell", fork5, fork1)
-
-philosopher1.start()
-philosopher2.start()
-philosopher3.start()
-philosopher4.start()
-philosopher5.start()
-
-philosopher1.join()
-philosopher2.join()
-philosopher3.join()
-philosopher4.join()
-philosopher5.join()
+# ______ th..
+# ______ ti..
+# ______ ra..
+#
+# c_ Philosopher ?.T..
+#
+#   ___ -  name leftFork rightFork
+#     print("@ Has Sat Down At the Table".f.. ?
+#     ?.T... -  n.._n..
+#     ? ?
+#     ? ?
+#
+#   ___ run
+#     print("@ has started thinking".f.. ?.cT.. .gN..
+#     w__ T..
+#       t__.s.. ra__.r_i.. 1 5
+#       print("@ has finished thinking".f.. ?.cT.. .gN..
+#       l__.ac..
+#       t__.s.. r__.r_i.. 1 5
+#       ___
+#         print("@ has acquired the left fork".f.. ?.cT.. .gN..
+#
+#         r__.a..
+#         ___
+#           print("@ has attained both forks, currently eating".f.. ?.cT.. .gN..
+#         f..
+#           r__.r..
+#           print("@ has released the right fork".f.. ?.cT.. .gN..
+#       f..
+#         l__.r..
+#         print "@ has released the left fork".f.. ?.cT.. .gN..
+#
+# fork1 _ ?.RL..
+# fork2 _ ?.RL..
+# fork3 _ ?.RL..
+# fork4 _ ?.RL..
+# fork5 _ ?.RL..
+#
+# philosopher1 _ ?("Kant", fork1, fork2)
+# philosopher2 _ ?("Aristotle", fork2, fork3)
+# philosopher3 _ ?("Spinoza", fork3, fork4)
+# philosopher4 _ ?("Marx", fork4, fork5)
+# philosopher5 _ ?("Russell", fork5, fork1)
+#
+# philosopher1.start()
+# philosopher2.start()
+# philosopher3.start()
+# philosopher4.start()
+# philosopher5.start()
+#
+# philosopher1.join()
+# philosopher2.join()
+# philosopher3.join()
+# philosopher4.join()
+# philosopher5.join()
