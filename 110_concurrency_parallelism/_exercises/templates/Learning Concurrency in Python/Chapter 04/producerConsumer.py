73936d84f4ce75da014519ad512ef3e96aba8c27
Grey <sergejyurskyj@yahoo.com>
2020-04-19 20:34:18 -0700

    Committed producerConsumer templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/producerConsumer.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 04/producerConsumer.py
@@ -1,82 +1,82 @@
-______ threading
-______ random
-______ time
-
-class Producer(threading.Thread):
-    """
-    Produces random integers to a list
-    """
-
-    def __init__(self, integers, condition):
-        """
-        Constructor.
-
-        @param integers list of integers
-        @param condition condition synchronization object
-        """
-        threading.Thread.__init__(self)
-        self.integers = integers
-        self.condition = condition
-    
-    def run(self):
-        """
-        Thread run method. Append random integers to the integers list
-        at random time.
-        """
-        while True:
-            integer = random.randint(0, 256)
-            self.condition.acquire()
-            print 'condition acquired by %s' % self.name
-            self.integers.append(integer) 
-            print '%d appended to list by %s' % (integer, self.name)
-            print 'condition notified by %s' % self.name
-            self.condition.notify()
-            print 'condition released by %s' % self.name
-            self.condition.release()
-            time.sleep(1)
-
-class Consumer(threading.Thread):
-    """
-    Consumes random integers from a list
-    """
-
-    def __init__(self, integers, condition):
-        """
-        Constructor.
-
-        @param integers list of integers
-        @param condition condition synchronization object
-        """
-        threading.Thread.__init__(self)
-        self.integers = integers
-        self.condition = condition
-    
-    def run(self):
-        """
-        Thread run method. Consumes integers from list
-        """
-        while True:
-            self.condition.acquire()
-            print 'condition acquired by %s' % self.name
-            while True:
-                if self.integers:
-                    integer = self.integers.pop()
-                    print '%d popped from list by %s' % (integer, self.name)
-                    break
-                print 'condition wait by %s' % self.name
-                self.condition.wait()
-            print 'condition released by %s' % self.name
-            self.condition.release()
-
-def main():
-    integers = []
-    condition = threading.Condition()
-    t1 = Producer(integers, condition)
-    t2 = Consumer(integers, condition)
-    t1.start()
-    t2.start()
-    t1.join()
-    t2.join()
-
-if __name__ == '__main__':
-    main()
+# ______ th..
+# ______ ra..
+# ______ ti..
+#
+# c_ Producer ?.T..
+#     """
+#     Produces random integers to a list
+#     """
+#
+#     ___  -   integers condition
+#         """
+#         Constructor.
+#
+#         @param integers list of integers
+#         @param condition condition synchronization object
+#         """
+#         ?.Thread. -
+#         ? ?
+#         ? ?
+#
+#     ___ run
+#         """
+#         Thread run method. Append random integers to the integers list
+#         at random time.
+#         """
+#         w__ T..
+#             integer _ ra__.r_i.. 0 256
+#             c__.a..
+#             print 'condition acquired by @'  n..
+#             i__.ap.. i..
+#             print '@ appended to list by @'  i.. n..
+#             print 'condition notified by @'  n..
+#             c__.n..
+#             print 'condition released by @'  n..
+#             c__.r..
+#             t__.s.. 1
+#
+# c_ Consumer(?.T..
+#     """
+#     Consumes random integers from a list
+#     """
+#
+#     ___  -  integers condition
+#         """
+#         Constructor.
+#
+#         @param integers list of integers
+#         @param condition condition synchronization object
+#         """
+#         ?.Thread. -
+#         ? ?
+#         ? ?
+#
+#     ___ run
+#         """
+#         Thread run method. Consumes integers from list
+#         """
+#         w__ T..
+#             c__.a..
+#             print 'condition acquired by @'  n..
+#             w__ T..
+#                 __ i..
+#                     integer _ i__.po.
+#                     print '@ popped from list by @'  i... n..
+#                     b..
+#                 print 'condition wait by @'  n..
+#                 c__.w..
+#             print 'condition released by @'  n..
+#             c__.r..
+#
+# ___ main
+#     integers _   # list
+#     condition _ ?.C..
+#     t1 _ P.. i.. c..
+#     t2 _ C.. i.. c..
+#     t1.s..
+#     t2.s..
+#     t1.j..
+#     t2.j..
+#
+# __ _________ __ ________
+#     ?
