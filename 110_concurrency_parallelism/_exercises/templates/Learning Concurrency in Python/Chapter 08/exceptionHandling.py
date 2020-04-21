aa5576b3c0fded6ac1e21c1a39c0925ee5dfd0ff
Grey <sergejyurskyj@yahoo.com>
2020-04-21 13:02:32 -0700

    Committed exceptionHandling templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/exceptionHandling.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/exceptionHandling.py
@@ -1,35 +1,35 @@
-______ m..
-______ __ ___
-______ tr..
-
-class MyProcess(multiprocessing.Process):
-  
-  ___ __init__(self, pipein):
-    super(MyProcess, self).__init__()
-    self.pipein = pipein
-
-  ___ run(self):
-    try:
-      raise Exception("This broke stuff")
-    except:
-      except_type, except_class, tb = sys.exc_info()
-
-      self.pipein = os.fdopen(self.pipein, 'w')
-      self.pipein.write(str(tb))
-      self.pipein.close()
-
-___ main():
-  pipeout, pipein = os.pipe()
-
-  childProcess = MyProcess(pipein)
-  childProcess.start()
-  childProcess.join()
-
-  os.close(pipein)
-  pipeout = os.fdopen(pipeout)
-
-  pipeContent = pipeout.read()
-  print("Exception: {}".format(pipeContent))
-
-if __name__ == '__main__':
-  main()
+# ______ m..
+# ______ __ ___
+# ______ tr..
+#
+# c_ MyProcess ?.P..
+#
+#   ___ -  pipein
+#     s__ ? ? -
+#     ? ?
+#
+#   ___ run
+#     ___
+#       r_ E.. "This broke stuff"
+#     _____
+#       ex.._t.., ex.._cl.. tb _ ___.e..
+#
+#       pipein _ __.fd.. p_i. _
+#       ?.w.. st. t.
+#       ?.c..
+#
+# ___ main
+#   pipeout, pipein _ __.p..
+#
+#   childProcess _ ? p..
+#   ?.s..
+#   ?.j..
+#
+#   __.c.. ?
+#   pipeout _ __.fd.. ?
+#
+#   pipeContent _ ?.r..
+#   print("Exception: @".f.. ?
+#
+# __ _________ __ ________
+#   ?
