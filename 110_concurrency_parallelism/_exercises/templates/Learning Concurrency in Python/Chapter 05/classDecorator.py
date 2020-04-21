6432a424c4b5068cf76db8d2f705271438eaf987
Grey <sergejyurskyj@yahoo.com>
2020-04-20 18:13:50 -0700

    Committed classDecorator templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 05/classDecorator.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 05/classDecorator.py
@@ -1,38 +1,38 @@
-from threading ______ Lock
-
-def lock_class(methodnames, lockfactory):
-    return lambda cls: make_threadsafe(cls, methodnames, lockfactory)
-
-def lock_method(method):
-    if getattr(method, '__is_locked', False):
-        raise TypeError("Method %r is already locked!" % method)
-    def locked_method(self, *arg, **kwarg):
-        with self._lock:
-            return method(self, *arg, **kwarg)
-    locked_method.__name__ = '%s(%s)' % ('lock_method', method.__name__)
-    locked_method.__is_locked = True
-    return locked_method
-
-
-def make_threadsafe(cls, methodnames, lockfactory):
-    init = cls.__init__
-    def newinit(self, *arg, **kwarg):
-        init(self, *arg, **kwarg)
-        self._lock = lockfactory()
-    cls.__init__ = newinit
-
-    for methodname in methodnames:
-        oldmethod = getattr(cls, methodname)
-        newmethod = lock_method(oldmethod)
-        setattr(cls, methodname, newmethod)
-
-    return cls
-
-
-@lock_class(['add','remove'], Lock)
-class ClassDecoratorLockedSet(set):
-
-    @lock_method # if you double-lock a method, a TypeError is raised
-    def lockedMethod(self):
-        print("This section of our code would be thread safe")
-        pass
+# ____ t.. ______ L...
+#
+# ___ lock_class methodnames lockfactory
+#     r_ l___ cls m_t..|? m.. l..
+#
+# ___ lock_method method
+#     __ g_a.. ? '__is_locked' F..
+#         r_ T.. "Method @ is already locked!"  ?
+#     ___ l.. $ $$
+#         w__ _lock
+#             r_ m.. $ $$
+#     l__. -n _ '@(@)'  ('lock_method' ?. -n
+#     l__.__is_locked _ T..
+#     r_ ?
+#
+#
+# ___ make_threadsafe cls methodnames lockfactory
+#     init _ ?. -
+#     ___ newinit $ $$
+#         i.. $ $$
+#         ._lock _ l..
+#     ___. - _ n..
+#
+#     ___ methodname __ ?s
+#         oldmethod _ g_a.. ___ ?
+#         newmethod _ l.. ?
+#         s_a.. ___ m.. n..
+#
+#     r_ ___
+#
+#
+# ?? 'add','remove'|, L..
+# c_ ClassDecoratorLockedSet se.
+#
+#     @lock_method # __ you double-lock a method, a TypeError is raised
+#     ___ lockedMethod
+#         print("This section of our code would be thread safe")
+#         p..
