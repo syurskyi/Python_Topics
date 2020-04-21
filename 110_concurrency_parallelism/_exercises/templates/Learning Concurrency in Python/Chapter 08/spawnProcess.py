055fed795108c305d4049a283cc61811eab422f3
Grey <sergejyurskyj@yahoo.com>
2020-04-21 13:27:28 -0700

    Committed spawnProcess templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/spawnProcess.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/spawnProcess.py
@@ -1,14 +1,14 @@
-______ m..
-______ ti..
-
-___ myProcess():
-  current_process = multiprocessing.current_process().pid 
-  print("Child Process PID: {}".format(current_process.pid))
-  time.sleep(20)
-
-current_process = multiprocessing.current_process()
-print("Main process PID: {}".format(current_process.pid))
-
-myProcess = multiprocessing.Process(target=myProcess)
-myProcess.start()
-print("My Process has terminated, terminating main thread")
+# ______ m..
+# ______ ti..
+#
+# ___ myProcess
+#   current_process _ ?.c_p.. .p..
+#   print("Child Process PID: @".f.. c_p...p..
+#   time.sleep(20)
+#
+# current_process _ ?.c_p..
+# print("Main process PID: @".f.. c_p...p..
+#
+# myProcess _ ?.P.. t.._?
+# ?.s..
+# print("My Process has terminated, terminating main thread")
