bf39557e9544e985396a7d788a13a0dec2978f33
Grey <sergejyurskyj@yahoo.com>
2020-04-21 13:59:53 -0700

    Committed subclass templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/subclass.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/subclass.py
@@ -1,29 +1,29 @@
-______ m..
-______ __
-
-class MyProcess(multiprocessing.Process):
-
-  def __init__(self):
-    super(MyProcess, self).__init__()
-
-  def run(self):
-    print("Child Process PID: {}".format(multiprocessing.current_process().pid))
-
-def main():
-  print("Main Process PID: {}".format(multiprocessing.current_process().pid))
-  myProcess = MyProcess()
-  myProcess.start()
-  myProcess.join()
-
-  processes = []
-
-  for i in range(os.cpu_count()):
-    process = MyProcess()
-    processes.append(process)
-    process.start()
-
-  for process in processes:
-    process.join()
-    
-if __name__ == '__main__':
-  main()
+# ______ m..
+# ______ __
+#
+# c_ MyProcess ?.P..
+#
+#   ___ -
+#     s__ ? ? . -
+#
+#   ___ run
+#     print("Child Process PID: @".f..(?.c_p.. .p..
+#
+# ___ main():
+#   print("Main Process PID: @".f.. ?.c_p.. .p..
+#   myProcess _ ?
+#   ?.s..
+#   ?.j..
+#
+#   processes _    # list
+#
+#   ___ i __ ra.. __.c_c..
+#     process _ ?
+#     ?.ap.. ?
+#     ?.s..
+#
+#   ___ process __ ?
+#     ?.j..
+#
+# __ _________ __ ________
+#   ?
