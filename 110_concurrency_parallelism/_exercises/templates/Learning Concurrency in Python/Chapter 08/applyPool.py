2bba11e46e99ecb59f32168b3a3095963ac7c249
Grey <sergejyurskyj@yahoo.com>
2020-04-21 11:04:01 -0700

    Committed applyPool templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/applyPool.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/applyPool.py
@@ -1,27 +1,27 @@
-____ m.. ______ P..
-______ ti..
-
-
-___ myTask(n):
-  time.sleep(n/2)
-  return n*2
-
-___ myCallback(result):
-  print("hi")
-  print(result)
-
-___ main():
-  print("apply_async")
-  with Pool(4) as p:
-    task1 = p.apply_async(func=myTask, args=(4,))
-    task2 = p.apply_async(func=myTask, args=(3,))
-    task3 = p.apply_async(func=myTask, args=(2,))
-    task4 = p.apply_async(func=myTask, args=(1,))
-
-    print(task1.get())
-    print(task2.get())
-    print(task3.get())
-    print(task4.get())]
-
-if __name__ == '__main__':
-  main()
+# ____ m.. ______ P..
+# ______ ti..
+#
+#
+# ___ myTask n
+#   t__.s.. ?/2
+#   r_ ?*2
+#
+# ___ myCallback result
+#   print("hi")
+#   print ?
+#
+# ___ main
+#   print("apply_async")
+#   w__ P.. 4 __ p
+#     task1 _ ?.a_a.. ? a.._ 4
+#     task2 _ ?.a_a.. ? a.._ 3
+#     task3 _ ?.a_a.. ? a.._ 2
+#     task4 _ ?.a_a.. ? a.._ 1
+#
+#     print _1.g..
+#     print _2.g..
+#     print _3.g..
+#     print _4.g..
+#
+# __ _________ __ ________
+#   ?
