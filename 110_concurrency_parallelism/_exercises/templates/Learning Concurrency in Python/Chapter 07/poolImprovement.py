7fa67e279f1de30edf8821ecce015ec5fa899233
Grey <sergejyurskyj@yahoo.com>
2020-04-20 21:55:29 -0700

    Committed poolImprovement templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 07/poolImprovement.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 07/poolImprovement.py
@@ -1,57 +1,57 @@
-______ t_i.
-____ c__.f.. ______ TPE..
-____ c__.f.. ______ PPE..
-______ ma..
-
-values = [40000000, 30000000, 20000000, 10000000]
-
-PRIMES = [
-    112272535095293,
-    112582705942171,
-    112272535095293,
-    115280095190773,
-    115797848077099,
-    112272535095293,
-    115280095190773,
-    115797848077099,
-    112272535095293,
-    115280095190773,
-    115797848077099,
-    112272535095293,
-    115280095190773,
-    115797848077099,
-    1099726899285419]
-
-___ is_prime(n):
-    if n % 2 == 0:
-        return False
-
-    sqrt_n = int(math.floor(math.sqrt(n)))
-    for i in range(3, sqrt_n + 1, 2):
-        if n % i == 0:
-            return False
-    return True
-    
-___ main():
-
-  t1 = timeit.default_timer()
-  with ProcessPoolExecutor(max_workers=4) as executor:
-        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
-            print('%d is prime: %s' % (number, prime))
-
-  print("{} Seconds Needed for ProcessPoolExecutor".format(timeit.default_timer() - t1))
-  
-  t2 = timeit.default_timer()
-  with ThreadPoolExecutor(max_workers=4) as executor:
-        for number, prime in zip(PRIMES, executor.map(is_prime, PRIMES)):
-            print('%d is prime: %s' % (number, prime))
-  print("{} Seconds Needed for ThreadPoolExecutor".format(timeit.default_timer() - t2))
-
-  t3 = timeit.default_timer()
-  for number in PRIMES:
-    isPrime = is_prime(number)
-    print("{} is prime: {}".format(number, isPrime))
-  print("{} Seconds needed for single threaded execution".format(timeit.default_timer()-t3))
-
-__ _________ __ ________
-  main()
+# ______ t_i.
+# ____ c__.f.. ______ TPE..
+# ____ c__.f.. ______ PPE..
+# ______ ma..
+#
+# values _ |40000000, 30000000, 20000000, 10000000|
+#
+# PRIMES _ |
+#     112272535095293,
+#     112582705942171,
+#     112272535095293,
+#     115280095190773,
+#     115797848077099,
+#     112272535095293,
+#     115280095190773,
+#     115797848077099,
+#     112272535095293,
+#     115280095190773,
+#     115797848077099,
+#     112272535095293,
+#     115280095190773,
+#     115797848077099,
+#     1099726899285419|
+#
+# ___ is_prime n
+#     __ ? % 2 __ 0
+#         r_ F..
+#
+#     sqrt_n _ in. ma__.fl.. ma__.sq.. ?
+#     ___ i __ ra.. 3, _n + 1, 2
+#         __ ? % i __ 0:
+#             r_ F..
+#     r_ T..
+#
+# ___ main
+#
+#   t1 _ t_i_.d_t..
+#   w__ PPE.. m_w.._4 __ executor
+#         ___ number prime __ z.. P.. ?.m.. i_p.. P..
+#             print('@ is prime: @'  n.. p..
+#
+#   print("@ Seconds Needed ___ ProcessPoolExecutor".f.. t_i_.d_t.. - t1
+#
+#   t2 _ t_i_.d_t..
+#   w__ TPE.. m_w.._4 __ executor
+#         ___ number prime __ z..  P.. ?.m.. ? P..
+#             print('@ is prime: @'   n.. p..
+#   print("@ Seconds Needed for ThreadPoolExecutor".f.. t_i_.d_t.. - t2
+#
+#   t3 _ t_i_.d_t..
+#   ___ number __ P..
+#     isPrime _ i_p.. ?
+#     print("@ is prime: @".f.. n.. iP..
+#   print("@ Seconds needed for single threaded execution".f.. t_i_.d_t..-t3
+#
+# __ _________ __ ________
+#   ?
