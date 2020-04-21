fe496e2b78880a632cb5b6ba258f097155d605df
Grey <sergejyurskyj@yahoo.com>
2020-04-20 19:05:39 -0700

    Committed queueOperations templates

--- 110_concurrency_parallelism/_examples/Learning Concurrency in Python/Chapter 05/queueOperations.py
+++ 110_concurrency_parallelism/_examples/Learning Concurrency in Python/Chapter 05/queueOperations.py
@@ -1,28 +1,28 @@
-import threading
-import queue
-import random
-import time
-
-def mySubscriber(queue):
-  while True:
-    item = queue.get()
-    if item is None:
-      break
-    print("{} removed {} from the queue".format(threading.current_thread(), item))
-    print("Queue Size is now: {}".format(queue.qsize()))
-    queue.task_done()
-
-myQueue = queue.Queue()
-for i in range(10):
-  myQueue.put(i)
-
-print("Queue Populated")
-
-threads = []
-for i in range(4):
-  thread = threading.Thread(target=mySubscriber, args=(myQueue,))
-  thread.start()
-  threads.append(thread)
-
-myQueue.join()
-
+# ______ th..
+# ______ qu..
+# ______ ra..
+# ______ ti..
+#
+# ___ mySubscriber queue
+#   w__ T..
+#     item _ ?.g..
+#     __ i.. __ N..
+#       b..
+#     print("@ removed @ from the queue".f.. ?.c_t.. i..
+#     print("Queue Size is now: @".f.. ?.qs..
+#     ?.t_d..
+#
+# myQueue _ ?.Q..
+# ___ i __ ra.. 10
+#   ?.p.. ?
+#
+# print("Queue Populated")
+#
+# threads _   # list
+# ___ i __ ra.. 4
+#   thread _ ?.T.. t.._? a.._ ?,
+#   ?.s..
+#   ?s.ap.. ?
+#
+# ?.j..
+#
