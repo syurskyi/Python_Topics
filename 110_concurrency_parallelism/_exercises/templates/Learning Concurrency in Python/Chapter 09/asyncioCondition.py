828b75f6751acf08cb8dc15c2ae357c55828d74e
Grey <sergejyurskyj@yahoo.com>
2020-04-21 16:38:30 -0700

    Committed asyncioCondition templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 09/asyncioCondition.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 09/asyncioCondition.py
@@ -1,52 +1,52 @@
-______ asyncio
-
-
-async def consumer(condition, n):
-    with await condition:
-        print('consumer {} is waiting'.format(n))
-        await condition.wait()
-        print('consumer {} triggered'.format(n))
-    print('ending consumer {}'.format(n))
-
-
-async def manipulate_condition(condition):
-    print('starting manipulate_condition')
-
-    # pause to let consumers start
-    await asyncio.sleep(0.1)
-
-    for i in range(1, 3):
-        with await condition:
-            print('notifying {} consumers'.format(i))
-            condition.notify(n=i)
-        await asyncio.sleep(0.1)
-
-    with await condition:
-        print('notifying remaining consumers')
-        condition.notify_all()
-
-    print('ending manipulate_condition')
-
-
-async def main(loop):
-    # Create a condition
-    condition = asyncio.Condition()
-
-    # Set up tasks watching the condition
-    consumers = [
-        consumer(condition, i)
-        for i in range(5)
-    ]
-
-    # Schedule a task to manipulate the condition variable
-    loop.create_task(manipulate_condition(condition))
-
-    # Wait for the consumers to be done
-    await asyncio.wait(consumers)
-
-
-event_loop = asyncio.get_event_loop()
-try:
-    result = event_loop.run_until_complete(main(event_loop))
-finally:
-    event_loop.close()
+# ______ a..
+#
+#
+# ? ___ consumer condition n
+#     w__ ? condition
+#         print('consumer @ is waiting'.f.. ?
+#         ? ?.w..
+#         print('consumer @ triggered'.f.. ?
+#     print('ending consumer @'.f.. ?
+#
+#
+# ? ___ manipulate_condition condition
+#     print('starting manipulate_condition')
+#
+#     # pause to let consumers start
+#     ? ?.s.. 0.1
+#
+#     ___ i in ra.. 1 3
+#         w__ ? ?
+#             print('notifying @ consumers'.f.. ?
+#             ?.n.. n_i
+#         ? ?.s.. 0.1
+#
+#     w__ ? condition
+#         print('notifying remaining consumers')
+#         ?.n_a..
+#
+#     print('ending manipulate_condition')
+#
+#
+# ? ___ main loop
+#     # Create a condition
+#     condition _ ?.C..
+#
+#     # Set up tasks watching the condition
+#     consumers _ |
+#         consumer|c.. i
+#         ___ i __ ra.. 5
+#     |
+#
+#     # Schedule a task to manipulate the condition variable
+#     ?.c_t.. ? c..
+#
+#     # Wait for the consumers to be done
+#     ? ?.w.. ?s
+#
+#
+# event_loop _ ?.g_e_l..
+# ___
+#     result _ ?.r_u_c.. m.. ?
+# f...
+#     ?.c..
