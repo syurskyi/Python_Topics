a24edf80a8b57c01d3a33358476ad9910bffe58d
Grey <sergejyurskyj@yahoo.com>
2020-04-21 13:29:27 -0700

    Committed starmap templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/starmap.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/starmap.py
@@ -1,13 +1,13 @@
-____ m.. ______ P..
-______ ti..
-
-___ myTask(x, y):
-  time.sleep(x/2)
-  return y*2
-
-___ main():
-  with Pool(4) as p:
-     print(p.starmap(myTask, [(4,3),(2,1)]))
-
-if __name__ == '__main__':
-  main()
+# ____ m.. ______ P..
+# ______ ti..
+#
+# ___ myTask x y
+#   t__.s.. x/2)
+#   r_ y*2
+#
+# ___ main
+#   w__ P..4 __ p
+#      print ?.s_m.. ? |(4,3),(2,1)
+#
+# __ _________ __ ________
+#   ?
