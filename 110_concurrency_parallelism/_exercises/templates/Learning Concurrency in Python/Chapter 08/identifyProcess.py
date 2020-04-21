98c2d7b6ba955f36c72623d6be97767d375f6501
Grey <sergejyurskyj@yahoo.com>
2020-04-21 11:10:57 -0700

    Committed identifyProcess templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/identifyProcess.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 08/identifyProcess.py
@@ -1,16 +1,16 @@
-______ m..
-______ ti..
-
-___ childTask():
-    print("Child Process With PID: {}".format(multiprocessing.current_process().pid))
-    time.sleep(3)
-    print("Child process terminating")
-
-___ main():
-    print("Main process PID: {}".format(multiprocessing.current_process().pid))
-    myProcess = multiprocessing.Process(target=childTask)
-    myProcess.start()
-    myProcess.join()
-
-if __name__ == '__main__':
-    main()
+# ______ m..
+# ______ ti..
+#
+# ___ childTask
+#     print("Child Process With PID: @".f.. ?.c_p.. .p..
+#     t__.s.. 3
+#     print("Child process terminating")
+#
+# ___ main
+#     print("Main process PID: @".f.. ?.c_p.. .p..
+#     myProcess = ?.P.. t.._?
+#     ?.s..
+#     ?.j..
+#
+# __ _________ __ ________
+#     ?
