7ce606157791da1eade0bff2f9256aa17929802f
Grey <sergejyurskyj@yahoo.com>
2020-04-20 21:25:04 -0700

    Committed mapCallback templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 07/mapCallback.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 07/mapCallback.py
@@ -1,21 +1,21 @@
-______ ti..
-____ c__.f.. ______ TPE..
-____ c__.f.. ______ a_c..
-
-values = [2,3,4,5,6,7,8]
-
-___ multiplyByTwo(n):
-  return 2 * n
-
-___ done():
-  print("Task Done")
-
-___ main():
-  with ThreadPoolExecutor(max_workers=3) as executor:
-    results = executor.map(multiplyByTwo, values)
-
-    for result in results:
-      print(result)
-
-if __name__ == '__main__':
-  main()
+# ______ ti..
+# ____ c__.f.. ______ TPE..
+# ____ c__.f.. ______ a_c..
+#
+# values _ [2,3,4,5,6,7,8]
+#
+# ___ multiplyByTwo n
+#   r_ 2 * ?
+#
+# ___ done
+#   print("Task Done")
+#
+# ___ main
+#   w__ TPE.. m.._3 __ executor
+#     results _ ?.m.. ? v..
+#
+#     ___ result __ ?
+#       print ?
+#
+# __ _________ __ ________
+#   ?
