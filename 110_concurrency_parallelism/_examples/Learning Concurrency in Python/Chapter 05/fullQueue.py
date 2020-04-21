5b6eaadd37e209ce42eacd450afb28d037bb3b9e
Grey <sergejyurskyj@yahoo.com>
2020-04-20 18:45:01 -0700

    Committed fullQueue templates

--- 110_concurrency_parallelism/_examples/Learning Concurrency in Python/Chapter 05/fullQueue.py
+++ 110_concurrency_parallelism/_examples/Learning Concurrency in Python/Chapter 05/fullQueue.py
@@ -1,22 +1,22 @@
-import threading
-import queue
-import time
-
-def myPublisher(queue):
-  while not queue.full():
-    queue.put(1)
-    print("{} Appended 1 to queue: {}".format(threading.current_thread(), queue.qsize()))
-    time.sleep(1)
-
-myQueue = queue.Queue()
-for i in range(10):
-  myQueue.put(i)
-
-threads = []
-for i in range(4):
-  thread = threading.Thread(target=mySubscriber, args=(myQueue,))
-  thread.start()
-  threads.append(thread)
-
-myQueue.join
-
+# ______ th..
+# ______ q..
+# ______ ti..
+#
+# ___ myPublisher queue
+#   w__ no. ?.fu..
+#     ?.pu. 1
+#     print("@ Appended 1 to queue: @".f.. ?.c_t.. ?.qs..
+#     t__.s.. 1
+#
+# myQueue _ q__.Q..
+# ___ i __ ra.. 10
+#   ?.pu. ?
+#
+# threads _   # list
+# ___ i __ ra.. 4
+#   thread _ ?.T.. t.._mySubscriber, a.._ ?,
+#   ?.s..
+#   ?s.ap.. ?
+#
+# ?.j..
+#
