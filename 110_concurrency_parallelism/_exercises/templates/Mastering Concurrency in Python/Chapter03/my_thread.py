3667cf56a1ccf9cd428429bb2cb4ec079a774d82
Grey <sergejyurskyj@yahoo.com>
2020-04-22 10:55:27 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter03/my_thread.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter03/my_thread.py
@@ -1,24 +1,24 @@
-# ch3/my_thread.py
-
-import threading
-import time
-
-
-class MyThread(threading.Thread):
-    def __init__(self, name, delay):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.delay = delay
-
-    def run(self):
-        print('Starting thread %s.' % self.name)
-        thread_count_down(self.name, self.delay)
-        print('Finished thread %s.' % self.name)
-
-def thread_count_down(name, delay):
-    counter = 5
-
-    while counter:
-        time.sleep(delay)
-        print('Thread %s counting down: %i...' % (name, counter))
-        counter -= 1
+# # ch3/my_thread.py
+#
+# ______ th..
+# ______ ti..
+#
+#
+# c_ MyThread( .T..
+#     ___  -  name delay
+#         ?.T... - ?
+#         ? ?
+#         ? ?
+#
+#     ___ run
+#         print('Starting thread @.' ?
+#         ? ? ?
+#         print('Finished thread @.' ?
+#
+# ___ thread_count_down name delay
+#     counter _ 5
+#
+#     w__ ?
+#         t__.s.. ?
+#         print('Thread @ counting down: i...' ? ?
+#         ? -_ 1
