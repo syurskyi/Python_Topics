bf48dd86449b2fe1cba554a5f02d34f10ef58bb7
Grey <sergejyurskyj@yahoo.com>
2020-04-22 13:21:29 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter06/example1.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter06/example1.py
@@ -1,30 +1,30 @@
-# ch6/example1.py
-
-____ m.. ______ Process
-______ ti..
-
-
-___ count_down(name, delay):
-    print('Process %s starting...' % name)
-
-    counter _ 5
-
-    w__ counter:
-        t__.s..(delay)
-        print('Process %s counting down: %i...' % (name, counter))
-        counter -_ 1
-
-    print('Process %s exiting...' % name)
-
-
-__ _______ __ _______
-    process1 _ P..(target_count_down, args_('A', 0.5))
-    process2 _ P..(target_count_down, args_('B', 0.5))
-
-    process1.s..
-    process2.s..
-
-    process1.j..
-    process2.j..
-
-    print('Done.')
+# # ch6/example1.py
+#
+# ____ m.. ______ P..
+# ______ ti..
+#
+#
+# ___ count_down name delay
+#     print('Process @ starting...' ?
+#
+#     counter _ 5
+#
+#     w__ ?
+#         t__.s.. d..
+#         print('Process @ counting down: i...' ? ?
+#         ? -_ 1
+#
+#     print('Process @ exiting...' ?
+#
+#
+# __ _______ __ _______
+#     process1 _ P.. t.._? a.._ 'A', 0.5
+#     process2 _ P.. t.._? a.._ 'B', 0.5
+#
+#     _1.s..
+#     _2.s..
+#
+#     _1.j..
+#     _2.j..
+#
+#     print('Done.')
