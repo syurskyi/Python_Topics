ba19f2bd01ea6aa9f85f78317057531ab9562437
Grey <sergejyurskyj@yahoo.com>
2020-04-21 22:05:00 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter02/example1.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter02/example1.py
@@ -1,56 +1,56 @@
-# ch2/example1.py
-
-from math import sqrt
-
-import concurrent.futures
-import multiprocessing
-
-from timeit import default_timer as timer
-
-
-def is_prime(x):
-    if x < 2:
-        return False
-
-    if x == 2:
-        return x
-
-    if x % 2 == 0:
-        return False
-
-    limit = int(sqrt(x)) + 1
-    for i in range(3, limit, 2):
-        if x % i == 0:
-            return False
-
-    return x
-
-def concurrent_solve(n_workers):
-    print('Number of workers: %i.' % n_workers)
-
-    start = timer()
-    result = []
-
-    with concurrent.futures.ProcessPoolExecutor(max_workers=n_workers) as executor:
-
-        futures = [executor.submit(is_prime, i) for i in input]
-        completed_futures = concurrent.futures.as_completed(futures)
-
-        sub_start = timer()
-
-        for i, future in enumerate(completed_futures):
-            if future.result():
-                result.append(future.result())
-
-        sub_duration = timer() - sub_start
-
-    duration = timer() - start
-    print('Sub took: %.4f seconds.' % sub_duration)
-    print('Took: %.4f seconds.' % duration)
-
-
-input = [i for i in range(10 ** 13, 10 ** 13 + 1000)]
-
-for n_workers in range(1, multiprocessing.cpu_count() + 1):
-    concurrent_solve(n_workers)
-    print('_' * 20)
+# # ch2/example1.py
+#
+# ____ ma__ ______ sqrt
+#
+# ______ c__.f..
+# ______ m..
+#
+# ____ t_i_ ______ d_t_ __ timer
+#
+#
+# ___ is_prime x
+#     __ ? < 2
+#         r_ F..
+#
+#     __ ? __ 2
+#         r_ ?
+#
+#     __ x % 2 __ 0
+#         r_ F..
+#
+#     limit _ in. sq.. ?|| + 1
+#     ___ i __ ra.. 3 ? 2
+#         __ x % i __ 0
+#             r_ F..
+#
+#     r_ x
+#
+# ___ concurrent_solve n_workers
+#     print('Number of workers: @i.'  ?
+#
+#     start _ t..
+#     result _    # list
+#
+#     w__ c__.f...PPE.. m_w.._? __ executor
+#
+#         futures _ ?.s.. ? i| ___ ? __ i..
+#         completed_futures _ c__.f__.a.. f..
+#
+#         sub_start _ t..
+#
+#         ___ i future __ en.. c_f..
+#             __ f__.r..
+#                 r__.ap..f__.r..
+#
+#         sub_duration _ t.. - s_s..
+#
+#     duration _ t.. - s..
+#     print('Sub took: @.4_ seconds.'  s_d..
+#     print('Took: @.4_ seconds.'  d..
+#
+#
+# input _ i ___ ? __ ra..|10 ** 13, 10 ** 13 + 1000
+#
+# ___ n_workers __ ra.. 1 ?.c_c.. + 1)
+#     c_s.. ?
+#     print('_' * 20)
