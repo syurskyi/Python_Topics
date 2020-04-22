c6e76be58a2aa6df479f50f279ca06b5acfe1e1c
Grey <sergejyurskyj@yahoo.com>
2020-04-22 12:13:22 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter05/example4.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter05/example4.py
@@ -1,38 +1,38 @@
-# ch05/example4.py
-
-______ th..
-______ req..
-______ ti..
-
-c_ MyThread(?.T..
-    ___  - (self, url):
-        ?.T... - (self)
-        self.url _ url
-        self.result _ N..
-
-    ___ run(self):
-        res _ ?.get(self.url)
-        self.result _ f'{self.url}: {res.text}'
-
-urls _ [
-    'http://httpstat.us/200',
-    'http://httpstat.us/400',
-    'http://httpstat.us/404',
-    'http://httpstat.us/408',
-    'http://httpstat.us/500',
-    'http://httpstat.us/524'
-]
-
-start _ t__.t__()
-
-threads _ [MyThread(url) ___ url __ urls]
-___ thread __ threads:
-    thread.s..
-___ thread __ threads:
-    thread.j..
-___ thread __ threads:
-    print(thread.result)
-
-print(f'Took {t__.t__() - start : .2f} seconds')
-
-print('Done.')
+# # ch05/example4.py
+#
+# ______ th..
+# ______ req..
+# ______ ti..
+#
+# c_ MyThread ?.T..
+#     ___  - url
+#         ?.T... - ?
+#         ? ?
+#         result _ N..
+#
+#     ___ run
+#         res _ ?.g.. ?
+#         result _ _*|u.. : |?.t..'
+#
+# urls _ |
+#     'http://httpstat.us/200',
+#     'http://httpstat.us/400',
+#     'http://httpstat.us/404',
+#     'http://httpstat.us/408',
+#     'http://httpstat.us/500',
+#     'http://httpstat.us/524'
+# |
+#
+# start _ t__.t__
+#
+# threads _ ? u.. ___ ? __ ?s
+# ___ thread __ ?
+#     ?.s..
+# ___ ? __ ?
+#     ?.j..
+# ___ ? __ ?
+#     print ?.r..
+#
+# print _*Took |t__.t__ - s.. : .2f| seconds')
+#
+# print('Done.')
