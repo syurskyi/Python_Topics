b3f2165a076b19031eef17c60fd22052713242f6
Grey <sergejyurskyj@yahoo.com>
2020-04-22 16:31:19 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter08/example1.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter08/example1.py
@@ -1,14 +1,14 @@
-# ch8/example1.py
-
-______ cv2
-
-im _ cv2.imread('input/ship.jpg')
-cv2.imshow('Test', im)
-cv2.waitKey(0) # press any key to move forward here
-
-print(im)
-print('Type:', type(im))
-print('Shape:', im.shape)
-print('Top-left pixel:', im[0, 0])
-
-print('Done.')
+# # ch8/example1.py
+#
+# ______ ?2
+#
+# im _ ?2.?r..('input/ship.jpg')
+# ?2.?s.. 'Test' ?
+# ?2.wK.. 0 # press any key to move forward here
+#
+# print ?
+# print('Type:' ty.. ?
+# print('Shape:', ?.s..
+# print('Top-left pixel:' ? 0, 0
+#
+# print('Done.')
