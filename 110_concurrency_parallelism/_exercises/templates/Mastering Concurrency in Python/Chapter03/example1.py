8ecf5bb48e75f0cd63c86ffee1fc44f3ef28fa89
Grey <sergejyurskyj@yahoo.com>
2020-04-21 22:05:40 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter03/example1.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter03/example1.py
@@ -1,16 +1,16 @@
-# ch3/example1.py
-
-from my_thread import MyThread
-
-
-thread1 = MyThread('A', 0.5)
-thread2 = MyThread('B', 0.5)
-
-thread1.start()
-thread2.start()
-
-thread1.join()
-thread2.join()
-
-
-print('Finished.')
+# # ch3/example1.py
+#
+# ____ my_thread ______ MyThread
+#
+#
+# thread1 _ ? 'A', 0.5
+# thread2 _ ? 'B', 0.5
+#
+# +1.s..
+# +2.s..
+#
+# _1.j..
+# _2.j..
+#
+#
+# print('Finished.')
