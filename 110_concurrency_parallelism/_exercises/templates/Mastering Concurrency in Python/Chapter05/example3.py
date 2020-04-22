12cac24cf059e3b687aece2599eaf9e16c6d2cef
Grey <sergejyurskyj@yahoo.com>
2020-04-22 12:06:55 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter05/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter05/example3.py
@@ -1,36 +1,36 @@
-# ch05/example3.py
-
-______ th..
-______ req..
-______ ti..
-
-___ ping(url
-    res _ ?.get(url)
-    print(f'{url}: {res.text}')
-
-urls _ [
-    'http://httpstat.us/200',
-    'http://httpstat.us/400',
-    'http://httpstat.us/404',
-    'http://httpstat.us/408',
-    'http://httpstat.us/500',
-    'http://httpstat.us/524'
-]
-
-start _ t__.t__()
-___ url __ urls:
-    ping(url)
-print(f'Sequential: {t__.t__() - start : .2f} seconds')
-
-print()
-
-start _ t__.t__()
-threads _    # list
-___ url __ urls:
-    thread _ ?.T..(target_ping, args_(url,))
-    threads.ap..(thread)
-    thread.s..
-___ thread __ threads:
-    thread.j..
-
-print(f'Threading: {t__.t__() - start : .2f} seconds')
+# # ch05/example3.py
+#
+# ______ th..
+# ______ req..
+# ______ ti..
+#
+# ___ ping url
+#     res _ ?.g.. ?
+#     print _*|? |?.t..
+#
+# urls _ |
+#     'http://httpstat.us/200',
+#     'http://httpstat.us/400',
+#     'http://httpstat.us/404',
+#     'http://httpstat.us/408',
+#     'http://httpstat.us/500',
+#     'http://httpstat.us/524'
+# |
+#
+# start _ t__.t__
+# ___ url __ ?
+#     ? ?
+# print _*Sequential: |t__.t__ - s.. : .2f| seconds')
+#
+# print()
+#
+# start _ t__.t__
+# threads _    # list
+# ___ url __ ?
+#     thread _ ?.T.. t_p.. a.._ ?
+#     ?s.ap.. ?
+#     ?.s..
+# ___ t.. __ ?
+#     ?.j..
+#
+# print(f'Threading: |t__.t__ - s.. : .2f| seconds')
