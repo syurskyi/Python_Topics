80c6213b5aeab6fec205c27652aab3aded478426
Grey <sergejyurskyj@yahoo.com>
2020-04-22 16:34:03 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter08/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter08/example3.py
@@ -1,11 +1,11 @@
-# ch8/example3.py
-
-______ cv2
-
-im _ cv2.imread('input/ship.jpg')
-gray_im _ cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-
-ret, custom_thresh_im _ cv2.threshold(gray_im, 127, 255, cv2.THRESH_BINARY)
-cv2.imwrite('output/custom_thresh_ship.jpg', custom_thresh_im)
-
-print('Done.')
+# # ch8/example3.py
+#
+# ______ ?2
+#
+# im _ ?2.?r.. 'input/ship.jpg'
+# gray_im _ ?2.cC.. ? ?2.C_B..
+#
+# ret, custom_thresh_im _ ?2.th.. ? 127, 255, ?2.T_B..
+# ?2.?w..('output/custom_thresh_ship.jpg' c..
+#
+# print('Done.')
