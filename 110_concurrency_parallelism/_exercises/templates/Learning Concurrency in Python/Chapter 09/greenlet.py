2bb042bf92a0a12eba30897bbdb69792ab1c80f4
Grey <sergejyurskyj@yahoo.com>
2020-04-21 19:09:20 -0700

    Committed greenlet templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 09/greenlet.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 09/greenlet.py
@@ -1,20 +1,20 @@
-______ gevent
-from gevent ______ Greenlet
-
-class MyNoopGreenlet(Greenlet):
-
-  def __init__(self, seconds):
-    Greenlet.__init__(self)
-    self.seconds = seconds
-
-  def _run(self):
-    print("My Greenlet executing")
-    gevent.sleep(self.seconds)
-
-  def __str__(self):
-    return 'MyNoopGreenlet(%s)' % self.seconds
-
-g = MyNoopGreenlet(4)
-g.start()
-g.join()
-print(g.dead)
+# ______ ge..
+# ____ ge.. ______ G..
+#
+# c_ MyNoopGreenlet Greenlet
+#
+#   ___ -  seconds
+#     ?. -
+#     ? ?
+#
+#   ___ _run
+#     print("My Greenlet executing")
+#     ?.s.. ?
+#
+#   ___ -s
+#     r_ 'MyNoopGreenlet(%)'  s..
+#
+# g _ ?4
+# ?.s..
+# ?.j..
+# print ?.d..
