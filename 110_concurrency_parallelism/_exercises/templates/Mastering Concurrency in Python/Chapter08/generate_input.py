9449d00f038b83940fa0c2c916d30391f17b1556
Grey <sergejyurskyj@yahoo.com>
2020-04-22 16:56:52 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter08/generate_input.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter08/generate_input.py
@@ -1,19 +1,19 @@
-______ ?2
-
-n _ 20
-
-im _ ?2.?r..('input/ship.jpg')
-h, w, a _ im.shape
-h_unit _ h // n; w_unit _ w // n
-
-___ i __ ra..(n):
-    start_x _ h_unit * i
-    ___ j __ ra..(n):
-        #print(i, j)
-        start_y _ w_unit * j
-        ?2.?w..(
-            'input/large_input/ship_%i_%i.jpg' % (i, j),
-            im[start_x : start_x + h_unit, start_y : start_y + w_unit, :]
-        )
-
-print('Done.')
+# ______ ?2
+#
+# n _ 20
+#
+# im _ ?2.?r..('input/ship.jpg')
+# h, w, a _ ?.s..
+# h_unit _ h // n; w_unit _ w // n
+#
+# ___ i __ ra.. n
+#     start_x _ h_unit * ?
+#     ___ j __ ra.. ?
+#         #print(i, j)
+#         start_y _ w_unit * j
+#         ?2.?w..(
+#             'input/large_input/ship_@_@.jpg' ? ?
+#             i.|_x ; _x + h_ _y ; _y + w_ ;
+#         |
+#
+# print('Done.')
