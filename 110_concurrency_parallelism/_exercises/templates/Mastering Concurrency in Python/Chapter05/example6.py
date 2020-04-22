b3d74e4472d280c2e7a37af200e90213b2ec28f6
Grey <sergejyurskyj@yahoo.com>
2020-04-22 12:34:30 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter05/example6.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter05/example6.py
@@ -1,47 +1,47 @@
-# ch05/example6.py
-
-______ th..
-______ req..
-______ ti..
-
-UPDATE_INTERVAL _ 0.01
-
-c_ MyThread(?.T..):
-    ___  - (self, url):
-        ?.T... - (self)
-        self.url _ url
-        self.result _ f'{self.url}: Custom timeout'
-
-    ___ run(self):
-        res _ ?.g..(self.url)
-        self.result _ f'{self.url}: {res.text}'
-
-___ process_requests(threads, timeout_5):
-    ___ alive_count():
-        alive _ [1 __ thread.isAlive() else 0 ___ thread __ threads]
-        r_ su.(alive)
-
-    w__ alive_count() > 0 and timeout > 0:
-        timeout -_ UPDATE_INTERVAL
-        t__.s..(UPDATE_INTERVAL)
-    ___ thread __ threads:
-        print(thread.result)
-
-urls _ [
-    'http://httpstat.us/200',
-    'http://httpstat.us/200?sleep=4000',
-    'http://httpstat.us/200?sleep=20000',
-    'http://httpstat.us/400'
-]
-
-start _ t__.t__()
-
-threads _ [MyThread(url) ___ url __ urls]
-___ thread __ threads:
-    thread.setDaemon(T..)
-    thread.s..
-process_requests(threads)
-
-print(f'Took {t__.t__() - start : .2f} seconds')
-
-print('Done.')
+# # ch05/example6.py
+#
+# ______ th..
+# ______ req..
+# ______ ti..
+#
+# UPDATE_INTERVAL _ 0.01
+#
+# c_ MyThread ?.T..
+#     ___  -  url
+#         ?.T... - ?
+#         ? ?
+#         result _ _*|u.. |: Custom timeout'
+#
+#     ___ run
+#         res _ ?.g.. ?
+#         result _ _*|u.. : |?.t..
+#
+# ___ process_requests threads t.._5
+#     ___ alive_count
+#         alive _ |1 __ thread.iA.. ____ 0 ___ t.. __ ?
+#         r_ su. ?
+#
+#     w__ a.. > 0 an. timeout > 0
+#         t.. -_ U..
+#         t__.s.. U..
+#     ___ t.. __ ?s
+#         print ?.r..
+#
+# urls _ |
+#     'http://httpstat.us/200',
+#     'http://httpstat.us/200?sleep=4000',
+#     'http://httpstat.us/200?sleep=20000',
+#     'http://httpstat.us/400'
+# |
+#
+# start _ t__.t__
+#
+# threads _ ? u__ ? __ ?s
+# ___ thread __ ?
+#     ?.sD..
+#     ?.s..
+# p.. ?s
+#
+# print _*Took |t__.t__ - s.. : .2_| seconds')
+#
+# print('Done.')
