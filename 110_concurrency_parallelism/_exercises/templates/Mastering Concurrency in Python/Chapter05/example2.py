2a114fb0b230f12d0fd35c0cb70b15b689dc9273
Grey <sergejyurskyj@yahoo.com>
2020-04-22 11:15:49 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter05/example2.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter05/example2.py
@@ -1,21 +1,21 @@
-# ch05/example2.py
-
-______ req..
-
-___ ping(url):
-    res _ ?.get(url)
-    print(f'{url}: {res.text}')
-
-urls _ [
-    'http://httpstat.us/200',
-    'http://httpstat.us/400',
-    'http://httpstat.us/404',
-    'http://httpstat.us/408',
-    'http://httpstat.us/500',
-    'http://httpstat.us/524'
-]
-
-___ url __ urls:
-    ping(url)
-
-print('Done.')
+# # ch05/example2.py
+#
+# ______ req..
+#
+# ___ ping url
+#     res _ ?.g.. ?
+#     print _*|? |?.t..
+#
+# urls _ |
+#     'http://httpstat.us/200',
+#     'http://httpstat.us/400',
+#     'http://httpstat.us/404',
+#     'http://httpstat.us/408',
+#     'http://httpstat.us/500',
+#     'http://httpstat.us/524'
+# |
+#
+# ___ url __ ?
+#     ? ?
+#
+# print('Done.')
