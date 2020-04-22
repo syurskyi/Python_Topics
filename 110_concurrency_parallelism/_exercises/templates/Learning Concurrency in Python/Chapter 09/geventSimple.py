4fad1b2be79d8ab84434d7869a9c19eece994ffa
Grey <sergejyurskyj@yahoo.com>
2020-04-21 19:07:40 -0700

    Committed geventSimple templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 09/geventSimple.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 09/geventSimple.py
@@ -1,11 +1,11 @@
-______ gevent
-from gevent ______ socket
-
-def main():
-  urls = ['www.google.com', 'www.example.com', 'www.python.org']
-  jobs = [gevent.spawn(socket.gethostbyname, url) for url in urls]
-  gevent.joinall(jobs, timeout=2)
-  print([job.value for job in jobs])
-
-if __name__ == '__main__':
-  main()
+# ______ ge..
+# ____ ge.. ______ s..
+#
+# ___ main
+#   urls _ ['www.google.com', 'www.example.com', 'www.python.org']
+#   jobs _ |?.sp.. s__.g_b_n.. url| ___ ? __ ?
+#   ?.j_a.. ? t.._2
+#   print |j__.v.. ___ j.. __ ?
+#
+# __ _________ __ ________
+#   ?
