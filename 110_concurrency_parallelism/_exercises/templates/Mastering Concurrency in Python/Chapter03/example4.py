837208d6245198d78b9f8fdc073fafc5fa9835f6
Grey <sergejyurskyj@yahoo.com>
2020-04-22 10:49:27 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter03/example4.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter03/example4.py
@@ -1,39 +1,39 @@
-# ch3/example4.py
-
-import threading
-import time
-
-class MyThread(threading.Thread):
-    def __init__(self, name, delay):
-        threading.Thread.__init__(self)
-        self.name = name
-        self.delay = delay
-
-    def run(self):
-        print('Starting thread %s.' % self.name)
-        thread_lock.acquire()
-        thread_count_down(self.name, self.delay)
-        thread_lock.release()
-        print('Finished thread %s.' % self.name)
-
-def thread_count_down(name, delay):
-    counter = 5
-
-    while counter:
-        time.sleep(delay)
-        print('Thread %s counting down: %i...' % (name, counter))
-        counter -= 1
-
-thread_lock = threading.Lock()
-
-thread1 = MyThread('A', 0.5)
-thread2 = MyThread('B', 0.5)
-
-thread1.start()
-thread2.start()
-
-thread1.join()
-thread2.join()
-
-
-print('Finished.')
+# # ch3/example4.py
+#
+# ______ th..
+# ______ ti..
+#
+# c_ MyThread ?.T..
+#     ___  -  name delay
+#         ?.T... - ?
+#         ? ?
+#         ? ?
+#
+#     ___ run
+#         print('Starting thread @.' ?
+#         t_l_.a..
+#         t.. ? ?
+#         t_l__.r..
+#         print('Finished thread @.' ?
+#
+# ___ thread_count_down name delay
+#     counter _ 5
+#
+#     w__ ?
+#         t__.s.. d..
+#         print('Thread @ counting down: i...' ?
+#         ? -_ 1
+#
+# thread_lock _ ?.L..
+#
+# thread1 _ ?('A', 0.5)
+# thread2 _ ?('B', 0.5)
+#
+# _1.s..
+# _2.s..
+#
+# _1.j..
+# _2.j..
+#
+#
+# print('Finished.')
