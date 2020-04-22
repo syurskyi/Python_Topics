330ca011f42392981b5003c6f50eff5550fb49f8
Grey <sergejyurskyj@yahoo.com>
2020-04-21 18:49:07 -0700

    Committed asyncioLock templates

--- 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 09/asyncioLock.py
+++ 110_concurrency_parallelism/_exercises/templates/Learning Concurrency in Python/Chapter 09/asyncioLock.py
@@ -1,18 +1,18 @@
-______ asyncio
-______ time
-
-async def myWorker(lock):
-  # with await lock:  
-  print("myWorker has attained lock, modifying variable")
-  time.sleep(2)
-  print("myWorker has release the lock")
-    
-async def main(loop):
-  lock = asyncio.Lock()
-  await asyncio.wait([myWorker(lock), myWorker(lock)]),
-
-loop = asyncio.get_event_loop()
-try:
-  loop.run_until_complete(main(loop))
-finally:
-  loop.close()
+# ______ a..
+# ______ ti..
+#
+# ? ___ myWorker lock
+#   # with await lock:
+#   print("myWorker has attained lock, modifying variable")
+#   t__.s.. 2
+#   print("myWorker has release the lock")
+#
+# ? ___ main loop
+#   lock _ ?.L..
+#   ? ?.w..|? ? ? ?
+#
+# loop _ ?.g_e_l..
+# ___
+#   ?.r_u_c.. m.. ?
+# f..
+#   ?.c..
