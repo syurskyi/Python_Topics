0e98c56f2bf97e549eee226226c7dc4e576f6d55
Grey <sergejyurskyj@yahoo.com>
2020-04-22 13:26:04 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter06/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter06/example3.py
@@ -1,32 +1,32 @@
-# ch6/example3.py
-
-____ m.. ______ Process, current_process
-______ ti..
-______ __
-
-
-___ print_info(title):
-    print(title)
-
-    __ hasattr(__, 'getppid'):
-        print('Parent process ID: %s.' % st.(__.g_p_
-
-    print('Current Process ID: %s.\n' % st.(__.g_p_
-
-___ f():
-    print_info('Function f')
-
-    pname _ current_process().name
-    print('Starting process %s...' % pname)
-    t__.s..(1)
-    print('Exiting process %s...' % pname)
-
-
-__ _______ __ _______
-    print_info('Main program')
-
-    p _ P..(target_f)
-    p.s..
-    p.j..
-
-    print('Done.')
+# # ch6/example3.py
+#
+# ____ m.. ______ P.., c_p__
+# ______ ti..
+# ______ __
+#
+#
+# ___ print_info title
+#     print ?
+#
+#     __ h_a.. __ 'getppid'
+#         print('Parent process ID: @.'  st. __.g_p_
+#
+#     print('Current Process ID: @.\n'  st. __.g_p_
+#
+# ___ f
+#     print_info('Function f')
+#
+#     pname _ c_p__.n..
+#     print('Starting process @...' ?
+#     t__.s.. 1
+#     print('Exiting process @...' ?
+#
+#
+# __ _______ __ _______
+#     p.. ('Main program')
+#
+#     p _ P.. t.._f
+#     ?.s..
+#     ?.j..
+#
+#     print('Done.')
