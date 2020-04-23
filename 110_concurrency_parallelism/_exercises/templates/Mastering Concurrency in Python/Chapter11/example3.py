5fff6cd1e4a429110f3a62878de00e61b8e997e4
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:32:10 -0700

    Committed Mastering Concurrency in Python example3 templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter11/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter11/example3.py
@@ -1,34 +1,34 @@
-# ch11/example3.py
-
-______ a..
-
-c_ EchoServerClientProtocol(?.Protocol):
-    ___ connection_made(self, transport):
-        peername _ transport.get_extra_info('peername')
-        print('Connection from {}'.format(peername))
-        self.transport _ transport
-
-    ___ data_received(self, data):
-        message _ data.decode()
-        print('Data received: {!r}'.format(message))
-
-        self.transport.write(('Echoed back: {}'.format(message)).encode())
-
-        print('Close the client socket')
-        self.transport.close()
-
-loop _ ?.g_e_l..
-coro _ loop.create_server(EchoServerClientProtocol, '127.0.0.1', 8888)
-server _ loop.r_u_c..(coro)
-
-# Serve requests until Ctrl+C is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
-___
-    loop.run_forever()
-______ K..
-    p..
-
-# Close the server
-server.close()
-loop.r_u_c..(server.wait_closed())
-loop.close()
+# # ch11/example3.py
+#
+# ______ a..
+#
+# c_ EchoServerClientProtocol ?.P..
+#     ___ connection_made transport
+#         peername _ ?.g_e_i..('peername')
+#         print('Connection from @'.f.. ?
+#         ? ?
+#
+#     ___ data_received data
+#         message _ data.d..
+#         print('Data received: @'.f.. ?
+#
+#         t__.w.. 'Echoed back: @'.f.. ? .e..
+#
+#         print('Close the client socket')
+#         t__.c..
+#
+# loop _ ?.g_e_l..
+# coro _ ?.c_s..(?, '127.0.0.1' 8888
+# server _ l__.r_u_c.. ?
+#
+# # Serve requests until Ctrl+C is pressed
+# print('Serving on @'.f.. s__.s.. 0 .g_s_n..
+# ___
+#     l__.r_f..
+# ______ K..
+#     p..
+#
+# # Close the server
+# s__.c..
+# l__.r_u_c.. s__.w_c..
+# l__.c..
