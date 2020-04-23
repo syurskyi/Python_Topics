4559df034e4361498529a7bdccadf2a7d5582800
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:53:27 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter18/example2.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter18/example2.py
@@ -1,19 +1,19 @@
-# ch18/example2.py
-
-____ operator ______ mul
-____ functools ______ reduce
-
-___
-    w__ T..:
-        line _ in..('Please enter a list of integer, separated by commas: ')
-        ___
-            nums _ li..(m..(int, line.split(',')))
-        ______ ValueError:
-            print('ERROR. Enter only integers separated by commas')
-            c..
-
-        print('Sum of input integers', su.(nums))
-        print('Product of input integers', reduce(mul, nums, 1))
-
-______ K..
-    print('\nFinished.')
+# # ch18/example2.py
+#
+# ____ op. ______ mul
+# ____ fu.. ______ reduce
+#
+# ___
+#     w__ T..
+#         line _ in..('Please enter a list of integer, separated by commas: ')
+#         ___
+#             nums _ li.. m..(in. ?.sp.. ','
+#         ______ V..
+#             print('ERROR. Enter only integers separated by commas')
+#             c..
+#
+#         print('Sum of input integers', su. ?
+#         print('Product of input integers', re.. mu. ? 1
+#
+# ______ K..
+#     print('\nFinished.')
