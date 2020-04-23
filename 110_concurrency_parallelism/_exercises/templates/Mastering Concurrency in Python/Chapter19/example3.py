c2ac10844dfd7f79b6ceea046c45a8fcd44430be
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:54:19 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter19/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter19/example3.py
@@ -1,27 +1,27 @@
-# ch19/example3.py
-
-____ d_t_ ______ d_t_
-______ ti..
-______ __
-
-____ apscheduler.schedulers.background ______ BackgroundScheduler
-
-___ task():
-    print(f'From process {__.getpid()}: The time is {d_t_.now()}')
-    print(f'Starting job inside {__.getpid()}')
-    t__.s..(4)
-    print(f'Ending job inside {__.getpid()}')
-
-__ _______ __ _______
-    scheduler _ BackgroundScheduler()
-    scheduler.add_executor('processpool')
-    scheduler.add_job(task, 'interval', seconds_3, max_instances_3)
-    scheduler.s..
-
-    ___
-        w__ T..:
-            t__.s..(1)
-    ______ K..
-        p..
-
-scheduler.shutdown()
+# # ch19/example3.py
+#
+# ____ d_t_ ______ d_t_
+# ______ ti..
+# ______ __
+#
+# ____ apscheduler.schedulers.background ______ BackgroundScheduler
+#
+# ___ task
+#     print _*From process |__.g_p.. : The time is |d_t_.n..
+#     print _*Starting job inside |__.g_p..
+#     t__.s.. 4
+#     print _*Ending job inside |__.g_p..
+#
+# __ _______ __ _______
+#     scheduler _ ?
+#     ?.a_e..('processpool')
+#     ?.a_j.. ? 'interval' s.._3 m.._3
+#     ?.s..
+#
+#     ___
+#         w__ T..
+#             t__.s.. 1
+#     ______ K..
+#         p..
+#
+# ?.sh..
