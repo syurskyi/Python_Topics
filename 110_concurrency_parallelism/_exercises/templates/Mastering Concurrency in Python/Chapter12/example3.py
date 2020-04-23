b4a28ce36a4d4966d0a4cc3b006422cdac0361c0
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:33:21 -0700

    Committed Mastering Concurrency in Python example3 templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter12/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter12/example3.py
@@ -1,24 +1,24 @@
-# ch12/example3.py
-
-______ th..
-
-# The philosopher thread
-___ philosopher(left, right):
-    w__ T..:
-        w__ left:
-             w__ right:
-                 print(f'Philosopher at {?.currentThread()} is eating.')
-
-# The chopsticks
-N_FORKS _ 5
-forks _ [?.Lock() ___ n __ ra..(N_FORKS)]
-
-# Create all of the philosophers
-phils _ [?.T..(
-    target_philosopher,
-    args_(forks[n], forks[(n + 1) % N_FORKS])
-) ___ n __ ra..(N_FORKS)]
-
-# Run all of the philosophers
-___ p __ phils:
-    p.s..
+# # ch12/example3.py
+#
+# ______ th..
+#
+# # The philosopher thread
+# ___ philosopher left right
+#     w__ T..
+#         w__ l..
+#              w__ r..
+#                  print _*Philosopher at |?.cT..} is eating.')
+#
+# # The chopsticks
+# N_FORKS _ 5
+# forks _ [?.L.. ___ n __ ra.. N..
+#
+# # Create all of the philosophers
+# phils _ |?.T..|
+#     t.._?
+#     a.._ f..|?  f..||? + 1) % N..
+# ) ___ n __ ra.. N..
+#
+# # Run all of the philosophers
+# ___ p __ ?
+#     ?.s..
