e7e5f97254b4f67a538b7a18e7975514c76dc597
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:35:44 -0700

    Committed Mastering Concurrency in Python example3 templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter14/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter14/example3.py
@@ -1,48 +1,48 @@
-# ch14/example3.py
-
-______ th..
-______ random; random.seed(0)
-______ ti..
-
-___ update(pause_period):
-    g.. counter
-
-    w__ count_lock:
-        current_counter _ counter # reading in shared resource
-        t__.s..(pause_period) # simulating heavy calculations
-        counter _ current_counter + 1 # updating shared resource
-
-pause_periods _ [random.randint(0, 1) ___ i __ ra..(20)]
-
-###########################################################################
-
-counter _ 0
-count_lock _ ?.Lock()
-
-start _ t__.p_c..
-___ i __ ra..(20):
-    update(pause_periods[i])
-
-print('--Sequential version--')
-print(f'Final counter: {counter}.')
-print(f'Took {t__.p_c.. - start : .2f} seconds.')
-
-###########################################################################
-
-counter _ 0
-
-threads _ [?.T..(target_update, args_(pause_periods[i],)) ___ i __ ra..(20)]
-
-start _ t__.p_c..
-___ thread __ threads:
-    thread.s..
-___ thread __ threads:
-    thread.j..
-
-print('--Concurrent version--')
-print(f'Final counter: {counter}.')
-print(f'Took {t__.p_c.. - start : .2f} seconds.')
-
-###########################################################################
-
-print('Finished.')
+# # ch14/example3.py
+#
+# ______ th..
+# ______ ra..; ra__.se.. 0
+# ______ ti..
+#
+# ___ update pause_period
+#     g.. counter
+#
+#     w__ c_l..
+#         current_counter _ ? # reading in shared resource
+#         t__.s.. ? # simulating heavy calculations
+#         counter _ c_c.. + 1 # updating shared resource
+#
+# pause_periods _ ra__.r_i. 0, 1 ___ i __ ra.. 20
+#
+# ###########################################################################
+#
+# counter _ 0
+# count_lock _ ?.L..
+#
+# start _ t__.p_c..
+# ___ i __ ra.. 20
+#     ? p_p.. ?
+#
+# print('--Sequential version--')
+# print _*Final counter: |c.. .')
+# print _*Took |t__.p_c.. - s.. : .2_| seconds.')
+#
+# ###########################################################################
+#
+# counter _ 0
+#
+# threads _ ?.T.. t.._? a.._ p.._p.. i| || ___ ? __ ra.. 20
+#
+# start _ t__.p_c..
+# ___ thread __ ?
+#     ?.s..
+# ___ ? __ ?
+#     ?.j..
+#
+# print('--Concurrent version--')
+# print _*Final counter: |c.. .')
+# print _*Took |t__.p_c.. - s.. : .2_| seconds.')
+#
+# ###########################################################################
+#
+# print('Finished.')
