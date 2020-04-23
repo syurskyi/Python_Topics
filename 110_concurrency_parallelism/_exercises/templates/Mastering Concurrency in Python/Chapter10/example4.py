a8d32c14da307f0b09b736f3df4b6fd809f4020f
Grey <sergejyurskyj@yahoo.com>
2020-04-22 18:00:01 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter10/example4.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter10/example4.py
@@ -1,48 +1,48 @@
-# ch9/example1.py
-
-____ ma__ ______ sqrt
-______ a..
-____ t_i_ ______ d_t_ as timer
-
-? ___ is_prime(x):
-    print('Processing %i...' % x)
-
-    __ x < 2:
-        print('%i is not a prime number.' % x)
-
-    ____ x __ 2:
-        print('%i is a prime number.' % x)
-
-    ____ x % 2 __ 0:
-        print('%i is not a prime number.' % x)
-
-    ____
-        limit _ int(sqrt(x)) + 1
-        ___ i __ ra..(3, limit, 2):
-            __ x % i __ 0:
-                print('%i is not a prime number.' % x)
-                r_
-            ____ i % 100000 __ 1:
-                ? ?.s..(0)
-
-        print('%i is a prime number.' % x)
-
-? ___ main():
-
-    task1 _ loop.create_task(is_prime(9637529763296797))
-    task2 _ loop.create_task(is_prime(427920331))
-    task3 _ loop.create_task(is_prime(157))
-
-    ? ?.wait([task1, task2, task3])
-
-__ _______ __ _______
-    ___
-        start _ timer()
-        loop _ ?.g_e_l..
-        loop.r_u_c..(main())
-        print('Took %.2f seconds.' % (timer() - start))
-    ______ E.. as e:
-        print('There was a problem:')
-        print(st.(e))
-    f..
-        loop.close()
+# # ch9/example1.py
+#
+# ____ ma__ ______ sqrt
+# ______ a..
+# ____ t_i_ ______ d_t_ __ timer
+#
+# ? ___ is_prime x
+#     print('Processing @...'  ?
+#
+#     __ ? < 2
+#         print('@ is not a prime number.'  ?
+#
+#     ____ ? __ 2
+#         print('@ is a prime number.'  ?
+#
+#     ____ ?  2 __ 0
+#         print('@ is not a prime number.'  ?
+#
+#     ____
+#         limit _ in. sq.. ? + 1
+#         ___ i __ ra.. 3 ? 2
+#             __ ?  ? __ 0
+#                 print('@ is not a prime number.'  ?
+#                 r_
+#             ____ ?  100000 __ 1
+#                 ? ?.s.. 0
+#
+#         print('@ is a prime number.'  ?)
+#
+# ? ___ main
+#
+#     task1 _ ?.c_t.. ?(9637529763296797
+#     task2 _ ?.c_t.. ?(427920331
+#     task3 _ ?.c_t.. ?(157
+#
+#     ? ?.w.. _1 _2 _3
+#
+# __ _______ __ _______
+#     ___
+#         start _ ti..
+#         loop _ ?.g_e_l..
+#         ?.r_u_c.. m..
+#         print('Took .2_ seconds.'   t.. - s..
+#     ______ E.. __ e
+#         print('There was a problem:')
+#         print(st. ?
+#     f..
+#         l__.c..
