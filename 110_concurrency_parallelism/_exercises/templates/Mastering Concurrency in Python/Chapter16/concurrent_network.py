5de2930bcee759470b09fc469f4ffe00681d2992
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:36:21 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter16/concurrent_network.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter16/concurrent_network.py
@@ -1,36 +1,36 @@
-# ch16/concurrent_network.py
-
-____ copy ______ deepcopy
-______ ti..
-____ random ______ choice
-
-c_ Network:
-    ___  - (self, primary_key, primary_value):
-        self.primary_key _ primary_key
-        self.data _ {primary_key: primary_value}
-
-    ___ -s(self):
-        result _ '{\n'
-        ___ key __ self.data:
-            result +_ f'\t{key}: {self.data[key]};\n'
-
-        r_ result + '}'
-
-    ___ add_node(self, key, value):
-        __ key not __ self.data:
-            self.data[key] _ value
-            r_ T..
-
-        r_ F..
-
-    # precondition: the object has more than one node left
-    ___ refresh_primary(self):
-        del self.data[self.primary_key]
-        self.primary_key _ choice(li..(self.data))
-
-    ___ get_primary_value(self):
-        copy_network _ deepcopy(self)
-
-        primary_key _ copy_network.primary_key
-        t__.s..(1) # creating a delay
-        r_ copy_network.data[primary_key]
+# # ch16/concurrent_network.py
+#
+# ____ copy ______ de..
+# ______ ti..
+# ____ ra.. ______ ch..
+#
+# c_ Network
+#     ___  -  primary_key, primary_value):
+#         ? ?
+#         data _ |p.. ; ?
+#
+#     ___ -s
+#         result _ '{\n'
+#         ___ key __ d..
+#             r.. +_ _*\t|k.. : |d..|k..||;\n'
+#
+#         r_ ? + '}'
+#
+#     ___ add_node key, value
+#         __ ? no. __ d..
+#             d..|? _ v..
+#             r_ T..
+#
+#         r_ F..
+#
+#     # precondition: the object has more than one node left
+#     ___ refresh_primary
+#         de. d..|p..
+#         primary_key _ ch.. li.. d..
+#
+#     ___ get_primary_value
+#         copy_network _ de..
+#
+#         primary_key _ ?.p_k..
+#         t__.s.. 1 # creating a delay
+#         r_ c_n_.d..|p..
