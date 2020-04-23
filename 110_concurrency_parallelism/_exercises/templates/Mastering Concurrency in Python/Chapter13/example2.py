bfeebd2eebf1afe30b7b70936468c57cb3d54fcc
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:34:49 -0700

    Committed Mastering Concurrency in Python example2 templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter13/example2.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter13/example2.py
@@ -1,54 +1,54 @@
-# ch13/example2.py
-
-______ th..
-
-___ writer():
-    g.. text
-    g.. wcount
-
-    w__ T..:
-        w__ wcounter:
-            wcount +_ 1
-            __ wcount __ 1:
-                read_try.a..
-
-        w__ resource:
-            print(f'Writing being done by {?.current_thread().name}.')
-            text +_ f'Writing was done by {?.current_thread().name}. '
-
-        w__ wcounter:
-            wcount -_ 1
-            __ wcount __ 0:
-                read_try.release()
-
-___ reader():
-    g.. rcount
-
-    w__ T..:
-        w__ read_try:
-            w__ rcounter:
-                rcount +_ 1
-                __ rcount __ 1:
-                    resource.a..
-
-            print(f'Reading being done by {?.current_thread().name}:')
-            print(text)
-
-            w__ rcounter:
-                rcount -_ 1
-                __ rcount __ 0:
-                    resource.release()
-
-text _ 'This is some text. '
-wcount _ 0
-rcount _ 0
-
-wcounter _ ?.Lock()
-rcounter _ ?.Lock()
-resource _ ?.Lock()
-read_try _ ?.Lock()
-
-threads _ [?.T..(target_reader) ___ i __ ra..(3)] + [?.T..(target_writer) ___ i __ ra..(2)]
-
-___ thread __ threads:
-    thread.s..
+# # ch13/example2.py
+#
+# ______ th..
+#
+# ___ writer
+#     g.. text
+#     g.. wcount
+#
+#     w__ T..
+#         w__ wc..
+#             ? +_ 1
+#             __ ? __ 1
+#                 r_t_.a..
+#
+#         w__ r..
+#             print _*Writing being done by |?.c_t_ .n.. .')
+#             t.. +_ f'Writing was done by |?.c_t_ .n.. . '
+#
+#         w__ wc..
+#             ? -_ 1
+#             __ ? __ 0
+#                 r_t_.r..
+#
+# ___ reader
+#     g.. rcount
+#
+#     w__ T..
+#         w__ r_t..
+#             w__ rc..
+#                 ? +_ 1
+#                 __ ? __ 1
+#                     r__.a..
+#
+#             print _*Reading being done by |?.c_t__ .n.. :')
+#             print t..
+#
+#             w__ rc..
+#                 ? -_ 1
+#                 __ ? __ 0
+#                     r__.r..
+#
+# text _ 'This is some text. '
+# wcount _ 0
+# rcount _ 0
+#
+# wcounter _ ?.L..
+# rcounter _ ?.L..
+# resource _ ?.L..
+# read_try _ ?.L..
+#
+# threads _ ?.T.. t_r.. ___ i __ ra.. 3||+ |?.T.. t.._w.. ___ i __ ra.. 2
+#
+# ___ thread __ ?
+#     ?.s..
