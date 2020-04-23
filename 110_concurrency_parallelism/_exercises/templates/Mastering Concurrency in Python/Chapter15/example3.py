7be454fc508f2d20c922a9cbadc661eba4d22227
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:52:18 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter15/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter15/example3.py
@@ -1,52 +1,52 @@
-# ch15/example3.py
-
-______ ti..
-______ th..
-____ m.. ______ Pool
-
-COUNT _ 50000000
-
-___ countdown(n):
-    w__ n > 0:
-        n -_ 1
-
-__ _______ __ _______
-
-    #######################################################################
-    # Sequential
-
-    start _ t__.t__()
-    countdown(COUNT)
-
-    print('Sequential program finished.')
-    print(f'Took {t__.t__() - start : .2f} seconds.')
-    print()
-
-    #######################################################################
-    # Multithreading
-
-    thread1 _ ?.T..(target_countdown, args_(COUNT // 2,))
-    thread2 _ ?.T..(target_countdown, args_(COUNT // 2,))
-
-    start _ t__.t__()
-    thread1.s..
-    thread2.s..
-    thread1.j..
-    thread2.j..
-
-    print('Multithreading program finished.')
-    print(f'Took {t__.t__() - start : .2f} seconds.')
-    print()
-
-    #######################################################################
-    # Multiprocessing
-
-    pool _ Pool(processes_2)
-    start _ t__.t__()
-    pool.apply_async(countdown, args_(COUNT//2,))
-    pool.apply_async(countdown, args_(COUNT//2,))
-    pool.c..
-    pool.j..
-
-    print('Multiprocessing program finished.')
-    print(f'Took {t__.t__() - start : .2f} seconds.')
+# # ch15/example3.py
+#
+# ______ ti..
+# ______ th..
+# ____ m.. ______ Pool
+#
+# COUNT _ 50000000
+#
+# ___ countdown n
+#     w__ ? > 0:
+#         ? -_ 1
+#
+# __ _______ __ _______
+#
+#     #######################################################################
+#     # Sequential
+#
+#     start _ t__.t__
+#     c.. C..
+#
+#     print('Sequential program finished.')
+#     print _*Took |t__.t__ - s.. : .2_| seconds.')
+#     print()
+#
+#     #######################################################################
+#     # Multithreading
+#
+#     thread1 _ ?.T.. t.._? a.._ C.. // 2,
+#     thread2 _ ?.T.. t.._? a.._ C.. // 2,
+#
+#     start _ t__.t__
+#     _1.s..
+#     _2.s..
+#     _1.j..
+#     _2.j..
+#
+#     print('Multithreading program finished.')
+#     print _*Took {t__.t__ - s.. : .2_ seconds.')
+#     print()
+#
+#     #######################################################################
+#     # Multiprocessing
+#
+#     pool _ P.. p.._2
+#     start _ t__.t__
+#     ?.a_a.. ? a.._ C..//2,
+#     ?.a_a.. c.., a.._ C..//2
+#     ?.c..
+#     ?.j..
+#
+#     print('Multiprocessing program finished.')
+#     print _*Took {t__.t__ - s.. : .2_ seconds.')
