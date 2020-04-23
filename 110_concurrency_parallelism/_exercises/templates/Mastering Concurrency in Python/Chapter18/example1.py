e5a4922d5719abbc02356719ecdba6b269aa7981
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:53:19 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter18/example1.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter18/example1.py
@@ -1,42 +1,42 @@
-# ch18/example1.py
-
-______ socket
-
-# Main event loop
-___ reactor(host, port):
-    sock _ socket.socket()
-    sock.bind((host, port))
-    sock.listen(5)
-    print(f'Server up, running, and waiting for call on {host} {port}')
-
-    ___
-        w__ T..:
-            conn, cli_address _ sock.accept()
-            process_request(conn, cli_address)
-
-    f..
-        sock.close()
-
-___ process_request(conn, cli_address):
-    file _ conn.makefile()
-
-    print(f'Received connection from {cli_address}')
-
-    ___
-        w__ T..:
-            line _ file.readline()
-            __ line:
-                line _ line.rstrip()
-                __ line __ 'quit':
-                    conn.sendall(b'connection closed\r\n')
-                    r_
-
-                print(f'{cli_address} --> {line}')
-                conn.sendall(b'Echoed: %a\r\n' % line)
-    f..
-        print(f'{cli_address} quit')
-        file.close()
-        conn.close()
-
-__ _______ __ _______
-    reactor('localhost', 8080)
+# # ch18/example1.py
+#
+# ______ so..
+#
+# # Main event loop
+# ___ reactor host port
+#     sock _ s__.s__
+#     ?.bind host port
+#     ?.li..  5
+#     print _*Server up, running, and waiting for call on |h.. |p..
+#
+#     ___
+#         w__ T..
+#             conn, cli_address _ s__.a..
+#             process_request c.. c..
+#
+#     f..
+#         s__.c..
+#
+# ___ process_request conn, cli_address
+#     file _ ?.m_f_
+#
+#     print _*Received connection from |c_a..
+#
+#     ___
+#         w__ T..
+#             line _ f__.r_l..
+#             __ ?
+#                 l.. _ ?.rs..
+#                 __ ? __ 'quit'
+#                     c__.s_a.. _'connection closed\r\n')
+#                     r_
+#
+#                 print _$|c_a.. --> |l..
+#                 c__.s_a.. _'Echoed: @\r\n'  l..
+#     f..
+#         print _$|c_a..| quit')
+#         f__.c..
+#         c__.c..
+#
+# __ _______ __ _______
+#     ? 'localhost' 8080
