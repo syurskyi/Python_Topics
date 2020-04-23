88259c3e44e993db8b5fda06ce98b7464050f81f
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:53:46 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter18/example4.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter18/example4.py
@@ -1,13 +1,13 @@
-# ch18/example4.py
-
-___ read_data():
-    ___ i __ ra..(5):
-        print('Inside the inner for loop...')
-        yield i * 2
-
-result _ read_data()
-___ i __ ra..(6):
-    print('Inside the outer for loop...')
-    print(next(result))
-
-print('Finished.')
+# # ch18/example4.py
+#
+# ___ read_data
+#     ___ i __ ra.. 5
+#         print('Inside the inner for loop...')
+#         ? ? * 2
+#
+# result _ ?
+# ___ i __ ra.. 6
+#     print('Inside the outer for loop...')
+#     print n.. ?
+#
+# print('Finished.')
