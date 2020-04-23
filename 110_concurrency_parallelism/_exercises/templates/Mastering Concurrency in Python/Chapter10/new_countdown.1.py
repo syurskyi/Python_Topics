aaf8681a8ba99bdd7a4e73dc8815b888d0192b89
Grey <sergejyurskyj@yahoo.com>
2020-04-22 18:11:35 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter10/new_countdown.1.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter10/new_countdown.1.py
@@ -1,31 +1,31 @@
-# ch10/example2.py
-
-______ a..
-______ ti..
-
-? ___ count_down(name, delay):
-    indents _ (or.(name) - or.('A')) * '\t'
-
-    n _ 3
-    w__ n:
-        ? ?.s..(delay)
-
-        duration _ t__.p_c.. - start
-        print('-' * 40)
-        print('%.4f \t%s%s = %i' % (duration, indents, name, n))
-
-        n -_ 1
-
-? ___ main():
-
-    ? ?.gather(*[
-        count_down('A', 1),
-        count_down('B', 0.8),
-        count_down('C', 0.5)
-    ])
-
-start _ t__.p_c..
-?.run(main())
-
-print('-' * 40)
-print('Done.')
+# # ch10/example2.py
+#
+# ______ a..
+# ______ ti..
+#
+# ? ___ count_down name delay
+#     indents _ (or. ? - or.('A')) * '\t'
+#
+#     n _ 3
+#     w__ n:
+#         ? ?.s.. d..
+#
+#         duration _ t__.p_c.. - s..
+#         print('-' * 40)
+#         print('@.4_ \t@@ = @'  d.. i.. n.. n
+#
+#         ? -_ 1
+#
+# ? ___ main
+#
+#     ? ?.g.. $|
+#         ? 'A', 1),
+#         ? 'B', 0.8),
+#         ? 'C', 0.5)
+#     ||
+#
+# start _ t__.p_c..
+# ?.r.. m..
+#
+# print('-' * 40)
+# print('Done.')
