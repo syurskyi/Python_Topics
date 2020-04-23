8b1d40ef96a18e72f8093bb05cfe0a8c170cd7df
Grey <sergejyurskyj@yahoo.com>
2020-04-22 17:15:28 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter10/example1.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter10/example1.py
@@ -1,25 +1,25 @@
-# ch10/example1.py
-
-______ ti..
-
-___ count_down(name, delay):
-    indents _ (ord(name) - ord('A')) * '\t'
-
-    n _ 3
-    w__ n:
-        t__.s..(delay)
-
-        duration _ t__.perf_counter() - start
-        print('-' * 40)
-        print('%.4f \t%s%s = %i' % (duration, indents, name, n))
-
-        n -_ 1
-
-start _ t__.perf_counter()
-
-count_down('A', 1)
-count_down('B', 0.8)
-count_down('C', 0.5)
-
-print('-' * 40)
-print('Done.')
+# # ch10/example1.py
+#
+# ______ ti..
+#
+# ___ count_down name delay
+#     indents _ or. n.. - or.('A')) * '\t'
+#
+#     n _ 3
+#     w__ n:
+#         t__.s.. d..
+#
+#         duration _ t__.p_c.. - s..
+#         print('-' * 40)
+#         print('@.4_ \t@@ = @'  d.. i.. n.. n
+#
+#         n -_ 1
+#
+# start _ t__.p_c..
+#
+# ?('A', 1)
+# ?('B', 0.8)
+# ?('C', 0.5)
+#
+# print('-' * 40)
+# print('Done.')
