db5b57d2d5790b487011d4409b0f606eed33241f
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:32:21 -0700

    Committed Mastering Concurrency in Python example4 templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter11/example4.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter11/example4.py
@@ -1,16 +1,16 @@
-# ch11/example4.py
-
-______ ai..
-______ a..
-
-? ___ get_html(session, url):
-    ? w__ session.g..(url, ssl_F..) as res:
-        r_ ? res.text()
-
-? ___ main():
-    ? w__ ?.ClientSession() as session:
-        html _ ? get_html(session, 'http://packtpub.com')
-        print(html)
-
-loop _ ?.g_e_l..
-loop.r_u_c..(main())
+# # ch11/example4.py
+#
+# ______ ai..
+# ______ a..
+#
+# ? ___ get_html session url
+#     ? w__ s__.g.. ? s.._F.. __ res
+#         r_ ? ?.t..
+#
+# ? ___ main
+#     ? w__ ?.CS.. __ session
+#         html _ ? g_h.. ? 'http://packtpub.com')
+#         print ?
+#
+# loop _ ?.g_e_l..
+# ?.r_u_c.. ?
