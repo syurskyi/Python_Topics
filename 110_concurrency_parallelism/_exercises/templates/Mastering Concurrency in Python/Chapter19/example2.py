46675e1b3d07efa1135158548c2401c04d7ae1a1
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:54:13 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter19/example2.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter19/example2.py
@@ -1,23 +1,23 @@
-# ch19/example2.py
-
-____ d_t_ ______ d_t_
-______ ti..
-
-____ apscheduler.schedulers.background ______ BackgroundScheduler
-
-___ tick():
-    print(f'Tick! The time is: {d_t_.now()}')
-
-__ _______ __ _______
-    scheduler _ BackgroundScheduler()
-    scheduler.add_job(tick, 'interval', seconds_3)
-    scheduler.s..
-
-    ___
-        w__ T..:
-            t__.s..(2)
-            print('Printing in the main thread.')
-    ______ K..
-        p..
-
-scheduler.shutdown()
+# # ch19/example2.py
+#
+# ____ d_t_ ______ d_t_
+# ______ ti..
+#
+# ____ apscheduler.schedulers.background ______ BackgroundScheduler
+#
+# ___ tick
+#     print _*Tick! The time is: {d_t_.no..
+#
+# __ _______ __ _______
+#     scheduler _ ?
+#     ?.a_j.. ? 'interval' s_3
+#     ?.s..
+#
+#     ___
+#         w__ T..
+#             t__.s.. 2
+#             print('Printing in the main thread.')
+#     ______ K..
+#         p..
+#
+# ?.sh
