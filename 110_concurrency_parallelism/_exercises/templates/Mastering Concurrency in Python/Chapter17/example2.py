fcd0bd5f181ad0abbaabb2c2a31b193dcb38f249
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:53:10 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter17/example2.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter17/example2.py
@@ -1,25 +1,25 @@
-# ch17/example2.py
-
-______ ___; ___.setswitchinterval(.000001)
-______ th..
-
-___ foo():
-    g.. my_list
-    my_list.ap..(1)
-
-my_list _    # list
-
-threads _    # list
-___ i __ ra..(1000):
-    thread _ ?.T..(target_foo)
-    threads.ap..(thread)
-
-___ thread __ threads:
-    thread.s..
-
-___ thread __ threads:
-    thread.j..
-
-print(f'Final list length: {le.(my_list)}.')
-
-print('Finished.')
+# # ch17/example2.py
+#
+# ______ ___; ___.setswitchinterval(.000001)
+# ______ th..
+#
+# ___ foo
+#     g.. my_list
+#     ?.ap.. 1
+#
+# my_list _    # list
+#
+# threads _    # list
+# ___ i __ ra.. 1000
+#     thread _ ?.T.. t.._?
+#     ?s.ap.. ?
+#
+# ___ ? __ ?
+#     ?.s..
+#
+# ___ ? __ ?
+#     ?.j..
+#
+# print _*Final list length: |le. m__.')
+#
+# print('Finished.')
