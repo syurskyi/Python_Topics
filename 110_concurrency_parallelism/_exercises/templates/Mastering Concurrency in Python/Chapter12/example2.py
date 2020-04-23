391fe0b2b5df0177ce9c76190263ea3f9d8a582e
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:33:10 -0700

    Committed Mastering Concurrency in Python example2 templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter12/example2.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter12/example2.py
@@ -1,52 +1,52 @@
-# ch12/example2.py
-
-______ th..
-______ ti..
-
-___ thread_a():
-    print('Thread A is starting...')
-
-    print('Thread A waiting to acquire lock A.')
-    lock_a.a..
-    print('Thread A has acquired lock A, performing some calculation...')
-    t__.s..(2)
-
-    print('Thread A waiting to acquire lock B.')
-    lock_b.a..
-    print('Thread A has acquired lock B, performing some calculation...')
-    t__.s..(2)
-
-    print('Thread A releasing both locks.')
-    lock_a.release()
-    lock_b.release()
-
-___ thread_b():
-    print('Thread B is starting...')
-
-    print('Thread B waiting to acquire lock A.')
-    lock_a.a..
-    print('Thread B has acquired lock A, performing some calculation...')
-    t__.s..(5)
-
-    print('Thread B waiting to acquire lock B.')
-    lock_b.a..
-    print('Thread B has acquired lock B, performing some calculation...')
-    t__.s..(5)
-
-    print('Thread B releasing both locks.')
-    lock_b.release()
-    lock_a.release()
-
-lock_a _ ?.Lock()
-lock_b _ ?.Lock()
-
-thread1 _ ?.T..(target_thread_a)
-thread2 _ ?.T..(target_thread_b)
-
-thread1.s..
-thread2.s..
-
-thread1.j..
-thread2.j..
-
-print('Finished.')
+# # ch12/example2.py
+#
+# ______ th..
+# ______ ti..
+#
+# ___ thread_a
+#     print('Thread A is starting...')
+#
+#     print('Thread A waiting to acquire lock A.')
+#     lock_a.a..
+#     print('Thread A has acquired lock A, performing some calculation...')
+#     t__.s.. 2
+#
+#     print('Thread A waiting to acquire lock B.')
+#     lock_b.a..
+#     print('Thread A has acquired lock B, performing some calculation...')
+#     t__.s.. 2
+#
+#     print('Thread A releasing both locks.')
+#     _a.r..
+#     _b.r..
+#
+# ___ thread_b
+#     print('Thread B is starting...')
+#
+#     print('Thread B waiting to acquire lock A.')
+#     _a.a..
+#     print('Thread B has acquired lock A, performing some calculation...')
+#     t__.s.. 5
+#
+#     print('Thread B waiting to acquire lock B.')
+#     _b.a..
+#     print('Thread B has acquired lock B, performing some calculation...')
+#     t__.s.. 5
+#
+#     print('Thread B releasing both locks.')
+#     _b.r..
+#     _a.r..
+#
+# lock_a _ ?.L
+# lock_b _ ?.L
+#
+# thread1 _ ?.T.. t_?_a
+# thread2 _ ?.T.. t_?_b
+#
+# _1.s..
+# _2.s..
+#
+# _1.j..
+# _2.j..
+#
+# print('Finished.')
