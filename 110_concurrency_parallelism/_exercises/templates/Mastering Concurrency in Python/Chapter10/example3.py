092389273c21d5b956f44de427fda66b4824cbde
Grey <sergejyurskyj@yahoo.com>
2020-04-22 17:57:06 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter10/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter10/example3.py
@@ -1,33 +1,33 @@
-# ch9/example1.py
-
-____ ma__ ______ sqrt
-____ t_i_ ______ d_t_ as timer
-
-___ is_prime(x):
-    print('Processing %i...' % x)
-
-    __ x < 2:
-        print('%i is not a prime number.' % x)
-
-    ____ x __ 2:
-        print('%i is a prime number.' % x)
-
-    ____ x % 2 __ 0:
-        print('%i is not a prime number.' % x)
-
-    ____
-        limit _ int(sqrt(x)) + 1
-        ___ i __ ra..(3, limit, 2):
-            __ x % i __ 0:
-                print('%i is not a prime number.' % x)
-                r_
-
-        print('%i is a prime number.' % x)
-
-__ _______ __ _______
-
-    start _ timer()
-    is_prime(9637529763296797)
-    is_prime(427920331)
-    is_prime(157)
-    print('Took %.2f seconds.' % (timer() - start))
+# # ch9/example1.py
+#
+# ____ ma__ ______ sqrt
+# ____ t_i_ ______ d_t_ __ timer
+#
+# ___ is_prime x
+#     print('Processing @...'  ?
+#
+#     __ ? < 2
+#         print('@ is not a prime number.'  ?
+#
+#     ____ ? __ 2
+#         print('@ is a prime number.'  ?
+#
+#     ____ ? % 2 __ 0
+#         print('@ is not a prime number.'  ?
+#
+#     ____
+#         limit _ in. sq.. ? + 1
+#         ___ i __ ra.. 3 ? 2
+#             __ ? % ? __ 0
+#                 print('@ is not a prime number.'  ?
+#                 r_
+#
+#         print('@ is a prime number.'  ?
+#
+# __ _______ __ _______
+#
+#     start _ ti..
+#     ? 9637529763296797)
+#     ? 427920331)
+#     ? 157)
+#     print('Took @.2_ seconds.'  t.. - s..
