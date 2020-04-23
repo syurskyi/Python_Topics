170d4b07b2a525a7a8894eb6ff2f0bc8213cbf11
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:35:01 -0700

    Committed Mastering Concurrency in Python example3 templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter13/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter13/example3.py
@@ -1,46 +1,46 @@
-# ch13/example3.py
-
-______ th..
-
-___ writer():
-    g.. text
-
-    w__ T..:
-        w__ service:
-            resource.a..
-
-        print(f'Writing being done by {?.current_thread().name}.')
-        text +_ f'Writing was done by {?.current_thread().name}. '
-
-        resource.release()
-
-___ reader():
-    g.. rcount
-
-    w__ T..:
-        w__ service:
-            rcounter.a..
-            rcount +_ 1
-            __ rcount __ 1:
-                resource.a..
-        rcounter.release()
-
-        print(f'Reading being done by {?.current_thread().name}:')
-        #print(text)
-
-        w__ rcounter:
-            rcount -_ 1
-            __ rcount __ 0:
-                resource.release()
-
-text _ 'This is some text. '
-rcount _ 0
-
-rcounter _ ?.Lock()
-resource _ ?.Lock()
-service _ ?.Lock()
-
-threads _ [?.T..(target_reader) ___ i __ ra..(3)] + [?.T..(target_writer) ___ i __ ra..(2)]
-
-___ thread __ threads:
-    thread.s..
+# # ch13/example3.py
+#
+# ______ th..
+#
+# ___ writer
+#     g.. text
+#
+#     w__ T..
+#         w__ s..
+#             r__.a..
+#
+#         print _*Writing being done by |?.c_t__ ....
+#         text +_ _*Writing was done by |?.c_t__ ....
+#
+#         resource.r..
+#
+# ___ reader
+#     g.. rcount
+#
+#     w__ T..
+#         w__ s..
+#             rc__.a..
+#             ? +_ 1
+#             __ ? __ 1
+#                 r__.a..
+#         rc__.r..
+#
+#         print(f'Reading being done by {?.c_t.. .n.. :')
+#         #print(text)
+#
+#         w__ rc..
+#             ? -_ 1
+#             __ ? __ 0:
+#                 r_.r..
+#
+# text _ 'This is some text. '
+# rcount _ 0
+#
+# rcounter _ ?.L..
+# resource _ ?.L..
+# service _ ?.L..
+#
+# threads _ ?.T.. t_r.. ___ i __ ra.. 3|| + |?.T.. t_w.. ___ ? __ ra.. 2
+#
+# ___ thread __ ?
+#     ?.s..
