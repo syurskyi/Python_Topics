2a112d68cce43f6c92566b0a49d07454708cf829
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:34:22 -0700

    Committed Mastering Concurrency in Python example8 templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter12/example8.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter12/example8.py
@@ -1,40 +1,40 @@
-# ch12/example8.py
-
-______ th..
-______ ti..
-
-c_ Spouse(?.T..):
-
-    ___  - (self, name, partner):
-        ?.T... - (self)
-        self.name _ name
-        self.partner _ partner
-        self.hungry _ T..
-
-    ___ run(self):
-        w__ self.hungry:
-            print('%s is hungry and wants to eat.' % self.name)
-
-            __ self.partner.hungry:
-                print('%s is waiting for their partner to eat first...' % self.name)
-            ____
-                w__ fork:
-                    print('%s has stared eating.' % self.name)
-                    t__.s..(5)
-
-                    print('%s is now full.' % self.name)
-                    self.hungry _ F..
-
-fork _ ?.Lock()
-
-partner1 _ Spouse('Wife', N..)
-partner2 _ Spouse('Husband', partner1)
-partner1.partner _ partner2
-
-partner1.s..
-partner2.s..
-
-partner1.j..
-partner2.j..
-
-print('Finished.')
+# # ch12/example8.py
+#
+# ______ th..
+# ______ ti..
+#
+# c_ Spouse ?.T..
+#
+#     ___  - name partner
+#         ?.T... - ?
+#         ? ?
+#         ? ?
+#         hungry _ T..
+#
+#     ___ run
+#         w__ hungry
+#             print('@ is hungry and wants to eat.'  n..
+#
+#             __ p__.hu..
+#                 print('@ is waiting for their partner to eat first...' n..
+#             ____
+#                 w__ fork
+#                     print('@ has stared eating.' n..
+#                     t__.s.. 5
+#
+#                     print('@ is now full.' n..
+#                     self.hungry _ F..
+#
+# fork _ ?.L..
+#
+# partner1 _ S.. ('Wife', N..
+# partner2 _ S.. ('Husband', partner1
+# partner1.partner _ partner2
+#
+# partner1.s..
+# partner2.s..
+#
+# partner1.j..
+# partner2.j..
+#
+# print('Finished.')
