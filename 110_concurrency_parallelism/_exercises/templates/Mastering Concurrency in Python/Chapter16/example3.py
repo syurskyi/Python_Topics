119f61ccedce0af002b70070177bb6044eea46b4
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:36:44 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter16/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter16/example3.py
@@ -1,46 +1,46 @@
-# ch16/example3.py
-
-______ th..
-____ c__.f.. ______ TPE..
-______ ti..
-______ matplotlib.pyplot as plt
-
-c_ LockedCounter:
-    ___  - (self):
-        self.value _ 0
-        self.lock _ ?.Lock()
-
-    ___ increment(self, x):
-        w__ self.lock:
-            new_value _ self.value + x
-            t__.s..(0.001) # creating a delay
-            self.value _ new_value
-
-    ___ get_value(self):
-        w__ self.lock:
-            value _ self.value
-
-        r_ value
-
-n_threads _    # list
-times _    # list
-___ n_workers __ ra..(1, 11):
-    n_threads.ap..(n_workers)
-
-    counter _ LockedCounter()
-
-    start _ t__.t__()
-
-    w__ TPE..(max_workers_n_workers) as executor:
-        executor.m..(counter.increment, [1 ___ i __ ra..(100 * n_workers)])
-
-    times.ap..(t__.t__() - start)
-
-    print(f'Number of threads: {n_workers}')
-    print(f'Final counter: {counter.get_value()}.')
-    print(f'Time taken: {times[-1] : .2f} seconds.')
-    print('-' * 40)
-
-plt.plot(n_threads, times)
-plt.xlabel('Number of threads'); plt.ylabel('Time in seconds')
-plt.show()
+# # ch16/example3.py
+#
+# ______ th..
+# ____ c__.f.. ______ TPE..
+# ______ ti..
+# ______ matplotlib.pyplot __ plt
+#
+# c_ LockedCounter
+#     ___  -
+#         value _ 0
+#         lock _ ?.L..
+#
+#     ___ increment x
+#         w__ l..
+#             new_value _ v.. + ?
+#             t__.s.. 0.001 # creating a delay
+#             value _ ?
+#
+#     ___ get_value
+#         w__ l..
+#             ? ?
+#
+#         r_ ?
+#
+# n_threads _    # list
+# times _    # list
+# ___ n_workers __ ra.. 1, 11
+#     n_t_.ap.. ?
+#
+#     counter _ ?
+#
+#     start _ t__.t__
+#
+#     w__ TPE.. m_w.._? __ executor
+#         ?.m.. c__.i..  1 ___ i __ ra.. 100 * n_
+#
+#     t__.ap.. t__.t__ - s..
+#
+#     print _*Number of threads: |n_ ')
+#     print _*Final counter: |c__.g_v.. .
+#     print _*Time taken: |t.. -1 ; .2_| seconds.
+#     print('-' * 40)
+#
+# ?.pl.. n_t.. t..
+# ?.xl..('Number of threads'); ?.yl..('Time in seconds')
+# ?.s..
