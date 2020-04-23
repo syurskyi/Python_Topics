f84fe230ee3b7233ee0dee8a2c24da04ba5ad8b1
Grey <sergejyurskyj@yahoo.com>
2020-04-23 09:53:39 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter18/example3.py
+++ 110_concurrency_parallelism/_exercises/templates/Mastering Concurrency in Python/Chapter18/example3.py
@@ -1,67 +1,67 @@
-# ch18/example3.py
-
-______ socket
-____ operator ______ mul
-____ functools ______ reduce
-
-# Main event loop
-___ reactor(host, port):
-    sock _ socket.socket()
-    sock.bind((host, port))
-    sock.listen(5)
-    print(f'Server up, running, and waiting for call on {host} {port}')
-
-    ___
-        w__ T..:
-            conn, cli_address _ sock.accept()
-            process_request(conn, cli_address)
-
-    f..
-        sock.close()
-
-___ process_request(conn, cli_address):
-    file _ conn.makefile()
-
-    print(f'Received connection from {cli_address}')
-    mode _ 'sum'
-
-    ___
-        conn.sendall(b'<welcome: starting in sum mode>\n')
-        w__ T..:
-            line _ file.readline()
-            __ line:
-                line _ line.rstrip()
-                __ line __ 'quit':
-                    conn.sendall(b'connection closed\r\n')
-                    r_
-
-                __ line __ 'sum':
-                    conn.sendall(b'<switching to sum mode>\r\n')
-                    mode _ 'sum'
-                    c..
-                __ line __ 'product':
-                    conn.sendall(b'<switching to product mode>\r\n')
-                    mode _ 'product'
-                    c..
-
-                print(f'{cli_address} --> {line}')
-                ___
-                    nums _ li..(m..(int, line.split(',')))
-                ______ ValueError:
-                    conn.sendall(
-                        b'ERROR. Enter only integers separated by commas\n')
-                    c..
-
-                __ mode __ 'sum':
-                    conn.sendall(b'Sum of input numbers: %a\r\n'
-                        % st.(su.(nums)))
-                ____
-                    conn.sendall(b'Product of input numbers: %a\r\n'
-                        % st.(reduce(mul, nums, 1)))
-    f..
-        print(f'{cli_address} quit')
-        file.close()
-        conn.close()
-
-__ _______ __ _______
-    reactor('localhost', 8080)
+# # ch18/example3.py
+#
+# ______ so..
+# ____ op..______ mul
+# ____ fu..______ reduce
+#
+# # Main event loop
+# ___ reactor host port
+#     sock _ s__.s__
+#     ?.b.. ? ?
+#     ?.l.. 5
+#     print _*Server up, running, and waiting for call on |? |?
+#
+#     ___
+#         w__ T..
+#             conn, cli_address _ ?.a..
+#             p_r.. ?
+#
+#     f..
+#         ?.c..
+#
+# ___ process_request ? ?
+#     file _ conn.m_f..
+#
+#     print _*Received connection from |c..
+#     mode _ 'sum'
+#
+#     ___
+#         c__.s_a.. _'<welcome: starting in sum mode>\n')
+#         w__ T..
+#             line _ f__.r_l..
+#             __ line:
+#                 line _ ?.rs..
+#                 __ line __ 'quit':
+#                     c__.s_a.. _'connection closed\r\n')
+#                     r_
+#
+#                 __ line __ 'sum':
+#                     c__.s_a.. _'<switching to sum mode>\r\n')
+#                     mode _ 'sum'
+#                     c..
+#                 __ line __ 'product':
+#                     c__.s_a.. _'<switching to product mode>\r\n')
+#                     mode _ 'product'
+#                     c..
+#
+#                 print _$|c.. --> |l..')
+#                 ___
+#                     nums _ li.. m.. in. l__.s.. ','
+#                 ______ V..
+#                     c__.s_a..|
+#                         _'ERROR. Enter only integers separated by commas\n')
+#                     c..
+#
+#                 __ mode __ 'sum':
+#                     c__.s_a.. _'Sum of input numbers: %a\r\n'
+#                         % st.(su. n..
+#                 ____
+#                     c__.s_a.. _'Product of input numbers: %a\r\n'
+#                         % st. re.. m.. n.. 1
+#     f..
+#         print _$|c.. quit')
+#         f__.c..
+#         c__.c..
+#
+# __ _______ __ _______
+#     ? 'localhost' 8080
