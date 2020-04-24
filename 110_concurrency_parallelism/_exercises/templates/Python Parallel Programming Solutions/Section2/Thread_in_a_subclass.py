bd9f55f710abf296e6ba80e3d755fae70d242c4f
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:09:14 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/Thread_in_a_subclass.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/Thread_in_a_subclass.py
@@ -1,41 +1,41 @@
-import threading
-import time
-
-exit_Flag = 0
-
-class myThread (threading.Thread):
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
-    def run(self):
-        print ("Starting " + self.name + "\n")
-        print_time(self.name, self.counter, 5)
-        print ("Exiting " + self.name + "\n")
-
-def print_time(threadName, delay, counter):
-    while counter:
-        if exit_Flag:
-            thread.exit()
-        time.sleep(delay)
-        print ("%s: %s" % (threadName, time.ctime(time.time())))
-        counter -= 1
-
-#Main program
-if __name__ == '__main__':
-    # Create two threads
-    thread1 = myThread(1, "Thread-1", 1)
-    thread2 = myThread(2, "Thread-2", 2)
-
-    # Start the Threads created
-    thread1.start()
-    thread2.start()
-
-    # Wait for all thread to complete 
-    thread1.join()
-    thread2.join()
-
-    print ("Exiting Main Thread")
-
-
+# ______ th..
+# ______ ti..
+#
+# exit_Flag _ 0
+#
+# c_ myThread ?.T..
+#     ___  - threadID name counter
+#         ?.T... -
+#         ? ?
+#         ? ?
+#         ? ?
+#     ___ run
+#         print ("Starting " + n.. + "\n")
+#         p_t.. n.. c.. 5)
+#         print ("Exiting " + n.. + "\n")
+#
+# ___ print_time threadName delay counter
+#     w__ c..
+#         __ exit_F..
+#             t__.e..
+#         t__.s.. d..
+#         print ("@: @"  tN.. t__.ct.. t__.t__
+#         c.. -_ 1
+#
+# #Main program
+# __ _______ __ _______
+#     # Create two threads
+#     thread1 _ ? 1, "Thread-1", 1
+#     thread2 _ ? 2, "Thread-2", 2
+#
+#     # Start the Threads created
+#     _1.s..
+#     _2.s..
+#
+#     # Wait for all thread to complete
+#     _1.r..
+#     _2.r..
+#
+#     print ("Exiting Main Thread")
+#
+#
