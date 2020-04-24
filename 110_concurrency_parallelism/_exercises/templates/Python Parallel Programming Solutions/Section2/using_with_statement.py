98380fea94630d7ce6595008eeff039ed41c7add
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:09:24 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/using_with_statement.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/using_with_statement.py
@@ -1,40 +1,34 @@
-import threading
-import logging
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='(%(threadName)-10s) %(message)s',)
-
-def threading_with(statement):
-    with statement:
-        logging.debug('%s acquired via with'  %statement)
-        
-def threading_not_with(statement):
-    statement.a..
-    try:
-        logging.debug('%s acquired directly' %statement )
-    finally:
-        statement.release()
-
-
-
-if __name__ == '__main__':
-
-#let's create a test battery
-    lock = threading.Lock()
-    rlock = threading.RLock()
-    condition = threading.Condition()
-    mutex = threading.Semaphore(1)
-    threading_synchronization_list = [lock ,rlock , condition , mutex]
-
-#in the for cycle we call the threading_with e threading_no_with function
-    for statement in threading_synchronization_list :
-        t1 = threading.Thread(target=threading_with, args=(statement,))
-        t2 = threading.Thread(target=threading_not_with, args=(statement,))
-        t1.start()
-        t2.start()
-        t1.join()
-        t2.join()
-
-
-    
-    
+# ______ th..
+# ______ lo..
+#
+# ?.b.. l.._?.D..
+#                     f.._'|@|tN..-10_| @|m.. _',)
+#
+# ___ threading_with statement
+#     w__ ?
+#         ?.d.. '@ acquired via with' ?
+#
+# ___ threading_not_with( tatement
+#     ?.a..
+#     ___
+#         ?.d.. '@ acquired directly' ?
+#     f..
+#         ?.r..
+#
+# __ _______ __ _______
+#
+# #let's create a test battery
+#     lock _ ?.L..
+#     rlock _ ?.RL..
+#     condition _ ?.C..
+#     mutex _ ?.S.. 1
+#     threading_synchronization_list _ |? ? ? ?
+#
+# #in the for cycle we call the threading_with e threading_no_with function
+#     ___ s.. __ t_s_l..
+#         t1 _ ?.T.. t.._t_w.. a.._ s..
+#         t2 _ ?.T.. t.._t_n_w.. a.._ s..
+#         _1.s..
+#         _2.s..
+#         _1.r..
+#         _2.r..
