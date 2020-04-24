dc1986b38b87ea7e15766a400055990e1f60be65
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:10:12 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section3/deadLockProblems.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section3/deadLockProblems.py
@@ -1,32 +1,32 @@
-from mpi4py import MPI
-
-comm=MPI.COMM_WORLD
-rank = comm.rank
-print("my rank is : " , rank)
-
-if rank==1:
-    data_send= "a"
-    destination_process = 5
-    source_process = 5
-
-    data_received=comm.sendrecv(data_send,dest=destination_process,source=source_process)
-
-
-    print ("sending data %s " %data_send + \
-       "to process %d" %destination_process)
-    print ("data received is = %s" %data_received)
-
-
-
-if rank==5:
-    data_send= "b"
-    destination_process = 1
-    source_process = 1
-
-
-    data_received=comm.sendrecv(data_send,dest=destination_process,source=source_process)
-
-
-    print ("sending data %s :" %data_send + \
-            "to process %d" %destination_process)
-    print ("data received is = %s" %data_received)
+# ____ mpi4py ______ MPI
+#
+# comm_MPI.COMM_WORLD
+# rank _ comm.rank
+# print("my rank is : " , rank)
+#
+# __ rank__1:
+#     data_send_ "a"
+#     destination_process _ 5
+#     source_process _ 5
+#
+#     data_received_comm.sendrecv(data_send,dest_destination_process,source_source_process)
+#
+#
+#     print ("sending data @ " data_send + \
+#        "to process @d" destination_process)
+#     print ("data received is = @" data_received)
+#
+#
+#
+# __ rank__5:
+#     data_send_ "b"
+#     destination_process _ 1
+#     source_process _ 1
+#
+#
+#     data_received_comm.sendrecv(data_send,dest_destination_process,source_source_process)
+#
+#
+#     print ("sending data @ :" data_send + \
+#             "to process @d" destination_process)
+#     print ("data received is = @" data_received)
