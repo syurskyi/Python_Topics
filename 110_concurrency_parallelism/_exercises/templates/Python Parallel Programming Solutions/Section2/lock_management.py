61103ce40095d0cc02c4b4828b219a3ffed31557
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:08:18 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/lock_management.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/lock_management.py
@@ -1,53 +1,53 @@
-import threading
-
-shared_resource_with_lock     = 0
-shared_resource_with_no_lock     = 0
-COUNT = 100000
-shared_resource_lock = threading.Lock()
-
-
-####LOCK MANAGEMENT##
-def increment_with_lock():
-    global shared_resource_with_lock
-    for i in range(COUNT):
-        shared_resource_lock.a..
-        shared_resource_with_lock += 1
-        shared_resource_lock.release()
-
-def decrement_with_lock():
-    global shared_resource_with_lock
-    for i in range(COUNT):
-        shared_resource_lock.a..
-        shared_resource_with_lock -= 1
-        shared_resource_lock.release()
-        
-####NO LOCK MANAGEMENT ##
-def increment_without_lock():
-    global shared_resource_with_no_lock
-    for i in range(COUNT):
-        shared_resource_with_no_lock += 1
- 
-def decrement_without_lock():
-    global shared_resource_with_no_lock
-    for i in range(COUNT):
-        shared_resource_with_no_lock -= 1
- 
-####the Main program
-if __name__ == "__main__":
-    t1 = threading.Thread(target = increment_with_lock)
-    t2 = threading.Thread(target = decrement_with_lock)
-    t3 = threading.Thread(target = increment_without_lock)
-    t4 = threading.Thread(target = decrement_without_lock)
-    t1.start()
-    t2.start()
-    t3.start()
-    t4.start()
-    t1.join()
-    t2.join()
-    t3.join()
-    t4.join()
-    print ("the value of shared variable with lock management is %s"\
-           %shared_resource_with_lock)
-    print ("the value of shared variable with race condition is %s"\
-           %shared_resource_with_no_lock)
-    
+# ______ th..
+#
+# shared_resource_with_lock     _ 0
+# shared_resource_with_no_lock     _ 0
+# COUNT _ 100000
+# shared_resource_lock _ ?.L..
+#
+#
+# ####LOCK MANAGEMENT##
+# ___ increment_with_lock
+#     g.. shared_resource_with_lock
+#     ___ i __ ra.. C..
+#         _l__.a..
+#         ? +_ 1
+#         _l__.r..
+#
+# ___ decrement_with_lock
+#     g.. shared_resource_with_lock
+#     ___ i __ ra.. C..
+#         _l__.a..
+#         ? -_ 1
+#         _l__.r..
+#
+# ####NO LOCK MANAGEMENT ##
+# ___ increment_without_lock
+#     g.. shared_resource_with_no_lock
+#     ___ i __ ra.. C..
+#         ? +_ 1
+#
+# ___ decrement_without_lock
+#     g.. shared_resource_with_no_lock
+#     ___ i __ ra.. C..
+#         ? -_ 1
+#
+# ####the Main program
+# __ _______ __ _______
+#     t1 _ ?.T..(t.. _ i..
+#     t2 _ ?.T..(t.. _ d..
+#     t3 _ ?.T..(t.. _ i..
+#     t4 _ ?.T..(t.. _ d..
+#     _1.s..
+#     _2.s..
+#     _3.s..
+#     _4.s..
+#     _1.r..
+#     _2.r..
+#     _3.r..
+#     _4.r..
+#     print ("the value of shared variable with lock management is @"\
+#            ?
+#     print ("the value of shared variable with race condition is @"\
+#            ?
+#
