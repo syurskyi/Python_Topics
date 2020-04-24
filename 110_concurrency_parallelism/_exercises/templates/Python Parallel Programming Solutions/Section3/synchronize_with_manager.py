bbd3108944d3ba8a529408e6034016e5f8f2bba0
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:12:54 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section3/synchronize_with_manager.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section3/synchronize_with_manager.py
@@ -1,18 +1,18 @@
-#Synchronize processes with managers – Section 3: Process Based Parallelism
-import multiprocessing
-
-def worker(dictionary, key, item):
-    dictionary[key] = item
-
-if __name__ == '__main__':
-    mgr = multiprocessing.Manager()
-    dictionary = mgr.dict()
-    jobs = [ multiprocessing.Process\
-             (target=worker, args=(dictionary, i, i*2))
-             for i in range(10)
-             ]
-    for j in jobs:
-        j.start()
-    for j in jobs:
-        j.join()
-    print ('Results:', dictionary)
+# #Synchronize processes with managers – Section 3: Process Based Parallelism
+# ______ m..
+#
+# ___ worker dictionary key item
+#     d..|k.. _ i..
+#
+# __ _______ __ _______
+#     mgr _ ?.M..
+#     dictionary _ ?.di..
+#     jobs _  ?.P..\
+#              (t.._? a.._ d.. i i*2
+#              ___ i __ ra.. 10
+#              |
+#     ___ j __ ?
+#         ?.s..
+#     ___ j __ ?
+#         ?.r..
+#     print ('Results:' d..
