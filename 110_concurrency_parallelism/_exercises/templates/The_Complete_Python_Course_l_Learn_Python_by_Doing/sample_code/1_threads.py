32c00e9cae44c5e06795e59fdd58d12649696107
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:15:41 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/sample_code/1_threads.py
+++ 110_concurrency_parallelism/_exercises/templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/sample_code/1_threads.py
@@ -1,44 +1,44 @@
-import time
-from threading import Thread
-
-####### SINGLE THREAD
-
-def ask_user():
-	start = time.time()
-	user_input = input('Enter your name: ')
-	greet = f'Hello, {user_input}'
-	print(greet)
-	print('ask_user: ', time.time() - start)
-
-def complex_calculation():
-	print('Started calculating...')
-	start = time.time()
-	[x**2 for x in range(20000000)]
-	print('complex_calculation: ', time.time() - start)
-
-
-# With a single thread, we can do one at a time—e.g.
-start = time.time()
-ask_user()
-complex_calculation()
-print('Single thread total time: ', time.time() - start, '\n\n')
-
-
-####### TWO THREADS
-
-
-# With two threads, we can do them both at once...
-thread = Thread(target=complex_calculation)
-thread2 = Thread(target=ask_user)
-
-start = time.time()
-
-thread.start()
-thread2.start()
-
-thread.join()
-thread2.join()
-
-print('Two thread total time: ', time.time() - start)
-
-# Run this and see what happens!
+# ______ t__
+# ____ ? ______ T..
+#
+# ####### SINGLE THREAD
+#
+# ___ ask_user
+# 	start _ t__.t__
+# 	user_input _ __..('Enter your name: ')
+# 	greet _ _*Hello, |?
+# 	print ?
+# 	print('ask_user: ', t__.t__ - s..
+#
+# ___ complex_calculation
+# 	print('Started calculating...')
+# 	start _ t__.t__
+# 	[x**2 ___ x __ ra..(20000000)]
+# 	print('complex_calculation: ', t__.t__ - s..
+#
+#
+# # With a single thread, we can do one at a t__—e.g.
+# start _ t__.t__
+# a..
+# c..
+# print('Single thread total t__: ', t__.t__ - s.. '\n\n')
+#
+#
+# ####### TWO THREADS
+#
+#
+# # With two threads, we can do them both at once...
+# thread _ T..(t.._c..
+# thread2 _ T..(t.._a..
+#
+# start _ t__.t__
+#
+# ?.s..
+# ?2.s..
+#
+# ?.j..
+# ?2.j..
+#
+# print('Two thread total t__: ', t__.t__ - s..
+#
+# # Run this and see what happens!
