6e8609c8ea6abc8e00848ad229f3032c6f977d00
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:07:00 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section1/hello_Python_WithThreads.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section1/hello_Python_WithThreads.py
@@ -1,42 +1,42 @@
-# #  To use threads you need import Thread using the following code:
-from threading import Thread
-
-# # Also we use the sleep function to make the thread "sleep"
-from time import sleep
-
-# # To create a thread in Python you'll want to make your class work as a thread.
-# # For this, you should subclass your class from the Thread class
-
-
-class CookBook(Thread):
-    def __init__(self):
-        Thread.__init__(self)
-        self.message = "Hello Parallel Python CookBook!!\n"
-
-# # this method thod prints only the message
-    def print_message(self):
-        print(self.message)
-
-# # The run method prints ten times the message
-
-    def run(self):
-        print("Thread Starting\n")
-        x=0
-        while (x < 10):
-            self.print_message()
-            sleep(2)
-            x += 1
-        print("Thread Ended\n")
-
-
-#start the main process
-print("Process Started")
-
-# create an instance of the HelloWorld class
-hello_Python = CookBook()
-
-# print the message...starting the thread
-hello_Python.start()
-
-#end the main process
-print("Process Ended")
+# # #  To use threads you need ______ Thread using the following code:
+# ____ th.. ______ T..
+#
+# # # Also we use the sleep function to make the thread "sleep"
+# ____ ti.. ______ s..
+#
+# # # To create a thread in Python you'll want to make your class work as a thread.
+# # # For this, you should subclass your class from the Thread class
+#
+#
+# c_ CookBook T..
+#     ___ -
+#         T... - ?
+#         message _ "Hello Parallel Python CookBook!!\n"
+#
+# # # this method thod prints only the message
+#     ___ print_message
+#         print ?
+#
+# # # The run method prints ten times the message
+#
+#     ___ run
+#         print("Thread Starting\n")
+#         x_0
+#         w__ |? < 10
+#             ?
+#             s.. 2
+#             ? +_ 1
+#         print("Thread Ended\n")
+#
+#
+# #start the main process
+# print("Process Started")
+#
+# # create an instance of the HelloWorld class
+# hello_Python _ ?
+#
+# # print the message...starting the thread
+# ?.s..
+#
+# #end the main process
+# print("Process Ended")
