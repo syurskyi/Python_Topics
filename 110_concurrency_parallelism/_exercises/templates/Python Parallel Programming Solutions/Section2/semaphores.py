94097f4f27e32161c2ddc0be348fb737ab415edd
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:09:04 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/semaphores.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/semaphores.py
@@ -1,46 +1,46 @@
-###Using a Semaphore to synchronize threads
-
-import threading
-import time
-import random
-
-##The optional argument gives the initial value for the internal counter;
-##it defaults to 1.
-##If the value given is less than 0, ValueError is raised.
-semaphore = threading.Semaphore(0)
-
-def consumer():
-    print ("consumer is waiting.")
-    ##Acquire a semaphore
-    semaphore.a..
-    ##The consumer have access to the shared resource
-    print ("Consumer notify : consumed item number %s " %item)
-
-
-def producer():
-    global item
-    time.sleep(10)
-    ##create a random item
-    item = random.randint(0,1000)
-    print ("producer notify : producted item number %s" %item)
-    
-    ##Release a semaphore, incrementing the internal counter by one.
-    ##When it was zero on entry and another thread is waiting for it
-    ##to become larger than zero again, wake up that thread.
-    semaphore.release()
-
-
-#Main program
-if __name__ == '__main__':
-    for i in range (0,5) :
-        t1 = threading.Thread(target=producer)
-        t2 = threading.Thread(target=consumer)
-        t1.start()
-        t2.start()
-        t1.join()
-        t2.join()
-    print ("program terminated")
-
-        
-
-
+# ###Using a Semaphore to synchronize threads
+#
+# ______ th..
+# ______ ti..
+# ______ ra__
+#
+# ##The optional argument gives the initial value for the internal counter;
+# ##it defaults to 1.
+# ##If the value given is less than 0, ValueError is raised.
+# semaphore _ ?.S.. 0
+#
+# ___ consumer
+#     print ("consumer is waiting.")
+#     ##Acquire a semaphore
+#     ?.a..
+#     ##The consumer have access to the shared resource
+#     print ("Consumer notify : consumed item number @ " i..
+#
+#
+# ___ producer
+#     g.. item
+#     t__.s.. 10
+#     ##create a random item
+#     item _ ra__.r_i.. 0,1000
+#     print ("producer notify : producted item number @" i..
+#
+#     ##Release a semaphore, incrementing the internal counter by one.
+#     ##When it was zero on entry and another thread is waiting for it
+#     ##to become larger than zero again, wake up that thread.
+#     s__.r..
+#
+#
+# #Main program
+# __ _______ __ _______
+#     ___ i __ ra..  0,5
+#         t1 _ ?.T.. t.._p
+#         t2 _ ?.T.. t.._c
+#         _1.s..
+#         _2.s..
+#         _1.r..
+#         _2.r..
+#     print ("program terminated")
+#
+#
+#
+#
