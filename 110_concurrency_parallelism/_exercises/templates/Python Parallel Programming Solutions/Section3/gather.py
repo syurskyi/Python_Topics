428df7b17409545bf2e9981583850087e014d72c
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:10:25 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section3/gather.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section3/gather.py
@@ -1,17 +1,17 @@
-#gather communication – Section 3: Process Based Parallelism
-from mpi4py import MPI
-
-comm = MPI.COMM_WORLD
-size = comm.Get_size()
-rank = comm.Get_rank()
-data = (rank+1)**2
-
-data = comm.gather(data, root=0)
-if rank == 0:
-   print ("rank = %s " %rank +\
-          "...receiving data to other process")
-   for i in range(1,size):
-      data[i] = (i+1)**2
-      value = data[i]
-      print(" process %s receiving %s from process %s"\
-            %(rank , value , i))
+# #gather communication – Section 3: Process Based Parallelism
+# ____ mpi4py ______ MPI
+#
+# comm _ MPI.COMM_WORLD
+# size _ comm.Get_size()
+# rank _ comm.Get_rank()
+# data _ (rank+1)**2
+#
+# data _ comm.gather(data, root_0)
+# __ rank __ 0:
+#    print ("rank = @ " @rank +\
+#           "...receiving data to other process")
+#    ___ i __ ra..(1,size):
+#       data[i] _ (i+1)**2
+#       value _ data[i]
+#       print(" process @ receiving @ from process @"\
+#             @(rank , value , i))
