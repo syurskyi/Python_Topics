1765052f40aee6e294890d26a5b35b69ac848160
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:16:18 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/sample_code/6_queued_threads.py
+++ 110_concurrency_parallelism/_exercises/templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/sample_code/6_queued_threads.py
@@ -1,56 +1,56 @@
-from threading import Thread
-import time
-import random
-import queue
-
-counter = 0
-job_queue = queue.Queue()
-counter_queue = queue.Queue()
-
-
-def increment_manager():
-	global counter
-	while True:
-		increment = counter_queue.get()  # this waits until an item is available and locks the queue
-		time.sleep(random.random())
-		old_counter = counter
-		time.sleep(random.random())
-		counter = old_counter + increment
-		time.sleep(random.random())
-		job_queue.put((f'New counter value {counter}', '------------'))
-		time.sleep(random.random())
-		counter_queue.task_done()  # this unlocks the queue
-
-
-# printer_manager and increment_manager run continuously because of the `daemon` flag.
-Thread(target=increment_manager, daemon=True).start()
-
-
-
-def printer_manager():
-	while True:
-		for line in job_queue.get():
-			time.sleep(random.random())
-			print(line)
-		job_queue.task_done()
-
-# printer_manager and increment_manager run continuously because of the `daemon` flag.
-Thread(target=printer_manager, daemon=True).start()
-
-
-def increment_counter():
-	counter_queue.put(1) 
-	time.sleep(random.random())
-
-
-worker_threads = [Thread(target=increment_counter) for thread in range(10)]
-
-for thread in worker_threads:
-	time.sleep(random.random())
-	thread.start()
-
-for thread in worker_threads:
-	thread.join()  # wait for it to finish
-
-counter_queue.join()  # wait for counter_queue to be empty
-job_queue.join()  # wait for job_queue to be empty
+# ____ ? ______ T..
+# ______ t__
+# ______ ra__
+# ______ q..
+#
+# counter _ 0
+# job_queue _ q__.Q..
+# counter_queue _ q__.Q..
+#
+#
+# ___ increment_manager
+# 	g.. counter
+# 	w__ T..
+# 		increment _ c__.get  # this waits until an item is available and locks the queue
+# 		t__.s.. ra__.ra__
+# 		old_counter _ c..
+# 		t__.s.. ra__.ra__
+# 		counter _ o.. + i..
+# 		t__.s.. ra__.ra__
+# 		j__.p.. _*New counter value |c..', '------------'))
+# 		t__.s.. ra__.ra__
+# 		c__.t_d..  # this unlocks the queue
+#
+#
+# # printer_manager and increment_manager run continuously because of the `daemon` flag.
+# T..(t.._? d..T.. .s..
+#
+#
+#
+# ___ printer_manager
+# 	w__ T..
+# 		___ line __ j__.g..
+# 			t__.s.. ra__.ra__
+# 			print ?
+# 		j_q_.t_d..
+#
+# # printer_manager and increment_manager run continuously because of the `daemon` flag.
+# T.. t.._? d..T.. .s..
+#
+#
+# ___ increment_counter
+# 	c_q_.p.. 1
+# 	t__.s.. ra__.ra__
+#
+#
+# worker_threads _ T.. t.._? ___ thread __ ra.. 10
+#
+# ___ thread __ ?
+# 	t__.s.. ra__.ra__
+# 	?.s..
+#
+# ___ thread __ ?
+# 	?.j..  # wait for it to finish
+#
+# c__.j..  # wait for counter_queue to be empty
+# j__.j..  # wait for job_queue to be empty
