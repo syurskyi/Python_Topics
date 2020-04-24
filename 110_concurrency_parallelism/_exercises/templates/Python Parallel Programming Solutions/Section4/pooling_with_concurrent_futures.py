a70d471f909b17794f4e4f89800bf03f222fd19e
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:14:38 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section4/pooling_with_concurrent_futures.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section4/pooling_with_concurrent_futures.py
@@ -1,43 +1,43 @@
-##Concurrent.Futures Pooling - Section 4 Asynchronous Programming
-
-import concurrent.futures
-import time
-
-number_list = [1,2,3,4,5,6,7,8,9,10]
-
-def evaluate_item(x):
-    #count...just to make an operation
-    result_item = count(x)
-    #print the input item and the result
-    print ("item " + str(x) + " result " + str(result_item))
-
-def count(number) :
-    for i in range(0,10000000):
-        i=i+1
-    return i*number
-
-if __name__ == "__main__":
-    ##Sequential Execution
-    start_time = time.clock()
-    for item in number_list:
-        evaluate_item(item)
-    print ("Sequential execution in " + \
-           str(time.clock() - start_time), "seconds")
-
-    ##Thread pool Execution
-    start_time_1 = time.clock()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5)\
-         as executor:
-        for item in number_list:
-            executor.submit(evaluate_item, item)
-    print ("Thread pool execution in " + \
-           str(time.clock() - start_time_1), "seconds")
-
-    ##Process pool Execution
-    start_time_2 = time.clock()
-    with concurrent.futures.ProcessPoolExecutor(max_workers=5)\
-         as executor:
-        for item in number_list:
-            executor.submit(evaluate_item, item)
-    print ("Process pool execution in " + \
-           str(time.clock() - start_time_2), "seconds")
+# ##Concurrent.Futures Pooling - Section 4 Asynchronous Programming
+#
+# ______ c__.f..
+# ______ ti..
+#
+# number_list _ [1,2,3,4,5,6,7,8,9,10]
+#
+# ___ evaluate_item x
+#     #count...just to make an operation
+#     result_item _ c.. x
+#     #print the input item and the result
+#     print ("item " + st. x + " result " + st. ?
+#
+# ___ count number
+#     ___ i __ ra.. 0, 10000000
+#         ?_?+1
+#     r_ ?*n..
+#
+# __ _______ __ _______
+#     ##Sequential Execution
+#     start_time _ t__.c..
+#     ___ item __ n_l..
+#         e.. ?
+#     print ("Sequential execution in " + \
+#            st. t__.c.. - s_t..|, "seconds")
+#
+#     ##Thread pool Execution
+#     start_time_1 _ t__.c..
+#     w__ c__.f__.TPE.. m.._5 \
+#          __ executor
+#         ___ i.. __ n_l..
+#             e__.s.. e_i.. i..
+#     print ("Thread pool execution in " + \
+#            st. t__.c.. - _1| "seconds")
+#
+#     ##Process pool Execution
+#     start_time_2 _ t__.c..
+#     w__ c__.f__.PPE.. m.._5 \
+#          __ executor
+#         ___ item __ n_l..
+#             e__.s.. e_i.. i..
+#     print ("Process pool execution in " + \
+#            st. t__.c.. - _2| "seconds")
