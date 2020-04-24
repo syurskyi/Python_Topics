c69d270f52f8413312066879b42b030d0a35894c
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:06:49 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section1/calling_Process.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section1/calling_Process.py
@@ -1,13 +1,10 @@
-##The following modules must be imported
-import os
-import sys
-##this is the code to execute
-program = "python"
-print("Process calling")
-arguments = ["called_Process.py"]
-
-##we call the called_Process.py script
-os.execvp(program, (program,) + tuple(arguments))
-
-
-
+# ##The following modules must be imported
+# ______ __
+# ______ ___
+# ##this is the code to execute
+# program _ "python"
+# print("Process calling")
+# arguments _ ["called_Process.py"]
+#
+# ##we call the called_Process.py script
+# __.e_c.. p... |p.. + tu.. a..
