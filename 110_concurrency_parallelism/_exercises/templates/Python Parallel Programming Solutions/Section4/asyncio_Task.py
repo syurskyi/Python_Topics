793cbc80cfac28ef3d16be1bb39caa779e32821b
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:14:25 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section4/asyncio_Task.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section4/asyncio_Task.py
@@ -1,39 +1,39 @@
-"""
-Asyncio using Asyncio.Task to execute three math function in parallel
-"""
-import asyncio
-
-@asyncio.coroutine
-def factorial(number):
-    f = 1
-    for i in range(2, number+1):
-        print("Asyncio.Task: Compute factorial(%s)" % (i))
-        yield from asyncio.sleep(1)
-        f *= i
-    print("Asyncio.Task - factorial(%s) = %s" % (number, f))
-
-@asyncio.coroutine
-def fibonacci(number):
-    a, b = 0, 1
-    for i in range(number):
-        print("Asyncio.Task: Compute fibonacci (%s)" % (i))
-        yield from asyncio.sleep(1)
-        a, b = b, a + b
-    print("Asyncio.Task - fibonacci(%s) = %s" % (number, a))
-
-@asyncio.coroutine
-def binomialCoeff(n, k):
-    result = 1
-    for i in range(1, k+1):
-        result = result * (n-i+1) / i
-        print("Asyncio.Task: Compute binomialCoeff (%s)" % (i))
-        yield from asyncio.sleep(1)
-    print("Asyncio.Task - binomialCoeff(%s , %s) = %s" % (n,k,result))
-
-if __name__ == "__main__":
-    tasks = [asyncio.Task(factorial(10)),
-             asyncio.Task(fibonacci(10)),
-             asyncio.Task(binomialCoeff(20,10))]
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait(tasks))
-    loop.close()
+# """
+# Asyncio using Asyncio.Task to execute three math function in parallel
+# """
+# ______ a..
+#
+# ??.?
+# ___ factorial number
+#     f _ 1
+#     ___ i __ ra.. 2 ?+1
+#         print("Asyncio.Task: Compute factorial(@)" ?
+#         ? ? ?.s.. 1
+#         f *_ ?
+#     print("Asyncio.Task - factorial(@) = @"   n.. f
+#
+# ??.?
+# ___ fibonacci number
+#     a, b _ 0, 1
+#     ___ i __ ra. ?
+#         print("Asyncio.Task: Compute fibonacci (@)" ?
+#         ? ? ?.s.. 1
+#         a, b _ b, a + b
+#     print("Asyncio.Task - fibonacci(@) = @" n.. a
+#
+# ??.?
+# ___ binomialCoeff n k
+#     result _ 1
+#     ___ i __ ra.. 1 k+1
+#         r.. _ R.. * (n-?+1) / ?
+#         print("Asyncio.Task: Compute binomialCoeff (@)" ?
+#         ? ? ?.s.. 1
+#     print("Asyncio.Task - binomialCoeff(@ , @) = @" ? ? r..
+#
+# __ _______ __ _______
+#     tasks _ |?.T.. fa.. 10
+#              ?.T.. fi.. 10
+#              ?.T.. bi.. 20 10
+#     loop _ ?.g_e_l..
+#     ?.r_u_c.. ?.w.. t..
+#     ?.c..
