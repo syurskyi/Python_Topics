67986df53236d0c2e1ac707afdd74773a8b11081
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:15:57 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/sample_code/3_processes.py
+++ 110_concurrency_parallelism/_exercises/templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/sample_code/3_processes.py
@@ -1,43 +1,43 @@
-from multiprocessing import Process
-import time
-
-####### SINGLE PROCESS
-
-def ask_user():
-	start = time.time()
-	user_input = input('Enter your name: ')
-	greet = f'Hello, {user_input}'
-	print(greet)
-	print('ask_user: ', time.time() - start)
-
-def complex_calculation():
-	print('Started calculating...')
-	start = time.time()
-	[x**2 for x in range(20000000)]
-	print('complex_calculation: ', time.time() - start)
-
-
-# With a single thread, we can do one at a time—e.g.
-start = time.time()
-ask_user()
-complex_calculation()
-print('Single thread total time: ', time.time() - start, '\n\n')
-
-
-####### TWO PROCESSES
-
-
-# With two processes, we can do them both at once...
-process = Process(target=complex_calculation)
-process.start()
-
-start = time.time()
-
-ask_user()
-
-process.join()  # this waits for the process to finish
-
-print('Two process total time: ', time.time() - start)
-
-# Run this and see what happens!
-
+# ____ ? ______ P..
+# ______ t__
+#
+# ####### SINGLE PROCESS
+#
+# ___ ask_user
+# 	start _ t__.t__
+# 	user_input _ __..('Enter your name: ')
+# 	greet _ _*Hello, |?
+# 	print ?
+# 	print('ask_user: ', t__.t__ - s..
+#
+# ___ complex_calculation
+# 	print('Started calculating...')
+# 	start _ t__.t__
+# 	x**2 ___ x __ ra.. 20000000
+# 	print('complex_calculation: ', t__.t__ - s..
+#
+#
+# # With a single thread, we can do one at a t__—e.g.
+# start _ t__.t__
+# a..
+# c..
+# print('Single thread total t__: ', t__.t__ - s.. '\n\n')
+#
+#
+# ####### TWO PROCESSES
+#
+#
+# # With two processes, we can do them both at once...
+# process _ P.. t.._c..
+# ?.s..
+#
+# start _ t__.t__
+#
+# a..
+#
+# ?.j..  # this waits for the process to finish
+#
+# print('Two process total t__: ', t__.t__ - s..
+#
+# # Run this and see what happens!
+#
