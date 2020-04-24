c5e1baab9106d7c9003bb9d51688d04ea5c737bb
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:10:02 -0700

    Committed Python Parallel Programming Solutions templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section3/broadcast.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section3/broadcast.py
@@ -1,14 +1,14 @@
-#broadcast communication Section 3: Process Based Parallelism
-from mpi4py import MPI
-
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-
-if rank == 0:
-   variable_to_share = 100
-
-else:
-   variable_to_share = None
-
-variable_to_share = comm.bcast(variable_to_share, root=0)
-print("process = %d" %rank + " variable shared  = %d " %variable_to_share)
+# #broadcast communication Section 3: Process Based Parallelism
+# ____ mpi4py ______ MPI
+#
+# comm _ MPI.COMM_WORLD
+# rank _ comm.Get_rank()
+#
+# __ rank __ 0
+#    variable_to_share _ 100
+#
+# ____
+#    variable_to_share _ N..
+#
+# variable_to_share _ comm.bcast(variable_to_share, root_0)
+# print("process = @d" @rank + " variable shared  = @d " @variable_to_share)
