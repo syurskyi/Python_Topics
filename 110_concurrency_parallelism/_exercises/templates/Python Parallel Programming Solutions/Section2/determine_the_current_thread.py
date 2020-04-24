398b64341822c3b809816024e02d5987a30070ef
Grey <sergejyurskyj@yahoo.com>
2020-04-24 10:07:34 -0700

    Committed Mastering Concurrency in Python templates

--- 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/determine_the_current_thread.py
+++ 110_concurrency_parallelism/_exercises/templates/Python Parallel Programming Solutions/Section2/determine_the_current_thread.py
@@ -1,42 +1,42 @@
-import threading
-import time
-
-def first_function():
-    print (threading.currentThread().getName()+\
-           str(' is Starting \n'))
-    time.sleep(2)
-    print (threading.currentThread().getName()+\
-           str( ' is Exiting \n'))
-    return
-
-def second_function():
-    print (threading.currentThread().getName()+\
-           str(' is Starting \n'))
-    time.sleep(2)
-    print (threading.currentThread().getName()+\
-           str( ' is Exiting \n'))
-    return
-
-def third_function():
-    print (threading.currentThread().getName()+\
-           str(' is Starting \n'))
-    time.sleep(2)
-    print (threading.currentThread().getName()+\
-           str( ' is Exiting \n'))
-    return
-
-
-if __name__ == "__main__":
-    t1 = threading.Thread\
-         (name='first_function', target=first_function)
-    t2 = threading.Thread\
-         (name='second_function', target=second_function)
-    t3 = threading.Thread\
-         (name='third_function',target=third_function)
-
-    t1.start()
-    t2.start()
-    t3.start()
-    t1.join()
-    t2.join()
-    t3.join()
+# ______ th..
+# ______ ti..
+#
+# ___ first_function
+#     print ?.cT.. .gN..+\
+#            st.(' is Starting \n'
+#     t__.s.. 2
+#     print ?.cT.. .gN..+\
+#            st.( ' is Exiting \n'
+#     r_
+#
+# ___ second_function(
+#     print ?.cT.. .gN..+\
+#            st.(' is Starting \n'
+#     t__.s.. 2
+#     print ?.cT.. .gN..+\
+#            st.( ' is Exiting \n'
+#     r_
+#
+# ___ third_function
+#     print ?.cT.. .gN..+\
+#            st.(' is Starting \n'
+#     t__.s.. 2
+#     print (?.cT.. .gN..+\
+#            st.( ' is Exiting \n'
+#     r_
+#
+#
+# __ _______ __ _______
+#     t1 _ ?.T..\
+#          (name_'first_function', t.._?
+#     t2 _ ?.T..\
+#          (name_'second_function', t.._?
+#     t3 _ ?.T..\
+#          (name_'third_function',t.._?
+#
+#     t1.s..
+#     t2.s..
+#     t3.s..
+#     t1.r..
+#     t2.r..
+#     t3.r..
