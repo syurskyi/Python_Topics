52ba09015d497e295a842ce40746ceef46ed6e5f
Grey <sergejyurskyj@yahoo.com>
2020-04-25 14:04:20 -0700

    Committed Telnet templates

--- 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/Telnet.py
+++ 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/Telnet.py
@@ -1,22 +1,22 @@
-______ t_l_, getp__s
-
-# user = getpass.getuser()
-user _ "ric"
-pw _ getp__s.getp__s
-host _ "172.30.42.127"
-
-t _ t_l_.Telnet(host)
-
-___
-    t.read_until("login: ")
-    t.w..(user + '\n')
-    t.read_until("Password: ")
-    t.w..(pw + '\n')
-
-    t.read_until("~ $ ")
-    t.w..("ls\n")
-    print(t.read_until("~ $ "))
-______ E.. __ e:
-    print(e)
-f..
-    t.c..
+# ______ t_l_, g_p..
+#
+# # user = getpass.getuser()
+# user _ "ric"
+# pw _ g__.g..
+# host _ "172.30.42.127"
+#
+# t _ t_l_.T.. ?
+#
+# ___
+#     ?.r_u.. "login: "
+#     ?.w.. u.. + '\n'
+#     ?.r_u.. "Password: "
+#     ?.w.. p.. + '\n'
+#
+#     ?.r_u.. "~ $ "
+#     ?.w.. "ls\n"
+#     print(?.r_u.. "~ $ "
+# ______ E.. __ e
+#     print ?
+# f..
+#     ?.c..
