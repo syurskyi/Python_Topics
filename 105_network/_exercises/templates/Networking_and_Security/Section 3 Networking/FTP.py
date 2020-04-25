fba78ff6e3436a31879100333b813d17936ca8de
Grey <sergejyurskyj@yahoo.com>
2020-04-25 13:43:07 -0700

    Committed FTP templates

--- 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/FTP.py
+++ 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/FTP.py
@@ -1,16 +1,16 @@
-______ f_l.
-
-f _ ?.FTP("172.30.42.127")
-
-___
-    f.l..("ric", "P4ssw0rd!")
-    print(f.g_w_
-    f.delete("myfile")
-    print(f.dir)
-    f.set_p__v(1)
-    f.storbinary("STOR myfile", o..("myfile", __))
-    print(f.dir)
-______ E.. __ e:
-    print("Exception: ", e)
-f..
-    f.c..
+# ______ f_l.
+#
+# f _ ?.F.. "172.30.42.127"
+#
+# ___
+#     ?.l.. "ric", "P4ssw0rd!"
+#     print ?.g_w_
+#     ?.d.. "myfile"
+#     print(?.d..
+#     ?.s_p.. 1
+#     ?.s_b.. "STOR myfile", o.. "myfile" __
+#     print(?.d..
+# ______ E.. __ e
+#     print("Exception: " ?
+# f..
+#     ?.c..
