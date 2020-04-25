df820e952501eb0097e466fe34c055b5c4985cf6
Grey <sergejyurskyj@yahoo.com>
2020-04-25 14:11:27 -0700

    Committed Section 4 Network Programming using Python templates

--- 105_network/_exercises/templates/TCP_IP Socket Programming/Section 4 Network Programming using Python/server.py
+++ 105_network/_exercises/templates/TCP_IP Socket Programming/Section 4 Network Programming using Python/server.py
@@ -1,63 +1,63 @@
-______ so..
-______ ___
-
-
-# Create a Socket ( connect two computers)
-___ create_socket
-    ___
-        g.. host
-        g.. port
-        g.. s
-        host _ ""
-        port _ 9999
-        s _ ?.?
-
-    ______ ?.error __ msg:
-        print("Socket creation error: " + st.(msg))
-
-
-# Binding the socket and listening for connections
-___ b.._socket
-    ___
-        g.. host
-        g.. port
-        g.. s
-        print("Binding the Port: " + st.(port))
-
-        s.b..((host, port))
-        s.l..(5)
-
-    ______ ?.error __ msg:
-        print("Socket Binding error" + st.(msg) + "\n" + "Retrying...")
-        b.._socket
-
-
-# Establish connection with a client (socket must be listening)
-
-___ socket_a..:
-    conn, address _ s.a..
-    print("Connection has been established! |" + " IP " + address[0] + " | Port" + st.(address[1]))
-    s.._commands(conn)
-    conn.c..
-
-# Send commands to client/victim or a friend
-___ s.._commands(conn
-    w__ T..:
-        cmd _ in__
-        __ cmd __ 'quit':
-            conn.c..
-            s.c..
-            ___.e..
-        __ le.(st..en..(cmd)) > 0:
-            conn.s..(st..en..(cmd))
-            client_response _ st.(conn.r..(1024),"utf-8")
-            print(client_response, end_"")
-
-
-___ main
-    create_socket
-    b.._socket
-    socket_a..
-
-
-main
+# ______ so..
+# ______ ___
+#
+#
+# # Create a Socket ( connect two computers)
+# ___ create_socket
+#     ___
+#         g.. h..
+#         g.. p..
+#         g.. s
+#         h.. _ ""
+#         p.. _ 9999
+#         s _ ?.?
+#
+#     ______ ?.e.. __ msg
+#         print("Socket creation error: " + st. ?
+#
+#
+# # Binding the socket and listening for connections
+# ___ b.._socket
+#     ___
+#         g.. h..
+#         g.. p..
+#         g.. s
+#         print("Binding the Port: " + st. ?
+#
+#         s.b.. ? ?
+#         s.l.. 5
+#
+#     ______ ?.e.. __ msg
+#         print("Socket Binding error" + st. ? + "\n" + "Retrying...")
+#         b.._s..
+#
+#
+# # Establish connection with a client (socket must be listening)
+#
+# ___ socket_a..
+#     conn, address _ s.a..
+#     print("Connection has been established! |" + " IP " + a.. 0 + " | Port" + st. a.. 1
+#     s.._c.. ?
+#     ?.c..
+#
+# # Send commands to client/victim or a friend
+# ___ s.._commands conn
+#     w__ T..
+#         cmd _ in__
+#         __ ? __ 'quit'
+#             ?.c..
+#             s.c..
+#             ___.e..
+#         __ le. st..en.. ? > 0
+#             ?.s.. st..en.. ?
+#             client_response _ st. ?.r.. 1024 "utf-8"
+#             print ? e.._""
+#
+#
+# ___ main
+#     ?
+#     b.._s..
+#     s.._a..
+#
+#
+# ?
