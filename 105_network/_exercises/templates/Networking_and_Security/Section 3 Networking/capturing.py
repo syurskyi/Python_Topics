339fe79bd40a9db2dd60c3db4fac10dec7d377d1
Grey <sergejyurskyj@yahoo.com>
2020-04-25 13:40:46 -0700

    Committed capturing templates

--- 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/capturing.py
+++ 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/capturing.py
@@ -1,13 +1,13 @@
-______ pcapy
-
-devs _ pcapy.findalldevs
-print(devs)
-
-#  device, # of byte to capture per packet, promiscuous mode, timeout (ms)
-cap _ pcapy.open_live("eth0", 65536 , 1 , 0)
-
-count _ 1
-w__ count:
-    (header, payload) _ cap.n..
-    print(count)
-    count _ count + 1
+# ______ pc..
+#
+# devs _ ?.f..
+# print ?
+#
+# #  device, # of byte to capture per packet, promiscuous mode, timeout (ms)
+# cap _ ?.o_l.. "eth0", 65536 , 1 , 0
+#
+# count _ 1
+# w__ ?
+#     h.. p.. _ ?.n..
+#     print ?
+#     ? _ ? + 1
