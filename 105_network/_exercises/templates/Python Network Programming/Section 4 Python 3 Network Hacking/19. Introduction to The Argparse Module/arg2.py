29b7fdb7a24d30f8d569518e7e610bfc81171a8d
Grey <sergejyurskyj@yahoo.com>
2020-04-25 16:28:48 -0700

    Committed 19. Introduction to The Argparse Module templates

--- 105_network/_exercises/templates/Python Network Programming/Section 4 Python 3 Network Hacking/19. Introduction to The Argparse Module/arg2.py
+++ 105_network/_exercises/templates/Python Network Programming/Section 4 Python 3 Network Hacking/19. Introduction to The Argparse Module/arg2.py
@@ -1,17 +1,17 @@
-______ a_p_
-
-#define ArgumentParser
-parser _ a_p_.A_P..(d.._'Example with long argument names')
-
-#define long argument names
-?.a_a..('--noarg', action_"store_true", default_False)
-?.a_a..('--string', action_"store")
-?.a_a..('--integer', action_"store", type_int)
-
-#parse arguments
-args _ ?.parse_args
-
-#print arguments to screen
-print ("noarg argument   : " + st.(args.noarg))
-print ("string argument  : " + st.(args.string))
-print ("integer argument : " + st.(args.integer))
+# ______ a_p_
+#
+# #define ArgumentParser
+# parser _ a_p_.A_P..(d.._'Example with long argument names')
+#
+# #define long argument names
+# ?.a_a.. --noarg a.._ store_true d.._F..
+# ?.a_a.. --string a.._ store
+# ?.a_a.. --integer a.._ store ty.._in.
+#
+# #parse arguments
+# args _ ?.p_a..
+#
+# #print arguments to screen
+# print ("noarg argument   : " + st. ?.n..
+# print ("string argument  : " + st. ?.s..
+# print ("integer argument : " + st. ?.i..
