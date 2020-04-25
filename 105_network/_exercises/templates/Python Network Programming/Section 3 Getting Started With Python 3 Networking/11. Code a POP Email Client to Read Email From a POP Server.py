b264fe2cd9f0a35e4f8cc6101947d0213a8efee0
Grey <sergejyurskyj@yahoo.com>
2020-04-25 15:58:57 -0700

    Committed 11. Code a POP Email Client to Read Email From a POP Server templates

--- 105_network/_exercises/templates/Python Network Programming/Section 3 Getting Started With Python 3 Networking/11. Code a POP Email Client to Read Email From a POP Server.py
+++ 105_network/_exercises/templates/Python Network Programming/Section 3 Getting Started With Python 3 Networking/11. Code a POP Email Client to Read Email From a POP Server.py
@@ -1,16 +1,16 @@
-______ p_l_
-____ email.message ______ EmailMessage
-
-server _ "127.0.0.1"
-user _ "phil"
-p__swd _ "pythoncode"
-
-server _ p_l_.POP3(server)
-server.user(user)
-server.p__s_(p__swd)
-
-msgNum _ le.(server.list[1])
-
-___ i __ ra.. msgNum
-    ___ msg __ server.retr(i+1)[1]:
-        print (msg.d..)
+# ______ p_l_
+# ____ e_.m.. ______ EM..
+#
+# server _ "127.0.0.1"
+# user _ "phil"
+# passwd _ "pythoncode"
+#
+# server _ p_l_.POP3 s..
+# ?.u.. ?
+# ?.p.. p..
+#
+# msgNum _ le. ?.li.. 1
+#
+# ___ i __ ra.. ?
+#     ___ msg __ s__.r.. ?+1 1
+#         print ?.d..
