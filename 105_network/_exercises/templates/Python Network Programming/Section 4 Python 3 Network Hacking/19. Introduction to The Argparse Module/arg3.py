db086ccd21fe3b8e73de6a2ac50c53d2b41fa246
Grey <sergejyurskyj@yahoo.com>
2020-04-25 16:31:41 -0700

    Committed 19. Introduction to The Argparse Module templates

--- 105_network/_exercises/templates/Python Network Programming/Section 4 Python 3 Network Hacking/19. Introduction to The Argparse Module/arg3.py
+++ 105_network/_exercises/templates/Python Network Programming/Section 4 Python 3 Network Hacking/19. Introduction to The Argparse Module/arg3.py
@@ -1,33 +1,33 @@
-______ a_p_
-
-#define ArgumentParser
-parser _ a_p_.A_P..
-
-#define argument names
-?.a_a..('-s', action_'store', help_'Store a simple value')
-
-?.a_a..('-c', action_'store_const', const_'value-to-store',
-                    help_'Store a constant value')
-
-?.a_a..('-a', action_'append', default_[],
-                    help_'Add repeated values to a list',)
-
-?.a_a..('-A', action_'append_const', const_'value-1-to-append',
-                    default_[], dest_'const_collection',
-                    help_'Add different values to list')
-
-?.a_a..('-B', action_'append_const', const_'value-2-to-append',
-                    dest_'const_collection',
-                    help_'Add different values to list')
-
-?.a_a..('--version', action_'version', version_'%(prog)s 1.0')
-
-#parse arguments
-args _ ?.parse_args
-
-#print arguments to screen
-print ('simple_value     : ' + st.(args.s))
-print ('constant_value   : ' + st.(args.c))
-print ('collection       : ' + st.(args.a))
-print ('const_collection : ' + st.(args.const_collection))
-print ('version          : ' + st.(args.version))
+# ______ a_p_
+#
+# #define ArgumentParser
+# parser _ a_p_.A_P..
+#
+# #define argument names
+# ?.a_a.. '-s' a.._'store', h.._'Store a simple value'
+#
+# ?.a_a.. '-c' a.._'store_const', c.._'value-to-store'
+#                     h.._'Store a constant value'
+#
+# ?.a_a.. '-a' a.._'append', d.._ # list
+#                     h.._'Add repeated values to a list'
+#
+# ?.a_a.. '-A' a.._'append_const', c.._'value-1-to-append'
+#                     d.._ || d.._'const_collection',
+#                     h.._'Add different values to list'
+#
+# ?.a_a.. '-B' a.._'append_const', c.._'value-2-to-append'
+#                     d.._'const_collection',
+#                     h.._'Add different values to list'
+#
+# ?.a_a.. '--version', a.._'version', v.._'%(prog)s 1.0'
+#
+# #parse arguments
+# args _ ?.p_a..
+#
+# #print arguments to screen
+# print ('simple_value     : ' + st. ?.s
+# print ('constant_value   : ' + st. ?.c
+# print ('collection       : ' + st. ?.a
+# print ('const_collection : ' + st. ?.c_c..
+# print ('version          : ' + st. ?.v..
