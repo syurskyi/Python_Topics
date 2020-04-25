a2bf14a72da352ddfafe102443b37d4b6f545c10
Grey <sergejyurskyj@yahoo.com>
2020-04-25 10:36:59 -0700

    Committed Build_5_Network_Applications templates

--- 105_network/_exercises/templates/Build_5_Network_Applications/Section 19 [APP] Network Application #1/ip_addr_valid.py
+++ 105_network/_exercises/templates/Build_5_Network_Applications/Section 19 [APP] Network Application #1/ip_addr_valid.py
@@ -1,15 +1,15 @@
-import sys
-
-#Checking octets
-def ip_addr_valid(list):
-
-    for ip in list:
-        ip = ip.rstrip("\n")
-        octet_list = ip.split('.')
-        
-        if (len(octet_list) == 4) and (1 <= int(octet_list[0]) <= 223) and (int(octet_list[0]) != 127) and (int(octet_list[0]) != 169 or int(octet_list[1]) != 254) and (0 <= int(octet_list[1]) <= 255 and 0 <= int(octet_list[2]) <= 255 and 0 <= int(octet_list[3]) <= 255):
-            continue
-             
-        else:
-            print('\n* There was an invalid IP address in the file: {} :(\n'.format(ip))
-            sys.exit()
+# ______ ___
+#
+# #Checking octets
+# ___ ip_addr_valid list
+#
+#     ___ ip __ list
+#         ip _ ?.rs.. "\n"
+#         octet_list _ ip.sp.. '.'
+#
+#         __ (le. ? __ 4) an. (1 <_ in. ? 0 <_ 223 an. in. ? 0 !_ 127 an. in. ? 0 !_ 169 o. in. ? 1 !_ 254 an. 0 <_ in. ? 1 <_ 255 an. 0 <_ in. ? 2 <_ 255 an. 0 <_ in. ? 3 <_ 255
+#             c..
+#
+#         ____
+#             print('\n* There was an invalid IP address in the file: # :(\n'.f.. ?
+#             ___.e..
