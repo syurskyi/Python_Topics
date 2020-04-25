7f9663160e10014fb91f9725fbb6017d21182a73
Grey <sergejyurskyj@yahoo.com>
2020-04-25 15:56:49 -0700

    Committed 10. Use the Urllib Library to Access URL Resources and Download Files templates

--- 105_network/_exercises/templates/Python Network Programming/Section 3 Getting Started With Python 3 Networking/10. Use the Urllib Library to Access URL Resources and Download Files.py
+++ 105_network/_exercises/templates/Python Network Programming/Section 3 Getting Started With Python 3 Networking/10. Use the Urllib Library to Access URL Resources and Download Files.py
@@ -1,18 +1,18 @@
-______ u_l_.request
+______ u_l_.r__
 
 headers _   # dict
-headers['User-Agent'] _ "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0"
+headers 'User-Agent' _ "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0"
 
-req _ u_l_.request.R..("https://www.python.org", headers_headers)
-html _ u_l_.request.u_o..(req).r..
-print(html.d..)
+req _ u_l_.r__.R.. "https://www.python.org" ?_?
+html _ u_l_.r__.u_o.. ? .r..
+print ? .d..
 
 print("\n\nDownloading Wordpress...")
-rsp _ u_l_.request.u_o..("https://wordpress.org/latest.zip")
-data _ rsp.r..
+rsp _ u_l_.r__.u_o.. "https://wordpress.org/latest.zip"
+data _ ?.r..
 
 filename _ "latest.zip"
-file_ _ o..(filename, __)
-file_.w..(data)
-file_.c..
+file_ _ o.. ? __
+?.w.. d..
+?.c..
 print("Download Complete")
