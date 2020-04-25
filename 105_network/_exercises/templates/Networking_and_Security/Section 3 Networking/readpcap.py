edb6212477a62076c9071ecfe0fb42fbcd6b1ea9
Grey <sergejyurskyj@yahoo.com>
2020-04-25 13:59:46 -0700

    Committed readpcap templates

--- 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/readpcap.py
+++ 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/readpcap.py
@@ -1,22 +1,22 @@
-______ pcapy
-____ struct ______ *
-
-pcap_file _ pcapy.open_offline("file.pcap")
-count _ 1
-
-w__ count:
-    print("Packet #: ", count)
-    count _ count + 1
-    (header,payload) _ pcap_file.n..
-    l2hdr _ payload[:14]
-    l2data _ unpack("!6s6sH", l2hdr)
-    srcmac _ "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (or.(l2hdr[0]), or.(l2hdr[1]), or.(l2hdr[2]), or.(l2hdr[3]), or.(l2hdr[4]), or.(l2hdr[5]))
-    dstmac _ "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (or.(l2hdr[6]), or.(l2hdr[7]),   or.(l2hdr[8]), or.(l2hdr[9]), or.(l2hdr[10]), or.(l2hdr[11]))
-    print("Source MAC: ", srcmac, " Destination MAC: ", dstmac)
-
-# get IP header, which is 20 bytes long
-# then unpack it into what it is
-    ipheader _ unpack('!BBHHHBBH4s4s' , payload[14:34])
-    timetolive _ ipheader[5]
-    protocol _ ipheader[6]
-    print("Protocol ", st.(protocol), " Time To Live: ", st.(timetolive))
+# ______ pc..
+# ____ struct ______ _
+#
+# pcap_file _ pcapy.open_offline("file.pcap")
+# count _ 1
+#
+# w__ ?
+#     print("Packet #: " ?
+#     ? _ ? + 1
+#     header payload _ p__.n..
+#     l2hdr _ p.. ;14
+#     l2data _ un.. "!6s6sH" ?
+#     srcmac _ "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x"  or. _h.. 0 or. _h..1 or. _h.. 2 or. _h.. 3 or. _h.. 4 or. _h.. 5
+#     dstmac _ "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x"  or. _h.. 6 or. _h..7 or. _h.. 8 or. _h.. 9 or. _h.. 10 or. _h.. 11
+#     print("Source MAC: " ? " Destination MAC: " ?
+#
+# # get IP header, which is 20 bytes long
+# # then unpack it into what it is
+#     ipheader _ u.. '!BBHHHBBH4s4s'  p.. 14;34
+#     timetolive _ i.. 5
+#     protocol _ i.. 6
+#     print("Protocol ", st. p.. " Time To Live: ", st. t..
