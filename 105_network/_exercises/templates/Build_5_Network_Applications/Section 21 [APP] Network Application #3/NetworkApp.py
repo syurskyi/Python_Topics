58c66895b085cbfac093df486ac8c5df032c48d9
Grey <sergejyurskyj@yahoo.com>
2020-04-25 13:05:01 -0700

    Committed Section 21 [APP] Network Application #3 templates

--- 105_network/_exercises/templates/Build_5_Network_Applications/Section 21 [APP] Network Application #3/NetworkApp.py
+++ 105_network/_exercises/templates/Build_5_Network_Applications/Section 21 [APP] Network Application #3/NetworkApp.py
@@ -1,35 +1,35 @@
-#Importing the necessary modules
-______ ___
-______ t__
-
-____ ip_file_valid ______ ip_file_valid
-____ ip_addr_valid ______ ip_addr_valid
-____ ip_reach ______ ip_reach
-____ ssh_connection ______ ssh_connection
-____ create_threads ______ create_threads
-
-#Saving the list of IP addresses in ip.txt to a variable
-ip_list _ ip_file_valid
-
-#Verifying the validity of each IP address in the list
-___
-    ip_addr_valid(ip_list)
-    
-______ K..
-    print("\n\n* Program aborted by user. Exiting...\n")
-    ___.e..
-
-#Verifying the reachability of each IP address in the list
-___
-    ip_reach(ip_list)
-    
-______ K..
-    print("\n\n* Program aborted by user. Exiting...\n")
-    ___.e..
-
-#Calling threads creation function for one or multiple SSH connections
-w__ T..:
-    create_threads(ip_list, ssh_connection)
-    t__.s..(10)
-
-#End of program
+# #Importing the necessary modules
+# ______ ___
+# ______ t__
+#
+# ____ i_f_v_ ______ i_f_v_
+# ____ i_a_v_ ______ i_a_v_
+# ____ i_r_ ______ i_r_
+# ____ s_c.. ______ s_c..
+# ____ c_t.. ______ c_t..
+#
+# #Saving the list of IP addresses in ip.txt to a variable
+# ip_list _ i_f_v_
+#
+# #Verifying the validity of each IP address in the list
+# ___
+#     i_a_v_ i_l..
+#
+# ______ K..
+#     print("\n\n* Program aborted by user. Exiting...\n")
+#     ___.e..
+#
+# #Verifying the reachability of each IP address in the list
+# ___
+#     i_r_ i_l..
+#
+# ______ K..
+#     print("\n\n* Program aborted by user. Exiting...\n")
+#     ___.e..
+#
+# #Calling threads creation function for one or multiple SSH connections
+# w__ T..
+#     c_t.. i_l.. s_c..
+#     t__.s.. 10
+#
+# #End of program
