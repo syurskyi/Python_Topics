26b4d1cf05079b2bd2cb121df96238ca8122a7b0
Grey <sergejyurskyj@yahoo.com>
2020-04-25 13:51:36 -0700

    Committed POPIMAP templates

--- 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/POPIMAP.py
+++ 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/POPIMAP.py
@@ -1,29 +1,29 @@
-______ p_l_, imaplib, getp__s
-
-p _ p_l_.POP3("172.30.42.127", 110)
-# p = poplib.POP3_SSL("172.30.42.126", 995)
-
-print(p.g_w_
-p.user("ric")
-p.p__s_("P4ssw0rd!")
-
-print(p.list)
-p.quit
-
-i _ imaplib.IMAP4("172.30.42.127", 143)
-# i.login(getpass.getuser(), getpass.getpass_())
-i.l..("ric", "P4ssw0rd!")
-i.select
-t, l _ i.list
-print("Response code: ", t)
-print(l)
-
-t, ids _ i.s..(None, "ALL")
-print("Response code: ", t)
-print(ids)
-t, msg _ i.fetch('5', "(UID BODY[TEXT])")
-#  store messages on the server
-#  i.store()
-print(msg)
-i.c..
-i.logout
+# ______ p_l_ im.. g_p..
+#
+# p _ p_l_.POP3("172.30.42.127", 110)
+# # p = poplib.POP3_SSL("172.30.42.126", 995)
+#
+# print p.g_w_
+# ?.u.. "ric"
+# ?.p__s_ "P4ssw0rd!"
+#
+# print ?.l..
+# ?.q..
+#
+# i _ i_l_._4 "172.30.42.127", 143
+# # i.login(getpass.getuser(), getpass.getpass_())
+# ?.l..("ric", "P4ssw0rd!")
+# ?.s..
+# t, l _ ?.l..
+# print("Response code: " ?
+# print ?
+#
+# t, ids _ i.s.. N.. "ALL"
+# print("Response code: " ?
+# print ?
+# t, msg _ ?.f.. '5', "(UID BODY[TEXT])"
+# #  store messages on the server
+# #  i.store()
+# print ?
+# ?.c..
+# ?.l..
