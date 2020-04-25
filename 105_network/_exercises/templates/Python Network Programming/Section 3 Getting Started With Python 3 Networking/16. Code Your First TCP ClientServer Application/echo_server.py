9e78ee5bbffc0817b75185e9a5684b1d9ba35fb9
Grey <sergejyurskyj@yahoo.com>
2020-04-25 14:26:18 -0700

    Committed 16. Code Your First TCP ClientServer Application templates

--- 105_network/_exercises/templates/Python Network Programming/Section 3 Getting Started With Python 3 Networking/16. Code Your First TCP ClientServer Application/echo_server.py
+++ 105_network/_exercises/templates/Python Network Programming/Section 3 Getting Started With Python 3 Networking/16. Code Your First TCP ClientServer Application/echo_server.py
@@ -1,16 +1,16 @@
-______ so..
-
-HOST _ '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT _ 8081        # Port to listen on (non-privileged ports are > 1023)
-
-w__ ?.?(?.A.. ?.S.. __ s:
-    s.b..((HOST, PORT))
-    s.l..
-    conn, addr _ s.a..
-    w__ conn:
-        print('Connected by', addr)
-        w__ T..:
-            data _ conn.r..(1024)
-            __ not data:
-                b..
-            conn.s_a..(data)
+# ______ so..
+#
+# HOST _ '127.0.0.1'  # Standard loopback interface address (localhost)
+# PORT _ 8081        # Port to listen on (non-privileged ports are > 1023)
+#
+# w__ ?.? ?.A.. ?.S.. __ s
+#     s.b.. ?
+#     s.l..
+#     conn, addr _ s.a..
+#     w__ ?
+#         print('Connected by' ?
+#         w__ T..
+#             data _ ?.r.. 1024
+#             __ no. ?
+#                 b..
+#             ?.s_a.. ?
