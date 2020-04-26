47dead273e56f9f77f39bc39882e8d217ad83069
Grey <sergejyurskyj@yahoo.com>
2020-04-25 17:23:32 -0700

    Committed UsingSSL templates

--- 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/UsingSSL.py
+++ 105_network/_exercises/templates/Networking_and_Security/Section 3 Networking/UsingSSL.py
@@ -1,26 +1,26 @@
-______ ssl
-______ so..
-
-s _ ?.?(?.A.. ?.S..
-ssock _ ssl.wrap_socket(s)
-
-___
-    ssock.c..(("www.google.com", 443))
-    print(ssock.cipher)
-______:
-    print("error")
-
-___
-    ssock.w..(b"GET / HTTP/1.1 \r\n")
-    ssock.w..(b"Host: www.google.com\n\n")
-______ E.. __ e:
-    print("write error: ", e)
-
-data _ b_a_
-___
-    data _ ssock.r..
-______ E.. __ e:
-    print("read error: ", e)
-
-print(data.d..("utf-8"))
-
+# ______ ss.
+# ______ so..
+#
+# s _ ?.? ?.A.. ?.S..
+# ssock _ ?.w_s.. ?
+#
+# ___
+#     ?.c.. "www.google.com" 443
+#     print ?.ci..
+# ______:
+#     print("error")
+#
+# ___
+#     ?.w.. _"GET / HTTP/1.1 \r\n")
+#     ?.w.. _"Host: www.google.com\n\n")
+# ______ E.. __ e
+#     print("write error: " ?
+#
+# data _ b_a_
+# ___
+#     d.. _ ?.r..
+# ______ E.. __ e
+#     print("read error: " ?
+#
+# print ?.d.. "utf-8"
+#
