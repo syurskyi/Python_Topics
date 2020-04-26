73370486d6fda383a0622f05672938e020370d52
Grey <sergejyurskyj@yahoo.com>
2020-04-25 17:32:04 -0700

    Committed HTTPUserAgent templates

--- 105_network/_exercises/templates/Networking_and_Security/Section 4 HTTP Programming/HTTPUserAgent.py
+++ 105_network/_exercises/templates/Networking_and_Security/Section 4 HTTP Programming/HTTPUserAgent.py
@@ -1,15 +1,15 @@
-______ h_l_
-
-h _ "www.infiniteskills.com"
-
-req _ h_l_.H..(h)
-req.p_r_("GET", "/")
-req.p_h_("Host", h)
-req.p_h_("User-Agent", "Garbage browser: 5.6")
-req.p_h_("My-Header", "My value")
-req.e_h_
-req.s..("")
-
-statusCode, statusMsg, headers _ req.g_r..
-print("Response: ", statusMsg)
-
+# ______ h_l_
+#
+# h _ "www.infiniteskills.com"
+#
+# req _ h_l_.H..
+# ?.p_r_ "GET", "/")
+# ?.p_h_ "Host" ?
+# ?.p_h_ "User-Agent", "Garbage browser: 5.6"
+# ?.p_h_ "My-Header", "My value"
+# ?.e_h_
+# ?.s.. ""
+#
+# statusCode, statusMsg, headers _ req.g_r..
+# print("Response: " sM..
+#
