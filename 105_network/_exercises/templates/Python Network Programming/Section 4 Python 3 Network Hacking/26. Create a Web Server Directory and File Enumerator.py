b63cb8384cc176a360d69e7b957db85dfae409cd
Grey <sergejyurskyj@yahoo.com>
2020-04-25 17:18:38 -0700

    Committed 26. Create a Web Server Directory and File Enumerator templates

--- 105_network/_exercises/templates/Python Network Programming/Section 4 Python 3 Network Hacking/26. Create a Web Server Directory and File Enumerator.py
+++ 105_network/_exercises/templates/Python Network Programming/Section 4 Python 3 Network Hacking/26. Create a Web Server Directory and File Enumerator.py
@@ -1,54 +1,54 @@
-______ requests, ___, t__, m.., a_p_
-____ d_t_ ______ d_t_
-
-
-___ request(url
-    ___
-        agent _ {
-            "User-Agent": ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
-                           "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36")
-        }
-        rsp _ requests.get(url)
-        __ rsp.status_code !_ 404:
-            print("[+] Status " + st.(rsp.status_code) + ": " + url)
-    ______ E.. __ err:
-        print(st.(err))
-
-
-___ scan(url, wor., ext
-    turl _ "http://" + url + wor..rs..
-    request(turl)
-    __ ext:
-        request(turl + ext)
-
-
-___ main(args
-    start _ d_t_.now
-    print("==================================================")
-    print("Started @ " + st.(start))
-    print("==================================================")
-    __ args.url.endswith("/") __ False: args.url +_ "/"
-    w__ o..(args.wor.list) __ fle:
-        ___ wor. __ fle:
-            __ wor..startswith("#") __ False:
-                p _ ?.Process(t.._scan, args_(args.url, wor., args.extension))
-                p.start
-    stop _ d_t_.now
-    print("==================================================")
-    print("Scan Duration: " + st.(stop - start))
-    print("Completed @ " + st.(stop))
-    print("==================================================")
-
-
-__ _______ __ _______
-    parser _ a_p_.A_P..
-    parser.a_a..("url", action_"store", help_"starting url")
-    parser.a_a..("wordlist", action_"store", help_"list of paths/files")
-    parser.a_a..("-e", "--extension", action_"store", help_"file extension")
-
-    __ le.(___.argv[2:]) __ 0:  # Show help if required arg not included
-        parser.print_help
-        parser.e..
-
-    args _ parser.parse_args  # Declare argumnets object to args
-    main(args)
+# ______ r__ ___ t__ m.. a_p_
+# ____ d_t_ ______ d_t_
+#
+#
+# ___ request url
+#     ___
+#         agent _ |
+#             "User-Agent": ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) "
+#                            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36")
+#         |
+#         rsp _ r__.g.. ?
+#         __ ?.s_c.. !_ 404
+#             print("[+] Status " + st. ?.s_c.. + ": " + ?
+#     ______ E.. __ err
+#         print(st. ?
+#
+#
+# ___ scan url word ext
+#     turl _ "http://" + ? + ?.rs..
+#     r... ?
+#     __ e..
+#         r.. t.. + e..
+#
+#
+# ___ main args
+#     start _ d_t_.n..
+#     print("==================================================")
+#     print("Started @ " + st. s..
+#     print("==================================================")
+#     __ ?.u__.e_w..("/") __ F.. ?.url +_ "/"
+#     w__ o.. ?.w.. __ fle
+#         ___ word __ f..
+#             __ w__.s_w.. "#") __ F..
+#                 p _ ?.P.. t.._s.. a.._ ?.u.. w.. ?.ex..
+#                 ?.s..
+#     stop _ d_t_.n..
+#     print("==================================================")
+#     print("Scan Duration: " + st. s.. - s..
+#     print("Completed @ " + st. s..
+#     print("==================================================")
+#
+#
+# __ _______ __ _______
+#     parser _ a_p_.A_P..
+#     ?.a_a.. "url" a.."store" h.._"starting url"
+#     ?.a_a.. "wordlist" a.."store" h.._"list of paths/files"
+#     ?.a_a.. "-e" "--extension" a.."store" h.._"file extension"
+#
+#     __ le. ___.a.. 2; __ 0  # Show help if required arg not included
+#         ?.p_h..
+#         ?.e..
+#
+#     args _ ?.p_a..  # Declare argumnets object to args
+#     m.. ?
