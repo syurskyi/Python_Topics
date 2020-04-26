8551e9d67ebfd00c30a365bab51deee03ce91590
Grey <sergejyurskyj@yahoo.com>
2020-04-25 17:31:15 -0700

    Committed HTTPProxy templates

--- 105_network/_exercises/templates/Networking_and_Security/Section 4 HTTP Programming/HTTPProxy.py
+++ 105_network/_exercises/templates/Networking_and_Security/Section 4 HTTP Programming/HTTPProxy.py
@@ -1,9 +1,9 @@
-______ _2
-
-proxy _ _2.ProxyHandler({'http': '127.0.0.1:8080'})
-opener _ _2.build_opener(proxy)
-_2.install_opener(opener)
-handle _ _2.u_o..('http://www.microsoft.com')
-
-print(handle.r..)
-
+# ______ _2
+#
+# proxy _ _2.PrH.. 'http': '127.0.0.1:8080'
+# opener _ _2.b_o.. ?
+# _2.i_o.. ?
+# handle _ _2.u_o.. 'http://www.microsoft.com'
+#
+# print ?.r..
+#
