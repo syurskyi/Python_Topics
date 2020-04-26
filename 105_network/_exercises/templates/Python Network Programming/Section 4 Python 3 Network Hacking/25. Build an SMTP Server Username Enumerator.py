211ce44283185d1166bfe6f8419899a47013107b
Grey <sergejyurskyj@yahoo.com>
2020-04-25 17:10:52 -0700

    Committed 25. Build an SMTP Server Username Enumerator templates

--- 105_network/_exercises/templates/Python Network Programming/Section 4 Python 3 Network Hacking/25. Build an SMTP Server Username Enumerator.py
+++ 105_network/_exercises/templates/Python Network Programming/Section 4 Python 3 Network Hacking/25. Build an SMTP Server Username Enumerator.py
@@ -1,69 +1,69 @@
-______ so.., ___, a_p_
-____ d_t_ ______ d_t_
-
-
-___ scan(ip, users
-    ___
-        s _ ?.?(?.A.. ?.S..
-        s.c..((ip, 25))
-        rsp _ s.r..(1024)
-        s.s..(b"HELO friend\n")
-        rsp _ s.r..(1024)
-        __ b"250" no. __ rsp:
-            print("[!] Something went wrong, exiting.")
-            ___.e..(0)
-        s.s..(b"MAIL FROM:nice@guy.com\n")
-        rsp _ s.r..(1024)
-        __ b"250" no. __ rsp:
-            print("[!] Something went wrong, exiting.")
-            ___.e..(0)
-        ___ user __ users:
-            s.s..(b"RCPT TO:" + user.rs...en.. + b"\n")
-            rsp _ s.r..(1024)
-            __ b"250" __ rsp:
-                print("[+] Valid: " + user.rs..)
-        s.s..(b"QUIT\n")
-        s.c..
-    ______ E.. __ err:
-        print(st.(err))
-
-
-___ main(args
-    start _ d_t_.now
-    print("==================================================")
-    print("Started @ " + st.(start))
-    print("==================================================")
-    w__ o..(args.wor.list) __ fle:
-        usr _   # list
-        __ args.batch !_ 0:
-            ___ user __ fle:
-                __ (le.(usr) + 1) !_ args.batch:
-                    usr.ap..(user)
-                ____
-                    usr.ap..(user)
-                    scan(args.ip, usr)
-                    del usr[:]
-            __ le.(usr) > 0:
-                scan(args.ip, usr)
-        ____  # No batches
-            scan(args.ip, fle)
-    stop _ d_t_.now
-    print("==================================================")
-    print("Duration: " + st.(stop - start))
-    print("Completed @ " + st.(stop))
-    print("==================================================")
-
-
-__ _______ __ _______
-    parser _ a_p_.A_P..
-    parser.a_a..("ip", action_"store", help_"smtp host address")
-    parser.a_a..("wordlist", action_"store", help_"wordlist of usernames")
-    parser.a_a..("-b", "--batch", action_"store", nargs_'?', const_10,
-                        default_0, help_"attempts per connection", type_int)
-
-    __ le.(___.argv[2:]) __ 0:  # Show help if required arg not included
-        parser.print_help
-        parser.e..
-
-    args _ parser.parse_args  # Declare argumnets object to args
-    main(args)
+# ______ so.., ___, a_p_
+# ____ d_t_ ______ d_t_
+#
+#
+# ___ scan ip users
+#     ___
+#         s _ ?.? ?.A.. ?.S..
+#         s.c.. ip 25
+#         rsp _ s.r.. 1024
+#         s.s.. _"HELO friend\n")
+#         rsp _ s.r.. 1024
+#         __ _"250" no. __ ?
+#             print("[!] Something went wrong, exiting.")
+#             ___.e.. 0
+#         s.s.. _"MAIL FROM:nice@guy.com\n")
+#         rsp _ s.r.. 1024
+#         __  _"250" no. __ ?
+#             print("[!] Something went wrong, exiting.")
+#             ___.e.. 0
+#         ___ user __ u..
+#             s.s.. _"RCPT TO:" + ?.rs...en.. + _"\n")
+#             rsp _ s.r.. 1024
+#             __ _"250" __ rsp
+#                 print("[+] Valid: " + ?.rs..
+#         s.s.. _)"QUIT\n"
+#         s.c..
+#     ______ E.. __ err
+#         print(st. ?
+#
+#
+# ___ main args
+#     start _ d_t_.n..
+#     print("==================================================")
+#     print("Started @ " + st. s..
+#     print("==================================================")
+#     w__ o..(?.w__.li.. __ fle
+#         usr _   # list
+#         __ ?.b.. !_ 0:
+#             ___ user __ f..
+#                 __  le. ? + 1 !_ ?.b..
+#                     u__.ap.. ?
+#                 ____
+#                     u__.ap.. ?
+#                     s.. ?.i. u..
+#                     de. u..|;
+#             __ le. u.. > 0
+#                 s.. ?.i. u..
+#         ____  # No batches
+#             s.. ?.i. f..
+#     stop _ d_t_.n..
+#     print("==================================================")
+#     print("Duration: " + st. s.. - s..
+#     print("Completed @ " + st. s..
+#     print("==================================================")
+#
+#
+# __ _______ __ _______
+#     parser _ a_p_.A_P..
+#     ?.a_a.. "ip" a.._"store" h.._"smtp host address")
+#     ?.a_a.. "wordlist" a.._"store" h.._"wordlist of usernames")
+#     ?.a_a.. "-b" "--batch" a.._"store" n.._'?' c.._10
+#                         d.._0 h.._"attempts per connection" ty.._in.
+#
+#     __ le. ___.a.. 2; __ 0  # Show help if required arg not included
+#         ?.p_h..
+#         ?.e..
+#
+#     args _ ?.p_a..  # Declare argumnets object to args
+#     m.. ?
