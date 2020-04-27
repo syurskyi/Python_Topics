6431b70d5e3cb9669c56a5e0a36359d4d00ea4d3
Grey <sergejyurskyj@yahoo.com>
2020-04-26 17:57:51 -0700

    Committed MongoDB_for_Developers_with_Python templates

---
+++ 100_databases/003_mongodb/_exercises/templates/MongoDB_for_Developers_with_Python/src/src/08_perf/big_dealership/nosql/engine.py
@@ -0,0 +1,10 @@
+_____ u..
+
+_____ m_e_
+
+
+c_ Engine(?.EmbeddedD..
+    horsepower _ ?.IF..(r.._True)
+    liters _ ?.FF..(r.._True)
+    mpg _ ?.FF..(r.._True)
+    serial_number _ ?.SF..(d.._lambda: st.(?.uuid4()))
---
+++ 100_databases/003_mongodb/_exercises/templates/MongoDB_for_Developers_with_Python/src/src/08_perf/big_dealership/nosql/service_record.py
@@ -0,0 +1,9 @@
+_____ d_t_
+_____ m_e_
+
+
+c_ ServiceRecord(?.ED..):
+    date _ ?.DTF..(d.._d_t_.d_t_.n..)
+    description _ ?.SF..()
+    price _ ?.FF..(r.._True)
+    customer_rating _ ?.IF..(r.._True)  # 1 - 5 satisfaction level.
