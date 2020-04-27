bd5116e1704d6baf4327c9f35c4011d6d3e9e87b
Grey <sergejyurskyj@yahoo.com>
2020-04-26 17:55:03 -0700

    Committed MongoDB_for_Developers_with_Python templates

---
+++ 100_databases/003_mongodb/_exercises/templates/MongoDB_for_Developers_with_Python/src/src/07_mongoengine/service_central/nosql/servicehistory.py
@@ -0,0 +1,10 @@
+# _____ m_e_
+# _____ d_t_
+#
+#
+# c_ ServiceHistory ?.ED..
+#     date _ ?.DTF.. d.._d_t_.d_t_.n..
+#     description _ ?.SF..
+#     price _ ?.FF..
+#     customer_rating _ ?.IF.. m.._1, m.._5
+#
