0a360827d3e8d47bd27605be584c270b2a8eacba
Grey <sergejyurskyj@yahoo.com>
2020-04-28 09:41:40 -0700

    Committed 4_12_tornado_async_server template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter04/4_12_tornado_async_server.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter04/4_12_tornado_async_server.py
@@ -1,30 +1,30 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 3
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-______ ?
-______ tornado.ioloop
-______ tornado.httpclient
-
-
-c_ TornadoAsync
-    ___ handle_requestresponse):
-        __ response.e..:
-            print ("Error:", response.e..)
-        ____
-            print (response.body)
-        tornado.ioloop.IOLoop.instance().stop()
-
-___ run_server(url):
-    tornadoAsync _ TornadoAsync()
-    http_client _ tornado.httpclient.AsyncHTTPClient()
-    http_client.fetch(url, tornadoAsync.handle_request)
-    tornado.ioloop.IOLoop.instance().s..
-
-__ _______ __ ______
-    parser _ ?.AP..(d.._'Async Server Example')
-    parser.a_a..('--url', a.._"store", d.._"url", type_str, r.._T..)
-    given_args _ parser.p_a..
-    url _ ?.url
-    run_server(url)
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 3
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+# ______ ?
+# ______ to__.io..
+# ______ to__.h..
+#
+#
+# c_ TornadoAsync
+#     ___ handle_requestresponse
+#         __ r__.e..
+#             print ("Error:" r__.e..
+#         ____
+#             print  r__.b..
+#         ?.io__.IOL__.i__.s..
+#
+# ___ run_server url
+#     tornadoAsync _ ?
+#     http_client _ ?.h_c_.AHC..
+#     ?.f.. ? ?.h_r..
+#     ?.io__.IOL__.i.. .s..
+#
+# __ _______ __ ______
+#     parser _ ?.AP.. d.._'Async Server Example'
+#     ?.a_a.. '--url' a.._"store" d.._"url" ty.._st. r.._T..
+#     given_args _ ?.p_a..
+#     url _ ?.u..
+#     ? ?
