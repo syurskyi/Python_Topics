72ed1dcc20c35991d5df6b1d69f4fd0757f66923
Grey <sergejyurskyj@yahoo.com>
2020-04-27 18:24:04 -0700

    Committed 1_9_socket_modes templates

---
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter01/1_9_socket_modes.py
@@ -0,0 +1,20 @@
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 1
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+# ______ s..
+#
+# ___ test_socket_modes
+#     s _ ?.? ?.A.. ?.S..
+#     ?.s_b.. 1
+#     ?.s_t_o.. 0.5
+#     ?.bind(("127.0.0.1", 0))
+#
+#     socket_address _ ?.g_s_n..
+#     print ("Trivial Server launched on socket: @" st.. ?
+#     w__ 1
+#         s.l.. 1
+#
+# __ _______ __ ______
+#     ?
