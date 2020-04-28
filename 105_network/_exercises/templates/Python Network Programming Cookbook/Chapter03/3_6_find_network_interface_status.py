71068fe4b0e9ba513bd964836d05be9579f2bb72
Grey <sergejyurskyj@yahoo.com>
2020-04-27 18:55:12 -0700

    Committed 3_6_find_network_interface_status templates

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter03/3_6_find_network_interface_status.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter03/3_6_find_network_interface_status.py
@@ -1,35 +1,35 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 3
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-
-______ a_p..
-______ ?
-______ st__
-______ fcntl
-______ nmap
-
-
-SAMPLE_PORTS _ '21-23'
-
-
-___ get_interface_status(ifname):
-    sock _ ?.?(?.A.. ?.S_D..)
-    ip_address _ ?.inet_ntoa(fcntl.ioctl(
-        sock.f_n..,
-        0x8915, #SIOCGIFADDR, C socket library sockios.h
-        st__.pack(b'256s', by..(ifname[:15], 'utf-8'))
-    )[20:24])
-    nm _ nmap.PortScanner()
-    nm.scan(ip_address, SAMPLE_PORTS)      
-    r_ nm[ip_address].state()
-
-    
-__  __name__ __ '__main__':
-    parser _ ?.AP..(d.._'Python networking utils')
-    ?.a_a..('--ifname', a.._"store", d.._"ifname", r.._T..)
-    given_args _ parser.parse_args()
-    ifname _ ?.ifname
-    print ("Interface [@] is: @" (ifname, get_interface_status(ifname)))
-
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 3
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+#
+# ______ a_p..
+# ______ ?
+# ______ st__
+# ______ fcntl
+# ______ nmap
+#
+#
+# SAMPLE_PORTS _ '21-23'
+#
+#
+# ___ get_interface_status ifname
+#     sock _ ?.? ?.A.. ?.S_D..
+#     ip_address _ ?.i_n.. fc__.io..
+#         ?.f_n..,
+#         0x8915, #SIOCGIFADDR, C socket library sockios.h
+#         st__.pa.. b'256s', by.. if.. ;15 'utf-8'
+#     20;24
+#     nm _ nm__.PS..
+#     ?.s.. i_a.. S_P..
+#     r_ ? i_a.. .s..
+#
+#
+# __ _____ __ _____
+#     parser _ ?.AP.. d.._'Python networking utils'
+#     ?.a_a.. '--ifname' a.._"store" d.._"ifname" r.._T..
+#     given_args _ ?.p_a..
+#     ifname _ ?.if..
+#     print ("Interface [@] is: @" ? ? ?
+#
