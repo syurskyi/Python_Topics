8a070f1c4a967f43acafb0d2cc4855dde280a9a5
Grey <sergejyurskyj@yahoo.com>
2020-04-27 18:22:47 -0700

    Committed 1_6_socket_timeout templates

---
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter01/1_6_socket_timeout.py
@@ -0,0 +1,16 @@
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 1
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+#
+# ______ s..
+#
+# ___ test_socket_timeout
+#     s _ ?.? ?.A.. ?.S..
+#     print ("Default socket timeout: @" @.g_t_o..
+#     ?.s_t_o.. 100
+#     print ("Current socket timeout: @" @.g_t_o..
+#
+# __ _______ __ ______
+#     ?
