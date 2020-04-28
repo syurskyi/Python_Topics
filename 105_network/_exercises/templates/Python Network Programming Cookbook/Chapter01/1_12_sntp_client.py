d0a9bed403799a5288a57dea721c306933d4ae57
Grey <sergejyurskyj@yahoo.com>
2020-04-27 18:25:11 -0700

    Committed 1_12_sntp_client templates

---
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter01/1_12_sntp_client.py
@@ -0,0 +1,28 @@
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 1
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+#
+# ______ s..
+# ______ struct
+# ______ ___
+# ______ t__
+#
+# N_S.. _ "0.uk.pool.ntp.org"
+# TIME1970 _ 2208988800
+#
+# ___ sntp_client
+#     client _ ?.? ?.A.. ?.S_D..
+#     data _ '\x1b' + 47 * '\0'
+#     ?.s_t.. ?.e.. 'utf-8' N_S.., 123
+#     data, address _ ?.r_f.. 1024
+#     __ data
+#         print ('Response received from:' a..
+#     t _ ?.u.. '!12I' ?  10
+#     t -_ TIME1970
+#     print ('\tTime=@'  t__.ct.. ?
+#
+#
+# __ _______ __ ______
+#     ?
