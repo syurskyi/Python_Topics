c8bb4ed374d5f93fbb0ce4e9786f5e112472d6d2
Grey <sergejyurskyj@yahoo.com>
2020-04-27 20:59:02 -0700

    Committed 4_1_download_data template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter04/4_1_download_data.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter04/4_1_download_data.py
@@ -1,38 +1,38 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook -- Chapter - 4
-# This program requires Python 3.5.2 or any later version
-# It may run on any other version with/without modifications.
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook -- Chapter - 4
+# # This program requires Python 3.5.2 or any later version
+# # It may run on any other version with/without modifications.
+# #
+# # Follow the comments inline to make it run on Python 2.7.x.
 #
-# Follow the comments inline to make it run on Python 2.7.x.
-
-______ ?
-
-______ urllib.r__
-# Comment out the above line and uncomment the below for Python 2.7.x.
-#import urllib2
-
-REMOTE_SERVER_HOST _ 'http://www.cnn.com'
-
-c_ HTTPClient:
-
-    ___ -  host):
-        host _ host
-
-    ___ fetch
-        response _ urllib.r__.urlopen(host)
-        # Comment out the above line and uncomment the below for Python 2.7.x.
-        #response = urllib2.urlopen(self.host)
-
-        data _ response.read()
-        text _ data.d..('utf-8')
-        r_ text
-
-__ _____ __ ______
-    parser _ ?.AP..(d.._'HTTP Client Example')
-    ?.a_a..('--host', a.._"store", d.._"host",  default_REMOTE_SERVER_HOST)
-
-    given_args _ ?.p_a..
-    host _ ?.host
-    client _ HTTPClient(host)
-    print (client.fetch())
-
+# ______ ?
+#
+# ______ u__.r__
+# # Comment out the above line and uncomment the below for Python 2.7.x.
+# #import urllib2
+#
+# REMOTE_SERVER_HOST _ 'http://www.cnn.com'
+#
+# c_ HTTPClient
+#
+#     ___ -  host
+#         ? ?
+#
+#     ___ fetch
+#         response _ ?.r__.u_o.. ?
+#         # Comment out the above line and uncomment the below for Python 2.7.x.
+#         #response = urllib2.urlopen(self.host)
+#
+#         data _ ?.r..
+#         text _ ?.d.. 'utf-8'
+#         r_ ?
+#
+# __ _____ __ ______
+#     parser _ ?.AP.. d.._'HTTP Client Example'
+#     ?.a_a..('--host', a.._"store", d.._"host",  d.._R_S_H..
+#
+#     given_args _ ?.p_a..
+#     host _ ?.h..
+#     client _ ? ?
+#     print ?.f..
+#
