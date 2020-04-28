724475f1adbe739976d5969971fe587a0d08dbfb
Grey <sergejyurskyj@yahoo.com>
2020-04-27 18:19:48 -0700

    Committed 1_2_remote_machine_info templates

---
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter01/1_2_remote_machine_info.py
@@ -0,0 +1,17 @@
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 1
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+#
+# ______ s..
+#
+# ___ get_remote_machine_info
+#     remote_host _ 'www.python.org'
+#     ___
+#         print ("IP address of @: @" ? ?.g_h_b_n.. ?
+#     ______ ?.e.. __ err_msg
+#         print ("@: @" ? ?
+#
+# __ _______ __ ______
+#     ?
