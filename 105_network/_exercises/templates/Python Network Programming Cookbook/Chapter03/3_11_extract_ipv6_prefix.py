a24a42c053ebdc4b4b579bc743f934fd7c192fdb
Grey <sergejyurskyj@yahoo.com>
2020-04-27 19:15:49 -0700

    Committed 3_11_extract_ipv6_prefix template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter03/3_11_extract_ipv6_prefix.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter03/3_11_extract_ipv6_prefix.py
@@ -1,36 +1,36 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 3
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-# This program depends on Python modules netifaces and netaddr.
-
-______ ?
-______ netifaces __ ni
-______ netaddr __ na
-
-___ extract_ipv6_info
-    """ Extracts IPv6 information"""
-    print ("IPv6 support built into Python: @" ?.has_ipv6)
-    ___ interface __ ni.interfaces
-        all_addresses _ ni.ifaddresses(interface)
-        print ("Interface @:" interface)
-        ___ family,addrs __ all_addresses.items
-            fam_name _ ni.address_families[family]
-
-            ___ addr __ addrs:
-                __ fam_name __ 'AF_INET6':
-                    addr _ addr['addr']
-                    has_eth_string _ addr.s..("eth")
-                    __ has_eth_string:
-                        addr _ addr.s..("eth")[0]
-                    ___
-                        print ("    IP Address: @" na.IPNetwork(addr))
-                        print ("    IP Version: @" na.IPNetwork(addr).version)
-                        print ("    IP Prefix length: @" na.IPNetwork(addr).prefixlen)
-                        print ("    Network: @" na.IPNetwork(addr).network)
-                        print ("    Broadcast: @" na.IPNetwork(addr).broadcast)
-                    ______ E.. __ e:
-                        print ("Skip Non-IPv6 Interface")
-
-__ _______ __ ______
-    extract_ipv6_info()
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 3
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+# # This program depends on Python modules netifaces and netaddr.
+#
+# ______ ?
+# ______ netifaces __ ni
+# ______ netaddr __ na
+#
+# ___ extract_ipv6_info
+#     """ Extracts IPv6 information"""
+#     print ("IPv6 support built into Python: @" ?.__6
+#     ___ interface __ ?.i..
+#         all_addresses _ ?.if.. ?
+#         print ("Interface @:" ?
+#         ___ family,addrs __ a_a__.i..
+#             fam_name _ ?.a_f.. f..
+#
+#             ___ addr __ a..
+#                 __ fam_name __ 'AF_INET6'
+#                     addr _ a.. 'addr'
+#                     has_eth_string _ ?.s.. "eth"
+#                     __ ?
+#                         addr _ a__.s.. "eth" 0
+#                     ___
+#                         print ("    IP Address: @" ?.IPN.. a..
+#                         print ("    IP Version: @" ?.IPN.. a.. .v..
+#                         print ("    IP Prefix length: @" ?.IPN.. a.. .p..
+#                         print ("    Network: @" ?.IPN.. a.. .n..
+#                         print ("    Broadcast: @" ?.IPN.. a.. .b..
+#                     ______ E.. __ e
+#                         print ("Skip Non-IPv6 Interface")
+#
+# __ _______ __ ______
+#     ?
