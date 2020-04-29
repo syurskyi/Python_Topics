df00b71aaba181119e6a8eb105b3551a4307f9c0
Grey <sergejyurskyj@yahoo.com>
2020-04-28 19:31:09 -0700

    Committed 5_1_list_files_on_ftp_server template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter05/5_1_list_files_on_ftp_server.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter05/5_1_list_files_on_ftp_server.py
@@ -1,27 +1,27 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 5
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-
-FTP_SERVER_URL _ 'ftp.ed.ac.uk'
-
-______ ft..
-____ ft.. ______ FTP
-
-___ test_ftp_connection(pa__, username, email):
-    #Open ftp connection
-    ftp _ ?.FTP(pa__, username, email)
-    
-   #List the files in the /pub directory
-    ftp.cwd("/pub")
-    print ("File list at @:" pa__)
-    files _ ftp.dir()
-    print (files)
-
-    ftp.quit()
-
-__ _______ __ ______
-    test_ftp_connection(path_FTP_SERVER_URL, username_'anonymous', 
-                        email_'nobody@nourl.com', 
-                        )
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 5
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+#
+# FTP_SERVER_URL _ 'ftp.ed.ac.uk'
+#
+# ______ ft..
+# ____ ft.. ______ F..
+#
+# ___ test_ftp_connection(pa__, username, email):
+#     #Open ftp connection
+#     ftp _ ?.? pa__ ? ?
+#
+#    #List the files in the /pub directory
+#     ?.cwd("/pub")
+#     print ("File list at @:" pa__
+#     files _ ?.d..
+#     print ?
+#
+#     ?.q..
+#
+# __ _______ __ ______
+#     ? pa.._F.. u.._'anonymous',
+#                         e_'nobody@nourl.com'
+#
