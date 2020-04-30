a9c86b603ae78e02171396e07e003b85beb4d07e
Grey <sergejyurskyj@yahoo.com>
2020-04-29 20:11:43 -0700

    Committed 6_7_configure_Apache_for_hosting_website_remotely template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter06/6_7_configure_Apache_for_hosting_website_remotely.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter06/6_7_configure_Apache_for_hosting_website_remotely.py
@@ -1,39 +1,39 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 6
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-____ g_p_ ______ g_p_
-____ fabric.api ______ env, put, sudo, p..
-____ fabric.contrib.files ______ e..
-
-WWW_DOC_ROOT _ "/data/apache/test/"
-WWW_USER _ "www-data"
-WWW_GROUP _ "www-data"
-APACHE_SITES_PATH _ "/etc/apache2/sites-enabled/"
-APACHE_INIT_SCRIPT _ "/etc/init.d/apache2 "
-
-
-___ remote_server
-    env.hosts _ ['127.0.0.1']
-    env.user _ p..('Enter user name: ')
-    env.password _ g_p_('Enter your system password: ')
-
-
-___ setup_vhost
-    """ Setup a test website """
-    print ("Preparing the Apache vhost setup...")
-    print ("Setting up the document root...")
-    __ e..(WWW_DOC_ROOT):
-        sudo("rm -rf @" WWW_DOC_ROOT)
-    sudo("mkdir -p @" WWW_DOC_ROOT)
-    sudo("chown -R @.@ @" (env.user, env.user, WWW_DOC_ROOT))
-    put(local_path_"index.html", remote_path_WWW_DOC_ROOT)
-    sudo("chown -R @.@ @" (WWW_USER, WWW_GROUP, WWW_DOC_ROOT))
-    print ("Setting up the vhost...")
-    sudo("chown -R @.@ @" (env.user, env.user, APACHE_SITES_PATH))
-    put(local_path_"vhost.conf", remote_path_APACHE_SITES_PATH)
-    sudo("chown -R @.@ @" ('root', 'root', APACHE_SITES_PATH))
-    sudo("@ restart" APACHE_INIT_SCRIPT)
-    print ("Setup complete. Now open the server path http://abc.remote-server.org/ in your web browser.")
-
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 6
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+# ____ g_p_ ______ g_p_
+# ____ f__.api ______ env  put  sudo  p..
+# ____ f__.contrib.files ______ e..
+#
+# WWW_DOC_ROOT _ "/data/apache/test/"
+# WWW_USER _ "www-data"
+# WWW_GROUP _ "www-data"
+# APACHE_SITES_PATH _ "/etc/apache2/sites-enabled/"
+# APACHE_INIT_SCRIPT _ "/etc/init.d/apache2 "
+#
+#
+# ___ remote_server
+#     e__.h.. _ '127.0.0.1'
+#     e__.u.. _ p..('Enter user name: ')
+#     e__.p.. _ g_p_('Enter your system password: ')
+#
+#
+# ___ setup_vhost
+#     """ Setup a test website """
+#     print ("Preparing the Apache vhost setup...")
+#     print ("Setting up the document root...")
+#     __ e..?_D..
+#         su.. "rm -rf @" _D..
+#     ? "mkdir -p @" _D..
+#     ? "chown -R @.@ @" e__.u.. e__.u.. _D..
+#     p.. l_p_"index.html" r_p..__D..
+#     ? "chown -R @.@ @" _U  _G _D
+#     print ("Setting up the vhost...")
+#     ? "chown -R @.@ @" e__.u.. e__.u..  A..
+#     p.. l_p.._"vhost.conf"  r.._A..
+#     ? "chown -R @.@ @" ('root'  'root'  A..
+#     ? "@ restart" A_I..
+#     print ("Setup complete. Now open the server path http://abc.remote-server.org/ in your web browser.")
+#
