6403312a94356bc13df4f0091efbe0dcb4b5a2d8
Grey <sergejyurskyj@yahoo.com>
2020-04-30 10:01:01 -0700

    Committed example3_tcp_client template

--- 105_network/001_socket_programming/_exercises/templates/ITDVN/simple/tcp/example3_tcp_client.py
+++ 105_network/001_socket_programming/_exercises/templates/ITDVN/simple/tcp/example3_tcp_client.py
@@ -1,7 +1,7 @@
-# example 1 (UDP client socket )
-______  ?
-
-sock _ ?.?(?.A_I.., ?.S_S..)
-?.connect(('', 8888))
-?.send(b'Test message')
-?.c..
+# # example 1 (UDP client socket )
+# ______  ?
+#
+# sock _ ?.?(?.A_I.., ?.S_S..)
+# ?.c.. '' 8888
+# ?.s.. _'Test message'
+# ?.c..
