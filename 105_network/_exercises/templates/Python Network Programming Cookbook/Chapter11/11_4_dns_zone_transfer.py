bd116e736b268f56913e1c328180b2f547cb0a32
Grey <sergejyurskyj@yahoo.com>
2020-04-30 08:52:28 -0700

    Committed 11_4_dns_zone_transfertemplate

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_4_dns_zone_transfer.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_4_dns_zone_transfer.py
@@ -4,29 +4,29 @@
 # It may run on any other version with/without modifications.
 
 ______ a_p..
-______ d__.zone
-______ d__.resolver
+______ d__.zo..
+______ d__.re..
 ______ ?
 
-___ main(address):
-    soa_answer _ ?.resolver.query(address, 'SOA')
-    master_answer _ ?.resolver.query(soa_answer[0].mname, 'A')
+___ main address
+    soa_answer _ ?.r__.q.. ? 'SOA'
+    master_answer _ ?.r__.q.. ? 0 .mn.. 'A')
     ___
-        z _ ?.zone.from_xfr(?.query.xfr(master_answer[0].address, address))
-        names _ z.nodes.keys()
-        names.sort()
-        ___ n __ names:
-            print(z[n].to_text(n))
-    ______ ?.e.. __ e:
-        print('Failed to perform zone transfer:', e)
-    ______ d__.exception.FormError __ e:
-        print('Failed to perform zone transfer:', e)
+        z _ ?.zo__.fr_xf. ?.q__.xfr ? 0 .a.. a..
+        names _ z.n__.k..
+        ?.so..
+        ___ n __ ?
+            print ? ?.t_t.. ?
+    ______ ?.e.. __ e
+        print('Failed to perform zone transfer:' ?
+    ______ d__.e___.F.. __ e
+        print('Failed to perform zone transfer:' ?
 
 
 __ _______ __ ______
-    parser _ ?.AP..(d.._'DNS Python')
-    ?.a_a..('--address', a.._"store", d.._"address",  d.._'dnspython.org')
+    parser _ ?.AP..(d.._'DNS Python'
+    ?.a_a..('--address' a.._"store" d.._"address"  d.._'dnspython.org'
     given_args _ ?.p_a..
-    address _ ?.address
-    main (address)
+    address _ ?.a...
+    ? ?
 
