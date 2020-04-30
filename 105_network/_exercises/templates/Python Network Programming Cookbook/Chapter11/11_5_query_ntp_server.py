b5f616c6d865af8b002a694175d5b7a2f308dd48
Grey <sergejyurskyj@yahoo.com>
2020-04-29 21:31:04 -0700

    Committed  template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_5_query_ntp_server.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_5_query_ntp_server.py
@@ -1,30 +1,30 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 11
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-______ a_p..
-______ ntplib
-____ t__ ______ ctime
-
-
-___ main(address, v):
-    c _ ntplib.NTPClient()
-    response _ c.r__(address, version_v)
-    print("Response Offset: ", response.offset)
-    print("Version: ", response.version)
-    print("Response (Time): ", ctime(response.tx_time))
-    print("Leap: ", ntplib.leap_to_text(response.leap))
-    print("Root Delay: ", response.root_delay)
-    print(ntplib.ref_id_to_text(response.ref_id))
-
-
-__ _______ __ ______
-    parser _ ?.AP..(d.._'Query NTP Server')
-    ?.a_a..('--address', a.._"store", d.._"address",  d.._'pool.ntp.org')
-    ?.a_a..('--version', a.._"store", d.._"version",  ty.._in., d.._3)
-    given_args _ ?.p_a..
-    address _ ?.address
-    version _ ?.version
-    main (address, version)
-
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 11
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+# ______ a_p..
+# ______ ntplib
+# ____ t__ ______ ctime
+#
+#
+# ___ main(address, v):
+#     c _ ntplib.NTPClient()
+#     response _ c.r__(address, version_v)
+#     print("Response Offset: ", response.offset)
+#     print("Version: ", response.version)
+#     print("Response (Time): ", ctime(response.tx_time))
+#     print("Leap: ", ntplib.leap_to_text(response.leap))
+#     print("Root Delay: ", response.root_delay)
+#     print(ntplib.ref_id_to_text(response.ref_id))
+#
+#
+# __ _______ __ ______
+#     parser _ ?.AP..(d.._'Query NTP Server')
+#     ?.a_a..('--address', a.._"store", d.._"address",  d.._'pool.ntp.org')
+#     ?.a_a..('--version', a.._"store", d.._"version",  ty.._in., d.._3)
+#     given_args _ ?.p_a..
+#     address _ ?.address
+#     version _ ?.version
+#     main (address, version)
+#
--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_7_query_ldap_server_b.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_7_query_ldap_server_b.py
@@ -1,36 +1,36 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 11
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-______ a_p..
-____ ldap3 ______ Server, Connection, ALL, core
-
-
-___ main(address, dn, password):
-    # Create the Server object with the given address.
-    server _ Server(address, get_info_ALL)
-    #Create a connection object, and bind with the given DN and password.
-    ___
-        conn _ Connection(server, dn, password, auto_bind_True)
-        print('LDAP Bind Successful.')
-        # Perform a search for a pre-defined criteria.
-        # Mention the search filter / filter type and attributes.
-        conn.search('dc=example,dc=com', '(&(uid=euler))' , attributes_['sn'])
-        # Print the resulting entries.
-        print(conn.entries[0])
-    ______ core.exceptions.LDAPBindError __ e:
-        # If the LDAP bind failed for reasons such as authentication failure.
-        print('LDAP Bind Failed: ', e)
-
-__ _______ __ ______
-    parser _ ?.AP..(d.._'Query LDAP Server')
-    ?.a_a..('--address', a.._"store", d.._"address",  d.._'ldap.forumsys.com')
-    ?.a_a..('--dn', a.._"store", d.._"dn",  d.._'cn=read-only-admin,dc=example,dc=com')
-    ?.a_a..('--password', a.._"store", d.._"password",  d.._'password')
-    given_args _ ?.p_a..
-    address _ ?.address
-    dn _ ?.dn
-    password _ ?.password
-    main (address, dn, password)
-
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 11
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+# ______ a_p..
+# ____ ldap3 ______ Server, Connection, ALL, core
+#
+#
+# ___ main(address, dn, password):
+#     # Create the Server object with the given address.
+#     server _ Server(address, get_info_ALL)
+#     #Create a connection object, and bind with the given DN and password.
+#     ___
+#         conn _ Connection(server, dn, password, auto_bind_True)
+#         print('LDAP Bind Successful.')
+#         # Perform a search for a pre-defined criteria.
+#         # Mention the search filter / filter type and attributes.
+#         conn.search('dc=example,dc=com', '(&(uid=euler))' , attributes_['sn'])
+#         # Print the resulting entries.
+#         print(conn.entries[0])
+#     ______ core.exceptions.LDAPBindError __ e:
+#         # If the LDAP bind failed for reasons such as authentication failure.
+#         print('LDAP Bind Failed: ', e)
+#
+# __ _______ __ ______
+#     parser _ ?.AP..(d.._'Query LDAP Server')
+#     ?.a_a..('--address', a.._"store", d.._"address",  d.._'ldap.forumsys.com')
+#     ?.a_a..('--dn', a.._"store", d.._"dn",  d.._'cn=read-only-admin,dc=example,dc=com')
+#     ?.a_a..('--password', a.._"store", d.._"password",  d.._'password')
+#     given_args _ ?.p_a..
+#     address _ ?.address
+#     dn _ ?.dn
+#     password _ ?.password
+#     main (address, dn, password)
+#
