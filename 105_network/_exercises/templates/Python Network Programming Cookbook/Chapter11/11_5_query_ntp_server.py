c6d29b835248e093f8b7f310ccb0f3ffa57b9066
Grey <sergejyurskyj@yahoo.com>
2020-04-30 08:55:26 -0700

    Committed 11_5_query_ntp_server template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_5_query_ntp_server.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_5_query_ntp_server.py
@@ -1,30 +1,30 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 11
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-______ a_p..
-______ ntplib
-____ t__ ______ ctime
-
-
-___ main(address, v):
-    c _ ntplib.NTPClient()
-    response _ c.r__(address, version_v)
-    print("Response Offset: ", response.offset)
-    print("Version: ", response.version)
-    print("Response (Time): ", ctime(response.tx_time))
-    print("Leap: ", ntplib.leap_to_text(response.leap))
-    print("Root Delay: ", response.root_delay)
-    print(ntplib.ref_id_to_text(response.ref_id))
-
-
-__ _______ __ ______
-    parser _ ?.AP..(d.._'Query NTP Server')
-    ?.a_a..('--address', a.._"store", d.._"address",  d.._'pool.ntp.org')
-    ?.a_a..('--version', a.._"store", d.._"version",  ty.._in., d.._3)
-    given_args _ ?.p_a..
-    address _ ?.address
-    version _ ?.version
-    main (address, version)
-
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 11
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+# ______ a_p..
+# ______ ntplib
+# ____ t__ ______ ct..
+#
+#
+# ___ main address v
+#     c _ ntplib.NTPC..
+#     response _ ?.r__ a.. v.._?
+#     print("Response Offset: "? ?.o..
+#     print("Version: "? ?.v..
+#     print("Response (Time): "? ct.. ?.t_t..
+#     print("Leap: "? ntp__.l_t_t.. ?.l..
+#     print("Root Delay: "? ?.r_d..
+#     print(ntp__.r_i_t_t.. ?.r_i.
+#
+#
+# __ _______ __ ______
+#     parser _ ?.AP.. d.._'Query NTP Server'
+#     ?.a_a.. '--address'? a.._"store"? d.._"address"?  d.._'pool.ntp.org'
+#     ?.a_a.. '--version'? a.._"store"? d.._"version"?  ty.._in.? d.._3
+#     given_args _ ?.p_a..
+#     address _ ?.a..
+#     version _ ?.v..
+#      ? a.. ? v..
+#
