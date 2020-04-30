51eb237363f8d6bb2071cd6d5db7f565737efbd3
Grey <sergejyurskyj@yahoo.com>
2020-04-29 21:44:26 -0700

    Committed 11_4_dns_zone_transfer template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_3_find_dns_rr_details.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_3_find_dns_rr_details.py
@@ -1,69 +1,69 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 11
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-______ a_p..
-______ d__.zone
-______ d__.resolver
-______ ?
-
-___ main(address):
-    # IPv4 DNS Records
-    answer _ ?.resolver.query(address, 'A')
-    ___ i __ xrange(0, le.(answer)):
-        print("Default: ", answer[i])
-
-    # IPv6 DNS Records
-    ___
-        answer6 _ ?.resolver.query(address, 'AAAA')
-        ___ i __ xrange(0, le.(answer6)):
-            print("Default: ", answer6[i])
-    ______ ?.resolver.NoAnswer __ e:
-        print("Exception in resolving the IPv6 Resource Record:", e)
-
-    # MX (Mail Exchanger) Records
-    ___
-        mx _ ?.resolver.query(address, 'MX')
-        ___ i __ xrange(0, le.(mx)):
-            print("Default: ", mx[i])
-    ______ ?.resolver.NoAnswer __ e:
-        print("Exception in resolving the MX Resource Record:", e)
-
-    ___
-        cname_answer _ ?.resolver.query(address, 'CNAME')
-        print("CNAME: ", cname_answer)
-    ______ ?.resolver.NoAnswer __ e:
-        print('Exception retrieving CNAME', e)
-
-    ___
-        ns_answer _ ?.resolver.query(address, 'NS')
-        print(ns_answer)
-    ______ ?.resolver.NoAnswer __ e:
-        print("Exception in resolving the NS Resource Record:", e)
-
-    ___
-        sig_answer _ ?.resolver.query(address, 'SIG')
-        print("SIG: ", sig_answer)
-    ______ ?.resolver.NoAnswer __ e:
-        print('Exception retrieving SIG', e)
-
-    ___
-        key_answer _ ?.resolver.query(address, 'KEY')
-        print("KEY: ", key_answer)
-    ______ ?.resolver.NoAnswer __ e:
-        print('Exception retrieving KEY', e)
-
-    soa_answer _ ?.resolver.query(address, 'SOA')
-    print("SOA Answer: ", soa_answer[0].mname)
-    master_answer _ ?.resolver.query(soa_answer[0].mname, 'A')
-    print("Master Answer: ", master_answer[0].address)
-
-
-__ _______ __ ______
-    parser _ ?.AP..(d.._'DNS Python')
-    ?.a_a..('--address', a.._"store", d.._"address",  d.._'dnspython.org')
-    given_args _ ?.p_a..
-    address _ ?.address
-    main (address)
-
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 11
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+# ______ a_p..
+# ______ d__.z..
+# ______ d__.r..
+# ______ ?
+#
+# ___ main address
+#     # IPv4 DNS Records
+#     answer _ ?.r__.q.. ? 'A'
+#     ___ i __ xr.. 0 le. ?
+#         print("Default: ", a.. ?
+#
+#     # IPv6 DNS Records
+#     ___
+#         answer6 _ ?.r__.q.. ? 'AAAA'
+#         ___ i __ xr.. 0 le. a_6
+#             print("Default: ", a_6 ?
+#     ______ ?.r__.NA.. __ e
+#         print("Exception in resolving the IPv6 Resource Record:", e)
+#
+#     # MX (Mail Exchanger) Records
+#     ___
+#         mx _ ?.r__.q.. ? 'MX'
+#         ___ i __ xr.. 0 le. ?
+#             print("Default: " ? ?
+#     ______ ?.r__.NA.. __ e
+#         print("Exception in resolving the MX Resource Record:" ?
+#
+#     ___
+#         cname_answer _ ?.r__.q.. ? 'CNAME'
+#         print("CNAME: " ?
+#     ______ ?.r__.NA.. __ e
+#         print('Exception retrieving CNAME' ?
+#
+#     ___
+#         ns_answer _ ?.r__.q.. ? 'NS'
+#         print ?
+#     ______ ?.r__.NA.. __ e
+#         print("Exception in resolving the NS Resource Record:" ?
+#
+#     ___
+#         sig_answer _ ?.r__.q..(?, 'SIG')
+#         print("SIG: " ?
+#     ______ ?.r__.NA.. __ e
+#         print('Exception retrieving SIG' ?
+#
+#     ___
+#         key_answer _ ?.r__.q.. ? 'KEY')
+#         print("KEY: " ?
+#     ______ ?.r__.NA.. __ e
+#         print('Exception retrieving KEY' ?
+#
+#     soa_answer _ ?.r__.q.. ? 'SOA'
+#     print("SOA Answer: ", ? 0 .mn..
+#     master_answer _ ?.r__.q.. ? 0 .mn.. 'A'
+#     print("Master Answer: " ? 0 .?
+#
+#
+# __ _______ __ ______
+#     parser _ ?.AP.. d.._'DNS Python'
+#     ?.a_a.. '--address' a.._"store" d.._"address"  d.._'dnspython.org'
+#     given_args _ ?.p_a..
+#     address _ ?.a..
+#     ? ?
+#
