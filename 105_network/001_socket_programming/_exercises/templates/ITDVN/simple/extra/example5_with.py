ae8d8bb73be2b9a2df68dbec6ee79d3ed334c73d
Grey <sergejyurskyj@yahoo.com>
2020-04-30 10:00:24 -0700

    Committed example5_with template

--- 105_network/001_socket_programming/_exercises/templates/ITDVN/simple/extra/example5_with.py
+++ 105_network/001_socket_programming/_exercises/templates/ITDVN/simple/extra/example5_with.py
@@ -1,12 +1,12 @@
-# with operator
-______  ?
-
-# __enter__ / __exit__
-# sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-with ?.?(?.A_I.., ?.S_D..) as sock:
-    print('8888 is bind')
-    ?.b..(('127.0.0.1', 8888))
-
-    w__ T..:
-        result _ ?.r..(1024)
-        print('Message', result.d..('utf-8'))
+# # with operator
+# ______  ?
+#
+# # __enter__ / __exit__
+# # sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# w__ ?.? ?.A_I.., ?.S_D.. __ sock
+#     print('8888 is bind')
+#     ?.b.. '127.0.0.1', 8888
+#
+#     w__ T..
+#         result _ ?.r.. 1024
+#         print('Message' ?.d.. 'utf-8'
