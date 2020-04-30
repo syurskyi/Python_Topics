a5313ef8f8c4568b3ebda66dde105d7c3219a819
Grey <sergejyurskyj@yahoo.com>
2020-04-30 09:10:02 -0700

    Committed 11_9_eve_basic_auth template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_9_eve_basic_auth.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_9_eve_basic_auth.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 11
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-____ eve ______ Eve
-____ eve.auth ______ BasicAuth
-
-c_ MyBasicAuth(BasicAuth):
-    ___ check_auth username, password, allowed_roles, resource,
-                   method):
-        r_ username __ 'admin' and password __ 'secret'
-
-___ run_server
-    app _ Eve(auth_MyBasicAuth)
-    app.r..
-
-__ _______ __ ______
-    run_server()
-
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 11
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+# ____ eve ______ E..
+# ____ ___.auth ______ BA..
+#
+# c_ MyBasicAuth BA..
+#     ___ check_auth username password allowed_roles resource
+#                    method
+#         r_ u.. __ 'admin' an. p.. __ 'secret'
+#
+# ___ run_server
+#     app _ E.. au.._MBA..
+#     ?.r..
+#
+# __ _______ __ ______
+#     ?
 
