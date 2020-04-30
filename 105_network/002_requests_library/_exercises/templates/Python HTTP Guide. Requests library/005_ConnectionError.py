e3ce7e24ca0b62800dcba877c9e4514f274ca9d3
Grey <sergejyurskyj@yahoo.com>
2020-04-30 10:21:58 -0700

    Committed Python HTTP Guide. Requests library template

--- 105_network/002_requests_library/_exercises/templates/Python HTTP Guide. Requests library/005_ConnectionError.py
+++ 105_network/002_requests_library/_exercises/templates/Python HTTP Guide. Requests library/005_ConnectionError.py
@@ -1,7 +1,7 @@
-______ r__
-___
-    response = ?.g..('http://urldoesnotexistforsure.bom')
-_____ ?.e__.ConnectionError:
-    print('Seems like dns lookup failed..')
-
-# Seems like dns lookup failed..
+# ______ r__
+# ___
+#     response _ ?.g..('http://urldoesnotexistforsure.bom')
+# _____ ?.e__.C..
+#     print('Seems like dns lookup failed..')
+#
+# # Seems like dns lookup failed..
