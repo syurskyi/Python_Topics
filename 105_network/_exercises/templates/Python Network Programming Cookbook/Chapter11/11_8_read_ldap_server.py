14c1005fcdc3807d7b118c524a02270693d92248
Grey <sergejyurskyj@yahoo.com>
2020-04-30 09:04:23 -0700

    Committed 11_7_query_ldap_server_b template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_8_read_ldap_server.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter11/11_8_read_ldap_server.py
@@ -8,8 +8,8 @@ ____ ldap3 ______ Server, Connection, ObjectDef, AttrDef, Reader, Writer, ALL
 
 
 ___ main
-    server _ Server('ipa.demo1.freeipa.org', get_info_ALL)
-    conn _ Connection(server, 'uid=admin,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org', 'Secret123', auto_bind_True)
+    server _ Server('ipa.demo1.freeipa.org', g_i_A..)
+    conn _ Connection(server, 'uid=admin,cn=users,cn=accounts,dc=demo1,dc=freeipa,dc=org', 'Secret123', a_b.._T..)
     person _ ObjectDef('person', conn)
     r _ Reader(conn, person, 'ou=ldap3-tutorial,dc=demo1,dc=freeipa,dc=org')
     print(r)
