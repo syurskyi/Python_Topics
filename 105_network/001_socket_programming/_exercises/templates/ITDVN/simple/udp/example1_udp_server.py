f0736cda1c96bbd9c2fc475a8743ba120d2b2189
Grey <sergejyurskyj@yahoo.com>
2020-04-30 10:02:50 -0700

    Committed example1_udp_server template

--- 105_network/001_socket_programming/_exercises/templates/ITDVN/simple/udp/example1_udp_server.py
+++ 105_network/001_socket_programming/_exercises/templates/ITDVN/simple/udp/example1_udp_server.py
@@ -1,8 +1,8 @@
-# example 1 (UDP server socket)
-______  ?
-
-sock _ ?.?(?.A_I.., ?.S_D..)
-?.b..(('', 8888))
-result _ ?.r..(1024)
-print('Message', result.d..('utf-8'))
-?.c..
+# # example 1 (UDP server socket)
+# ______  ?
+#
+# sock _ ?.? ?.A_I.. ?.S_D..
+# ?.b.. '' 8888
+# result _ ?.r.. 1024
+# print('Message', ?.d.. 'utf-8'
+# ?.c..
