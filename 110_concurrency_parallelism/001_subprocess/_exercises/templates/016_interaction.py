44600ec3f3b99001dcd46b008688b388fa574f0c
Grey <sergejyurskyj@yahoo.com>
2020-04-30 11:26:29 -0700

    Committed 016_interaction template

--- 110_concurrency_parallelism/001_subprocess/_exercises/templates/016_interaction.py
+++ 110_concurrency_parallelism/001_subprocess/_exercises/templates/016_interaction.py
@@ -1,69 +1,69 @@
-# interaction.py
-
-_____ io
-_____ ?
-
-print('One line at a time:')
-proc _ ?.P..(
-    'python3 repeater.py',
-    s.._T..,
-    s_i._?.P..,
-    s_o__?.P..,
-)
-s_i. _ io.TextIOWrapper(
-    proc.s_i.,
-    encoding_'utf-8',
-    line_buffering_T..,  # send data on newline
-)
-s_o_ _ io.TextIOWrapper(
-    proc.s_o_,
-    encoding_'utf-8',
-)
-___ i __ range(5):
-    line _ '@\n'.f..(i)
-    s_i..w..(line)
-    output _ s_o_.readline()
-    print(output.rstrip())
-remainder _ proc.c..()[0].d..('utf-8')
-print(remainder)
-
-print()
-print('All output at once:')
-proc _ ?.P..(
-    'python3 repeater.py',
-    s.._T..,
-    s_i._?.P..,
-    s_o__?.P..,
-)
-s_i. _ io.TextIOWrapper(
-    proc.s_i.,
-    encoding_'utf-8',
-)
-___ i __ range(5):
-    line _ '@\n'.f..(i)
-    s_i..w..(line)
-s_i..f..
-
-output _ proc.c..()[0].d..('utf-8')
-print(output)
-
-# $ python3 -u interaction.py
+# # interaction.py
 #
-# One line at a time:
-# repeater.py: starting
-# 0
-# 1
-# 2
-# 3
-# 4
-# repeater.py: exiting
+# _____ i.
+# _____ ?
 #
+# print('One line at a time:')
+# proc _ ?.P..
+#     'python3 repeater.py',
+#     s.._T..
+#     s_i._?.P..
+#     s_o__?.P..
+# )
+# s_i. _ ?.TW..
+#     ?.s_i.
+#     e.._'utf-8'
+#     l_b.._T..  # send data on newline
+# )
+# s_o_ _ ?.TW..
+#     ?.s_o_
+#     e.._'utf-8',
+# )
+# ___ i __ ra.. 5
+#     line _ '@\n'.f.. ?
+#     s_i..w.. ?
+#     output _ s_o_.r_l..
+#     print ?.rs..
+# remainder _ ?.c.. 0 .d.. 'utf-8'
+# print ?
 #
-# All output at once:
-# repeater.py: starting
-# repeater.py: exiting
-# 0
-# 1
-# 2
-# 3
-# 4
+# print()
+# print('All output at once:')
+# proc _ ?.P..
+#     'python3 repeater.py',
+#     s.._T..
+#     s_i._?.P..
+#     s_o__?.P..
+# )
+# s_i. _ ?.TW..
+#     ?.s_i.
+#     e.._'utf-8'
+# )
+# ___ i __ ra.. 5
+#     line _ '@\n'.f.. ?
+#     s_i..w.. ?
+# s_i..f..
+#
+# output _ ?.c.. 0 .d.. 'utf-8'
+# print ?
+#
+# # $ python3 -u interaction.py
+# #
+# # One line at a time:
+# # repeater.py: starting
+# # 0
+# # 1
+# # 2
+# # 3
+# # 4
+# # repeater.py: exiting
+# #
+# #
+# # All output at once:
+# # repeater.py: starting
+# # repeater.py: exiting
+# # 0
+# # 1
+# # 2
+# # 3
+# # 4
