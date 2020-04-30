f28fbb39480fedad4a22f86ea1c3950aadfaa946
Grey <sergejyurskyj@yahoo.com>
2020-04-29 20:27:50 -0700

    Committed 7_3a_xmlrpc_server_with_http_auth template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter07/7_3a_xmlrpc_server_with_http_auth.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter07/7_3a_xmlrpc_server_with_http_auth.py
@@ -12,64 +12,64 @@ ______ xmlrpc
 #import xmlrpclib
 ____ base64 ______ b64decode
 
-____ xmlrpc.s.. ______ SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
+____ xmlrpc.s.. ______ SimpleXMLRPCServer   SimpleXMLRPCRequestHandler
 # Comment out the above line and uncomment the below line for Python 2.x.
 #from SimpleXMLRPCServer  import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
 
 
-c_ SecureXMLRPCServer(SimpleXMLRPCServer):
+c_ SecureXMLRPCServer S_S..
 
-    ___ -  host, port, username, password, $, **kargs):
-        username _ username
-        password _ password
+    ___ -  host   port   username   password   $ $$
+        u.. _ u..
+        p.. _ p..
         # authenticate method is called from inner class
-        c_ VerifyingRequestHandler(SimpleXMLRPCRequestHandler):
+        c_ VerifyingRequestHandler(S_RH..
               # method to override
-              ___ parse_request(request):
-                  __ SimpleXMLRPCRequestHandler.parse_request(request):
+              ___ p_r.. r..
+                  __ S_RH__.p_r.. r..
                       # authenticate
-                      __ authenticate(r__.headers):
+                      __ authenticate r__.h..
                           r_ T..
                       ____
                           # if authentication fails return 401
-                          request.send_error(401, 'Authentication failed, Try agin.')
+                          r__.s_e.. 401 'Authentication failed, Try agin.'
                   r_ F..
         # initialize
-        SimpleXMLRPCServer.-  (host, port), requestHandler_VerifyingRequestHandler, $, **kargs)
+        S_S__.-  h.. p.. rH.._VRH.. $ $$
 
-    ___ authenticate headers):
-        headers _ headers.get('Authorization').s..()
-        basic, encoded _ headers[0], headers[1]
-        __ basic !_ 'Basic':
+    ___ authenticate headers
+        headers _ ?.g.. 'Authorization' .s..
+        basic   encoded _ ? 0 ? 1
+        __ b.. !_ 'Basic'
             print ('Only basic authentication supported')
             r_ F..
-        secret _ b64decode(encoded).s..(b':')
+        secret _ b6_d.. e.. .s.. _':'
         
-        username, password _ secret[0].d..("utf-8"), secret[1].d..("utf-8")
-        r_ T.. __ (username __ username and password __ password) ____ F..
+        username  password _ ? 0 .d.. "utf-8" ? 1 .d.. "utf-8"
+        r_ T.. __ ? __ ? an. ? __ ? ____ F..
     
 
-___ run_server(host, port, username, password):
-    server _ SecureXMLRPCServer(host, port, username, password)
+___ run_server host  port  username  password
+    server _ S_S.. ? ? ? ?
     # simple test function
-    ___ echo(msg):
+    ___ echo msg
         """Reply client in  uppser case """
-        reply _ msg.upper()
-        print ("Client said: @. So we echo that in uppercase: @" (msg, reply))
-        r_ reply
-    server.register_function(echo, 'echo')
-    print ("Running a HTTP auth enabled XMLRPC server on @:@..." (host, port))
-    server.serve_forever()
+        reply _ ?.u..
+        print ("Client said: @. So we echo that in uppercase: @" ? ?
+        r_ ?
+    s__.r_f.. e.. 'echo'
+    print ("Running a HTTP auth enabled XMLRPC server on @:@..." h.. p..
+    s__.s_f..
 
 
 __ _______ __ ______
-    parser _ ?.AP..(d.._'Multithreaded multicall XMLRPC Server/Proxy')
-    ?.a_a..('--host', a.._"store", d.._"host", d.._'localhost')
-    ?.a_a..('--port', a.._"store", d.._"port", d.._8000, ty.._in.)
-    ?.a_a..('--username', a.._"store", d.._"username", d.._'user')
-    ?.a_a..('--password', a.._"store", d.._"password", d.._'pass')
+    parser _ ?.AP.. d.._'Multithreaded multicall XMLRPC Server/Proxy'
+    ?.a_a.. '--host'  a.._"store"  d.._"host"  d.._'localhost'
+    ?.a_a.. '--port'  a.._"store"  d.._"port"  d.._8000  ty.._in.
+    ?.a_a.. '--username'  a.._"store"  d.._"username"  d.._'user'
+    ?.a_a.. '--password'  a.._"store"  d.._"password"  d.._'pass'
     # parse arguments
     given_args _ ?.p_a..
-    host, port _  given_args.host, given_args.port
-    username, password _ given_args.username, given_args.password
-    run_server(host, port, username, password)
+    host  port _  ?.?  ?.?
+    username  password _ ?.?  ?.?
+    ? h.. p.. u.. p..
