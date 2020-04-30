38b16e2d0e1c6e211cd36f067e0237d0b0d50680
Grey <sergejyurskyj@yahoo.com>
2020-04-30 11:13:14 -0700

    Committed 008_Suppressing Output template

--- 110_concurrency_parallelism/001_subprocess/_exercises/templates/008_Suppressing Output.py
+++ 110_concurrency_parallelism/001_subprocess/_exercises/templates/008_Suppressing Output.py
@@ -1,23 +1,23 @@
-# subprocess_run_output_error_suppress.py
-
-_____ ?
-
-___
-    completed _ ?.r..(
-        'echo to stdout; echo to s_e.. 1>&2; exit 1',
-        s.._T..,
-        s_o__?.DEVNULL,
-        s_e.._?.DEVNULL,
-    )
-_____ ?.C.. __ err:
-    print('ERROR:', err)
-____
-    print('returncode:', completed.r_c..)
-    print('stdout is {!r}'.f..(completed.s_o_))
-    print('s_e.. is {!r}'.f..(completed.s_e..))
-
-# $ python3 subprocess_run_output_error_suppress.py
+# # subprocess_run_output_error_suppress.py
 #
-# returncode: 1
-# stdout is None
-# s_e.. is None
+# _____ ?
+#
+# ___
+#     completed _ ?.r..
+#         'echo to stdout; echo to s_e.. 1>&2; exit 1',
+#         s.._T..
+#         s_o__?.D..
+#         s_e.._?.D..
+#     )
+# _____ ?.C.. __ err
+#     print('ERROR:' ?
+# ____
+#     print('returncode:', ?.r_c..
+#     print('stdout is @'.f..(?.s_o_
+#     print('s_e.. is @'.f..(?.s_e..
+#
+# # $ python3 subprocess_run_output_error_suppress.py
+# #
+# # returncode: 1
+# # stdout is None
+# # s_e.. is None
