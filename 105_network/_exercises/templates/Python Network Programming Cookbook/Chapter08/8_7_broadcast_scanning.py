a7506be2d6c574d461ad924ea3efeca29b5fc662
Grey <sergejyurskyj@yahoo.com>
2020-04-29 21:17:57 -0700

    Committed 8_7_broadcast_scanning template

--- 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter08/8_7_broadcast_scanning.py
+++ 105_network/_exercises/templates/Python Network Programming Cookbook/Chapter08/8_7_broadcast_scanning.py
@@ -1,33 +1,33 @@
-#!/usr/bin/env python
-# Python Network Programming Cookbook, Second Edition -- Chapter - 8
-# This program is optimized for Python 2.7.12 and Python 3.5.2.
-# It may run on any other version with/without modifications.
-
-
-____ scapy.all ______ *
-______ __
-captured_data _ dict()
-
-END_PORT _ 1000
- 
-___ monitor_packet(pkt):
-    __ IP __ pkt:
-        __ pkt[IP].src no. __ captured_data:
-            captured_data[pkt[IP].src] _ []
- 
-    __ TCP __ pkt:
-        __ pkt[TCP].sport <_  END_PORT:
-            __ no. st..(pkt[TCP].sport) __ captured_data[pkt[IP].src]:
-                captured_data[pkt[IP].src].ap..(st..(pkt[TCP].sport))
- 
-    __.system('clear')
-    ip_list _ sorted(captured_data.keys())
-    ___ key __ ip_list:
-        ports_', '.j..(captured_data[key])
-        __ le. (captured_data[key]) __ 0:
-            print ('@'  key)
-        ____
-            print ('@ (@)'  (key, ports))
-
-__ _______ __ ______
-    sniff(prn_monitor_packet, store_0)
+# #!/usr/bin/env python
+# # Python Network Programming Cookbook, Second Edition -- Chapter - 8
+# # This program is optimized for Python 2.7.12 and Python 3.5.2.
+# # It may run on any other version with/without modifications.
+#
+#
+# ____ scapy.all ______ _
+# ______ __
+# captured_data _ di..
+#
+# END_PORT _ 1000
+#
+# ___ monitor_packet pkt
+#     __ IP __ ?
+#         __ ? I. .sr. no. __ c_d..
+#             c_d.. ? I. .s.. _  # list
+#
+#     __ TCP __ ?
+#         __ ? T. .s.. <_  E..
+#             __ no. st..(? T. .sp.. __ c_d.. ? I. .sr.
+#                 c_d.. ? I. .sr. .ap.. st.. ? T. .sp..
+#
+#     __.sy.. 'clear'
+#     ip_list _ so.. c_d_.k..
+#     ___ key __ ?
+#         ports_', '.j.. c_d.. ?
+#         __ le.  c_d.. ? __ 0
+#             print ('@'  ?
+#         ____
+#             print ('@ (@)' k.. p..
+#
+# __ _______ __ ______
+#     sn.. prn_m_p.. st.._0)
