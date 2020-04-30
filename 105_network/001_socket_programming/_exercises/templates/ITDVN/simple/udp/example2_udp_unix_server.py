3fcf2015a8c97e54eea221dd5d539a826fcf822e
Grey <sergejyurskyj@yahoo.com>
2020-04-30 10:05:19 -0700

    Committed example2_udp_unix_server template

--- 105_network/001_socket_programming/_exercises/templates/ITDVN/simple/udp/example2_udp_unix_server.py
+++ 105_network/001_socket_programming/_exercises/templates/ITDVN/simple/udp/example2_udp_unix_server.py
@@ -1,20 +1,20 @@
-# example 2 (UDP unix socket)
-______  os
-______  ?
-
-unix_sock_name _ 'unix.sock'
-sock _ ?.?(?.AF_UNIX, ?.S_D..)
-
-if os.path.exists(unix_sock_name):
-    os.remove(unix_sock_name)
-
-?.b..(unix_sock_name)
-
-w__ T..:
-    ___
-        result _ ?.r..(1024)
-    _____ K..
-        ?.c..
-        b..
-    ____
-        print('Message', result.d..('utf-8'))
+# # example 2 (UDP unix socket)
+# ______  __
+# ______  ?
+#
+# unix_sock_name _ 'unix.sock'
+# sock _ ?.? ?.A_U.. ?.S_D..
+#
+# __ __.pa__.ex.. u..
+#     __.r.. u..
+#
+# ?.b.. u..
+#
+# w__ T..
+#     ___
+#         result _ ?.r.. 1024
+#     _____ K..
+#         ?.c..
+#         b..
+#     ____
+#         print('Message' ?.d.. 'utf-8'
