865c514300c2220706e1c1c6cda96c6231a21a1e
Grey <sergejyurskyj@yahoo.com>
2020-04-30 10:12:29 -0700

    Committed twisted_server template

--- 105_network/001_socket_programming/_exercises/templates/ITDVN/socket_twisted/twisted_server.py
+++ 105_network/001_socket_programming/_exercises/templates/ITDVN/socket_twisted/twisted_server.py
@@ -1,36 +1,36 @@
-____ tw__.internet ______  reactor, protocol, endpoints
-____ tw__.internet.protocol ______  connectionDone
-
-
-c_ ProcessClient(protocol.Protocol):
-
-    ___ - (self, server):
-        self.server _ server
-
-    ___ connectionMade
-        print('Client connected...')
-        self.server.concurrentClientCount +_ 1
-
-    ___ connectionLost(self, reason_connectionDone):
-        self.server.concurrentClientCount -_ 1
-
-    ___ dataReceived(self, data: str):
-        data _ data.strip()
-        print('Data: ', data)
-        self.transport.write(data)
-
-
-c_ Server(protocol.Factory):
-    commands _ ('init', 'send', 'get', 'close')
-
-    ___ -
-        self.concurrentClientCount _ 0
-        self.database _ {}
-
-    ___ buildProtocol(self, addr):
-        r_ ProcessClient(self)
-
-
-server _ endpoints.serverFromString(reactor, 'tcp:8888')
-server.l..(Server())
-reactor.run()
+# ____ tw__.i__ ______  r.. p.. e..
+# ____ tw__.i__.p.. ______  cD..
+#
+#
+# c_ ProcessClient p__.P..
+#
+#     ___ - server
+#         ? ?
+#
+#     ___ connectionMade
+#         print('Client connected...')
+#         ?.cCC.. +_ 1
+#
+#     ___ connectionLost reason_connectionDone
+#         ?.cCC.. -_ 1
+#
+#     ___ dataReceived data st.
+#         data _ ?.st..
+#         print('Data: ' ?
+#         t__.w.. ?
+#
+#
+# c_ Server p__.F..
+#     commands _ ('init', 'send', 'get', 'close')
+#
+#     ___ -
+#         cCC.. _ 0
+#         database _    # dict
+#
+#     ___ buildProtocol addr
+#         r_ ?
+#
+#
+# server _ e__.sFS.. r.. 'tcp:8888'
+# ?.l.. S..
+# r__.r..
