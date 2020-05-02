8f0447b0ee2b3079cbdd01c3a13357f2274aab55
Grey <sergejyurskyj@yahoo.com>
2020-05-01 18:53:04 -0700

    Committed 005_customsignal template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/004_Events and signals/005_customsignal.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/004_Events and signals/005_customsignal.py
@@ -1,35 +1,35 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-______ ___
-____ ?.?C.. ______ pyqtSignal, QObject
-____ ?.?W.. ______ ?M.., ?A..
-
-
-c_ Communicate(QObject):
-    closeApp _ pyqtSignal
-
-
-c_ Example(?M..):
-
-    ___ -
-        s__ .-
-
-        ?
-
-    ___ initUI
-        c _ Communicate
-        c.closeApp.c..(close)
-
-        sG__(300, 300, 290, 150)
-        sWT__('Emit signal')
-        show
-
-    ___ mousePressEvent(self, event):
-        c.closeApp.emit
-
-
-__ _____ __ _______
-    app _ ?A..(___.argv)
-    ex _ Example
-    ___.e..(app.e..())
+# #!/usr/bin/python3
+# # -*- coding: utf-8 -*-
+#
+# ______ ___
+# ____ ?.?C.. ______ pyqtS.. ?O..
+# ____ ?.?W.. ______ ?M.., ?A..
+#
+#
+# c_ Communicate ?O..
+#     closeApp _ pyqtS..
+#
+#
+# c_ Example ?M..
+#
+#     ___ -
+#         s__ .-
+#
+#         ?
+#
+#     ___ initUI
+#         c _ C..
+#         ?.cA__.c.. c..
+#
+#         sG__ 300, 300, 290, 150
+#         sWT__('Emit signal')
+#         s..
+#
+#     ___ mousePressEvent event
+#         ?.cA__.e..
+#
+#
+# __ _____ __ _______
+#     app _ ?A..
+#     ex _ ?
+#     ___.e.. ?.e..
