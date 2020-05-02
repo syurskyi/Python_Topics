fb8599de37725c00e8b778e5abbb47124516aec3
Grey <sergejyurskyj@yahoo.com>
2020-05-01 18:51:36 -0700

    Committed 005_Connect_of_signal_QSlider_to_slotQLCDNumber template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/004_Events and signals/005_Connect_of_signal_QSlider_to_slotQLCDNumber.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/004_Events and signals/005_Connect_of_signal_QSlider_to_slotQLCDNumber.py
@@ -1,35 +1,35 @@
-______ ___
-____ ?.?C.. ______ __
-____ ?.?W.. ______ *
-
-
-c_ Example(W..):
-
-    ___ -
-        s__ ?  .-
-
-        ?
-
-    ___ initUI
-
-        lcd _ ?LCDN..(
-        sld _ ?S..(__.H..,
-
-        vbox _ ?VB..
-        vbox.aW..(lcd)
-        vbox.aW..(sld)
-
-        sL..(vbox)
-        sld.vC__.c..(lcd.display)
-
-        sG__(300, 300, 250, 150)
-        sWT__('Signal & slot')
-        show
-
-
-___ start :
-    #global form
-    start.form _ Example
-    start.form.show
-
-start
+# ______ ___
+# ____ ?.?C.. ______ __
+# ____ ?.?W.. ______ _
+#
+#
+# c_ Example W..
+#
+#     ___ -
+#         s__ ?  .-
+#
+#         ?
+#
+#     ___ initUI
+#
+#         lcd _ ?LCDN..
+#         sld _ ?S.. __.H..
+#
+#         vbox _ ?VB..
+#         vbox.aW.. l..
+#         vbox.aW.. s..
+#
+#         sL.. v..
+#         s__.vC__.c.. l__.d..
+#
+#         sG__ 300, 300, 250, 150
+#         sWT__ 'Signal & slot'
+#         s..
+#
+#
+# ___ start
+#     #global form
+#     ?.f.. _ ?
+#     ?.f...s..
+#
+# ?
