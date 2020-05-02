b8b1d76f10e8fe160cd429df7bda83c8ece5c443
Grey <sergejyurskyj@yahoo.com>
2020-05-01 19:07:35 -0700

    Committed 004_filedialog template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/005_Dialogs/new/004_filedialog.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/005_Dialogs/new/004_filedialog.py
@@ -1,49 +1,49 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-____ ?.?W.. ______ (?M.., QTextEdit,
-                             QAction, QFileDialog, ?A..)
-____ ?.?G__ ______ QIcon
-______ ___
-
-
-c_ Example(?M..):
-
-    ___ -
-        s__ .-
-
-        ?
-
-    ___ initUI
-        textEdit _ QTextEdit
-        setCentralWidget(textEdit)
-        sB__
-
-        openFile _ QAction(QIcon('open.png'), 'Open',
-        openFile.setShortcut('Ctrl+O')
-        openFile.setStatusTip('Open new File')
-        openFile.triggered.c..(sD..)
-
-        menubar _ menuBar
-        fileMenu _ menubar.addMenu('&File')
-        fileMenu.addAction(openFile)
-
-        sG__(300, 300, 350, 300)
-        sWT__('File dialog')
-        show
-
-    ___ showDialog
-        fname _ QFileDialog.getOpenFileName(self, 'Open file', '/home')
-
-        __ fname[0]:
-            f _ open(fname[0], 'r')
-
-            with f:
-                data _ f.read
-                textEdit.sT..(data)
-
-
-__ _____ __ _______
-    app _ ?A..(___.argv)
-    ex _ Example
-    ___.e..(app.e..())
+# #!/usr/bin/python3
+# # -*- coding: utf-8 -*-
+#
+# ____ ?.?W.. ______ ?M.. ?TE..,
+#                              ?A.., ?FD.. ?A..
+# ____ ?.?G__ ______ ?I..
+# ______ ___
+#
+#
+# c_ Example ?M..
+#
+#     ___ -
+#         s__ .-
+#
+#         ?
+#
+#     ___ initUI
+#         textEdit _ ?TE..
+#         sCW.. ?
+#         sB__
+#
+#         openFile _ ?A.. ?I.. 'open.png' 'Open'
+#         ?.sS.. 'Ctrl+O'
+#         ?.sST.. 'Open new File'
+#         ?.t__.c.. sD..
+#
+#         menubar _ mB..
+#         fileMenu _ ?.aM.. '&File'
+#         ?.aA.. ?
+#
+#         sG__ 300 300 350 300
+#         sWT__'File dialog'
+#         s..
+#
+#     ___ showDialog
+#         fname _ ?FD...gOFN..  'Open file' '/home'
+#
+#         __ ? 0
+#             f _ o.. ? 0  _
+#
+#             w__ ?
+#                 data _ ?.r..
+#                 tE__.sT.. ?
+#
+#
+# __ _____ __ _______
+#     app _ ?A..
+#     ex _ ?
+#     ___.e.. ?.e..
