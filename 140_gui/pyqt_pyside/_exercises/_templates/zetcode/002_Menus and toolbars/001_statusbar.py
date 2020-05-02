504739103c88797b1a6d268c06dc872b64fe438b
Grey <sergejyurskyj@yahoo.com>
2020-05-01 18:05:54 -0700

    Committed 001_statusbar template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/002_Menus and toolbars/001_statusbar.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/002_Menus and toolbars/001_statusbar.py
@@ -1,32 +1,32 @@
-import sys
-from PySide2.QtWidgets import QMainWindow, QApplication
-
-
-class Example(QMainWindow):
-    def __init__(self):
-        super(Example, self).__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        self.statusBar().showMessage('Ready')
-
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Statusbar')
-        self.show()
-
-
-if __name__ == '__main__':
-    import sys
-
-    app = None
-    try:
-        import nuke
-    except ImportError:
-        app = QApplication(sys.argv)
-    main = Example()
-    main.show()
-
-    if app is not None:
-        app.exec_()
-
+# ______ ___
+# ____ ?.?W.. ______ QM.. QA..
+#
+#
+# c_ Example QM..
+#     ___ -
+#         s__ ?  .-
+#
+#         ?
+#
+#     ___ initUI
+#         sB__ .sM.. 'Ready'
+#
+#         sG__(300, 300, 250, 150)
+#         sWT__ 'Statusbar'
+#         s..
+#
+#
+# __ _____ __ _______
+#     ______ ___
+#
+#     app = N..
+#     ___
+#         ______ n..
+#     ______ I..
+#         ? ?
+#     main _ ?
+#     ?.s..
+#
+#     __ ? __ no. N..
+#         ?.e.._
+#
