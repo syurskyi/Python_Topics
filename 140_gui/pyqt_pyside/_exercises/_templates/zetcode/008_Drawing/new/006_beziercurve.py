f6ea092239ece81f7b6de40a11bf28a92adcff93
Grey <sergejyurskyj@yahoo.com>
2020-05-02 13:29:03 -0700

    Committed 006_beziercurve template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/008_Drawing/new/006_beziercurve.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/008_Drawing/new/006_beziercurve.py
@@ -1,40 +1,40 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-____ ?.?W.. ______ W.., ?A..
-____ ?.?G__ ______ ?P.., QPainterPath
-____ ?.?C.. ______ __
-______ ___
-
-
-c_ Example(W..):
-
-    ___ -
-        s__ .-
-
-        ?
-
-    ___ initUI
-        sG__(300, 300, 380, 250)
-        sWT__('Bézier curve')
-        s..
-
-    ___ paintEvent(self, e):
-        qp _ ?P..
-        qp.b..(
-        qp.setRenderHint(?P...Antialiasing)
-        drawBezierCurve(qp)
-        qp.end
-
-    ___ drawBezierCurve(self, qp):
-        path _ QPainterPath
-        path.moveTo(30, 30)
-        path.cubicTo(30, 30, 200, 350, 350, 30)
-
-        qp.drawPath(path)
-
-
-__ _____ __ _______
-    app _ ?A..(___.argv)
-    ex _ Example
-    ___.e..(app.e..())
+# #!/usr/bin/python3
+# # -*- coding: utf-8 -*-
+#
+# ____ ?.?W.. ______ W.. ?A..
+# ____ ?.?G__ ______ ?P.. ?PP..
+# ____ ?.?C.. ______ __
+# ______ ___
+#
+#
+# c_ Example W..
+#
+#     ___ -
+#         s__ .-
+#
+#         ?
+#
+#     ___ initUI
+#         sG__ 300  300  380  250
+#         sWT__ 'Bézier curve'
+#         s..
+#
+#     ___ paintEvent self  e
+#         qp _ ?P..
+#         ?.b..
+#         ?.sRH.. ?P...Antialiasing
+#         ? ?
+#         ?.e..
+#
+#     ___ drawBezierCurve  qp
+#         path _ ?PP..
+#         ?.mT. 30  30
+#         ?.cT. 30  30  200  350  350  30
+#
+#         ?.dP.. ?
+#
+#
+# __ _____ __ _______
+#     app _ ?A..
+#     ex _ ?
+#     ___.e.. ?.e..
