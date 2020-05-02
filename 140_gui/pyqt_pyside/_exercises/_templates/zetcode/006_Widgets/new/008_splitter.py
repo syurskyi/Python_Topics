979632b73bd012a2721130338af3fed803fc7854
Grey <sergejyurskyj@yahoo.com>
2020-05-02 12:40:46 -0700

    Committed 008_splitter template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/006_Widgets/new/008_splitter.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/006_Widgets/new/008_splitter.py
@@ -1,49 +1,49 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-
-____ ?.?W.. ______ (W.., ?HB.., ?F.,
-                             QSplitter, QStyleFactory, ?A..)
-____ ?.?C.. ______ __
-______ ___
-
-
-c_ Example(W..):
-
-    ___ -
-        s__ .-
-
-        ?
-
-    ___ initUI
-        hbox _ ?HB..(
-
-        topleft _ ?F.(
-        topleft.setFrameShape(?F..StyledPanel)
-
-        topright _ ?F.(
-        topright.setFrameShape(?F..StyledPanel)
-
-        bottom _ ?F.(
-        bottom.setFrameShape(?F..StyledPanel)
-
-        splitter1 _ QSplitter(__.H..)
-        splitter1.aW..(topleft)
-        splitter1.aW..(topright)
-
-        splitter2 _ QSplitter(__.Vertical)
-        splitter2.aW..(splitter1)
-        splitter2.aW..(bottom)
-
-        hbox.aW..(splitter2)
-        sL..(hbox)
-
-        sG__(300, 300, 300, 200)
-        sWT__('QSplitter')
-        show
-
-
-__ _____ __ _______
-    app _ ?A..(___.argv)
-    ex _ Example
-    ___.e..(app.e..())
+# #!/usr/bin/python3
+# # -*- coding: utf-8 -*-
+#
+#
+# ____ ?.?W.. ______ W.. ?HB.. ?F.
+#                              ?S.. ?SF.. ?A..
+# ____ ?.?C.. ______ __
+# ______ ___
+#
+#
+# c_ Example W..
+#
+#     ___ -
+#         s__ .-
+#
+#         ?
+#
+#     ___ initUI
+#         hbox _ ?HB..
+#
+#         topleft _ ?F.
+#         ?.sFS.. ?F..aS..
+#
+#         topright _ ?F.
+#         ?.sFS.. ?F..aS..
+#
+#         bottom _ ?F.
+#         ?.sFS.. ?F..aS..
+#
+#         splitter1 _ ?S.. __.H..
+#         ?.aW.. t_l..
+#         ?.aW.. t_r..
+#
+#         splitter2 _ ?S.. __.V..
+#         ?.aW.. _1
+#         ?.aW.. b..
+#
+#         h__.aW.. _2
+#         sL.. h..
+#
+#         sG__ 300 300 300 200
+#         sWT__('QSplitter')
+#         s..
+#
+#
+# __ _____ __ _______
+#     app _ ?A..
+#     ex _ ?
+#     ___.e.. ?.e..
