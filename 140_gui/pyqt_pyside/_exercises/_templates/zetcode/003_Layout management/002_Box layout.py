7ed8ff964a1de32fb6e489342f8ac2f194655629
Grey <sergejyurskyj@yahoo.com>
2020-05-01 18:28:53 -0700

    Committed 002_Box layout template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/003_Layout management/002_Box layout.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/003_Layout management/002_Box layout.py
@@ -1,44 +1,44 @@
-import sys
-from PySide2.QtWidgets import (QWidget, QPushButton,
-    QHBoxLayout, QVBoxLayout, QApplication)
-
-class Example(QWidget):
-
-    def __init__(self):
-        super(Example, self).__init__()
-
-        self.initUI()
-
-    def initUI(self):
-
-        okButton = QPushButton("OK")
-        cancelButton = QPushButton("Cancel")
-
-        hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(okButton)
-        hbox.addWidget(cancelButton)
-
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-
-        self.setLayout(vbox)
-
-        self.setGeometry(300, 300, 300, 150)
-        self.setWindowTitle('Buttons')
-        self.show()
-
-if __name__ == '__main__':
-    import sys
-
-    app = None
-    try:
-        import nuke
-    except ImportError:
-        app = QApplication(sys.argv)
-    main = Example()
-    main.show()
-
-    if app is not None:
-        app.exec_()
+# ______ ___
+# ____ PySide2.?W.. ______ (W.., ?P..,
+#     ?HB.., ?VB.., QApplication)
+#
+# c_ Example W..
+#
+#     ___ -
+#         s__ ?  .-
+#
+#         ?
+#
+#     ___ initUI
+#
+#         okButton _ ?P.. "OK"
+#         cancelButton _ ?P.. "Cancel"
+#
+#         hbox _ ?HB..
+#         ?.aS.. 1
+#         ?.aW.. ?
+#         ?.aW.. ?
+#
+#         vbox _ ?VB..
+#         ?.aS.. 1
+#         ?.aL.. h..
+#
+#         sL.. v..
+#
+#         sG__ 300, 300, 300, 150
+#         sWT__ 'Buttons'
+#         show
+#
+# __ _____ __ _______
+#     ______ ___
+#
+#     app _ N..
+#     ___
+#         ______ n..
+#     ______ I..
+#         ? ?
+#     main _ ?
+#     ?.s..
+#
+#     __ ? __ no. N..
+#         ?.e..
