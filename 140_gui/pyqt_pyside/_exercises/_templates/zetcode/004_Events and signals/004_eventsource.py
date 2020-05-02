2a9b569341ec1760ab59ba91dc01001fae2fc352
Grey <sergejyurskyj@yahoo.com>
2020-05-01 18:50:14 -0700

    Committed 004_eventsource template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/004_Events and signals/004_eventsource.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/004_Events and signals/004_eventsource.py
@@ -1,39 +1,39 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-______ ___
-____ ?.?W.. ______ ?M.., ?P.., ?A..
-
-
-c_ Example(?M..):
-
-    ___ -
-        s__ .-
-
-        ?
-
-    ___ initUI
-        btn1 _ ?P..("Button 1",
-        btn1.m..(30, 50)
-
-        btn2 _ ?P..("Button 2",
-        btn2.m..(150, 50)
-
-        btn1.c__.c..(buttonClicked)
-        btn2.c__.c..(buttonClicked)
-
-        sB__
-
-        sG__(300, 300, 290, 150)
-        sWT__('Event sender')
-        show
-
-    ___ buttonClicked
-        sender _ sender
-        sB__ .sM..(sender.text  + ' was pressed')
-
-
-__ _____ __ _______
-    app _ ?A..(___.argv)
-    ex _ Example
-    ___.e..(app.e..())
+# #!/usr/bin/python3
+# # -*- coding: utf-8 -*-
+#
+# ______ ___
+# ____ ?.?W.. ______ ?M.. ?P.. ?A..
+#
+#
+# c_ Example ?M..
+#
+#     ___ -
+#         s__ .-
+#
+#         ?
+#
+#     ___ initUI
+#         btn1 _ ?P.. "Button 1"
+#         ?.m.. 30  50
+#
+#         btn2 _ ?P.. "Button 2"
+#         ?.m.. 150 50
+#
+#         ?.c__.c.. bC..
+#         ?.c__.c.. bC..
+#
+#         sB__
+#
+#         sG__(300, 300, 290, 150)
+#         sWT__('Event sender')
+#         s..
+#
+#     ___ buttonClicked
+#         sender _ s..
+#         sB__ .sM.. ?.t..  + ' was pressed')
+#
+#
+# __ _____ __ _______
+#     app _ ?A..
+#     ex _ ?
+#     ___.e.. ?.e..
