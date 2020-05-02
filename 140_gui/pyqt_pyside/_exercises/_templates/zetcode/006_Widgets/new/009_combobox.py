2a402cc60e12b98ddd7e649b5e3308652a88cb1d
Grey <sergejyurskyj@yahoo.com>
2020-05-02 12:43:17 -0700

    Committed 009_combobox template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/006_Widgets/new/009_combobox.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/006_Widgets/new/009_combobox.py
@@ -1,44 +1,44 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-
-____ ?.?W.. ______ (W.., ?L..,
-                             QComboBox, ?A..)
-______ ___
-
-
-c_ Example(W..):
-
-    ___ -
-        s__ .-
-
-        ?
-
-    ___ initUI
-        lbl _ ?L..("Ubuntu",
-
-        combo _ QComboBox(
-        combo.addItem("Ubuntu")
-        combo.addItem("Mandriva")
-        combo.addItem("Fedora")
-        combo.addItem("Arch")
-        combo.addItem("Gentoo")
-
-        combo.m..(50, 50)
-        lbl.m..(50, 150)
-
-        combo.activated[st.].c..(onActivated)
-
-        sG__(300, 300, 300, 200)
-        sWT__('QComboBox')
-        show
-
-    ___ onActivated(self, text):
-        lbl.sT..(text)
-        lbl.aS..
-
-
-__ _____ __ _______
-    app _ ?A..(___.argv)
-    ex _ Example
-    ___.e..(app.e..())
+# #!/usr/bin/python3
+# # -*- coding: utf-8 -*-
+#
+#
+# ____ ?.?W.. ______ W.. ?L..
+#                              ?CB.., ?A..
+# ______ ___
+#
+#
+# c_ Example(W..):
+#
+#     ___ -
+#         s__ .-
+#
+#         ?
+#
+#     ___ initUI
+#         lbl _ ?L.. "Ubuntu",
+#
+#         combo _ ?CB..
+#         ?.aI.. "Ubuntu"
+#         ?.aI.. "Mandriva"
+#         ?.aI.. "Fedora"
+#         ?.aI.. "Arch"
+#         ?.aI.. "Gentoo"
+#
+#         ?.m.. 50 50
+#         l__.m.. 50 150
+#
+#         ?.ac..|st. .c.. ?
+#
+#         sG__ 300 300 300 200
+#         sWT__ '?CB..'
+#         s..
+#
+#     ___ onActivated text
+#         l__.sT.. ?
+#         l__.aS..
+#
+#
+# __ _____ __ _______
+#     app _ ?A..
+#     ex _ ?
+#     ___.e.. ?.e..
