69d7d958d756048a44fe552dc38cc749427932b6
Grey <sergejyurskyj@yahoo.com>
2020-05-01 18:42:25 -0700

    Committed 002_escape template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/004_Events and signals/002_escape.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/004_Events and signals/002_escape.py
@@ -1,29 +1,29 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-______ ___
-____ ?.?C.. ______ __
-____ ?.?W.. ______ W.., ?A..
-
-
-c_ Example(W..):
-
-    ___ -
-        s__ .-
-
-        ?
-
-    ___ initUI
-        sG__(300, 300, 250, 150)
-        sWT__('Event handler')
-        show
-
-    ___ keyPressEvent(self, e):
-        __ e.key  __ __.Key_Escape:
-            close
-
-
-__ _____ __ _______
-    app _ ?A..(___.argv)
-    ex _ Example
-    ___.e..(app.e..())
+# #!/usr/bin/python3
+# # -*- coding: utf-8 -*-
+#
+# ______ ___
+# ____ ?.?C.. ______ __
+# ____ ?.?W.. ______ W.., ?A..
+#
+#
+# c_ Example(W..):
+#
+#     ___ -
+#         s__ .-
+#
+#         ?
+#
+#     ___ initUI
+#         sG__ 300, 300, 250, 150
+#         sWT__ 'Event handler'
+#         s..
+#
+#     ___ keyPressEvent  e
+#         __ ?.k..  __ __.K_E..
+#             c..
+#
+#
+# __ _____ __ _______
+#     app _ ?A..
+#     ex _ ?
+#     ___.e.. ?.e..
