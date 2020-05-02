9ab531748150c79b716bf3f3254b1ca2daaa2109
Grey <sergejyurskyj@yahoo.com>
2020-05-01 18:56:24 -0700

    Committed 001_inputdialog template

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/005_Dialogs/new/001_inputdialog.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/005_Dialogs/new/001_inputdialog.py
@@ -1,40 +1,40 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-
-____ ?.?W.. ______ (W.., ?P.., ?L..,
-                             QInputDialog, ?A..)
-______ ___
-
-
-c_ Example(W..):
-
-    ___ -
-        s__ .-
-
-        ?
-
-    ___ initUI
-        btn _ ?P..('Dialog',
-        btn.m..(20, 20)
-        btn.c__.c..(showDialog)
-
-        le _ ?L..(
-        le.m..(130, 22)
-
-        sG__(300, 300, 290, 150)
-        sWT__('Input dialog')
-        show
-
-    ___ showDialog
-        text, ok _ QInputDialog.getText(self, 'Input Dialog',
-                                        'Enter your name:')
-
-        __ ok:
-            le.sT..(st.(text))
-
-
-__ _____ __ _______
-    app _ ?A..(___.argv)
-    ex _ Example
-    ___.e..(app.e..())
+# #!/usr/bin/python3
+# # -*- coding: utf-8 -*-
+#
+#
+# ____ ?.?W.. ______ (W.., ?P.., ?L..,
+#                              ?ID.. ?A..
+# ______ ___
+#
+#
+# c_ Example W..
+#
+#     ___ -
+#         s__ .-
+#
+#         ?
+#
+#     ___ initUI
+#         btn _ ?P.. 'Dialog',
+#         ?.m.. 20 20
+#         ?.c__.c.. sD..
+#
+#         le _ ?L..
+#         ?.m.. 130 22
+#
+#         sG__ 300 300 290 150
+#         sWT__('Input dialog')
+#         s..
+#
+#     ___ showDialog
+#         text, ok _ ?ID...gT.  'Input Dialog'
+#                                         'Enter your name:')
+#
+#         __ ok
+#             le.sT.. st. t..
+#
+#
+# __ _____ __ _______
+#     app _ ?A..
+#     ex _ ?
+#     ___.e.. ?.e..
