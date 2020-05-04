b6431d764d91bca32dd58b45bd97a390be1d4927
Grey <sergejyurskyj@yahoo.com>
2020-05-04 11:15:25 -0700

    Committed 002_simplemenu exercise

--- 140_gui/pyqt_pyside/_exercises/exercises/zetcode/002_Menus and toolbars/002_simplemenu.py
+++ 140_gui/pyqt_pyside/_exercises/exercises/zetcode/002_Menus and toolbars/002_simplemenu.py
@@ -1,42 +1,42 @@
-# ______ ___
-# ____ _2._W.. ______ ?MW.. ?Ac.. ?Ap.. ?A..
-# ____ _2._G.. ______ ?I..
-#
-#
-# c_ Example ?MW..
-#
-#     ___ -
-#         s__. -
-#
-#         ?
-#
-#     ___ initUI
-#         exitAct _ ?A.. ?I.. *e...png *&Exit ?
-#         ?.sS.. *Ctrl+Q
-#         ?.sST.. *Exit application
-#         ?.t__.c.. ?A__.q..
-#
-#         sB..
-#
-#         menubar _ mB..
-#         fileMenu _ ?.aM.. *&File
-#         ?.aA.. eA..
-#
-#         sG.. 300, 300, 300, 200
-#         sWT.. *Simple menu
-#         ?
-#
-#
-# __ ______ __ ______
-#     ______ ___
-#
-#     app _ N..
-#     ___
-#         ______ n..
-#     ______ I..
-#         ? _ ?
-#     main _ ?
-#     ?*.s..
-#
-#     __ ? __ no. N..
-#         ?.e.._
+import sys
+from PyQt5.QtWidgets import QMainWindow, QAction, QApplication, qApp
+from PyQt5.QtGui import QIcon
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        exitAct = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAct)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Simple menu')
+        self.show()
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = None
+    try:
+        import nuke
+    except ImportError:
+        app = QApplication(sys.argv)
+    main = Example()
+    main.show()
+
+    if app is not None:
+        app.exec_()
