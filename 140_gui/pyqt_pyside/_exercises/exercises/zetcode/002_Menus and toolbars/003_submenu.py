7eb245f90ce58f4452ad7533808e106433a48621
Grey <sergejyurskyj@yahoo.com>
2020-05-04 11:24:31 -0700

    Committed 003_submenu exercise

--- 140_gui/pyqt_pyside/_exercises/exercises/zetcode/002_Menus and toolbars/003_submenu.py
+++ 140_gui/pyqt_pyside/_exercises/exercises/zetcode/002_Menus and toolbars/003_submenu.py
@@ -1,42 +1,42 @@
-# ______ ___
-# ____ _2._W..______ ?W.. ?A.. ?M.. ?A..
-#
-#
-# c_ Example ?M..
-#
-#     ___ -
-#         s___. -
-#
-#         ?
-#
-#     ___ initUI
-#         menubar _ mB..
-#         fileMenu _ ?.aM.. *File
-#
-#         impMenu _ ?M.. *Import ?
-#         impAct _ ?A.. *Import mail ?
-#         iM__.aA.. iA..
-#
-#         newAct _ ?A.. *New ?
-#
-#         fM__.aA.. nA..
-#         fM__.aM.. iM..
-#
-#         sG.. 300, 300, 300, 200
-#         sWT.. *Submenu
-#         ?
-#
-#
-# __ ______ __ ______
-#     ______ ___
-#
-#     app _ N..
-#     ___
-#         ______ n..
-#     ______ I..
-#         ? _ ?
-#     main _ ?
-#     ?*.s..
-#
-#     __ ? __ no. N..
-#         ?.e.._
+import sys
+from PySide2.QtWidgets import QMainWindow, QAction, QMenu, QApplication
+
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        impMenu = QMenu('Import', self)
+        impAct = QAction('Import mail', self)
+        impMenu.addAction(impAct)
+
+        newAct = QAction('New', self)
+
+        fileMenu.addAction(newAct)
+        fileMenu.addMenu(impMenu)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Submenu')
+        self.show()
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = None
+    try:
+        import nuke
+    except ImportError:
+        app = QApplication(sys.argv)
+    main =Example()
+    main.show()
+
+    if app is not None:
+        app.exec_()
