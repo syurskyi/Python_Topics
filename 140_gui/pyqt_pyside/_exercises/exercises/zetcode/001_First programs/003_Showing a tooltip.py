dd997ac08d2766e995b847b7513bf47a8a1051c8
Grey <sergejyurskyj@yahoo.com>
2020-05-04 09:51:46 -0700

    Committed 003_Showing a tooltip exercise

--- 140_gui/pyqt_pyside/_exercises/exercises/zetcode/001_First programs/003_Showing a tooltip.py
+++ 140_gui/pyqt_pyside/_exercises/exercises/zetcode/001_First programs/003_Showing a tooltip.py
@@ -1,40 +1,39 @@
-# ______ ___
-# ____ _2._W.. ______ ?W.. ?TT..
-#     ?PB.. ?Ap..
-# ____ _2._G.. ______ ?F..
-#
-#
-# c_ Example ?W..
-#     ___ -
-#            s__. -
-#
-#         ?
-#
-#     ___ initUI
-#         ?TT__.sF.. ?F.. *SansSerif 10
-#
-#         sTT.. *This is a <b>QWidget</b> widget
-#
-#         btn _ ?PB..  *Button ?
-#         ?.sTT.. *This is a <b>?P..</b> widget
-#         ?.r.. ?.sH..
-#         ?.m.. 50, 50
-#
-#         sG.. 300, 300, 250, 150
-#         sWT.. *Tooltips
-#         ?
-#
-#
-# __ ______ __ ______
-#     ______ ___
-#
-#     app _ N..
-#     ___
-#         ______ n..
-#     ______ I..
-#         ? _ ?
-#     main _ ?
-#     ?*.s..
-#
-#     __ ? __ no. N..
-#         ?.e.._
+import sys
+from PySide2.QtWidgets import QWidget, QToolTip, QPushButton, QApplication
+from PySide2.QtGui import QFont
+
+
+class Example (QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+
+        self.setWindowTitle('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Tooltips')
+        self.show()
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = None
+    try:
+        import nuke
+    except ImportError:
+        app = QApplication(sys.argv)
+    main = Example()
+    main.show()
+
+    if app is not None:
+        app.exec_()
