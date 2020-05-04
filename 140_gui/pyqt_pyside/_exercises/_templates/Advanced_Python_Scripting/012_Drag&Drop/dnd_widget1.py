ccd1ca24f07f31b75e72e5a6bad1e03dc336684b
Grey <sergejyurskyj@yahoo.com>
2020-05-03 18:56:25 -0700

    Committed 012_Drag&Drop template

--- 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/012_Drag&Drop/dnd_widget1.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/Advanced_Python_Scripting/012_Drag&Drop/dnd_widget1.py
@@ -1,36 +1,36 @@
-_____ ___
-_____ __
-____ ?.?C.. _____ _
-____ ?.?G.. _____ _
-
-c_ listWidgetClass(?LW..
-    ___  -  
-        s__(listWidgetClass, self). -
-        sWF..(__.WSOTH..)          # #okno bydet vsegda poverh drygih okon
-        sDDM..(?AIV...DO..)      # shto bu dropEvent zarabotal nado vklychit dlja etogo vidgeta setDragDropMode
-
-    ___ dropEvent , event                # to shto proishodit kogda mu sbrasuvaem dannue na vidget
-        print 'DROP', ty..(event)
-        mimedata _ event.mD..            # kogda mu peretaskivaem element, to pomimo togo shto srabatuvaet DropEvent,
-                                               # srabatuvaet echjo 2 eventa, dragEnterEvent and dragMoveEvent
-                                               # obuchno oni odinakovue i govorjat mozet li nash vidget prinjat' eti dannue kotorue mu peretaskivaem
-                                               # i vnytri etogo eventa kak raz proverjaetsja kakogo tipa dannue k nam prishli
-                                               # i etot event dolzen skazat' mozet li nash event eti dannue prinjat'
-        __ mimedata.hasText(
-            print 'text'
-        ____ ?.hU..(
-            print 'urls'
-
-    ___ dragEnterEvent , event
-        event.a..
-        print 'ENTER', ty..(event)
-
-    ___ dragMoveEvent , event
-        event.a..
-        print 'MOVE'
-
-__ _____ __ ______
-    app _ ?A..([])
-    w _ listWidgetClass
-    w.s..
-    app.exec_
+# _____ ___
+# _____ __
+# ____ ?.?C.. _____ _
+# ____ ?.?G.. _____ _
+#
+# c_ listWidgetClass ?LW..
+#     ___  -
+#         s__ ? . -
+#         sWF.. __.WSOTH..)         # #okno bydet vsegda poverh drygih okon
+#         sDDM.. ?AIV...DO..      # shto bu dropEvent zarabotal nado vklychit dlja etogo vidgeta setDragDropMode
+#
+#     ___ dropEvent event                # to shto proishodit kogda mu sbrasuvaem dannue na vidget
+#         print 'DROP', ty.. ?
+#         mimedata _ ?.mD..            # kogda mu peretaskivaem element, to pomimo togo shto srabatuvaet DropEvent,
+#                                                # srabatuvaet echjo 2 eventa, dragEnterEvent and dragMoveEvent
+#                                                # obuchno oni odinakovue i govorjat mozet li nash vidget prinjat' eti dannue kotorue mu peretaskivaem
+#                                                # i vnytri etogo eventa kak raz proverjaetsja kakogo tipa dannue k nam prishli
+#                                                # i etot event dolzen skazat' mozet li nash event eti dannue prinjat'
+#         __ ?.hT..
+#             print 'text'
+#         ____ ?.hU..
+#             print 'urls'
+#
+#     ___ dragEnterEvent event
+#         ?.a..
+#         print 'ENTER', ty.. ?
+#
+#     ___ dragMoveEvent  event
+#         ?.a..
+#         print 'MOVE'
+#
+# __ _____ __ ______
+#     app _ ?A..
+#     w _ ?
+#     w.s..
+#     ?.e..
