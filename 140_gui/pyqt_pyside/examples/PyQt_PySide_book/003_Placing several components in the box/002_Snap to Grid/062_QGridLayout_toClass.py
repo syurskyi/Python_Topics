ed8d94e7a3bb7f1b5eff8d23914ee5eb4bc6c2ac
Grey <sergejyurskyj@yahoo.com>
2020-05-05 11:18:35 -0700

    Committed 062_QGridLayout_toClass examples

--- 140_gui/pyqt_pyside/examples/PyQt_PySide_book/003_Placing several components in the box/002_Snap to Grid/062_QGridLayout_toClass.py
+++ 140_gui/pyqt_pyside/examples/PyQt_PySide_book/003_Placing several components in the box/002_Snap to Grid/062_QGridLayout_toClass.py
@@ -1,26 +1,36 @@
-from PySide import QtCore, QtGui
+from PyQt5 import QtWidgets
 import sys
 
-class SampleWindow(QtGui.QWidget):
+class SampleWindow(QtWidgets.QWidget):
     def __init__(self):
         super(SampleWindow, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
         self.setWindowTitle("Выравнивание по сетке")
         self.resize(300, 50)
-        button1 = QtGui.QPushButton("1")
-        button2 = QtGui.QPushButton("2")
-        button3 = QtGui.QPushButton("3")
-        button4 = QtGui.QPushButton("4")
-        grid = QtGui.QGridLayout()
+        button1 = QtWidgets.QPushButton("1")
+        button2 = QtWidgets.QPushButton("2")
+        button3 = QtWidgets.QPushButton("3")
+        button4 = QtWidgets.QPushButton("4")
+        grid = QtWidgets.QGridLayout()
         grid.addWidget(button1, 0, 0)
         grid.addWidget(button2, 0, 1)
         grid.addWidget(button3, 1, 0)
         grid.addWidget(button4, 1, 1)
         self.setLayout(grid)
 
-def main():
-    global c
-    c = SampleWindow()
+if __name__ == '__main__':
+    import sys
 
-    c.show()
+    app = None
+    try:
+        import nuke
+    except ImportError:
+        app = QtWidgets.QApplication(sys.argv)
+    main = SampleWindow()
+    main.show()
 
-main()
+    if app is not None:
+        app.exec_()
