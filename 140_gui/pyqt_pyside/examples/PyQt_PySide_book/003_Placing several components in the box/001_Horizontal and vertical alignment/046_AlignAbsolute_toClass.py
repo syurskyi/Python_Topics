8a2c3f1e7ea29045b212d05f2ea1bfef99376aae
Grey <sergejyurskyj@yahoo.com>
2020-05-05 10:58:09 -0700

    Committed 046_AlignAbsolute_toClass examples

--- 140_gui/pyqt_pyside/examples/PyQt_PySide_book/003_Placing several components in the box/001_Horizontal and vertical alignment/046_AlignAbsolute_toClass.py
+++ 140_gui/pyqt_pyside/examples/PyQt_PySide_book/003_Placing several components in the box/001_Horizontal and vertical alignment/046_AlignAbsolute_toClass.py
@@ -1,38 +1,47 @@
-from PySide import QtCore, QtGui
+from PyQt5 import QtCore, QtWidgets, QtGui
 import sys
 
-class SampleWindow(QtGui.QWidget):
+class SampleWindow(QtWidgets.QWidget):
+
     def __init__(self):
         super(SampleWindow, self).__init__()
-        self.setWindowTitle("AlignAbsolute")
-        self.resize(400, 150)
-        button1 = QtGui.QPushButton("1")
-        button2 = QtGui.QPushButton("2")
-        button3 = QtGui.QPushButton("3")
-        button4 = QtGui.QPushButton("4")
-        button5 = QtGui.QPushButton("5")
-        button6 = QtGui.QPushButton("6")
 
-        # self.setLayoutDirection(QtCore.Qt.RightToLeft)
-        vbox = QtGui.QVBoxLayout()
+        self.initUI()
+
+    def initUI(self):
+
+        button1 = QtWidgets.QPushButton("1")
+        button2 = QtWidgets.QPushButton("2")
+        button3 = QtWidgets.QPushButton("3")
+        button4 = QtWidgets.QPushButton("4")
+        button5 = QtWidgets.QPushButton("5")
+        button6 = QtWidgets.QPushButton("6")
         #
-        hbox = QtGui.QHBoxLayout()
+        self.setLayoutDirection(QtCore.Qt.RightToLeft)
+        vbox = QtWidgets.QVBoxLayout()
+
+        hbox = QtWidgets.QHBoxLayout()
         hbox.addWidget(button1)
         hbox.addWidget(button2)
         hbox.addWidget(button3, alignment=QtCore.Qt.AlignLeft)
         vbox.addLayout(hbox)
-        #
-        hbox2 = QtGui.QHBoxLayout()
+
+        hbox2 = QtWidgets.QHBoxLayout()
         hbox2.addWidget(button4)
         hbox2.addWidget(button5)
-        hbox2.addWidget(button6, alignment= QtCore.Qt.AlignAbsolute | QtCore.Qt.AlignLeft)
+        hbox2.addWidget(button6,
+                        alignment=QtCore.Qt.AlignAbsolute | QtCore.Qt.AlignLeft)
         vbox.addLayout(hbox2)
+        # window = QtWidgets.QWidget()
+        self.setWindowTitle("AlignAbsolute")
+        self.resize(400, 150)
+        self.setLayout(vbox)
+        self.show()
 
-def main():
-    global c
-    c = SampleWindow()
 
-    c.show()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ex = SampleWindow()
+    sys.exit(app.exec_())
 
-main()
 
