912df5455c5847f2caf6d27a5aa31874df373e8e
Grey <sergejyurskyj@yahoo.com>
2020-05-05 11:11:14 -0700

    Committed 049_QHBoxLayout_toClass examples

--- 140_gui/pyqt_pyside/examples/PyQt_PySide_book/003_Placing several components in the box/001_Horizontal and vertical alignment/049_QHBoxLayout_toClass.py
+++ 140_gui/pyqt_pyside/examples/PyQt_PySide_book/003_Placing several components in the box/001_Horizontal and vertical alignment/049_QHBoxLayout_toClass.py
@@ -1,25 +1,32 @@
-from PySide import QtGui, QtCore
+from PyQt5 import QtWidgets
 import sys
 
-class MyWindow(QtGui.QWidget):
+class MyWindow(QtWidgets.QWidget):
     def __init__(self):
         super(MyWindow, self).__init__()
 
         self.setWindowTitle("Horizontal Alignment")
         self.resize(300, 50)
-        button1 = QtGui.QPushButton("1")
-        button2 = QtGui.QPushButton("2")
-        button3 = QtGui.QPushButton("3")
-        hbox = QtGui.QHBoxLayout()
+        button1 = QtWidgets.QPushButton("1")
+        button2 = QtWidgets.QPushButton("2")
+        button3 = QtWidgets.QPushButton("3")
+        hbox = QtWidgets.QHBoxLayout()
         hbox.addWidget(button1)
         hbox.addWidget(button2)
         hbox.addWidget(button3)
         self.setLayout(hbox)
 
 
-def main():
-    global c
-    c = MyWindow()
-    c.show()
+if __name__ == '__main__':
+    import sys
 
-main()
+    app = None
+    try:
+        import nuke
+    except ImportError:
+        app = QtWidgets.QApplication(sys.argv)
+    main = MyWindow()
+    main.show()
+
+    if app is not None:
+        app.exec_()
