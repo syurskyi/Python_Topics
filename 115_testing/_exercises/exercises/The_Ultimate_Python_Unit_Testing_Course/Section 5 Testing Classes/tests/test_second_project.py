b280e11dfc6ea262443abcca0fa0c82f53980519
Grey <sergejyurskyj@yahoo.com>
2020-05-07 09:01:56 -0700

    Committed Section 5 Testing Classes exercise

--- 115_testing/_exercises/exercises/The_Ultimate_Python_Unit_Testing_Course/Section 5 Testing Classes/tests/test_second_project.py
+++ 115_testing/_exercises/exercises/The_Ultimate_Python_Unit_Testing_Course/Section 5 Testing Classes/tests/test_second_project.py
@@ -2,65 +2,65 @@
 # from second_project ______ Counter
 #
 #
-# c_ EasyTestCase u___.T...
+# c_ EasyTestCase ?.?
 #
-#     ___ setUp ____
-#         ____.counter _ C__
+#     ___ setUp
+#         counter _ C__
 #
-#     ___ test_easy_input ____
-#         ____.aE_ ____.c___.g._v.  , 0
+#     ___ test_easy_input
+#         aE_ c___.g._v.  , 0
 #
-#     ___ test_easy_input_two ____
-#         ____.c___.cl..
-#         ____.aE_ ____.c___.g._v.  , 0
+#     ___ test_easy_input_two
+#         c___.cl..
+#         aE_ c___.g._v.  , 0
 #
-#     ___ tearDown ____
-#         ____.c___ _ N..
-#
-#
-# c_ MediumTestCase u___.T...
-#
-#     ___ setUp ____
-#         ____.counter _ C__
-#
-#     ___ test_medium_input ____
-#         ____.c___.ad.
-#         ____.c___.ad.
-#         ____.c___.ad.
-#         ____.aE_ ____.c___.g._v.  , 3
-#
-#     ___ test_medium_input_two ____
-#         ____.c___.ad.
-#         ____.c___.ad.
-#         ____.c___.ad.
-#         ____.c___.re..
-#         ____.c___.re..
-#         ____.aE_ ____.c___.g._v.  , 1
-#
-#     ___ tearDown ____
-#         ____.c___ _ N..
+#     ___ tearDown
+#         c___ _ N..
 #
 #
-# c_ HardTestCase u___.T...
+# c_ MediumTestCase ?.?
 #
-#     ___ setUp ____
-#         ____.counter _ C__
+#     ___ setUp
+#         counter _ C__
 #
-#     ___ test_hard_input ____
-#         ____.c___.re..
-#         ____.c___.re..
-#         ____.c___.re..
-#         ____.c___.re..
-#         ____.aE_ ____.c___.g._v.  , 0
+#     ___ test_medium_input
+#         c___.ad.
+#         c___.ad.
+#         c___.ad.
+#         aE_ c___.g._v.  , 3
 #
-#     ___ test_hard_input_two ____
+#     ___ test_medium_input_two
+#         c___.ad.
+#         c___.ad.
+#         c___.ad.
+#         c___.re..
+#         c___.re..
+#         aE_ c___.g._v.  , 1
+#
+#     ___ tearDown
+#         c___ _ N..
+#
+#
+# c_ HardTestCase ?.?
+#
+#     ___ setUp
+#         counter _ C__
+#
+#     ___ test_hard_input
+#         c___.re..
+#         c___.re..
+#         c___.re..
+#         c___.re..
+#         aE_ c___.g._v.  , 0
+#
+#     ___ test_hard_input_two
 #         ___ _ i_ ra.. 0, 1000
-#             ____.c___.ad.
-#         ____.aE_ ____.c___.g._v.  , 1000
+#             c___.ad.
+#         aE_ c___.g._v.  , 1000
 #
-#     ___ tearDown ____
-#         ____.c___ _ N...
+#     ___ tearDown
+#         c___ _ N...
 #
 #
 # __ ______ __ ______
-#     u___.m..
+#     ?.m..
