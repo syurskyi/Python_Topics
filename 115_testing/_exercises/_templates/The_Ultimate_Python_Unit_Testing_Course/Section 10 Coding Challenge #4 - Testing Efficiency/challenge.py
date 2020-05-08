10519b17efbbc5ddc7a447e5de4f4a8bd92b818a
Grey <sergejyurskyj@yahoo.com>
2020-05-07 18:36:09 -0700

    Committed Section 17 Before Production Checklist template

---
+++ 115_testing/_exercises/_templates/The_Ultimate_Python_Unit_Testing_Course/Section 10 Coding Challenge #4 - Testing Efficiency/challenge.py
@@ -0,0 +1,16 @@
+"""Coding Challenge Skeleton #4
+"""
+
+
+class EfficiencyAdding:
+
+    def adding_two_first_method(self, number_reached):
+        while number_reached > 0:
+            number_reached -= 2
+
+    def adding_two_second_method(self, number_reached):
+        while number_reached > 0:
+            number_reached -= 1
+            number_reached -= 1
+
+
