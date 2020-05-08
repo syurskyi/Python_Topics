872900781ef188279db8759a9570098d07e9e783
Grey <sergejyurskyj@yahoo.com>
2020-05-07 18:26:19 -0700

    Committed Section 8 Coding Challenge #3 - Testing Console Printed OutPut template

---
+++ 115_testing/_exercises/_templates/The_Ultimate_Python_Unit_Testing_Course/Section 8 Coding Challenge #3 - Testing Console Printed OutPut/challenge.py
@@ -0,0 +1,15 @@
+# """Coding Challenge Skeleton #3
+# """
+#
+#
+# c_ Printer
+#     ___ -
+#         _print _ ""
+#
+#     ___ set_value value
+#         _print _ ?
+#
+#     ___ print_value
+#         print _p..
+#
+#
---
+++ 115_testing/_exercises/_templates/The_Ultimate_Python_Unit_Testing_Course/Section 8 Coding Challenge #3 - Testing Console Printed OutPut/tests/test_chanllege.py
@@ -0,0 +1,28 @@
+# ______ ___
+# ______ ?
+# ____ io ______ SIO
+# ____ challenge ______ Printer
+#
+#
+# c_ TestPrintedOutPut ?.?
+#
+#     ___ setUp
+#         held, ___.s_o_ _ ___.s_o_, SIO
+#         printer _ P..
+#
+#     ___ test_value_name
+#         ?.s_v.. 'Muhammad Ali'
+#         ?.p_v..
+#         aE.. ___.s_o_.g_v_.st.. 'Muhammad Ali'
+#
+#     ___ test_value_job
+#         ?.s_v.. 'Boxer'
+#         ?.p_v..
+#         aE.. ___.s_o_.g_v_.st.. 'Boxer'
+#
+#     ___ tearDown
+#         ? _ N..
+#
+#
+# __ ______ __ _____
+#     ?.m..
