ae9068fd0cd3155b3e35c55ce49de7da3c2339f6
Grey <sergejyurskyj@yahoo.com>
2020-05-08 20:28:22 -0700

    Committed 2_classes template update

--- 115_testing/_exercises/_templates/The Complete Python Course/20_unit_testing/code/2_classes/printer.py
+++ 115_testing/_exercises/_templates/The Complete Python Course/20_unit_testing/code/2_classes/printer.py
@@ -2,15 +2,15 @@
 #     p..
 #
 #
-# c_ Printer:
-#     ___ - ____ pages_per_s in. capacity in.
-#         ____.p.. _ p..
-#         ____._c... _ c..
+# c_ Printer
+#     ___ -  pages_per_s in. capacity in.
+#         p.. _ p..
+#         _c... _ c..
 #
-#     ___ print ____ pages
-#         i_ ? > ____._c...
+#     ___ print  pages
+#         __ ? > _c...
 #             r____ P... Printer does not have enough capacity for all these pages.
 #
-#         ____._capacity -= pages
+#         _capacity -= pages
 #
 #         r_ _ Printed |pages| pages in |pages/____.pages_per_s:.2f| seconds.'
--- 115_testing/_exercises/_templates/The Complete Python Course/20_unit_testing/code/2_classes/test_printer.py
+++ 115_testing/_exercises/_templates/The Complete Python Course/20_unit_testing/code/2_classes/test_printer.py
@@ -2,44 +2,44 @@
 # f___ u_____ _______ T...
 #
 #
-# c_ TestPrinter T..
-#     ___ setUp ____
-#         ____.pr__ _ Pr... pages_per_s=2.0, capacity=300)
+# c_ TestPrinter ?
+#     ___ setUp
+#         pr__ _ Pr... pages_per_s=2.0, capacity=300)
 #
-#     ___ test_print_within_capacity ____
-#         ____.pr__.print(25)
+#     ___ test_print_within_capacity
+#         ?.print(25)
 #
-#     ___ test_print_outside_capacity ____
-#         with ____.aR_ P....
-#             ____.pr__.print(301)
+#     ___ test_print_outside_capacity
+#         with .aR_ P....
+#             ?.print 301
 #
-#     ___ test_print_exact_capacity ____
-#         ____.pr__.print(____.pr__._ca...
+#     ___ test_print_exact_capacity
+#         ?.print ?._ca...
 #
-#     ___ test_printer_speed ____
-#         pages = 10
-#         expected = 'Printed 10 pages in 5.00 seconds.'
+#     ___ test_printer_speed
+#         pages _ 10
+#         expected _ 'Printed 10 pages in 5.00 seconds.'
 #
-#         result _ ____.pr__.print pa...
+#         result _ ?.print pa...
 #         ____.aE_ ? e...
 #
-#     ___ test_speed_always_two_decimals ____
-#         fast_printer = Pr... pages_per_s=3.0, capacity=300)
-#         pages = 11
-#         expected = 'Printed 11 pages in 3.67 seconds.'
+#     ___ test_speed_always_two_decimals
+#         fast_printer _ Pr... pages_per_s=3.0, capacity=300)
+#         pages _ 11
+#         expected _ 'Printed 11 pages in 3.67 seconds.'
 #
-#         result = fa._p_.print p..
-#         ____.aE_ ? e...
+#         result _ fa._p_.print p..
+#         aE_ ? e...
 #
-#     ___ test_multiple_print_runs ____
-#         ____.pr__.print(25)
-#         ____.pr__.print(50)
-#         ____.pr__.print(225)
+#     ___ test_multiple_print_runs
+#         ?.print 25
+#         ?.print 50
+#         ?.print 225
 #
-#     ___ test_multiple_runs_end_up_error ____
-#         ____.pr__.print(25)
-#         ____.pr__.print(50)
-#         ____.pr__.print(225)
+#     ___ test_multiple_runs_end_up_error
+#         ?.print 25
+#         ?.print 50
+#         ?.print 225
 #
-#         w___ ____.aR_ P...
-#             ____.pr__.print(1)
+#         w___ aR_ P...
+#             ?.print 1
