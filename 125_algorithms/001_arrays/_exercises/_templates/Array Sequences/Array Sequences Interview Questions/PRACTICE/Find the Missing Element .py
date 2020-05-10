e677fc231791b398894837c7564ef912f964a1d5
Grey <sergejyurskyj@yahoo.com>
2020-05-09 20:44:49 -0700

    Committed Find the Missing Element  exercise

--- 125_algorithms/001_arrays/_exercises/_templates/Array Sequences/Array Sequences Interview Questions/PRACTICE/Find the Missing Element .py
+++ 125_algorithms/001_arrays/_exercises/_templates/Array Sequences/Array Sequences Interview Questions/PRACTICE/Find the Missing Element .py
@@ -47,19 +47,19 @@
 # """
 # RUN THIS CELL TO TEST YOUR SOLUTION
 # """
-# # from nose.tools import assert_equal
+# # ____ n__.t.. ____ a_e..
 # #
-# # class TestFinder(object):
+# # c_ TestFinder o..
 # #
-# #     def test(self, sol):
-# #         assert_equal(sol([5,5,7,7],[5,7,7]),5)
-# #         assert_equal(sol([1,2,3,4,5,6,7],[3,7,2,1,4,6]),5)
-# #         assert_equal(sol([9,8,7,6,5,4,3,2,1],[9,8,7,5,4,3,2,1]),6)
+# #     def test sol
+# #         a_e.. ? 5,5,7,7],[5,7,7]),5)
+# #         a_e.. ? 1,2,3,4,5,6,7],[3,7,2,1,4,6]),5)
+# #         a_e.. ? 9,8,7,6,5,4,3,2,1],[9,8,7,5,4,3,2,1]),6)
 # #         print('ALL TEST CASES PASSED')
 # #
 # # # Run test
-# # t _ TestFinder()
-# # t.test(finder)
+# # t _ ?
+# # ?.t.. ?
 # #
 # # # %%
 # # '''
