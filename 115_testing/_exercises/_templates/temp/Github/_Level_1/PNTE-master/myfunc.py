7e4474635c59c0ba4bec1d2e263e47381f886101
Grey <sergejyurskyj@yahoo.com>
2020-05-10 19:21:30 -0700

    Committed PNTE-master templates

--- 115_testing/_exercises/_templates/temp/Github/_Level_1/PNTE-master/myfunc.py
+++ 115_testing/_exercises/_templates/temp/Github/_Level_1/PNTE-master/myfunc.py
@@ -1,6 +1,6 @@
-﻿# -*- coding: utf-8 -*-
-
-___ add(num1,num2
-  __(num1 __ N..
-    r_ R..('num1 is None')
-  r_ num1 + num2
+﻿# # -*- coding: utf-8 -*-
+#
+# ___ add num1 num2
+#   __ ? __ N..
+#     r_ R.. 'num1 is None'
+#   r_ ? + ?
