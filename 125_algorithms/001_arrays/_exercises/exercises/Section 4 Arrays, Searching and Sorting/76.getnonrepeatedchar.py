6a9757eea8c84cfb26261236d8f68af5e81411a9
Grey <sergejyurskyj@yahoo.com>
2020-05-11 10:33:05 -0700

    Committed 76.getnonrepeatedchar exercise

--- 125_algorithms/001_arrays/_exercises/exercises/Section 4 Arrays, Searching and Sorting/76.getnonrepeatedchar.py
+++ 125_algorithms/001_arrays/_exercises/exercises/Section 4 Arrays, Searching and Sorting/76.getnonrepeatedchar.py
@@ -1,21 +1,18 @@
-# ___ getfirstnonrepeated myarray
-# 	print("Given array:" ?
-# 	tab    # dict  # hash
-# 	# #print "tab created:", tab
-# 	___ ele __ ?.l..
-# 		__ ? __ t..
-# 			?|? +_ 1
-# 		____ ? !_ " "
-# 			t..|? _ 1
-# 		____
-# 			?|? _ 0
-# 		#print "in loop:",tab,"for","'",ele,"'","in",myarray
-#
-# 	___ ele __ ?.l..
-# 		__ t..|? __ 1
-# 			print("the first non repeated character is: @" ?
+# ___ getrepeatedelement A
+# 	print("given array:" ?
+# 	tab   # hash
+# 		# print "created a new tab:", tab
+# 	___ element __ ?.l..
+# 		__ ? __ ?
+# 			t.. ? +_ 1
+# 			print("the first repeated element is: @" ?
+# 			# print "in loop: tab",tab,"for element '",element,"' in array ",A
 # 			r_ ?
+# 		____ ? !_ " "
+# 			t.. ? _ 1
+# 		____
+# 			t.. ? _ 0
+# 		# print "in loop: tab",tab,"for element '",element,"' in array ",A
+# 	r_
 #
-# 	r)
-#
-# ? "abccdef"
+# ?("abcdefd")
