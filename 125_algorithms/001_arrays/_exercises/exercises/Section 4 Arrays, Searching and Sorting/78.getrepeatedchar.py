0f23b34d726ae37893f1c1b15a37527987db2d9c
Grey <sergejyurskyj@yahoo.com>
2020-05-11 11:38:08 -0700

    Committed 78.getrepeatedchar exercise

--- 125_algorithms/001_arrays/_exercises/exercises/Section 4 Arrays, Searching and Sorting/78.getrepeatedchar.py
+++ 125_algorithms/001_arrays/_exercises/exercises/Section 4 Arrays, Searching and Sorting/78.getrepeatedchar.py
@@ -1,18 +1,18 @@
-# ___ getrepeatedelement A
-# 	print("given array:" ?
-# 	tab _    # dict  # hash
-# 		# print "created a new tab:", tab
-# 	___ element __ ?.l..
-# 		__ ? __ t..
-# 			?|? +_ 1
-# 			print("the first repeated element is: @"  (?
-# 			# print "in loop: tab",tab,"for element '",element,"' in array ",A
-# 			r_ ?
-# 		____ ? !_ " ":
-# 			t..|? _ 1
-# 		____
-# 			t..|? _ 0
-# 		# print "in loop: tab",tab,"for element '",element,"' in array ",A
-# 	r_
-#
-# ? abcdefd
+def getrepeatedelement(A):
+    print("given array:", A)
+    tab = {}   # dict  # hash
+        # print "created a new tab:", tab
+    for element in A.lower():
+        if element in tab:
+            tab[element] += 1
+            print("the first repeated element is: %s" % (element))
+            # print "in loop: tab",tab,"for element '",element,"' in array ",A
+            return element
+        elif element != " ":
+            tab[element] = 1
+        else:
+            tab[element] = 0
+        # print "in loop: tab",tab,"for element '",element,"' in array ",A
+    return
+
+getrepeatedelement('abcdefd')
