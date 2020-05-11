6ab34cbb1aff9b9588b555290e1fc7cb8dfc0759
Grey <sergejyurskyj@yahoo.com>
2020-05-11 11:33:08 -0700

    Committed 77.printduplicatesfromunsortedarray exercise

--- 125_algorithms/001_arrays/_exercises/exercises/Section 4 Arrays, Searching and Sorting/76.getnonrepeatedchar.py
+++ 125_algorithms/001_arrays/_exercises/exercises/Section 4 Arrays, Searching and Sorting/76.getnonrepeatedchar.py
@@ -1,18 +1,16 @@
-# ___ getrepeatedelement A
-# 	print("given array:" ?
-# 	tab   # hash
-# 		# print "created a new tab:", tab
-# 	___ element __ ?.l..
-# 		__ ? __ ?
-# 			t.. ? +_ 1
-# 			print("the first repeated element is: @" ?
-# 			# print "in loop: tab",tab,"for element '",element,"' in array ",A
-# 			r_ ?
-# 		____ ? !_ " "
-# 			t.. ? _ 1
-# 		____
-# 			t.. ? _ 0
-# 		# print "in loop: tab",tab,"for element '",element,"' in array ",A
-# 	r_
-#
-# ?("abcdefd")
+def getrepeatedelement(A):
+    print("given array:", A)
+    tab = {}
+
+    for element in A.lower():
+        if element in tab:
+            tab[element] += 1
+            print("the first repeated element is: %s" % element)
+            return element
+        elif element != " ":
+            tab[element] = 1
+        else:
+            tab[element] = 0
+    return
+
+getrepeatedelement("abcdefd")
