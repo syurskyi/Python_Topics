abd030a84f1c6e4a1b8fdb9a80c64b3efbf9cbf1
Grey <sergejyurskyj@yahoo.com>
2020-05-11 18:35:38 -0700

    Committed unit testing templates

--- 115_testing/_exercises/_templates/temp/Github/_Level_1/hello_world_unittest-master/test_circles.py
+++ 115_testing/_exercises/_templates/temp/Github/_Level_1/hello_world_unittest-master/test_circles.py
@@ -1,18 +1,18 @@
-______ u__
-____ circles ______ circle_area
-____ math ______ pi
-
-c_ TestCircleArea?.?
-    ___ test_area
-        # Test areas when radius >= 0
-        aAE..(circle_area(1),pi)
-        aAE..(circle_area(0),0)
-        aAE..(circle_area(2.1), pi*2.1**2)
-
-    ___ test_values
-        aR..(V.., circle_area, -2)
-
-    ___ test_types
-        aR..(T.., circle_area, 3+5j)
-        aR..(T.., circle_area, T..)
-        aR..(T.., circle_area, "radius")
+# ______ u__
+# ____ circles ______ circle_area
+# ____ math ______ pi
+#
+# c_ TestCircleArea?.?
+#     ___ test_area
+#         # Test areas when radius >= 0
+#         aAE.. ? 1),pi
+#         aAE.. ? 0),0
+#         aAE.. ? 2.1), pi*2.1**2
+#
+#     ___ test_values
+#         aR.. V.., ?, -2
+#
+#     ___ test_types
+#         aR.. T.., ?, 3+5j
+#         aR.. T.., ?, T..
+#         aR.. T.., ?, "radius"
