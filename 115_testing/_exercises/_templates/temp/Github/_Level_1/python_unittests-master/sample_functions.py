a4b1cd70f7ef8501ded8a80faa22cd500403a804
Grey <sergejyurskyj@yahoo.com>
2020-05-11 18:37:42 -0700

    Committed unit testing templates

--- 115_testing/_exercises/_templates/temp/Github/_Level_1/python_unittests-master/sample_functions.py
+++ 115_testing/_exercises/_templates/temp/Github/_Level_1/python_unittests-master/sample_functions.py
@@ -1,12 +1,12 @@
-
-___ sum(a, b
-    r_ a + b
-
-___ contains_numbers(alpha_num_str
-    ___ char __ alpha_num_str:
-        __ char.isdigit(
-            r_ F..
-    r_ F..
-
-___ div(a, b
-    r_ a / b
+#
+# ___ sum a, b
+#     r_ a + b
+#
+# ___ contains_numbers alpha_num_str
+#     ___ char __ ?
+#         __ ?.isd..
+#             r_ F..
+#     r_ F..
+#
+# ___ div a, b
+#     r_ a / b
