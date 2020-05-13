f9deaf36fa959417d4f9b1aab14eeeefea99c18f
Grey <sergejyurskyj@yahoo.com>
2020-05-13 09:58:06 -0700

    Committed 001_inputdialog exercise

--- 140_gui/pyqt_pyside/_exercises/_templates/zetcode/005_Dialogs/new/002_colordialog.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/zetcode/005_Dialogs/new/002_colordialog.py
@@ -1,46 +1,46 @@
-# #!/usr/bin/python3
-# # -*- coding: utf-8 -*-
-#
-#
-# ____ ?.?W.. ______ (W.., ?P.., ?F.
-#                              ?CD.. ?A..
-# ____ ?.?G__ ______ ?C..
-# ______ ___
-#
-#
-# c_ Example W..
-#
-#     ___ -
-#         s__ .-
-#
-#         ?
-#
-#     ___ initUI
-#         col _ ?C.. 0 0 0
-#
-#         btn _ ?P.. 'Dialog'
-#         ?.m.. 20 20
-#
-#         btn.c__.c.. sD..
-#
-#         frm _ ?F.(
-#         ?.sSS.. "QWidget { background-color: @ }"
-#                                 c__.n..
-#         ?.sG__ 130 22 100 100
-#
-#         sG__ 300 300 250 180
-#         sWT__ 'Color dialog'
-#         s..
-#
-#     ___ showDialog
-#         col _ ?CD...gC..
-#
-#         __ ?.isValid
-#             ?.sSS..("QWidget { background-color: @ }"
-#                                     c_.n..
-#
-#
-# __ _____ __ _______
-#     app _ ?A..
-#     ex _ ?
-#     ___.e.. ?.e..
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+from PyQt5.QtWidgets import (QWidget, QPushButton, QFrame, QColorDialog, QApplication)
+from PyQt5.QtGui import QColor
+
+import sys
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        col = QColor(0, 0, 0)
+
+        self.btn = QPushButton('Dialog', self)
+        self.btn.move(20, 20)
+
+        self.btn.clicked.connect(self.showDialog)
+
+        self.frm = QFrame(self)
+        self.frm.setStyleSheet("QWidget { background-color: %s }" % col.name())
+        self.frm.setGeometry(130, 22, 100, 100)
+
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle('Color dialog')
+
+        self.show()
+
+    def showDialog(self):
+        col = QColorDialog.getColor()
+
+        if col.isValid():
+            self.frm.setStyleSheet("QWidget { background-color: @s }" %
+                                    col.name())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
