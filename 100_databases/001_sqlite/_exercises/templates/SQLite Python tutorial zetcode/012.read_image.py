15ba511b97d1289d9e34c1ceb7e6a335bbc3e394
Grey <sergejyurskyj@yahoo.com>
2020-05-16 20:31:53 -0700

    Committed SQLite Python tutorial zetcode 012.read_image template

--- 100_databases/001_sqlite/_exercises/templates/SQLite Python tutorial zetcode/012.read_image.py
+++ 100_databases/001_sqlite/_exercises/templates/SQLite Python tutorial zetcode/012.read_image.py
@@ -1,46 +1,46 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-_____ ? __ ?
-_____ ___
-
-
-___ writeImage(data):
-    fout _ w..
-
-    ___
-        fout _ o..('Camera.png', 'wb')
-        fout.write(data)
-
-    ______ IO.. __ e:
-
-        print(e)
-        ___.e.. 1)
-
-    f__
-
-        __ fout:
-            fout.c..
-
-
-con _ w..
-
-___
-    con _ ?.c..('ydb.db')
-
-    cur _ con.c..
-    cur.e..("S.. data F.. images LIMIT 1")
-    data _ cur.f_o..[0]
-
-    writeImage(data)
-
-
-______ ?.Er.. __ e:
-
-    print(e)
-    ___.e.. 1)
-
-f__
-
-    __ con:
-        con.c..
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+#
+# _____ ? __ ?
+# _____ ___
+#
+#
+# ___ writeImage data
+#     fout _ w..
+#
+#     ___
+#         fout _ o.. 'Camera.png' __
+#         ?.w.. ?
+#
+#     ______ IO.. __ e
+#
+#         print ?
+#         ___.e.. 1
+#
+#     f__
+#
+#         __ f..
+#             ?.c..
+#
+#
+# con _ w..
+#
+# ___
+#     con _ ?.c.. ydb.db
+#
+#     cur _ ?.c..
+#     ?.e..( S.. data F.. images LIMIT 1"
+#     data _ ?.f_o.. 0
+#
+#     w.. ?
+#
+#
+# ______ ?.Er.. __ e
+#
+#     print ?
+#     ___.e.. 1
+#
+# f__
+#
+#     __ c..
+#         ?.c..
