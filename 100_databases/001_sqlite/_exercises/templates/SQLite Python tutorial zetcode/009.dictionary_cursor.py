e576b9c51299b6ba4b09e2a60081439676853b27
Grey <sergejyurskyj@yahoo.com>
2020-05-16 20:26:39 -0700

    Committed SQLite Python tutorial zetcode 009.dictionary_cursor template

--- 100_databases/001_sqlite/_exercises/templates/SQLite Python tutorial zetcode/009.dictionary_cursor.py
+++ 100_databases/001_sqlite/_exercises/templates/SQLite Python tutorial zetcode/009.dictionary_cursor.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-_____ ? __ ?
-
-con _ ?.c..('ydb.db')
-
-w__ con:
-
-    con.r_f.. _ ?.Row
-
-    cur _ con.c..
-    cur.e..("S.. * F.. cars")
-
-    rows _ cur.f_a..
-
-    ___ row __ rows:
-        print _*{row['id']} {row['name']} {row['price']}")
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+#
+# _____ ? __ ?
+#
+# con _ ?.c.. ydb.db
+#
+# w__ ?
+#
+#     ?.r_f.. _ ?.R
+#
+#     cur _ ?.c..
+#     ?.e.. "S.. * F.. cars"
+#
+#     rows _ ?.f_a..
+#
+#     ___ row __ ?
+#         print _* ? 'id' ? 'name'  ?'price'
