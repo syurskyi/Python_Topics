b3567d1391bdcb8207a6a9aa8fae6a6e97c18a0a
Grey <sergejyurskyj@yahoo.com>
2020-05-16 20:41:43 -0700

    Committed SQLite Python tutorial zetcode 019.autocommit template

--- 100_databases/001_sqlite/_exercises/templates/SQLite Python tutorial zetcode/019.autocommit.py
+++ 100_databases/001_sqlite/_exercises/templates/SQLite Python tutorial zetcode/019.autocommit.py
@@ -1,29 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-_____ ? __ ?
-_____ ___
-
-con _ w..
-
-___
-    con _ ?.c..('ydb.db', isolation_level_None)
-
-    cur _ con.c..
-
-    cur.e..("D.. T.. I. E.. friends")
-    cur.e..("C.. T.. friends(id IN.. P.. K.., name T...)")
-    cur.e..("I.. I.. friends(name) V.. ('Tom')")
-    cur.e..("I.. I.. friends(name) V.. ('Rebecca')")
-    cur.e..("I.. I.. friends(name) V.. ('Jim')")
-    cur.e..("I.. I.. friends(name) V.. ('Robert')")
-
-______ ?.Er.. __ e:
-
-    print(e)
-    ___.e.. 1)
-
-f__
-
-    __ con:
-        con.c..
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+#
+# _____ ? __ ?
+# _____ ___
+#
+# con _ w..
+#
+# ___
+#     con _ ?.c.. ydb.db isolation_level_N..
+#
+#     cur _ ?.c..
+#
+#     ?.e.. "D.. T.. I. E.. friends"
+#     ?.e.. "C.. T.. friends(id IN.. P.. K.., name T...)"
+#     ?.e.. "I.. I.. friends(name) V.. ('Tom')"
+#     ?.e.. "I.. I.. friends(name) V.. ('Rebecca')"
+#     ?.e.. "I.. I.. friends(name) V.. ('Jim')"
+#     ?.e.. "I.. I.. friends(name) V.. ('Robert')"
+#
+# ______ ?.Er.. __ e
+#
+#     print ?
+#     ___.e.. 1
+#
+# f__
+#
+#     __ c..
+#         ?.c..
