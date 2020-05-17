e18ad00af557cfac241d9a85de534a63b3cd5036
Grey <sergejyurskyj@yahoo.com>
2020-05-17 14:24:23 -0700

    Committed 006_Call PostgreSQL Stored Procedures template

--- 100_databases/001_sqlite/_exercises/templates/templates/PostgreSQL Python postgresqltutorial/006_Call PostgreSQL Stored Procedures/stored_procedure.py
+++ 100_databases/001_sqlite/_exercises/templates/templates/PostgreSQL Python postgresqltutorial/006_Call PostgreSQL Stored Procedures/stored_procedure.py
@@ -1,34 +1,34 @@
-#!/usr/bin/python
-_____ ?
-____ c.. _____ c..
-
-
-___ get_parts(vendor_id):
-    """ get parts provided by a vendor specified by the vendor_id """
-    conn _ w..
-    ___
-        # read database configuration
-        params _ c..()
-        # connect to the PostgreSQL database
-        conn _ ?.c.. $$p..
-        # create a cursor object for execution
-        cur _ conn.c..
-        # another way to call a stored procedure
-        # cur.execute("S.. * F.. get_parts_by_vendor( %s); ",(vendor_id,))
-        cur.callproc('get_parts_by_vendor', (vendor_id,))
-        # process the result set
-        row _ cur.f_o..
-        w__ row __ no. w..:
-            print(row)
-            row _ cur.f_o..
-        # close the communication with the PostgreSQL database server
-        cur.c..
-    ______ (E.., ?.DE..) __ error:
-        print ?
-    f__
-        __ conn __ no. w..:
-            conn.c..
-
-
-__ _____ __ ______
-    get_parts(1)
+# #!/usr/bin/python
+# _____ ?
+# ____ c.. _____ c..
+#
+#
+# ___ get_parts vendor_id
+#     """ get parts provided by a vendor specified by the vendor_id """
+#     conn _ w..
+#     ___
+#         # read database configuration
+#         params _ c..
+#         # connect to the PostgreSQL database
+#         conn _ ?.c.. $$p..
+#         # create a cursor object for execution
+#         cur _ ?.c..
+#         # another way to call a stored procedure
+#         # cur.execute("S.. * F.. get_parts_by_vendor( %s); ",(vendor_id,))
+#         ?.c_p..('get_parts_by_vendor'  v_i..
+#         # process the result set
+#         row _ ?.f_o..
+#         w__ ? __ no. w..
+#             print ?
+#             row _ ?.f_o..
+#         # close the communication with the PostgreSQL database server
+#         cur.c..
+#     ______  E.., ?.DE.. __ error
+#         print ?
+#     f__
+#         __ c.. __ no. w..
+#             ?.c..
+#
+#
+# __ _____ __ ______
+#     ? 1
