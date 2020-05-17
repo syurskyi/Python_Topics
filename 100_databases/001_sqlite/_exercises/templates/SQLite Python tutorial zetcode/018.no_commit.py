57c647d5ee2a0a3b80bf54f1299e2eca5159669f
Grey <sergejyurskyj@yahoo.com>
2020-05-16 20:40:38 -0700

    Committed SQLite Python tutorial zetcode 018.no_commit template

--- 100_databases/001_sqlite/_exercises/templates/SQLite Python tutorial zetcode/018.no_commit.py
+++ 100_databases/001_sqlite/_exercises/templates/SQLite Python tutorial zetcode/018.no_commit.py
@@ -1,33 +1,33 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-_____ ? __ ?
-_____ ___
-
-con _ w..
-
-___
-    con _ ?.c..('ydb.db')
-
-    cur _ con.c..
-    cur.e..("D.. T.. I. E.. friends")
-    cur.e..("C.. T.. friends(id IN.. P.. K.., name T...)")
-    cur.e..("I.. I.. friends(name) V.. ('Tom')")
-    cur.e..("I.. I.. friends(name) V.. ('Rebecca')")
-    cur.e..("I.. I.. friends(name) V.. ('Jim')")
-    cur.e..("I.. I.. friends(name) V.. ('Robert')")
-
-    # con.c..
-
-______ ?.Er.. __ e:
-
-    __ con:
-        con.r..
-
-    print(e)
-    ___.e.. 1)
-
-f__
-
-    __ con:
-        con.c..
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+#
+# _____ ? __ ?
+# _____ ___
+#
+# con _ w..
+#
+# ___
+#     con _ ?.c.. ydb.db
+#
+#     cur _ con.c..
+#     ?.e.. "D.. T.. I. E.. friends"
+#     ?.e.. "C.. T.. friends(id IN.. P.. K.., name T...)"
+#     ?.e.. "I.. I.. friends(name) V.. ('Tom')"
+#     ?.e.. "I.. I.. friends(name) V.. ('Rebecca')"
+#     ?.e.. "I.. I.. friends(name) V.. ('Jim')"
+#     ?.e.. "I.. I.. friends(name) V.. ('Robert')"
+#
+#     # con.c..
+#
+# ______ ?.Er.. __ e
+#
+#     __ c..
+#         ?.r..
+#
+#     print ?
+#     ___.?.. 1
+#
+# f__
+#
+#     __ c..
+#         ?.c..
