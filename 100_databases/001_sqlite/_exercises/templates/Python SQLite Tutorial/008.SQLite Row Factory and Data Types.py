0a655653e4641338dc2a77a54a90d8d4db9b169c
Grey <sergejyurskyj@yahoo.com>
2020-05-16 18:46:53 -0700

    Committed 008.SQLite Row Factory and Data Types template

--- 100_databases/001_sqlite/_exercises/templates/Python SQLite Tutorial/008.SQLite Row Factory and Data Types.py
+++ 100_databases/001_sqlite/_exercises/templates/Python SQLite Tutorial/008.SQLite Row Factory and Data Types.py
@@ -1,11 +1,11 @@
-_____ ?
-
-db _ ?.c..('mydb.db')
-db.r_f.. _ ?.Row
-cursor _ db.c..
-cursor.e..('''S.. name, email, phone F.. users''')
-___ row __ cursor:
-    # row['name'] returns the name column in the query, row['email'] returns email column.
-    print('@ : @, @'.f..(row['name'], row['email'], row['phone']))
-db.c..
-
+# _____ ?
+#
+# db _ ?.c.. 'mydb.db'
+# ?.r_f.. _ ?.R..
+# cursor _ db.c..
+# ?.e.. '''S.. name, email, phone F.. users'''
+# ___ row __ ?
+#     # row['name'] returns the name column in the query, row['email'] returns email column.
+#     print('@ : @, @'.f.. ?'name' ? 'email' ? 'phone'
+# ?.c..
+#
