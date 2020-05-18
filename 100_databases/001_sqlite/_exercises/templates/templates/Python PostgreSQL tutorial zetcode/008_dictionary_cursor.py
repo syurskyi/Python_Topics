284870b54aa54019ef0d2700e802818948f9b271
Grey <sergejyurskyj@yahoo.com>
2020-05-17 18:44:08 -0700

    Committed Python PostgreSQL tutorial zetcode 008_dictionary_cursor template

--- 100_databases/001_sqlite/_exercises/templates/templates/Python PostgreSQL tutorial zetcode/008_dictionary_cursor.py
+++ 100_databases/001_sqlite/_exercises/templates/templates/Python PostgreSQL tutorial zetcode/008_dictionary_cursor.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-_____ ?
-_____ ?.extras
-
-con _ ?.c..(d.._'testdb', u.._'syurskyi',
-                    p.._'1234')
-
-w__ con:
-
-    cursor _ con.cursor(cursor_factory_?.extras.DictCursor)
-    ?.e..("S.. _ F.. cars")
-
-    rows _ ?.f_a..
-
-    ___ row __ rows:
-        print _*{row['id']} {row['name']} {row['price']}")
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+#
+# _____ ?
+# _____ ?.extras
+#
+# con _ ?.c.. d.._'testdb' u.._'syurskyi'
+#                     p.._'1234'
+#
+# w__ ?
+#
+#     c.. _ ?.c.. cursor_factory_?.e__.DC..
+#     ?.e.. "S.. _ F.. cars"
+#
+#     rows _ ?.f_a..
+#
+#     ___ row __ ?
+#         print _* ? 'id' ? 'name' ? 'price'
