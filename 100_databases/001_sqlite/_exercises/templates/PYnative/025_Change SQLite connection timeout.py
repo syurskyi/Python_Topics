bdc5f30b43577032630bc64d4964b32a04c965f6
Grey <sergejyurskyj@yahoo.com>
2020-05-18 15:14:02 -0700

    Committed 025_Change SQLite connection timeout templates

--- 100_databases/001_sqlite/_exercises/templates/PYnative/025_Change SQLite connection timeout.py
+++ 100_databases/001_sqlite/_exercises/templates/PYnative/025_Change SQLite connection timeout.py
@@ -1,29 +1,29 @@
-_____ ?
-
-___ readSqliteTable
-    ___
-        sqliteConnection _ ?.c..('SQLite_Python.db', timeout_20)
-        cursor _ ?.c..
-        print("Connected to SQLite")
-
-        sqlite_select_query _ """S.. count(_) f.. SqliteDb_developers"""
-        ?.e..(sqlite_select_query)
-        totalRows _ ?.f_o..
-        print("Total rows are:  ", totalRows)
-        ?.c..
-
-    _____ ?.E.. __ error:
-        print("Failed to read data from sqlite table" ?
-    f..
-        __ (?
-            ?.c..
-            print("The Sqlite connection is closed")
-
-readSqliteTable()
-
-
-# Output:
+# _____ ?
 #
-# Connected to SQLite
-# Total rows are:   (2,)
-# The Sqlite connection is closed
+# ___ readSqliteTable
+#     ___
+#         sqliteConnection _ ?.c.. 'SQLite_Python.db' t.._20
+#         cursor _ ?.c..
+#         print("Connected to SQLite")
+#
+#         sqlite_select_query _ """S.. count(_) f.. SqliteDb_developers"""
+#         ?.e.. ?
+#         totalRows _ ?.f_o..
+#         print("Total rows are:  " ?
+#         ?.c..
+#
+#     _____ ?.E.. __ error
+#         print("Failed to read data from sqlite table" ?
+#     f..
+#         __ (?
+#             ?.c..
+#             print("The Sqlite connection is closed")
+#
+# ?
+#
+#
+# # Output:
+# #
+# # Connected to SQLite
+# # Total rows are:   (2,)
+# # The Sqlite connection is closed
