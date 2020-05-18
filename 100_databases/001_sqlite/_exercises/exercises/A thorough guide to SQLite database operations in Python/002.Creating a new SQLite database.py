4070aaf0a0bb7431fdb6d701cff22ae4717e1e69
Grey <sergejyurskyj@yahoo.com>
2020-05-18 12:40:08 -0700

    Committed 002.Creating a new SQLite database exercises

--- 100_databases/001_sqlite/_exercises/exercises/A thorough guide to SQLite database operations in Python/002.Creating a new SQLite database.py
+++ 100_databases/001_sqlite/_exercises/exercises/A thorough guide to SQLite database operations in Python/002.Creating a new SQLite database.py
@@ -1,24 +1,23 @@
-# _____ _3
-#
-# sqlite_file _ 'my_first_db.sqlite'    # name of the sqlite database file
-# table_name1 _ 'my_table_1'  # name of the table to be created
-# table_name2 _ 'my_table_2'  # name of the table to be created
-# new_field _ 'my_1st_column' # name of the column
-# field_type _ 'IN..'  # column data type
-#
-# # Connecting to the database file
-# conn _ _3.co.. s_f..
-# c _ ?.cu..
-#
-# # Creating a new SQLite table with 1 column
-# ?.ex..('C.. T.. @ (@ @)'\
-#         .f.. tn_? nf_? ft_?
-#
-# # Creating a second table with 1 column and set it as P.. K..
-# # note that P.. K.. column must consist of unique values!
-# ?.ex..('C.. T.. @ (@ @ P.. K..)'\
-#         .f.. tn_? nf_? ft_?
-#
-# # Committing changes and closing the connection to the database file
-# ?.co..
-# ?.cl..
+import sqlite3
+
+sqlite_file = 'my_first_db.sqlite'    # name of the sqlite database file
+table_name1 = 'my_table_1'  # name of the table to be created
+table_name2 = 'my_table_2'  # name of the table to be created
+new_field = 'my_1st_column' # name of the column
+field_type = 'INTEGER'  # column data type
+
+# Connecting to the database file
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+# Creating a new SQLite table with 1 column
+c.execute('CREATE TABLE {tn} {nf} {ft})'.format(tn=table_name1, nf=new_field, ft=field_type))
+
+# Creating a second table with 1 column and set it as P.. K..
+# note that P.. K.. column must consist of unique values!
+c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
+        .format(tn=table_name2, nf=new_field, ft=field_type))
+
+# Committing changes and closing the connection to the database file
+conn.commit()
+conn.close()
