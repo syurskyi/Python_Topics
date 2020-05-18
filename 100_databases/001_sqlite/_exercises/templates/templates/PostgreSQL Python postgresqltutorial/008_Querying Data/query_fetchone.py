b74228e91cf6b07a91552a2e4ad36145fb2716c9
Grey <sergejyurskyj@yahoo.com>
2020-05-17 17:28:45 -0700

    Committed query_fetchone template

--- 100_databases/001_sqlite/_exercises/templates/templates/PostgreSQL Python postgresqltutorial/008_Querying Data/query_fetchone.py
+++ 100_databases/001_sqlite/_exercises/templates/templates/PostgreSQL Python postgresqltutorial/008_Querying Data/query_fetchone.py
@@ -1,30 +1,30 @@
-#!/usr/bin/python
-_____ ?
-____ c.. _____ c..
-
-
-___ get_vendors():
-    """ query data from the vendors table """
-    conn _ w..
-    ___
-        params _ c..()
-        conn _ ?.c.. $$p..
-        cur _ conn.c..
-        cur.e..("S.. vendor_id, vendor_name F.. vendors O.. B. vendor_name")
-        print("The number of parts: ", cur.rowcount)
-        row _ cur.f_o..
-
-        w__ row __ no. w..:
-            print(row)
-            row _ cur.f_o..
-
-        cur.c..
-    ______ (E.., ?.DE..) __ error:
-        print ?
-    f__
-        __ conn __ no. w..:
-            conn.c..
-
-
-__ _____ __ ______
-    get_vendors()
+# #!/usr/bin/python
+# _____ ?
+# ____ c.. _____ c..
+#
+#
+# ___ get_vendors
+#     """ query data from the vendors table """
+#     conn _ w..
+#     ___
+#         params _ c..
+#         conn _ ?.c.. $$p..
+#         cur _ ?.c..
+#         ?.e..("S.. vendor_id, vendor_name F.. vendors O.. B. vendor_name")
+#         print("The number of parts: " ?.r..
+#         row _ ?.f_o..
+#
+#         w__ r.. __ no. w..
+#             print ?
+#             row _ ?.f_o..
+#
+#         ?.c..
+#     ______ E.. ?.DE.. __ error
+#         print ?
+#     f__
+#         __ c.. __ no. w..
+#             ?.c..
+#
+#
+# __ _____ __ ______
+#     ?
