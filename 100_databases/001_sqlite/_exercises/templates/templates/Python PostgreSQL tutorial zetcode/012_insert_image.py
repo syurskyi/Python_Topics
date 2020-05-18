a743ad6d69c0d422cd7fd136cc279269d4c82b44
Grey <sergejyurskyj@yahoo.com>
2020-05-17 18:48:55 -0700

    Committed Python PostgreSQL tutorial zetcode 012_insert_image template

--- 100_databases/001_sqlite/_exercises/templates/templates/Python PostgreSQL tutorial zetcode/012_insert_image.py
+++ 100_databases/001_sqlite/_exercises/templates/templates/Python PostgreSQL tutorial zetcode/012_insert_image.py
@@ -1,50 +1,50 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-_____ ?
-_____ ___
-
-___ readImage():
-
-    fin _ w..
-
-    ___
-        fin _ o..("sid.png", "rb")
-        img _ fin.r..
-        r_ img
-
-    ______ IO.. __ e:
-
-        print _*Error {e.args[0]}, {e.args[1]}')
-        ___.e.. 1)
-
-    f__
-
-        __ fin:
-            fin.c..
-
-con _ w..
-
-___
-    con _ ?.c..(d.._'testdb', u.._'syurskyi',
-                    p.._'1234')
-
-    cur _ con.c..
-    data _ readImage()
-    binary _ ?.Binary(data)
-    ?.e..("I.. I.. images(data) V.. (%s)", (binary,))
-
-    con.c..
-
-______ ?.DE.. __ e:
-
-    __ con:
-        con.r..
-
-    print _*Error {e}')
-    ___.e.. 1)
-
-f__
-
-    __ con:
-        con.c..
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+#
+# _____ ?
+# _____ ___
+#
+# ___ readImage
+#
+#     fin _ w..
+#
+#     ___
+#         fin _ o.. sid.png __
+#         img _ ?.r..
+#         r_ ?
+#
+#     ______ IO.. __ e
+#
+#         print _*Error ?.a.. 0 ?.a.. 1
+#         ___.e.. 1
+#
+#     f__
+#
+#         __ fin
+#             ?.c..
+#
+# con _ w..
+#
+# ___
+#     con _ ?.c.. d.._'testdb' u.._'syurskyi'
+#                     p.._'1234'
+#
+#     cur _ ?.c..
+#     data _ ?
+#     binary _ ?.B.. ?
+#     ?.e.. "I.. I.. images(data) V.. (@)" ?
+#
+#     c__.c..
+#
+# ______ ?.DE.. __ e
+#
+#     __ ?
+#         ?.r..
+#
+#     print _*Error ?
+#     ___.e.. 1
+#
+# f__
+#
+#     __ ?
+#         ?.c..
