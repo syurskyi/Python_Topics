2f989d923921746fbf16c135bd77a38bbf575ee4
Grey <sergejyurskyj@yahoo.com>
2020-05-17 18:02:18 -0700

    Committed 003_Python PostgreSQL Insert, Update and Delete table data to Perform CRUD Operations template

--- 100_databases/001_sqlite/_exercises/templates/templates/Pynative/003_Python PostgreSQL Insert, Update and Delete table data to Perform CRUD Operations/006_delete_multiple_rows.py
+++ 100_databases/001_sqlite/_exercises/templates/templates/Pynative/003_Python PostgreSQL Insert, Update and Delete table data to Perform CRUD Operations/006_delete_multiple_rows.py
@@ -1,35 +1,35 @@
-_____ ?
-
-___ deleteInBulk(records):
-    ___
-        ps_connection _ ?.c..(u.._"syurskyi",
-                                         p.._"1234",
-                                         h.._"127.0.0.1",
-                                         p.._"5432",
-                                         d.._"postgres_db")
-        cursor _ ps_connection.c..
-        ps_delete_query _ """D.. f.. mobile w.. id = %s"""
-        ?.e_m_(ps_delete_query, records)
-        ps_connection.c..
-
-        row_count _ ?.r..
-        print(row_count, "Record Deleted")
-
-    ______ (E.., ?.Er..) __ error:
-        print("Error while connecting to PostgreSQL", error)
-
-    f__
-        # closing database connection.
-        __ (ps_connection):
-            ?.c..
-            ps_connection.c..
-            print("PostgreSQL connection is closed")
-
-# list of tuples contains database IDs
-tuples _ [(5, ), (4, ), (3, )]
-deleteInBulk(tuples)
-
-# Output:
+# _____ ?
 #
-# 2 Records Deleted
-# PostgreSQL connection is closed
+# ___ deleteInBulk records
+#     ___
+#         ps_connection _ ?.c.. u.._"syurskyi"
+#                                          p.._"1234"
+#                                          h.._"127.0.0.1"
+#                                          p.._"5432"
+#                                          d.._"postgres_db"
+#         cursor _ ?.c..
+#         ps_delete_query _ """D.. f.. mobile w.. id = @"""
+#         ?.e_m_ ? r..
+#         ?.c..
+#
+#         row_count _ ?.r..
+#         print ? "Record Deleted"
+#
+#     ______ E.., ?.Er.. __ error
+#         print("Error while connecting to PostgreSQL" ?
+#
+#     f__
+#         # closing database connection.
+#         __ (p..
+#             ?.c..
+#             ?.c..
+#             print("PostgreSQL connection is closed")
+#
+# # list of tuples contains database IDs
+# tuples _ [(5, ), (4, ), (3, )]
+# ? ?
+#
+# # Output:
+# #
+# # 2 Records Deleted
+# # PostgreSQL connection is closed
