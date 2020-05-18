d8f285d63dec0285ecbfd78fc20ef5c2fb43aab4
Grey <sergejyurskyj@yahoo.com>
2020-05-17 18:34:39 -0700

    Committed Python PostgreSQL tutorial zetcode 002_version2 template

--- 100_databases/001_sqlite/_exercises/templates/templates/Python PostgreSQL tutorial zetcode/002_version2.py
+++ 100_databases/001_sqlite/_exercises/templates/templates/Python PostgreSQL tutorial zetcode/002_version2.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-_____ ?
-
-
-con _ ?.c..(d.._'testdb', u.._'syurskyi',
-    p.._'1234')
-
-w__ con:
-
-    cur _ con.c..
-    cur.e..('S.. version()')
-
-    version _ cur.f_o..[0]
-    print(version)
+# #!/usr/bin/env python
+# # -*- coding: utf-8 -*-
+#
+# _____ ?
+#
+#
+# con _ ?.c.. d.._'testdb' u.._'syurskyi'
+#     p.._'1234'
+#
+# w__ ?
+#
+#     cur _ ?.c..
+#     ?.e.. 'S.. version()'
+#
+#     version _ ?.f_o.. 0
+#     print ?
