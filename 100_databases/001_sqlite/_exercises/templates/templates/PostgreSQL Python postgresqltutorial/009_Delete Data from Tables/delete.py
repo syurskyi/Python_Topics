69b1f75be0672cb8b195aa890776775a56750679
Grey <sergejyurskyj@yahoo.com>
2020-05-17 17:30:10 -0700

    Committed 009_Delete Data from Tables template

--- 100_databases/001_sqlite/_exercises/templates/templates/PostgreSQL Python postgresqltutorial/009_Delete Data from Tables/delete.py
+++ 100_databases/001_sqlite/_exercises/templates/templates/PostgreSQL Python postgresqltutorial/009_Delete Data from Tables/delete.py
@@ -1,35 +1,35 @@
-#!/usr/bin/python
-
-_____ ?
-____ c.. _____ c..
-
-___ delete_part(part_id):
-    """ delete part by part id """
-    conn _ w..
-    rows_deleted _ 0
-    ___
-        # read database configuration
-        params _ c..()
-        # connect to the PostgreSQL database
-        conn _ ?.c.. $$p..
-        # create a new cursor
-        cur _ conn.c..
-        # execute the U..  statement
-        cur.e..("D.. F.. parts W.. part_id = %s", (part_id,))
-        # get the number of updated rows
-        rows_deleted _ cur.rowcount
-        # Commit the changes to the database
-        conn.c..
-        # Close communication with the PostgreSQL database
-        cur.c..
-    ______ (E.., ?.DE..) __ error:
-        print ?
-    f__
-        __ conn __ no. w..:
-            conn.c..
-
-    r_ rows_deleted
-
-__ _____ __ ______
-    deleted_rows _ delete_part(2)
-    print('The number of deleted rows: ', deleted_rows)
+# #!/usr/bin/python
+#
+# _____ ?
+# ____ c.. _____ c..
+#
+# ___ delete_part part_id
+#     """ delete part by part id """
+#     conn _ w..
+#     rows_deleted _ 0
+#     ___
+#         # read database configuration
+#         params _ c..
+#         # connect to the PostgreSQL database
+#         conn _ ?.c.. $$p..
+#         # create a new cursor
+#         cur _ ?.c..
+#         # execute the U..  statement
+#         ?.e..("D.. F.. parts W.. part_id = @", p_i.
+#         # get the number of updated rows
+#         rows_deleted _ ?.r..
+#         # Commit the changes to the database
+#         ?.c..
+#         # Close communication with the PostgreSQL database
+#         ?.c..
+#     ______ E.., ?.DE.. __ error
+#         print ?
+#     f__
+#         __ c.. __ no. w..
+#             ?.c..
+#
+#     r_ ?
+#
+# __ _____ __ ______
+#     deleted_rows _ ? 2
+#     print('The number of deleted rows: ' ?
