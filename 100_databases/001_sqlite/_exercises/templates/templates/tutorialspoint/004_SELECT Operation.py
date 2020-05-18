a9d8d70f22864419548f6ed738e6fab89bfd323f
Grey <sergejyurskyj@yahoo.com>
2020-05-17 18:19:56 -0700

    Committed 004_SELECT Operation template

--- 100_databases/001_sqlite/_exercises/templates/templates/tutorialspoint/004_SELECT Operation.py
+++ 100_databases/001_sqlite/_exercises/templates/templates/tutorialspoint/004_SELECT Operation.py
@@ -1,44 +1,44 @@
-#!/usr/bin/python
-
-_____ ?
-
-conn _ ?.c..(d.. _ "testdb", u.. _ "postgres", p.. _ "pass123", h.. _ "127.0.0.1", p.. _ "5432")
-print("Opened database successfully")
-
-cur _ conn.c..
-
-cur.e..("S.. id, name, address, salary  f.. COMPANY")
-rows _ cur.f_a..
-___ row __ rows:
-   print("ID = ", row[0])
-   print("NAME = ", row[1])
-   print("ADDRESS = ", row[2])
-   print("SALARY = ", row[3], "\n")
-
-print("Operation done successfully")
-conn.c..
-
-
-
-# Opened database successfully
-# ID =  1
-# NAME =  Paul
-# ADDRESS =  California
-# SALARY =  20000.0
+# #!/usr/bin/python
 #
-# ID =  2
-# NAME =  Allen
-# ADDRESS =  Texas
-# SALARY =  15000.0
+# _____ ?
 #
-# ID =  3
-# NAME =  Teddy
-# ADDRESS =  Norway
-# SALARY =  20000.0
+# conn _ ?.c.. d.. _ "testdb" u.. _ "postgres" p.. _ "pass123" h.. _ "127.0.0.1" p.. _ "5432"
+# print("Opened database successfully")
 #
-# ID =  4
-# NAME =  Mark
-# ADDRESS =  Rich-Mond
-# SALARY =  65000.0
+# cur _ ?.c..
 #
-# Operation done successfully
+# ?.e.. "S.. id, name, address, salary  f.. COMPANY"
+# rows _ ?.f_a..
+# ___ row __ ?
+#    print("ID = ", ? 0
+#    print("NAME = ", ? 1
+#    print("ADDRESS = ", ? 2
+#    print("SALARY = ", ? 3 "\n")
+#
+# print("Operation done successfully")
+# ?.c..
+#
+#
+#
+# # Opened database successfully
+# # ID =  1
+# # NAME =  Paul
+# # ADDRESS =  California
+# # SALARY =  20000.0
+# #
+# # ID =  2
+# # NAME =  Allen
+# # ADDRESS =  Texas
+# # SALARY =  15000.0
+# #
+# # ID =  3
+# # NAME =  Teddy
+# # ADDRESS =  Norway
+# # SALARY =  20000.0
+# #
+# # ID =  4
+# # NAME =  Mark
+# # ADDRESS =  Rich-Mond
+# # SALARY =  65000.0
+# #
+# # Operation done successfully
