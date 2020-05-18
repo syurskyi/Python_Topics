d955f30b3f30ce843b2f332d9d6dd0501a75b6cd
Grey <sergejyurskyj@yahoo.com>
2020-05-18 14:36:52 -0700

    Committed 019_Redefine existing SQLite SQL Functions templates

--- 100_databases/001_sqlite/_exercises/templates/PYnative/019_Redefine existing SQLite SQL Functions.py
+++ 100_databases/001_sqlite/_exercises/templates/PYnative/019_Redefine existing SQLite SQL Functions.py
@@ -1,32 +1,32 @@
-_____ ?
-
-___ lower(string
-    r_ st.(string).upper()
-
-___ getDeveloperName(id
-    ___
-        sqliteConnection _ ?.c..('SQLite_Python.db')
-        cursor _ ?.c..
-        print("Connected to SQLite")
-
-        ?.create_function("lower", 1, lower)
-        select_query _ "S.. lower(name) FROM SqliteDb_developers w.. id = ?"
-        ?.e..(select_query, (id,))
-        name _ ?.f_o..
-        print("Developer Name is", name)
-        ?.c..
-
-    _____ ?.E.. __ error:
-        print("Failed to read data from sqlite table" ?
-    f..
-        __ (?
-            ?.c..
-            print("sqlite connection is closed")
-
-getDeveloperName(1)
-
-# Output:
+# _____ ?
 #
-# Connected to SQLite
-# Developer Name is ('JAMES',)
-# sqlite connection is closed
+# ___ lower string
+#     r_ st.? .u..
+#
+# ___ getDeveloperName id
+#     ___
+#         sqliteConnection _ ?.c.. 'SQLite_Python.db'
+#         cursor _ ?.c..
+#         print("Connected to SQLite")
+#
+#         ?.create_function "lower" 1 ?
+#         select_query _ "S.. l..(name) F.. SqliteDb_developers w.. id = ?"
+#         ?.e.. ? ?
+#         name _ ?.f_o..
+#         print("Developer Name is" ?
+#         ?.c..
+#
+#     _____ ?.E.. __ error
+#         print("Failed to read data from sqlite table" ?
+#     f..
+#         __ (?
+#             ?.c..
+#             print("sqlite connection is closed")
+#
+# ? 1
+#
+# # Output:
+# #
+# # Connected to SQLite
+# # Developer Name is ('JAMES',)
+# # sqlite connection is closed
