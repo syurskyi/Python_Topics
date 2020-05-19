3b7024d61c074f0c9f0a49545039c0480f432d61
Grey <sergejyurskyj@yahoo.com>
2020-05-18 20:22:59 -0700

    Committed create_sqlite_ver2 exercises

--- 100_databases/001_sqlite/_exercises/exercises/Python for network engineers/create_sqlite_ver2.py
+++ 100_databases/001_sqlite/_exercises/exercises/Python for network engineers/create_sqlite_ver2.py
@@ -1,33 +1,33 @@
-# ______ _3
-# ______ __
-#
-# regex _ __.co.. ('(\S+) +(\S+) +\d+ +\S+ +(\d+) +(\S+)')
-#
-# result _    # list
-#
-# w___ o.. dhcp_snooping.txt __ data
-#     ___ line __ ?
-#         match _ r__.se.. ?
-#         __ ?
-#             r___.ap.. ? gr..
-#
-# conn _ _3.c.. dhcp_snooping.db
-#
-# print('Creating schema...')
-# w___ o.. dhcp_snooping_schema.sql _ __ f
-#     schema _ ?.r..
-#     ?.e.. ?
-# print('Done')
-#
-# print('Inserting DHCP Snooping data')
-#
-# ___ row __ r..
-#     ___
-#         w___ ?
-#             query _ I.. I.. dhcp |mac ip vlan interface
-#                        V.. @ @ @ @
-#             ?.e.. q.. r..
-#     ________ _3.I... __ e
-#         print('Error occured: ' ?
-#
-# ?.c..
+import sqlite3
+import re
+
+regex = re.compile('(\S+) +(\S+) +\d+ +\S+ +(\d+) +(\S+)')
+
+result = []    # list
+
+with open('dhcp_snooping.txt') as data:
+    for line in data:
+        match = regex.search(line)
+        if match:
+            result.append(match.groups())
+
+conn = sqlite3.connect('dhcp_snooping.db')
+
+print('Creating schema...')
+with open('dhcp_snooping_schema.sql', 'r') as f:
+    schema = f.read()
+    conn.executescript(schema)
+print('Done')
+
+print('Inserting DHCP Snooping data')
+
+for row in result:
+    try:
+        with conn:
+            query = '''INSERT INTO dhcp (mac, ip, vlan, interface)
+                       VALUES(?, ?, ?, ?)'''
+            conn.execute(query, row)
+    except sqlite3.IntegrityError as e:
+        print('Error occured: ', e)
+
+conn.close()
