a4fce16703e97b49162cab777c49b33dabcd9d08
Grey <sergejyurskyj@yahoo.com>
2020-05-18 19:26:37 -0700

    Committed 002_Create SQLite table from Python exercises

--- 100_databases/001_sqlite/_exercises/exercises/PYnative/002_Create SQLite table from Python.py
+++ 100_databases/001_sqlite/_exercises/exercises/PYnative/002_Create SQLite table from Python.py
@@ -1,30 +1,30 @@
-# _____ ?
-#
-# ___
-#     sqliteConnection _ ?.c.. 'SQLite_Python.db'
-#     sqlite_create_table_query _ '''C.. T.. SqliteDb_developers (
-#                                 id I.. P.. K..,
-#                                 name T.. N.. N..,
-#                                 email text N.. N.. U..,
-#                                 joining_date datetime,
-#                                 salary R.. N.. N..);'''
-#
-#     cursor _ ?.c..
-#     print("Successfully Connected to SQLite")
-#     ?.e.. ?
-#     ?.c..
-#     print("SQLite table created")
-#
-#     ?.c..
-#
-# _____ ?.E.. __ error
-#     print("Error while creating a sqlite table" ?
-# f..
-#     __ (?
-#         ?.c..
-#         print("sqlite connection is closed")
-#
-#
-# # Successfully Connected to SQLite
-# # SQLite table created:
-# # the sqlite connection is closed
+import sqlite3
+
+try:
+    sqliteConnection = sqlite3.connect('SQLite_Python.db')
+    sqlite_create_table_query = '''CREATE TABLE SqliteDb_developers (
+                                id INTEGER PRIMARY KEY,
+                                name TEXT NOT NULL,
+                                email text NOT NULL UNIQUE,
+                                joining_date datetime,
+                                salary REAL NOT NULL);'''
+
+    cursor = sqliteConnection.cursor()
+    print("Successfully Connected to SQLite")
+    cursor.execute(sqlite_create_table_query)
+    sqliteConnection.commit()
+    print("SQLite table created")
+
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Error while creating a sqlite table", error)
+finally:
+    if (sqliteConnection):
+        sqliteConnection.close()
+        print("sqlite connection is closed")
+
+
+# Successfully Connected to SQLite
+# SQLite table created:
+# the sqlite connection is closed
