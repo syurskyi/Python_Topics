33f2f0939d0422bbf71b4a3e8b82fcb09f0d31ca
Grey <sergejyurskyj@yahoo.com>
2020-05-18 19:31:56 -0700

    Committed 003_insert a single row exercises

--- 100_databases/001_sqlite/_exercises/exercises/PYnative/003_insert a single row.py
+++ 100_databases/001_sqlite/_exercises/exercises/PYnative/003_insert a single row.py
@@ -1,27 +1,27 @@
-# _____ ?
-#
-# ___
-#     sqliteConnection _ ?.c.. 'SQLite_Python.db'
-#     cursor _ ?.c..
-#     print("Successfully Connected to SQLite")
-#
-#     sqlite_insert_query _ """I.. I.. SqliteDb_developers
-#                           (id, name, email, joining_date, salary)
-#                            V..
-#                           (1,'James','james@pynative.com','2019-03-17',8000)"""
-#
-#     count _ ?.e.. ?
-#     ?.c..
-#     print("Record inserted successfully into SqliteDb_developers table ", ?.r..
-#     ?.c..
-#
-# _____ ?.E.. __ error
-#     print("Failed to insert data into sqlite table" ?
-# f..
-#     __ (?
-#         ?.c..
-#         print("The SQLite connection is closed")
-#
-# # Successfully Connected to SQLite
-# # Record inserted successfully into SqliteDb_developers table
-# # The SQLite connection is closed
+import sqlite3
+
+try:
+    sqliteConnection = sqlite3.connect('SQLite_Python.db')
+    cursor = sqliteConnection.cursor()
+    print("Successfully Connected to SQLite")
+
+    sqlite_insert_query = """INSERT INTO SqliteDb_developers
+                          (id, name, email, joining_date, salary)
+                           VALUES
+                          (1,'James','james@pynative.com','2019-03-17',8000)"""
+
+    count = cursor.execute(sqlite_insert_query)
+    sqliteConnection.commit()
+    print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Failed to insert data into sqlite table", error)
+finally:
+    if (sqliteConnection):
+        sqliteConnection.close()
+        print("The SQLite connection is closed")
+
+# Successfully Connected to SQLite
+# Record inserted successfully into SqliteDb_developers table
+# The SQLite connection is closed
