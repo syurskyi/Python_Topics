678e6f8d3bd1fe34c1caae8b96b1b2f1d18fffd3
Grey <sergejyurskyj@yahoo.com>
2020-05-19 21:17:58 -0700

    Committed example6_dump templates

--- 100_databases/001_sqlite/_exercises/templates/ITDVN/example6_dump.py
+++ 100_databases/001_sqlite/_exercises/templates/ITDVN/example6_dump.py
@@ -1,21 +1,21 @@
-_____ os
-_____ ?
-
-conn _ ?.c..(':memory:')
-?.e..
-    """C.. T.. "users" (
-           id INTEGER PRIMARY KEY,
-           first_name VARCHAR(30) NOT NULL,
-           last_name VARCHAR(30),
-           birthday VARCHAR(30)
-   )""")
-?.e.. """
-        I.. I.. users(id, first_name, last_name, birthday)
-        V.. (1, "Eugene", "Hatsko", "09-11-1992"),
-               (2, "Dmitry", "Ivanov", "01-09-1993")
-   """)
-
-with open('dump.sql', 'w') as f:
-    for line in ?.iterdump():
-        f.write('@\n' % line)
-        print('{}\n'.format(line))
+# _____ os
+# _____ ?
+#
+# conn _ ?.c.. m..
+# ?.e..
+#     """C.. T.. "users" (
+#            id I.. P.. K..
+#            first_name V..(30) N.. N..
+#            last_name V..(30),
+#            birthday V..(30)
+#    )""")
+# ?.e.. """
+#         I.. I.. users(id, first_name, last_name, birthday)
+#         V.. (1, "Eugene", "Hatsko", "09-11-1992"),
+#                (2, "Dmitry", "Ivanov", "01-09-1993")
+#    """)
+#
+# w__ o.. du__.s.. _ __ f
+#     __ line __ ?.i_d..
+#         ?.w.. '@\n' % ?
+#         print('@\n'.f.. ?
