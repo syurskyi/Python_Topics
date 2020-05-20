33d88dbb883e948c6473558d792586d782b3cbe4
Grey <sergejyurskyj@yahoo.com>
2020-05-19 21:01:06 -0700

    Committed example4_aggregation templates

--- 100_databases/001_sqlite/_exercises/templates/ITDVN/example5_repr_results.py
+++ 100_databases/001_sqlite/_exercises/templates/ITDVN/example5_repr_results.py
@@ -1,23 +1,23 @@
-_____ ?
-
-conn _ ?.c..('db.sqlite3')
-?.e..
-    """C.. T.. "users" (
-           id INTEGER PRIMARY KEY AUTOINCREMENT,
-           first_name,
-           last_name,
-           birthday
-   )""")
-?.e.. """
-        insert into users(id, first_name, last_name, birthday)
-        V.. (1, "Eugene", "Hatsko", "09-11-1992"),
-               (2, "Dmitry", "Ivanov", "01-09-1993")
-   """)
-
-?.row_factory _ ?.Row
-users _ ?.e.. 'S.. _ F.. "users"').f_a..
-
-user _ users[0]
-print(user.keys())
-print(user['id'], user['iD'])
-print(user['first_name'], user['first_NAME'], user['FIRST_NAME'])
+# _____ ?
+#
+# conn _ ?.c.. db.sqlite3
+# ?.e..
+#     """C.. T.. "users" (
+#            id I.. P.. K.. A..
+#            first_name,
+#            last_name,
+#            birthday
+#    )""")
+# ?.e.. """
+#         i.. i.. users(id, first_name, last_name, birthday)
+#         V.. (1, "Eugene", "Hatsko", "09-11-1992"),
+#                (2, "Dmitry", "Ivanov", "01-09-1993")
+#    """)
+#
+# ?.row_factory _ ?.R..
+# users _ ?.e.. 'S.. _ F.. "users"' .f_a..
+#
+# user _ ? 0
+# print ?.k..
+# print ? 'id' ? 'iD'
+# print ? 'first_name' ? 'first_NAME' ? 'FIRST_NAME'
