88a976a55d66beb307a135314b6d1eaeb73e9982
Grey <sergejyurskyj@yahoo.com>
2020-05-24 20:27:25 -0700

    Committed MongoDB exercise

--- 100_databases/003_mongodb/_exercises/exercises/MongoDB_for_Developers_with_Python/src/src/07_mongoengine/service_central/nosql/mongo_setup.py
+++
@@ -1,5 +0,0 @@
-# _____ m_e_
-#
-#
-# ___ global_init
-#     ?.r_c.. a_'core' n.._'demo_dealership'
--- 100_databases/003_mongodb/_exercises/exercises/MongoDB_for_Developers_with_Python/src/src/08_perf/big_dealership/nosql/service_record.py
+++
@@ -1,9 +0,0 @@
-# _____ d_t_
-# _____ m_e_
-#
-#
-# c_ ServiceRecord ?.ED..
-#     date _ ?.DTF.. d.._d_t_.d_t_.n..
-#     description _ ?.SF..
-#     price _ ?.FF..(r.._T..
-#     customer_rating _ ?.IF.. r.._T..  # 1 - 5 satisfaction level.
