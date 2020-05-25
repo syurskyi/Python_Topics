4988cc5372e1f5b0a5823491d53e8209d232acb8
Grey <sergejyurskyj@yahoo.com>
2020-05-25 10:11:02 -0700

    Committed 002_Creating a Collection exercises

--- 100_databases/003_mongodb/_exercises/exercises/w3schools_Python MongoDB/002_Creating a Collection.py
+++ 100_databases/003_mongodb/_exercises/exercises/w3schools_Python MongoDB/002_Creating a Collection.py
@@ -1,27 +1,27 @@
-# # Creating a Collection
-# # To create a collection in MongoDB, use database object and specify the name of the collection you want to create.
-# # MongoDB will create the collection if it does not exist.
+# Creating a Collection
+# To create a collection in MongoDB, use database object and specify the name of the collection you want to create.
+# MongoDB will create the collection if it does not exist.
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+
+mycol = mydb["customers"]
+
+
+# Important: In MongoDB, a collection is not created until it gets content!
 #
-# _____ ?
+# MongoDB waits until you have inserted a document before it actually creates the collection.
+# Check if Collection Exists
 #
-# myclient _ ?.M.. "mongodb://localhost:27017/")
-# mydb _ ? "mydatabase"
+# Remember: In MongoDB, a collection is not created until it gets content, so if this is your first time creating
+# a collection, you should complete the next chapter (create document) before you check if the collection exists!
 #
-# mycol _ ? "customers"
-#
-#
-# # Important: In MongoDB, a collection is not created until it gets content!
-# #
-# # MongoDB waits until you have inserted a document before it actually creates the collection.
-# # Check if Collection Exists
-# #
-# # Remember: In MongoDB, a collection is not created until it gets content, so if this is your first time creating
-# # a collection, you should complete the next chapter (create document) before you check if the collection exists!
-# #
-# # You can check if a collection exist in a database by listing all collections:
-#
-# print ?.l_c_n..
-#
-# collist _ ?.l_c_n..
-# __ "customers" __ ?
-#   print("The collection exists.")
+# You can check if a collection exist in a database by listing all collections:
+
+print(mydb.list_collection_names())
+
+collist = mydb.list_collection_names()
+if "customers" in collist:
+  print("The collection exists.")
