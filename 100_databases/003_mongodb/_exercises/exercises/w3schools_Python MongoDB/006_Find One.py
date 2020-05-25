e320f5626c9bcfbe84184810dc631950728e7b7f
Grey <sergejyurskyj@yahoo.com>
2020-05-25 10:38:40 -0700

    Committed 006_Find One exercises

--- 100_databases/003_mongodb/_exercises/exercises/w3schools_Python MongoDB/006_Find One.py
+++ 100_databases/003_mongodb/_exercises/exercises/w3schools_Python MongoDB/006_Find One.py
@@ -1,15 +1,15 @@
-# # Find One
-# #
-# # To select data from a collection in MongoDB, we can use the find_one() method.
-# #
-# # The find_one() method returns the first occurrence in the selection.
+# Find One
 #
-# _____ ?
+# To select data from a collection in MongoDB, we can use the find_one() method.
 #
-# myclient _ ?.M.. "mongodb://localhost:27017/"
-# mydb _ ? "mydatabase"
-# mycol _ ? "customers"
-#
-# x _ ?.f_o..
-#
-# print ?
+# The find_one() method returns the first occurrence in the selection.
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+x = mycol.find_one()
+
+print(x)
