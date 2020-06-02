05739e4d442b547b46335d8ea665b1497bd92490
Grey <sergejyurskyj@yahoo.com>
2020-06-02 15:41:43 -0700

    Committed 11 PyQt Thread example template

--- 140_gui/pyqt_pyside/_exercises/_templates/temp/002/11 PyQt Thread example/03_with_threadutil.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/temp/002/11 PyQt Thread example/03_with_threadutil.py
@@ -1,43 +1,43 @@
-____ ?.?C.. ______ *
-____ ?.?W.. ______ *
-____ requests ______ Session
-____ th.. ______ Thread
-____ threadutil ______ run_in_main_thread
-____ time ______ sl..
-
-name _ __.. ("Please enter your name: ")
-chat_url _ "https://build-system.fman.io/chat"
-server _ Session()
-
-# GUI:
-app _ ?
-text_area _ ?PTE..
-text_area.sFP..(__.NF..)
-message _ ?LE..
-layout _ ?VBL..
-layout.aW..(text_area)
-layout.aW..(message)
-window _ ?W..
-window.sL..(layout)
-window.s..
-
-append_message _ run_in_main_thread(text_area.aPT..)
-
-___ fetch_new_messages
-    w__ T..
-        response _ server.g..(chat_url).t__
-        __ response:
-            append_message(response)
-        sl..(.5)
-
-___ send_message
-    server.post(chat_url, {"name": name, "message": message.t__()})
-    message.c..
-
-# Signals:
-message.rP__.c..(send_message)
-
-thread _ Thread(target_fetch_new_messages, d.._T..
-thread.start()
-
-app.e..
+# ____ ?.?C.. ______ _
+# ____ ?.?W.. ______ _
+# ____ requests ______ S..
+# ____ th.. ______ T..
+# ____ threadutil ______ run_in_main_thread
+# ____ time ______ sl..
+#
+# name _ __.. ("Please enter your name: ")
+# chat_url _ "https://build-system.fman.io/chat"
+# server _ ?
+#
+# # GUI:
+# app _ ?
+# text_area _ ?PTE..
+# ?.sFP.. __.NF..
+# message _ ?LE..
+# layout _ ?VBL..
+# ?.aW.. t_a..
+# ?.aW.. m..
+# window _ ?W..
+# ?.sL.. l..
+# ?.s..
+#
+# append_message _ r_i_m_t.. t_a_.aPT..
+#
+# ___ fetch_new_messages
+#     w__ T..
+#         response _ s__.g.. c_u.. .t__
+#         __ ?
+#             a_m.. ?
+#         sl.. .5
+#
+# ___ send_message
+#     s__.p.. c_u.. |"name" ? "message" ?.t__
+#     m__.c..
+#
+# # Signals:
+# m__.rP__.c.. s_m..
+#
+# thread _ ? t.._f.. d.._T..
+# ?.s..
+#
+# ?.e..
