fd80659e650facf03e800f9572a7a5f123699b21
Grey <sergejyurskyj@yahoo.com>
2020-06-02 15:36:35 -0700

    Committed Chapter 4 Building Applications with QMainWindow template

--- 140_gui/pyqt_pyside/_exercises/_templates/temp/Mastering GUI Programming with Python/Chapter 4 Building Applications with QMainWindow/qt_template.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/temp/Mastering GUI Programming with Python/Chapter 4 Building Applications with QMainWindow/qt_template.py
@@ -1,27 +1,27 @@
-______ ___
-____ ? ______ ?W.. __ qtw
-____ ? ______ ?G.. __ qtg
-____ ? ______ ?C.. __ qtc
-
-
-c_ MainWindow(qtw.?MW..):
-
-    ___  -
-        """MainWindow constructor.
-
-        This widget will be our main window.
-        We'll define all the UI components in here.
-        """
-        s_. - ()
-        # Main UI code goes here
-
-        # End main UI code
-        s..
-
-
-__ ______ __ ______
-    app _ qtw.?A..(___.a..
-    # it's required to save a reference to MainWindow.
-    # if it goes out of scope, it will be destroyed.
-    mw _ MainWindow()
-    ___.e.. ?.e..
+# ______ ___
+# ____ ? ______ ?W.. __ qtw
+# ____ ? ______ ?G.. __ qtg
+# ____ ? ______ ?C.. __ qtc
+#
+#
+# c_ MainWindow ?.?MW..
+#
+#     ___  -
+#         """MainWindow constructor.
+#
+#         This widget will be our main window.
+#         We'll define all the UI components in here.
+#         """
+#         s_. -
+#         # Main UI code goes here
+#
+#         # End main UI code
+#         s..
+#
+#
+# __ ______ __ ______
+#     app _ ?.?A.. ___.a..
+#     # it's required to save a reference to MainWindow.
+#     # if it goes out of scope, it will be destroyed.
+#     mw _ ?
+#     ___.e.. ?.e..
