91ac2531e56b9a69203fd854f1888d00669d7e11
Grey <sergejyurskyj@yahoo.com>
2020-06-02 15:44:21 -0700

    Committed 11 PyQt Thread example template

--- 140_gui/pyqt_pyside/_exercises/_templates/temp/002/11 PyQt Thread example/threadutil.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/temp/002/11 PyQt Thread example/threadutil.py
@@ -1,22 +1,22 @@
-____ ?.?C.. ______ ?O.., pS..
-
-c_ CurrentThread(?O..):
-
-    _on_execute _ pS..(object, tuple, dict)
-
-    ___  -
-        super(?O.., self). - ()
-        _on_execute.c..(_execute_in_thread)
-
-    ___ execute  f, args, kwargs):
-        _on_execute.e..(f, args, kwargs)
-
-    ___ _execute_in_thread  f, args, kwargs):
-        f($ $$)
-
-main_thread _ CurrentThread()
-
-___ run_in_main_thread(f):
-    ___ result($ $$
-        main_thread.execute(f, args, kwargs)
-    r_ result
+# ____ ?.?C.. ______ ?O.., pS..
+#
+# c_ CurrentThread ?O..
+#
+#     _on_execute _ pS.. ob.. tu.. di..
+#
+#     ___  -
+#         s__ ?O.. . -
+#         _o__.c.. _e..
+#
+#     ___ execute  f args kwargs
+#         _o__.e.. f a.. kw..
+#
+#     ___ _execute_in_thread  f a.. kw..
+#         ? $ $$
+#
+# main_thread _ ?
+#
+# ___ run_in_main_thread f
+#     ___ result $ $$
+#         m_t__.e.. f a.. kw..
+#     r_ r..
