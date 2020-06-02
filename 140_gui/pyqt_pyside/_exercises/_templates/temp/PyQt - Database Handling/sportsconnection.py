6b4cac0582a509ec51bac17da0cad0be77631c9c
Grey <sergejyurskyj@yahoo.com>
2020-06-02 16:03:58 -0700

    Committed PyQt - Database Handling template

--- 140_gui/pyqt_pyside/_exercises/_templates/temp/PyQt - Database Handling/sportsconnection.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/temp/PyQt - Database Handling/sportsconnection.py
@@ -1,35 +1,35 @@
-____ ? ______ ?S.., ?G.., ?W..
-
-
-___ createDB
-    db _ ?S...?SD...aD..('QSQLITE')
-    db.sDN..('sports.db')
-
-    __ no. db.o..
-        ?G...?MB...c..(N.., ?G...qApp.tr("Cannot open database"),
-                                   ?G...qApp.tr("Unable to establish a database connection.\n"
-                                                 "This example needs SQLite support. Please read "
-                                                 "the Qt SQL driver documentation for information "
-                                                 "how to build it.\n\n" "Click Cancel to exit."),
-                                   ?G...?MB...Cancel)
-
-        r_ F..
-
-    query _ ?S...QSqlQuery()
-
-    query.exec_("create table sportsmen(id int primary key, "
-                "firstname varchar(20), lastname varchar(20))")
-
-    query.exec_("insert into sportsmen values(101, 'Roger', 'Federer')")
-    query.exec_("insert into sportsmen values(102, 'Christiano', 'Ronaldo')")
-    query.exec_("insert into sportsmen values(103, 'Ussain', 'Bolt')")
-    query.exec_("insert into sportsmen values(104, 'Sachin', 'Tendulkar')")
-    query.exec_("insert into sportsmen values(105, 'Saina', 'Nehwal')")
-    r_ T..
-
-
-__ ______ __ ______
-    ______ ___
-
-    app _ ?W...?A..(___.a..
-    createDB()
+# ____ ? ______ ?S.., ?G.., ?W..
+#
+#
+# ___ createDB
+#     db _ ?S...?SD...aD.. QSQLITE
+#     db.sDN.. sports.db
+#
+#     __ no. db.o..
+#         ?G...?MB...c..(N.., ?G...qA__.tr("Cannot open database"),
+#                                    ?G...qA__.tr("Unable to establish a database connection.\n"
+#                                                  "This example needs SQLite support. Please read "
+#                                                  "the Qt SQL driver documentation for information "
+#                                                  "how to build it.\n\n" "Click Cancel to exit."),
+#                                    ?G...?MB...C..
+#
+#         r_ F..
+#
+#     query _ ?S...?SQ..
+#
+#     ?.e.. c.. t.. sportsmen(id i.. p.. k.., "
+#                 firstname v.. 20 lastname v.. 20
+#
+#     ?.e.. i.. i.. sportsmen v.. 101 'Roger', 'Federer'
+#     ?.e.. i.. i.. sportsmen v.. 102 'Christiano', 'Ronaldo'
+#     ?.e.. i.. i.. sportsmen v.. 103 'Ussain', 'Bolt'
+#     ?.e.. i.. i.. sportsmen v.. 104 'Sachin', 'Tendulkar'
+#     ?.e.. i.. i.. sportsmen v.. 105 'Saina', 'Nehwal'
+#     r_ T..
+#
+#
+# __ ______ __ ______
+#     ______ ___
+#
+#     app _ ?W...?A.. ___.a..
+#     ?
