e2d425baa1685451382a89b330c5b490b5fe37c4
Grey <sergejyurskyj@yahoo.com>
2020-06-04 11:45:37 -0700

    Committed MongoClusterManager templates

--- 140_gui/pyqt_pyside/_exercises/_templates/temp/mongodb/MongoClusterManager/MongoManager/app/ConfigurationManager.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/temp/mongodb/MongoClusterManager/MongoManager/app/ConfigurationManager.py
@@ -1,23 +1,23 @@
-______ ____
-
-
-c_ InvalidSchemeException(E..):
-    p..
-
-
-c_ ConfigurationManager:
-    ___  - 
-        p..
-
-    ___ get  configuration):
-        r_ ____.loads(open(configuration, 'r').read())
-
-    ___ validate  json_str):
-        """
-        validate input
-        raise InvalidSchemeException
-        raise json.JSONDecodeError
-        :param json_str:
-        :return:
-        """
-        ret _ ____.loads(json_str)
+# ______ ____
+#
+#
+# c_ InvalidSchemeException E..
+#     p..
+#
+#
+# c_ ConfigurationManager
+#     ___  -
+#         p..
+#
+#     ___ get  configuration
+#         r_ ____.l.. o.. ? _ .r..
+#
+#     ___ validate  json_str
+#         """
+#         validate input
+#         raise InvalidSchemeException
+#         raise json.JSONDecodeError
+#         :param json_str:
+#         :return:
+#         """
+#         ret _ ____.l.. ?
