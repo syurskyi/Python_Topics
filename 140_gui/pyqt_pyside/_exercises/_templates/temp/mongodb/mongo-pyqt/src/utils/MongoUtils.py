6b87db4be814c5b346f412008f69afd9fbe1e0aa
Grey <sergejyurskyj@yahoo.com>
2020-06-04 11:42:10 -0700

    Committed mongo-pyqt templates

--- 140_gui/pyqt_pyside/_exercises/_templates/temp/mongodb/mongo-pyqt/src/utils/MongoUtils.py
+++ 140_gui/pyqt_pyside/_exercises/_templates/temp/mongodb/mongo-pyqt/src/utils/MongoUtils.py
@@ -1,45 +1,38 @@
-#!/usr/bin/env python  
-# -*- coding: utf-8 -*-  
-'''
-Created on 2014年9月20日
-
-@author: wanghch
-'''
-______ __,____
-c_ MongoUtils o..
-    '''
-    classdocs
-    '''
-
-
-    ___  -
-        '''
-        Constructor
-        '''
-    ___ preview col,queryjson,page,limit,sort):
-        skipNum _ (page - 1) * limit
-        __ le.(queryjson.items()) > 0:
-            query _ ____.dumps(queryjson)
-        ____
-            query _ ""
-            
-        __ sort __ N..
-            r_ "db."+col+".find("+query+").limit("+st.(limit)+").skip("+st.(skipNum)+")"
-        ____
-            sortStr _ ____.dumps(sort)
-            r_ "db."+col+".find("+query+").sort("+sortStr+").limit("+st.(limit)+").skip("+st.(skipNum)+")"
-       
-            
-            
-    ___ parse mongostmt):
-        pattern _ __.compile(r'db\.([\.a-zA-Z0-9]+)\.find\(({.*})\)')
-        match _ pattern.match(mongostmt.replace("'", "\""))
-        __ match:
-            jsonstr _ match.group(2)
-            r_ (match.group(1),____.loads(jsonstr))
-        r_ None
-
-__ __name__ __ '__main__':
-    MongoUtils()
-    
-    
+# ______ __ ____
+# c_ MongoUtils o..
+#     '''
+#     classdocs
+#     '''
+#
+#
+#     ___  -
+#         '''
+#         Constructor
+#         '''
+#     ___ preview col queryjson page limit sort
+#         skipNum _ p.. - 1 * l..
+#         __ le. qj__.i.. > 0
+#             query _ ____.d.. qj__
+#         ____
+#             query _ ""
+#
+#         __ sort __ N..
+#             r_ "db."+c..+".find("+q..+").limit("+st. l.. +").skip("+st. sN.. +")"
+#         ____
+#             sortStr _ ____.d.. so..
+#             r_ "db."+c..+".find("+q..+").sort("+soS..+").limit("+st. l.. +").skip("+st. sN..+")"
+#
+#
+#
+#     ___ parse mongostmt
+#         pattern _ __.c.. _*db\.([\.a-zA-Z0-9]+)\.find\(({.*})\)')
+#         match _ ?.m.. m__.r.. "'", "\""
+#         __ ?
+#             jsonstr _ m__.g.. 2
+#             r_ m__.g.. 1 ____.l.. ?
+#         r_ N..
+#
+# __ _______ __ ________
+#     ?
+#
+#
