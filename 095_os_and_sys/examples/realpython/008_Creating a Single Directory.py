a68cdf2cd78591c57d98b57639a5c499a12983a6
Grey <sergejyurskyj@yahoo.com>
2020-06-06 10:31:02 -0700

    Committed os and sys example

--- 095_os_and_sys/examples/realpython/008_Creating a Single Directory.py
+++ 095_os_and_sys/examples/realpython/008_Creating a Single Directory.py
@@ -1,16 +1,16 @@
 # To create a single directory, pass a path to the directory as a parameter to os.mkdir():
 #
 # import os
-#
 # os.mkdir('example_directory/')
-#
-# If a directory already exists, os.mkdir() raises FileExistsError. Alternatively, you can create a directory using pathlib:
-#
-# from pathlib import Path
-#
-# p = Path('example_directory/')
-# p.mkdir()
-#
+
+# If a directory already exists, os.mkdir() raises FileExistsError. Alternatively, you can create a directory using
+# pathlib:
+
+from pathlib import Path
+
+p = Path('example_directory/')
+p.mkdir()
+
 # If the path already exists, mkdir() raises a FileExistsError:
 #
 # >>> p.mkdir()
@@ -24,20 +24,20 @@
 # [Errno 17] File exists: '.'
 #
 # To avoid errors like this, catch the error when it happens and let your user know:
-#
-# from pathlib import Path
-#
-# p = Path('example_directory')
-# try:
-#     p.mkdir()
-# except FileExistsError as exc:
-#     print(exc)
-#
+
+from pathlib import Path
+
+p = Path('example_directory')
+try:
+    p.mkdir()
+except FileExistsError as exc:
+    print(exc)
+
 # Alternatively, you can ignore the FileExistsError by passing the exist_ok=True argument to .mkdir():
-#
-# from pathlib import Path
-#
-# p = Path('example_directory')
-# p.mkdir(exist_ok=True)
-#
+
+from pathlib import Path
+
+p = Path('example_directory')
+p.mkdir(exist_ok=True)
+
 # This will not raise an error if the directory already exists.
