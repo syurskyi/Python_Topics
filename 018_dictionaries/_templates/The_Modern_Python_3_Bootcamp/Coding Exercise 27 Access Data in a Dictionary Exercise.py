82f81864371023db1535c3b4b8ee1925ea84b328
Grey <sergejyurskyj@yahoo.com>
2020-06-21 13:53:12 -0700

    Committed Coding Exercise 27 Access Data in a Dictionary Exercise template

--- 018_dictionaries/_templates/The_Modern_Python_3_Bootcamp/Coding Exercise 27 Access Data in a Dictionary Exercise.py
+++ 018_dictionaries/_templates/The_Modern_Python_3_Bootcamp/Coding Exercise 27 Access Data in a Dictionary Exercise.py
@@ -1,32 +1,32 @@
-# Concatenation Solution
+# # Concatenation Solution
+# #
+# # Solution using string concatenation:
 #
-# Solution using string concatenation:
-
-artist = {
-    "first": "Neil",
-    "last": "Young",
-}
-
-# concat first and last name with a space
-full_name = artist["first"] + " " + artist["last"]
-
-# Format() Solution
+# artist = {
+#     "first": "Neil",
+#     "last": "Young",
+# }
 #
-# Solution using the format() method:
-
-artist = {
-    "first": "Neil",
-    "last": "Young",
-}
-
-full_name = "{} {}".format(artist["first"], artist["last"])
-
-# F-String Solution
-
-artist = {
-    "first": "Neil",
-    "last": "Young",
-}
-
-full_name = f"{artist['first']} {artist['last']}"
-
+# # concat first and last name with a space
+# full_name = ? *f.. + " " + ? *l..
+#
+# # Format() Solution
+# #
+# # Solution using the format() method:
+#
+# artist = {
+#     "first": "Neil",
+#     "last": "Young",
+# }
+#
+# full_name = *@ @ .f.. ? *f.. ? *l..
+#
+# # F-String Solution
+#
+# artist = {
+#     "first": "Neil",
+#     "last": "Young",
+# }
+#
+# full_name = _*|? *f..|| |? *l..
+#
