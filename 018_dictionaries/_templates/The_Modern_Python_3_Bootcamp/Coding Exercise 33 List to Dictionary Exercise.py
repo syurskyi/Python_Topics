462baee0193f72acde185ee5ca5f9f7f41cb2de4
Grey <sergejyurskyj@yahoo.com>
2020-06-21 09:52:36 -0700

    Committed dictionaries update

---
+++ 018_dictionaries/_templates/The_Modern_Python_3_Bootcamp/Coding Exercise 33 List to Dictionary Exercise.py
@@ -0,0 +1,17 @@
+# There are many potential solutions for this:
+#
+# Using a dictionary comprehension
+
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+answer = {thing[0]: thing[1] for thing in person}
+
+# An alternate solution using a dict comprehension, without any references to list indexes:
+
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+answer = {k: v for k, v in person}
+
+# Finally, a really simple solution.
+# If you have a list of pairs, you can very easily turn it into a dictionary using dict()
+
+person = [["name", "Jared"], ["job", "Musician"], ["city", "Bern"]]
+answer = dict(person)
