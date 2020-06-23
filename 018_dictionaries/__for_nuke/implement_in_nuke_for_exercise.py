0e0f1d12199998879ab56a97ec243fdadb6dde5e
Grey <sergejyurskyj@yahoo.com>
2020-06-23 13:14:22 -0700

    Committed dict exercises

--- 018_dictionaries/__for_nuke/implement_in_nuke_for_exercise.py
+++ 018_dictionaries/__for_nuke/implement_in_nuke_for_exercise.py
@@ -289,3 +289,38 @@ for c in text:
 
 for cat in categories:
     print(f'{cat}:', ''.join(categories[cat]))
+
+
+# In this example we have some dictionaries we use to configure our application. One dictionary specifies
+# some configuration defaults for every configuration parameter our application will need.
+# Another dictionary is used to configure some global configuration, and another set of dictionaries is used
+# to define environment specific configurations, maybe dev and prod.
+conf_defaults = dict.fromkeys(('host', 'port', 'user', 'pwd', 'database'), None)
+print(conf_defaults)
+
+conf_global = {
+    'port': 5432,
+    'database': 'deepdive'}
+
+conf_dev = {
+    'host': 'localhost',
+    'user': 'test',
+    'pwd': 'test'}
+
+
+conf_prod = {
+    'host': 'prodpg.deepdive.com',
+    'user': '$prod_user',
+    'pwd': '$prod_pwd',
+    'database': 'deepdive_prod'}
+
+
+# Now we can generate a full configuration for our dev environment this way:
+
+config_dev = {**conf_defaults, **conf_global, **conf_dev}
+print(conf_dev)
+
+# and a config for our prod environment:
+
+config_prod = {**conf_defaults, **conf_global, **conf_prod}
+print(config_prod)
