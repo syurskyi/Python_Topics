f8f91d3451533cf78561c88f205949c556ae9e70
Grey <sergejyurskyj@yahoo.com>
2020-06-23 13:32:30 -0700

    Committed dict exercises

--- 018_dictionaries/__for_nuke/implement_in_nuke_for_exercise.py
+++ 018_dictionaries/__for_nuke/implement_in_nuke_for_exercise.py
@@ -67,6 +67,36 @@ print(numbers_dict)
 for key in range(5):
     print('{}:'.format(key), numbers_dict.get(key, 0))
 
+d = {'x': 1, 'y': 2, 'z': 3}
+for key in d.keys():          # Использование метода keys()
+    print("({0} => {1})".format(key, d[key],  end=" "))
+# Выведет: (y => 2) (x => 1) (z => 3)
+print()                       # Вставляем символ перевода строки
+for key in d:                 # Словари также поддерживают итерации
+    print("({0} => {1})".format(key, d[key], end=" "))
+# Выведет: (y => 2) (x => 1) (z => 3)
+
+
+d = {"x": 1, "y": 2, "z": 3}
+k = list(d.keys())           # Получаем список ключей
+k.sort()                     # Сортируем список ключей
+for key in k:
+    print("({0} => {1})".format(key,  d[key]), end=" ")
+# Выведет: (x => 1) (y => 2) (z => 3)
+print()
+
+d = {'x': 1, 'y': 2, 'z': 3}
+for key in sorted(d.keys()):
+    print("({0} => {1})".format(key, d[key]), end=" ")
+# Выведет: (x => 1) (y => 2) (z => 3)
+print()
+
+d = {'x': 1, 'y': 2, 'z': 3}
+for key in sorted(d):
+    print("({0} => {1})".format(key, d[key]), end=" ")
+# Выведет: (x => 1) (y => 2) (z => 3)
+
+
 # d.items
 #  в Python 3 возвращает объект представления словаря, соответствующий парам  двухэлементным кортежам
 #  вида  ключ, значение .  В  Python 2 возвращает соответствующий список, а метод iteritems   возвращает итератор.
