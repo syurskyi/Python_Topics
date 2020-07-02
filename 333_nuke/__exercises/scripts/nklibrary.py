816d691834ee9decf2f8511affd9a2a41341f3d0
Grey <sergejyurskyj@yahoo.com>
2020-07-02 15:24:27 -0700

    Committed nuke exercise checkenv

---
+++ 333_nuke/__exercises/scripts/nklibrary.py
@@ -0,0 +1,60 @@
+# #coding:utf8
+# ____ ?.?W.. ______ _
+# ______ __
+# ______ nuke
+#
+# c_ NkLibrary?W..
+# 	___ -
+# 		s_ ? .-
+# 		#variable
+# 		libpath _ __.g.. ("NUKE_PATH")+"/nk/"
+# 		appname _ "Nuke Library"
+# 		version _ "v0.1"
+#
+# 		#widget
+# 		nklist _ ?LW..
+# 		a..
+# 		ok _ ?PB.. *?
+# 		cancel _ ?PB.. *?
+#
+# 		#layout
+# 		layout _ ?VB..
+# 		?.aW.. n..
+# 		?.aW.. o.
+# 		?.aW.. c..
+# 		sQT.. a.. + " " + v..
+# 		sL.. ?
+#
+# 		#event
+# 		o_.c__.c.. p..
+# 		c_.c__.c.. c..
+# 		n_.iC__.c.. i..
+#
+# 	___ pushOK
+# 		?.nP.. l.. + c..
+# 		c__
+#
+# 	___ itemClick item
+# 		currentItem _ ?.t..
+#
+# 	___ addNkList
+# 		__ no. __.p__.ex.. l..
+# 			?.m.. l.. + "The path does not exist."
+# 		___ i __ __.l_d.. l..
+# 			base, ext _ __.p__.s.. ?
+# 			__ ext !_ ".nk":
+# 				c..
+# 			n__.aI.. ?LWI.. ?
+#
+# ___ main
+# 	g__ customApp
+# 	___
+# 		?.c__
+# 	______
+# 		p..
+#
+# 	customApp _ ?
+# 	___
+# 		?.s__
+# 	______
+# 		p..
--- 333_nuke/_templates/007_Image and nodes/005_Saving a Python script inside a node.py
+++ 333_nuke/_templates/007_Image and nodes/005_Saving a Python script inside a node.py
@@ -1,28 +1,28 @@
-set cut_paste_input [stack 0]
-version 10.0 v4
-push $cut_paste_input
-Blur {
- channels rgba
- name Blur1
- label "---------------\n\[value size]"
- selected true
- xpos 94
- ypos -92
- addUserKnob {20 User}
- addUserKnob {22 test l Test T "______ nuke\nnode = nuke.thisNode()\nprint node.name()\nknob = nuke.thisKnob()\nprint knob.name()" +STARTLINE}
-}
-########################################################################################################################
-
-set cut_paste_input [stack 0]
-version 10.0 v4
-push $cut_paste_input
-Blur {
- channels rgba
- name Blur1
- label "---------------\n\[value size]"
- selected true
- xpos 58
- ypos -103
- addUserKnob {20 User}
- addUserKnob {22 test l Test T "______ nuke\nnode = nuke.thisNode()\nnode\['size'].setValue(10)" +STARTLINE}
-}
+# set cut_paste_input [stack 0]
+# version 10.0 v4
+# push $cut_paste_input
+# Blur {
+#  channels rgba
+#  name Blur1
+#  label "---------------\n\[value size]"
+#  selected true
+#  xpos 94
+#  ypos -92
+#  addUserKnob {20 User}
+#  addUserKnob {22 test l Test T "______ nuke\nnode = nuke.thisNode()\nprint node.name()\nknob = nuke.thisKnob()\nprint knob.name()" +STARTLINE}
+# }
+# ########################################################################################################################
+#
+# set cut_paste_input [stack 0]
+# version 10.0 v4
+# push $cut_paste_input
+# Blur {
+#  channels rgba
+#  name Blur1
+#  label "---------------\n\[value size]"
+#  selected true
+#  xpos 58
+#  ypos -103
+#  addUserKnob {20 User}
+#  addUserKnob {22 test l Test T "______ nuke\nnode = nuke.thisNode()\nnode\['size'].setValue(10)" +STARTLINE}
+# }
