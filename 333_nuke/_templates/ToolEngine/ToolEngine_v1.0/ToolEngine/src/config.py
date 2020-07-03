7f4e928f641a77339b08ed36c273e4e35e8eb37e
Grey <sergejyurskyj@yahoo.com>
2020-07-02 22:49:28 -0700

    Committed nuke template ToolEngine config

--- 333_nuke/_templates/ToolEngine/ToolEngine_v1.0/ToolEngine/src/config.py
+++ 333_nuke/_templates/ToolEngine/ToolEngine_v1.0/ToolEngine/src/config.py
@@ -1,21 +1,21 @@
-______ __
-
-########################################################################################################################
+# ______ __
 #
-# user config variables
-#
-########################################################################################################################
-#
-# TOOLS_TEMP
-# This is the folder- and menu name for the temp tools as it will be displayed in nuke's menu
-TOOLS_TEMP = "TEMP"
-#
-#
-# PATH_SETTINGS_FILE
-# This is the path where to find the ToolsEngine's settings json file
-PATH_SETTINGS_FILE = __.path.join(__.path.expanduser("~"), ".nuke", "ToolEngine", "settings.json")
-#
-#
-# TOOLSDIR_IGNORE
-# ignore these folders when building tools menu
-TOOLSDIR_IGNORE = []
+# ########################################################################################################################
+# #
+# # user config variables
+# #
+# ########################################################################################################################
+# #
+# # TOOLS_TEMP
+# # This is the folder- and menu name for the temp tools as it will be displayed in nuke's menu
+# TOOLS_TEMP _ *TEMP
+# #
+# #
+# # PATH_SETTINGS_FILE
+# # This is the path where to find the ToolsEngine's settings json file
+# PATH_SETTINGS_FILE _ __.p__.j.. __.p__.e__u.. ~ *.nuke *ToolEngine *settings.json
+# #
+# #
+# # TOOLSDIR_IGNORE
+# # ignore these folders when building tools menu
+# TOOLSDIR_IGNORE _ # list
