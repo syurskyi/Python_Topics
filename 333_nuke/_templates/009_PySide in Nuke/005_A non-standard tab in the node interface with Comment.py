ed2cd515c2b598c2e928757c8137229f92b2f163
Grey <sergejyurskyj@yahoo.com>
2020-07-11 21:38:00 -0700

    Committed nuke template 005_A non-standard tab in the node interface with Comment

--- 333_nuke/_templates/009_PySide in Nuke/005_A non-standard tab in the node interface with Comment.py
+++ 333_nuke/_templates/009_PySide in Nuke/005_A non-standard tab in the node interface with Comment.py
@@ -1,59 +1,59 @@
-# Esli mu vstavljaem widget v nodu s pompocjiy PyQt, to on ne ostavljaet nikakih sledov na samoj node
-# To est'  nam ne nyzno sozdavat' knob, mu prosto sozdajom widget i vstavljaem ego v imejychijsja interfais
-# Esli vu sozdajote knob  Tab to pri pereotkrutii Nuke etot knob"Tab" tak ze bydet nahoditsja na node, no esli vu delaete eto skriptom
-# To esli vash skript ne zagryzen to vasha taba prosto ne bydyt pojavitsja
-# Nachat' nado s polychenija widgeta okna ili nodu kak QT  objekta. Y lybogo okna, lyboj nodu on prisytstvyet i on ravnjaetsja imeni etoj nodu
-
-____ ?.?C.. ______ _
-____ ?.?G.. ______ _
-
-___ getMainWindow():
-    app = ?A...ins..)
-    ___ widget __ app.tLW..
-        __ widget.mO.. .cN.. __ 'Foundry::UI::DockMainWindow':
-            r_ widget
-
-qnuke = getMainWindow()
-qnuke.findChildren(?W.., 'Transform1')
-
-#####################################################################################################################################################
-
-____ ?.?C.. ______ _
-____ ?.?G.. ______ _
-
-___ getMainWindow():
-    app = ?A...ins..)
-    ___ widget __ app.tLW..
-        __ widget.mO.. .cN.. __ 'Foundry::UI::DockMainWindow':
-            r_ widget
-
-qnuke = getMainWindow()
-w = qnuke.findChildren(?W.., 'Transform1')
-w.sQT..('Transform11')
-
-
-#####################################################################################################################################################
-
-____ ?.?C.. ______ _
-____ ?.?G.. ______ _
-
-___ getMainWindow():
-    app = ?A...ins..)
-    ___ widget __ app.tLW..
-        __ widget.mO.. .cN.. __ 'Foundry::UI::DockMainWindow':
-            r_ widget
-
-qnuke = getMainWindow()
-w = qnuke.findChild(?W.., 'Transform1')
-w.sQT..('Transform11')
-t = w.findChild(QTabWidget)
-t.count()
-nw = ?W..()
-ly = ?VB..
-nw.sL..(ly)
-___ i __ ra..(5):
-    ly.aW..(?PB..())
-t.addTab(nw, 'My Tab')
-c = ?CW..
-t.insertTab(0, c, 'Calendar')
-t.setCurrentIndex(0)
+# # Esli mu vstavljaem widget v nodu s pompocjiy PyQt, to on ne ostavljaet nikakih sledov na samoj node
+# # To est'  nam ne nyzno sozdavat' knob, mu prosto sozdajom widget i vstavljaem ego v imejychijsja interfais
+# # Esli vu sozdajote knob  Tab to pri pereotkrutii Nuke etot knob"Tab" tak ze bydet nahoditsja na node, no esli vu delaete eto skriptom
+# # To esli vash skript ne zagryzen to vasha taba prosto ne bydyt pojavitsja
+# # Nachat' nado s polychenija widgeta okna ili nodu kak QT  objekta. Y lybogo okna, lyboj nodu on prisytstvyet i on ravnjaetsja imeni etoj nodu
+#
+# ____ ?.?C.. ______ _
+# ____ ?.?G.. ______ _
+#
+# ___ getMainWindow
+#     app _ ?A...ins..
+#     ___ widget __ ?.tLW..
+#         __ ?.mO.. .cN.. __ 'F..||U.||DMW..'
+#             r_ ?
+#
+# qnuke _ ?
+# ?.fC.. ?W.., 'Transform1'
+#
+# #####################################################################################################################################################
+#
+# ____ ?.?C.. ______ _
+# ____ ?.?G.. ______ _
+#
+# ___ getMainWindow
+#     app _ ?A...ins..
+#     ___ widget __ ?.tLW..
+#         __ ?.mO.. .cN.. __ 'F..||U.||DMW..'
+#             r_ ?
+#
+# qnuke _ ?
+# w _ ?.fC.. ?W.. 'Transform1'
+# ?.sQT.. 'Transform11'
+#
+#
+# #####################################################################################################################################################
+#
+# ____ ?.?C.. ______ _
+# ____ ?.?G.. ______ _
+#
+# ___ getMainWindow
+#     app _ ?A...ins..
+#     ___ widget __ ?.tLW..
+#         __ ?.mO.. .cN.. __ 'F..||U.||DMW..'
+#             r_ ?
+#
+# qnuke _ getMainWindow
+# w _ ?.fC.. ?W.. 'Transform1'
+# ?.sQT.. 'Transform11'
+# t _ ?.fC.. ?TW..
+# ?.co..
+# nw _ ?W..
+# ly _ ?VB..
+# ?.sL.. ?
+# ___ i __ ra.. 5
+#     ly.aW.. ?PB..
+# t.aT.. n. 'My Tab'
+# c _ ?CW..
+# ?.iT.. 0, c, 'Calendar'
+# ?.sCI.. 0
