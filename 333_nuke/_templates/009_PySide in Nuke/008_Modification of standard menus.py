f094f4bfd1a912191ccb8099419f5705b3125d67
Grey <sergejyurskyj@yahoo.com>
2020-07-11 21:52:06 -0700

    Committed nuke template 008_Modification of standard menus

--- 333_nuke/_templates/009_PySide in Nuke/008_Modification of standard menus.py
+++ 333_nuke/_templates/009_PySide in Nuke/008_Modification of standard menus.py
@@ -1,38 +1,38 @@
-# linearAnim
-
-______ ?
-
-n = ?.sN__
-k = n['translate']
-k.setAnimated()
-___ i __ ra..(100):
-    k.setValueAt( i, i )
-
-# change to function doit
-
-___ doit():
-    k = ?.thisKnob()
-    __ k.arraySize() > 1:
-        index = getKnobIndex()
-    ____
-        index = 0
-    k.setAnimated()
-    ___ i __ ra..(100):
-        k.setValueAt( i, i, index)
-
-# shtobu polychit' index knoba, kotoruj nahoditsja v kontexte, dostatochno vuzvat' ety fynkcijy
-# no vuzuvat' ejo sledyet tol'ko dlja knobov gde kanalov bol'she odnogo
-
-___ getKnobIndex():
-
-    tclGetAnimIndex = """
-
- set thisanimation [animations]
- if {[llength $thisanimation] > 1} {
-  return "-1"
-  } else {
-   return [lsearch [in [join [lrange [split [in $thisanimation {animations}] .] 0 end-1] .] {animations}] $thisanimation]
-  }
- """
-
-    r_ int(?.tcl(tclGetAnimIndex))
+# # linearAnim
+#
+# ______ ?
+#
+# n _ ?.sN__
+# k _ ? translate
+# ?.sA..
+# ___ i __ ra.. 100
+#     ?.sVA. ? ?
+#
+# # change to function doit
+#
+# ___ doit
+#     k _ ?.tK..
+#     __ ?.aS.. > 1
+#         index _ gKI..
+#     ____
+#         index _ 0
+#     ?.sA..
+#     ___ i __ ra.. 100
+#         ?.sVA.  ? ? ?
+#
+# # shtobu polychit' index knoba, kotoruj nahoditsja v kontexte, dostatochno vuzvat' ety fynkcijy
+# # no vuzuvat' ejo sledyet tol'ko dlja knobov gde kanalov bol'she odnogo
+#
+# ___ getKnobIndex
+#
+#     tclGetAnimIndex _ """
+#
+#  set thisanimation [animations]
+#  if {[llength $thisanimation] > 1} {
+#   return "-1"
+#   } else {
+#    return [lsearch [in [join [lrange [split [in $thisanimation {animations}] .] 0 end-1] .] {animations}] $thisanimation]
+#   }
+#  """
+#
+#     r_ in. ?.t.. ?
