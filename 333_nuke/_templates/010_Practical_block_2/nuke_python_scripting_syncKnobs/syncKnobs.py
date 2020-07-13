811856a1152cf3e12aa88e3bfa3e9eec823c9a93
Grey <sergejyurskyj@yahoo.com>
2020-07-13 11:11:51 -0700

    Committed nuke syncKnobs template

--- 333_nuke/_templates/010_Practical_block_2/nuke_python_scripting_syncKnobs/syncKnobs.py
+++ 333_nuke/_templates/010_Practical_block_2/nuke_python_scripting_syncKnobs/syncKnobs.py
@@ -1,38 +1,38 @@
-______ ?
-
-d _ __import__('__main__').__dict__
-
-___ switch():
-    __ 'syncItem' __ d:
-        syncItem _ d['syncItem']
-        a _ syncItem.action()
-        data _ a.data()
-        __ data:
-            # off
-            a.setData(N..)
-            a.setText('Enable SYNC')
-            callback(F..)
-        ____
-            # ON
-            a.setData(T..)
-            a.setText('Disable SYNC')
-            callback(T..)
-
-___ callback(v):
-    __ v:
-       ?.aKC..(sync)
-    ____
-        ?.rKC..(sync)
-
-___ sync():
-    k _ ?.tK..
-    __ no. k.name() __ ['name', 'selected', 'showPanel', 'hidePanel']:
-        n _ ?.thisNode()
-        other _ ?.selectedNodes(n.Class())
-        v _ k.gV..
-        #>>>>>
-        d['lastVal'] _ v
-        ___ node __ other:
-            __ no. node __ n:
-                node[k.name()].sV..(v)
-
+# ______ ?
+#
+# d _ -i ' -m ' . -d
+#
+# ___ switch
+#     __ 'syncItem' __ d
+#         syncItem _ ? '?'
+#         a _ ?.a..
+#         data _ ?.d..
+#         __ ?
+#             # off
+#             ?.sD.. N..
+#             a.sT.. 'Enable SYNC'
+#             c.. F..
+#         ____
+#             # ON
+#             ?.sD.. T..
+#             ?.sT.. 'Disable SYNC'
+#             c.. T..
+#
+# ___ callback v
+#     __ ?
+#        ?.aKC.. ?
+#     ____
+#         ?.rKC.. ?
+#
+# ___ sync
+#     k _ ?.tK..
+#     __ no. ?.n.. __ 'name', 'selected', 'showPanel', 'hidePanel'
+#         n _ ?.tN..
+#         other _ ?.sN.. ?.C..
+#         v _ ?.gV..
+#         #>>>>>
+#         d 'lastVal' _ v
+#         ___ node __ o..
+#             __ no. ? __ n
+#                 ? ?.n.. .sV.. ?
+#
