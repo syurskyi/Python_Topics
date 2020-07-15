0578d9d2f3ef115eff92633140927f02ee27b6ee
Grey <sergejyurskyj@yahoo.com>
2020-07-14 20:42:45 -0700

    Committed nuke 003_Widget registration as a panel  exercise

--- 333_nuke/__exercises/Nuke_Scripting_for_Pipeline_TD/009_PySide in Nuke/003_Widget registration as a panel.py
+++ 333_nuke/__exercises/Nuke_Scripting_for_Pipeline_TD/009_PySide in Nuke/003_Widget registration as a panel.py
@@ -1,21 +1,22 @@
-# ______ ?
-# ______ ?
-# ____ ? ______ panels
-#
-# ____ ?.?C.. ______ _
-# ____ ?.?G.. ______ _
-#
-#
-# c_ simplePanel ?W..
-#     ___  -
-#         ?W... -
-#         sL.. ?VL..
-#         locationEdit _ ?CW..
-#         la__ .aW.. ?
-#
-# p__.rWAP.. simplePanel Simple
-#
-# ########################################################################################################################
-#
-# ______ modulefinder
-# w _ m__.sP..
+import nuke
+import nukescripts
+from nukescripts import panels
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+
+class simplePanel(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setLayout(QVBoxLayout())
+        self.locationEdit = QCalendarWidget()
+        self.layout().addWidget(self.locationEdit)
+
+panels.registerWidgetAsPanel('simplePanel', 'Simple','')
+
+########################################################################################################################
+
+______ modulefinder
+w _ m__.sP..
