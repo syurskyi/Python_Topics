c8bcfbff5aa13e8d3e1552da03a25509a7f39d15
Grey <sergejyurskyj@yahoo.com>
2020-08-09 14:56:04 -0700

    Committed Section 8 INTERVIEW QUESTIONS Hash Tables_Maps two sum template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 8 INTERVIEW QUESTIONS Hash Tables_Maps/two sum/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 8 INTERVIEW QUESTIONS Hash Tables_Maps/two sum/index.py
@@ -1,10 +1,11 @@
-c_ Solution:
-    ___ twoSum(, nums: L.. in., target: in.) -> L.. in.:
-        m _ {}
-        n _ le.(nums)
-        ___ i __ ra..(0,n
-            goal _ target - nums[i]
-            __(goal __ m
-                r_ [m[goal], i]
-            m[nums[i]] _ i
-        
+# c_ Solution
+#     ___ twoSum nums L.. in. target in. L.. in.
+#         m _  # dict
+#         n _ le. ?
+#         ___ i __ ra.. 0 ?
+#             goal _ ? - ? ?
+#             __? __ ?
+#                 r_ |? ?| ?|
+#
+#             ? ? ? _ ?
+#
