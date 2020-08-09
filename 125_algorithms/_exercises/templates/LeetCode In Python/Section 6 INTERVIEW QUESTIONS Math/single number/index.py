4866119a33e9f21d7a5dbaddeeb6195c332911f9
Grey <sergejyurskyj@yahoo.com>
2020-08-09 14:46:48 -0700

    Committed Section 6 INTERVIEW QUESTIONS Math robot single number template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 6 INTERVIEW QUESTIONS Math/single number/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 6 INTERVIEW QUESTIONS Math/single number/index.py
@@ -1,7 +1,7 @@
-c_ Solution(object
-    ___ singleNumber(, nums
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        r_ 2*su.(set(nums))-su.(nums)
+# c_ Solution object
+#     ___ singleNumber nums
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         r_ 2*su. se.? - su.?
