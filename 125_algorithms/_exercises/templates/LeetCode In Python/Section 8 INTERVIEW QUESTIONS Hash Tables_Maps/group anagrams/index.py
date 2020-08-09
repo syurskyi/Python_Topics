067dc6c060d39dca315a04635e0473e8d5f4f0f0
Grey <sergejyurskyj@yahoo.com>
2020-08-09 14:53:19 -0700

    Committed Section 8 INTERVIEW QUESTIONS Hash Tables_Maps group anagrams template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 8 INTERVIEW QUESTIONS Hash Tables_Maps/group anagrams/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 8 INTERVIEW QUESTIONS Hash Tables_Maps/group anagrams/index.py
@@ -1,18 +1,18 @@
-c_ Solution:
-    ___ findHash(,s
-        r_ ''.j..(sorted(s))
-    ___ groupAnagrams(, strs: L..[st.]) -> L..[L..[st.]]:
-        answers _ []
-        m _ {}
-
-        ___ s __ strs:
-            hashed _ .findHash(s)
-            __(hashed no. __ m
-                m[hashed] _ []
-            m[hashed].ap..(s)
-        
-        ___ p __ m.values(
-            answers.ap..(p)
-        
-        r_ answers
-
+# c_ Solution
+#     ___ findHash s
+#         r_ ''.j.. so.. ?
+#     ___ groupAnagrams strs L..|st.  L..|L..|st.
+#         answers _  # list
+#         m _  # dict
+#
+#         ___ s __ ?
+#             hashed _ .? ?
+#             __ ? no. __ ?
+#                 ? ? _   # list
+#             ? ?.ap..?
+#
+#         ___ p __ ?.va..
+#             ?.ap.. ?
+#
+#         r_ ?
+#
