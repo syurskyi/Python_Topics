d2ddcdded54a196d93ae0b7a1e9938e6b1943e5e
Grey <sergejyurskyj@yahoo.com>
2020-08-09 14:54:36 -0700

    Committed Section 8 INTERVIEW QUESTIONS Hash Tables_Maps majority element template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 8 INTERVIEW QUESTIONS Hash Tables_Maps/majority element/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 8 INTERVIEW QUESTIONS Hash Tables_Maps/majority element/index.py
@@ -1,10 +1,10 @@
-c_ Solution:
-    ___ majorityElement(, nums: L.. in.) -> in.:
-        m _ {}
-        ___ num __ nums:
-            m[num] _ m.get(num,0)+1
-        ___ num __ nums:
-            __(m[num]>le.(nums)//2
-                r_ num
-        
-                
+# c_ Solution
+#     ___ majorityElement nums L.. in. in.
+#         m _   # dict
+#         ___ num __ ?
+#             ? ? _ ?.ge. ? 0 +1
+#         ___ num __ ?
+#             __ ? ? > le.? //2
+#                 r_ ?
+#
+#
