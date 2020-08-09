b1bec515d980e649b2ff2de56e6164fba1bcc649
Grey <sergejyurskyj@yahoo.com>
2020-08-09 14:36:46 -0700

    Committed Section 5 INTERVIEW QUESTIONS Arrays and Strings move zeroes template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 5 INTERVIEW QUESTIONS Arrays and Strings/move zeroes/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 5 INTERVIEW QUESTIONS Arrays and Strings/move zeroes/index.py
@@ -1,10 +1,10 @@
-c_ Solution:
-    ___ moveZeroes(, nums: L.. in.) -> None:
-        j _ 0
-        ___ num __ nums:
-            __(num !_ 0
-                nums[j] _ num
-                j +_ 1
-
-        ___ x __ ra..(j, le.(nums)):
-            nums[x] _ 0
+# c_ Solution
+#     ___ moveZeroes nums: L.. in.  N..
+#         j _ 0
+#         ___ num __ ?
+#             __ ? !_ 0
+#                 ? ? _ ?
+#                 ? +_ 1
+#
+#         ___ x __ ra.. ? le. ?
+#             ? ? _ 0
