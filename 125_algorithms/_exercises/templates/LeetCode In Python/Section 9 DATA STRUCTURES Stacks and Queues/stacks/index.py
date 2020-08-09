916280fc96ea0f442189016e65718189822dbb35
Grey <sergejyurskyj@yahoo.com>
2020-08-09 14:59:02 -0700

    Committed Section 9 DATA STRUCTURES Stacks and Queues stacks template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 9 DATA STRUCTURES Stacks and Queues/stacks/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 9 DATA STRUCTURES Stacks and Queues/stacks/index.py
@@ -1,19 +1,19 @@
-c_ PlateStack:
-
-    ___  - (
-        .st _   # list
-
-    ___ push(, x: in.)  N..
-        .st.ap..(x)
-
-    ___ pop()  N..
-        __(le.(.st) > 0
-            .st.pop()
-
-    ___ top()  in.:
-        __(le.(.st) __ 0
-            r_ N..
-        r_ .st[-1]
-
-    ___ getLen()  in.:
-      r_ le.(.st)
+# c_ PlateStack
+#
+#     ___  -
+#         .st _   # list
+#
+#     ___ push x in.  N..
+#         .st.ap.. ?
+#
+#     ___ pop  N..
+#         __ le. .st > 0
+#             .st.po.
+#
+#     ___ top  in.
+#         __le. .st __ 0
+#             r_ N..
+#         r_ .st -1
+#
+#     ___ getLen  in.
+#       r_ le. .st
