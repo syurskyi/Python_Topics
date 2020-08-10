0195da5000a8535be1907b9050a1a3ba45bcd376
Grey <sergejyurskyj@yahoo.com>
2020-08-10 12:06:58 -0700

    Committed 309. File Modes templates

--- 095_os_and_sys/_exercises/templates/The_Modern_Python_3_Bootcamp/309. File Modes.py
+++ 095_os_and_sys/_exercises/templates/The_Modern_Python_3_Bootcamp/309. File Modes.py
@@ -1,21 +1,21 @@
-# w - writes and erases existing contents
-w__ o..("haiku.txt", "w") __ file:
-    file.w..("Here's one more haiku\n")
-    file.w..("What about the older one?\n")
-    file.w..("Let's go check it out")
-
-# a - appends to end, preserving original contents
-# NO CONTROL OVER CURSOR
-
-w__ o..("haiku.txt", "a") __ file:
-    file.seek(0)
-    file.w..(":)\n")
-
-# r+ read and write
-w__ o..("haiku.txt", "r+") __ file:
-    file.w..(":)")
-    file.seek(10)
-    file.w..(":(")
-# r+ will not create a file if it doesn't exist
-w__ o..("hello.txt", "a") __ file:
-    file.w..("HELLO!!!")
+# # w - writes and erases existing contents
+# w__ o.. haiku.txt _ __ file
+#     ?.w.. "Here's one more haiku\n"
+#     ?.w.. "What about the older one?\n"
+#     ?.w.. "Let's go check it out"
+#
+# # a - appends to end, preserving original contents
+# # NO CONTROL OVER CURSOR
+#
+# w__ o.. haiku.txt _ __ file
+#     ?.s.. 0
+#     ?.w.. ":)\n"
+#
+# # r+ read and write
+# w__ o.. haiku.txt __ __ file
+#     ?.w.. ":)"
+#     ?.s.. 10
+#     ?.w.. ":("
+# # r+ will not create a file if it doesn't exist
+# w__ o.. hello.txt _ __ file
+#     ?.w.. HELLO!!!
