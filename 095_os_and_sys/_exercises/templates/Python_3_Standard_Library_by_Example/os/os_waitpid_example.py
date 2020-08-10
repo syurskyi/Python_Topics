765a494eb74f9d08b744cb6c2c5cc834ebfd24ba
Grey <sergejyurskyj@yahoo.com>
2020-08-10 08:49:11 -0700

    Committed os_waitpid_example templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_waitpid_example.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_waitpid_example.py
@@ -1,24 +1,24 @@
-#!/usr/bin/env python3
-"""Wait for a worker process.
-"""
-
-#end_pymotw_header
-______ __
-______ ___
-______ ti__
-
-workers _ []
-___ i __ ra..(2):
-    print('PARENT @: Forking @'.f..(__.g_p_, i))
-    worker_pid _ __.f..
-    __ no. worker_pid:
-        print('WORKER @: Starting'.f..(i))
-        ti__.sleep(2 + i)
-        print('WORKER @: Finishing'.f..(i))
-        ___.exit(i)
-    workers.append(worker_pid)
-
-___ pid __ workers:
-    print('PARENT: Waiting for @'.f..(pid))
-    done _ __.waitpid(pid, 0)
-    print('PARENT: Child done:', done)
+# #!/usr/bin/env python3
+# """Wait for a worker process.
+# """
+#
+# #end_pymotw_header
+# ______ __
+# ______ ___
+# ______ ti__
+#
+# workers _ # List
+# ___ i __ ra.. 2
+#     print('PARENT @: Forking @'.f.. __.g_p_ ?
+#     worker_pid _ __.f..
+#     __ no. ?
+#         print('WORKER @: Starting'.f.. ?
+#         ti__.sl.. 2 + ?
+#         print('WORKER @: Finishing'.f.. ?
+#         ___.ex.. ?
+#     ?.ap.. ?
+#
+# ___ pid __ ?
+#     print('PARENT: Waiting for @'.f.. ?
+#     done _ __.w_p.. ? 0
+#     print('PARENT: Child done:' ?
