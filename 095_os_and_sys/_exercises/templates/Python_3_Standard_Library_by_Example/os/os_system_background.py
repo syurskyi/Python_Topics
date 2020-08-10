dd8dbff6aec2ec6f44fc195de5ab2b145e54bc20
Grey <sergejyurskyj@yahoo.com>
2020-08-10 08:45:48 -0700

    Committed os_system_background templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_system_background.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_system_background.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
-"""Running a command in the background
-"""
-
-#end_pymotw_header
-______ __
-______ ti__
-
-print('Calling...')
-__.sy..('date; (sleep 3; date) &')
-
-print('Sleeping...')
-ti__.sleep(5)
+# #!/usr/bin/env python3
+# """Running a command in the background
+# """
+#
+# #end_pymotw_header
+# ______ __
+# ______ ti__
+#
+# print('Calling...')
+# __.sy..('date; (sleep 3; date) &')
+#
+# print('Sleeping...')
+# ti__.sl.. 5
