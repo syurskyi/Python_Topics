798f9a75ab01ccef3db8bf1939e2acc958644be5
Grey <sergejyurskyj@yahoo.com>
2020-08-10 08:46:08 -0700

    Committed os_system_example templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_system_example.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_system_example.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python3
-"""Using os.system() to run external commands.
-"""
-
-#end_pymotw_header
-______ __
-
-# Simple command
-__.sy..('pwd')
+# #!/usr/bin/env python3
+# """Using os.system() to run external commands.
+# """
+#
+# #end_pymotw_header
+# ______ __
+#
+# # Simple command
+# __.sy..('pwd')
