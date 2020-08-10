f516e909336d6133fad99ab166aee630185fb47f
Grey <sergejyurskyj@yahoo.com>
2020-08-10 08:52:32 -0700

    Committed ospath_abspath templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os.path/ospath_basename.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os.path/ospath_basename.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
-# encoding: utf-8
+# #!/usr/bin/env python3
+# # encoding: utf-8
+# #
+# # Copyright (c) 2008 Doug Hellmann All rights reserved.
+# #
+# """Determine the base filename from a path.
+# """
 #
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
 #
-"""Determine the base filename from a path.
-"""
-
-
-#end_pymotw_header
-______ __.p..
-
-PATHS _ [
-    '/one/two/three',
-    '/one/two/three/',
-    '/',
-    '.',
-    '',
-]
-
-___ p.. __ PATHS:
-    print('{!r:>17} : {!r}'.f..(p.., __.p...b..(p..)))
+# #end_pymotw_header
+# ______ __.p..
+#
+# PATHS _
+#     '/one/two/three'
+#     '/one/two/three/'
+#     '/'
+#     '.'
+#     ''
+#
+#
+# ___ p.. __ ?
+#     print('|___>17 _ |__'.f.. p.. __.p...b.. p..
