bda58753a37cba204c2512bc95836ba4f5a9dfec
Grey <sergejyurskyj@yahoo.com>
2020-08-10 08:53:20 -0700

    Committed ospath_commonpath templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os.path/ospath_commonpath.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os.path/ospath_commonpath.py
@@ -1,21 +1,21 @@
-#!/usr/bin/env python3
-# encoding: utf-8
+# #!/usr/bin/env python3
+# # encoding: utf-8
+# #
+# # Copyright (c) 2008 Doug Hellmann All rights reserved.
+# #
+# """Find the prefix string common to a group of paths.
+# """
 #
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
 #
-"""Find the prefix string common to a group of paths.
-"""
-
-
-#end_pymotw_header
-______ __.p..
-
-paths _ ['/one/two/three/four',
-         '/one/two/threefold',
-         '/one/two/three/',
-         ]
-___ p.. __ paths:
-    print('PATH:', p..)
-
-print()
-print('PREFIX:', __.p...commonpath(paths))
+# #end_pymotw_header
+# ______ __.p..
+#
+# paths _ '/one/two/three/four'
+#          '/one/two/threefold'
+#          '/one/two/three/'
+#
+# ___ p.. __ ?
+#     print('PATH:' p..
+#
+# print()
+# print('PREFIX:', __.p...c_p.. ?
