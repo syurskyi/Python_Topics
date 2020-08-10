74ee0371c6229c77ba6c55c824321697b1001a5a
Grey <sergejyurskyj@yahoo.com>
2020-08-10 09:01:27 -0700

    Committed ospath_split templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os.path/ospath_split.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os.path/ospath_split.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
-# encoding: utf-8
+# #!/usr/bin/env python3
+# # encoding: utf-8
+# #
+# # Copyright (c) 2008 Doug Hellmann All rights reserved.
+# #
+# """Separate a path into its directory and base components.
+# """
 #
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
 #
-"""Separate a path into its directory and base components.
-"""
-
-
-#end_pymotw_header
-______ __.p..
-
-PATHS _ [
-    '/one/two/three',
-    '/one/two/three/',
-    '/',
-    '.',
-    '',
-]
-
-___ p.. __ PATHS:
-    print('{!r:>17} : @'.f..(p.., __.p...split(p..)))
+# #end_pymotw_header
+# ______ __.p..
+#
+# PATHS _
+#     '/one/two/three'
+#     '/one/two/three/'
+#     '/'
+#     '.'
+#     ''
+#
+#
+# ___ p.. __ ?
+#     print('|___>17 _ @'.f.. p.. __.p...sp.. p..
