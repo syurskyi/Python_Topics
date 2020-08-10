1c3f90d793e4e36beb870218fa041c3da56bed40
Grey <sergejyurskyj@yahoo.com>
2020-08-10 09:21:59 -0700

    Committed 014_Filename Pattern Matching Using glob templates

--- 095_os_and_sys/_exercises/templates/realpython/014_Filename Pattern Matching Using glob.py
+++ 095_os_and_sys/_exercises/templates/realpython/014_Filename Pattern Matching Using glob.py
@@ -1,74 +1,74 @@
-# Another useful module for pattern matching is glob.
-# .glob() in the glob module works just like fnmatch.fnmatch(), but unlike fnmatch.fnmatch(), it treats files beginning
-# with a period (.) as special. UNIX and related systems translate name patterns with wildcards like ? and * into
-# a list of files. This is called globbing.
+# # Another useful module for pattern matching is glob.
+# # .glob() in the glob module works just like fnmatch.fnmatch(), but unlike fnmatch.fnmatch(), it treats files beginning
+# # with a period (.) as special. UNIX and related systems translate name patterns with wildcards like ? and * into
+# # a list of files. This is called globbing.
+# #
+# # For example, typing mv *.py python_files/ in a UNIX shell moves (mv) all files with the .py extension from
+# # the current directory to the directory python_files. The * character is a wildcard that means “any number
+# # of characters,” and *.py is the glob pattern. This shell capability is not available in the Windows Operating System.
+# # The glob module adds this capability in Python, which enables Windows programs to use this feature.
+# #
+# # Here’s an example of how to use glob to search for all Python (.py) source files in the current directory:
 #
-# For example, typing mv *.py python_files/ in a UNIX shell moves (mv) all files with the .py extension from
-# the current directory to the directory python_files. The * character is a wildcard that means “any number
-# of characters,” and *.py is the glob pattern. This shell capability is not available in the Windows Operating System.
-# The glob module adds this capability in Python, which enables Windows programs to use this feature.
+# ______ g__
+# g__.g__ '@.py'
+# # ['admin.py', 'tests.py']
 #
-# Here’s an example of how to use glob to search for all Python (.py) source files in the current directory:
-
-______ g__
-g__.g__('*.py')
-# ['admin.py', 'tests.py']
-
-# glob.glob('*.py') searches for all files that have the .py extension in the current directory and returns them
-# as a list. glob also supports shell-style wildcards to match patterns:
-
-______ g__
-___ name __ g__.g__('*[0-9]*.txt'):
-    print(name)
-
-# This finds all text (.txt) files that contain digits in the filename:
+# # glob.glob('*.py') searches for all files that have the .py extension in the current directory and returns them
+# # as a list. glob also supports shell-style wildcards to match patterns:
 #
-# data_01.txt
-# data_03.txt
-# data_03_backup.txt
-# data_02_backup.txt
-# data_02.txt
-# data_01_backup.txt
+# ______ g__
+# ___ name __ g__.g__('@[0-9]@.txt'
+#     print ?
 #
-# glob makes it easy to search for files recursively in subdirectories too:
-
-______ g__
-___ file __ g__.iglob('**/*.py', recursive_True):
-    print(file)
-
-# This example makes use of glob.iglob() to search for .py files in the current directory and subdirectories.
-# Passing recursive=True as an argument to .iglob() makes it search for .py files in the current directory and
-# any subdirectories. The difference between glob.iglob() and glob.glob() is that .iglob() returns an iterator instead
-# of a list.
+# # This finds all text (.txt) files that contain digits in the filename:
+# #
+# # data_01.txt
+# # data_03.txt
+# # data_03_backup.txt
+# # data_02_backup.txt
+# # data_02.txt
+# # data_01_backup.txt
+# #
+# # glob makes it easy to search for files recursively in subdirectories too:
 #
-# Running the program above produces the following:
+# ______ g__
+# ___ file __ g__.ig.. '@@/@.py' rec.._T..
+#     print ?
 #
-# admin.py
-# tests.py
-# sub_dir/file1.py
-# sub_dir/file2.py
+# # This example makes use of glob.iglob() to search for .py files in the current directory and subdirectories.
+# # Passing recursive=True as an argument to .iglob() makes it search for .py files in the current directory and
+# # any subdirectories. The difference between glob.iglob() and glob.glob() is that .iglob() returns an iterator instead
+# # of a list.
+# #
+# # Running the program above produces the following:
+# #
+# # admin.py
+# # tests.py
+# # sub_dir/file1.py
+# # sub_dir/file2.py
+# #
+# # pathlib contains similar methods for making flexible file listings. The example below shows how you can
+# # use .Path.glob() to list file types that start with the letter p:
 #
-# pathlib contains similar methods for making flexible file listings. The example below shows how you can
-# use .Path.glob() to list file types that start with the letter p:
-
-____ p_l_ ______ P..
-p _ P..('.')
-___ name __ p.g__('*.p*'):
-    print(name)
-
-# admin.py
-# scraper.py
-# docs.pdf
+# ____ p_l_ ______ P..
+# p _ P.. '.'
+# ___ name __ p.g__ '@.p@'
+#     print ?
 #
-# Calling p.glob('*.p*') returns a generator object that points to all files in the current directory that start with
-# the letter p in their file extension. Path.glob() is similar to os.glob() discussed above. As you can see,
-# pathlib combines many of the best features of the os, os.path, and glob modules into one single module, which makes
-# it a joy to use.
-#
-# To recap, here is a table of the functions we have covered in this section:
-# Function 	Description
-# startswith() 	Tests if a string starts with a specified pattern and returns True or False
-# endswith() 	Tests if a string ends with a specified pattern and returns True or False
-# fnmatch.fnmatch(filename, pattern) 	Tests whether the filename matches the pattern and returns True or False
-# glob.glob() 	Returns a list of filenames that match a pattern
-# pathlib.Path.glob() 	Finds patterns in path names and returns a generator object
+# # admin.py
+# # scraper.py
+# # docs.pdf
+# #
+# # Calling p.glob('*.p*') returns a generator object that points to all files in the current directory that start with
+# # the letter p in their file extension. Path.glob() is similar to os.glob() discussed above. As you can see,
+# # pathlib combines many of the best features of the os, os.path, and glob modules into one single module, which makes
+# # it a joy to use.
+# #
+# # To recap, here is a table of the functions we have covered in this section:
+# # Function 	Description
+# # startswith() 	Tests if a string starts with a specified pattern and returns True or False
+# # endswith() 	Tests if a string ends with a specified pattern and returns True or False
+# # fnmatch.fnmatch(filename, pattern) 	Tests whether the filename matches the pattern and returns True or False
+# # glob.glob() 	Returns a list of filenames that match a pattern
+# # pathlib.Path.glob() 	Finds patterns in path names and returns a generator object
