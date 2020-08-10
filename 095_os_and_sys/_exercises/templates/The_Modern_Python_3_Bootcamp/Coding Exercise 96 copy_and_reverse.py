7186e122929293155b3eedf4a5a799a4ea1c8baf
Grey <sergejyurskyj@yahoo.com>
2020-08-10 12:08:47 -0700

    Committed Coding Exercise 96 copy_and_reverse templates

--- 095_os_and_sys/_exercises/templates/The_Modern_Python_3_Bootcamp/Coding Exercise 96 copy_and_reverse.py
+++ 095_os_and_sys/_exercises/templates/The_Modern_Python_3_Bootcamp/Coding Exercise 96 copy_and_reverse.py
@@ -1,12 +1,12 @@
-# Copy and Reverse Solution
+# # Copy and Reverse Solution
+# #
+# # copy_and_reverse  is very similar to the previous exercise, copy ,
+# # ______ that we reverse the text using a slice before we write it to the new file:
 #
-# copy_and_reverse  is very similar to the previous exercise, copy ,
-# ______ that we reverse the text using a slice before we write it to the new file:
-
-
-___ copy_and_reverse(file_name, new_file_name):
-    w__ o..(file_name) __ file:
-        text _ file.r..
-
-    w__ o..(new_file_name, "w") __ new_file:
-        new_file.w..(text[::-1])
+#
+# ___ copy_and_reverse file_name new_file_name
+#     w__ o.. ? __ file
+#         text _ ?.r..
+#
+#     w__ o.. new_file_name _ __ new_file
+#         ?.w.. ? ||-1
