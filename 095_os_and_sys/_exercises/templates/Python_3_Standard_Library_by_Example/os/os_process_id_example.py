d13fbfda56d8832be5fb363091c42ace9cdc4df0
Grey <sergejyurskyj@yahoo.com>
2020-08-10 08:26:09 -0700

    Committed os_process_id_example templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_process_id_example.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_process_id_example.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python3
-"""Find information about the current process.
-"""
-
-#end_pymotw_header
-______ __
-
-print('This process id:', __.getpid())
-print('Parent process :', __.getppid())
-print('Process group  :', __.getpgid(__.getpid()))
-print('Parent group   :', __.getpgid(__.getppid()))
-print('Session id     :', __.getsid(0))
+# #!/usr/bin/env python3
+# """Find information about the current process.
+# """
+#
+# #end_pymotw_header
+# ______ __
+#
+# print('This process id:', __.g_p_
+# print('Parent process :', __.g_pp..
+# print('Process group  :', __.g_pg.. __.g_p_
+# print('Parent group   :', __.g_pg.. __.g_pp..
+# print('Session id     :', __.g_s.. 0
