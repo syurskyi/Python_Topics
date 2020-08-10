3c83989c613709169a9aa82c12bb7862f7899de7
Grey <sergejyurskyj@yahoo.com>
2020-08-10 12:45:18 -0700

    Committed 03-with_statement execises

---
+++ 095_os_and_sys/_exercises/exercises/___repeat_again/02-write_file_!cool!.py
@@ -0,0 +1,27 @@
+﻿# # -*- coding: utf-8 -*-
+#
+# """Пример записи данных в текстовый файл"""
+#
+# _______ __.p..
+#
+#
+# text = '''Hello!
+# I am a text file. And I had been written with a Python script
+# before you opened me, so look up the docs and try to delete
+# me using Python, too.
+# '''
+#
+#
+# ___ write_text_to_file filename text
+# 	"""Функция для записи в	файл filename строки text"""
+#
+# 	# Открытие файла для записи
+# 	f _ o.. f.. _
+# 	# Запись строки text в файл
+# 	?.w.. t..
+# 	# Закрытие файла
+# 	?.c..
+#
+#
+# __ _______ __ ______
+#     ? __.p___.j.. data example02.txt t...
