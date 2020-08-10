9d51fca95b509710c1c788035c19602354dff514
Grey <sergejyurskyj@yahoo.com>
2020-08-10 09:37:54 -0700

    Committed 027_Creating New ZIP Archives templates

--- 095_os_and_sys/_exercises/templates/realpython/027_Creating New ZIP Archives.py
+++ 095_os_and_sys/_exercises/templates/realpython/027_Creating New ZIP Archives.py
@@ -1,23 +1,23 @@
-# To create a new ZIP archive, you open a ZipFile object in write mode (w) and add the files you want to archive:
-
-______ z_f_
-
-file_list _ ['file1.py', 'sub_dir/', 'sub_dir/bar.py', 'sub_dir/foo.py']
-w__ z_f_.ZipFile('new.zip', 'w') __ new_zip:
-    ___ name __ file_list:
-        new_zip.w..(name)
-
-# In the example, new_zip is opened in write mode and each file in file_list is added to the archive.
-# When the with statement suite is finished, new_zip is closed. Opening a ZIP file in write mode erases
-# the contents of the archive and creates a new archive.
+# # To create a new ZIP archive, you open a ZipFile object in write mode (w) and add the files you want to archive:
 #
-# To add files to an existing archive, open a ZipFile object in append mode and then add the files:
+# ______ z_f_
 #
-# Open a ZipFile object in append mode
-w__ z_f_.ZipFile('new.zip', 'a') __ new_zip:
-    new_zip.w..('data.txt')
-    new_zip.w..('latin.txt')
-
-# Here, you open the new.zip archive you created in the previous example in append mode. Opening the ZipFile object
-# in append mode allows you to add new files to the ZIP file without deleting its current contents.
-# After adding files to the ZIP file, the with statement goes out of context and closes the ZIP file.
+# file_list _  'file1.py', 'sub_dir/', 'sub_dir/bar.py', 'sub_dir/foo.py'
+# w__ z_f_.ZF.. 'new.zip' _ __ new_zip
+#     ___ name __ ?
+#         ?.w.. ?
+#
+# # In the example, new_zip is opened in write mode and each file in file_list is added to the archive.
+# # When the with statement suite is finished, new_zip is closed. Opening a ZIP file in write mode erases
+# # the contents of the archive and creates a new archive.
+# #
+# # To add files to an existing archive, open a ZipFile object in append mode and then add the files:
+# #
+# # Open a ZipFile object in append mode
+# w__ z_f_.ZF.. 'new.zip' _ __ new_zip
+#     ?.w.. 'data.txt'
+#     ?.w.. 'latin.txt'
+#
+# # Here, you open the new.zip archive you created in the previous example in append mode. Opening the ZipFile object
+# # in append mode allows you to add new files to the ZIP file without deleting its current contents.
+# # After adding files to the ZIP file, the with statement goes out of context and closes the ZIP file.
