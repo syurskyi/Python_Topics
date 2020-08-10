6b6a1d461610fa5d2fa06b0c3ece803df3d65048
Grey <sergejyurskyj@yahoo.com>
2020-08-10 12:07:54 -0700

    Committed Coding Exercise 95 copy templates

--- 095_os_and_sys/_exercises/templates/The_Modern_Python_3_Bootcamp/Coding Exercise 95 copy.py
+++ 095_os_and_sys/_exercises/templates/The_Modern_Python_3_Bootcamp/Coding Exercise 95 copy.py
@@ -1,19 +1,19 @@
-# Copy Solution
+# # Copy Solution
+# #
+# # Copy should copy contents from one file to another.  For example, after running:
 #
-# Copy should copy contents from one file to another.  For example, after running:
-
-"""
-copy('story.txt', 'story_copy.txt') # None
-"""
-
-# We would expect the contents of story.txt and story_copy.txt to now be the same.
+# """
+# copy('story.txt', 'story_copy.txt') # None
+# """
 #
-# Here's my solution:
-
-
-___ co..(file_name, new_file_name):
-    w__ o..(file_name) __ file:
-        text _ file.r..
-
-    w__ o..(new_file_name, "w") __ new_file:
-        new_file.w..(text)
+# # We would expect the contents of story.txt and story_copy.txt to now be the same.
+# #
+# # Here's my solution:
+#
+#
+# ___ co.. file_name new_file_name
+#     w__ o.. ? __ file
+#         text _ ?.r..
+#
+#     w__ o.. ? _ __ new_file
+#         ?.w.. ?
