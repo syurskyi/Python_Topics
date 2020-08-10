b81862383df1481ce369bff3ff72981d700544b4
Grey <sergejyurskyj@yahoo.com>
2020-08-10 08:56:23 -0700

    Committed ospath_expanduser templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os.path/ospath_expanduser.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os.path/ospath_expanduser.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
-# encoding: utf-8
+# #!/usr/bin/env python3
+# # encoding: utf-8
+# #
+# # Copyright (c) 2008 Doug Hellmann All rights reserved.
+# #
+# """Expand tilde in filenames.
+# """
 #
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
 #
-"""Expand tilde in filenames.
-"""
-
-
-#end_pymotw_header
-______ __.p..
-
-___ user __ ['', 'dhellmann', 'nosuchuser']:
-    lookup _ '~' + user
-    print('{!r:>15} : {!r}'.f..(
-        lookup, __.p...expanduser(lookup)))
+# #end_pymotw_header
+# ______ __.p..
+#
+# ___ user __ '', 'dhellmann', 'nosuchuser'
+#     lookup _ '~' + ?
+#     print('|___>15 _ |__'.f..
+#         ? __.p...e_u.. ?
