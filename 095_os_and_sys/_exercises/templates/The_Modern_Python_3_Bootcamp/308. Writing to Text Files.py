b0df3be24ca3edbe6a59fb7bbb3141e6938dbfbc
Grey <sergejyurskyj@yahoo.com>
2020-08-10 12:05:17 -0700

    Committed 308. Writing to Text Files templates

--- 095_os_and_sys/_exercises/templates/The_Modern_Python_3_Bootcamp/308. Writing to Text Files.py
+++ 095_os_and_sys/_exercises/templates/The_Modern_Python_3_Bootcamp/308. Writing to Text Files.py
@@ -1,9 +1,9 @@
-# To write to a file, first open it in "w" mode
-w__ o..("haiku.txt", "w") __ file:
-    file.w..("Here's one more haiku\n")
-    file.w..("What about the older one?\n")
-    file.w..("Let's go check it out")
-
-# You can also write to files that don't yet exist
-w__ o..("lol.txt", "w") __ file:
-    file.w..("haha" * 10000)
+# # To write to a file, first open it in "w" mode
+# w__ o.. haiku.txt _ __ file
+#     ?.w.. "Here's one more haiku\n"
+#     ?.w.. "What about the older one?\n"
+#     ?.w.. "Let's go check it out"
+#
+# # You can also write to files that don't yet exist
+# w__ o.. lol.txt _ __ file
+#     ?.w..("haha" * 10000)
