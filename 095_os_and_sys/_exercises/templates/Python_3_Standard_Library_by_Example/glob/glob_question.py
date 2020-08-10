d9623eda80763ca46eced37a934baeacfe05473d
Grey <sergejyurskyj@yahoo.com>
2020-08-09 21:44:48 -0700

    Committed glob_question templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/glob/glob_question.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/glob/glob_question.py
@@ -1,33 +1,8 @@
-#!/usr/bin/env python3
+# """Example expansion of question mark wild card.
+# """
 #
-# Copyright 2007 Doug Hellmann.
+# #end_pymotw_header
+# ______ g__
 #
-#
-#                         All Rights Reserved
-#
-# Permission to use, copy, modify, and distribute this software
-# and its documentation for any purpose and without fee is hereby
-# granted, provided that the above copyright notice appear in all
-# copies and that both that copyright notice and this permission
-# notice appear in supporting documentation, and that the name of
-# Doug Hellmann not be used in advertising or publicity
-# pertaining to distribution of the software without specific,
-# written prior permission.
-#
-# DOUG HELLMANN DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
-# SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
-# AND FITNESS, IN NO EVENT SHALL DOUG HELLMANN BE LIABLE FOR ANY
-# SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
-# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
-# ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
-# THIS SOFTWARE.
-
-"""Example expansion of question mark wild card.
-"""
-
-#end_pymotw_header
-______ g__
-
-___ name __ so..(g__.g__('dir/file?.txt')):
-    print(name)
+# ___ name __ so.. g__.g__ dir/file@.txt
+#     print ?
