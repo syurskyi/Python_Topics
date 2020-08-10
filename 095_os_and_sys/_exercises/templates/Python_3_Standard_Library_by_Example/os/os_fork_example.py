36771182138fc5233543836fc8a1e86fcde54515
Grey <sergejyurskyj@yahoo.com>
2020-08-10 08:21:25 -0700

    Committed os_fork_example templates

--- 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_fork_example.py
+++ 095_os_and_sys/_exercises/templates/Python_3_Standard_Library_by_Example/os/os_fork_example.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
-"""Simple example of using os.fork to create a new child process.
-"""
-
-#end_pymotw_header
-______ __
-
-pid = __.fork()
-
-__ pid:
-    print('Child process id:', pid)
-____
-    print('I am the child')
+# #!/usr/bin/env python3
+# """Simple example of using os.fork to create a new child process.
+# """
+#
+# #end_pymotw_header
+# ______ __
+#
+# pid = __.f..
+#
+# __ ?
+#     print('Child process id:' ?
+# ____
+#     print('I am the child')
