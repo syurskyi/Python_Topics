d8fac03afcae63e4881b0b961ba400252bddf1cf
Grey <sergejyurskyj@yahoo.com>
2020-08-09 19:32:49 -0700

    Committed 008_Other Configuration Methods_yaml template

--- 090_logging/_exercises/_templates/Logging in Python/008_Other Configuration Methods_yaml.py
+++ 090_logging/_exercises/_templates/Logging in Python/008_Other Configuration Methods_yaml.py
@@ -1,11 +1,11 @@
-______ l____.config
-______ yaml
-
-with open('config.yaml', 'r') __ f:
-    config _ yaml.safe_load(f.read())
-    l____.config.dictConfig(config)
-
-logger _ l____.getLogger(__name__)
-
-logger.debug('This is a debug message')
-# 2018-07-13 14:05:03,766 - __main__ - DEBUG - This is a debug message
+# ______ l____.c..
+# ______ y..
+#
+# w_ o..  config.yaml _ __ f
+#     config _ y__.s_l.. ?.r..
+#     l____.c__.dC.. ?
+#
+# logger _ l____.gL.. -n
+#
+# ?.d.. This is a debug message
+# # 2018-07-13 14:05:03,766 - __main__ - DEBUG - This is a debug message
