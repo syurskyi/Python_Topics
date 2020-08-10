10e05cd57c41a1b83dc06d58d2569a96f1994773
Grey <sergejyurskyj@yahoo.com>
2020-08-10 09:42:27 -0700

    Committed 030_Creating New TAR Archives templates

--- 095_os_and_sys/_exercises/templates/realpython/030_Creating New TAR Archives.py
+++ 095_os_and_sys/_exercises/templates/realpython/030_Creating New TAR Archives.py
@@ -1,43 +1,43 @@
-# Here’s how you do it:
-
-______ t_f_
-
-file_list _ ['app.py', 'config.py', 'CONTRIBUTORS.md', 'tests.py']
-w__ t_f_.o..('packages.tar', mode_'w') __ ta_:
-    ___ file __ file_list:
-        ta_.add(file)
-
-# Read the contents of the newly created archive
-w__ t_f_.o..('package.tar', mode_'r') __ t:
-    ___ member __ t.getmembers():
-        print(member.name)
-
-# app.py
-# config.py
-# CONTRIBUTORS.md
-# tests.py
+# # Here’s how you do it:
 #
-# First, you make a list of files to be added to the archive so that you don’t have to add each file manually.
-# The next line uses the with context manager to open a new archive called packages.tar in write mode.
-# Opening an archive in write mode('w') enables you to write new files to the archive.
-# Any existing files in the archive are deleted and a new archive is created.
-# After the archive is created and populated, the with context manager automatically closes it and saves
-# it to the filesystem. The last three lines open the archive you just created and print out the names of the files
-# contained in it.
+# ______ t_f_
 #
-# To add new files to an existing archive, open the archive in append mode ('a'):
-
-w__ t_f_.o..('package.tar', mode_'a') __ ta_:
-    ta_.add('foo.bar')
-
-w__ t_f_.o..('package.tar', mode_'r') __ ta_:
-   ___ member __ ta_.getmembers():
-       print(member.name)
-
-# app.py
-# config.py
-# CONTRIBUTORS.md
-# tests.py
-# foo.bar
+# file_list _ 'app.py', 'config.py', 'CONTRIBUTORS.md', 'tests.py'
+# w__ t_f_.o.. 'packages.tar' m.. _ _ __ ta_
+#     ___ file __ ?
+#         ta_.a.. ?
 #
-# Opening an archive in append mode allows you to add new files to it without deleting the ones already in it.
+# # Read the contents of the newly created archive
+# w__ t_f_.o.. 'package.tar' m.._ _ __ t
+#     ___ member __ ?.g_m..
+#         print ?.n..
+#
+# # app.py
+# # config.py
+# # CONTRIBUTORS.md
+# # tests.py
+# #
+# # First, you make a list of files to be added to the archive so that you don’t have to add each file manually.
+# # The next line uses the with context manager to open a new archive called packages.tar in write mode.
+# # Opening an archive in write mode('w') enables you to write new files to the archive.
+# # Any existing files in the archive are deleted and a new archive is created.
+# # After the archive is created and populated, the with context manager automatically closes it and saves
+# # it to the filesystem. The last three lines open the archive you just created and print out the names of the files
+# # contained in it.
+# #
+# # To add new files to an existing archive, open the archive in append mode ('a'):
+#
+# w__ t_f_.o.. 'package.tar' m.._ _ __ ta_
+#     ta_.a.. 'foo.bar'
+#
+# w__ t_f_.o.. 'package.tar' m.._ _ __ ta_
+#    ___ member __ ta_.g_m..
+#        print ?.n..
+#
+# # app.py
+# # config.py
+# # CONTRIBUTORS.md
+# # tests.py
+# # foo.bar
+# #
+# # Opening an archive in append mode allows you to add new files to it without deleting the ones already in it.
