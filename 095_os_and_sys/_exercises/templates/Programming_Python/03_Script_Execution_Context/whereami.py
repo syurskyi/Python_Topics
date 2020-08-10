5d04feb8362aa64c1e49755c15ef1ce3dd3e41e9
Grey <sergejyurskyj@yahoo.com>
2020-08-09 21:48:30 -0700

    Committed os_cwd_example templates

--- 095_os_and_sys/_exercises/templates/Programming_Python/03_Script_Execution_Context/whereami.py
+++ 095_os_and_sys/_exercises/templates/Programming_Python/03_Script_Execution_Context/whereami.py
@@ -1,4 +1,4 @@
 ______ __, sys
-print('my os.getcwd =>', __.getcwd())           # show my cwd execution dir
+print('my os.getcwd =>', __.g..())           # show my cwd execution dir
 print('my sys.path  =>', sys.path[:6])          # show first 6 ______ paths
 input()                                         # wait for keypress if clicked
