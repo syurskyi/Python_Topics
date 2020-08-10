e996ff475b3d8c9de70d7f90418bd64a77b89008
Grey <sergejyurskyj@yahoo.com>
2020-08-10 09:17:58 -0700

    Committed 008_Creating a Single Directory templates

--- 095_os_and_sys/_exercises/templates/realpython/008_Creating a Single Directory.py
+++ 095_os_and_sys/_exercises/templates/realpython/008_Creating a Single Directory.py
@@ -1,43 +1,43 @@
-# To create a single directory, pass a path to the directory as a parameter to os.mkdir():
+# # To create a single directory, pass a path to the directory as a parameter to os.mkdir():
+# #
+# # ______ os
+# # os.mkdir('example_directory/')
 #
-# ______ os
-# os.mkdir('example_directory/')
-
-# If a directory already exists, os.mkdir() raises FileExistsError. Alternatively, you can create a directory using
-# pathlib:
-
-____ pathlib ______ P..
-
-p _ P..('example_directory/')
-p.mkdir()
-
-# If the path already exists, mkdir() raises a FileExistsError:
+# # If a directory already exists, os.mkdir() raises FileExistsError. Alternatively, you can create a directory using
+# # pathlib:
 #
-# >>> p.mkdir()
-# Traceback (most recent call last):
-#   File '<stdin>', line 1, in <module>
-#   File '/usr/lib/python3.5/pathlib.py', line 1214, in mkdir
-#     self._accessor.mkdir(self, mode)
-#   File '/usr/lib/python3.5/pathlib.py', line 371, in wrapped
-#     return strfunc(str(pathobj), *args)
-# FileExistsError: [Errno 17] File exists: '.'
-# [Errno 17] File exists: '.'
+# ____ pathlib ______ P..
 #
-# To avoid errors like this, catch the error when it happens and let your user know:
-
-____ pathlib ______ P..
-
-p _ P..('example_directory')
-___
-    p.mkdir()
-______ FileExistsError __ exc:
-    print(exc)
-
-# Alternatively, you can ignore the FileExistsError by passing the exist_ok=True argument to .mkdir():
-
-____ pathlib ______ P..
-
-p _ P..('example_directory')
-p.mkdir(exist_ok_True)
-
-# This will not raise an error if the directory already exists.
+# p _ P..('example_directory/')
+# ?.m..
+#
+# # If the path already exists, mkdir() raises a FileExistsError:
+# #
+# # >>> p.mkdir()
+# # Traceback (most recent call last):
+# #   File '<stdin>', line 1, in <module>
+# #   File '/usr/lib/python3.5/pathlib.py', line 1214, in mkdir
+# #     self._accessor.mkdir(self, mode)
+# #   File '/usr/lib/python3.5/pathlib.py', line 371, in wrapped
+# #     return strfunc(str(pathobj), *args)
+# # FileExistsError: [Errno 17] File exists: '.'
+# # [Errno 17] File exists: '.'
+# #
+# # To avoid errors like this, catch the error when it happens and let your user know:
+#
+# ____ pathlib ______ P..
+#
+# p _ P.. 'example_directory'
+# ___
+#     ?.m..
+# ______ F.. __ exc
+#     print ?
+#
+# # Alternatively, you can ignore the FileExistsError by passing the exist_ok=True argument to .mkdir():
+#
+# ____ pathlib ______ P..
+#
+# p _ P.. 'example_directory'
+# ?.m.. exist_ok_T..
+#
+# # This will not raise an error if the directory already exists.
