ea96ca750e0a704e094f37a08d87cff8fcbbaea5
Grey <sergejyurskyj@yahoo.com>
2020-08-10 09:20:50 -0700

    Committed 013_More Advanced Pattern Matching templates

--- 095_os_and_sys/_exercises/templates/realpython/013_More Advanced Pattern Matching.py
+++ 095_os_and_sys/_exercises/templates/realpython/013_More Advanced Pattern Matching.py
@@ -1,22 +1,22 @@
-# Let’s suppose you want to find .txt files that meet certain criteria. For example, you could be only interested
-# in finding .txt files that contain the word data, a number between a set of underscores, and the word backup
-# in their filename. Something similar to data_01_backup, data_02_backup, or data_03_backup.
+# # Let’s suppose you want to find .txt files that meet certain criteria. For example, you could be only interested
+# # in finding .txt files that contain the word data, a number between a set of underscores, and the word backup
+# # in their filename. Something similar to data_01_backup, data_02_backup, or data_03_backup.
+# #
+# # Using fnmatch.fnmatch(), you could do it this way:
+# #
 #
-# Using fnmatch.fnmatch(), you could do it this way:
+# ______ __
+# ______ f_m_
 #
-
-______ __
-______ f_m_
-
-___ filename __ __.l_d_('./some_directory'):
-    __ f_m_.f_m_(filename, 'data_*_backup.txt'):
-        print(filename)
-
-# Here, you print only the names of files that match the data_*_backup.txt pattern. The asterisk in the pattern
-# will match any character, so running this will find all text files whose filenames start with the word data
-# and end in backup.txt, as you can see from the output below:
+# ___ filename __ __.l_d_ './some_directory'
+#     __ f_m_.f_m_ ? 'data_@_backup.txt'
+#         print ?
 #
-# data_03_backup.txt
-# data_02_backup.txt
-# data_01_backup.txt
-
+# # Here, you print only the names of files that match the data_*_backup.txt pattern. The asterisk in the pattern
+# # will match any character, so running this will find all text files whose filenames start with the word data
+# # and end in backup.txt, as you can see from the output below:
+# #
+# # data_03_backup.txt
+# # data_02_backup.txt
+# # data_01_backup.txt
+#
