40c7c215b47613f1ce3973ad5a327b7e353778eb
Grey <sergejyurskyj@yahoo.com>
2020-08-11 14:05:42 -0700

    Committed ospath_abspath execises

--- 095_os_and_sys/_exercises/exercises/Python_3_Standard_Library_by_Example/os/os_listdir.py
+++ 095_os_and_sys/_exercises/exercises/Python_3_Standard_Library_by_Example/os/os_listdir.py
@@ -1,9 +1,9 @@
-# #!/usr/bin/env python3
-# """A simple directory listing.
-# """
-#
-# #end_pymotw_header
-# ______ __
-# ______ ___
-#
-# print so.. __.l_d_ ___.a.. 1
+#!/usr/bin/env python3
+"""A simple directory listing.
+"""
+
+#end_pymotw_header
+import os
+import sys
+
+print(sorted(os.listdir(sys.argv[''])))
