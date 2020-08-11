5b4d4b598226b1d3b13fdc8cf3bb348858a3c1f8
Grey <sergejyurskyj@yahoo.com>
2020-08-11 14:27:47 -0700

    Committed ospath_join execises

--- 095_os_and_sys/_exercises/exercises/Python_3_Standard_Library_by_Example/os.path/ospath_join.py
+++ 095_os_and_sys/_exercises/exercises/Python_3_Standard_Library_by_Example/os.path/ospath_join.py
@@ -1,19 +1,19 @@
-# #!/usr/bin/env python3
-# # encoding: utf-8
-# #
-# #
-# """Combine path components to create a single path.
-# """
+#!/usr/bin/env python3
+# encoding: utf-8
 #
 #
-# #end_pymotw_header
-# ______ __.p..
-#
-# PATHS _
-#     ('one', 'two', 'three')
-#     ('/', 'one', 'two', 'three')
-#     ('/one', '/two', '/three')
-#
-#
-# ___ parts __ ?
-#     print('@ _ |__'.f.. ? __.p...j.. @?
+"""Combine path components to create a single path.
+"""
+
+
+#end_pymotw_header
+import os.path
+
+PATHS = [
+    ('one', 'two', 'three'),
+    ('/', 'one', 'two', 'three'),
+    ('/one', '/two', '/three')]
+
+
+for parts in PATHS:
+    print('{} : {!r}'.format(parts, os.path.join(*parts)))
