8608b36cc9c553ea67bac8fb5f179e0bb685a470
Grey <sergejyurskyj@yahoo.com>
2020-08-11 14:30:29 -0700

    Committed ospath_normpath execises

---
+++ 095_os_and_sys/_exercises/exercises/___repeat_again/ospath_expanduser.py
@@ -0,0 +1,14 @@
+# #!/usr/bin/env python3
+# # encoding: utf-8
+# #
+# #
+# """Expand tilde in filenames.
+# """
+#
+#
+# #end_pymotw_header
+# ______ __.p..
+#
+# ___ user __ '', 'dhellmann', 'nosuchuser'
+#     lookup _ '~' + ?
+#     print('|___>15 _ |__'.f.. ? __.p...e_u.. ?
