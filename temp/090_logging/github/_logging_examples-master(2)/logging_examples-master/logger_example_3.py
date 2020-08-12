e3851a5cc364526c2e074033498fb70dbb390f98
Grey <sergejyurskyj@yahoo.com>
2020-08-12 16:47:09 -0700

    Committed logging and path github template

--- temp/090_logging/github/_logging_examples-master(2)/logging_examples-master/logger_example_3.py
+++ temp/090_logging/github/_logging_examples-master(2)/logging_examples-master/logger_example_3.py
@@ -1,42 +1,42 @@
-"""Demonstrate basic logging functionality with multiple modules."""
-
-______ argparse
-______ ?
-______ ___
-
-____ ex3_sub_module ______ foo
-
-
-___ simple_parser():
-    
-    # principal logger
-    logger _ ?.gL..( -n)
-    logger.sL..(?.I..)
-    
-    # set a file handler to write output to file
-    fh _ ?.FH..("ex3_log.log.log")
-    
-    # specify a format for the log output
-    formatter _ ?.F..('%(a_t_)s - %(name)s - %(l..)s - %(m..)s')
-    fh.sF..(formatter)
-    
-    # assign the formatted file handler to the logger
-    logger.aH..(fh)
-    
-    logger.i..("Command Line inputs: " + ' '.j..(___.argv[1:]))
-    
-    parser _ argparse.AP..
-    
-    parser.add_argument('A', type_int)
-    parser.add_argument('B', type_int)
-    parser.add_argument('C', type_int)
-    
-    args _ parser.parse_args()
-    
-    r_ args.A, args.B, args.C
-
-
-__  -n __ '__main__':
-    args _ simple_parser()
-    print(foo(*args))
-    
+# """Demonstrate basic logging functionality with multiple modules."""
+#
+# ______ argparse
+# ______ ?
+# ______ ___
+#
+# ____ ex3_sub_module ______ foo
+#
+#
+# ___ simple_parser
+#
+#     # principal logger
+#     logger _ ?.gL..  -n
+#     ?.sL..(?.I..
+#
+#     # set a file handler to write output to file
+#     fh _ ?.FH.. "ex3_log.log.log"
+#
+#     # specify a format for the log output
+#     formatter _ ?.F.. '_ a_t_ s - _ n.. s - % l.. s - _ m.. s'
+#     f_.sF.. ?
+#
+#     # assign the formatted file handler to the logger
+#     ?.aH.. ?
+#
+#     ?.i..("Command Line inputs: " + ' '.j.. ___.ar.. 1|
+#
+#     parser _ a__.AP..
+#
+#     ?.a.. 'A' t.._in.
+#     ?.a.. 'B' t.._in.
+#     ?.a.. 'C' t.._in.
+#
+#     args _ ?.p_a..
+#
+#     r_ ?.? ?.? ?.?
+#
+#
+# __  -n __ _____
+#     args _ s..
+#     print f.. @
+#
