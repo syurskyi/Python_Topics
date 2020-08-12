272e03a0ec11672b8b61dfcfeb9766165c52c9d1
Grey <sergejyurskyj@yahoo.com>
2020-08-12 16:50:29 -0700

    Committed logging and path github template

--- temp/090_logging/github/_pyloggingExample-master/Module.py
+++ temp/090_logging/github/_pyloggingExample-master/Module.py
@@ -1,19 +1,19 @@
-______ ?
-
-# create logger
-module_logger _ ?.gL..('build.log.gingerModule')
-
-c_ Copy:
-    ___  -
-        logger _ ?.gL..('build.log.gingerModule.Copy')
-    ___ execute
-        logger.i..('COPY')
-        logger.i..('Sucessfull')
-
-
-c_ Delete:
-    ___  -
-        logger _ ?.gL..('build.log.gingerModule.Delete')
-    ___ execute
-        logger.i..('DELETE')
-        logger.i..('Sucessfull')
+# ______ ?
+#
+# # create logger
+# module_logger _ ?.gL.. 'build.log.gingerModule'
+#
+# c_ Copy
+#     ___  -
+#         logger _ ?.gL.. 'build.log.gingerModule.Copy'
+#     ___ execute
+#         ?.i.. 'COPY'
+#         ?.i.. 'Sucessfull'
+#
+#
+# c_ Delete:
+#     ___  -
+#         logger _ ?.gL.. 'build.log.gingerModule.Delete'
+#     ___ execute
+#         ?.i.. 'DELETE'
+#         ?.i.. 'Sucessfull'
--- temp/090_logging/github/_pyloggingExample-master/logger.py
+++ temp/090_logging/github/_pyloggingExample-master/logger.py
@@ -1,19 +1,19 @@
-______ ? __ logcuBABA
-
-    
-___ whoami():
-    logcuBABA.i..("Now I'm there")
-
-___ foo():
-    logcuBABA.i..("I'm here")
-    whoami()
-    logcuBABA.i..("I'm back here again")
-
-logcuBABA.bC..(
-    format_"%(a_t_)-15s [%(l..)s] %(funcName)s: %(m..)s",
-    level_logcuBABA.I..)
-foo()
-
-
-__  -n __ '__main__':
-    
+# ______ ? __ logcuBABA
+#
+#
+# ___ whoami
+#     logcuBABA.i.. "Now I'm there"
+#
+# ___ foo
+#     logcuBABA.i.. "I'm here"
+#     whoami()
+#     logcuBABA.i.. "I'm back here again")
+#
+# logcuBABA.bC..
+#     format_"%(a_t_)-15s [%(l..)s] %(funcName)s: %(m..)s",
+#     level_logcuBABA.I..)
+# foo()
+#
+#
+# __  -n __ '__main__':
+#
