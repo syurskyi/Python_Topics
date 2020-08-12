8b3fed8b13744e28a10c5b788249229daba633c4
Grey <sergejyurskyj@yahoo.com>
2020-08-12 15:59:26 -0700

    Committed logging and path github template

--- temp/090_logging/github/_logging_examples-master(2)/logging_examples-master/ex3_sub_module.py
+++ temp/090_logging/github/_logging_examples-master(2)/logging_examples-master/ex3_sub_module.py
@@ -1,18 +1,18 @@
-______ ?
-
-___ foo(A, B, C):
-
-    logger _ ?.gL..( -n)
-    logger.sL..(?.I..)
-    fh _ ?.FH..("ex3_log.log.log")
-    formatter _ ?.F..('%(a_t_)s - %(name)s - %(l..)s - %(m..)s')
-    fh.sF..(formatter)
-    logger.aH..(fh)
-
-    logger.i..(f'args: ({A}, {B}, {C})')
-    
-    result _ (A - B) * C
-
-    logger.i..(f'result: {result}')
-    
-    r_ result
+# ______ ?
+#
+# ___ foo A, B, C
+#
+#     logger _ ?.gL..(-n
+#     logger.sL.. ?.I..
+#     fh _ ?.FH.. "ex3_log.log.log"
+#     formatter _ ?.F.. '_ a_t_ s - _ n.. s - _ l.. s - _ m.. s'
+#     ?.sF.. ?
+#     ?.aH.. ?
+#
+#     ?.i.. _*args: ? ? ?
+#
+#     result _ (A - B) * C
+#
+#     ?.i.. _*result: ?
+#
+#     r_ ?
