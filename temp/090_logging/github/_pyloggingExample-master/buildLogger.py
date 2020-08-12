67d69dadf0187a16a88b86b8aff7ff5777fd62ea
Grey <sergejyurskyj@yahoo.com>
2020-08-12 16:49:11 -0700

    Committed logging and path github template

--- temp/090_logging/github/_pyloggingExample-master/buildLogger.py
+++ temp/090_logging/github/_pyloggingExample-master/buildLogger.py
@@ -1,33 +1,33 @@
-______ ?
-______ Module
-
-c_ setLogger():
-    ___  -
-        # create logger with 'spam_application'
-        logger _ ?.gL..('build.log')
-        logger.sL..(?.D..)
-        # create file handler which logs even d.. messages
-        fh _ ?.FH..('TarihliBuild.log')
-        fh.sL..(?.D..)
-        # create console handler with a higher log level
-        ch _ ?.SH..
-        ch.sL..(?.E.)
-        # create formatter and add it to the handlers
-        formatter _ ?.F..('%(a_t_)s - %(name)s - %(l..)s - %(funcName)s: %(m..)s')
-        fh.sF..(formatter)
-        ch.sF..(formatter)
-        # add the handlers to the logger
-        logger.aH..(fh)
-        logger.aH..(ch)
-        logger _ logger
-    ___ gL..
-        r_ logger
-
-__  -n __ '__main__':
-    sLog _ setLogger()
-    logger _ sLog.gL..()
-    
-    logger.i..('Build Started...')
-    a _ gingerModule.Copy().execute()
-    b _ gingerModule.Delete().execute()
-    logger.i..('Finish')
+# ______ ?
+# ______ Module
+#
+# c_ setLogger
+#     ___  -
+#         # create logger with 'spam_application'
+#         logger _ ?.gL.. 'build.log'
+#         ?.sL.. ?.D..
+#         # create file handler which logs even d.. messages
+#         fh _ ?.FH.. 'TarihliBuild.log'
+#         ?.sL.. ?.D..
+#         # create console handler with a higher log level
+#         ch _ ?.SH..
+#         ch.sL.. ?.E.
+#         # create formatter and add it to the handlers
+#         formatter _ ?.F..('%(a_t_)s - %(name)s - %(l..)s - %(funcName)s: %(m..)s')
+#         f_.sF.. ?
+#         c_.sF.. ?
+#         # add the handlers to the logger
+#         ?.aH.. ?
+#         ?.aH.. ?
+#         logger _ l..
+#     ___ gL..
+#         r_ ?
+#
+# __  -n __ _____
+#     sLog _ ?
+#     logger _ ?.gL..
+#
+#     ?.i.. 'Build Started...'
+#     a _ gM...C__ .ex..
+#     b _ gM...D__ .ex..
+#     ?.i.. 'Finish'
