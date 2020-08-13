7957d76f5c288b4c575b8e6b4a30f2e63ae9922d
Grey <sergejyurskyj@yahoo.com>
2020-08-12 18:28:27 -0700

    Committed logging and path github template

--- temp/090_logging/nuke/34-python_in_production-logging_to_a_qt_widget/qt_test_app.py
+++ temp/090_logging/nuke/34-python_in_production-logging_to_a_qt_widget/qt_test_app.py
@@ -1,51 +1,51 @@
-______ ___
-
-____ PySide2 ______ QtWidgets
-
-____ qt_logger ______ QtLogger
-
-
-c_ StandaloneWindow(QtWidgets.QWidget):
-
-    ___  -
-        s___(StandaloneWindow, ). - (parent_None)
-
-        setWindowTitle("Standalone App")
-        setMinimumSize(400, 300)
-
-        plain_text_edit _ QtWidgets.QPlainTextEdit()
-
-        warning_btn _ QtWidgets.QPushButton("Warning")
-        warning_btn.clicked.connect(print_warning)
-
-        error_btn _ QtWidgets.QPushButton("Error")
-        error_btn.clicked.connect(print_error)
-
-        button_layout _ QtWidgets.QHBoxLayout()
-        button_layout.addStretch()
-        button_layout.addWidget(warning_btn)
-        button_layout.addWidget(error_btn)
-
-        main_layout _ QtWidgets.QVBoxLayout()
-        main_layout.addWidget(plain_text_edit)
-        main_layout.addLayout(button_layout)
-
-        QtLogger.signal_handler().emitter.message_logged.connect(plain_text_edit.appendPlainText)
-
-    ___ print_warning
-        QtLogger.w..("warning m..")
-
-    ___ print_error
-        QtLogger.e..("error m..")
-
-
-__  -n __ "__main__":
-    # Create the main Qt application
-    app _ QtWidgets.QApplication(___.argv)
-
-    window _ StandaloneWindow()
-    window.show()
-
-    # Enter Qt main loop (start event handling)
-    app.exec_()
-
+# ______ ___
+#
+# ____ ? ______ ?W..
+#
+# ____ q.. ______ ?L..
+#
+#
+# c_ StandaloneWindow ?W__.?W..
+#
+#     ___  -
+#         s___ ? . - (p_N..
+#
+#         sWT.. "Standalone App"
+#         sMS.. 400, 300
+#
+#         plain_text_edit _ ?W__.?PTE..
+#
+#         warning_btn _ ?W__.?PB__ "Warning"
+#         ?.c__.c.. p_w..
+#
+#         error_btn _ ?W__.?PB__ "Error"
+#         ?.c__.c.. p_e..
+#
+#         button_layout _ ?W__.?HBL..
+#         ?.aS..
+#         ?.aW.. w..
+#         ?.aW.. e..
+#
+#         main_layout _ ?W__.?VBL..
+#         ?.aW.. p..
+#         ?.aL__ b..
+#
+#         ?L__.s_h__ .e__.m_l__.c... p_t__.aPT..
+#
+#     ___ print_warning
+#         ?L__.w.. "warning m.."
+#
+#     ___ print_error
+#         ?L__.e.. "error m.."
+#
+#
+# __  -n __ ____
+#     # Create the main Qt application
+#     app _ ?W__.?A.. ___.a..
+#
+#     window _ ?
+#     ?.s..
+#
+#     # Enter Qt main loop (start event handling)
+#     ?.e..
+#
