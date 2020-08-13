7cc6a6fe8a8d498828143d9c66fe27b3b38aaa07
Grey <sergejyurskyj@yahoo.com>
2020-08-12 18:16:53 -0700

    Committed logging and path github template

--- temp/090_logging/nuke/34-python_in_production-logging_to_a_qt_widget/logger.py
+++ temp/090_logging/nuke/34-python_in_production-logging_to_a_qt_widget/logger.py
@@ -1,112 +1,106 @@
-______ ?
-______ ___
-
-c_ Logger(object):
-
-    LOGGER_NAME _ "Zurbrigg"
-
-    FORMAT_DEFAULT _ "[%(name)s][%(l..)s] %(m..)s"
-
-    LEVEL_DEFAULT _ ?.D..
-    PROPAGATE_DEFAULT _ T..
-
-    _logger_obj _ N..
-
-
-    ??
-    ___ logger_obj(___):
-
-        __ no. ___.?:
-            __ ___.l_e..
-                ___.? _ ?.gL..(___._N..)
-            ____
-                ___.? _ ?.gL..(___._N..)
-
-                ___.?.sL..(___.L..)
-                ___.?.propagate _ ___.P..
-
-                fmt _ ?.F..(___.F..)
-
-                stream_handler _ ?.SH..(___.s_e..)
-                stream_handler.sF..(fmt)
-                ___.?.aH..(stream_handler)
-
-        r_ ___.?
-
-    ??
-    ___ logger_exists(___):
-        r_ ___._N.. __ ?.Logger.m__.lD__.k..
-
-    ??
-    ___ set_level(___, level):
-        lg _ ___.l..
-        lg.sL..(level)
-        
-    ??
-    ___ set_propagate(___, propagate):
-        lg _ ___.l..
-        lg.propagate _ propagate
-
-    ??
-    ___ d..(___, msg, @ @@:
-        lg _ ___.l..
-        lg.d..(msg, @ @@
-
-    ??
-    ___ i..(___, msg, @ @@:
-        lg _ ___.l..
-        lg.i..(msg, @ @@
-
-    ??
-    ___ w..(___, msg, @ @@:
-        lg _ ___.l..
-        lg.w..(msg, @ @@
-
-    ??
-    ___ e..(___, msg, @ @@:
-        lg _ ___.l..
-        lg.e..(msg, @ @@
-
-    ??
-    ___ c..(___, msg, @ @@:
-        lg _ ___.l..
-        lg.c..(msg, @ @@
-
-    ??
-    ___ log(___, level, msg, @ @@:
-        lg _ ___.l..
-        lg.log(level, msg, @ @@
-
-    ??
-    ___ exception(___, msg, @ @@:
-        lg _ ___.l..
-        lg.exception(msg, @ @@
-
-    ??
-    ___ write_to_file(___, path, level_?.WARNING):
-        file_handler _ ?.FH..(path)
-        file_handler.sL..(level)
-
-        fmt _ ?.F..("[%(a_t_)s][%(l..)s] %(m..)s")
-        file_handler.sF..(fmt)
-
-        lg _ ___.l..
-        lg.aH..(file_handler)
-
-
-__  -n __ "__main__":
-    
-    Logger.set_propagate(F..)
-
-    Logger.d..("d.. m..")
-    Logger.i..("i.. m..")
-    Logger.w..("warning m..")
-    Logger.e..("error m..")
-    Logger.c..("c.. m..")
-
-
-
-
-
-
+# ______ ?
+# ______ ___
+#
+# c_ Logger o..
+#
+#     LOGGER_NAME _ "Zurbrigg"
+#
+#     FORMAT_DEFAULT _ " _ n.. s _ l.. s _ m.. s"
+#
+#     LEVEL_DEFAULT _ ?.D..
+#     PROPAGATE_DEFAULT _ T..
+#
+#     _logger_obj _ N..
+#
+#
+#     ??
+#     ___ logger_obj ___
+#
+#         __ no. ___.?
+#             __ ___.l_e..
+#                 ___.? _ ?.gL.. ___._N..
+#             ____
+#                 ___.? _ ?.gL.. ___._N..
+#
+#                 ___.?.sL.. ___.L..
+#                 ___.?.p.. _ ___.P..
+#
+#                 fmt _ ?.F.. ___.F..
+#
+#                 stream_handler _ ?.SH.. ___.s_e..
+#                 ?.sF.. ?
+#                 ___.?.aH.. ?
+#
+#         r_ ___.?
+#
+#     ??
+#     ___ logger_exists ___
+#         r_ ___._N.. __ ?.L___.m__.lD__.k..
+#
+#     ??
+#     ___ set_level ___ level
+#         lg _ ___.l..
+#         ?.sL.. ?
+#
+#     ??
+#     ___ set_propagate ___ propagate
+#         lg _ ___.l..
+#         ?.p.. _ ?
+#
+#     ??
+#     ___ d.. ___ msg @ @@
+#         lg _ ___.l..
+#         ?.d.. ? @ @@
+#
+#     ??
+#     ___ i.. ___ msg @ @@
+#         lg _ ___.l..
+#         lg.i.. ? @ @@
+#
+#     ??
+#     ___ w.. ___ msg @ @@
+#         lg _ ___.l..
+#         ?.w.. ? @ @@
+#
+#     ??
+#     ___ e.. ___ msg @ @@
+#         lg _ ___.l..
+#         ?.e.. ? @ @@
+#
+#     ??
+#     ___ c..___ msg @ @@
+#         lg _ ___.l..
+#         ?.c.. ? @ @@
+#
+#     ??
+#     ___ log ___ level msg @ @@
+#         lg _ ___.l..
+#         ?.l.. ? ? @ @@
+#
+#     ??
+#     ___ exception ___ msg @ @@
+#         lg _ ___.l..
+#         ?.e.. ? @ @@
+#
+#     ??
+#     ___ write_to_file ___ path l.._?.W..
+#         file_handler _ ?.FH.. ?
+#         ?.sL.. ?
+#
+#         fmt _ ?.F.. " _ a_t_ s _ l.. s _ m.. s"
+#         ?.sF.. ?
+#
+#         lg _ ___.l..
+#         ?.aH.. ?
+#
+#
+# __  -n __ _____
+#
+#     ?.s_p.. F..
+#
+#     ?.d.. "d.. m.."
+#     ?.i.. "i.. m.."
+#     ?.w.. "warning m.."
+#     ?.e.. "error m.."
+#     ?.c.. "c.. m.."
 
