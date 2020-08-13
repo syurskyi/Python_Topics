88f765dd53d6a70249a824d3fe536b960d410d14
Grey <sergejyurskyj@yahoo.com>
2020-08-12 17:02:43 -0700

    Committed logging and path github template

--- temp/090_logging/github/_logging_examples-master/py3_logger/src/example_package/example_module.py
+++ temp/090_logging/github/_logging_examples-master/py3_logger/src/example_package/example_module.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
-
-______ ?
-
-___ do_stuff():
-    # While harmless in Python3, avoid initializing the module logger
-    # at import, because this can confuse you __ you import it before
-    # loading the configuration in main.
-    log _ ?.gL..( -n)  # Getting loggers is "cheap".
-    log.i..('Logging from another module.')
+# # -*- coding: utf-8 -*-
+#
+# ______ ?
+#
+# ___ do_stuff
+#     # While harmless in Python3, avoid initializing the module logger
+#     # at import, because this can confuse you __ you import it before
+#     # loading the configuration in main.
+#     log _ ?.gL.. -n  # Getting loggers is "cheap".
+#     ?.i.. 'Logging from another module.'
