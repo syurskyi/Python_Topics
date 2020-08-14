dde451439ba4b6491119e08aee42419d528238de
Grey <sergejyurskyj@yahoo.com>
2020-08-14 12:16:53 -0700

    Committed os_and_sys template

--- temp/095_os_and_sys/github/npath/src/npath/FileObject.py
+++ temp/095_os_and_sys/github/npath/src/npath/FileObject.py
@@ -1,38 +1,37 @@
-____ abc ______ ABCMeta, abstractmethod, abstractproperty
-
-____ . ______ Path
-
-
-c_ FileObject(Path):
-    FILE_OBJ_FACTORY _ N.. # set in __init__.py
-
-
-    ___ __repr__
-        r_ "%s('%s')" % ( -c . -n, __path)
-
-    ??
-    ___ path
-        '''Return path to file object as a Path (not normally needed)'''
-        r_ Path()
-
-
-    @abstractproperty
-    ___ is_file
-        '''Is this a file object'''
-
-
-    @abstractproperty
-    ___ is_dir
-        '''Is this a directory object'''
-
-
-    ??
-    ___ isfile
-        r_ is_file
-
-
-    ??
-    ___ isdir
-        r_ is_dir
-
-
+# ____ a.. ______ A.. a_m.. a_p..
+#
+# ____ . ______ P..
+#
+#
+# c_ FileObject P..
+#     FILE_OBJ_FACTORY _ N.. # set in __init__.py
+#
+#
+#     ___ -r
+#         r_ "@('@')"   -c . -n __pa..
+#
+#     ??
+#     ___ path
+#         '''Return path to file object as a Path (not normally needed)'''
+#         r_ P..
+#
+#     ?a_p..
+#     ___ is_file
+#         '''Is this a file object'''
+#
+#
+#     ?a_p..
+#     ___ is_dir
+#         '''Is this a directory object'''
+#
+#
+#     ??
+#     ___ isfile
+#         r_ ?
+#
+#
+#     ??
+#     ___ isdir
+#         r_ ?
+#
+#
