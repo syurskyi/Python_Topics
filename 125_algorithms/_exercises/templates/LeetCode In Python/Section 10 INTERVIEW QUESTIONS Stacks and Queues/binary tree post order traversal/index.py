368b4a7da29df4b1111575515b2a290a38761780
Grey <sergejyurskyj@yahoo.com>
2020-08-17 11:46:09 -0700

    Committed binary tree post order traversal template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 10 INTERVIEW QUESTIONS Stacks and Queues/binary tree post order traversal/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 10 INTERVIEW QUESTIONS Stacks and Queues/binary tree post order traversal/index.py
@@ -1,33 +1,33 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-c_ Solution:
-    ___ postorderTraversal(, root: TreeNode)  L.. in.:
-        __(no. root
-            r_
-        
-        ans _   # list
-        s1 _   # list
-        s2 _   # list
-
-        s1.ap..(root)
-
-        w___(s1
-            x _ s1[-1]
-            s1.pop()
-            s2.ap..(x)
-
-            __(x.left
-                s1.ap..(x.left)
-            __(x.right
-                s1.ap..(x.right)
-        
-        w___(s2
-            y _ s2[-1]
-            s2.pop()
-            ans.ap..(y.val)
-        r_ ans
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     ___ __init__(self, x
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+#
+# c_ Solution
+#     ___ postorderTraversal root TN..  L.. in.
+#         __ no. root
+#             r_
+#
+#         ans _   # list
+#         s1 _   # list
+#         s2 _   # list
+#
+#         s1.ap.. ?
+#
+#         w___ ?
+#             x _ ? -1
+#             ?.p..
+#             _2.ap.. ?
+#
+#             __ ?.l..
+#                 ?.ap.. ?.l..
+#             __ ?.r..
+#                 ?.ap.. ?.r..
+#
+#         w___ ?
+#             y _ ? -1
+#             ?.p..
+#             ?.ap.. ?.v..
+#         r_ ?
