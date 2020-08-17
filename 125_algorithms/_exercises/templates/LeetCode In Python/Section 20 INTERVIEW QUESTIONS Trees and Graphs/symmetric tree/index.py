20fb41786cf678e59bff333c54ff7f8411f0d86a
Grey <sergejyurskyj@yahoo.com>
2020-08-17 12:20:32 -0700

    Committed symmetric tree template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/symmetric tree/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/symmetric tree/index.py
@@ -1,16 +1,16 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-c_ Solution:
-    ___ isMirror(,t1,t2
-        __(t1 pa__ N.. a.. t2 pa__ N..
-            r_ T..
-        __(t1 pa__ N.. o.. t2 pa__ N..
-            r_ F..
-        r_ (t1.val__t2.val) a.. .isMirror(t1.right,t2.left) a.. .isMirror(t1.left,t2.right)
-    ___ isSymmetric root: TreeNode)  bool:
-        r_ .isMirror(root,root)
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     ___ __init__(self, x
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+#
+# c_ Solution
+#     ___ isMirror t1 t2
+#         __ ? pa__ N.. a.. ? pa__ N..
+#             r_ T..
+#         __ ? pa__ N.. o.. ? pa__ N..
+#             r_ F..
+#         r_ ?.v.. __ ?.v.. a.. .? ?.r.. ?.l.. a.. .? ?.l.. ?.r..
+#     ___ isSymmetric root TN..  bool
+#         r_ .? ? ?
