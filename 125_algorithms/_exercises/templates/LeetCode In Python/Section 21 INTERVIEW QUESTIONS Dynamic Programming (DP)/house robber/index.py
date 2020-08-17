8dbdc59e2ae90965509d0d4240f6bbf94c91a884
Grey <sergejyurskyj@yahoo.com>
2020-08-17 12:10:53 -0700

    Committed house robber template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 21 INTERVIEW QUESTIONS Dynamic Programming (DP)/house robber/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 21 INTERVIEW QUESTIONS Dynamic Programming (DP)/house robber/index.py
@@ -1,18 +1,18 @@
-c_ Solution:
-    ___ rob nums: L.. in.)  in.:
-        n _ le.(nums)
-        __(n__0
-            r_ 0
-        
-        dp _ [0] * n
-        dp[0] _ nums[0]
-
-        ___ i __ ra..(1,n
-            __(i__1
-                dp[i] _ ma.(nums[0],nums[1])
-            ____
-                dp[i] _ ma.(dp[i-1], dp[i-2]+nums[i])
-            
-        r_ dp[-1]
-
-        
+# c_ Solution
+#     ___ rob nums L.. in.  in.
+#         n _ le. ?
+#         __ ? __0
+#             r_ 0
+#
+#         dp _ 0 * ?
+#         ? 0 _ ? 0
+#
+#         ___ i __ ra.. 1 ?
+#             __ ? __1
+#                 ? ? _ ma. ? 0 ? 1
+#             ____
+#                 ? ? _ ma. ? ?-1 ? ?-2 + ? ?
+#
+#         r_ ?-1
+#
+#
--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 21 INTERVIEW QUESTIONS Dynamic Programming (DP)/longest palindromic substring/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 21 INTERVIEW QUESTIONS Dynamic Programming (DP)/longest palindromic substring/index.py
@@ -1,20 +1,20 @@
-c_ Solution:
-    ___ longestPalindrome s: st.)  st.:
-        n _ le.(s)
-        __(n<2
-            r_ s
-        left _ 0
-        right _ 0
-
-        palindrome _ [[0]*n ___ _ __ ra..(n)]
-
-        ___ j __ ra..(1,n
-            ___ i __ ra..(0,j
-                innerIsPalindrome _ palindrome[i+1][j-1] o.. j-i<_2
-                __(s[i] __ s[j] a.. innerIsPalindrome
-                    palindrome[i][j] _ T..
-                    __(j-i>right-left
-                        left _ i
-                        right _ j
-
-        r_ s[left:right+1]
+# c_ Solution
+#     ___ longestPalindrome s st.  st.
+#         n _ le. ?
+#         __ ?<2
+#             r_ ?
+#         left _ 0
+#         right _ 0
+#
+#         palindrome _ 0  ___ _ __ ra.. ?
+#
+#         ___ j __ ra.. 1 ?
+#             ___ i __ ra.. 0 ?
+#                 innerIsPalindrome _ ? ?+1 ?-1 o.. ?-?<_2
+#                 __ ? ? __ ? ? a.. ?
+#                     ? ? ? _ T..
+#                     __ ?-?>?-?
+#                         l.. _ ?
+#                         r.. _ ?
+#
+#         r_ ? ?|?+1
