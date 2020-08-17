32aa1e1add86e3c57e30af7e22dab829570e984c
Grey <sergejyurskyj@yahoo.com>
2020-08-17 13:03:25 -0700

    Committed lowest common ancestor of a binary tree template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/lowest common ancestor of a binary tree/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/lowest common ancestor of a binary tree/index.py
@@ -1,24 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-c_ Solution:
-    ___ lowestCommonAncestor root: 'TreeNode', p: 'TreeNode', q: 'TreeNode')  'TreeNode':
-        __(root pa__ N..
-            r_ N..
-        __(root.val__p.val o.. root.val__q.val
-            r_ root
-        
-        left _ .lowestCommonAncestor(root.left,p,q)
-        right _ .lowestCommonAncestor(root.right,p,q)
-
-        __(left pa__ N.. a.. right pa__ N..
-            r_ N..
-        __(left pa__ no. N.. a.. right pa__ no. N..
-            r_ root
-        __(left pa__ N..
-            r_ right
-        r_ left
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     ___ __init__(self, x
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+#
+# c_ Solution
+#     ___ lowestCommonAncestor root 'TreeNode' p 'TreeNode' q 'TreeNode' 'TreeNode'
+#         __ ? pa__ N..
+#             r_ N..
+#         __ ?.v.. __ ?.v.. o.. ?.v. __ ?.v..
+#             r_ ?
+#
+#         left _ .? ?.l.. ? ?
+#         right _ .? ?.r.. ? ?
+#
+#         __ ? pa__ N.. a.. ? pa__ N..
+#             r_ N..
+#         __ ? pa__ no. N.. a.. ? pa__ no. N..
+#             r_ ?
+#         __ ? pa__ N..
+#             r_ ?
+#         r_ ?
