b3ff8bf5e2c7ed844ba72f06c36ba7c1c7a3aaaa
Grey <sergejyurskyj@yahoo.com>
2020-08-17 12:15:13 -0700

    Committed climbing stairs template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 21 INTERVIEW QUESTIONS Dynamic Programming (DP)/climbing stairs/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 21 INTERVIEW QUESTIONS Dynamic Programming (DP)/climbing stairs/index.py
@@ -1,12 +1,12 @@
-c_ Solution:
-    ___ climbStairs n: in.)  in.:
-        __(n __ 1
-            r_ 1
-        dp _ [0]*(n+1)
-        dp[1] _ 1
-        dp[2] _ 2
-
-        ___ i __ ra..(3, n+1
-            dp[i] _ dp[i-1]+d[i-2]
-
-        r_ dp[n]
+# c_ Solution
+#     ___ climbStairs n in.  in.
+#         __ ? __ 1
+#             r_ 1
+#         dp _  0 * ? + 1
+#         ? 1 _ 1
+#         ? 2 _ 2
+#
+#         ___ i __ ra.. 3 ?+1
+#             ? ? _ ? ?-1 + ? ?-2
+#
+#         r_ ? ?
