39edbaf85a5e74654af3aacc0c1c31bb08fa86da
Grey <sergejyurskyj@yahoo.com>
2020-08-17 13:00:05 -0700

    Committed maximum depth of a binary tree template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/maximum depth of a binary tree/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/maximum depth of a binary tree/index.py
@@ -1,18 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-c_ Solution:
-    ___ maxDepth root: TreeNode)  in.:
-        __(root pa__ N..
-            r_ 0
-        __(root.left pa__ N.. a..  root.right pa__ N..
-            r_ 1
-        
-        left _ .maxDepth(root.left)
-        right _ .maxDepth(root.right)
-
-        r_ ma.(left,right)+1
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     ___ __init__(self, x
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+#
+# c_ Solution
+#     ___ maxDepth root TN..  in.
+#         __ ? pa__ N..
+#             r_ 0
+#         __ ?.l.. pa__ N.. a..  ?.r.. pa__ N..
+#             r_ 1
+#
+#         left _ .? ?.l..
+#         right _ .? ?.r..
+#
+#         r_ ma. ? ? +1
