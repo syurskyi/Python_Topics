67bcaa08f2ffa82672c45ac1b36289e938f24c39
Grey <sergejyurskyj@yahoo.com>
2020-08-17 13:07:44 -0700

    Committed Binary Tree Maximum Path Sum template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/Binary Tree Maximum Path Sum/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/Binary Tree Maximum Path Sum/index.py
@@ -1,24 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-
-c_ Solution:
-    ans _ -fl..("inf")
-    ___ solution(,node
-        __(node pa__ N..
-            r_ 0
-        left _ .solution(node.left)
-        right _ .solution(node.right)
-
-        mxSide _ ma.(node.val,ma.(left,right)+node.val)
-        mxTop _ ma.(mxSide,left+right+node.val)
-        .ans _ ma.(.ans,mxTop)
-        r_ mxSide
-
-    ___ maxPathSum root: TreeNode)  in.:
-        .solution(root)
-        r_ .ans
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     ___ __init__(self, x
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+#
+#
+# c_ Solution
+#     ans _ -fl.. "inf"
+#     ___ solution node
+#         __ ? pa__ N..
+#             r_ 0
+#         left _ .s.. ?.l..
+#         right _ .s.. ?.r..
+#
+#         mxSide _ ma. ?.v.. ma. l.. r.. + ?.v..
+#         mxTop _ ma. ? l.. + r.. + ?.v..
+#         .ans _ ma. .a.. ?
+#         r_ ?
+#
+#     ___ maxPathSum root TN..  in.
+#         .s.. ?
+#         r_ ?
