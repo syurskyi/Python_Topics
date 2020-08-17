5b800d533961c1c8bc9e5c569326ed09e58ce4fa
Grey <sergejyurskyj@yahoo.com>
2020-08-17 12:58:41 -0700

    Committed path sum template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/path sum/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 20 INTERVIEW QUESTIONS Trees and Graphs/path sum/index.py
@@ -1,17 +1,17 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-c_ Solution:
-    ___ hasSum(,root,su.,cur
-        __(root pa__ N..
-            r_ F..
-        cur+_root.val
-        __(cur__sum a.. root.left pa__ N.. a.. root.right pa__ N..
-            r_ T..
-        r_ (.hasSum(root.right,su.,cur) o.. .hasSum(root.left,su.,cur))
-    ___ hasPathSum root: TreeNode, su.: in.)  bool:
-        r_ .hasSum(root,su., 0)
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     ___ __init__(self, x
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+#
+# c_ Solution
+#     ___ hasSum root su. cur
+#         __ ? pa__ N..
+#             r_ F..
+#         ?+_ ?.v..
+#         __ ? __ sum a.. ?.l.. pa__ N.. a.. ?.r.. pa__ N..
+#             r_ T..
+#         r_  .? ?.r.. su. ? o.. .? ?.l.. su. ?
+#     ___ hasPathSum ? TN.. su. in.  bo..
+#         r_ .? ? su. 0
