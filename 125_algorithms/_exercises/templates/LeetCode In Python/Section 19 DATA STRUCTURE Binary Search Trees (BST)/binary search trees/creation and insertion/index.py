3f7d3564eb6bef763b37e185d92f5a81dbc70c50
Grey <sergejyurskyj@yahoo.com>
2020-08-18 09:31:48 -0700

    Committed binary search trees creation and insertion template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 19 DATA STRUCTURE Binary Search Trees (BST)/binary search trees/creation and insertion/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 19 DATA STRUCTURE Binary Search Trees (BST)/binary search trees/creation and insertion/index.py
@@ -1,89 +1,89 @@
-
-
-c_ Node:
-    ___  -  value
-        .left _ N..
-        .right _ N..
-        .data _ value
-
-
-___ insert(root, node
-    __(root pa__ N..
-        root _ node
-        r_
-
-    __(root.data < node.data
-        __(root.right pa__ N..
-            root.right _ node
-        ____
-            insert(root.right, node)
-    ____
-        __(root.left pa__ N..
-            root.left _ node
-        ____
-            insert(root.left, node)
-
-
-___ preorder(node
-    __(node pa__ no. N..
-        print(node.data)
-        preorder(node.left)
-        preorder(node.right)
-
-
-#	         5
-#       /  	      \
-#     3	            7
-#   /   \        /     \
-#  2     4      6        8
-tree _ Node(5)
-
-#	         5
-#       /  	      \
-#     None	       None
-
-insert(tree, Node(3))
-
-#	         5
-#       /  	      \
-#     3	            None
-
-insert(tree, Node(2))
-
-#	         5
-#       /  	      \
-#     3	            None
-#   /
-#  2
-insert(tree, Node(4))
-
-#	         5
-#       /  	      \
-#     3	            None
-#   /   \
-#  2     4
-insert(tree, Node(7))
-
-#	         5
-#       /  	      \
-#     3	            7
-#   /   \
-#  2     4
-insert(tree, Node(6))
-
-#	         5
-#       /  	      \
-#     3	            7
-#   /   \        /
-#  2     4      6
-insert(tree, Node(8))
-
-#	         5
-#       /  	      \
-#     3	            7
-#   /   \        /     \
-#  2     4      6        8
-
-
-# 5 3 2 4 7 6 8
-preorder(tree)
+#
+#
+# c_ Node
+#     ___  -  value
+#         .left _ N..
+#         .right _ N..
+#         .data _ ?
+#
+#
+# ___ insert root node
+#     __ ? pa__ N..
+#         ? _ ?
+#         r_
+#
+#     __ ?.d.. < ?.d..
+#         __ ?.r.. pa__ N..
+#             ?.r.. _ ?
+#         ____
+#             ? ?.r.. ?
+#     ____
+#         __ ?.l.. pa__ N..
+#             ?.l.. _ ?
+#         ____
+#             ? ?.l.. ?
+#
+#
+# ___ preorder node
+#     __ ? pa__ no. N..
+#         print ?.d..
+#         ? ?.l..
+#         ? ?.r..
+#
+#
+# #	         5
+# #       /  	      \
+# #     3	            7
+# #   /   \        /     \
+# #  2     4      6        8
+# tree _ Node(5)
+#
+# #	         5
+# #       /  	      \
+# #     None	       None
+#
+# insert(tree, Node(3))
+#
+# #	         5
+# #       /  	      \
+# #     3	            None
+#
+# insert(tree, Node(2))
+#
+# #	         5
+# #       /  	      \
+# #     3	            None
+# #   /
+# #  2
+# insert(tree, Node(4))
+#
+# #	         5
+# #       /  	      \
+# #     3	            None
+# #   /   \
+# #  2     4
+# insert(tree, Node(7))
+#
+# #	         5
+# #       /  	      \
+# #     3	            7
+# #   /   \
+# #  2     4
+# insert(tree, Node(6))
+#
+# #	         5
+# #       /  	      \
+# #     3	            7
+# #   /   \        /
+# #  2     4      6
+# insert(tree, Node(8))
+#
+# #	         5
+# #       /  	      \
+# #     3	            7
+# #   /   \        /     \
+# #  2     4      6        8
+#
+#
+# # 5 3 2 4 7 6 8
+# preorder(tree)
