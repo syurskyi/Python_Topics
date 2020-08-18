033b474f3faa6feb1b81e063a47a340a547324af
Grey <sergejyurskyj@yahoo.com>
2020-08-18 09:52:33 -0700

    Committed Section 18 DATA STRUCTURE Trees postorder template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 18 DATA STRUCTURE Trees/binary trees/traversal/postorder/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 18 DATA STRUCTURE Trees/binary trees/traversal/postorder/index.py
@@ -1,57 +1,57 @@
-
-
-c_ Node:
-    ___  -  value
-        .left _ N..
-        .right _ N..
-        .data _ value
-
-
-___ postorder(node
-    __(node pa__ no. N..
-        postorder(node.left)
-        postorder(node.right)
-        print(node.data)
-
-
-# create root
-root _ Node(4)
-''' following is the tree after above statement 
-	    4 
-	  /   \ 
-	None  None
-'''
-
-root.left _ Node(5)
-root.right _ Node(6)
-
-''' 5 and 6 become left and right children of 1 
-		           4 
-		       /       \ 
-		      5	        6 
-	      /  \      /   \ 
-      None None  None  None
-'''
-
-
-root.left.left _ Node(7)
-'''
-7 becomes left child of 5
-		           4 
-		       /       \ 
-		      5	        6 
-	      /  \      /   \ 
-       7   None  None  None
-      / \
-  None   None
-
-'''
-postorder(root)
-
-#              4
-#          /       \
-#         5         6
-#       /  \      /   \
-#      7   None  None  None
-#     / \
-# None   None
+#
+#
+# c_ Node
+#     ___  -  value
+#         .left _ N..
+#         .right _ N..
+#         .data _ ?
+#
+#
+# ___ postorder node
+#     __ ? pa__ no. N..
+#         ? ?.l..
+#         ? ?.r..
+#         print(?.d..
+#
+#
+# # create root
+# root _ N.. 4
+# ''' following is the tree after above statement
+# 	    4
+# 	  /   \
+# 	None  None
+# '''
+#
+# ?.left _ ? 5
+# ?.right _ ? 6
+#
+# ''' 5 and 6 become left and right children of 1
+# 		           4
+# 		       /       \
+# 		      5	        6
+# 	      /  \      /   \
+#       None None  None  None
+# '''
+#
+#
+# ?.l__.l.. _ ? 7
+# '''
+# 7 becomes left child of 5
+# 		           4
+# 		       /       \
+# 		      5	        6
+# 	      /  \      /   \
+#        7   None  None  None
+#       / \
+#   None   None
+#
+# '''
+# postorder ?
+#
+# #              4
+# #          /       \
+# #         5         6
+# #       /  \      /   \
+# #      7   None  None  None
+# #     / \
+# # None   None
