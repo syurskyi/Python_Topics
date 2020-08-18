294c2ab03871cbe9f210b7291304d452f1904c57
Grey <sergejyurskyj@yahoo.com>
2020-08-18 15:56:34 -0700

    Committed Section 18 DATA STRUCTURE Trees preorder template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 18 DATA STRUCTURE Trees/binary trees/traversal/preorder/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 18 DATA STRUCTURE Trees/binary trees/traversal/preorder/index.py
@@ -1,57 +1,57 @@
-
-
-c_ Node:
-    ___  -  value
-        .left _ N..
-        .right _ N..
-        .data _ value
-
-
-___ preorder(node
-    __(node pa__ no. N..
-        print(node.data)
-        preorder(node.left)
-        preorder(node.right)
-
-
-# create root
-root _ Node(4)
-''' following is the tree after above statement 
-	    4 
-	  /   \ 
-	None  None
-'''
-
-root.left _ Node(5)
-root.right _ Node(6)
-
-''' 5 and 6 become left and right children of 1 
-		           4 
-		       /       \ 
-		      5	        6 
-	      /  \      /   \ 
-      None None  None  None
-'''
-
-
-root.left.left _ Node(7)
-'''
-7 becomes left child of 5
-		           4 
-		       /       \ 
-		      5	        6 
-	      /  \      /   \ 
-       7   None  None  None
-      / \
-  None   None
-
-'''
-preorder(root)
-
-#              4
-#          /       \
-#         5         6
-#       /  \      /   \
-#      7   None  None  None
-#     / \
-# None   None
+#
+#
+# c_ Node
+#     ___  -  value
+#         .left _ N..
+#         .right _ N..
+#         .data _ ?
+#
+#
+# ___ preorder node
+#     __ ? pa__ no. N..
+#         print ?.d..
+#         ? ?.l..
+#         ? ?.r..
+#
+#
+# # create root
+# root _ ? 4
+# ''' following is the tree after above statement
+# 	    4
+# 	  /   \
+# 	None  None
+# '''
+#
+# ?.l.. _ ? 5
+# ?.r.. _ ? 6
+#
+# ''' 5 and 6 become left and right children of 1
+# 		           4
+# 		       /       \
+# 		      5	        6
+# 	      /  \      /   \
+#       None None  None  None
+# '''
+#
+#
+# ?.l__.l.. _ ?(7)
+# '''
+# 7 becomes left child of 5
+# 		           4
+# 		       /       \
+# 		      5	        6
+# 	      /  \      /   \
+#        7   None  None  None
+#       / \
+#   None   None
+#
+# '''
+# ? ?
+#
+# #              4
+# #          /       \
+# #         5         6
+# #       /  \      /   \
+# #      7   None  None  None
+# #     / \
+# # None   None
