e2c8fdc5fd41fcd070893fce7e6a63107edc4231
Grey <sergejyurskyj@yahoo.com>
2020-08-18 09:22:42 -0700

    Committed search template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 19 DATA STRUCTURE Binary Search Trees (BST)/binary search trees/search/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 19 DATA STRUCTURE Binary Search Trees (BST)/binary search trees/search/index.py
@@ -1,62 +1,62 @@
-
-
-c_ Node:
-    ___  -  value
-        .left _ N..
-        .right _ N..
-        .data _ value
-
-
-___ insert(root, node
-    __(root pa__ N..
-        root _ node
-        r_
-
-    __(root.data < node.data
-        __(root.right pa__ N..
-            root.right _ node
-        ____
-            insert(root.right, node)
-    ____
-        __(root.left pa__ N..
-            root.left _ node
-        ____
-            insert(root.left, node)
-
-
-___ search(node, key
-    print("Current Node is: ", node.data)
-    __(node pa__ N..
-        print("No node found")
-        r_ N..
-    __(node.data __ key
-        print("Node found !")
-        r_ node
-
-    __(node.data < key
-        r_ search(node.right, key)
-
-    r_ search(node.left, key)
-
-
-#	           5
-#       /  	      \
-#     3	            7
-#   /   \        /     \
-#  2     4      6        8
-tree _ Node(5)
-
-insert(tree, Node(3))
-
-
-insert(tree, Node(2))
-
-insert(tree, Node(4))
-
-insert(tree, Node(7))
-
-insert(tree, Node(6))
-
-insert(tree, Node(8))
-
-search(tree, 8)
+#
+#
+# c_ Node
+#     ___  -  value
+#         .left _ N..
+#         .right _ N..
+#         .data _ ?
+#
+#
+# ___ insert root node
+#     __ ? pa__ N..
+#         root _ ?
+#         r_
+#
+#     __ ?.d.. < ?.d..
+#         __ ?.r.. pa__ N..
+#             ?.r.. _ ?
+#         ____
+#             i.. ?.r.. ?
+#     ____
+#         __ ?.l.. pa__ N..
+#             ?.l.. _ ?
+#         ____
+#             i.. ?.l.. ?
+#
+#
+# ___ search node key
+#     print("Current Node is: " ?.d..
+#     __ ? pa__ N..
+#         print("No node found")
+#         r_ N..
+#     __ ?.d.. __ ?
+#         print("Node found !")
+#         r_ ?
+#
+#     __ ?.d.. < ?
+#         r_ ? ?.r.. ?
+#
+#     r_ ? ?.l.. ?
+#
+#
+# #	           5
+# #       /  	      \
+# #     3	            7
+# #   /   \        /     \
+# #  2     4      6        8
+# tree _ Node(5)
+#
+# insert(tree, Node(3))
+#
+#
+# insert(tree, Node(2))
+#
+# insert(tree, Node(4))
+#
+# insert(tree, Node(7))
+#
+# insert(tree, Node(6))
+#
+# insert(tree, Node(8))
+#
+# search(tree, 8)
