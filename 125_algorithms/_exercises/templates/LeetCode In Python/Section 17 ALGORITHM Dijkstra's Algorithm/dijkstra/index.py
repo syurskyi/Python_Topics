2bba576bd88a2c81385acdb9b123c8455c960028
Grey <sergejyurskyj@yahoo.com>
2020-08-18 16:00:56 -0700

    Committed Section 17 ALGORITHM Dijkstra's Algorithm template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 17 ALGORITHM Dijkstra's Algorithm/dijkstra/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 17 ALGORITHM Dijkstra's Algorithm/dijkstra/index.py
@@ -1,27 +1,27 @@
-# network delay time - leetcode
-c_ Solution:
-    ___ networkDelayTime times: L..[L..[in.]], N: in., K: in.)  in.:
-
-        g _ co...d_d_(list)
-        ___ u, v, cost __ times:
-            g[u].ap..((cost, v))
-
-        # cost,node
-        min_heap _ [(0, K)]
-        visited _ set()
-        distance _ {i: fl..('inf') ___ i __ ra..(1, N+1)}
-        distance[K] _ 0
-
-        w___ min_heap:
-            cur_dist, u _ heapq.heappop(min_heap)
-            __ u __ visited:
-                continue
-            visited.add(u)
-            __ le.(visited) __ N:
-                r_ cur_dist
-
-            ___ direct_distance, v __ g[u]:
-                __ cur_dist + direct_distance < distance[v] a.. v no. __ visited:
-                    distance[v] _ cur_dist + direct_distance
-                    heapq.heappush(min_heap, (cur_dist + direct_distance, v))
-        r_ -1
+# # network delay time - leetcode
+# c_ Solution
+#     ___ networkDelayTime times L.. L.. in. N in. K in.  in.
+#
+#         g _ co...d_d_ list
+#         ___ u, v, cost __ ?
+#             ? u .ap.. c.. v
+#
+#         # cost,node
+#         min_heap _ 0, K
+#         visited _ se.
+#         distance _ i fl.. 'inf' ___ ? __ ra.. 1 ?+1
+#         ? K _ 0
+#
+#         w___ m..
+#             c.. u _ h__.h_p.. ?
+#             __ u __ v..
+#                 c..
+#             v__.a.. u
+#             __ le. v.. __ N
+#                 r_ c..
+#
+#             ___ d_d.. v __ g u
+#                 __ c.. + d_d.. < d.. v a.. v no. __ v..
+#                     d.. v _ c.. + d_d..
+#                     h__.h_p.. m.. c.. + d_d.., v
+#         r_ -1
