f35ac3919126e6704ef18ba786f6ade5a4a37a79
Grey <sergejyurskyj@yahoo.com>
2020-08-18 09:28:32 -0700

    Committed binary search trees detection template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 19 DATA STRUCTURE Binary Search Trees (BST)/binary search trees/deletion/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 19 DATA STRUCTURE Binary Search Trees (BST)/binary search trees/deletion/index.py
@@ -1,105 +1,105 @@
-
-
-c_ Node:
-    ___  -  value
-        .left _ N..
-        .right _ N..
-        .data _ value
-
-
-___ insert(root, node
-    __(root pa__ N..
-        root _ node
-        r_
-
-    __(root.data < node.data
-        __(root.right pa__ N..
-            root.right _ node
-        ____
-            insert(root.right, node)
-    ____
-        __(root.left pa__ N..
-            root.left _ node
-        ____
-            insert(root.left, node)
-
-
-___ preorder(node
-    __(node pa__ no. N..
-        print(node.data)
-        preorder(node.left)
-        preorder(node.right)
-
-
-___ minValueNode(node
-    w___(node.left pa__ no. N..
-        node _ node.left
-    r_ node
-
-
-___ deleteNode(node, key
-    __(node pa__ N..
-        r_ node
-    # If the key to be deleted is smaller than the node's
-    # key then it lies in  left subtree
-    __ key < node.data:
-        node.left _ deleteNode(node.left, key)
-    # If the kye to be delete is greater than the node's key
-    # then it lies in right subtree
-    ____(key > node.data
-        node.right _ deleteNode(node.right, key)
-    # If key is same as node's key, then this is the node
-    # to be deleted
-    ____
-        # Node with only one child or no child
-        __ node.left pa__ N..
-            temp _ node.right
-            node _ N..
-            r_ temp
-        ____ node.right pa__ N..
-            temp _ node.left
-            node _ N..
-            r_ temp
-
-        # Node with two children: Get the inorder successor
-        # (smallest in the right subtree)
-        temp _ minValueNode(node.right)
-        # Copy the inorder successor's content to this node
-        node.data _ temp.data
-        # Delete the inorder successor
-        node.right _ deleteNode(node.right, temp.data)
-
-    r_ node
-
-
-#	           5
-#       /  	      \
-#     3	            7
-#   /   \        /     \
-#  2     4      6        8
-tree _ Node(5)
-
-insert(tree, Node(3))
-
-
-insert(tree, Node(2))
-
-insert(tree, Node(4))
-
-insert(tree, Node(7))
-
-insert(tree, Node(6))
-
-insert(tree, Node(8))
-
-deleteNode(tree, 7)
-
-#	           5
-#       /  	      \
-#     3	            8
-#   /   \        /     \
-#  2     4      6       None
-
-
-# 5 3 2 4 8 6
-preorder(tree)
+#
+#
+# c_ Node
+#     ___  -  value
+#         .left _ N..
+#         .right _ N..
+#         .data _ ?
+#
+#
+# ___ insert root node
+#     __ ? pa__ N..
+#         ? _ ?
+#         r_
+#
+#     __ ?.d.. < ?.d..
+#         __ ?.r.. pa__ N..
+#             ?.r.. _ ?
+#         ____
+#             i.. ?.r.. ?
+#     ____
+#         __ ?.l.. pa__ N..
+#             ?.l.. _ ?
+#         ____
+#             i.. ?.l.. ?
+#
+#
+# ___ preorder node
+#     __ ? pa__ no. N..
+#         print ?.d..
+#         p.. ?.l..
+#         p.. ?.r..
+#
+#
+# ___ minValueNode node
+#     w___?.l.. pa__ no. N..
+#         node _ ?.l..
+#     r_ ?
+#
+#
+# ___ deleteNode node key
+#     __ ? pa__ N..
+#         r_ ?
+#     # If the key to be deleted is smaller than the node's
+#     # key then it lies in  left subtree
+#     __ ? < ?.d..
+#         ?.l.. _ d.. ?.l.. ?
+#     # If the kye to be delete is greater than the node's key
+#     # then it lies in right subtree
+#     ____ ? > ?.d..
+#         ?.r.. _ d.. ?.r.. ?
+#     # If key is same as node's key, then this is the node
+#     # to be deleted
+#     ____
+#         # Node with only one child or no child
+#         __ ?.l.. pa__ N..
+#             temp _ ?.r..
+#             ? _ N..
+#             r_ ?
+#         ____ ?.r.. pa__ N..
+#             temp _ ?.l..
+#             ? _ N..
+#             r_ ?
+#
+#         # Node with two children: Get the inorder successor
+#         # (smallest in the right subtree)
+#         temp _ m.. ?.r..
+#         # Copy the inorder successor's content to this node
+#         ?.d.. _ ?.d..
+#         # Delete the inorder successor
+#         ?.r.. _ d.. ?.r.. ?.d..
+#
+#     r_ ?
+#
+#
+# #	           5
+# #       /  	      \
+# #     3	            7
+# #   /   \        /     \
+# #  2     4      6        8
+# tree _ Node(5)
+#
+# insert(tree, Node(3))
+#
+#
+# insert(tree, Node(2))
+#
+# insert(tree, Node(4))
+#
+# insert(tree, Node(7))
+#
+# insert(tree, Node(6))
+#
+# insert(tree, Node(8))
+#
+# deleteNode(tree, 7)
+#
+# #	           5
+# #       /  	      \
+# #     3	            8
+# #   /   \        /     \
+# #  2     4      6       None
+#
+#
+# # 5 3 2 4 8 6
+# preorder(tree)
