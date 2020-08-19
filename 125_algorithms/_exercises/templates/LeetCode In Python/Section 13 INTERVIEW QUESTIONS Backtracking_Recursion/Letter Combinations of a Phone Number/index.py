96e94334bcba56b87602efcf50429764cd9d3cf2
Grey <sergejyurskyj@yahoo.com>
2020-08-19 10:22:14 -0700

    Committed Section 13 INTERVIEW QUESTIONS Backtracking_Recursion template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 13 INTERVIEW QUESTIONS Backtracking_Recursion/Letter Combinations of a Phone Number/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 13 INTERVIEW QUESTIONS Backtracking_Recursion/Letter Combinations of a Phone Number/index.py
@@ -1,35 +1,35 @@
-c_ Solution:
-
-    ___ backtracking ans, m, digits, combination, index
-        __(index > le.(digits)):
-            r_
-        __(le.(combination) __ le.(digits)):
-            ans.ap..(combination[:])
-            r_
-
-        currentDigit _ digits[index]
-        curString _ m[currentDigit]
-
-        ___ i __ ra..(le.(curString)):
-            .backtracking(ans, m, digits, combination +
-                              curString[i], index+1)
-
-    ___ letterCombinations digits: st.)  L..[st.]:
-        ans _   # list
-        __(le.(digits) __ 0
-            r_ ans
-
-        m _ {}
-
-        m['2'] _ "abc"
-        m['3'] _ "___"
-        m['4'] _ "ghi"
-        m['5'] _ "jkl"
-        m['6'] _ "mno"
-        m['7'] _ "pqrs"
-        m['8'] _ "tuv"
-        m['9'] _ "wxyz"
-
-        .backtracking(ans, m, digits, "", 0)
-
-        r_ ans
+# c_ Solution
+#
+#     ___ backtracking ans m digits combination index
+#         __ i.. > le. d..
+#             r_
+#         __ le. c.. __ le. d..
+#             a__.ap.. c.. |
+#             r_
+#
+#         currentDigit _ d.. i..
+#         curString _ m c..
+#
+#         ___ i __ ra.. le. curString
+#             .b.. ? ? ? ? +
+#                               c.. ? i.. + 1
+#
+#     ___ letterCombinations digits st.  L.. st.
+#         ans _   # list
+#         __ le. ? __ 0
+#             r_ ?
+#
+#         m _   # dict
+#
+#         m['2'] _ "abc"
+#         m['3'] _ "___"
+#         m['4'] _ "ghi"
+#         m['5'] _ "jkl"
+#         m['6'] _ "mno"
+#         m['7'] _ "pqrs"
+#         m['8'] _ "tuv"
+#         m['9'] _ "wxyz"
+#
+#         .b.. ? ? ? "" 0
+#
+#         r_ ?
--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 13 INTERVIEW QUESTIONS Backtracking_Recursion/palindrome partitioning/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 13 INTERVIEW QUESTIONS Backtracking_Recursion/palindrome partitioning/index.py
@@ -1,28 +1,28 @@
-c_ Solution:
-    ___ isPalin seg
-        i _ 0
-        j _ le.(seg)-1
-        w___(i < j
-            __(seg[i] !_ seg[j]
-                r_ F..
-            i +_ 1
-            j -_ 1
-        r_ T..
-
-    ___ dfs s: st.
-        __(le.(s) __ 0 a.. le.(.temp) > 0
-            .res.ap..(.temp[:])
-            r_
-        n _ le.(s)+1
-        ___ i __ ra..(1, n
-            seg _ s[0:i]
-            __(.isPalin(seg)):
-                .temp.ap..(seg)
-                .dfs(s[i:])
-                .temp.p..
-
-    ___ partition s: st.)  L..[L..[st.]]:
-        .res _   # list
-        .temp _   # list
-        .dfs(s)
-        r_ .res
+# c_ Solution
+#     ___ isPalin seg
+#         i _ 0
+#         j _ le. ? - 1
+#         w___ ? < ?
+#             __ ? ? !_ ? ?
+#                 r_ F..
+#             ? +_ 1
+#             ? -_ 1
+#         r_ T..
+#
+#     ___ dfs s st.
+#         __ le. ? __ 0 a.. le. .t.. > 0
+#             .r__.ap.. .t.. |
+#             r_
+#         n _ le. ? + 1
+#         ___ i __ ra.. 1 ?
+#             seg _ ? 0 | ?
+#             __ .i.. ?
+#                 .t__.ap.. ?
+#                 .d.. ? ?|
+#                 .t__.p..
+#
+#     ___ partition s st.  L.. L.. st.
+#         .r.. _   # list
+#         .t.. _   # list
+#         .d.. ?
+#         r_ .r..
