f39d187f02512c06a50b21750f0287b4569f14c5
Grey <sergejyurskyj@yahoo.com>
2020-08-19 10:12:37 -0700

    Committed Section 13 INTERVIEW QUESTIONS Backtracking_Recursion template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 13 INTERVIEW QUESTIONS Backtracking_Recursion/word search/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 13 INTERVIEW QUESTIONS Backtracking_Recursion/word search/index.py
@@ -1,36 +1,36 @@
-c_ Solution:
-    dx _ [0, 0, -1, 1]
-    dy _ [1, -1, 0, 0]
-
-    ___ solution board, word, x, y, cur
-        __(x < 0 o.. x >_ le.(board) o.. y < 0 o.. y >_ le.(board[x]) o.. board[x][y] __ ' '
-            r_ F..
-        cur +_ board[x][y]
-
-        __(le.(cur) > le.(word)):
-            r_ F..
-        __(cur[le.(cur)-1] !_ word[le.(cur)-1]
-            r_ F..
-        __(cur __ word
-            r_ T..
-
-        temp _ board[x][y]
-        board[x][y] _ ' '
-
-        ___ i __ ra..(4
-            __(.solution(board, word, x+.dx[i], y+.dy[i], cur)):
-                r_ T..
-
-        board[x][y] _ temp
-        r_ F..
-
-    ___ exist board: L..[L..[st.]], word: st.)  bool:
-        __(le.(word) __ 0
-            r_ T..
-        n _ le.(board)
-        ___ i __ ra..(n
-            m _ le.(board[i])
-            ___ j __ ra..(m
-                __(word[0] __ board[i][j] a.. .solution(board, word, i, j, "")):
-                    r_ T..
-        r_ F..
+# c_ Solution
+#     dx _ 0, 0, -1, 1
+#     dy _ 1, -1, 0, 0
+#
+#     ___ solution board word x y cur
+#         __ ? < 0 o.. ? >_ le. b.. o.. ? < 0 o.. ? >_ le. b.. ? o.. b.. ? ? __ ' '
+#             r_ F..
+#         cur +_ b.. ? ?
+#
+#         __ le. c.. > le. w..
+#             r_ F..
+#         __ c.. le. c.. -1 !_ w.. le. c.. - 1
+#             r_ F..
+#         __ c.. __ w..
+#             r_ T..
+#
+#         temp _ b.. ? ?
+#         b.. ? ? _ ' '
+#
+#         ___ i __ ra.. 4
+#             __ .s.. b.. w.. ? + .dx ? ? + .dy ? c..
+#                 r_ T..
+#
+#         b.. ? ? _ t..
+#         r_ F..
+#
+#     ___ exist b.. L.. L.. st. word st.  bo..
+#         __ le. w.. __ 0
+#             r_ T..
+#         n _ le. b..
+#         ___ i __ ra.. n
+#             m _ le. b.. ?
+#             ___ j __ ra.. ?
+#                 __ w.. 0 __ b.. ? ? a.. .s.. b.. w.. ? ? ""
+#                     r_ T..
+#         r_ F..
