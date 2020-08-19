45c8238a7d23593bb69e9c2bad881f7826afa892
Grey <sergejyurskyj@yahoo.com>
2020-08-19 10:28:49 -0700

    Committed Section 13 INTERVIEW QUESTIONS Backtracking_Recursion template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 12 INTERVIEW QUESTIONS Linked Lists/remove nth node from end of list/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 12 INTERVIEW QUESTIONS Linked Lists/remove nth node from end of list/index.py
@@ -1,25 +1,25 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.next = None
-
-c_ Solution:
-    ___ removeNthFromEnd head: ListNode, n: in.)  ListNode:
-        ans _ ListNode(0)
-        ans.n.. _ head
-
-        first _ ans
-        second _ ans
-
-        ___ i __ ra..(1,n+2
-            first _ first.n..
-        
-        w___ (first pa__ no. N..
-            first _ first.n..
-            second _ second.n..
-
-        second.n.. _ second.n...n..
-        
-
-        r_ ans.n..
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     ___ __init__(self, x
+# #         self.val = x
+# #         self.next = None
+#
+# c_ Solution
+#     ___ removeNthFromEnd head LN.. n in.  LN..
+#         ans _ LN.. 0
+#         ?.n.. _ h..
+#
+#         first _ a..
+#         second _ a..
+#
+#         ___ i __ ra.. 1 ? + 2
+#             first _ f__.n..
+#
+#         w___ first pa__ no. N..
+#             f.. _ f__.n..
+#             second _ s__.n..
+#
+#         s__.n.. _ s__.n...n..
+#
+#
+#         r_ a__.n..
--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 12 INTERVIEW QUESTIONS Linked Lists/reverse linked list/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 12 INTERVIEW QUESTIONS Linked Lists/reverse linked list/index.py
@@ -1,16 +1,16 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.next = None
-
-
-c_ Solution:
-    ___ reverseList head: ListNode)  ListNode:
-        node _ N..
-        w___(head pa__ no. N..
-            n.. _ head.n..
-            head.n.. _ node
-            node _ head
-            head _ n..
-        r_ node
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     ___ __init__(self, x
+# #         self.val = x
+# #         self.next = None
+#
+#
+# c_ Solution
+#     ___ reverseList head LN..  LN..
+#         node _ N..
+#         w___ ? pa__ no. N..
+#             n.. _ h__.n..
+#             ?.n.. _ n...
+#             n.. _ h..
+#             h.. _ n..
+#         r_ ?
