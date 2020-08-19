5f9807185d074bb86a74ac7ca0eacc1255dd6b79
Grey <sergejyurskyj@yahoo.com>
2020-08-19 10:32:52 -0700

    Committed Section 12 INTERVIEW QUESTIONS Linked Lists template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 12 INTERVIEW QUESTIONS Linked Lists/merge 2 sorted lists/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 12 INTERVIEW QUESTIONS Linked Lists/merge 2 sorted lists/index.py
@@ -1,30 +1,30 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.next = None
-
-c_ Solution:
-    ___ mergeTwoLists l1: ListNode, l2: ListNode)  ListNode:
-		cur _ ListNode(0)
-		ans _ cur
-		
-		w___(l1 a.. l2
-			__(l1.val>l2.val
-				cur.n.. _ l2
-				l2 _ l2.n..
-			____
-				cur.n.. _ l1
-				l1 _ l1.n..
-			cur _ cur.n..
-			
-		w___(l1
-			cur.n.. _ l1
-			l1 _ l1.n..
-			cur _ cur.n..
-		
-		w___(l2
-			cur.n.. _ l2
-			l2 _ l2.n..
-			cur _ cur.n..
-		r_ ans.n..
+# # Definition for singly-linked list.
+# # # class ListNode:
+# # #     ___ __init__(self, x
+# # #         self.val = x
+# # #         self.next = None
+# #
+# # c_ Solution
+# #     ___ mergeTwoLists l1 LN.. l2 LN..  LN..
+# # 		cur _ LN.. 0
+# # 		ans _ ?
+# #
+# # 		w___ ? a.. ?
+# # 			__ ?.v.. > ?.v..
+# # 				c__.n.. _ ?
+# # 				? _ ?.n..
+# # 			____
+# # 				c__.n.. _ ?
+# # 				? _ ?.n..
+# # 			cur _ c__.n..
+# #
+# # 		w___ ?
+# # 			c__.n.. _ ?
+# # 			? _ ?.n..
+# # 			c__ _ c__.n..
+# #
+# # 		w___ ?
+# # 			c__.n.. _ ?
+# # 			? _ ?.n..
+# # 			c__ _ c__.n..
+# # 		r_ a__.n..
