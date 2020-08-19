c103f2b22a6b0da7ccf706577a1bf9312b310eda
Grey <sergejyurskyj@yahoo.com>
2020-08-19 09:18:15 -0700

    Committed Section 14 DATA STRUCTURE Graphs template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 14 DATA STRUCTURE Graphs/graphs/graphs creation/undirected graph/adjacency matrix/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 14 DATA STRUCTURE Graphs/graphs/graphs creation/undirected graph/adjacency matrix/index.py
@@ -1,31 +1,31 @@
-____ co.. ______ d_d_
-
-
-c_ Graph:
-    ___  -  numberOfNodes
-        .numberOfNodes _ numberOfNodes+1
-        .graph _ [[0 ___ x __ ra..(numberOfNodes+1)]
-                      ___ y __ ra..(numberOfNodes+1)]
-
-    ___ withInBounds v1, v2
-        r_ (v1 >_ 0 a.. v1 <_ .numberOfNodes) a.. (v2 >_ 0 a.. v2 <_ .numberOfNodes)
-
-    ___ insertEdge v1, v2
-        __(.withInBounds(v1, v2)):
-            .graph[v1][v2] _ 1
-            .graph[v2][v1] _ 1
-
-    ___ printGraph(
-        ___ i __ ra..(.numberOfNodes
-            ___ j __ ra..(le.(.graph[i])):
-                __(.graph[i][j]
-                    print(i, "->", j)
-
-
-g _ Graph(5)
-
-g.insertEdge(1, 2)
-g.insertEdge(2, 3)
-g.insertEdge(4, 5)
-
-g.printGraph()
+# ____ co.. ______ d_d_
+#
+#
+# c_ Graph
+#     ___  -  numberOfNodes
+#         .? _ ?+1
+#         .g.. _ 0 ___ x __ ra.. ?+1
+#                       ___ y __ ra.. ?+1
+#
+#     ___ withInBounds v1 v2
+#         r_ ? >_ 0 a.. ? <_ .? a.. ? >_ 0 a.. ? <_ .?
+#
+#     ___ insertEdge v1 v2
+#         __ .w.. ? ?
+#             .g.. ? ? _ 1
+#             .g.. ? ? _ 1
+#
+#     ___ printGraph
+#         ___ i __ ra.. .?
+#             ___ j __ ra.. le. .g.. ?
+#                 __ .g.. ? ?
+#                     print ? "->" ?
+#
+#
+# g _ ? 5
+#
+# ?.i.. 1 2
+# ?.i.. 2 3
+# ?.i.. 4 5
+#
+# ?.p..
