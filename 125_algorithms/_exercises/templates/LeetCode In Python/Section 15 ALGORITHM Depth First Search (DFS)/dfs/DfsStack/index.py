f050317de8f15c6a590325824c976fca46141ddd
Grey <sergejyurskyj@yahoo.com>
2020-08-19 09:23:24 -0700

    Committed Section 15 ALGORITHM Depth First Search (DFS) template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 15 ALGORITHM Depth First Search (DFS)/dfs/DfsStack/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 15 ALGORITHM Depth First Search (DFS)/dfs/DfsStack/index.py
@@ -1,39 +1,39 @@
-____ co.. ______ d_d_
-
-
-c_ Graph:
-
-    ___  - (
-        .graph _ d_d_(list)
-
-    ___ insertEdge v1, v2
-        .graph[v1].ap..(v2)
-
-    ___ DFS startNode
-        visited _ set()
-        st _   # list
-        st.ap..(startNode)
-
-        w___(le.(st)):
-            cur _ st[-1]
-            st.p..
-
-            __(cur no. __ visited
-                print(cur, end_" ")
-                visited.add(cur)
-
-            ___ vertex __ .graph[cur]:
-                __(vertex no. __ visited
-                    st.ap..(vertex)
-
-
-g _ Graph()
-
-g.insertEdge(2, 1)
-g.insertEdge(2, 5)
-g.insertEdge(5, 6)
-g.insertEdge(5, 8)
-g.insertEdge(6, 9)
-
-
-g.DFS(2)
+# ____ co.. ______ d_d_
+#
+#
+# c_ Graph
+#
+#     ___  -
+#         .g.. _ d_d_ li..
+#
+#     ___ insertEdge v1 v2
+#         .g.. ? .ap.. ?
+#
+#     ___ DFS startNode
+#         visited _ se.
+#         st _   # list
+#         ?.ap.. ?
+#
+#         w___ le. ?
+#             cur _ ? -1
+#             ?.p..
+#
+#             __ cur no. __ v..
+#                 print ? e.._" "
+#                 v__.a.. ?
+#
+#             ___ vertex __ .g.. ?
+#                 __ ? no. __ v...
+#                     ?.ap.. ?
+#
+#
+# g _ ?
+#
+# ?.i.. 2 1
+# ?.i.. 2 5
+# ?.i.. 5 6
+# ?.i.. 5 8
+# ?.i.. 6 9
+#
+#
+# ?.D.. 2
