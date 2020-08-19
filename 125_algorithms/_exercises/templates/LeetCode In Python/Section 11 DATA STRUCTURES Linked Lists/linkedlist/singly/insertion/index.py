8ec956f0b1bcf0cbd28cdb010e7ed40469c1d245
Grey <sergejyurskyj@yahoo.com>
2020-08-19 10:01:58 -0700

    Committed Section 11 DATA STRUCTURES Linked Lists singly template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 11 DATA STRUCTURES Linked Lists/linkedlist/singly/insertion/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 11 DATA STRUCTURES Linked Lists/linkedlist/singly/insertion/index.py
@@ -1,57 +1,57 @@
-c_ Node:
-
-    ___  -  data
-        .data _ data
-        .n.. _ N..
-
-
-c_ LinkedList:
-
-    ___  - (
-        .head _ N..
-
-    ___ printList(
-        temp _ .head
-        linked_list _ ""
-        w___(temp
-            linked_list +_ (st.(temp.data) + " ")
-            temp _ temp.n..
-        print(linked_list)
-
-    # list start at 0
-    ___ insertNode val, pos
-        target _ Node(val)
-        __(pos __ 0
-            target.n.. _ .head
-            .head _ target
-            r_
-
-        ___ getPrev(pos
-            temp _ .head
-            count _ 1
-            w___(count < pos
-                temp _ temp.n..
-                count +_ 1
-            r_ temp
-
-        prev _ getPrev(pos)
-        nextNode _ prev.n..
-
-        prev.n.. _ target
-        target.n.. _ nextNode
-
-
-# List Structure : 5 => 1 => 3 => 7
-linked_list _ LinkedList()
-linked_list.head _ Node(5)
-
-second_node _ Node(1)
-third_node _ Node(3)
-fourth_node _ Node(7)
-
-linked_list.head.n.. _ second_node
-second_node.n.. _ third_node
-third_node.n.. _ fourth_node
-
-linked_list.insertNode(2, 2)
-linked_list.printList()
+# c_ Node:
+#
+#     ___  -  data
+#         .data _ ?
+#         .n.. _ N..
+#
+#
+# c_ LinkedList
+#
+#     ___  -
+#         .h.. _ N..
+#
+#     ___ printList
+#         temp _ .h..
+#         linked_list _ ""
+#         w___ ?
+#             ? +_ st. t__.data) + " ")
+#             temp _ t__.n..
+#         print ?
+#
+#     # list start at 0
+#     ___ insertNode val pos
+#         target _ N.. ?
+#         __ p.. __ 0
+#             t__.n.. _ .h..
+#             .h.. _ t..
+#             r_
+#
+#         ___ getPrev pos
+#             temp _ .h..
+#             count _ 1
+#             w___ c.. < ?
+#                 t.. _ t__.n..
+#                 c.. +_ 1
+#             r_ t__
+#
+#         prev _ g.. p..
+#         nextNode _ p__.n..
+#
+#         p__.n.. _ t..
+#         target.n.. _ n..
+#
+#
+# # List Structure : 5 => 1 => 3 => 7
+# linked_list _ ?
+# linked_list.h.. _ N.. 5
+#
+# second_node _ N.. 1
+# third_node _ N.. 3
+# fourth_node _ N.. 7
+#
+# ?.h...n.. _ s..
+# s__.n.. _ t..
+# t__.n.. _ f..
+#
+# ?.i.. 2 2
+# ?.p..
