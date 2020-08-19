c468e25cfd0387ea173867ee6732e22cff0a7aad
Grey <sergejyurskyj@yahoo.com>
2020-08-19 10:07:44 -0700

    Committed Section 12 INTERVIEW QUESTIONS Linked Lists template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 12 INTERVIEW QUESTIONS Linked Lists/add two numbers/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 12 INTERVIEW QUESTIONS Linked Lists/add two numbers/index.py
@@ -1,32 +1,32 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     ___ __init__(self, x
-#         self.val = x
-#         self.next = None
-
-c_ Solution:
-    ___ addTwoNumbers l1: ListNode, l2: ListNode)  ListNode:
-        ans _ ListNode(N..)
-        pointer _ ans
-
-        carry _ 0
-        su. _ 0
-
-        w___(l1!_None o.. l2!_None
-            su. _ carry
-            __(l1!_None
-                su.+_l1.val
-                l1 _ l1.n..
-            __(l2!_None
-                su.+_l2.val
-                l2 _ l2.val
-            
-            carry _ in.(su./10)
-            pointer.n..  _ ListNode(su.%10)
-
-            pointer _ pointer.n..
-        
-        __(carry>0
-            pointer.n.. _ ListNode(carry)
-        
-        r_ ans.n..
+# # Definition for singly-linked list.
+# # class ListNode:
+# #     ___ __init__(self, x
+# #         self.val = x
+# #         self.next = None
+#
+# c_ Solution
+#     ___ addTwoNumbers l1 LN.. l2 LN..  LN..
+#         ans _ LN.. N..
+#         pointer _ ?
+#
+#         carry _ 0
+#         su. _ 0
+#
+#         w___ l1!N.. o.. l2!N..
+#             su. _ c..
+#             __ ?!N..
+#                 su.+_?.v..
+#                 l1 _ ?.n..
+#             __ ?!N..
+#                 su.+_?.v..
+#                 l2 _ ?.v..
+#
+#             carry _ in.(su./10)
+#             p__.n..  _ LN.. su.%10
+#
+#             p.. _ p__.n..
+#
+#         __ c.. > 0
+#             p__.n.. _ LN.. c..
+#
+#         r_ a__.n..
