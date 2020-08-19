28cd2f67488e265984fd0a666bc143d6f5230b6b
Grey <sergejyurskyj@yahoo.com>
2020-08-19 09:30:02 -0700

    Committed Section 10 INTERVIEW QUESTIONS Stacks and Queues template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 10 INTERVIEW QUESTIONS Stacks and Queues/min stack/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 10 INTERVIEW QUESTIONS Stacks and Queues/min stack/index.py
@@ -1,18 +1,18 @@
-c_ MinStack
-    ___  -
-        .st _   # list
-
-    ___ push x: in.)  N..
-        curMin _ .getMin()
-        __ curMin __ N.. o.. curMin > x:
-            curMin _ x
-        .st.ap..((x, curMin))
-
-    ___ p..  N..
-        .st.p..
-
-    ___ top()  in.:
-        r_ .st[-1][0] __ .st ____  N..
-
-    ___ getMin()  in.:
-        r_ .st[-1][1] __ .st ____  N..
+# c_ MinStack
+#     ___  -
+#         .st _   # list
+#
+#     ___ push x in.  N..
+#         curMin _ .getMin
+#         __ ? __ N.. o.. ? > x:
+#             ? _ ?
+#         .st.ap.. ? ?
+#
+#     ___ p..  N..
+#         .st.p..
+#
+#     ___ top  in.
+#         r_ .st -1 0 __ .st ____  N..
+#
+#     ___ getMin  in.
+#         r_ .st -1 1 __ .st ____  N..
