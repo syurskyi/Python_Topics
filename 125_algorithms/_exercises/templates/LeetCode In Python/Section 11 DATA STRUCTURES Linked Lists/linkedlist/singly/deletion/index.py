42b05518dc01d803787284e41a481a468c69fd3c
Grey <sergejyurskyj@yahoo.com>
2020-08-19 09:57:48 -0700

    Committed Section 11 DATA STRUCTURES Linked Lists singly template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 11 DATA STRUCTURES Linked Lists/linkedlist/singly/deletion/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 11 DATA STRUCTURES Linked Lists/linkedlist/singly/deletion/index.py
@@ -1,52 +1,52 @@
-c_ Node:
-
-    ___  -  data
-        .data _ data
-        .n.. _ N..
-
-
-c_ LinkedList:
-
-    ___  - (
-        .head _ N..
-
-    ___ printList(
-        temp _ .head
-        linked_list _ ""
-        w___(temp
-            linked_list +_ (st.(temp.data) + " ")
-            temp _ temp.n..
-        print(linked_list)
-
-    ___ deleteNode key
-        temp _ .head
-        __(temp pa__ N..
-            r_
-        __(temp.data __ key
-            .head _ temp.n..
-            temp _ N..
-            r_
-
-        w___(temp.n...data !_ key
-            temp _ temp.n..
-
-        target_node _ temp.n..
-        temp.n.. _ target_node.n..
-        target_node.n.. _ N..
-
-
-# List Structure : 5 => 1 => 3 => 7
-# 5 => 1 => 7
-linked_list _ LinkedList()
-linked_list.head _ Node(5)
-
-second_node _ Node(1)
-third_node _ Node(3)
-fourth_node _ Node(7)
-
-linked_list.head.n.. _ second_node
-second_node.n.. _ third_node
-third_node.n.. _ fourth_node
-
-linked_list.deleteNode(3)
-linked_list.printList()
+# c_ Node:
+#
+#     ___  -  data
+#         .data _ ?
+#         .n.. _ N..
+#
+#
+# c_ LinkedList
+#
+#     ___  -
+#         .h.. _ N..
+#
+#     ___ printList
+#         temp _ .h..
+#         linked_list _ ""
+#         w___ ?
+#             ? +_ st. t__.data + " "
+#             temp _ t__.n..
+#         print ?
+#
+#     ___ deleteNode key
+#         temp _ .h..
+#         __ t__ pa__ N..
+#             r_
+#         __ t__.d.. __ ?
+#             .h.. _ t__.n..
+#             t__ _ N..
+#             r_
+#
+#         w___ t__.n...d.. !_ ?
+#             t__ _ t__.n..
+#
+#         target_node _ t__.n..
+#         t__.n.. _ ?.n..
+#         ?.n.. _ N..
+#
+#
+# # List Structure : 5 => 1 => 3 => 7
+# # 5 => 1 => 7
+# linked_list _ ?
+# ?.h.. _ N.. 5
+#
+# second_node _ N.. 1
+# third_node _ N.. 3
+# fourth_node _ N.. 7
+#
+# l__.h...n.. _ s..
+# s__.n.. _ t..
+# t__.n.. _ f..
+#
+# l__.d.. 3
+# l__.p..
