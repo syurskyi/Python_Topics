747aaf66ef686014f7f0e4174d0fb0b8517fb703
Grey <sergejyurskyj@yahoo.com>
2020-08-19 10:04:03 -0700

    Committed Section 11 DATA STRUCTURES Linked Lists singly template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 11 DATA STRUCTURES Linked Lists/linkedlist/singly/intro/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 11 DATA STRUCTURES Linked Lists/linkedlist/singly/intro/index.py
@@ -1,33 +1,33 @@
-c_ Node:
-    ___  -  data
-        .data _ data
-        .n.. _ N..
-
-
-c_ LinkedList:
-    ___  - (
-        .head _ N..
-
-    ___ printList(
-        temp _ .head
-        linked_list _ ""
-        w___(temp
-            linked_list +_ (st.(temp.data) + " ")
-            temp _ temp.n..
-        print(linked_list)
-
-# Node strucutre: 5 => 1 => 3 => 7
-
-
-linked_list _ LinkedList()
-linked_list.head _ Node(5)
-
-second_node _ Node(1)
-third_node _ Node(3)
-fourth_node _ Node(7)
-
-linked_list.head.n.. _ second_node
-second_node.n.. _ third_node
-third_node.n.. _ fourth_node
-
-linked_list.printList()
+# c_ Node:
+#     ___  -  data
+#         .data _ ?
+#         .n.. _ N..
+#
+#
+# c_ LinkedList
+#     ___  -
+#         .h.. _ N..
+#
+#     ___ printList
+#         temp _ .h..
+#         linked_list _ ""
+#         w___ ?
+#             ? +_ st. t__.data + " "
+#             t__ _ t__.n..
+#         print ?
+#
+# # Node strucutre: 5 => 1 => 3 => 7
+#
+#
+# linked_list _ ?
+# linked_list.h.. _ N.. 5
+#
+# second_node _ N.. 1
+# third_node _ N.. 3
+# fourth_node _ N.. 7
+#
+# ?.h...n.. _ s..
+# s__.n.. _ t..
+# t__.n.. _ f..
+#
+# ?.p..
