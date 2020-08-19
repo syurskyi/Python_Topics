33ccadccb393dfec996b205b4435c917d7dedc94
Grey <sergejyurskyj@yahoo.com>
2020-08-19 09:25:48 -0700

    Committed Section 16 ALGORITHM Breadth First Search (BFS) template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 16 ALGORITHM Breadth First Search (BFS)/bfs/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 16 ALGORITHM Breadth First Search (BFS)/bfs/index.py
@@ -1,38 +1,38 @@
-
-____ co.. ______ d_d_
-
-
-c_ Graph:
-
-    ___  - (
-        .graph _ d_d_(list)
-
-    ___ setEdge u, v
-        .graph[u].ap..(v)
-
-    ___ bfs s
-        visited _ set()
-
-        queue _   # list
-        queue.ap..(s)
-
-        visited.add(s)
-
-        w___ queue:
-            u _ queue.pop(0)
-            print(u, end_" ")
-
-            ___ v __ .graph[u]:
-                __ v no. __ visited:
-                    queue.ap..(v)
-                    visited.add(v)
-
-
-g _ Graph()
-g.setEdge(2, 1)
-g.setEdge(2, 5)
-g.setEdge(5, 6)
-g.setEdge(5, 8)
-g.setEdge(6, 9)
-
-g.bfs(2)
+#
+# ____ co.. ______ d_d_
+#
+#
+# c_ Graph
+#
+#     ___  -
+#         .g.. _ d_d_ li..
+#
+#     ___ setEdge u v
+#         .g.. ? .ap.. ?
+#
+#     ___ bfs s
+#         visited _ se.
+#
+#         queue _   # list
+#         ?.ap.. ?
+#
+#         v__.a.. ?
+#
+#         w___ q..
+#             u _ ?.p.. 0
+#             print ? e.._" "
+#
+#             ___ v __ .g.. ?
+#                 __ v no. __ v..
+#                     q__.ap.. ?
+#                     v__.a.. ?
+#
+#
+# g _ ?
+# ?.s.. 2 1
+# ?.s.. 2 5
+# ?.s.. 5 6
+# ?.s.. 5 8
+# ?.s.. 6 9
+#
+# ?.b.. 2
