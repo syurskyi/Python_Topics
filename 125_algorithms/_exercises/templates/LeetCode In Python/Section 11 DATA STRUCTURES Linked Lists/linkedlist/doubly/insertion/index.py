048c0cf09d0ae6a0b9f52043db363ed5c64a5e16
Grey <sergejyurskyj@yahoo.com>
2020-08-19 09:53:15 -0700

    Committed Section 11 DATA STRUCTURES Linked Lists template

--- 125_algorithms/_exercises/templates/LeetCode In Python/Section 11 DATA STRUCTURES Linked Lists/linkedlist/doubly/insertion/index.py
+++ 125_algorithms/_exercises/templates/LeetCode In Python/Section 11 DATA STRUCTURES Linked Lists/linkedlist/doubly/insertion/index.py
@@ -1,100 +1,100 @@
-c_ Node:
-    ___  -  data
-        .data _ data
-        .prev _ N..
-        .n.. _ N..
-
-
-c_ LinkedList:
-    ___  - (
-        .head _ N..
-
-    ___ createList arr
-        start _ .head
-        n _ le.(arr)
-
-        temp _ start
-        i _ 0
-
-        w___(i < n
-            newNode _ Node(arr[i])
-            __(i __ 0
-                start _ newNode
-                temp _ start
-            ____
-                temp.n.. _ newNode
-                newNode.prev _ temp
-                temp _ temp.n..
-            i +_ 1
-        .head _ start
-        r_ start
-
-    ___ printList(
-        temp _ .head
-        linked_list _ ""
-        w___(temp
-            linked_list +_ (st.(temp.data) + " ")
-            temp _ temp.n..
-
-        print(linked_list)
-
-    ___ countList(
-        temp _ .head
-        count _ 0
-        w___(temp pa__ no. N..
-            temp _ temp.n..
-            count +_ 1
-        r_ count
-
-    # we will consider that the index begin at 1
-    ___ insertAtLocation value, index
-        temp _ .head
-
-        count _ .countList()
-
-        #index is 6, count is 5, valid 
-        #index is 7, count is 5, 
-        __(count+1<index
-            r_ temp
-        
-        newNode _ Node(value)
-
-        __(index __ 1
-            newNode.n.. _ temp
-            temp.prev _ newNode
-            .head _ newNode
-            r_ .head
-        
-        __(index __ count +1
-            w___(temp.n.. pa__ no. N..
-                temp _ temp.n..
-
-            temp.n.. _ newNode
-            newNode.prev _ temp
-            r_ .head
-        
-        i _ 1
-        w___(i < index-1
-            temp _ temp.n..
-            i+_1
-        
-        nodeAtTarget _ temp.n..
-
-        newNode.n.. _ nodeAtTarget
-        nodeAtTarget.prev _ newNode
-
-        temp.n.. _ newNode
-        newNode.prev _ temp
-
-        r_ .head
-
-
-arr _ [1, 2, 3, 4, 5]
-
-llist _ LinkedList()
-
-llist.createList(arr)
-
-llist.insertAtLocation(5,6)
-
-llist.printList()
+# c_ Node
+#     ___  -  data
+#         .data _ ?
+#         .prev _ N..
+#         .n.. _ N..
+#
+#
+# c_ LinkedList
+#     ___  -
+#         .h.. _ N..
+#
+#     ___ createList arr
+#         start _ .h..
+#         n _ le. ?
+#
+#         temp _ start
+#         i _ 0
+#
+#         w___ ? < ?
+#             newNode _ N.. a.. ?
+#             __ i __ 0
+#                 start _ n..
+#                 temp _ ?
+#             ____
+#                 t__.n.. _ n..
+#                 n__.p.. _ t__
+#                 temp _ t__.n..
+#             i +_ 1
+#         .h.. _ s..
+#         r_ ?
+#
+#     ___ printList
+#         temp _ .h..
+#         linked_list _ ""
+#         w___ t..
+#             ? +_  st. t__.d.. + " "
+#             temp _ t__.n..
+#
+#         print ?
+#
+#     ___ countList
+#         temp _ .h..
+#         count _ 0
+#         w___ t.. pa__ no. N..
+#             temp _ t__.n..
+#             c.. +_ 1
+#         r_ ?
+#
+#     # we will consider that the index begin at 1
+#     ___ insertAtLocation value index
+#         temp _ .h..
+#
+#         count _ .c..
+#
+#         #index is 6, count is 5, valid
+#         #index is 7, count is 5,
+#         __ c.. + 1< i..
+#             r_ t..
+#
+#         newNode _ N.. v..
+#
+#         __ index __ 1
+#             n__.n.. _ t..
+#             temp.p.. _ n..
+#             .h.. _ n..
+#             r_ .h..
+#
+#         __ i.. __ c.. +1
+#             w___ t__.n.. pa__ no. N..
+#                 temp _ t__.n..
+#
+#             t__.n.. _ n..
+#             n__.p.. _ t..
+#             r_ .h..
+#
+#         i _ 1
+#         w___ ? < i.. - 1
+#             temp _ t__.n..
+#             ?+_1
+#
+#         nodeAtTarget _ t__.n..
+#
+#         n__.n.. _ ?
+#         ?.p.. _ n..
+#
+#         t__.n.. _ n..
+#         n__.p.. _ t..
+#
+#         r_ .h..
+#
+#
+# arr _ [1, 2, 3, 4, 5]
+#
+# llist _ ?
+#
+# ?.c.. ?
+#
+# ?.i.. 5 6
+#
+# ?.p..
