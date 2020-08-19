e083d9fece690d4c5683cef64e0e42c8502e5186
Grey <sergejyurskyj@yahoo.com>
2020-08-19 10:52:28 -0700

    Committed LeetCode in Python exercises

--- 125_algorithms/_exercises/exercises/LeetCode In Python/network delay time/index.py
+++ 125_algorithms/_exercises/exercises/LeetCode In Python/network delay time/index.py
@@ -1,27 +1,27 @@
-# network delay time - leetcode
-c_ Solution:
-    ___ networkDelayTime times: L..[L..[in.]], N: in., K: in.)  in.:
-
-        g _ co...d_d_(list)
-        ___ u, v, cost __ times:
-            g[u].ap..((cost, v))
-
-        # cost,node
-        min_heap _ [(0, K)]
-        visited _ set()
-        distance _ {i: fl..('inf') ___ i __ ra..(1, N+1)}
-        distance[K] _ 0
-
-        w___ min_heap:
-            cur_dist, u _ heapq.heappop(min_heap)
-            __ u __ visited:
-                continue
-            visited.add(u)
-            __ le.(visited) __ N:
-                r_ cur_dist
-
-            ___ direct_distance, v __ g[u]:
-                __ cur_dist + direct_distance < distance[v] a.. v no. __ visited:
-                    distance[v] _ cur_dist + direct_distance
-                    heapq.heappush(min_heap, (cur_dist + direct_distance, v))
-        r_ -1
+# # network delay time - leetcode
+# c_ Solution
+#     ___ networkDelayTime times L.. L.. in. N in. K in.  in.
+#
+#         g _ co...d_d_ l..
+#         ___ u, v, cost __ t..
+#             ? ? .ap.. c.. ?
+#
+#         # cost,node
+#         min_heap _ 0 K
+#         visited _ se.
+#         distance _ i fl.. 'inf' ___ ? __ ra.. 1 N+1
+#         ? K _ 0
+#
+#         w___ m..
+#             cur_dist u _ h__.h_p_ m..
+#             __ u __ v..
+#                 c..
+#             v__.a.. ?
+#             __ le. v.. __ N
+#                 r_ ?
+#
+#             ___ direct_distance v __ g u
+#                 __ c.. + ? < d.. v a.. v no. __ v..
+#                     d.. v _ c.. + d..
+#                     h__.h_p__ m.. c.. + d.. v
+#         r_ -1
