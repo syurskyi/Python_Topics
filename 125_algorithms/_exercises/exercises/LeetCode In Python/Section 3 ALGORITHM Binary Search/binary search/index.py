2e93bf9e6a1d4b639fb84184c268b02fcdf3bef8
Grey <sergejyurskyj@yahoo.com>
2020-08-20 20:31:06 -0700

    Committed binary search exercises

--- 125_algorithms/_exercises/exercises/LeetCode In Python/Section 3 ALGORITHM Binary Search/binary search/index.py
+++ 125_algorithms/_exercises/exercises/LeetCode In Python/Section 3 ALGORITHM Binary Search/binary search/index.py
@@ -1,33 +1,33 @@
-# ______ ma__
-#
-# ___ binarySearch arr target
-#     left _ 0
-#     right _ le. ? - 1
-#     w___ ? <_ ?
-#
-#         mid _ ma__.fl.. ? + (r.. - ?)/2)
-#         # Check if x is present at mid
-#         __ ?|? __ ?
-#             r_ ?
-#
-#         # If x is greater, ignore left half
-#         ____ ?|? < ?
-#             l.. _ m.. + 1
-#
-#         # If x is smaller, ignore right half
-#         ____
-#             r.. _ ? - 1
-#
-#     # If we reach here, then the element was not present
-#     r_ -1
-#
-#
-# arr _ [1, 2, 3, 4, 5, 6]
-# target _ 6
-#
-# result _ ? ? ?
-#
-# __ result !_ -1
-#     print("Element is present at index @" ?
-# ____
-#     print("Element is not present in array")
+import math
+
+def binarySearch(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+
+        mid = math.floor(left + (right - left)/2)
+        # Check if x is present at mid
+        if arr[mid] == target:
+            return mid
+
+        # If x is greater, ignore left half
+        elif arr[mid] < target:
+            left = mid + 1
+
+        # If x is smaller, ignore right half
+        else:
+            right = mid - 1
+
+    # If we reach here, then the element was not present
+    return -1
+
+
+arr = [1, 2, 3, 4, 5, 6]
+target = 6
+
+result = binarySearch(arr, target)
+
+if result != -1:
+    print("Element is present at index %d" % result)
+else:
+    print("Element is not present in array")
