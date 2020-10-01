0844ba5c81518acc6082439c70a3cbdb7c558f67
Grey <sergejyurskyj@yahoo.com>
2020-09-30 21:59:22 -0700

    Committed Python_Hand-on_Solve_200_Problems, Section 15 Sort templates

--- 125_algorithms/_exercises/templates/Python_Hand-on_Solve_200_Problems/Section 15 Sort/merge_sort_implement_solution.py
+++ 125_algorithms/_exercises/templates/Python_Hand-on_Solve_200_Problems/Section 15 Sort/merge_sort_implement_solution.py
@@ -1,53 +1,53 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-# Write a Python program to sort a list of elements using the merge sort algorithm.
-# Note: According to Wikipedia "Merge sort (also commonly spelled mergesort) is an O (n log n)
-# comparison-based sorting algorithm. Most implementations produce a stable sort, which means that 
-# the implementation preserves the input order of equal elements in the sorted output."
-
-# Algorithm:
-# Conceptually, a merge sort works as follows :
-
-# Divide the unsorted list into n sublists, each containing 1 element (a list of 1 element is considered sorted).
-# Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining. This will be the sorted list.
-
-
-# %%
-___ mergeSort(nlist):
-    print("Splitting ",nlist)
-    __ le.(nlist)>1:
-        mid _ le.(nlist)//2
-        lefthalf _ nlist[:mid]
-        righthalf _ nlist[mid:]
-
-        mergeSort(lefthalf)
-        mergeSort(righthalf)
-        i_j_k_0
-        w___ i < le.(lefthalf) an. j < le.(righthalf):
-            __ lefthalf[i] < righthalf[j]:
-                nlist[k]_lefthalf[i]
-                i_i+1
-            ____
-                nlist[k]_righthalf[j]
-                j_j+1
-            k_k+1
-
-        w___ i < le.(lefthalf):
-            nlist[k]_lefthalf[i]
-            i_i+1
-            k_k+1
-
-        w___ j < le.(righthalf):
-            nlist[k]_righthalf[j]
-            j_j+1
-            k_k+1
-    print("Merging ",nlist)
-
-nlist _ [14,46,43,27,57,41,45,21,70]
-mergeSort(nlist)
-print(nlist)
-
-# %% [markdown]
-# ![merge_sort.png](attachment:merge_sort.png)
-
+# # To add a new cell, type '# %%'
+# # To add a new markdown cell, type '# %% [markdown]'
+# # %%
+# # Write a Python program to sort a list of elements using the merge sort algorithm.
+# # Note: According to Wikipedia "Merge sort (also commonly spelled mergesort) is an O (n log n)
+# # comparison-based sorting algorithm. Most implementations produce a stable sort, which means that
+# # the implementation preserves the input order of equal elements in the sorted output."
+#
+# # Algorithm:
+# # Conceptually, a merge sort works as follows :
+#
+# # Divide the unsorted list into n sublists, each containing 1 element (a list of 1 element is considered sorted).
+# # Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining. This will be the sorted list.
+#
+#
+# # %%
+# ___ mergeSort nlist
+#     print("Splitting " ?
+#     __ le. ? > 1
+#         mid _ le. ? // 2
+#         lefthalf _ ? |?
+#         righthalf _ ? ?|
+#
+#         ? ?
+#         ? ?
+#         i_j_k_0
+#         w___ i < le. ? an. j < le. ?
+#             __ ? ? < ? ?
+#                 ? k _ ? ?
+#                 i _ ?+1
+#             ____
+#                 ? k _ ? ?
+#                 j _ ?+1
+#             k _ ?+1
+#
+#         w___ i < le. ?
+#             ? k _ ? ?
+#             i _ ? + 1
+#             k _ ? + 1
+#
+#         w___ j < le. ?
+#             ? k _ ? ?
+#             j _ ? + 1
+#             k_ ? + 1
+#     print("Merging " ?
+#
+# nlist _ [14,46,43,27,57,41,45,21,70]
+# ? ?
+# print ?
+#
+# # %% [markdown]
+# # ![merge_sort.png](attachment:merge_sort.png)
+#
--- 125_algorithms/_exercises/templates/Python_Hand-on_Solve_200_Problems/Section 15 Sort/shell_sort_implement_solution.py
+++ 125_algorithms/_exercises/templates/Python_Hand-on_Solve_200_Problems/Section 15 Sort/shell_sort_implement_solution.py
@@ -1,40 +1,40 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-# Write a Python program to sort a list of elements using shell sort algorithm.
-# Note : According to Wikipedia "Shell sort or Shell's method, is an in-place comparison sort. 
-# It can be seen as either a generalization of sorting by exchange (bubble sort) or sorting by insertion (insertion sort). 
-# The method starts by sorting pairs of elements far apart from each other, then progressively reducing the gap between
-# elements to be compared. Starting with far apart elements can move some out-of-place elements into position faster than
-# a simple nearest neighbor exchange."
-
-
-# %%
-___ shellSort(alist):
-    sublistcount _ le.(alist)//2
-    w___ sublistcount > 0:
-      ___ start_position __ ra..(sublistcount):
-        gap_InsertionSort(alist, start_position, sublistcount)
-
-      print("After increments of size",sublistcount, "The list is",nlist)
-
-      sublistcount _ sublistcount // 2
-
-___ gap_InsertionSort(nlist,start,gap):
-    ___ i __ ra..(start+gap,le.(nlist),gap):
-
-        current_value _ nlist[i]
-        position _ i
-
-        w___ position>_gap an. nlist[position-gap]>current_value:
-            nlist[position]_nlist[position-gap]
-            position _ position-gap
-
-        nlist[position]_current_value
-
-
-nlist _ [14,46,43,27,57,41,45,21,70]
-shellSort(nlist)
-print(nlist)
-
-
+# # To add a new cell, type '# %%'
+# # To add a new markdown cell, type '# %% [markdown]'
+# # %%
+# # Write a Python program to sort a list of elements using shell sort algorithm.
+# # Note : According to Wikipedia "Shell sort or Shell's method, is an in-place comparison sort.
+# # It can be seen as either a generalization of sorting by exchange (bubble sort) or sorting by insertion (insertion sort).
+# # The method starts by sorting pairs of elements far apart from each other, then progressively reducing the gap between
+# # elements to be compared. Starting with far apart elements can move some out-of-place elements into position faster than
+# # a simple nearest neighbor exchange."
+#
+#
+# # %%
+# ___ shellSort alist
+#     sublistcount _ le. ? //2
+#     w___ ? > 0
+#       ___ start_position __ ra.. ?
+#         ? ? ? ?
+#
+#       print("After increments of size" ? "The list is" ?
+#
+#       sublistcount _ ? // 2
+#
+# ___ gap_InsertionSort nlist start gap
+#     ___ i __ ra.. s.. + ? le. ? ?
+#
+#         current_value _ ? ?
+#         position _ ?
+#
+#         w___ ? >_ g.. an. ? ? - g.. > c..
+#             ? ? _ ? ? - ?
+#             position _ ? - g..
+#
+#         ? ? _ c..
+#
+#
+# nlist _ [14,46,43,27,57,41,45,21,70]
+# ? ?
+# print ?
+#
+#
