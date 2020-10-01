cdfa2deb757d9b3b8e8208a92b234bab870569f9
Grey <sergejyurskyj@yahoo.com>
2020-09-30 21:43:42 -0700

    Committed Python_Hand-on_Solve_200_Problems, Section 16 Search templates

--- 125_algorithms/_exercises/templates/Python_Hand-on_Solve_200_Problems/Section 16 Search/binary_search_implement_solution.py
+++ 125_algorithms/_exercises/templates/Python_Hand-on_Solve_200_Problems/Section 16 Search/binary_search_implement_solution.py
@@ -1,27 +1,28 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-# Write a Python program for binary search. 
-# Binary Search : In computer science, a binary search or half-interval search algorithm finds the position of a target value
-# within a sorted array. The binary search algorithm can be classified as a dichotomies divide-and-conquer search algorithm and 
-# executes in logarithmic time.
-
-___ binary_search(item_listac,item):
-	first _ 0
-	last _ le.(item_list)-1
-	found _ F..
-	w___( first<_last an. no. found):
-		mid _ (first + last)//2
-		__ item_list[mid] __ item :
-			found _ T..
-		____
-			__ item < item_list[mid]:
-				last _ mid - 1
-			____
-				first _ mid + 1	
-	r_ found
-	
-print(binary_search([1,2,3,5,8], 6))
-print(binary_search([1,2,3,5,8], 5))
-
-
+# # To add a new cell, type '# %%'
+# # To add a new markdown cell, type '# %% [markdown]'
+# # %%
+# # Write a Python program for binary search.
+# # Binary Search : In computer science, a binary search or half-interval search algorithm finds the position of a target value
+# # within a sorted array. The binary search algorithm can be classified as a dichotomies divide-and-conquer search algorithm and
+# # executes in logarithmic time.
+#
+# ___ binary_search item_listac item
+# 	first _ 0
+# 	last _ le. ? - 1
+# 	found _ F..
+# 	w___ ? <_ ? an. no. ?
+# 		mid _ ? + ? // 2
+# 		__ item_list ? __ i..
+#
+# 			found _ T..
+# 		____
+# 			__ i.. < ? ?
+# 				last _ m.. - 1
+# 			____
+# 				first _ ? + 1
+# 	r_ ?
+#
+# print(?([1,2,3,5,8], 6))
+# print(?([1,2,3,5,8], 5))
+#
+#
