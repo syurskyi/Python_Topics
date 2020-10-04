eaafe12ad68e0ef46cb537e21a54b2472fe1a85b
Grey <sergejyurskyj@yahoo.com>
2020-10-03 18:19:47 -0700

    Committed cyclic_rotation exercises

--- 125_algorithms/001_arrays/_exercises/_templates/Beat the Codility Coding Interview in Python/cyclic_rotation_!cool!.py
+++ 125_algorithms/001_arrays/_exercises/_templates/Beat the Codility Coding Interview in Python/cyclic_rotation_!cool!.py
@@ -3,12 +3,12 @@
 # # This is marked as PAINLESS difficulty
 #
 # ___ solution A K
-#     result _ |N.. * le. A     # Alocate space for a new array, a new array having the same length as our input array A
+#     result _ |N.. * le. ?    # Alocate space for a new array, a new array having the same length as our input array A
 #
-#     ___ i __ ra.. le A
-#         ? ||i + K| % le.|A|| _ A|i
+#     ___ i __ ra.. le ?
+#         ? ||? + ?| % le.|?|| _ ?|?
 #
-#     r_ r..
+#     r_ ?
 #
 # print(? 1, 2, 3, 4, 5| 2
 #
--- 125_algorithms/001_arrays/_exercises/exercises/Beat the Codility Coding Interview in Python/cyclic_rotation_!cool!.py
+++ 125_algorithms/001_arrays/_exercises/exercises/Beat the Codility Coding Interview in Python/cyclic_rotation_!cool!.py
@@ -1,15 +1,15 @@
-# # This is the solution for Arrays > CyclicRotation
-# #
-# # This is marked as PAINLESS difficulty
+# This is the solution for Arrays > CyclicRotation
 #
-# ___ solution A K
-#     result _ |N.. * le. A     # Alocate space for a new array, a new array having the same length as our input array A
-#
-#     ___ i __ ra.. le A
-#         ? ||i + K| % le.|A|| _ A|i
-#
-#     r_ r..
-#
-# print(? 1, 2, 3, 4, 5| 2
-#
-# print(? 1, 2, 3, 4, 5| 5
+# This is marked as PAINLESS difficulty
+
+def solution(A, K):
+    result = [None] * len(A)    # Alocate space for a new array, a new array having the same length as our input array A
+
+    for i in range(len(A)):
+        result[(i + K) % len(A)] = A[i]
+
+    return result
+
+print(solution([1, 2, 3, 4, 5], 2))
+
+print(solution([1, 2, 3, 4, 5], 5))
