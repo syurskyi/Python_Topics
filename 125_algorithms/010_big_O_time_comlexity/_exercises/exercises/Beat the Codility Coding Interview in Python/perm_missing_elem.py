6e700e7af935bce793e986af442d8d88a8168153
Grey <sergejyurskyj@yahoo.com>
2020-10-03 18:01:50 -0700

    Committed 010_big_O_time_comlexity exercises

---
+++ 125_algorithms/010_big_O_time_comlexity/_exercises/exercises/Beat the Codility Coding Interview in Python/perm_missing_elem.py
@@ -0,0 +1,48 @@
+# """
+#
+# An array A consisting of N different integers is given. The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
+#
+# Your goal is to find that missing element.
+#
+# Write a function:
+#
+#     def solution(A)
+#
+# that, given an array A, returns the value of the missing element.
+#
+# For example, given array A such that:
+#   A[0] = 2
+#   A[1] = 3
+#   A[2] = 1
+#   A[3] = 5
+#
+# the function should return 4, as it is the missing element.
+#
+# Write an efficient algorithm for the following assumptions:
+#
+#         N is an integer within the range [0..100,000];
+#         the elements of A are all distinct;
+#         each element of array A is an integer within the range [1..(N + 1)].
+#
+# """
+#
+#
+# # This is the solution for Time Complexity > PermMissingElem
+# #
+# # This is marked as PAINLESS difficulty
+#
+#
+# ___ solution A
+#     actual_sum _ 0
+#     ___ number __ ?
+#         ? +_ ?
+#     max_number _ le. ? + 1
+#     expected_sum _ ? * |? + 1) // 2)
+#     r_ ? - a..
+#
+#
+# print(?([2, 3, 1, 5]))
+#
+# print(?([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+#
+# print(?([]))
