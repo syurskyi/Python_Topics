7523c3dbfad354af32224fccc118637d311bb001
Grey <sergejyurskyj@yahoo.com>
2020-10-30 18:16:26 -0700

    Committed 362. insertItem_toClass

--- 140_gui/pyqt_pyside/examples/PyQt_PySide_book/005_Lists and tables/001_Drop-down list/362. insertItem_toClass.py
+++ 140_gui/pyqt_pyside/examples/PyQt_PySide_book/005_Lists and tables/001_Drop-down list/362. insertItem_toClass.py
@@ -12,27 +12,25 @@ class InsertItem(QtWidgets.QWidget):
 
     def initUI(self):
 
-def on_clicked():
-    print("Текст:", comboBox.currentText())
+        self.setWindowTitle("Класс QComboBox")
+        self.resize(300, 90)
+        self.comboBox = QtWidgets.QComboBox()
+        for i in range(1, 11):
+            self.comboBox.addItem("Пункт {0}".format(i), i)
+        ico = self.style().standardIcon(
+            QtWidgets.QStyle.SP_MessageBoxCritical)
+        self.comboBox.insertItem(0, ico, "Пункт 11", 11)
+        self.comboBox.insertItem(1, "Пункт 12", 12)
+        button = QtWidgets.QPushButton("Получить значение")
+        button.clicked.connect(self.on_clicked)
+        box = QtWidgets.QVBoxLayout()
+        box.addWidget(self.comboBox)
+        box.addWidget(button)
+        self.setLayout(box)
+        self.show()
 
-app = QtWidgets.QApplication(sys.argv)
-window = QtWidgets.QWidget()
-window.setWindowTitle("Класс QComboBox")
-window.resize(300, 90)
-comboBox = QtWidgets.QComboBox()
-for i in range(1, 11):
-    comboBox.addItem("Пункт {0}".format(i), i)
-ico = window.style().standardIcon(
-    QtWidgets.QStyle.SP_MessageBoxCritical)
-comboBox.insertItem(0, ico, "Пункт 11", 11)
-comboBox.insertItem(1, "Пункт 12", 12)
-button = QtWidgets.QPushButton("Получить значение")
-button.clicked.connect(on_clicked)
-box = QtWidgets.QVBoxLayout()
-box.addWidget(comboBox)
-box.addWidget(button)
-window.setLayout(box)
-window.show()
+    def on_clicked(self):
+        print("Текст:", self.comboBox.currentText())
 
 
 if __name__ == '__main__':
