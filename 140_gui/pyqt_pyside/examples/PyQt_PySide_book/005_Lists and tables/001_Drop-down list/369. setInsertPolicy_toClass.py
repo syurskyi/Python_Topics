396621f56676f877d648b60e39730027c2b6e190
Grey <sergejyurskyj@yahoo.com>
2020-10-30 18:29:28 -0700

    Committed 369. setInsertPolicy_toClass

--- 140_gui/pyqt_pyside/examples/PyQt_PySide_book/005_Lists and tables/001_Drop-down list/369. setInsertPolicy_toClass.py
+++ 140_gui/pyqt_pyside/examples/PyQt_PySide_book/005_Lists and tables/001_Drop-down list/369. setInsertPolicy_toClass.py
@@ -20,18 +20,15 @@ class SetInsertPopicy(QtWidgets.QWidget):
         for i in range(1, 11):
             self.comboBox.addItem("Пункт {0}".format(i))
         button = QtWidgets.QPushButton("Получить индекс текущего элемента")
-        button.clicked.connect(on_clicked)
+        button.clicked.connect(self.on_clicked)
         box = QtWidgets.QVBoxLayout()
         box.addWidget(self.comboBox)
         box.addWidget(button)
         self.setLayout(box)
         self.show()
 
-    def on_clicked():
+    def on_clicked(self):
         print(self.comboBox.currentIndex())
-
-
-
 
 
 if __name__ == '__main__':
