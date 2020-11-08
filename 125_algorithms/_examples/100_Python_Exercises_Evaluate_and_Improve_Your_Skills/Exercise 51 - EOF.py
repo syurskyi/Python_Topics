63a1c7090f56f7cd397fb3cc7b51923431eb5170
Grey <sergejyurskyj@yahoo.com>
2020-11-08 15:23:56 -0800

    Committed 100_Python_Exercises_Evaluate_and_Improve_Your_Skills

--- 125_algorithms/_examples/100_Python_Exercises_Evaluate_and_Improve_Your_Skills/Exercise 51 - EOF.py
+++ 125_algorithms/_examples/100_Python_Exercises_Evaluate_and_Improve_Your_Skills/Exercise 51 - EOF.py
@@ -1,3 +1,4 @@
 #The script is trying to print out the type of the last character of updated string.
 #A: One more bracket is needed to close the print function
 print(type("Hey".replace("ey","i")[-1]))
+print("Hey".replace("ey","i")[-1])
