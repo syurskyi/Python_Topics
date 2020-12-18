e8a1e2b261d9e660e5ee152ae7c60a1cbe053d71
Grey <sergejyurskyj@yahoo.com>
2020-12-17 20:13:53 -0800

    Committed 00-inheritance exercise

--- 070_oop/001_classes/_exercises/exercises/ITDN Python RUS/002_Nasledovanie i Polimorfizm/00-inheritance.py
+++ 070_oop/001_classes/_exercises/exercises/ITDN Python RUS/002_Nasledovanie i Polimorfizm/00-inheritance.py
@@ -1,35 +1,35 @@
-# # -*- coding: utf-8 -*-
-#
-# """
-# Пример наследования классов
-# """
-#
-# c_ Figure o..
-#     ___ - ____ side
-#         ____.s.. _ s..
-#
-#
-# c_ Square ?
-#     ___ draw ____
-#         _ i i_ ra__ ____.s..
-#             print('*' * ____.s..
-#
-#
-# c_ Triangle ?
-#     ___ draw ____
-#         ___ i i_ ra.. ____.s..
-#             print '*' * _
-#
-#
-# ___ m..
-#     square _ S.. 10
-#     ?.d..
-#
-#     print()
-#
-#     triangle = T.. 5
-#     t__.d..
-#
-#
-# _____ _____ __ _____
-#     m..
+# -*- coding: utf-8 -*-
+
+"""
+Пример наследования классов
+"""
+
+class Figure(object):
+    def __init__(self, side):
+        self.side = side
+
+
+class Square(Figure):
+    def draw(self):
+        for i in range(self.side):
+            print('*' * self.side)
+
+
+class Triangle(Figure):
+    def draw(self):
+        for i in range(self.side):
+            print('*' * i)
+
+
+def main():
+    square = Square(10)
+    square.draw()
+
+    print()
+
+    triangle = Triangle(5)
+    triangle.draw()
+
+
+if __name__ == '__main__':
+    main()
