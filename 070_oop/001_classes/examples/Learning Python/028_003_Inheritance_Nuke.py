787c3f7a5cc93106c0ab4442df17fcf767348a5b
Grey <sergejyurskyj@yahoo.com>
2020-12-19 15:09:38 -0800

    Committed 028_003_Inheritance_Nuke exercise

---
+++ 070_oop/001_classes/examples/Learning Python/028_003_Inheritance_Nuke.py
@@ -0,0 +1,46 @@
+import nuke
+
+class Super:
+    def method(self):
+        nuke.tprint('#' * 23 + ' Default behavior')
+        self.grade = nuke.createNode('Grade', inpanel=False)
+        nuke.tprint('#' * 23 + ' in Super.method')
+
+    def delegate(self):
+        self.action()
+
+class  Inheritor(Super):
+    pass
+
+class Replacer(Super):
+    def method(self):
+        nuke.tprint('#' * 23 + ' Replace method completely')
+        blur = nuke.createNode('Blur', inpanel=False)
+        nuke.tprint('#' * 23 + ' in Replacer.method')
+
+
+
+class Extender(Super):
+    def method(self):
+        nuke.tprint('#' * 23 + ' Extend method behavior')
+        nuke.tprint('starting Extender.method')
+        Super.method(self)
+        self.grade['white'].setValue(5)
+        nuke.tprint('ending Extender.method')
+
+class Provider(Super):
+    def action(self):
+        nuke.tprint('#' * 23 + ' Fill in a required method')
+        transform = nuke.createNode('Transform', inpanel=False)
+        nuke.tprint('ending Provider.method')
+
+
+if __name__ == '__main__':
+    for klass in (Inheritor, Replacer, Extender):
+        print('\n' + klass.__name__ + '...')
+        klass().method()
+    print('\nProvider...')
+    x = Provider()
+    x.delegate()
+
+
