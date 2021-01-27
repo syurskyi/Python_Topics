a5e214adcdafe81efbe31bc6eaf155dfe6c2aa10
Grey <sergejyurskyj@yahoo.com>
2021-01-27 11:15:34 -0800

    Committed FibonacciHead exercises

--- 125_algorithms/_exercises/exercises/Recursion, Backtracking and Dynamic Programming in Python/Section 2 Recursion/FibonacciHead.py
+++ 125_algorithms/_exercises/exercises/Recursion, Backtracking and Dynamic Programming in Python/Section 2 Recursion/FibonacciHead.py
@@ -1,21 +1,20 @@
-#
-# ___ fibonacci_head n
-#
-#     __ ? __ 0
-#         r_ 0
-#     __ ? __ 1
-#         r_ 1
-#
-#     # we make the recursive function call(s)
-#     # we are going to do 2 recursion - we keep calculating the fibonacci numbers
-#     # some values are calculate twice - there are multiple stack frames with the same value
-#     fib1 = ? ?-1
-#     fib2 = ? ?-2
-#
-#     # make some operations
-#     result = ? + ?
-#
-#     r_ ?
-#
-#
-# print ? 20
+
+def fibonacci_head(n):
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    # we make the recursive function call(s)
+    # we are going to do 2 recursion - we keep calculating the fibonacci numbers
+    # some values are calculate twice - there are multiple stack frames with the same value
+    fib1 = fibonacci_head(n-1)
+    fib2 = fibonacci_head(n-2)
+
+    # make some operations
+    result = fib1 + fib2
+    return result
+
+
+print(fibonacci_head(20))
