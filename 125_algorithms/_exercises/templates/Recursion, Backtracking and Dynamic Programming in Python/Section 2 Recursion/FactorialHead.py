7d55103627c718630b78bfa2c84ad435a6a41af8
Grey <sergejyurskyj@yahoo.com>
2021-01-26 21:15:38 -0800

    Committed Section 2 Recursion template

--- 125_algorithms/_exercises/templates/Recursion, Backtracking and Dynamic Programming in Python/Section 2 Recursion/FactorialHead.py
+++ 125_algorithms/_exercises/templates/Recursion, Backtracking and Dynamic Programming in Python/Section 2 Recursion/FactorialHead.py
@@ -1,24 +1,24 @@
-
-___ factorial_head(n
-
-    # this is the base case - 0!=1
-    __ n __ 0:
-        r_ 1
-
-    # use recursion
-    result1 = factorial_head(n-1)
-
-    # we do some operations
-    result2 = n * result1
-    r_ result2
-
-
-___ factorial_tail(n, accumulator=1
-
-    __ n __ 1:
-        r_ accumulator
-
-    r_ factorial_tail(n-1, n * accumulator)
-
-
-print(factorial_tail(4))
+#
+# ___ factorial_head n
+#
+#     # this is the base case - 0!=1
+#     __ ? __ 0
+#         r_ 1
+#
+#     # use recursion
+#     result1 = ? ?-1
+#
+#     # we do some operations
+#     result2 = ? * _1
+#     r_ ?
+#
+#
+# ___ factorial_tail n accumulator=1
+#
+#     __ ? __ 1
+#         r_ a..
+#
+#     r_ ? ?-1 ? * a__
+#
+#
+# print ? 4
--- 125_algorithms/_exercises/templates/Recursion, Backtracking and Dynamic Programming in Python/Section 2 Recursion/HeadTailRecursion.py
+++ 125_algorithms/_exercises/templates/Recursion, Backtracking and Dynamic Programming in Python/Section 2 Recursion/HeadTailRecursion.py
@@ -1,33 +1,33 @@
-
-___ tail(n
-
-    print('Calling tail with n=' + st.(n))
-
-    # BASE CASE
-    __ n __ 0:
-        r_
-
-    # first of all we do some operations
-    # operation = print()
-    print(n)
-
-    # make the recursive function call
-    tail(n-1)
-
-
-___ head(n
-
-    print('Calling head() with n=' + st.(n))
-
-    __ n __ 0:
-        r_
-
-    # we make the recursive function call
-    head(n-1)
-
-    # we can do any operations
-    # operation - print()
-    print(n)
-
-
-head(5)
+#
+# ___ tail n
+#
+#     print('Calling tail with n=' + st. ?
+#
+#     # BASE CASE
+#     __ ? __ 0
+#         r_
+#
+#     # first of all we do some operations
+#     # operation = print()
+#     print ?
+#
+#     # make the recursive function call
+#     ? ? - 1
+#
+#
+# ___ head n
+#
+#     print('Calling head() with n=' + st. ?
+#
+#     __ n __ 0
+#         r_
+#
+#     # we make the recursive function call
+#     ? ? - 1
+#
+#     # we can do any operations
+#     # operation - print()
+#     print ?
+#
+#
+# head(5)
