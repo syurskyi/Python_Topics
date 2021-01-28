b562ef0a331026c764dd37c84dd0ac249f728c94
Grey <sergejyurskyj@yahoo.com>
2021-01-27 20:59:24 -0800

    Committed Memoization exercises

--- 125_algorithms/003_Recursion/_exercises/exercises/15 Recursion/Recursion Homework Example Problems - SOLUTIONS.py
+++ 125_algorithms/003_Recursion/_exercises/exercises/15 Recursion/Recursion Homework Example Problems - SOLUTIONS.py
@@ -9,23 +9,23 @@
 # # This problem is very similar to the factorial problem presented during the introduction to recursion. Remember,
 # # always think of what the base case will look like. In this case, we have a base case of n =0 (Note, you could have
 # # also designed the cut off to be 1).
-# #
-# # In this case, we have: n + (n-1) + (n-2) + .... + 0
-# #
-# # Check out a sample solution:
 #
-# ___ rec_sum n
+# In this case, we have: n + (n-1) + (n-2) + .... + 0
 #
-#     # Base Case
-#     __ ? __ 0
-#         r_ 0
-#
-#     # Recursion
-#     ____
-#         r_ ? + ? ?-1
-#
-# ?(4)
-# # 10
+# Check out a sample solution:
+
+def rec_sum(n):
+
+    # Base Case
+    if n == 0:
+        return 0
+
+    # Recursion
+    else:
+        return n + rec_sum(n-1)
+
+print(rec_sum(4))
+# 10
 # #
 # # Problem 2
 # # Given an integer, create a function which returns the sum of all the individual digits in that integer.
@@ -69,36 +69,36 @@
 # word_split('themanran',['clown','ran','man'])
 # # []
 # #
-# ___ word_split phrase list_of_words output _ N..
-#     '''
-#     Note: This is a very "python-y" solution.
-#     '''
-#
-#     # Checks to see if any output has been initiated.
-#     # If you default output=[], it would be overwritten for every recursion!
-#     __ o.. __ N..
-#         o... _    # list
-#
-#     # For every word in list
-#     ___ word __ l..
-#
-#         # If the current phrase begins with the word, we have a split point!
-#         __ phrase.s_s_ w..
-#
-#             # Add the word to the output
-#             o__.ap.. w..
-#
-#             # Recursively call the split function on the remaining portion of the phrase--- phrase[len(word):]
-#             # Remember to pass along the output and list of words
-#             r_ w..  ph.. |le. w.. ;| l.. o..
-#
-#     # Finally r_ output if no phrase.startswith(word) returns True
-#     r_ o..
-#
-# word_split('themanran',['the','ran','man'])
+def word_split(phrase, list_of_words, output = None):
+    '''
+    Note: This is a very "python-y" solution.
+    '''
+
+    # Checks to see if any output has been initiated.
+    # If you default output=[], it would be overwritten for every recursion!
+    if output == None:
+        output = []    # list
+
+    # For every word in list
+    for word in list_of_words:
+
+        # If the current phrase begins with the word, we have a split point!
+        if phrase.startswith(word):
+
+            # Add the word to the output
+            output.append(word)
+
+            # Recursively call the split function on the remaining portion of the phrase--- phrase[len(word):]
+            # Remember to pass along the output and list of words
+            return word_split(phrase[len(word):], list_of_words, output)
+
+    # Finally r_ output if no phrase.startswith(word) returns True
+    return output
+
+print(word_split('themanran',['the','ran','man']))
 # # ['the', 'man', 'ran']
 # #
-# word_split('ilovedogsJohn',['i','am','a','dogs','lover','love','John'])
+word_split('ilovedogsJohn',['i','am','a','dogs','lover','love','John'])
 # # ['i', 'love', 'dogs', 'John']
 # #
 # word_split('themanran',['clown','ran','man'])
