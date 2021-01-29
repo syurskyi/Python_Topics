4d92a4055abceac7c01686e2e3cefc6500bcfce3
Grey <sergejyurskyj@yahoo.com>
2021-01-28 20:22:37 -0800

    Committed isPalindrome exercises

--- 125_algorithms/_exercises/exercises/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/isPalindrome.py
+++ 125_algorithms/_exercises/exercises/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/isPalindrome.py
@@ -1,18 +1,18 @@
-# #   Created by Elshad Karimov
-# #   Copyright © AppMillers. All rights reserved.
-#
-# # isPalindrome Solution
-#
-#
-# ___ isPalindrome strng
-#     __ le_ ? __ 0
-#         r_ T..
-#     __ ? 0 !_ ? le_ ? - 1
-#         r_ F..
-#     r_ ? ? 1| - 1
-#
-# print(isPalindrome('awesome')) # false
-# print(isPalindrome('foobar')) # false
-# print(isPalindrome('tacocat')) # true
-# print(isPalindrome('amanaplanacanalpanama')) # true
-# print(isPalindrome('amanaplanacanalpandemonium')) # false
+#   Created by Elshad Karimov
+#   Copyright © AppMillers. All rights reserved.
+
+# isPalindrome Solution
+
+
+def isPalindrome(strng):
+    if len(strng) == 0:
+        return True
+    if strng[0] != strng[len(strng) - 1]:
+        return False
+    return isPalindrome(strng[1: -1])
+
+print(isPalindrome('awesome')) # false
+print(isPalindrome('foobar')) # false
+print(isPalindrome('tacocat')) # true
+print(isPalindrome('amanaplanacanalpanama')) # true
+print(isPalindrome('amanaplanacanalpandemonium')) # false
