5bdf507a8876680d87c4082b44c93e8d1217799d
Grey <sergejyurskyj@yahoo.com>
2021-01-29 10:06:38 -0800

    Committed fibonaci_solution examples

--- 125_algorithms/_exercises/exercises/Python_Hand-on_Solve_200_Problems/Section 17 Recursion/fibonaci_solution.py
+++ 125_algorithms/_exercises/exercises/Python_Hand-on_Solve_200_Problems/Section 17 Recursion/fibonaci_solution.py
@@ -1,14 +1,14 @@
-# # To add a new cell, type '# %%'
-# # To add a new markdown cell, type '# %% [markdown]'
-# # %%
-# #  Write a Python program to solve the Fibonacci sequence using recursion.
-#
-# ___ fibonacci n
-#   __ ? __ 1 o. ? __ 2
-#     r_ 1
-#   ____
-#     r_  ? ? - 1 +  ? ? - 2
-#
-# print ? 7
-#
-#
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+#  Write a Python program to solve the Fibonacci sequence using recursion.
+
+def fibonacci(n):
+  if n == 1 or n == 2:
+    return 1
+  else:
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(7))
+
+
