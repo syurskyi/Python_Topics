42ea95a80372a3911a82888f7deed1313f09156a
Grey <sergejyurskyj@yahoo.com>
2021-01-29 09:48:49 -0800

    Committed stringfyiNumber examples

--- 125_algorithms/_exercises/exercises/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/stringfyiNumber.py
+++ 125_algorithms/_exercises/exercises/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/stringfyiNumber.py
@@ -1,36 +1,36 @@
-# #   Created by Elshad Karimov
-# #   Copyright © AppMillers. All rights reserved.
-#
-# # stringifyNumbers Solution
-#
-# ___ stringifyNumbers obj
-#     newObj _ ?
-#     ___ key __ ?
-#         __ ty.. ? ? __ in.
-#             ? ? _ st. ? ?
-#         __ ty.. ? ? __ di..
-#             ? ? _ ? ? ?
-#     r_ ?
-#
-#
-#
-# obj = {
-#   "num": 1,
-#   "test": [],
-#   "data": {
-#     "val": 4,
-#     "info": {
-#       "isRight": T..,
-#       "random": 66
-#     }
-#   }
-# }
-#
-# print(stringifyNumbers(obj))
-#
-# {'num': '1',
-#  'test': [],
-#  'data': {'val': '4',
-#           'info': {'isRight': T.., 'random': '66'}
-#           }
-# }
+#   Created by Elshad Karimov
+#   Copyright © AppMillers. All rights reserved.
+
+# stringifyNumbers Solution
+
+def stringifyNumbers(obj):
+    newObj = obj
+    for key in obj:
+        if type(newObj[key]) is int:
+            newObj[key] = str(newObj[key])
+        if type(newObj[key]) == dict:
+            newObj[key] = stringifyNumbers(newObj[key])
+    return newObj
+
+
+
+obj = {
+  "num": 1,
+  "test": [],
+  "data": {
+    "val": 4,
+    "info": {
+      "isRight": True,
+      "random": 66
+    }
+  }
+}
+
+print(stringifyNumbers(obj))
+
+{'num': '1',
+ 'test': [],
+ 'data': {'val': '4',
+          'info': {'isRight': True, 'random': '66'}
+          }
+}
