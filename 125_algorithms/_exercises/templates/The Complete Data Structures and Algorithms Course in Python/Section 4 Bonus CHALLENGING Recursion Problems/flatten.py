cd0c66ea00194b6225272ef2f0edcfeeac72e05b
Grey <sergejyurskyj@yahoo.com>
2021-01-28 18:02:58 -0800

    Committed flatten templates

--- 125_algorithms/_exercises/templates/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/flatten.py
+++ 125_algorithms/_exercises/templates/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/flatten.py
@@ -1,19 +1,19 @@
-#   Created by Elshad Karimov 
-#   Copyright © AppMillers. All rights reserved.
-
-# flatten Solution
-___ flatten(arr
-    resultArr = []
-    ___ custItem __ arr:
-        __ ty..(custItem) __ li__:
-            resultArr.ex..(flatten(custItem))
-        ____
-            resultArr.ap..(custItem)
-    r_ resultArr 
-
-
-
-print(flatten([1, 2, 3, [4, 5]])) # [1, 2, 3, 4, 5]
-print(flatten([1, [2, [3, 4], [[5]]]])) # [1, 2, 3, 4, 5]
-print(flatten([[1], [2], [3]])) # [1, 2, 3]
-print(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) # [1, 2, 3]
+# #   Created by Elshad Karimov
+# #   Copyright © AppMillers. All rights reserved.
+#
+# # flatten Solution
+# ___ flatten arr
+#     resultArr _ # list
+#     ___ custItem __ ?
+#         __ ty.. c.. __ li__
+#             ?.ex.. ? ?
+#         ____
+#             ?.ap.. c..
+#     r_ ?
+#
+#
+#
+# print(flatten([1, 2, 3, [4, 5]])) # [1, 2, 3, 4, 5]
+# print(flatten([1, [2, [3, 4], [[5]]]])) # [1, 2, 3, 4, 5]
+# print(flatten([[1], [2], [3]])) # [1, 2, 3]
+# print(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]])) # [1, 2, 3]
