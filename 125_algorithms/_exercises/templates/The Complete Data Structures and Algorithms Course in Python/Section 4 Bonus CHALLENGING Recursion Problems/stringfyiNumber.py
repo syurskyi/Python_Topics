94b6ed617d8134384f5270fd8b1d59f2307cc9fe
Grey <sergejyurskyj@yahoo.com>
2021-01-28 18:12:07 -0800

    Committed stringfyiNumber templates

--- 125_algorithms/_exercises/templates/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/stringfyiNumber.py
+++ 125_algorithms/_exercises/templates/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/stringfyiNumber.py
@@ -1,36 +1,36 @@
-#   Created by Elshad Karimov 
-#   Copyright © AppMillers. All rights reserved.
-
-# stringifyNumbers Solution
-
-___ stringifyNumbers(obj
-    newObj = obj
-    ___ key __ newObj:
-        __ ty..(newObj[key]) __ in.:
-            newObj[key] = st.(newObj[key])
-        __ ty..(newObj[key]) __ dict:
-            newObj[key] = stringifyNumbers(newObj[key])
-    r_ newObj
-
-
-
-obj = {
-  "num": 1,
-  "test": [],
-  "data": {
-    "val": 4,
-    "info": {
-      "isRight": T..,
-      "random": 66
-    }
-  }
-}
-
-print(stringifyNumbers(obj))
-
-{'num': '1', 
- 'test': [], 
- 'data': {'val': '4', 
-          'info': {'isRight': T.., 'random': '66'}
-          }
-}
+# #   Created by Elshad Karimov
+# #   Copyright © AppMillers. All rights reserved.
+#
+# # stringifyNumbers Solution
+#
+# ___ stringifyNumbers obj
+#     newObj _ ?
+#     ___ key __ ?
+#         __ ty.. ? ? __ in.
+#             ? ? _ st. ? ?
+#         __ ty.. ? ? __ di..
+#             ? ? _ ? ? ?
+#     r_ ?
+#
+#
+#
+# obj = {
+#   "num": 1,
+#   "test": [],
+#   "data": {
+#     "val": 4,
+#     "info": {
+#       "isRight": T..,
+#       "random": 66
+#     }
+#   }
+# }
+#
+# print(stringifyNumbers(obj))
+#
+# {'num': '1',
+#  'test': [],
+#  'data': {'val': '4',
+#           'info': {'isRight': T.., 'random': '66'}
+#           }
+# }
