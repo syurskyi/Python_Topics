b0c09c015f71358dd7d7fadab17ae26953b161c5
Grey <sergejyurskyj@yahoo.com>
2021-01-28 20:05:06 -0800

    Committed captalizeFirst exercises

--- 125_algorithms/_exercises/exercises/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/captalizeFirst.py
+++ 125_algorithms/_exercises/exercises/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/captalizeFirst.py
@@ -1,16 +1,16 @@
-# #   Created by Elshad Karimov
-# #   Copyright © AppMillers. All rights reserved.
-#
-# # capitalizeFirst Solution
-#
-# ___ capitalizeFirst arr
-#     result _ # list
-#     __ le_ ? __ 0
-#         r_ ?
-#     ?.ap.. ? 0 0.u.. + ? 0 1|
-#     r_ ? + ? ? 1|
-#
-#
-#
-#
-# print ? 'car', 'taco', 'banana' # ['Car','Taco','Banana']
+#   Created by Elshad Karimov
+#   Copyright © AppMillers. All rights reserved.
+
+# capitalizeFirst Solution
+
+def capitalizeFirst(arr):
+    result = [] # list
+    if len(arr) == 0:
+        return result
+    result.append(arr[0][0].upper() + arr[0][1:])
+    return result + capitalizeFirst(arr[1:])
+
+
+
+
+print(capitalizeFirst(['car', 'taco', 'banana'])) # ['Car','Taco','Banana']
