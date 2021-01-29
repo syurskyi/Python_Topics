637a9f96430521605dec48f38f9b736064b76cf3
Grey <sergejyurskyj@yahoo.com>
2021-01-28 18:00:31 -0800

    Committed collectString templates

--- 125_algorithms/_exercises/templates/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/collectString.py
+++ 125_algorithms/_exercises/templates/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/collectString.py
@@ -1,33 +1,33 @@
-#   Created by Elshad Karimov 
-#   Copyright © AppMillers. All rights reserved.
-
-# collectStrings Solution
-
-___ collectStrings(obj
-    resultArr = []
-    ___ key __ obj:
-        __ ty..(obj[key]) __ st.:
-            resultArr.ap..(obj[key])
-        __ ty..(obj[key]) __ dict:
-            resultArr = resultArr + collectStrings(obj[key])
-    r_ resultArr
-
-
-
-obj = {
-  "stuff": 'foo',
-  "data": {
-    "val": {
-      "thing": {
-        "info": 'bar',
-        "moreInfo": {
-          "evenMoreInfo": {
-            "weMadeIt": 'baz'
-          }
-        }
-      }
-    }
-  }
-}
-
-print(collectStrings(obj)) # ['foo', 'bar', 'baz'])
+# #   Created by Elshad Karimov
+# #   Copyright © AppMillers. All rights reserved.
+#
+# # collectStrings Solution
+#
+# ___ collectStrings obj
+#     resultArr _  # list
+#     ___ key __ ?:
+#         __ ty..(? ? __ st.
+#             r__.ap.. ? ?
+#         __ ty.. ? ? __ di..
+#             r.. _ r.. + ? ? ?
+#     r_ ?
+#
+#
+#
+# obj = {
+#   "stuff": 'foo',
+#   "data": {
+#     "val": {
+#       "thing": {
+#         "info": 'bar',
+#         "moreInfo": {
+#           "evenMoreInfo": {
+#             "weMadeIt": 'baz'
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+#
+# print ? ? # ['foo', 'bar', 'baz'])
