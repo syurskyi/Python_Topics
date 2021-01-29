533234af9d7b2c4d5510dc07d0992364ea2714c3
Grey <sergejyurskyj@yahoo.com>
2021-01-28 17:56:52 -0800

    Committed capitalizeWords templates

--- 125_algorithms/_exercises/templates/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/capitalizeWords.py
+++ 125_algorithms/_exercises/templates/The Complete Data Structures and Algorithms Course in Python/Section 4 Bonus CHALLENGING Recursion Problems/capitalizeWords.py
@@ -1,16 +1,16 @@
-#   Created by Elshad Karimov 
-#   Copyright © AppMillers. All rights reserved.
-
-# capitalizeWords Solution
-
-___ capitalizeWords(arr
-    result = []
-    __ le_(arr) __ 0:
-        r_ result
-    result.ap..(arr[0].upper())
-    r_ result + capitalizeWords(arr[1:])
-
-
-
-words = ['i', 'am', 'learning', 'recursion']
-print(capitalizeWords(words)) # ['I', 'AM', 'LEARNING', 'RECURSION']
+# #   Created by Elshad Karimov
+# #   Copyright © AppMillers. All rights reserved.
+#
+# # capitalizeWords Solution
+#
+# ___ capitalizeWords arr
+#     result _  # list
+#     __ le_ ? __ 0
+#         r_ ?
+#     ?.ap.. ? 0 .u..
+#     r_ ? + ? ? 1|
+#
+#
+#
+# words = ['i', 'am', 'learning', 'recursion']
+# print ? ? # ['I', 'AM', 'LEARNING', 'RECURSION']
