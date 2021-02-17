39e700fa447d33bf9c6f7737e914205b6e2e9ab3
Grey <sergejyurskyj@yahoo.com>
2021-02-17 10:13:11 -0800

    Committed lambda exercises

--- 045_functions/007_lambda/_exercises/exercises/004_Closures.py
+++ 045_functions/007_lambda/_exercises/exercises/004_Closures.py
@@ -1,25 +1,25 @@
 # -*- coding: utf-8 -*-
 
-# ___ outer_func x
-#      y _ 4
-#      ___ inner_func z
-#          print _*x _ x| y _ y| z _ z|*|
-#          r_ x + y + z
-#      r_ ?
-#
-# ___ i __ ra.. 3
-#     closure _ ? i
-#     print |_*closure||i+5|| _ |closure|i+5||*|
-#
-# # Точно так же лямбда также может быть замыканием. Вот тот же пример с лямбда-функцией Python:
-# print('#' * 52 + '  ')
-# print('#' * 52 + '  ')
-# print('#' * 52 + '  ')
-#
-# ___ outer_func x
-#     y _ 4
-#     r_ l_____ z x + y + z
-# ___ i __ ra00 3
-#     closure _ ? i
-#     print|_*closure||i+5|| _ |closure|i+5||*|
-#
+def outer_func(x):
+     y = 4
+     def inner_func(z):
+         print(f"x = {x}, y = {y}, z = {z}")
+         return x + y + z
+     return inner_func
+
+for i in range(3):
+    closure = outer_func(i)
+    print(f'closure({i+5}) = {closure(i+5)}')
+
+# Точно так же лямбда также может быть замыканием. Вот тот же пример с лямбда-функцией Python:
+print('#' * 52 + '  ')
+print('#' * 52 + '  ')
+print('#' * 52 + '  ')
+
+def outer_func(x):
+    y = 4
+    return lambda z: x + y + z
+for i in range(3):
+    closure = outer_func(i)
+    print(f'closure({i+5}) = {closure(i+5)}')
+
