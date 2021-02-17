ca6fbf748988373598c86ae3a4d46719809575e6
Grey <sergejyurskyj@yahoo.com>
2021-02-17 09:55:05 -0800

    Committed lambda exercises

--- 045_functions/007_lambda/_exercises/templates/002_Arguments_!cool!.py
+++ 045_functions/007_lambda/_exercises/templates/002_Arguments_!cool!.py
@@ -1,12 +1,12 @@
-# print l_____ x y z x + y + z||1 2 3
+# print l_____ x y z ? + ? + ? | 1 2 3
 # # 6
-# print l_____ x y z_3 x + y + z | 1 2
+# print l_____ x y z_3 ? + ? + ? | 1 2
 # # 6
-# print l_____ x y z_3 x + y + z | 1 y_2
+# print l_____ x y z_3 ? + ? + ? | 1 y_2
 # # 6
-# print l_____ $ su.|assert | 1 2 3
+# print l_____ $ su.|? | 1 2 3
 # # 6
-# print l_____ $$ sukw..va.. | one_1 two_2 three_3
+# print l_____ $$ su. kw__.va.. | one_1 two_2 three_3
 # # 6
-# print l_____ x `, y_0 z_0 x + y + z |1 y_2 z_3
+# print l_____ x ` y_0 z_0 ? + ? + ? |1 y_2 z_3
 # # 6
