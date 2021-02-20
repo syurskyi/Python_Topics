e4f0e0b73d0a9dd21ab3c219951c90da24945701
Grey <sergejyurskyj@yahoo.com>
2021-02-20 10:17:09 -0800

    Committed filter exercises

--- 045_functions/008_built_in_functions/filter/_exercises/_templates/003_Python filter() Function.py
+++ 045_functions/008_built_in_functions/filter/_exercises/_templates/003_Python filter() Function.py
@@ -1,13 +1,13 @@
-ages = [5, 12, 17, 18, 24, 32]
-
-def myFunc(x):
-  if x < 18:
-    return False
-  else:
-    return True
-
-adults = filter(myFunc, ages)
-
-for x in adults:
-  print(x)
-
+# ages _ |5, 12, 17, 18, 24, 32|
+#
+# ___ myFunc x
+#   __ x < 18
+#     r_ F..
+#   ....
+#     r... T...
+#
+# adults _ f... ? a..
+#
+# ___ x __ a..
+#   print ?
+#
--- 045_functions/008_built_in_functions/filter/_exercises/_templates/004_.py
+++ 045_functions/008_built_in_functions/filter/_exercises/_templates/004_.py
@@ -1,7 +1,7 @@
 # # Python 3
 # scores _ |66, 90, 68, 59, 76, 60, 88, 74, 81, 65|
 #
-# _ is_A_student score
+# ___ is_A_student score
 #     r___ ? > 75
 #
 # over_75 _ l...|f....|? s..
