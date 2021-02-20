b88a0957845eb16c5f67d378ccf034fe6c73920c
Grey <sergejyurskyj@yahoo.com>
2021-02-20 12:32:26 -0800

    Committed map exercises

--- 045_functions/008_built_in_functions/map/__for_nuke/001_Map.py
+++ 045_functions/008_built_in_functions/map/__for_nuke/001_Map.py
@@ -1,5 +1,5 @@
 my_pets = ['alfred', 'tabitha', 'william', 'arla']
-uppered_pets = []    # list
+uppered_pets = []
 
 for pet in my_pets:
     pet_ = pet.upper()
@@ -16,13 +16,13 @@ print(uppered_pets)
 
 circle_areas = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.00013]
 
-result = list(map(round, circle_areas, range(1, 7)))
+result = list(map(round, circle_areas, range(1,7)))
 
 print(result)
 
 circle_areas = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.00013]
 
-result = list(map(round, circle_areas, range(1, 3)))
+result = list(map(round, circle_areas, range(1,3)))
 
 print(result)
 
