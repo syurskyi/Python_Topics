3f2f0099e31fbd6cb3fd8b0e38b2225b6d653c4a
Grey <sergejyurskyj@yahoo.com>
2021-02-20 13:24:44 -0800

    Committed range exercises

---
+++ 030_control_flow/004_range/_exercises/001.py
@@ -0,0 +1,58 @@
+captains = ['Janeway', 'Picard', 'Sisko']
+
+for captain in captains:
+    print(captain)
+
+# Janeway
+# Picard
+# Sisko
+
+numbers_divisible_by_three = [3, 6, 9, 12, 15]
+
+for num in numbers_divisible_by_three:
+    quotient = num / 3
+    print(f"{num} divided by 3 is {int(quotient)}")
+
+# 3 divided by 3 is 1.
+# 6 divided by 3 is 2.
+# 9 divided by 3 is 3.
+# 12 divided by 3 is 4.
+# 15 divided by 3 is 5.
+
+for i in range(3, 16, 3):
+    quotient = i / 3
+    print(f'{i} divided by 3 is {int(quotient)}')
+
+
+
+captains = ['Janeway', 'Picard', 'Sisko']
+
+for i in range(len(captains)):
+    print(captains[i])
+
+# Incrementing With ra__()
+
+# ___ i __ ra__3, 100, 25
+#     print ?
+#
+# # 3
+# # 28
+# # 53
+# # 78
+#
+# # Decrementing With ra__()
+#
+# ___ i __ ra__ 10, -6, -2
+#     print ?
+#
+# # 10
+# # 8
+# # 6
+# # 4
+# # 2
+# # 0
+# # -2
+# # -4
+#
+# ___ i __ re__ ra__ 5
+#     print ?
---
+++ 030_control_flow/004_range/_exercises/_templates/001.py
@@ -0,0 +1,58 @@
+# captains _ ['Janeway', 'Picard', 'Sisko']
+#
+# ___ captain __ ?
+#     print ?
+#
+# # Janeway
+# # Picard
+# # Sisko
+#
+# numbers_divisible_by_three _ [3, 6, 9, 12, 15]
+#
+# ___ num __ ?
+#     quotient _ ? / 3
+#     print _*|? divided by 3 is |in. ?.
+#
+# # 3 divided by 3 is 1.
+# # 6 divided by 3 is 2.
+# # 9 divided by 3 is 3.
+# # 12 divided by 3 is 4.
+# # 15 divided by 3 is 5.
+#
+# ___ i __ ra__ 3, 16, 3
+#     quotient _ ? / 3
+#     print _* |? divided by 3 is |in. ?.
+#
+#
+#
+# captains _ ['Janeway', 'Picard', 'Sisko']
+#
+# ___ i __ ra__ le_ ?
+#     print ?|?
+#
+# # Incrementing With ra__()
+#
+# ___ i __ ra__3, 100, 25
+#     print ?
+#
+# # 3
+# # 28
+# # 53
+# # 78
+#
+# # Decrementing With ra__()
+#
+# ___ i __ ra__ 10, -6, -2
+#     print ?
+#
+# # 10
+# # 8
+# # 6
+# # 4
+# # 2
+# # 0
+# # -2
+# # -4
+#
+# ___ i __ re__ ra__ 5
+#     print ?
