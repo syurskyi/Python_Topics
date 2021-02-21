dfa77d41118d12b3909e325eeb27a504cc608888
Grey <sergejyurskyj@yahoo.com>
2021-02-21 13:11:38 -0800

    Committed dictionary comprehension template

--- 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/006.py
+++ 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/006.py
@@ -1,28 +1,34 @@
-## dictionaries comprehensions
-
-square = {f"Square of {num} is":num**2 ___ num __ ra..(1, 11)}
-___ k, v __ square.i..:
-    print(f"{k} : {v}")
-
-string = "AnoopSingh"
-
-word_count = {char: string.count(char) ___ char __ string}
-print(word_count)
-
-___ k, v __ word_count.i..:
-    print(f"{k} : {v}")
-
-
-odd_even = {i :("even" __ i%2__0 ____ "odd") ___ i __ ra..(1, 11)}
-___ k, v __ odd_even.i..:\
-    print(f"{k} : {v}")
-
-###
-
-##This is sets comprehensions
-
-num = {k**2 ___ k __ ra..(1, 11)}
-print(num)
-
-num = {k**2 ___ k __ ra..(1, 11)}
-print(num)
+# ## dictionaries comprehensions
+#
+# square = _*Square of ? is*:num**2 ___ ? __ ra.. 1 11
+# # # k, v
+# ___ ? ? __ ?.i..
+#     print _*? : ?
+#
+# string = "AnoopSingh"
+#
+# # char
+# word_count = ? ?.co.. ? ___ ? __ ?
+# print ?
+#
+# # # k, v
+# ___ ? ? __ ?.i..
+#     print _* ? : ?
+#
+# # i
+# odd_even = ? *even __ ?%2__0 ____ *odd ___ ? __ ra.. 1 11
+# # k, v
+# ___ ? ? __ ?.i..
+#     print _* ? : ?
+#
+# ###
+#
+# ##This is sets comprehensions
+#
+# # k
+# num = ?**2 ___ ? __ ra.. 1 11
+# print ?
+#
+# # k
+# num = ?**2 ___ ? __ ra.. 1 11
+# print ?
