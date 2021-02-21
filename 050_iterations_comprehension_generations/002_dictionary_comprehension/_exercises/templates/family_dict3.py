9860029314f35b9b1642edd83f07fabdc7537894
Grey <sergejyurskyj@yahoo.com>
2021-02-21 13:33:10 -0800

    Committed dictionary comprehension template

--- 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/family_dict3.py
+++ 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/family_dict3.py
@@ -1,27 +1,28 @@
-my_family = {
-  "sister": {
-    "name": "Suzy",
-    "age": 32
-  },
-  "father": {
-    "name": "John",
-    "age": 55
-  },
-  "brother": {
-    "name": "Billy",
-    "age": 21
-  }
-}
-
-family_stuff = set()
-
-___ family_member, member_values __ my_family.i..:
-  family_stuff.add(f'{member_values["name"]} is my {family_member} and is {st_(member_values["age"])} years old.')
-
-print(family_stuff)
-
-more_family_stuff = {f'{member_values["name"]} is my {family_member} and is {st_(member_values["age"])} years old.'
-  ___ (family_member, member_values) __ my_family.i..
-  }
-
-print(more_family_stuff)
+# my_family = {
+#   "sister": {
+#     "name": "Suzy",
+#     "age": 32
+#   },
+#   "father": {
+#     "name": "John",
+#     "age": 55
+#   },
+#   "brother": {
+#     "name": "Billy",
+#     "age": 21
+#   }
+# }
+#
+# family_stuff = se.
+#
+# # family_member, member_values
+# ___ ? ? __ ?.i..
+#   ?.a.. _* ? *n.. is my ? and is  st_ ? *a.. years old.
+#
+# print ?
+#
+# more_family_stuff = _* ? *n.. is my ? and is  st_ ? *a.. years old.
+#   ___ ? ? __ ?.i..
+#
+#
+# print ?
