ed605933fa8b797b0a7c567a1f68f0b4dd84cb3e
Grey <sergejyurskyj@yahoo.com>
2021-02-21 13:28:11 -0800

    Committed dictionary comprehension template

--- 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/family_dict.py
+++ 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/family_dict.py
@@ -1,6 +1,7 @@
-my_family = {'sister': {'name': 'Krista', 'age': 42}, 'mother': {'name': 'Cathie', 'age': 70}}
-
-___ key, value __ my_family.i..:
-   family = {value['name']+" is my "+key+" and is "+st_(value['age'])+" years old." ___ key, value __ my_family.i..}
-   print(family)
-# {'Krista is my sister and is 42 years old.', 'Cathie is my mother and is 70 years old.'}
+# my_family = {'sister': {'name': 'Krista', 'age': 42}, 'mother': {'name': 'Cathie', 'age': 70}}
+#
+# # key, value
+# ___ ? ? __ ?.i..
+#    family =  ?|*name +" is my "+?+" and is "+st_ ?|*age +" years old." ___ ? ? __ ?.i..
+#    print ?
+# # {'Krista is my sister and is 42 years old.', 'Cathie is my mother and is 70 years old.'}
