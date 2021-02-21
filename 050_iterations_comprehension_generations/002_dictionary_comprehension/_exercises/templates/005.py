9ea34d49946ef553a7586201f31fb2836f7c535e
Grey <sergejyurskyj@yahoo.com>
2021-02-21 13:07:10 -0800

    Committed dictionary comprehension template

--- 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/005.py
+++ 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/005.py
@@ -1,14 +1,15 @@
-weather_c = {
-    "Monday": 12,
-    "Tuesday": 14,
-    "Wednesday": 15,
-    "Thursday": 14,
-    "Friday": 21,
-    "Saturday": 22,
-    "Sunday": 24,
-}
-
-weather_f = {day: temp_c*9/5 + 32 ___ (day, temp_c) __ weather_c.i..}
-
-
-print(weather_f)
+# weather_c = {
+#     "Monday": 12,
+#     "Tuesday": 14,
+#     "Wednesday": 15,
+#     "Thursday": 14,
+#     "Friday": 21,
+#     "Saturday": 22,
+#     "Sunday": 24,
+# }
+#
+# # day, temp_c
+# weather_f = ? ?*9/5 + 32 ___ ? ? __ ?.i..
+#
+#
+# print ?
