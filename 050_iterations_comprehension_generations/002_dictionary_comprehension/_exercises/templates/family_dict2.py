a929fccd0a30e0eea792f9d9286b63198aa3797a
Grey <sergejyurskyj@yahoo.com>
2021-02-21 13:29:12 -0800

    Committed dictionary comprehension template

--- 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/family_dict2.py
+++ 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/family_dict2.py
@@ -1,21 +1,22 @@
-my_family = {
-    'sister': {
-        'name': 'Krista',
-        'age': 42
-    },
-    'mother': {
-        'name': 'Cathie',
-        'age': 70
-    },
-    'brother': {
-    	'name': 'Kevin',
-    	'age': 900
-    },
-    'cousin': {
-    	'name': 'Dragons',
-    	'age': 6
-    }
-}
-
-new_family = {st_(v['name']) + ' is my ' + k +' and is ' + st_(v['age']) ___ (k,v) __ my_family.i..}
-print(new_family)
+# my_family = {
+#     'sister': {
+#         'name': 'Krista',
+#         'age': 42
+#     },
+#     'mother': {
+#         'name': 'Cathie',
+#         'age': 70
+#     },
+#     'brother': {
+#     	'name': 'Kevin',
+#     	'age': 900
+#     },
+#     'cousin': {
+#     	'name': 'Dragons',
+#     	'age': 6
+#     }
+# }
+#
+# # v, k
+# new_family = {st_ ? *name + ' is my ' + ? +' and is ' + st_ ? *age ___ ? ? __ ?.i..
+# print ?
