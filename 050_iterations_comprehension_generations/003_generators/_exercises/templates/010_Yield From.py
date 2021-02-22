1002b72ced55105b7b87218febd70245e421e4cd
Grey <sergejyurskyj@yahoo.com>
2021-02-21 21:01:24 -0800

    Committed generators exercise

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/010_Yield From.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/010_Yield From.py
@@ -3,36 +3,41 @@
 # # All we have done here is create a generator  (iterator)  that can be used to iterate of the elements
 # # of a nested iterator.
 #
+# # row, item
 # ___ matrix_iterator n
-#     ___ row i_ matrix n
-#         ___ item i_ row
-#             y____ i...
+#     ___ ? __ matrix ?
+#         ___ ? __ row
+#             y____ ?
 #
-# ___ i i_ matrix_iterator 3
-#     print i
+# # i
+# ___ ? __ ? 3
+#     print ?
 #
 # # Yield From
 # # But we can avoid using that nested ___ loop by using a special form of yield: yield from
 #
+# # # row,
 # ___ matrix_iterator n
-#     ___ row i_ matrix n
-#         y____ f... row
+#     ___ ? __ matrix n
+#         y____ f... ?
 #
-# ___ i i_ matrix_iterator 3
-#     print i
+# # i
+# ___ ? __ ? 3
+#     print ?
 #
 # # Yield From
 # #
 # # we need to read car brands from multiple files to get it as a single collection.
 #
+# # f_name
 # ___ brands 0files
-#     ___ f_name i_ files
-#         w... open f_name  a. f
-#             y____ f... f
+#     ___ ? __ ?
+#         w... o.. ?  __ f
+#             y____ f... ?
 #
 # files _ 'car-brands-1.txt', 'car-brands-2.txt', 'car-brands-3.txt'
 #
-# ___ brand i_ brands 0f..
+# ___ brand __ brands 0f..
 #     print b... e.._', '
 #
 # # Yield From
@@ -45,20 +50,23 @@
 # # instead of directly from the file iterator:
 # # I want to point out that i_ this particular instance, we are using yield from as a simple replacement ___ a ___ loop
 #
+# # line
 # ___ gen_clean_read file
-#     w... open file  a. f
-#         ___ line i_ f
-#             y____ li__.st.. '\n'
+#     w... o.. ?  __ f
+#         ___ ? __ ?
+#             y____ ?.st.. '\n'
 #
-# f1 _ gen_clean_read 'car-brands-1.txt'
-# ___ line i_ f1:
-#     print li.. e.._', '
+# # line
+# f1 _ ? *car-brands-1.txt
+# ___ ? __ ?
+#     print ? e.._', '
 #
 # files _ 'car-brands-1.txt', 'car-brands-2.txt', 'car-brands-3.txt'
 #
+# # file
 # ___ brands 0f...
-#     ___ file i_ f...
-#         y____ f... gen_clean_read f..
+#     ___ ? __ f...
+#         y____ f... ? f..
 #
-# ___ brand i_ br.. 0f..
+# ___ brand __ br.. 0f..
 #     print b.. e.._', '
