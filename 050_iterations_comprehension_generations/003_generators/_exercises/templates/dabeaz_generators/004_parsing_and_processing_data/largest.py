d3b8a2faa592774aa4ff4f624c12cacfc3f5ebc1
Grey <sergejyurskyj@yahoo.com>
2021-02-22 13:43:23 -0800

    Committed generators exercises

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/004_parsing_and_processing_data/largest.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/004_parsing_and_processing_data/largest.py
@@ -1,12 +1,13 @@
-# largest.py
+# # largest.py
+# #
+# # Find the largest file
 #
-# Find the largest file
-
-f.. linesdir ______ lines_from_dir
-f.. apachelog ______ apache_log
-
-lines = lines_from_dir *a.. *w..
-log = apache_log(lines)
-
-print("%d %s" % max((r['bytes'],r['request'])
-                    ___ r __ log))
+# f.. linesdir ______ lines_from_dir
+# f.. apachelog ______ apache_log
+#
+# lines _ l.. *a.. *w..
+# log _ a.. ?
+#
+# # r
+# print("@ @"  ma. ? *bytes ? *request
+#                     ___ ? __ ?
