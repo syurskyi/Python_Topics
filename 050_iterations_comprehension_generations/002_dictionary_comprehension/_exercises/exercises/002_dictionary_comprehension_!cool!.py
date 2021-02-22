deeead148dcaea0131574aa191b620ed9b054d1a
Grey <sergejyurskyj@yahoo.com>
2021-02-21 20:28:14 -0800

    Committed dictionary comprehension exercise

--- 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/exercises/002_dictionary_comprehension_!cool!.py
+++ 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/exercises/002_dictionary_comprehension_!cool!.py
@@ -1,38 +1,38 @@
 # # Comprehension Syntax Summary
 # # Dictionary comprehension
-#
-# print x x * x ___ ? __ r... 10   # # Dictionary comprehension
+# # x
+# print ? ? * ? ___ ? __ r... 10   # # Dictionary comprehension
 #
 # # Comprehending Set and Dictionary Comprehensions
 # # Comprehension
 # # Generator and type name
 # #  Set comprehension equivalent
 # # Dict comprehension equivalent
-#
-# print x * x ___ ? __ r__ 10    # dict
-# se_ x * x ___ ? __ r__ 10    # not dict
-# print x x * x ___ ? __ r__ 10  # dict
-# print di.. x x * x| ___ ? __ r__ 10
-#
-# res _    # set
-# ___ x __ r__ 10
-#     ?.a.. x * x
-# print ?
-#
-# res _    # dictionary
-# ___ x __ r__ 10
-#     ? |x _ x * x
-#
-# print ?
+# # x
+print({x * x for x in range(10)})    # dict)}
+print(set(x * x for x in range(10)))    # not dict
+print({x: x * x for x in range(10)})  # dict
+print(dict(x, x * x) for x in range(10))
+
+res = set()    # set
+for x in range(10):
+    res.add(x * x)
+print(res)
+
+res = {}    # dictionary
+for x in range(10):
+    res[x] = x * x
+
+print(res)
 #
 # # Notice that although both forms accept iterators, they have no notion of generating
 # # results on demand both forms build objects all at once. If you mean to produce keys
 # # and values upon request, a generator expression is more appropriate:
+
+G = ((x, x * x) for x in range(10))  # generator expression
+print(next(G))
 #
-# G _ x x * x ___ ? __ r__10  # generator expression
-# print n.. ?
-#
-# print n.. ?
+print(next(G))
 #
 # # Extended Comprehension Syntax for Sets and Dictionaries
 # # Lists are ordered
@@ -42,9 +42,12 @@
 # #  But sets do not
 # # Neither do dict keys
 #
-# print x * x ___ ? __ r__ 10 __ ? % 2 __ 0  # list
-# print x * x ___ ? __ r__ 10 __ ? % 2 __ 0  # set
-# print x x * x ___ ? __ r__ 10 __ ? % 2 __ 0  # dict
-# print x + y ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6 # list
-# print x + y ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6 # set
-# print x y ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6   # dict
+print()
+print()
+print([x * x for x in range(10) if x % 2 == 0])  # list
+print({x * x for x in range(10) if x % 2 == 0})  # set
+print({x: x * x for x in range(10) if x % 2 == 0})  # dict
+# # x, y
+# print ? + ? ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6 # list
+# print ? + ? ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6 # set
+# print ? ? ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6   # dict
--- 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/002_dictionary_comprehension_!cool!.py
+++ 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/templates/002_dictionary_comprehension_!cool!.py
@@ -1,35 +1,37 @@
 # # Comprehension Syntax Summary
 # # Dictionary comprehension
-#
-# print x x * x ___ ? __ r... 10   # # Dictionary comprehension
+# # x
+# print ? ? * ? ___ ? __ r... 10   # # Dictionary comprehension
 #
 # # Comprehending Set and Dictionary Comprehensions
 # # Comprehension
 # # Generator and type name
 # #  Set comprehension equivalent
 # # Dict comprehension equivalent
-#
-# print x * x ___ ? __ r__ 10    # dict
-# se_ x * x ___ ? __ r__ 10    # not dict
-# print x x * x ___ ? __ r__ 10  # dict
-# print di.. x x * x| ___ ? __ r__ 10
+# # x
+# print ? * ? ___ ? __ r__ 10    # dict
+# se_ ? * ? ___ ? __ r__ 10    # not dict
+# print ? ? * ? ___ ? __ r__ 10  # dict
+# print di.. ? ? * ?| ___ ? __ r__ 10
 #
 # res _    # set
-# ___ x __ r__ 10
-#     ?.a.. x * x
+# # x
+# ___ ? __ r__ 10
+#     ?.a.. ? * ?
 # print ?
 #
 # res _    # dictionary
-# ___ x __ r__ 10
-#     ? |x _ x * x
+# x
+# ___ ? __ r__ 10
+#     ? |? _ ? * ?
 #
 # print ?
 #
 # # Notice that although both forms accept iterators, they have no notion of generating
 # # results on demand both forms build objects all at once. If you mean to produce keys
 # # and values upon request, a generator expression is more appropriate:
-#
-# G _ x x * x ___ ? __ r__10  # generator expression
+# # x
+# G _ ? ? * ? ___ ? __ r__10  # generator expression
 # print n.. ?
 #
 # print n.. ?
@@ -42,9 +44,10 @@
 # #  But sets do not
 # # Neither do dict keys
 #
-# print x * x ___ ? __ r__ 10 __ ? % 2 __ 0  # list
-# print x * x ___ ? __ r__ 10 __ ? % 2 __ 0  # set
-# print x x * x ___ ? __ r__ 10 __ ? % 2 __ 0  # dict
-# print x + y ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6 # list
-# print x + y ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6 # set
-# print x y ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6   # dict
+# print ? * ? ___ ? __ r__ 10 __ ? % 2 __ 0  # list
+# print ? * ? ___ ? __ r__ 10 __ ? % 2 __ 0  # set
+# print ? ? * ? ___ ? __ r__ 10 __ ? % 2 __ 0  # dict
+# # x, y
+# print ? + ? ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6 # list
+# print ? + ? ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6 # set
+# print ? ? ___ ? __ 1, 2, 3 ___ ? __ 4, 5, 6   # dict
