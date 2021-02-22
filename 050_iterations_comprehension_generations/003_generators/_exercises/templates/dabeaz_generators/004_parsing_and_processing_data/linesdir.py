06badbb2ab8c6e2f636a448c972563b06c8dec77
Grey <sergejyurskyj@yahoo.com>
2021-02-22 13:46:19 -0800

    Committed generators exercises

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/004_parsing_and_processing_data/linesdir.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/004_parsing_and_processing_data/linesdir.py
@@ -1,20 +1,21 @@
-# linesdir.py
+# # linesdir.py
+# #
+# # Generate a sequence of lines from files in a directory
 #
-# Generate a sequence of lines from files in a directory
-
-f.. p_l_ ______ P..
-f.. gencat ______ *
-f.. genopen ______ *
-
-___ lines_from_dir(filepat, dirname):
-    names = P..(dirname).r_g_(filepat)
-    files = gen_open(names)
-    lines = gen_cat(files)
-    r_ lines
-
-# Example use
-
-__ __name__ __ '__main__':
-    loglines = lines_from_dir *a.. *w..
-    ___ line __ loglines:
-        print(line, e.._'')
+# f.. p_l_ ______ P..
+# f.. gencat ______ *
+# f.. genopen ______ *
+#
+# ___ lines_from_dir filepat dirname
+#     names _ P.. d.. .r_g_ f..
+#     files _ g.. ?
+#     lines _ g.. ?
+#     r_ ?
+#
+# # Example use
+#
+# # line
+# __ __name__ __ '__main__':
+#     loglines _ l.. *a.. *w..
+#     ___ ? __ ?
+#         print ? e.._'')
--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/004_parsing_and_processing_data/query404.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/004_parsing_and_processing_data/query404.py
@@ -1,15 +1,17 @@
-# query404.py
+# # query404.py
+# #
+# # Find the set of all documents that 404 in a log file
 #
-# Find the set of all documents that 404 in a log file
-
-f.. linesdir ______ lines_from_dir
-f.. apachelog ______ apache_log
-
-lines = lines_from_dir *a.. *w..
-log = apache_log(lines)
-
-stat404 =  {  r['request'] ___ r __ log
-              __ r['status'] __ 404 }
-
-___ r __ sorted(stat404):
-    print(r)
+# f.. linesdir ______ lines_from_dir
+# f.. apachelog ______ apache_log
+#
+# lines _ l.. *a.. *w..
+# log _ a.. ?
+#
+# # r
+# stat404 _  ? *request ___ ? __ ?
+#               __ ? *status __ 404
+#
+# # r
+# ___ ? __ so.. ?
+#     print ?
