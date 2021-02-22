fd2826329c4bd83b94386cd371f826639b9c8153
Grey <sergejyurskyj@yahoo.com>
2021-02-22 12:48:49 -0800

    Committed generators exercises

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/003_fun_with_files_and_directories/genfind.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/003_fun_with_files_and_directories/genfind.py
@@ -1,15 +1,16 @@
-# genfind.py
+# # genfind.py
+# #
+# # A function that generates files that match a given filename pattern
 #
-# A function that generates files that match a given filename pattern
-
-f.. p_l_ ______ P..
-
-___ gen_find(filepat, top):
-    y... f.. P..(top).r_g_(filepat)
-
-# Example use
-
-__ __name__ == '__main__':
-    lognames = gen_find("access-log*","www")
-    ___ name __ lognames:
-        print(name)
+# f.. p_l_ ______ P..
+#
+# ___ gen_find filepat top
+#     y... f.. P.. t.. .r_g_ ?
+#
+# # Example use
+#
+# # # name
+# __ __name__ == '__main__':
+#     lognames = ? *access-log* *www
+#     ___ ? __ ?
+#         print ?
