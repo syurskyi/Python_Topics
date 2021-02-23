790fdb3ff750982acabe934edfe33d7ea50e4a62
Grey <sergejyurskyj@yahoo.com>
2021-02-22 22:10:27 -0800

    Committed generators exercises

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/006_feeding_pipeline/genmessages.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/006_feeding_pipeline/genmessages.py
@@ -1,21 +1,22 @@
-# genmessages.py
+# # genmessages.py
+# #
+# # A generator that yields messages on a UDP socket
 #
-# A generator that yields messages on a UDP socket
-
-______ s__
-___ receive_messages(addr,maxsize):
-    s = s__.s__(s__.AF_INET,s__.SOCK_DGRAM)
-    s.bind(addr)
-    w____ T..
-        msg = s.recvfrom(maxsize)
-        y... msg
-
-# Example use
-# To send a message to this generator, use the code "msgtest.py"
-
-__ __name__ __ '__main__':
-    ___ msg, addr __ receive_messages(("",10000),1024):
-        print(msg, "from", addr)
-
-
-    
+# ______ s__
+# ___ receive_messages addr maxsize
+#     s = s__.s__ s__.A_I.. s__.S_D..
+#     ?.b.. ?
+#     w____ T..
+#         msg = ?.r_f.. ?
+#         y... ?
+#
+# # Example use
+# # To send a message to this generator, use the code "msgtest.py"
+#
+# # msg, addr
+# __ __name__ __ '__main__':
+#     ___ ? ? __ ? "",10000),1024
+#         print ? "from" ?
+#
+#
+#
