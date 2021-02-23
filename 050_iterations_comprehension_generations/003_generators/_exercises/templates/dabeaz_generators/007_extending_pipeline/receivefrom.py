cfa7a887b16df5c551d8b54ecaa43ea14a5f7618
Grey <sergejyurskyj@yahoo.com>
2021-02-22 22:24:21 -0800

    Committed generators exercises

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/007_extending_pipeline/receivefrom.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/007_extending_pipeline/receivefrom.py
@@ -1,21 +1,23 @@
-# receivefrom.py
+# # receivefrom.py
+# #
+# # Receive objects from a different machine
 #
-# Receive objects from a different machine
-
-______ s__
-f.. genpickle ______ gen_unpickle
-
-___ receivefrom(addr):
-    s = s__.s__(s__.A_I_,s__.S_S_)
-    s.s_s_o_(s__.S_S_,s__.S_R_, 1)
-    s.b..(addr)
-    s.l..(5)
-    c, a = s.a..
-    ___ item __ gen_unpickle(c.makefile('rb')):
-        y... item
-    c.c..
-
-# Example use:
-__ __name__ __ '__main__':
-    ___ r __ receivefrom(("",15000)):
-        print(r['host'], r['request'])
+# ______ s__
+# f.. genpickle ______ gen_unpickle
+#
+# ___ receivefrom addr
+#     s = s__.s__ s__.A_I_ s__.S_S_
+#     s.s_s_o_ s__.S_S_ s__.S_R_ 1
+#     s.b.. ?
+#     s.l.. 5
+#     c, a = s.a..
+#     # item
+#     ___ ? __ g.. ?.m.. *__
+#         y... ?
+#     ?.c..
+#
+# # Example use:
+# __ __name__ __ '__main__':
+# # r
+#     ___ ? __ ? "" 15000
+#         print ? *host ? *request
