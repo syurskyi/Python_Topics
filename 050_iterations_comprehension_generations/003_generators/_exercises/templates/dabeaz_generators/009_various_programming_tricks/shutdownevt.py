f0187adc6545d93e0221b8f722540e1b9536f2b6
Grey <sergejyurskyj@yahoo.com>
2021-02-22 22:44:40 -0800

    Committed generators exercises

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/009_various_programming_tricks/shutdownevt.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/009_various_programming_tricks/shutdownevt.py
@@ -1,40 +1,41 @@
-# shutdownevt.py
+# # shutdownevt.py
+# #
+# # Example of a generator that uses an event to shut down
 #
-# Example of a generator that uses an event to shut down
-
-______ ti__
-
-___ follow(thefile,shutdown=N..):
-    thefile.s..(0,2)
-    w____ T..
-        __ shutdown and shutdown.isSet(): break
-        line = thefile.readline()
-        __ no. line:
-           ti__.s..(0.1)
-           c...
-        y... line
-
-______ t___
-
-shutdown_event = t___.Event()
-
-___ run():
-    lines = follow(o..("run/foo/access-log"),shutdown_event)
-    ___ line __ lines:
-        print line,
-
-    print "Done"
-
-
-# Run the above in a separate thread
-t = t___.T...(t.._run)
-t.s..
-
-# Wait a while then shut down
-
-
-ti__.s..(60)
-print "Shutting down"
-
-shutdown_event.se.()
-
+# ______ ti__
+#
+# ___ follow thefile s_d.._N..
+#     ?.s.. 0 2
+#     w____ T..
+#         __ s_d_ a.. s_d_.iS.. b..
+#         line = ?.r_l..
+#         __ no. ?
+#            ti__.s.. 0.1
+#            c...
+#         y... ?
+#
+# ______ t___
+#
+# shutdown_event = t___.E..
+#
+# ___ run
+#     lines = follow o.. *run/foo/access-log ?
+# # line
+#     ___ ? __ ?
+#         print ?
+#
+#     print "Done"
+#
+#
+# # Run the above in a separate thread
+# t = t___.T... t.._run)
+# t.s..
+#
+# # Wait a while then shut down
+#
+#
+# ti__.s..(60)
+# print "Shutting down"
+#
+# ?.se.
+#
