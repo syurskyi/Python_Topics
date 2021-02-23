400840fdd9127308e56eaee610877b71253ecbfe
Grey <sergejyurskyj@yahoo.com>
2021-02-22 22:08:35 -0800

    Committed generators exercises

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/005_processing_infinite_data/realtime404.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/005_processing_infinite_data/realtime404.py
@@ -1,15 +1,16 @@
-# realtime404.py
+# # realtime404.py
+# #
+# # Print all 404 requests as they happen in the log
 #
-# Print all 404 requests as they happen in the log
-
-f.. apachelog ______ apache_log
-f.. follow ______ follow
-
-logfile  = o..("run/foo/access-log")
-loglines = follow(logfile)
-log      = apache_log(loglines)
-
-r404 = (r ___ r __ log __ r['status'] __ 404)
-
-___ r __ r404:
-    print(r['host'], r['datetime'], r['request'])
+# f.. apachelog ______ apache_log
+# f.. follow ______ follow
+#
+# logfile  = o.. *run/foo/access-log
+# loglines = f.. ?
+# log      = a.. ?
+#
+# # r
+# r404 = ? ___ ? __ ? __ ? *status __ 404
+#
+# ___ ? __ ?
+#     print ? *host ? *datetime ? *request
