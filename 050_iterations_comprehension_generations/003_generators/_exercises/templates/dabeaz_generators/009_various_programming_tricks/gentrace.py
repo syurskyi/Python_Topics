130183b6f1b2cbc94383683e48d94acf51bcb8ac
Grey <sergejyurskyj@yahoo.com>
2021-02-22 22:41:28 -0800

    Committed generators exercises

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/009_various_programming_tricks/gentrace.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/009_various_programming_tricks/gentrace.py
@@ -1,22 +1,24 @@
-# gentrace.py
+# # gentrace.py
+# #
+# # Trace a generator by printing items received
 #
-# Trace a generator by printing items received
-
-___ trace(source):
-    ___ item __ source:
-        print(item)
-        y... item
-
-# Example use
-__ __name__ __ '__main__':
-    f.. apachelog ______ *
-
-    lines = o..( *a..)
-    log =  trace(apache_log(lines))
-    r404 = (r ___ r __ log __ r['status'] __ 404)
-
-    ___ r __ r404:
-        pass
-
-
-                
+# # item
+# ___ trace source
+#     ___ ? __ ?
+#         print ?
+#         y... ?
+#
+# # Example use
+# __ __name__ __ '__main__':
+#     f.. apachelog ______ *
+#
+#     lines = o..  *a..
+#     log =  ? a.. ?
+# # r
+#     r404 = ? ___ ? __ ? __ ? *status __ 404
+#
+#     ___ ? __ ?
+#         p..
+#
+#
+#
