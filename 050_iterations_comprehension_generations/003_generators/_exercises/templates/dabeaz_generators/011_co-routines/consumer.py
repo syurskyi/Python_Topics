9f81e2be824773ee6f89a351935cae43d72f2d07
Grey <sergejyurskyj@yahoo.com>
2021-03-01 09:45:40 -0800

    Committed generators exercises

--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/011_co-routines/consumer.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/011_co-routines/consumer.py
@@ -1,29 +1,29 @@
-# consumer.py
+# # consumer.py
+# #
+# # consumer decorator and co-routine example
 #
-# consumer decorator and co-routine example
-
-___ consumer(func):
-    ___ start(*args,**kwargs):
-        c = func(*args,**kwargs)
-        c.s..(N..)
-        r_ c
-    r_ start
-
-# Example
-__ __name__ __ '__main__':
-
-    @consumer
-    ___ recv_count():
-        ___
-            w____ T..
-                n = y...
-                print("T-minus", n)
-        ______ GeneratorExit:
-            print("Kaboom!")
-
-    r = recv_count()
-    ___ i __ ra__(5,0,-1):
-        r.s..(i)
-
-    r.c..
-
+# ___ consumer func
+#     ___ start $ $$
+#         c = ? $ $$
+#         c.s.. N..
+#         r_ c
+#     r_ ?
+#
+# # Example
+# __ __name__ __ '__main__':
+#
+#     ??
+#     ___ recv_count
+#         ___
+#             w____ T..
+#                 n = y...
+#                 print("T-minus" ?
+#         ______ GE..
+#             print("Kaboom!")
+# # i
+#     r = ?
+#     ___ ? __ ra__ 5,0,-1
+#         ?.s.. ?
+#
+#     ?.c..
+#
--- 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/011_co-routines/logcoroutine.py
+++ 050_iterations_comprehension_generations/003_generators/_exercises/templates/dabeaz_generators/011_co-routines/logcoroutine.py
@@ -1,28 +1,28 @@
-# logcoroutine.py
+# # logcoroutine.py
+# #
+# # An example of using co-routines to define consumers for the Apache log data
 #
-# An example of using co-routines to define consumers for the Apache log data
-
-f.. consumer ______ *
-f.. apachelog ______ *
-f.. follow ______ *
-f.. broadcast ______ *
-
-@consumer
-___ find_404():
-    w____ T..
-        r = (y...)
-        __ r['status'] __ 404:
-            print(r['status'],r['datetime'],r['request'])
-
-@consumer
-___ bytes_transferred():
-    total = 0
-    w____ T..
-        r = (y...)
-        total += r['bytes']
-        print("Total bytes", total)
-
-lines = follow(o..("run/foo/access-log"))
-log   = apache_log(lines)
-
-broadcast(log, [find_404(),bytes_transferred()])
+# f.. consumer ______ _
+# f.. apachelog ______ _
+# f.. follow ______ _
+# f.. broadcast ______ _
+#
+# ??
+# ___ find_404
+#     w____ T..
+#         r _ (y...)
+#         __ ? *st.. __ 404
+#             print ? *st.. ? *datet.. ? *req..
+#
+# ??
+# ___ bytes_transferred
+#     total _ 0
+#     w____ T..
+#         r _ (y...)
+#         ? +_ ? *by..
+#         print("Total bytes" ?
+#
+# lines _ f.. o.. *run/foo/access-log
+# log   _ a.. ?
+#
+# br.. ? ? ?
