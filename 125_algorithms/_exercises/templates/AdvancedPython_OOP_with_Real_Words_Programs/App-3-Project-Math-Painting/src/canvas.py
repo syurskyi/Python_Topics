0ec382f56cbfaef99e047587be2422b1bf5b6011
syurskyi <sergejyurskyj@yahoo.com>
2021-12-03 13:50:11 -0800

    Committed AdvancedPython_OOP_with_Real_Words_Programs, App-3-Project-Math-Painting template

--- 125_algorithms/_exercises/templates/AdvancedPython_OOP_with_Real_Words_Programs/App-3-Project-Math-Painting/src/canvas.py
+++ 125_algorithms/_exercises/templates/AdvancedPython_OOP_with_Real_Words_Programs/App-3-Project-Math-Painting/src/canvas.py
@@ -1,21 +1,21 @@
-_____ numpy as np
-____ PIL _____ Image
-
-
-c_ Canvas:
-    """Object where all shapes are going to be drawn"""
-
-    ___  -    height, width, color):
-        color = color
-        height = height
-        width = width
-
-        # Create a 3d numpy array of zeros
-        data = np.zeros((height, width, 3), dtype=np.uint8)
-        # Change [0,0,0] with user given values for color
-        data[:] = color
-
-    ___ make   imagepath):
-        """Converts the current array into an image file"""
-        img = Image.fromarray(data, 'RGB')
-        img.save(imagepath)
+# _____ numpy as np
+# ____ PIL _____ Image
+#
+#
+# c_ Canvas
+#     """Object where all shapes are going to be drawn"""
+#
+#     ___  -    heigh, width color
+#         ? = ?
+#         ? = ?
+#         ? = ?
+#
+#         # Create a 3d numpy array of zeros
+#         data = __.z.. h.. w.. 3 dtype= __.uint8)
+#         # Change [0,0,0] with user given values for color
+#         d.. ||| = c..
+#
+#     ___ make   imagepath
+#         """Converts the current array into an image file"""
+#         img = I__.f.. d.. 'RGB'
+#         ?.s.. i..
--- 125_algorithms/_exercises/templates/AdvancedPython_OOP_with_Real_Words_Programs/App-3-Project-Math-Painting/src/shapes.py
+++ 125_algorithms/_exercises/templates/AdvancedPython_OOP_with_Real_Words_Programs/App-3-Project-Math-Painting/src/shapes.py
@@ -1,27 +1,27 @@
-c_ Rectangle:
-    """A rectangle shape that can be drawn on a Canvas object"""
-    ___  -    x, y, height, width, color):
-        x = x
-        y = y
-        height = height
-        width = width
-        color = color
-
-    ___ draw   canvas):
-        """Draws itself into the canvas"""
-        # Changes a slice of the array with new values
-        canvas.data[x: x + height, y: y + width] = color
-
-
-c_ Square:
-    """A square shape that can be drawn on a Canvas object"""
-    ___  -    x, y, side, color):
-        color = color
-        x = x
-        y = y
-        side = side
-
-    ___ draw   canvas):
-        """Draws itself into the canvas"""
-        # Changes a slice of the array with new values
-        canvas.data[x: x + side, y: y + side] = color
+# c_ Rectangle
+#     """A rectangle shape that can be drawn on a Canvas object"""
+#     ___  -    x y height width color
+#         ? = ?
+#         ? = ?
+#         ? = ?
+#         ? = ?
+#         ? = ?
+#
+#     ___ draw   canvas
+#         """Draws itself into the canvas"""
+#         # Changes a slice of the array with new values
+#         c__.d.. x| ? + h.. y| ? + w.. _ c..
+#
+#
+# c_ Square
+#     """A square shape that can be drawn on a Canvas object"""
+#     ___  -    x y side color
+#         ? = ?
+#         ? = ?
+#         ? = ?
+#         ? = ?
+#
+#     ___ draw   canvas
+#         """Draws itself into the canvas"""
+#         # Changes a slice of the array with new values
+#         c__.d.. x| ? + s.. y| ? + s.. _ c..
