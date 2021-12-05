dec30f38aa364fc219ebc44bbdb4907ed419ca1d
syurskyi <sergejyurskyj@yahoo.com>
2021-12-05 14:24:46 -0800

    Committed Learn Parallel Computing in Python template

--- 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/006_shared_mem_process/matrix_multiply_process.py
+++ 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/006_shared_mem_process/matrix_multiply_process.py
@@ -1,48 +1,48 @@
-______ _
-______ t___
-____ r___ ______ R..
-
-____ _ ______ B...
-
-____ _.context ______ P..
-
-process_count = 8
-matrix_size = 200
-r___ = R..()
-
-
-___ generate_random_matrix(matrix):
-    ___ row __ r...(matrix_size):
-        ___ col __ r...(matrix_size):
-            matrix[row * matrix_size + col] = r___.r..(-5, 5)
-
-
-___ work_out_row(id, matrix_a, matrix_b, result, work_start, work_complete):
-    w... T..
-        work_start.w..
-        ___ row __ r...(id, matrix_size, process_count):
-            ___ col __ r...(matrix_size):
-                ___ i __ r...(matrix_size):
-                    result[row * matrix_size + col] += matrix_a[row * matrix_size + i] * matrix_b[i * matrix_size + col]
-        work_complete.w..
-
-
-__ _____ __ _____
-    _.set_start_method('spawn')
-    work_start = B...(process_count + 1)
-    work_complete = B...(process_count + 1)
-    matrix_a = _.Array('i', [0] * (matrix_size * matrix_size), l.._F..)
-    matrix_b = _.Array('i', [0] * (matrix_size * matrix_size), l.._F..)
-    result = _.Array('i', [0] * (matrix_size * matrix_size), l.._F..)
-    ___ p __ r...(process_count):
-        P..(t.._work_out_row, a.._(p, matrix_a, matrix_b, result, work_start, work_complete)).s..
-    start = t___.t___()
-    ___ t __ r...(10):
-        generate_random_matrix(matrix_a)
-        generate_random_matrix(matrix_b)
-        ___ i __ r...(matrix_size * matrix_size):
-            result[i] = 0
-        work_start.w..
-        work_complete.w..
-    end = t___.t___()
-    print("Done, time taken", end - start)
+# ______ _
+# ______ t___
+# ____ r___ ______ R..
+#
+# ____ _ ______ B...
+#
+# ____ _.context ______ P..
+#
+# process_count = 8
+# matrix_size = 200
+# r___ = R..
+#
+#
+# ___ generate_random_matrix matrix
+#     ___ row __ r... m..
+#         ___ col __ r... m..
+#             ? r.. * m.. + c.. = r___.r.. -5 5
+#
+#
+# ___ work_out_row(id, matrix_a, matrix_b, result, work_start, work_complete
+#     w... T..
+#         w__.w..
+#         ___ row __ r... i. m.. p..
+#             ___ col __ r... m..
+#                 ___ i __ r...(m..
+#                     r.. r.. * m.. + c.. += m.. r.. * m.. + ? * m.. ? * m.. + c..
+#         w__.w..
+#
+#
+# __ _____ __ _____
+#     _.s.. 'spawn'
+#     work_start = B... p.. + 1
+#     work_complete = B...(p.. + 1
+#     matrix_a = _.A.. 'i', [0] * m.. * m.. l.._F..
+#     matrix_b = _.A.. 'i', [0] * m.. * m.. l.._F..
+#     result = _.A.. 'i', [0] * m.. * m.. l.._F..
+#     ___ p __ r... p..
+#         P..(t.._w.. a.._ ? m.. m.. r.. w.. w.. .s..
+#     start = t___.t___
+#     ___ t __ r... 10
+#         ? ?
+#         ? ?
+#         ___ i __ r... m.. * m..
+#             r.. ? = 0
+#         w__.w..
+#         w__.w..
+#     end = t___.t___
+#     print("Done, time taken", ? - ?
