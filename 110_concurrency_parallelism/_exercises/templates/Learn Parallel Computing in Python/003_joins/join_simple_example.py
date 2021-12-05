b8da53f861326b4824055eae0698d39b098a5a11
syurskyi <sergejyurskyj@yahoo.com>
2021-12-05 13:53:08 -0800

    Committed Learn Parallel Computing in Python template

--- 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/003_joins/join_simple_example.py
+++ 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/003_joins/join_simple_example.py
@@ -1,19 +1,19 @@
-______ t___
-____ _ ______ T..
-
-
-___ child
-    print("Child Thread doing work...")
-    t___.s..(5)
-    print("Child Thread done...")
-
-
-___ parent
-    t = T..(t.._child, a.._([]))
-    t.s..
-    print("Parent Thread is waiting...")
-    t.j..()
-    print("Parent Thread is unblocked...")
-
-
-parent()
+# ______ t___
+# ____ _ ______ T..
+#
+#
+# ___ child
+#     print("Child Thread doing work...")
+#     t___.s.. 5
+#     print("Child Thread done...")
+#
+#
+# ___ parent
+#     t = T..(t.._child, a.._ # list
+#     ?.s..
+#     print("Parent Thread is waiting...")
+#     ?.j..
+#     print("Parent Thread is unblocked...")
+#
+#
+# p..
