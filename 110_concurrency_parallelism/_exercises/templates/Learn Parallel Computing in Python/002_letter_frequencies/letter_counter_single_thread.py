accb7234fc1c25235df0301e1ccd1179237f0fd6
syurskyi <sergejyurskyj@yahoo.com>
2021-12-05 13:48:03 -0800

    Committed Learn Parallel Computing in Python template

--- 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/002_letter_frequencies/letter_counter_single_thread.py
+++ 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/002_letter_frequencies/letter_counter_single_thread.py
@@ -1,27 +1,27 @@
-______ j___
-______ u__.r..
-______ t___
-
-
-___ count_letters(url, frequency):
-    response = u__.r...u..(url)
-    txt = s..(response.r..
-    ___ l __ txt:
-        letter = l.l..
-        __ letter __ frequency:
-            frequency[letter] += 1
-
-
-___ main
-    frequency = {}
-    ___ c __ "abcdefghijklmnopqrstuvwxyz":
-        frequency[c] = 0
-    start = t___.t___()
-    ___ i __ r...(1000, 1020):
-        count_letters(f"https://www.rfc-editor.org/rfc/rfc{i}.txt", frequency)
-    end = t___.t___()
-    print(j___.d..(frequency, i.._4))
-    print("Done, time taken", end - start)
-
-
-main()
+# ______ j___
+# ______ u__.r..
+# ______ t___
+#
+#
+# ___ count_letters url frequency
+#     response = u__.r...u.. ?
+#     txt = s.. ?.r..
+#     ___ l __ ?
+#         letter = ?.l..
+#         __ ? __ f..
+#             ? ? += 1
+#
+#
+# ___ main
+#     frequency = # dict
+#     ___ c __ "abcdefghijklmnopqrstuvwxyz":
+#         ? ? = 0
+#     start = t___.t___
+#     ___ i __ r... 1000, 1020
+#         ? _*https://www.rfc-editor.org/rfc/rfc|?|.txt ?
+#     end = t___.t___
+#     print(j___.d.. ? i.._4
+#     print("Done, time taken", ? - ?
+#
+#
+# m..
