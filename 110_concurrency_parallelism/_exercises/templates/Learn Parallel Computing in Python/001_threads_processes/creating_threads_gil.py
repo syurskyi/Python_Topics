bf6546d2655b48bac70c121ff6db9984f80aa45a
syurskyi <sergejyurskyj@yahoo.com>
2021-12-05 13:38:01 -0800

    Committed Learn Parallel Computing in Python template

--- 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/001_threads_processes/creating_threads_gil.py
+++ 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/001_threads_processes/creating_threads_gil.py
@@ -1,15 +1,15 @@
-______ t___
-____ _ ______ T..
-
-
-___ do_work
-    print("Starting work")
-    i = 0
-    ___ _ __ r...(20000000):
-        i += 1
-    print("Finished work")
-
-
-___ _ __ r...(5):
-    t = T..(t.._do_work, a.._())
-    t.s..
+# ______ t___
+# ____ _ ______ T..
+#
+#
+# ___ do_work
+#     print("Starting work")
+#     i = 0
+#     ___ _ __ r... 20000000
+#         ? += 1
+#     print("Finished work")
+#
+#
+# ___ _ __ r... 5
+#     t = T.. t.._? a.._
+#     ?.s..
