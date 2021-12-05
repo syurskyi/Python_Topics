fb5620773a176adfc3b044ef87d4c50978c0ba84
syurskyi <sergejyurskyj@yahoo.com>
2021-12-04 20:57:10 -0800

    Committed Python-from-Zero-to-Hero template

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Python-from-Zero-to-Hero/asyncio/04_01_task_basics.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Python-from-Zero-to-Hero/asyncio/04_01_task_basics.py
@@ -1,28 +1,28 @@
-____ ____
-
-
-_____ ___ tick
-    print("Tick")
-    _____ ____.s..(1)
-    print("Tock")
-    r_ 'Tick-Tock'
-
-
-_____ ___ main
-    t1 = ____.create_task(tick(), name='tick1')
-    t2 = ____.ensure_future(tick())
-
-    # await t1
-    # await t2
-
-    results = _____ ____.gather(t1, t2)
-
-    print(f'{t1.get_name()}. Done = {t1.done()}')
-    print(f'{t2.get_name()}. Done = {t2.done()}')
-
-    ___ x __ results:
-        print(x)
-
-
-__ _______ __ _______
-    ____.run(main())
+# ____ ____
+#
+#
+# _____ ___ tick
+#     print("Tick")
+#     _____ ____.s.. 1
+#     print("Tock")
+#     r_ 'Tick-Tock'
+#
+#
+# _____ ___ main
+#     t1 = ____.c.. t.. name='tick1'
+#     t2 = ____.e.. t..
+#
+#     # await t1
+#     # await t2
+#
+#     results = _____ ____.g.. ? ?
+#
+#     print _ * ?.g... Done = ?.d..
+#     print _ * ?.g... Done = ?.d..
+#
+#     ___ x __ r..
+#         print ?
+#
+#
+# __ _______ __ _______
+#     ____.r.. m..
