6a5a54631a72812c8983343df200c9e174a90e49
syurskyi <sergejyurskyj@yahoo.com>
2021-12-05 14:31:33 -0800

    Committed Learn Parallel Computing in Python template

--- 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/007_message_passing/producer_consumer.py
+++ 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/007_message_passing/producer_consumer.py
@@ -1,24 +1,24 @@
-____ queue ______ Q..
-____ _ ______ T..
-
-______ t___
-
-
-___ consumer(q):
-    w... T..
-        txt = q.g..
-        print(txt)
-        t___.s..(1)
-
-
-___ producer(q):
-    w... T..
-        q.p..("Hello there")
-        print("Message Sent")
-
-
-q = Q..(m.._10)
-t1 = T..(t.._consumer, a.._(q,))
-t2 = T..(t.._producer, a.._(q,))
-t1.s..
-t2.s..
+# ____ queue ______ Q..
+# ____ _ ______ T..
+#
+# ______ t___
+#
+#
+# ___ consumer q
+#     w... T..
+#         txt = ?.g..
+#         print ?
+#         t___.s..
+#
+#
+# ___ producer q
+#     w... T..
+#         ?.p..("Hello there")
+#         print("Message Sent")
+#
+#
+# q = Q.. m.._10
+# t1 = T..(t.._ ? a.._ ?
+# t2 = T..(t.._ ? a.._ ?
+# ?.s..
+# ?.s..
