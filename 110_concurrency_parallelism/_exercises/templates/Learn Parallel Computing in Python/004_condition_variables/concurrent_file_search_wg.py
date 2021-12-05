2858331bee01db7e5a240aa84253028c27386724
syurskyi <sergejyurskyj@yahoo.com>
2021-12-05 13:56:50 -0800

    Committed Learn Parallel Computing in Python template

--- 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/004_condition_variables/concurrent_file_search_wg.py
+++ 110_concurrency_parallelism/_exercises/templates/Learn Parallel Computing in Python/004_condition_variables/concurrent_file_search_wg.py
@@ -1,36 +1,36 @@
-______ __
-____ __.p.. ______ i.., j..
-____ _ ______ L..., T..
-
-____ condition_variables.wait_group ______ WaitGroup
-
-mutex = L...()
-matches = []
-
-
-___ file_search(root, filename, wait_group):
-    print("Searching in:", root)
-    ___ file __ __.listdir(root):
-        full_path = j..(root, file)
-        __ filename __ file:
-            mutex.a...
-            matches.a..(full_path)
-            mutex.r..
-        __ i..(full_path):
-            wait_group.add(1)
-            t = T..(t.._file_search, a.._([full_path, filename, wait_group]))
-            t.s..
-    wait_group.done()
-
-
-___ main
-    wait_group = WaitGroup()
-    wait_group.add(1)
-    t = T..(t.._file_search, a.._(["c:/tools", "README.md", wait_group]))
-    t.s..
-    wait_group.wait()
-    ___ m __ matches:
-        print("Matched:", m)
-
-
-main()
+# ______ __
+# ____ __.p.. ______ i.., j..
+# ____ _ ______ L..., T..
+#
+# ____ condition_variables.wait_group ______ W..
+#
+# mutex = L...
+# matches = #  list
+#
+#
+# ___ file_search root filename wait_group
+#     print("Searching in:" ?
+#     ___ file __ __.l.. ?
+#         full_path = j.. ? ?
+#         __ filename __ ?
+#             m__.a...
+#             m__.a.. ?
+#             m__.r..
+#         __ i.. f..
+#             w__.a.. 1
+#             t = T.. t.._? a.._ ? ? ?
+#             ?.s..
+#     w__.d..
+#
+#
+# ___ main
+#     wait_group = W..
+#     ?.a.. 1
+#     t = T.. t.._ ? a.._ "c:/tools", "README.md" ?
+#     ?.s..
+#     ?.w..
+#     ___ m __ matches:
+#         print("Matched:" ?
+#
+#
+# m..
