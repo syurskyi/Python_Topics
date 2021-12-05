03f6b32a7380d28eb1c2e127d938bc7c2dd6eba4
syurskyi <sergejyurskyj@yahoo.com>
2021-12-04 20:59:34 -0800

    Committed Python-from-Zero-to-Hero template

--- 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Python-from-Zero-to-Hero/asyncio/05_02_task_as_completed.py
+++ 110_concurrency_parallelism/001_asynchronous/_exercises/templates/Python-from-Zero-to-Hero/asyncio/05_02_task_as_completed.py
@@ -1,31 +1,31 @@
-____ ____
-____ t___
-
-
-_____ ___ tick
-    _____ ____.s..(1)
-    r_ 'Tick'
-
-
-_____ ___ tock
-    _____ ____.s..(2)
-    r_ 'Tock'
-
-
-_____ ___ main
-    start = t___.perf_counter()
-
-    t1 = ____.create_task(tick())
-    t2 = ____.create_task(tock())
-
-    # results = await asyncio.gather(t1, t2)
-
-    ___ i, t __ e___(____.as_completed((t1, t2)), start=1):
-        result = _____ t
-        elapsed = t___.perf_counter() - start
-        print(f'Executed {i} in {elapsed:0.2f} seconds')
-        print(result)
-
-
-__ _______ __ _______
-    ____.run(main())
+# ____ ____
+# ____ t___
+#
+#
+# _____ ___ tick
+#     _____ ____.s.. 1
+#     r_ 'Tick'
+#
+#
+# _____ ___ tock
+#     _____ ____.s.. 2
+#     r_ 'Tock'
+#
+#
+# _____ ___ main
+#     start = t___.p..
+#
+#     t1 = ____.c.. t..
+#     t2 = ____.c.. t..
+#
+#     # results = await asyncio.gather(t1, t2)
+#
+#     ___ i, t __ e___ ____.a.. ? ? start=1
+#         result = _____ t
+#         elapsed = t___.p.. - s..
+#         print _*Executed ? __ |?|0.2f| seconds')
+#         print ?
+#
+#
+# __ _______ __ _______
+#     ____.r.. m..
