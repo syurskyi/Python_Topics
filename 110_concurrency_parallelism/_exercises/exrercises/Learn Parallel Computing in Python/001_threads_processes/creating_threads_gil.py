cfc99b7205aa07b965ee36035db5e36bb40f9a68
syurskyi <sergejyurskyj@yahoo.com>
2021-12-06 13:58:47 -0800

    Committed Learn Parallel Computing in Python exercise creating_threads_gil

--- 110_concurrency_parallelism/_exercises/exrercises/Learn Parallel Computing in Python/001_threads_processes/creating_threads_gil.py
+++ 110_concurrency_parallelism/_exercises/exrercises/Learn Parallel Computing in Python/001_threads_processes/creating_threads_gil.py
@@ -1,15 +1,15 @@
-# ______ t___
-# ____ _ ______ T..
-#
-#
-# ___ do_work
-#     print("Starting work")
-#     i = 0
-#     ___ _ __ r... 20000000
-#         ? += 1
-#     print("Finished work")
-#
-#
-# ___ _ __ r... 5
-#     t = T.. t.._? a.._
-#     ?.s..
+import time
+from threading import Thread
+
+
+def do_work():
+    print("Starting work")
+    i = 0
+    for _ in range(20000000):
+        i += 1
+    print("Finished work")
+
+
+for _ in range(5):
+    t = Thread(target=do_work, args=())
+    t.start()
