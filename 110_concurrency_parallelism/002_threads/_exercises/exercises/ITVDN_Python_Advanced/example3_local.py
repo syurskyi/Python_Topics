dcd0ac3bb8f51903f56d2ebaac56b47d9c8ed13e
syurskyi <sergejyurskyj@yahoo.com>
2021-12-05 17:35:08 -0800

    Committed ITVDN_Python_Advanced exercise example2_comparing

--- 110_concurrency_parallelism/002_threads/_exercises/exercises/ITVDN_Python_Advanced/example3_local.py
+++ 110_concurrency_parallelism/002_threads/_exercises/exercises/ITVDN_Python_Advanced/example3_local.py
@@ -1,54 +1,54 @@
-# ______ ...
-#
-# print ___.a_c..
-# current = ___.c_t..
-# print ?.gN..
-# print ?.i_a..
+import threading
+
+print(threading.active_count())
+current = threading.current_thread()
+print(current.getName())
+print(current.is_alive())
 # # что произойдет, если попробуем еще раз его запустить
-# ___
-#     ?.s..
-# ______ R.. __ e
-#     print('ERROR: |er..'.f.. e.._?
-# ?.sN..'SuperThread'
-# print ?.gN..
+try:
+    current.start()
+except RuntimeError as e:
+    print('ERROR: {error}'.format(error=e))
+current.setName('SuperThread')
+print(current.getName())
 #
 # # на самом деле setName, getName - это просто старый интерфейс.
 # # В реальных задачах вы смело можете работаь напрямую с атрибутом name
-# ?.name = 'SuperThread1'
-# print ?.n..
-# print ?.gN..
+current.name = 'SuperThread1'
+print(current.name)
+print(current.getName())
 #
 # # вывод всех запущенных и живых трэдов
-# print ___.en..
+print(threading.enumerate())
 #
 # # Напишем пример для демонстрации потокобезопасного хранилища данных.
-# thread_data _ ___.l..
-# ?.v.. _ 5
-#
-#
-# ___ print_results
-#     print ___.c_t..
-#     print('Result: @'.f.. t_d__.v..
-#
-#
-# ___ counter started to_value
-#     print h.. t.. 'value'
-#     t__.v.. _ ?
-#     ___ i __ ra.. ?
-#         t__.v.. +_ 1
-#     ?
-#
-#
-# task1 = ___.T.. t.._? a.._(0, 10), n.._'Task1'
-# task2 = ___.T.. t.._c.. a.._(100, 3), n.._'Task2')
-# ?.n.. = *?
-# ?.n.. = *?
-#
-# ?.s..
-# ?.s..
-#
-# p..
-#
-# ?.j..
-# ?.j..
-# p..
+thread_data = threading.local()
+thread_data.value = 5
+
+
+def print_results():
+    print(threading.current_thread())
+    print('Result: {}'.format(thread_data.value))
+
+
+def counter(started, to_value):
+    print(hasattr(thread_data, 'value')
+    thread_data.value =
+    ___ i __ ra.. ?
+        t__.v.. +_ 1
+    ?
+
+
+task1 = ___.T.. t.._? a.._(0, 10), n.._'Task1'
+task2 = ___.T.. t.._c.. a.._(100, 3), n.._'Task2')
+?.n.. = *?
+?.n.. = *?
+
+?.s..
+?.s..
+
+p..
+
+?.j..
+?.j..
+p..
