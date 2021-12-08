45b993c1e3f8753e17d33b2b80c3658d8e0b1781
syurskyi <sergejyurskyj@yahoo.com>
2021-12-07 18:55:24 -0800

    Committed Module Threading overview exercise 008

--- 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/008 - threading.main_thread.py
+++ 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/008 - threading.main_thread.py
@@ -1,42 +1,43 @@
-# _____ _ t__ l__
-#
-#
-# ___ worker
-#     name  _.c...n..
-#     t__.s.. 1
-#     l__.d.. _*Ending ?
-#
-#
-# l__.b..
-#     l.. l__.D..,
-#     f.. _ l.. _ _ m.. _
-#
-#
-# # использование 'threading.main_thread()'
-# # получаем экземпляр основного потока
-# main_thread  _.m..
-# # получаем имя основного потока
-# main_thread_name  ?.n..
-# l__.d.. _* ? starting...
-#
-# ___ _ __ r.. 3
-#     thread  _.? t.. ?
-#     ?.s..
-#
-# ___ t __ _.e..
-#     # Список 'threading.enumerate()' включает в себя основной
-#     # поток и т.к. присоединение основного потока самого к себе
-#     # приводит к тупиковой ситуации, то его необходимо пропустить
-#     __ t __ ?
-#         _____
-#     thead_name  ?.n..
-#     l__.d.. _* ? joining ?
-#     ?.j..
-#
-# # [DEBUG] MainThread starting...
-# # [DEBUG] MainThread joining Thread-1
-# # [DEBUG] Ending Thread-1
-# # [DEBUG] MainThread joining Thread-2
-# # [DEBUG] Ending Thread-3
-# # [DEBUG] Ending Thread-2
-# # [DEBUG] MainThread joining Thread-3
+import threading, time, logging
+
+
+def worker():
+    name = threading.current_thread().name
+    time.sleep(1)
+    logging.debug(f'Ending {name}')
+
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(levelname)s] %(message)s',
+)
+
+
+# использование 'threading.main_thread()'
+# получаем экземпляр основного потока
+main_thread = threading.main_thread()
+# получаем имя основного потока
+main_thread_name = main_thread.name
+logging.debug(f'{main_thread_name} starting...')
+
+for _ in range(3):
+    thread = threading.Thread(target=worker)
+    thread.start()
+
+for t in threading.enumerate():
+    # Список 'threading.enumerate()' включает в себя основной
+    # поток и т.к. присоединение основного потока самого к себе
+    # приводит к тупиковой ситуации, то его необходимо пропустить
+    if t is main_thread:
+        continue
+    thead_name = t.name
+    logging.debug(f'{main_thread_name} joining {thead_name}')
+    thread.join()
+
+# [DEBUG] MainThread starting...
+# [DEBUG] MainThread joining Thread-1
+# [DEBUG] Ending Thread-1
+# [DEBUG] MainThread joining Thread-2
+# [DEBUG] Ending Thread-3
+# [DEBUG] Ending Thread-2
+# [DEBUG] MainThread joining Thread-3
