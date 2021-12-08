981b823d6918d94fbe91520291271e4a22d23fdc
syurskyi <sergejyurskyj@yahoo.com>
2021-12-08 13:29:07 -0800

    Committed Module Threading overview exercise 014

--- 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/014.py
+++ 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/014.py
@@ -1,19 +1,19 @@
-# _____ _
-#
-#
-# ___ worker i
-#     """thread worker function"""
-#     print _*Worker- ?
-#
-#
-# threads  # list
-# # запускаем функцию 'worker()'
-# # для выполнения в 5-ти потоках
-# ___ i __ r.. 5
-#     t  _.? t.. ? a.. ?
-#     ?.a.. ?
-#     ?.s..
-#
-# # блокируем дальнейшее выполнение программы
-# # пока не закончат выполняться все 5 потоков
-# ?.j.. ___ ? __ ?
+import threading
+
+
+def worker(i):
+    """thread worker function"""
+    print(f'Worker- {i}')
+
+
+threads = []  # list
+# запускаем функцию 'worker()'
+# для выполнения в 5-ти потоках
+for i in range(5):
+    t = threading.Thread(target=worker, args=(i,))
+    threads.append(t)
+    t.start()
+
+# блокируем дальнейшее выполнение программы
+# пока не закончат выполняться все 5 потоков
+[thread.join() for thread in threads]
