118e9c746ac549facf339f6c5e50d211a4474078
syurskyi <sergejyurskyj@yahoo.com>
2021-12-08 12:58:19 -0800

    Committed Module Threading overview exercise 013

--- 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/013 - Thread.name.py
+++ 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/013 - Thread.name.py
@@ -1,19 +1,19 @@
-# _____ _ t__
-#
-# ___ worker
-#     # получаем экземпляр Thread и атрибутом
-#     # .name извлекаем имя потока
-#     thread_name  _.c...n..
-#     print _* Старт ?
-#     t__.s..(1)
-#
-# th1  _.? t.. ?
-# # присвоение имени потока через атрибут
-# ?.n..  FirstWorker
-# ?.s..
-# # присвоение имени потока в конструкторе
-# th2  _.? n.. SecondWorker t.. ?
-# ?.s..
-#
-# # Старт FirstWorker
-# # Старт SecondWorker
+import threading, time
+
+def worker():
+    # получаем экземпляр Thread и атрибутом
+    # .name извлекаем имя потока
+    thread_name = threading.current_thread().name
+    print(f'Старт {thread_name}')
+    time.sleep(1)
+
+th1 = threading.Thread(target=worker)
+# присвоение имени потока через атрибут
+th1.name = 'FirstWorker'
+th1.start()
+# присвоение имени потока в конструкторе
+th2 = threading.Thread(name='SecondWorker', target=worker)
+th2.start()
+
+# Старт FirstWorker
+# Старт SecondWorker
