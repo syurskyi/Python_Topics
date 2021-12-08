b0817948465202f62e7331079e09b69e80609f01
syurskyi <sergejyurskyj@yahoo.com>
2021-12-07 18:43:50 -0800

    Committed Module Threading overview exercise 007

--- 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/007 - threading.enumerate.py
+++ 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/007 - threading.enumerate.py
@@ -1,25 +1,26 @@
-# _____ _ t__ l__
-#
-# ___ worker
-#     t__.s.. 1
-#
-# l__.b..
-#     l.. l__.D..
-#     f.. _ l.. _ _ m.. _'
-#
-#
-# ___ _ __ r.. 3
-#     thread  _.? t.. ?
-#     ?.s..
-#
-# # использование 'threading.enumerate()'
-# # проходимся по экземплярам 'Thread'
-# # и из которых атрибутом '.name'
-# # извлекаем имена живых потоков
-# ___ t __ _.e..
-#     l__.d.. _*Thread Name: ?.n..
-#
-# # [DEBUG] Thread Name: MainThread
-# # [DEBUG] Thread Name: Thread-1
-# # [DEBUG] Thread Name: Thread-2
-# # [DEBUG] Thread Name: Thread-3
+import threading, time, logging
+
+def worker():
+    time.sleep(1)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(levelname)s] %(message)s',
+)
+
+
+for _ in range(3):
+    thread = threading.Thread(target=worker)
+    thread.start()
+
+# использование 'threading.enumerate()'
+# проходимся по экземплярам 'Thread'
+# и из которых атрибутом '.name'
+# извлекаем имена живых потоков
+for t in threading.enumerate():
+    logging.debug(f'Thread Name: {t.name}')
+
+# [DEBUG] Thread Name: MainThread
+# [DEBUG] Thread Name: Thread-1
+# [DEBUG] Thread Name: Thread-2
+# [DEBUG] Thread Name: Thread-3
