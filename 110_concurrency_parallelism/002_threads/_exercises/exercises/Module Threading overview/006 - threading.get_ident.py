f7cf12c5d96429ade313a928335e20a27ea50dbb
syurskyi <sergejyurskyj@yahoo.com>
2021-12-07 18:37:25 -0800

    Committed Module Threading overview exercise 006

--- 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/006 - threading.get_ident.py
+++ 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/006 - threading.get_ident.py
@@ -1,23 +1,24 @@
-# _____ _ t__ l__
-#
-# ___ worker
-#     name  _.c...n..
-#     # использование 'threading.get_ident()'
-#     ident  _.g..
-#     # тоже самое можно получить через атрибут
-#     ident  _.i..
-#     l__.d.. _*Starting ?, id: ?
-#     t__.s.. 1
-#
-# l__.b..
-#     l.. l__.D..
-#     f.. _ l.. _ _ m.. _'
-#
-#
-# ___ _ __ r.. 3
-#     thread  _.? t.. ?
-#     ?.s..
-#
-# # [DEBUG] Starting Thread-1, id: 140190611068672
-# # [DEBUG] Starting Thread-2, id: 140190602675968
-# # [DEBUG] Starting Thread-3, id: 140190594283264
+import threading, time, logging
+
+def worker():
+    name = threading.current_thread().name
+    # использование 'threading.get_ident()'
+    ident = threading.get_ident()
+    # тоже самое можно получить через атрибут
+    # ident = threading.ident
+    logging.debug(f'Starting {name}, id: {ident}')
+    time.sleep(1)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(levelname)s] %(message)s',
+)
+
+
+for _ in range(3):
+    thread = threading.Thread(target=worker)
+    thread.start()
+
+# [DEBUG] Starting Thread-1, id: 140190611068672
+# [DEBUG] Starting Thread-2, id: 140190602675968
+# [DEBUG] Starting Thread-3, id: 140190594283264
