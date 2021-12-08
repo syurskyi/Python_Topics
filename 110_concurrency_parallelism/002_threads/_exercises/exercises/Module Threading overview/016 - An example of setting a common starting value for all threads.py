f9b60eeba6df5508344ab06eb17c7c529e8dfa54
syurskyi <sergejyurskyj@yahoo.com>
2021-12-08 13:55:19 -0800

    Committed Module Threading overview exercise 016

--- 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/016 - An example of setting a common starting value for all threads.py
+++ 110_concurrency_parallelism/002_threads/_exercises/exercises/Module Threading overview/016 - An example of setting a common starting value for all threads.py
@@ -1,47 +1,47 @@
-# _____ _ r__
-#
-#
-# ___ show_value data
-#     name_thread  _.c...n..
-#     ___
-#         val  ?.v..
-#     _____ A..
-#         print _* ? : Нет локального значения value
-#     ____
-#         print _* ?: value= ?
-#
-#
-# ___ worker data
-#     name_thread  _.c...n..
-#     ? ?
-#     print _* ?: установка локального value
-#     ?.v..  r__.r.. 1 100
-#     ? ?
-#
-#
-# c_ MyLocal _.l..
-#     # !!! переопределяем конструктор класса
-#     ___  -  value
-#         s.. . -
-#         ?  ?
-#         n.. =_.c...n..
-#         print _* n.. стартовое значение ?
-#
-#
-# local_data  ? 1000
-# ? ?
-#
-# ___ i __ r.. 2
-#     t  _.? t.. ? a.. ?
-#     ?.s..
-#
-# # MainThread стартовое значение 1000
-# # MainThread: value=1000
-# # Thread-1 стартовое значение 1000
-# # Thread-1: value=1000
-# # Thread-1: установка локального value
-# # Thread-1: value=31
-# # Thread-2 стартовое значение 1000
-# # Thread-2: value=1000
-# # Thread-2: установка локального value
-# # Thread-2: value=69
+import threading, random
+
+
+def show_value(data):
+    name_thread = threading.current_thread().name
+    try:
+        val = data.value
+    except AttributeError:
+        print(f'{name_thread} : Нет локального значения value')
+    else:
+        print(f'{name_thread}: value= {val}')
+
+
+def worker(data):
+    name_thread = threading.current_thread().name
+    show_value(data)
+    print(f'{name_thread}: установка локального value')
+    data.value = random.randint(1, 100)
+    show_value(data)
+
+
+class MyLocal(threading.local):
+    # !!! переопределяем конструктор класса
+    def __init__(self, value):
+        super().__init__()
+        self.value = value
+        self.name = threading.current_thread().name
+        print(f' {self.name} стартовое значение {self.value}')
+
+
+local_data = MyLocal(1000)
+show_value(local_data)
+
+for i in range(2):
+    t = threading.Thread(target=worker, args=(local_data,))
+    t.start()
+
+# MainThread стартовое значение 1000
+# MainThread: value=1000
+# Thread-1 стартовое значение 1000
+# Thread-1: value=1000
+# Thread-1: установка локального value
+# Thread-1: value=31
+# Thread-2 стартовое значение 1000
+# Thread-2: value=1000
+# Thread-2: установка локального value
+# Thread-2: value=69
