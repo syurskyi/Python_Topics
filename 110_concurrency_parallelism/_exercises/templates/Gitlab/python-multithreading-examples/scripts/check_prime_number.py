97c3677bccc4302932f7f5f4541a982aa201f999
syurskyi <sergejyurskyj@yahoo.com>
2021-12-09 23:14:19 -0800

    Committed Threading templates

--- 110_concurrency_parallelism/_exercises/templates/Gitlab/python-multithreading-examples/scripts/check_prime_number.py
+++ 110_concurrency_parallelism/_exercises/templates/Gitlab/python-multithreading-examples/scripts/check_prime_number.py
@@ -1,27 +1,27 @@
-______ _
- 
-c_ PrimeNumber(_.?):
-  ___ -  number):
-    _.?.-
-    Number = number
- 
-  ___ run 
-    counter = 2 
-    w___ counter*counter < Number: 
-      __ Number @ counter == 0:
-        print "%d is no prime number, because %d = %d * %d" @ ( Number, Number, counter, Number / counter)
-                r_ 
-            counter += 1 
-        print "%d is a prime number" @ Number
-threads   # list
-w___ T..:
-    input = long(raw_input("number: ")) 
-    __ input < 1:
-        ____
- 
-    thread = PrimeNumber(input) 
-    threads += [thread] 
-    thread.s..
- 
-___ x __ threads: 
-    x.j..
+# ______ _
+#
+# c_ PrimeNumber _.?
+#   ___ -  number
+#     _.?.-
+#     Number = ?
+#
+#   ___ run
+#     counter = 2
+#     w___ c..*c.. < N..
+#       __ N.. @ c.. __ 0
+#         print "%d is no prime number, because @ = @ * @" @  N.. N.. c.. N.. / c..
+#                 r_
+#             counter += 1
+#         print @ is a prime number @ N..
+# threads   # list
+# w___ T..
+#     input  l.. raw_input "number: "))
+#     __ ? < 1
+#         ____
+#
+#     thread  ? ?
+#     ? += |?
+#     ?.s..
+#
+# ___ x __ ?
+#     ?.j..
