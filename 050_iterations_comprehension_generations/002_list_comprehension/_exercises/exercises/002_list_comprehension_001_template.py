1ab713989a23a2e925d55ed7e0a5b0c768f1d473
syurskyi <sergejyurskyj@yahoo.com>
2021-12-25 17:36:27 -0800

    Committed 002_list_comprehension_001_template exercise

--- 050_iterations_comprehension_generations/002_list_comprehension/_exercises/exercises/002_list_comprehension_001_template.py
+++ 050_iterations_comprehension_generations/002_list_comprehension/_exercises/exercises/002_list_comprehension_001_template.py
@@ -3,11 +3,11 @@
 # # Suppose, we want to separate the letters of the word human and add the letters as items of a list.
 # # The first thing that comes in mind would be using for loop.
 #
-# h_letters _
-#
-# ___ letter i_ 'human'
-#     h_l__.ap.. l..
-# print h_l__
+h_letters = []
+
+for letter in 'human':
+    h_letters.append(letter)
+print(h_letters)
 #
 # # When we run the program, the output will be:
 # #
@@ -19,9 +19,9 @@
 # # Let’s see how the above program can be written using list comprehensions.
 #
 # # Example 2: Iterating through a string Using List Comprehension
-#
-# h_letters _ letter ___ ? i_ 'human'
-# print ?
+
+h_letters = [letter for letter in 'human']
+print(h_letters)
 #
 #
 # # hen we run the program, the output will be:
@@ -46,9 +46,9 @@
 # # can create and modify lists in less lines of code.
 #
 # # Example 3: Using Lambda functions inside List
-#
-# letters _ li.. ma. l_____ x x 'human'
-# print l...
+
+letters = list(map(lambda x: x, 'human'))
+print(letters)
 #
 # # When we run the program, the output will be
 # #
@@ -62,9 +62,9 @@
 # # We will create list that uses mathematical operators, integers, and range().
 #
 # # Example 4: Using if with List Comprehension
-#
-# number_list _ x ___ ? i_ ra.. 20 i_ x % 2 _ 0
-# print ?
+
+number_list = [x for x in range(20) if x % 2 == 0]
+print(number_list)
 #
 # # When we run the above program, the output will be:
 # #
@@ -73,9 +73,9 @@
 # # The list ,number_list, will be populated by the items in range from 0-19 if the item's value is divisible by 2.
 #
 # # Example 5: Nested IF with List Comprehension
-#
-# num_list _ y ___ y i_ ra.. 100 i_ y % 2 __ 0 i_ y % 5 __ 0
-# print n.....
+
+num_list = [y for y in range(100) if y % 2 == 0 if y % 5 == 0]
+print(num_list)
 #
 # # When we run the above program, the output will be:
 # #
@@ -89,9 +89,9 @@
 # # If y satisfies both conditions, y is appended to num_list.
 #
 # # Example 6: if...else With List Comprehension
-#
-# obj _  "Even" i_ i % 2 ++ 0 e___ "Odd" ___ i i_ ra.. 10
-# print o..
+
+obj = ["Even" if i % 2 == 0 else "Odd" for i in range(10)]
+print(obj)
 #
 # # When we run the above program, the output will be:
 # #
@@ -105,19 +105,19 @@
 # # normal for loop first.
 #
 # # Example 7: Transpose of Matrix using Nested Loops
-#
-# transposed _
-# matrix _ [[1, 2, 3, 4], [4, 5, 6, 8]]
-#
-# ___ i i_ ra.. le. m... 0
-#     transposed_row _       # list
-#
-#     ___ row i_ matrix
-#         t.._r..ap.. ro. i
-#     t___.ap.. t._r..
-#
-# print t...
-#
+
+transposed = []
+matrix = [[1, 2, 3, 4], [4, 5, 6, 8]]
+
+for i in range(len(matrix[0])):
+    transposed_row = []       # list
+
+    for row in matrix:
+        transposed_row.append(row[i])
+    transposed.append(transposed_row)
+
+print(transposed)
+
 # # Output: [[1, 4], [2, 5], [3, 6]]
 #
 # # The above code use two for loops to find transpose of the matrix.
@@ -126,11 +126,11 @@
 # # of a matrix using nested loop inside list comprehension.
 #
 # # Example 8: Transpose of a Matrix using List Comprehension
-#
-# matrix _ [[1, 2], [3, 4], [5, 6], [7, 8]]
-# transpose _ ro. i ___ ro. i_ m... ___ i i_ ra.. 2
-# print t...
-#
+
+matrix = [[1, 2], [3, 4], [5, 6], [7, 8]]
+transpose = [[row[i] for row in matrix] for i in range(2)]
+print(transpose)
+
 # # n above program, we have a variable matrix which have 4 rows and 2 columns.We need to find transpose of the matrix.
 # # For that, we used list comprehension.
 # #
