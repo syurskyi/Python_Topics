b2d790dd17036e949932942301ac0531c802071f
syurskyi <sergejyurskyj@yahoo.com>
2021-12-25 17:06:39 -0800

    Committed iterations template

---
+++ 050_iterations_comprehension_generations/002_list_comprehension/_exercises/exercises/The_Modern_Python_3_Bootcamp/Coding Exercise 20 List Comprehension Exercises.py
@@ -0,0 +1,17 @@
+# # Using list comprehensions:
+#
+# answer person 0  ___ ? __ "Elie", "Tim", "Matt"
+# answer2  val ___ ? __ 1, 2, 3, 4, 5, 6 __ ? % 2 __ 0
+#
+# print ?
+#
+# # Using good old manual loops:
+#
+# answer  # list
+# ___ person __ "Elie", "Tim", "Matt"
+#     ?.a.. ? 0
+#
+# answer2  # list
+# ___ num __ 1, 2, 3, 4, 5, 6
+#     __ ? % 2 __ 0
+#         ?.a.. ?
---
+++ 050_iterations_comprehension_generations/002_list_comprehension/_exercises/exercises/The_Modern_Python_3_Bootcamp/Coding Exercise 23 List Exercises 4.py
@@ -0,0 +1,7 @@
+# # Using a string (preferable solution):
+#
+# answer  char ___ ? __ "amazing" __ ? n.. __ "aeiou"
+#
+# # Using a list:
+#
+# answer  char ___ ? __ "amazing" __ ? n.. __ "a", "e", "i", "o", "u"
