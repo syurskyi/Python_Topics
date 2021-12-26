21539ac27629a2f26f792792cb983cf905beb518
syurskyi <sergejyurskyj@yahoo.com>
2021-12-25 17:18:21 -0800

    Committed iterations template

--- 050_iterations_comprehension_generations/002_list_comprehension/_exercises/exercises/002_list_comprehension_001_How to Create Lists in Python.py
+++ 050_iterations_comprehension_generations/002_list_comprehension/_exercises/exercises/002_list_comprehension_001_How to Create Lists in Python.py
@@ -12,10 +12,10 @@
 # # If you want to create a list containing the first ten perfect squares, then you can complete these steps
 # # in three lines of code:
 #
-# squares _    # list
-# ___ i __ ra.. 10
-#     ?.ap.. i * i
-# print ?
+squares = []  # list
+for i in range(10):
+    squares.append(i * i)
+print(squares)
 # # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 #
 # # Here, you instantiate an empty list, squares. Then, you use a for loop to iterate over range(10). Finally,
@@ -26,16 +26,16 @@
 # # through the supplied function.
 # # As an example, consider a situation in which you need to calculate the price after tax for a list of transactions:
 #
-# txns _  1.09, 23.56, 57.84, 4.56, 6.78  # List
-# TAX_RATE _ .08
-#
-#
-# ___ get_price_with_tax txn
-#     r_ t.. *  1 + T..
-#
-#
-# final_prices _ ma. ? t...
-# l.. ?
+txns = [1.09, 23.56, 57.84, 4.56, 6.78]  # List
+TAX_RATE = .08
+
+
+def get_price_with_tax(txn):
+    return txn * (1 + TAX_RATE)
+
+
+final_prices = map(get_price_with_tax, txns)
+print(list(final_prices))
 # # [1.1772000000000002, 25.4448, 62.467200000000005, 4.9248, 7.322400000000001]
 #
 # # Here, you have an iterable txns and a function get_price_with_tax(). You pass both of these arguments to map(),
@@ -43,9 +43,9 @@
 # # Using List Comprehensions
 # # List comprehensions are a third way of making lists. With this elegant approach, you could rewrite the
 # # for loop from the first example in just a single line of code:
-#
-# squares _ i * i ___ ? __ ra.. 10
-# print ?
+
+squares = [i * i for i in range(10)]
+print(squares)
 # # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 #
 # # Rather than creating an empty list and adding each element to the end, you simply define the list and its contents
@@ -60,17 +60,17 @@
 # #     In the example above, the iterable is range(10).
 # # Because the expression requirement is so flexible, a list comprehension in Python works well in many places where
 # # you would use map(). You can rewrite the pricing example with its own list comprehension:
-#
-# txns _ 1.09, 23.56, 57.84, 4.56, 6.78   # List
-# TAX_RATE _ .08
-#
-#
-# ___ get_price_with_tax txn
-#     r_ t.. *  1 + T..
-#
-#
-# final_prices _ ? i ___ ? __ t..
-# print ?
+
+txns = [1.09, 23.56, 57.84, 4.56, 6.78]   # List
+TAX_RATE = .08
+
+
+def get_price_with_tax(txn):
+    return txn * (1 + TAX_RATE)
+
+
+final_prices = [get_price_with_tax(i) for i in txns]
+print(final_prices)
 # # [1.1772000000000002, 25.4448, 62.467200000000005, 4.9248, 7.322400000000001]
 #
 # # The only distinction between this implementation and map() is that the list comprehension in Python returns
