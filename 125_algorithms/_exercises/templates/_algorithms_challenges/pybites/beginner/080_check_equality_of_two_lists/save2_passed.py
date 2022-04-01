ac03e4c232228e518b196f7ec1ee7d997766bcbc
syurskyi <sergejyurskyj@yahoo.com>
2022-04-01 16:35:01 -0700

    Committed algorithms templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/080_check_equality_of_two_lists/save2_passed.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/080_check_equality_of_two_lists/save2_passed.py
@@ -1,35 +1,35 @@
-____ e.. _______ E..
-
-
-c_ Equality(E..):
-    SAME_REFERENCE = 4
-    SAME_ORDERED = 3
-    SAME_UNORDERED = 2
-    SAME_UNORDERED_DEDUPED = 1
-    NO_EQUALITY = 0
-
-
-___ check_equality(list1, list2):
-    """Check if list1 and list2 are equal returning the kind of equality.
-       Use the values in the Equality Enum:
-       - return SAME_REFERENCE if both lists reference the same object
-       - return SAME_ORDERED if they have the same content and order
-       - return SAME_UNORDERED if they have the same content unordered
-       - return SAME_UNORDERED_DEDUPED if they have the same unordered content
-         and reduced to unique items
-       - return NO_EQUALITY if none of the previous cases match"""
-    equality_number = 0
-    __ list1 __ list2:
-        # SAME_REFERENCE
-        equality_number += 4
-    ____:
-        # SAME_ORDERED
-        __ list1 __ list2:
-            equality_number += 1
-        # SAME_UNORDERED
-        __ s..(list1) __ s..(list2):
-            equality_number += 1
-        # SAME_UNORDERED_DEDUPED
-        __ s..(list1) __ s..(list2):
-            equality_number += 1
-    r.. Equality(equality_number)
+# ____ e.. _______ E..
+#
+#
+# c_ Equality E..
+#     SAME_REFERENCE = 4
+#     SAME_ORDERED = 3
+#     SAME_UNORDERED = 2
+#     SAME_UNORDERED_DEDUPED = 1
+#     NO_EQUALITY = 0
+#
+#
+# ___ check_equality list1, list2
+#     """Check if list1 and list2 are equal returning the kind of equality.
+#        Use the values in the Equality Enum:
+#        - return SAME_REFERENCE if both lists reference the same object
+#        - return SAME_ORDERED if they have the same content and order
+#        - return SAME_UNORDERED if they have the same content unordered
+#        - return SAME_UNORDERED_DEDUPED if they have the same unordered content
+#          and reduced to unique items
+#        - return NO_EQUALITY if none of the previous cases match"""
+#     equality_number = 0
+#     __ ? __ ?
+#         # SAME_REFERENCE
+#         ? += 4
+#     ____:
+#         # SAME_ORDERED
+#         __ ? __ ?
+#             ? += 1
+#         # SAME_UNORDERED
+#         __ s.. ? __ s.. ?
+#             ? += 1
+#         # SAME_UNORDERED_DEDUPED
+#         __ s.. ? __ s.. ?
+#             ? += 1
+#     r.. ? ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/80_v4/equality.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/80_v4/equality.py
@@ -1,29 +1,29 @@
-____ e.. _______ E..
-
-
-c_ Equality(E..):
-    SAME_REFERENCE = 4
-    SAME_ORDERED = 3
-    SAME_UNORDERED = 2
-    SAME_UNORDERED_DEDUPED = 1
-    NO_EQUALITY = 0
-
-
-___ check_equality(list1, list2):
-    """Check if list1 and list2 are equal returning the kind of equality.
-       Use the values in the Equality Enum:
-       - return SAME_REFERENCE if both lists reference the same object
-       - return SAME_ORDERED if they have the same content and order
-       - return SAME_UNORDERED if they have the same content unordered
-       - return SAME_UNORDERED_DEDUPED if they have the same unordered content
-         and reduced to unique items
-       - return NO_EQUALITY if none of the previous cases match"""
-    __ list1 __ list2:
-        r.. Equality.SAME_REFERENCE
-    __ list1 __ list2:
-        r.. Equality.SAME_ORDERED
-    __ s..(list1) __ s..(list2):
-        r.. Equality.SAME_UNORDERED
-    __ s..(list1) __ s..(list2):
-        r.. Equality.SAME_UNORDERED_DEDUPED
-    r.. Equality.NO_EQUALITY
+# ____ e.. _______ E..
+#
+#
+# c_ Equality E..
+#     SAME_REFERENCE  4
+#     SAME_ORDERED  3
+#     SAME_UNORDERED  2
+#     SAME_UNORDERED_DEDUPED  1
+#     NO_EQUALITY  0
+#
+#
+# ___ check_equality list1, list2
+#     """Check if list1 and list2 are equal returning the kind of equality.
+#        Use the values in the Equality Enum:
+#        - return SAME_REFERENCE if both lists reference the same object
+#        - return SAME_ORDERED if they have the same content and order
+#        - return SAME_UNORDERED if they have the same content unordered
+#        - return SAME_UNORDERED_DEDUPED if they have the same unordered content
+#          and reduced to unique items
+#        - return NO_EQUALITY if none of the previous cases match"""
+#     __ ? __ ?
+#         r..  ?.SR
+#     __ ? __ ?
+#         r..  ?.S_O
+#     __ s.. ? __ s.. ?
+#         r..  ?.S_U
+#     __ s.. ? __ s.. ?
+#         r..  ?.S_U_DE
+#     r..  ?.N..
