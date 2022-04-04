11cc2ec42e2f094c0190e3a67742befa3234f178
syurskyi <sergejyurskyj@yahoo.com>
2022-04-03 22:02:41 -0700

    Committed algorithms templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/279/armstrong.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/279/armstrong.py
@@ -1,27 +1,27 @@
-_______ m__
-
-___ is_armstrong(n: i..) __ b..:
-    # your code ...
-
-    actual_num = n 
-
-    d.. = m__.f..(m__.log(n,10 + 1
-
-    print(d..)
-
-
-    
-    sum_digits =0
-
-    w.... n:
-        sum_digits += (n % 10)**d..
-        n //= 10
-
-    
-    
-    r.. sum_digits __ actual_num
-
-
-
-
-
+# _______ m__
+#
+# ___ is_armstrong n i.. __ b..
+#     # your code ...
+#
+#     actual_num  ?
+#
+#     d.. = m__.f.. m__.l.. ? 10 + 1
+#
+#     print d..
+#
+#
+#
+#     sum_digits 0
+#
+#     w.... ?
+#         ? +_ ? % 10 **d..
+#         n //= 10
+#
+#
+#
+#     r.. ? __ ?
+#
+#
+#
+#
+#
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/279_armstrong_numbers/save1_passed.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/279_armstrong_numbers/save1_passed.py
@@ -1,9 +1,9 @@
-___ is_armstrong(n: i..) __ b..:
-    digit_count = l..(s..(n
-    input_n = n
-    s.. = 0
-    w.... (n != 0
-        rem = n % 10
-        s.. += rem ** digit_count
-        n = n // 10
-    r.. s.. __ input_n
+# ___ is_armstrong n i.. __ b..
+#     digit_count  l.. s.. ?
+#     input_n  n
+#     s..  0
+#     w.... ? !_ 0
+#         rem  ? % 10
+#         s.. +_ ? ** d..
+#         n  ? // 10
+#     r.. s.. __ ?
