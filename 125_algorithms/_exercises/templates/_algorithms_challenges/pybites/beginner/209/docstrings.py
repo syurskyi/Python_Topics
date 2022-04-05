a1ee539f78979347aafeaad7da79e154470318e4
syurskyi <sergejyurskyj@yahoo.com>
2022-04-04 18:48:46 -0700

    Committed algorithms templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/209/docstrings.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/209/docstrings.py
@@ -1,14 +1,14 @@
-___ sum_numbers(numbers
-    """Sums numbers
-
-    :param numbers: a list of numbers, defaults to [DefaultParamVal]
-    :type numbers: list
-    ...
-    :raises TypeError: if not all numeric values passed in
-    ...
-    :return: sum of numbers
-    :rtype: int
-    """
-    p..
-
-print(sum_numbers.__doc__)
+# ___ sum_numbers numbers
+#     """Sums numbers
+#
+#     :param numbers: a list of numbers, defaults to [DefaultParamVal]
+#     :type numbers: list
+#     ...
+#     :raises TypeError: if not all numeric values passed in
+#     ...
+#     :return: sum of numbers
+#     :rtype: int
+#     """
+#     p..
+#
+# print ?. -d
