2d1ffaa8fc10debe5b9f8142c907d7fc4dae0c22
syurskyi <sergejyurskyj@yahoo.com>
2022-04-10 15:26:37 -0700

    Committed algorithms templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/254/test_scoping.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/254/test_scoping.py
@@ -1,17 +1,17 @@
-_______ p__
-
-____ scoping _______ sum_numbers
-
-
-?p__.m__.p.("arg, ret, hundreds_value", [
-    ([], 0, -1),
-    ([1, 2, 3], 6, -1),
-    ([40, 50, 60], 150, 0),
-    ([140, 50, 60], 250, 2),
-    ([140, 150, 160], 450, 6),
-    ([1140, 150, 160], 1450, 20),
-])
-___ test_sum_numbers(arg, ret, hundreds_value
-    ... sum_numbers(arg) __ ret
-    ____ scoping _______ num_hundreds
-    ... num_hundreds __ hundreds_value
+# _______ p__
+#
+# ____ ? _______ ?
+#
+#
+# ?p__.m__.p. "arg, ret, hundreds_value", [
+#     ([], 0, -1),
+#     ([1, 2, 3], 6, -1),
+#     ([40, 50, 60], 150, 0),
+#     ([140, 50, 60], 250, 2),
+#     ([140, 150, 160], 450, 6),
+#     ([1140, 150, 160], 1450, 20),
+#
+# ___ test_sum_numbers arg, ret, hundreds_value
+#     ... ? __ ?
+#     ____ ? _______ ?
+#     ... ? __ ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/254_global_vs_local_variables/save1_passed.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/254_global_vs_local_variables/save1_passed.py
@@ -1,14 +1,14 @@
-num_hundreds = -1
-
-___ sum_numbers(numbers: l..) __ i..:
-    """Sums passed in numbers returning the total, also
-       update the global variable num_hundreds with the amount
-       of times 100 fits in total"""
-    g.. num_hundreds
-    total = s..(numbers)
-    hundreds_value = l..(l..(r..(0, total, 100))) - 1
-    __ (total __ N..) o. (total < 100
-        r.. total
-    ____
-        num_hundreds = num_hundreds + hundreds_value
-        r.. total
+# num_hundreds = -1
+#
+# ___ sum_numbers numbers l.. __ i..
+#     """Sums passed in numbers returning the total, also
+#        update the global variable num_hundreds with the amount
+#        of times 100 fits in total"""
+#     g.. ?
+#     total  s.. ?
+#     hundreds_value  l.. l.. r.. 0 ? 100 - 1
+#     __ ? __ N.. o. ? < 100
+#         r.. ?
+#     ____
+#         n..  ? + ?
+#         r.. ?
