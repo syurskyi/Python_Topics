1c5e9bbe851d8d5519ca45251d8a05654c4dcccc
syurskyi <sergejyurskyj@yahoo.com>
2022-04-10 19:22:55 -0700

    Committed algorithms templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/215_validate_license_key/save1_nopass.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/215_validate_license_key/save1_nopass.py
@@ -1,12 +1,12 @@
-_______ __
-
-
-___ validate_license key s..  __ b..
-    """Write a regex that matches a PyBites license key
-       (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
-    """
-    key_end = ''.j..(key.s..('-')[1:])
-    start = b..(__.m..(r'^PB', key
-    split_count = [b..(x) ___ x __ [l..(entry)  __ 8 ___ entry __ key_end]]
-    character_check = b..(__.m..('^[a-zA-Z0-9]+$', key_end
-    r.. start a.. split_count a.. character_check
+# _______ __
+#
+#
+# ___ validate_license key s..  __ b..
+#     """Write a regex that matches a PyBites license key
+#        (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
+#     """
+#     key_end  ''.j.. ?.s.. '-' 1|
+#     start  b.. __.m.. _'^PB' ?
+#     split_count  b.. x ___ ? __  l.. entry  __ 8 ___ ? __ ?
+#     character_check  b.. __.m.. '^[a-zA-Z0-9]+$' ?
+#     r.. s.. s.. c
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/215_validate_license_key/save2_passed.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/215_validate_license_key/save2_passed.py
@@ -1,14 +1,14 @@
-_______ __
-
-
-___ validate_license key s..  __ b..
-    """Write a regex that matches a PyBites license key
-       (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
-    """
-    start = b..(__.m..(r'^PB', key
-    split_count = a..(b..(x) 
-                      ___ x __ [l..(entry)  __ 8
-                                ___ entry __ key.s..('-')[1:]])
-    character_check = b..(
-        __.m..('^[a-zA-Z0-9]+$', ''.j..(key.s..('-')[1:])))
-    r.. start a.. split_count a.. character_check
+# _______ __
+#
+#
+# ___ validate_license key s..  __ b..
+#     """Write a regex that matches a PyBites license key
+#        (e.g. PB-U8N435EH-PG65PW87-IXPWQG5T-898XSZI4)
+#     """
+#     start  b.. __.m.. _'^PB' ?
+#     split_count  a.. b.. x
+#                       ___ ? __ l.. entry  __ 8
+#                                 ___ ? __ ?.s.. '-' 1|
+#     character_check  b..(
+#         __.m.. '^[a-zA-Z0-9]+$' ''.j.. ?.s.. '-' 1|
+#     r.. s.. s.. c
