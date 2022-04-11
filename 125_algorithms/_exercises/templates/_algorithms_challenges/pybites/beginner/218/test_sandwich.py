d6983499c517b3d32c7b02d31ada696e56182a96
syurskyi <sergejyurskyj@yahoo.com>
2022-04-10 19:31:34 -0700

    Committed algorithms templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/218/test_sandwich.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/218/test_sandwich.py
@@ -1,29 +1,29 @@
-____ Previous.sandwich _______ sandwich
-
-SANDWICH_BACON = """=== Upper bread slice ===
-bacon / lettuce / tomato
-=== Lower bread slice ===
-"""
-SANDWICH_EGG = """=== Upper bread slice ===
-fried egg / tomato / cucumber
-=== Lower bread slice ===
-"""
-
-
-@sandwich
-___ add_ingredients(ingredients
-    print(' / '.j..(ingredients
-
-
-___ test_bacon_sandwich(capfd
-    ingredients =  'bacon', 'lettuce', 'tomato'
-    add_ingredients(ingredients)
-    a.. = ?.r .. 0]
-    ... a.. __ SANDWICH_BACON
-
-
-___ test_fried_egg_sandwich(capfd
-    ingredients =  'fried egg', 'tomato', 'cucumber'
-    add_ingredients(ingredients)
-    a.. = ?.r .. 0]
-    ... a.. __ SANDWICH_EGG
+# ____ Previous.? _______ ?
+#
+# SANDWICH_BACON = """=== Upper bread slice ===
+# bacon / lettuce / tomato
+# === Lower bread slice ===
+# """
+# SANDWICH_EGG = """=== Upper bread slice ===
+# fried egg / tomato / cucumber
+# === Lower bread slice ===
+# """
+#
+#
+# ??
+# ___ add_ingredients ingredients
+#     print(' / '.j.. ?
+#
+#
+# ___ test_bacon_sandwich capfd
+#     ingredients   'bacon', 'lettuce', 'tomato'
+#     ? ?
+#     a..  ?.r .. 0
+#     ... a.. __ ?
+#
+#
+# ___ test_fried_egg_sandwich capfd
+#     ingredients  'fried egg', 'tomato', 'cucumber'
+#     ? ?
+#     a..  ?.r .. 0
+#     ... a.. __ ?
