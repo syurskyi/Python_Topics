8cb0a313ce312595a3b4178f48d1f4393b6303f0
syurskyi <sergejyurskyj@yahoo.com>
2022-04-10 19:18:12 -0700

    Committed algorithms templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/241/numbers_to_dec.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/241/numbers_to_dec.py
@@ -1,21 +1,21 @@
-____ t___ _______ L..
-
-
-___ list_to_decimal(nums: L..[i..]) __ i..:
-    """Accept a list of positive integers in the range(0, 10)
-       and return a integer where each int of the given list represents
-       decimal place values from first element to last.  E.g
-        [1,7,5] => 175
-        [0,3,1,2] => 312
-        Place values are 10**n where n represents the digit position
-        Eg to calculate 1345, we have 5 1's, 4 10's, 3 100's and 1 1000's
-        1,     3  ,  4  , 5
-        1000's, 100's, 10's, 1's
-    """
-    ___ num __ nums:
-        __ isi..(num, b..) o. n.. isi..(num, i..
-            r.. T..
-        ____ n.. num __ r..(0, 10
-            r.. V...
-
-    r.. i..(''.j.. m..(s.., nums)))
+# ____ t___ _______ L..
+#
+#
+# ___ list_to_decimal nums L.. i.. __ i..
+#     """Accept a list of positive integers in the range(0, 10)
+#        and return a integer where each int of the given list represents
+#        decimal place values from first element to last.  E.g
+#         [1,7,5] => 175
+#         [0,3,1,2] => 312
+#         Place values are 10**n where n represents the digit position
+#         Eg to calculate 1345, we have 5 1's, 4 10's, 3 100's and 1 1000's
+#         1,     3  ,  4  , 5
+#         1000's, 100's, 10's, 1's
+#     """
+#     ___ num __ ?
+#         __ isi.. ? b.. o. n.. isi.. ? i..
+#             r.. T..
+#         ____ n.. ? __ r.. 0 10
+#             r.. V...
+#
+#     r.. i.. ''.j.. m.. s.. ?
