4c1cbf07f34f6cee987cf52da918f76fd1f290a4
syurskyi <sergejyurskyj@yahoo.com>
2022-04-13 15:13:32 -0700

    Committed algorithms intermediate templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/60_v5/uno.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/60_v5/uno.py
@@ -1,26 +1,26 @@
-____ c.. _______ n..
-
-SUITS 'Red Green Yellow Blue'.s..
-
-UnoCard n..('UnoCard', 'suit name')
-
-SUIT_VALUES '1,2,3,4,5,6,7,8,9,Draw Two,Skip,Reverse'.s..(',')
-
-
-___ create_uno_deck
-    """Create a deck of 108 Uno cards.
-       Return a list of UnoCard namedtuples
-       (for cards w/o suit use None in the namedtuple)"""
-    cards    # list
-    ___ suit __ SUITS:
-        add_suit(cards.a.., suit)
-    ___ value __ 'Wild,Wild Draw Four'.s..(',') * 4:
-        cards.a..(UnoCard(N.., value
-    r.. cards
-
-
-___ add_suit(cards_append, suit: s..
-    cards_append(UnoCard(suit, '0'
-    ___ value __ SUIT_VALUES:
-        cards_append(UnoCard(suit, value
-        cards_append(UnoCard(suit, value
+# ____ c.. _______ n..
+#
+# SUITS 'Red Green Yellow Blue'.s..
+#
+# UnoCard n.. 'UnoCard', 'suit name'
+#
+# SUIT_VALUES '1,2,3,4,5,6,7,8,9,Draw Two,Skip,Reverse'.s..(',')
+#
+#
+# ___ create_uno_deck
+#     """Create a deck of 108 Uno cards.
+#        Return a list of UnoCard namedtuples
+#        (for cards w/o suit use None in the namedtuple)"""
+#     cards    # list
+#     ___ suit __ ?
+#         add_suit ?.a.. ?
+#     ___ value __ 'Wild,Wild Draw Four'.s.. ',' * 4
+#         ?.a.. U.. N.. ?
+#     r.. ?
+#
+#
+# ___ add_suit cards_append suit| s..
+#     cards_append U.. ? '0'
+#     ___ value __ S..
+#         ? ? ? ?
+#         ? ? ? ?
