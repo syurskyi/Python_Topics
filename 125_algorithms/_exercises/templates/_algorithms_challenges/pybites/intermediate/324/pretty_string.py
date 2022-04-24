23430213b0d24963e00b843d552eaea1c2a5fc3f
syurskyi <sergejyurskyj@yahoo.com>
2022-04-24 12:22:19 -0700

    Committed algorithms intermediate templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/324/pretty_string.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/324/pretty_string.py
@@ -1,7 +1,7 @@
-_______ pp__
-____ t___ _______ A..
-
-
-___ pretty_string(obj: A..) __ s..
-    pp pp__.p..(obj, depth=2, width=60)
-    r.. pp
+# _______ pp__
+# ____ t___ _______ A..
+#
+#
+# ___ pretty_string obj A.. __ s..
+#     pp pp__.p.. ? d.._2 w.._60
+#     r.. ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/324_v2/pretty_string.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/324_v2/pretty_string.py
@@ -1,7 +1,7 @@
-_______ pp__
-____ t___ _______ A..
-
-
-___ pretty_string(obj: A..) __ s..
-    # TODO: your code
-    r.. pp__.p..(obj, width=60, depth=2)
+# _______ pp__
+# ____ t___ _______ A..
+#
+#
+# ___ pretty_string obj A.. __ s..
+#     # TODO: your code
+#     r.. pp__.p.. ? w.._60 d.._2
