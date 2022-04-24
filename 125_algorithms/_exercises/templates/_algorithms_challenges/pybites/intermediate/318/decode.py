ac926201009a0de3d5184a66f168758cc05cbbb1
syurskyi <sergejyurskyj@yahoo.com>
2022-04-24 13:52:13 -0700

    Committed algorithms intermediate templates

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/318/decode.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/318/decode.py
@@ -1,14 +1,14 @@
-_______ base64
-____ t___ _______ L..  # will remove with 3.9
-
-
-___ get_credit_cards(data: bytes) __ L..[s..]:
-    """Decode the base64 encoded data which gives you a csv
-    of "first_name,last_name,credit_card", from which you have
-    to extract the credit card numbers.
-    """
-    decoded_data base64.b..(data)
-    decoded_string decoded_data.d.. "ascii")
-    decoded_list [row.s..(",") ___ row __ decoded_string.s...s..("\n")]
-    credit_card_numbers [decoded_list[i][2] ___ i __ r..(l..(decoded_list __ i !_ 0]
-    r.. credit_card_numbers
+# _______ base64
+# ____ t___ _______ L..  # will remove with 3.9
+#
+#
+# ___ get_credit_cards data bytes __ L.. s..
+#     """Decode the base64 encoded data which gives you a csv
+#     of "first_name,last_name,credit_card", from which you have
+#     to extract the credit card numbers.
+#     """
+#     decoded_data ?.b.. ?
+#     decoded_string ?.d.. "ascii"
+#     decoded_list row.s.. "," ___ ? __ ?.s...s.. "\n"
+#     credit_card_numbers ? ? 2 ___ i __ r.. l.. ? __ i !_ 0
+#     r.. ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/318_v2/decode.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/318_v2/decode.py
@@ -1,15 +1,15 @@
-_______ base64
-_______ c__
-____ t___ _______ L..  # will remove with 3.9
-
-
-___ get_credit_cards(data: bytes) __ L..[s..]:
-    """Decode the base64 encoded data which gives you a csv
-    of "first_name,last_name,credit_card", from which you have
-    to extract the credit card numbers.
-    """
-    text base64.decodebytes(data).d.. )
-    card_nums [row.s..(',' 2  ___ row __ text.s.. ]
-    card_nums.p.. 0)            # remove header
-
-    r.. card_nums
+# _______ base64
+# _______ c__
+# ____ t___ _______ L..  # will remove with 3.9
+#
+#
+# ___ get_credit_cards data bytes __ L.. s..
+#     """Decode the base64 encoded data which gives you a csv
+#     of "first_name,last_name,credit_card", from which you have
+#     to extract the credit card numbers.
+#     """
+#     text ?.d.. ?.d..
+#     card_nums row.s.. ',' 2  ___ ? __ ?.s..
+#     ?.p.. 0            # remove header
+#
+#     r.. ?
