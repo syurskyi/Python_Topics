d07477204f1d2ab7bea0947c1c03bfd8fc54a9ce
syurskyi <sergejyurskyj@yahoo.com>
2022-04-29 12:42:07 -0700

    Committed algorithms beginner exercices

--- 125_algorithms/_exercises/exercises/_algorithms_challenges/pybites/beginner/5/names.py
+++ 125_algorithms/_exercises/exercises/_algorithms_challenges/pybites/beginner/5/names.py
@@ -1,27 +1,27 @@
-# NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
-#          'julian sequeira', 'sandra bullock', 'keanu reeves',
-#          'julbob pybites', 'bob belderbos', 'julian sequeira',
-#          'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
-#
-#
-# ___ dedup_and_title_case_names names
-#     """Should return a list of title cased names,
-#        each name appears only once"""
-#     ___ i __ r.. l.. ?
-#         ? ? _ ? ?.t..
-#     r.. s.. l.. d...f.. ?
-#
-#
-# ___ sort_by_surname_desc names
-#     """Returns names list sorted desc by surname"""
-#     names _ ? ?
-#     r.. s.. ? r.._T.. k.. _ l.... x ?.s.. -1
-#
-#
-# ___ shortest_first_name names
-#     """Returns the shortest first name (str).
-#        You can assume there is only one shortest name.
-#     """
-#     names  ? ?
-#     fname, lname _ s.. ? k.. _ l.... x l.. ?.s..  0 0 .s..
-#     r.. ?
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+
+def dedup_and_title_case_names(names):
+    """Should return a list of title cased names,
+       each name appears only once"""
+    for i in range(len(names)):
+        names[i] = names[i].title()
+    return sorted(list(dict.fromkeys(names)))
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+    return sorted(names, reverse=True, key = lambda x: x.split()[-1])
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names = dedup_and_title_case_names(names)
+    fname, lname = sorted(names, key=lambda x: len(x.split()[0]))[0].split()
+    return  fname
