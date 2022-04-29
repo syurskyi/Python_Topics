176478e086918547fa4d2695361e8e48f84e6dd6
syurskyi <sergejyurskyj@yahoo.com>
2022-04-29 14:02:08 -0700

    Committed algorithms beginner exercices

--- 125_algorithms/_exercises/exercises/_algorithms_challenges/pybites/beginner/5_v2/test_names.py
+++ 125_algorithms/_exercises/exercises/_algorithms_challenges/pybites/beginner/5_v2/test_names.py
@@ -1,42 +1,42 @@
-# ____ names _______  N.. d..
-#                    s.. s..
-#
-# PY_CONTENT_CREATORS  ['brian okken', 'michael kennedy', 'trey hunner',
-#                        'matt harrison', 'julian sequeira', 'dan bader',
-#                        'michael kennedy', 'brian okken', 'dan bader']
-#
-#
-# ___ test_dedup_and_title_case_names
-#     names  ? ?
-#     ... ?.c.. 'Bob Belderbos' __ 1
-#     ... ?.c.. 'julian sequeira' __ 0
-#     ... ?.c.. 'Brad Pitt' __ 1
-#     ... l.. ? __ 10
-#     ... a..(n.t.. __ ? ___ ? __ N..
-#
-#
-# ___ test_dedup_and_title_case_names_different_names_list
-#     actual  s.. ? ?
-#     expected  ['Brian Okken', 'Dan Bader', 'Julian Sequeira',
-#                 'Matt Harrison', 'Michael Kennedy', 'Trey Hunner']
-#     ... ? __ ?
-#
-#
-# ___ test_sort_by_surname_desc
-#     names  ? ?
-#     ... ? 0 __ 'Julian Sequeira'
-#     ... ? -1 __ 'Alec Baldwin'
-#
-#
-# ___ test_sort_by_surname_desc_different_names_list
-#     names  ? ?
-#     ... ? 0 __ 'Julian Sequeira'
-#     ... ? -1 __ 'Dan Bader'
-#
-#
-# ___ test_shortest_first_name
-#     ... ? ? __ 'Al'
-#
-#
-# ___ test_shortest_first_name_different_names_list
-#     ... ? ? __ 'Dan'
+from names import (NAMES, dedup_and_title_case_names, sort_by_surname_desc, shortest_first_name)
+
+
+PY_CONTENT_CREATORS = ['brian okken', 'michael kennedy', 'trey hunner',
+                       'matt harrison', 'julian sequeira', 'dan bader',
+                       'michael kennedy', 'brian okken', 'dan bader']
+
+
+def test_dedup_and_title_case_names():
+    names = dedup_and_title_case_names(NAMES)
+    assert names.count('Bob Belderbos') == 1
+    assert names.count('julian sequeira') == 0
+    assert names.count('Brad Pitt') == 1
+    assert len(names) == 10
+    assert all(n.title() in  names for n in  NAMES)
+
+
+def test_dedup_and_title_case_names_different_names_list():
+    actual = sorted(dedup_and_title_case_names(NAMES))
+    expected = ['Brian Okken', 'Dan Bader', 'Julian Sequeira',
+                'Matt Harrison', 'Michael Kennedy', 'Trey Hunner']
+    assert actual == expected
+
+
+def test_sort_by_surname_desc():
+    names = dedup_and_title_case_names(NAMES)
+    assert names[0] == 'Julian Sequeira'
+    assert names[-1] == 'Alec Baldwin'
+
+
+def test_sort_by_surname_desc_different_names_list():
+    names = sort_by_surname_desc(PY_CONTENT_CREATORS)
+    assert  names[0] == 'Julian Sequeira'
+    assert  names[-1] == 'Dan Bader'
+
+
+def test_shortest_first_name():
+    assert  shortest_first_name(NAMES) == 'Al'
+
+
+def test_shortest_first_name_different_names_list():
+    assert shortest_first_name(PY_CONTENT_CREATORS) == 'Dan'
