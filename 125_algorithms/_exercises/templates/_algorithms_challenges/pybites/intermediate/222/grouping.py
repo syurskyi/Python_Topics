a52aa4c9f550c6789165e5df4f5c3cf3bff4194d
syurskyi <sergejyurskyj@yahoo.com>
2022-05-02 23:54:15 -0700

    Committed algorithms intermediate template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/222/grouping.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/222/grouping.py
@@ -1,32 +1,32 @@
-_______ types
-____ i.. _______ i..
-
-
-___ g.. iterable, n
-    """Splits an iterable set into groups of size n and a group
-       of the remaining elements if needed.
-
-       Args:
-         iterable (list): The list whose elements are to be split into
-                          groups of size n.
-         n (int): The number of elements per group.
-
-       Returns:
-         list: The list of groups of size n,
-               where each group is a list of n elements.
-    """
-
-    __ n.. isi..(iterable, types.GeneratorType
-      input (ele ___ ele __ iterable)
-    ____
-      input iterable
-
-    result    # list
-    w... T...
-      section l..(islice(input, n
-      __ section:
-        result.a..(section)
-      ____
-        _____
-
-    r.. result
+# _______ types
+# ____ i.. _______ i..
+#
+#
+# ___ g.. iterable, n
+#     """Splits an iterable set into groups of size n and a group
+#        of the remaining elements if needed.
+#
+#        Args:
+#          iterable (list): The list whose elements are to be split into
+#                           groups of size n.
+#          n (int): The number of elements per group.
+#
+#        Returns:
+#          list: The list of groups of size n,
+#                where each group is a list of n elements.
+#     """
+#
+#     __ n.. isi.. i.. t__.G..
+#       i.. ele ___ ? __ i..
+#     ____
+#       i.. i..
+#
+#     result    # list
+#     w... T...
+#       section l.. i.. i.. n
+#       __ ?
+#         ?.a.. ?
+#       ____
+#         _____
+#
+#     r.. ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/222_v2/grouping.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/222_v2/grouping.py
@@ -1,35 +1,35 @@
-_______ types
-____ i.. _______ i..
-
-
-___ g.. iterable, n
-    """Splits an iterable set into groups of size n and a group
-       of the remaining elements if needed.
-
-       Args:
-         iterable (list): The list whose elements are to be split into
-                          groups of size n.
-         n (int): The number of elements per group.
-
-       Returns:
-         list: The list of groups of size n,
-               where each group is a list of n elements.
-    """
-
-    results    # list
-    iterable l..(iterable)
-    ___ i __ r..(0,l..(iterable),n
-        results.a..(l..(islice(iterable,i,i+n)))
-
-    r.. results
-
-
-
-    r.. l..(islice(iterable,0,l..(iterable),n
-
-
-__ _____ __ _____
-    iterable [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    n 3
-    ret g.. iterable, n)
-    print(ret)
+# _______ t..
+# ____ i.. _______ i..
+#
+#
+# ___ g.. iterable, n
+#     """Splits an iterable set into groups of size n and a group
+#        of the remaining elements if needed.
+#
+#        Args:
+#          iterable (list): The list whose elements are to be split into
+#                           groups of size n.
+#          n (int): The number of elements per group.
+#
+#        Returns:
+#          list: The list of groups of size n,
+#                where each group is a list of n elements.
+#     """
+#
+#     results    # list
+#     iterable l.. ?
+#     ___ i __ r.. 0,l.. ? ?
+#         ?.a.. l.. i.. ? ? ?+?
+#
+#     r.. ?
+#
+#
+#
+#     r.. l.. i.. i.. 0 l.. i.. ?
+#
+#
+# __ _____ __ _____
+#     iterable [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#     n 3
+#     ret g.. ? ?
+#     print ?
