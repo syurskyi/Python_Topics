c0d80a2bdd1917b5b33df5363ba01f708e02e579
syurskyi <sergejyurskyj@yahoo.com>
2022-05-05 14:23:44 -0700

    Committed algorithms intermediate template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/236/files.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/236/files.py
@@ -1,52 +1,46 @@
-____ p.. _______ PosixPath
-_______ difflib
-_______ __
-
-
-___ get_matching_files(directory: PosixPath, filter_str: s..) __ l..:
-    """Get all file names in "directory" and (case insensitive) match the ones
-       that exactly match "filter_str"
-
-       In case there is no exact match, return closely matching files.
-       If there are no closely matching files either, return an empty list.
-       (Return file names, not full paths).
-
-       For example:
-
-       d = Path('.')
-       files in dir: bite1 test output
-
-       get_matching_files(d, 'bite1') => ['bite1']
-       get_matching_files(d, 'Bite') => ['bite1']
-       get_matching_files(d, 'pybites') => ['bite1']
-       get_matching_files(d, 'test') => ['test']
-       get_matching_files(d, 'test2') => ['test']
-       get_matching_files(d, 'output') => ['output']
-       get_matching_files(d, 'o$tput') => ['output']
-       get_matching_files(d, 'nonsense') => []
-    """
-
-
-    
-    exact_matches    # list
-    close_matches    # list
-    
-    files __.listdir(directory)
-    ___ file __ ?
-
-        __ file.l.. __ filter_str:
-            exact_matches.a..(file)
-    
-        
-    __ exact_matches:
-        r.. exact_matches
-
-
-    close_matches difflib.get_close_matches(filter_str,(file.l.. ___ file __ ?
-    r.. close_matches
-
-
-
-
-
-
+# ____ p.. _______ P..
+# _______ d..
+# _______ __
+#
+#
+# ___ get_matching_files directory P.. filter_str s.. __ l..
+#     """Get all file names in "directory" and (case insensitive) match the ones
+#        that exactly match "filter_str"
+#
+#        In case there is no exact match, return closely matching files.
+#        If there are no closely matching files either, return an empty list.
+#        (Return file names, not full paths).
+#
+#        For example:
+#
+#        d = Path('.')
+#        files in dir: bite1 test output
+#
+#        get_matching_files(d, 'bite1') => ['bite1']
+#        get_matching_files(d, 'Bite') => ['bite1']
+#        get_matching_files(d, 'pybites') => ['bite1']
+#        get_matching_files(d, 'test') => ['test']
+#        get_matching_files(d, 'test2') => ['test']
+#        get_matching_files(d, 'output') => ['output']
+#        get_matching_files(d, 'o$tput') => ['output']
+#        get_matching_files(d, 'nonsense') => []
+#     """
+#
+#
+#
+#     exact_matches    # list
+#     close_matches    # list
+#
+#     files __.l.. ?
+#     ___ file __ ?
+#
+#         __ ?.l.. __ f..
+#             e__.a.. ?
+#
+#
+#     __ e..
+#         r.. e..
+#
+#
+#     close_matches d__.g.. f.. f__.l.. ___ f.. __ ?
+#     r.. ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/236/test_files.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/236/test_files.py
@@ -1,47 +1,47 @@
-_______ p__
-
-____ files _______ get_matching_files
-
-FILES ('bite.html commands.sh out_grepped pytest_testrun.out '
-         'pytest_timings.out test_timings.py timings-template.py '
-         'timings.py').s..
-
-
-?p__.m__.p. "filter_str, expected", [
-   ('bite1',  'bite1' ),
-   ('Bite',  'bite1' ),
-   ('pybites',  'bite1' ),
-   ('test',  'test' ),
-   ('test2',  'test' ),
-   ('output',  'output' ),
-   ('o$tput',  'output' ),
-   ('nonsense',    # list),
-])
-___ test_example_docstring(tmp_path, filter_str, e..
-    # let's create some files in tmp
-    ___ fi __ 'bite1 test output'.s.. :
-        o.. tmp_path / fi, 'a').c..
-    a.. get_matching_files(tmp_path, filter_str)
-    ... s.. a.. __ s..(e..)
-
-
-?p__.m__.p. "filter_str, expected", [
-   ('bite.html',  'bite.html' ),
-   ('bite.htm1',  'bite.html' ),
-   ('bit$.htm1',  'bite.html' ),
-   ('bite.txt',  'bite.html' ),
-   ('_timing',  'timings.py', 'test_timings.py' ),
-   ('commando',  'commands.sh' ),
-   ('pytest_testruns.out',  'pytest_testrun.out', 'pytest_timings.out' ),
-   ('out_greped',  'out_grepped' ),
-   ('nonsensical',    # list),
-   ('commands.py',  'commands.sh' ),
-   ('pytest_t',  'pytest_testrun.out', 'pytest_timings.out' ),
-   ('timings-templates.PY',  'timings-template.py' ),
-])
-___ test_other_files(tmp_path, filter_str, e..
-    # let's create some files in tmp
-    ___ fi __ FILES:
-        o.. tmp_path / fi, 'a').c..
-    a.. get_matching_files(tmp_path, filter_str)
-    ... s.. a.. __ s..(e..)
+# _______ p__
+#
+# ____ ? _______ ?
+#
+# FILES ('bite.html commands.sh out_grepped pytest_testrun.out '
+#          'pytest_timings.out test_timings.py timings-template.py '
+#          'timings.py').s..
+#
+#
+# ?p__.m__.p. "filter_str, expected", [
+#    ('bite1',  'bite1' ),
+#    ('Bite',  'bite1' ),
+#    ('pybites',  'bite1' ),
+#    ('test',  'test' ),
+#    ('test2',  'test' ),
+#    ('output',  'output' ),
+#    ('o$tput',  'output' ),
+#    ('nonsense',    # list),
+#
+# ___ test_example_docstring tmp_path filter_str e..
+#     # let's create some files in tmp
+#     ___ fi __ 'bite1 test output'.s..
+#         o.. ? / ? _ .c..
+#     a.. ? ? ?
+#     ... s.. a.. __ s.. e..
+#
+#
+# ?p__.m__.p. "filter_str, expected", [
+#    ('bite.html',  'bite.html' ),
+#    ('bite.htm1',  'bite.html' ),
+#    ('bit$.htm1',  'bite.html' ),
+#    ('bite.txt',  'bite.html' ),
+#    ('_timing',  'timings.py', 'test_timings.py' ),
+#    ('commando',  'commands.sh' ),
+#    ('pytest_testruns.out',  'pytest_testrun.out', 'pytest_timings.out' ),
+#    ('out_greped',  'out_grepped' ),
+#    ('nonsensical',    # list),
+#    ('commands.py',  'commands.sh' ),
+#    ('pytest_t',  'pytest_testrun.out', 'pytest_timings.out' ),
+#    ('timings-templates.PY',  'timings-template.py' ),
+#
+# ___ test_other_files tmp_path filter_str e..
+#     # let's create some files in tmp
+#     ___ fi __ ?
+#         o.. ? / ? _ .c..
+#     a.. ? ?
+#     ... s.. a.. __ s.. e..
