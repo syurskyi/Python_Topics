ae5505da8235e628cc3ab433f9ff70152b339e52
syurskyi <sergejyurskyj@yahoo.com>
2022-05-05 14:52:01 -0700

    Committed algorithms intermediate template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/243/ips.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/243/ips.py
@@ -1,59 +1,59 @@
-_______ j__
-____ c.. _______ s__
-____ d.. _______ d..
-____ ipaddress _______ AddressValueError, IPv4Address, IPv4Network
-____ p.. _______ P..
-____ t___ _______ L..
-
-
-??(frozen=T..)
-c_ ServiceIPRange:
-    """
-    Represents an IPv4 public network range, allocated by AWS for use with
-    a specific service and region.
-    """
-
-    service: s..
-    region: s..
-    cidr: IPv4Network
-
-    ___ -s
-        r.. (f"{cidr} is allocated to the {service} "
-                f"service in the {region} region")
-
-
-___ parse_ipv4_service_ranges(source: P..) __ L..[ServiceIPRange]:
-    """
-    Given a JSON file containing AWS public IP addresses, return a list of
-    ServiceIPRange objects representing all IPv4 network ranges. See also:
-
-    https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html
-    """
-    data j__.l.. (source.read_text
-    w__ suppress(AddressValueError
-        prefixes data["prefixes"]
-        ipv4_service_ranges [
-            ServiceIPRange(
-                service=prefix["service"],
-                region=prefix["region"],
-                cidr=IPv4Network(prefix["ip_prefix"]),
-            )
-            ___ prefix __ prefixes
-        ]
-    r.. ipv4_service_ranges
-
-
-___ get_aws_service_range(address: s..,
-                          service_ranges: l..) __ L..[ServiceIPRange]:
-    """
-    Return a list of ServiceIPRange objects representing all AWS public
-    IP ranges that contain `address`. Raise a ValueError if `address`
-    is not a valid IPv4 address.
-    """
-    ___
-        ipv4_address IPv4Address(address)
-    ______ AddressValueError:
-        r.. V...("Address must be a valid IPv4 address")
-
-    r.. [range_ ___ range_ __ service_ranges
-            __ ipv4_address __ range_.cidr]
+# _______ j__
+# ____ c.. _______ s__
+# ____ d.. _______ d..
+# ____ i.. _______ A.. I.. I..
+# ____ p.. _______ P..
+# ____ t___ _______ L..
+#
+#
+# ?? frozen=T..
+# c_ ServiceIPRange
+#     """
+#     Represents an IPv4 public network range, allocated by AWS for use with
+#     a specific service and region.
+#     """
+#
+#     service: s..
+#     region: s..
+#     cidr: I..
+#
+#     ___ -s
+#         r.. _* c.. is allocated to the s..
+#                 _*service in the r.. region
+#
+#
+# ___ parse_ipv4_service_ranges source P.. __ L.. S..
+#     """
+#     Given a JSON file containing AWS public IP addresses, return a list of
+#     ServiceIPRange objects representing all IPv4 network ranges. See also:
+#
+#     https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html
+#     """
+#     data j__.l.. ?.r..
+#     w__ s.. A..
+#         prefixes ? "prefixes"
+#         ipv4_service_ranges
+#             S..
+#                 s.._p.. service
+#                 r.._p.. region
+#                 c.._I.. p.. ip_prefix
+#
+#             ___ prefix __ ?
+#
+#     r.. ?
+#
+#
+# ___ get_aws_service_range address s..
+#                           service_ranges l.. __ L.. S..
+#     """
+#     Return a list of ServiceIPRange objects representing all AWS public
+#     IP ranges that contain `address`. Raise a ValueError if `address`
+#     is not a valid IPv4 address.
+#     """
+#     ___
+#         ipv4_address I.. ?
+#     ______ A..
+#         r.. V...("Address must be a valid IPv4 address")
+#
+#     r.. range_ ___ ? __ s..
+#             __ i.. __ ?.c..
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/243/test_ips.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/intermediate/243/test_ips.py
@@ -1,49 +1,48 @@
-_______ __
-____ p.. _______ P..
-____ ipaddress _______ IPv4Network
-____ u__.r.. _______ u..
-
-_______ p__
-
-____ ips _______ (ServiceIPRange, parse_ipv4_service_ranges,
-                 get_aws_service_range)
-
-URL "https://bites-data.s3.us-east-2.amazonaws.com/ip-ranges.json"
-TMP __.g.. TMP  /tmp
-PATH P..(TMP, "ip-ranges.json")
-IP IPv4Network('192.0.2.8/29')
-
-
-?p__.f.. s.._='module'
-___ json_file
-    """Import data into tmp folder"""
-    u..(URL, PATH)
-    r.. PATH
-
-
-___ test_ServiceIPRange
-    serv ServiceIPRange(
-        service='Tester',
-        region='Bolton',
-        cidr=IPv4Network('158.152.1.0/24')
-    )
-    ... s..(serv) __ '158.152.1.0/24 is allocated to the Tester service in the Bolton region'
-
-
-___ test_parse_ipv4_service_ranges(json_file
-    services parse_ipv4_service_ranges(json_file)
-    ... l..(services) __ 1886
-    ... services[0].region __ 'eu-west-1'
-    ... services[0].service __ 'AMAZON'
-
-
-___ test_get_aws_service_range(json_file
-    services parse_ipv4_service_ranges(json_file)
-    service_range get_aws_service_range('13.248.118.1', services)
-    ... l..(service_range) __ 2
-    ... s..(s.region ___ s __ service_range) __ {'eu-west-1'}
-    ... s..(s.service ___ s __ service_range) __ {'AMAZON', 'GLOBALACCELERATOR'}
-    ... get_aws_service_range('158.152.1.65', services) __    # list
-    w__ p__.r.. V...) __ exc:
-        get_aws_service_range('0.0.0.256', services)
-    ... 'Address must be a valid IPv4 address' __ s..(exc.v..)
+# _______ __
+# ____ p.. _______ P..
+# ____ i.. _______ I..
+# ____ u__.r.. _______ u..
+#
+# _______ p__
+#
+# ____ ? _______ ? ? ?
+#
+# URL "https://bites-data.s3.us-east-2.amazonaws.com/ip-ranges.json"
+# TMP __.g.. ?  /tmp
+# PATH P.. ? "ip-ranges.json"
+# IP I.. '192.0.2.8/29'
+#
+#
+# ?p__.f.. s.._='module'
+# ___ json_file
+#     """Import data into tmp folder"""
+#     u.. ? ?
+#     r.. ?
+#
+#
+# ___ test_ServiceIPRange
+#     serv ?
+#         service_'Tester'
+#         region_'Bolton'
+#         cidr_I.. '158.152.1.0/24'
+#
+#     ... s.. ? __ '158.152.1.0/24 is allocated to the Tester service in the Bolton region'
+#
+#
+# ___ test_parse_ipv4_service_ranges json_file
+#     services ? ?
+#     ... l.. ? __ 1886
+#     ... ? 0 .r.. __ 'eu-west-1'
+#     ... s.. 0 .s.. __ 'AMAZON'
+#
+#
+# ___ test_get_aws_service_range json_file
+#     services ? ?
+#     service_range ? '13.248.118.1' ?
+#     ... l.. ? __ 2
+#     ... s.. s.r.. ___ ? __ ? __ eu-west-1
+#     ... s.. s.s.. ___ ? __ ? __ AMAZON', 'GLOBALACCELERATOR
+#     ... ? '158.152.1.65' ? __    # list
+#     w__ p__.r.. V... __ exc
+#         ? '0.0.0.256' ?
+#     ... 'Address must be a valid IPv4 address' __ s.. ?.v..
