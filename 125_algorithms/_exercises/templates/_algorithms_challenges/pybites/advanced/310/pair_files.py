b973aa9e820f08260112a0ca8973d26bf571e35d
syurskyi <sergejyurskyj@yahoo.com>
2022-05-28 20:32:11 -0700

    Committed algorithms advanced template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/310/pair_files.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/310/pair_files.py
@@ -1,73 +1,54 @@
-_______ __
-____ c.. _______ d..
-___ pair_files filenames
-    """
-    Function that pairs filenames
-
-    filenames: list[str] containing filenames
-    returns: list[tuple[str, str]] containing filename pairs
-    """
-    # TODO: You code
-
-    mapping d.. l..
-
-
-    ___ file __ filenames:
-        
-
-        m.. __.s.. _ (\S+?_S[1-9][0-9]?_L\d{3})_R[12]_([0-9]{2}[1-9]).fastq.gz$',file,flags=__.I)
-
-        __ m..
-            first_part m...g.. 1).l..
-            second_part m...g.. 2).l..
-
-            combined first_part + second_part
-
-            mapping[combined].a..(file)
-
-    
-    result    # list
-    ___ value __ mapping.v..
-        __ l.. ? __ 2:
-
-            __ '_R1_' __ value[1]:
-                value.r..
-            result.a..(t..(value
-
-
-    r.. ?
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Set up for your convenience during testing
-__ _______ __ _______
-    filenames [
-        "Sample1_S1_L001_R1_001.FASTQ.GZ",
-        "Sample1_S1_L001_R2_001.fastq.gz",
-        "Sample2_S2_L001_R1_001.fastq.gz",
-        "sample2_s2_l001_r2_001.fastq.gz",
-    ]
-    print(pair_files(filenames
-    # ('Sample1_S1_L001_R1_001.FASTQ.GZ', 'Sample1_S1_L001_R2_001.fastq.gz')
-    # ('Sample2_S2_L001_R1_001.fastq.gz', 'sample2_s2_l001_r2_001.fastq.gz')
-
-    ___ pair __ pair_files(filenames
-        print(pair)
+# _______ __
+# ____ c.. _______ d..
+# ___ pair_files filenames
+#     """
+#     Function that pairs filenames
+#
+#     filenames: list[str] containing filenames
+#     returns: list[tuple[str, str]] containing filename pairs
+#     """
+#     # TODO: You code
+#
+#     mapping d.. l..
+#
+#
+#     ___ file __ ?
+#
+#
+#         m.. __.s.. _ \S+?_S 1-9 0-9 ?_L\d 3 _R 12 _ 0-9 2 1-9 .fastq.gz$ ? f.. _ __.I
+#
+#         __ m..
+#             first_part m...g.. 1 .l..
+#             second_part m...g.. 2 .l..
+#
+#             combined ? + ?
+#
+#             ? ? .a.. ?
+#
+#
+#     result    # list
+#     ___ value __ m__.v..
+#         __ l.. ? __ 2
+#
+#             __ '_R1_' __ ? 1
+#                 ?.r..
+#             ?.a.. t.. ?
+#
+#
+#     r.. ?
+#
+#
+# # Set up for your convenience during testing
+# __ _______ __ _______
+#     filenames
+#         "Sample1_S1_L001_R1_001.FASTQ.GZ",
+#         "Sample1_S1_L001_R2_001.fastq.gz",
+#         "Sample2_S2_L001_R1_001.fastq.gz",
+#         "sample2_s2_l001_r2_001.fastq.gz",
+#
+#     print p.. ?
+#     # ('Sample1_S1_L001_R1_001.FASTQ.GZ', 'Sample1_S1_L001_R2_001.fastq.gz')
+#     # ('Sample2_S2_L001_R1_001.fastq.gz', 'sample2_s2_l001_r2_001.fastq.gz')
+#
+#     ___ pair __ p.. ?
+#         print ?
