d1c10121583c623257785e0981ad57061374bb2a
syurskyi <sergejyurskyj@yahoo.com>
2022-06-10 18:46:28 -0700

    Committed algorithms advanced template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/121/password.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/121/password.py
@@ -1,36 +1,36 @@
-_______ __
-
-
-___ password_complexity(password
-    """Input: password string, calculate score according to 5 criteria in bite,
-       return: score int"""
-    score 0
-    __ l.. ? >_ 8:
-        score += 1
-        first_eight password[:8]
-        __ n.. __.s.. _ (.)\1',password
-            score += 1
-
-    
-
-
-
-    __ __.s.. _ [a-z]',password) a.. __.s.. _ [A-Z]',password
-        score += 1
-    
-    __ __.s.. _ [^\sa-zA-Z0-9]',password
-        score += 1
-
-
-    __ __.s.. _ \d',password) a.. __.s.. _ [a-zA-Z]',password
-        score += 1
-
-
-    r.. score
-
-__ _______ __ _______
-    password '123$Abc1'
-
-    password_complexity ?
-
-
+# _______ __
+#
+#
+# ___ password_complexity password
+#     """Input: password string, calculate score according to 5 criteria in bite,
+#        return: score int"""
+#     score 0
+#     __ l.. ? >_ 8
+#         ? +_ 1
+#         first_eight ? |8
+#         __ n.. __.s.. _ (.)\1 ?
+#             ? +_ 1
+#
+#
+#
+#
+#
+#     __ __.s.. _ [a-z] ? a.. __.s.. _ [A-Z] ?
+#         ? +_ 1
+#
+#     __ __.s.. _ [^\sa-zA-Z0-9] ?
+#         ? +_ 1
+#
+#
+#     __ __.s.. _ \d ? a.. __.s.. _ [a-zA-Z] ?
+#         ? +_ 1
+#
+#
+#     r.. ?
+#
+# __ _______ __ _______
+#     password '123$Abc1'
+#
+#     p.. ?
+#
+#
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/121_v3/password.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/121_v3/password.py
@@ -1,28 +1,28 @@
-_______ __
-
-
-___ password_complexity(password: s..
-    """Input: password string, calculate score according to 5 criteria in bite,
-       return: score int"""
-    up lo num spec 0
-    ___ l __ password:
-        __ l.isupper
-            up += 1
-        ____ l.isl..
-            lo += 1
-        ____ l.i..
-            num += 1
-        ____
-            spec += 1
-    score 0
-    __ 0 < up a.. 0 < lo:
-        score += 1
-    __ 0 < up + lo a.. 0 < num:
-        score += 1
-    __ 0 < spec:
-        score += 1
-    __ l.. ? >_ 8:
-        score += 1
-        __ n.. __.s.. _ (.)\1', password
-            score += 1
-    r.. score
+# _______ __
+#
+#
+# ___ password_complexity password s..
+#     """Input: password string, calculate score according to 5 criteria in bite,
+#        return: score int"""
+#     up lo num spec 0
+#     ___ l __ ?
+#         __ l.i..
+#             up +_ 1
+#         ____ l.isl..
+#             lo +_ 1
+#         ____ l.i..
+#             num +_ 1
+#         ____
+#             spec +_ 1
+#     score 0
+#     __ 0 < u. a.. 0 < lo
+#         ? +_ 1
+#     __ 0 < u. + l. a.. 0 < ?
+#         ? +_ 1
+#     __ 0 < s..
+#         ? +_ 1
+#     __ l.. ? >_ 8:
+#         ? +_ 1
+#         __ n.. __.s.. _ (.)\1 ?
+#             ? +_ 1
+#     r.. ?
