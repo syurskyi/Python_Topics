c9c137df6cb933aa5694788ea1bb073dd8170481
syurskyi <sergejyurskyj@yahoo.com>
2022-06-10 18:07:03 -0700

    Committed algorithms advanced template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/105_intro_slice_and_dice/save1_nopass.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/105_intro_slice_and_dice/save1_nopass.py
@@ -1,30 +1,30 @@
-____ s__ _______ a..
-
-text """
-One really nice feature of Python is polymorphism: using the same operation
-on different types of objects.
-Let's talk about an elegant feature: slicing.
-You can use this on a string as well as a list for example
-'pybites'[0:2] gives 'py'.
- The first value is inclusive and the last one is exclusive so
-here we grab indexes 0 and 1, the letter p and y.
-  When you have a 0 index you can leave it out so can write this as 'pybites'[:2]
-but here is the kicker: you can use this on a list too!
-['pybites', 'teaches', 'you', 'Python'][-2:] would gives ['you', 'Python']
-and now you know about slicing from the end as well :)
-keep enjoying our bites!
-"""
-
-
-___ slice_and_dice text s.. text __ l..
-    """Get a list of words from the passed in text.
-       See the Bite description for step by step instructions"""
-    s.. ?.s.. " . !"
-    s.. ?.s..  \n
-    ___ words __ ?
-        s.., s..
-    ___ firstword __ ?
-        __ ?.isl..
-            ?.s..
-    results "" .s...s..
-    r.. ?
+# ____ s__ _______ a..
+#
+# text """
+# One really nice feature of Python is polymorphism: using the same operation
+# on different types of objects.
+# Let's talk about an elegant feature: slicing.
+# You can use this on a string as well as a list for example
+# 'pybites'[0:2] gives 'py'.
+#  The first value is inclusive and the last one is exclusive so
+# here we grab indexes 0 and 1, the letter p and y.
+#   When you have a 0 index you can leave it out so can write this as 'pybites'[:2]
+# but here is the kicker: you can use this on a list too!
+# ['pybites', 'teaches', 'you', 'Python'][-2:] would gives ['you', 'Python']
+# and now you know about slicing from the end as well :)
+# keep enjoying our bites!
+# """
+#
+#
+# ___ slice_and_dice text s.. text __ l..
+#     """Get a list of words from the passed in text.
+#        See the Bite description for step by step instructions"""
+#     s.. ?.s.. " . !"
+#     s.. ?.s..  \n
+#     ___ words __ ?
+#         s.., s..
+#     ___ firstword __ ?
+#         __ ?.isl..
+#             ?.s..
+#     results "" .s...s..
+#     r.. ?
