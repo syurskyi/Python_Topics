8fd8809a25960c439d65910749c823b6dfa2bdc2
syurskyi <sergejyurskyj@yahoo.com>
2022-06-12 16:15:38 -0700

    Committed algorithms advanced template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/229/best_programming_books.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/229/best_programming_books.py
@@ -1,131 +1,131 @@
-____ p.. _______ P..
-____ u__.r.. _______ u..
-____ d.. _______ d..
-
-____ ___ _______ B.., Tag
-
-url "https://bites-data.s3.us-east-2.amazonaws.com/" "best-programming-books.html"
-tmp  P..("/tmp")
-html_file tmp / "books.html"
-
-__ n.. html_file.exists
-    u..(url, html_file)
-
-
-??
-c_ Book:
-    """Book class should instatiate the following variables:
-
-    title - as it appears on the page
-    author - should be entered as lastname, firstname
-    year - four digit integer year that the book was published
-    rank - integer rank to be updated once the books have been sorted
-    rating - float as indicated on the page
-    """
-
-    title: s..
-    author: s..
-    year: i..
-    rank: i..
-    rating: f__
-
-    ___ _rating
-        res f"{rating:.2f}"
-        r.. res[:-1] __ res[-1] __ "0" ____ res
-
-    ___ -s
-        r.. (
-            f"[{rank:03}] {title} ({year})\n"
-            f"      {author} {_rating()}"
-        )
-
-
-___ _get_soup(file
-    r.. B..(file.read_text(), "html.parser")
-
-
-___ display_books(books, limit=10, year_ N..
-    """Prints the specified books to the console
-
-    :param books: list of all the books
-    :param limit: integer that indicates how many books to return
-    :param year: integer indicating the oldest year to include
-    :return: None
-    """
-    ___ b __ books:
-        __ limit __ 0:
-            _____
-        __ year __ N.. o. b.year >_ year:
-            print(b)
-            limit -_ 1
-
-
-___ load_data
-    """Loads the data from the html file
-
-    Creates the soup object and processes it to extract the information
-    required to create the Book class objects and returns a sorted list
-    of Book objects.
-
-    Books should be sorted by rating, year, title, and then by author's
-    last name. After the books have been sorted, the rank of each book
-    should be updated to indicate this new sorting order.The Book object
-    with the highest rating should be first and go down from there.
-    """
-    soup _get_soup(html_file)
-    book_list ?.f.. "div", {"class": "books"})
-    books    # list
-    book: Tag
-    ___ book __ book_list.f.. "div", {"class": "book"}
-        title book.s.. "h2.main" 0.text
-        __ "python" n.. __ title.l..:
-            _____
-        ___
-            author_a book.s.. "h3.authors > a" 0.?.s..(" ")
-            author _* author_a[-1]}, {" ".j..(author_a[:-1])}'
-            date_span book.s.. "span.date")
-            __ l..(date_span) __ 0:
-                _____
-            year i..(date_span[0].text[-4:])
-            rank i..(book.s.. "div.rank > span" 0.text)
-            rating f__(book.s.. "span.our-rating" 0.text)
-        ______ AttributeError:
-            _____
-        books.a..(
-            Book t.._title, author=author, year=year, rank=rank, rating=rating)
-        )
-    res    # list
-    ___ n, b __ e..(
-        s..(
-            books, k.._l.... b: (-b.rating, b.year, b.title.l.., b.author.l..
-        ),
-        start=1,
-
-        b.rank n
-        res.a..(b)
-    r.. res
-
-
-___ main
-    books load_data()
-    display_books(books, limit=5, year=2017)
-    """If done correctly, the previous function call should display the
-    output below.
-    """
-
-
-__ _______ __ _______
-    main()
-
-"""
-[001] Python Tricks (2017)
-      Bader, Dan 4.74
-[002] Mastering Deep Learning Fundamentals with Python (2019)
-      Wilson, Richard 4.7
-[006] Python Programming (2019)
-      Fedden, Antony Mc 4.68
-[007] Python Programming (2019)
-      Mining, Joseph 4.68
-[009] A Smarter Way to Learn Python (2017)
-      Myers, Mark 4.66
-"""
+# ____ p.. _______ P..
+# ____ u__.r.. _______ u..
+# ____ d.. _______ d..
+#
+# ____ ___ _______ B.., Tag
+#
+# url "https://bites-data.s3.us-east-2.amazonaws.com/" "best-programming-books.html"
+# tmp  P.. /tmp
+# html_file ? / books.html
+#
+# __ n.. ?.e..
+#     u.. ? ?
+#
+#
+# ??
+# c_ Book
+#     """Book class should instatiate the following variables:
+#
+#     title - as it appears on the page
+#     author - should be entered as lastname, firstname
+#     year - four digit integer year that the book was published
+#     rank - integer rank to be updated once the books have been sorted
+#     rating - float as indicated on the page
+#     """
+#
+#     title: s..
+#     author: s..
+#     year: i..
+#     rank: i..
+#     rating: f__
+#
+#     ___ _rating
+#         res _* ?:.2f
+#         r.. ? |-1 __ ?-1 __ "0" ____ ?
+#
+#     ___ -s
+#         r..
+#             _* ?:03 ? ?\n
+#             _*      a.. _?
+#
+#
+#
+# ___ _get_soup file
+#     r.. B.. ?.r.. html.parser
+#
+#
+# ___ display_books books limit=10 year_ N..
+#     """Prints the specified books to the console
+#
+#     :param books: list of all the books
+#     :param limit: integer that indicates how many books to return
+#     :param year: integer indicating the oldest year to include
+#     :return: None
+#     """
+#     ___ b __ ?
+#         __ l.. __ 0
+#             _____
+#         __ y.. __ N.. o. ?.y.. >_ y..
+#             print ?
+#             l.. -_ 1
+#
+#
+# ___ load_data
+#     """Loads the data from the html file
+#
+#     Creates the soup object and processes it to extract the information
+#     required to create the Book class objects and returns a sorted list
+#     of Book objects.
+#
+#     Books should be sorted by rating, year, title, and then by author's
+#     last name. After the books have been sorted, the rank of each book
+#     should be updated to indicate this new sorting order.The Book object
+#     with the highest rating should be first and go down from there.
+#     """
+#     soup _? h..
+#     book_list ?.f.. "div", {"class": "books"})
+#     books    # list
+#     book: Tag
+#     ___ ? __ b__.f.. div class : book
+#         title ?.s.. "h2.main" 0.t..
+#         __ python n.. __ ?.l..
+#             _____
+#         ___
+#             author_a ?.s.. h3.authors > a 0.?.s.. " "
+#             author _* ? -1 " ".j.. ? |-1
+#             date_span ?.s.. span.date
+#             __ l.. ? __ 0
+#                 _____
+#             year i.. d.. 0 .t.. -4|
+#             rank i.. b__.s.. div.rank > span 0.t..
+#             rating f__ b...s.. span.our-rating 0.t..
+#         ______ A..
+#             _____
+#         books.a..
+#             Book t.._? a.._? y.._? r.._? r.._?
+#
+#     res    # list
+#     ___ n, b __ e..
+#         s..
+#             b.. k.._l.... b -?.r.. ?.y.. ?.t__.l.. ?.a__.l..
+#
+#         start=1,
+#
+#         ?.r.. n
+#         r__.a.. b
+#     r.. ?
+#
+#
+# ___ main
+#     books ?
+#     d.. ? l.._5 y.._2017
+#     """If done correctly, the previous function call should display the
+#     output below.
+#     """
+#
+#
+# __ _______ __ _______
+#     main()
+#
+# """
+# [001] Python Tricks (2017)
+#       Bader, Dan 4.74
+# [002] Mastering Deep Learning Fundamentals with Python (2019)
+#       Wilson, Richard 4.7
+# [006] Python Programming (2019)
+#       Fedden, Antony Mc 4.68
+# [007] Python Programming (2019)
+#       Mining, Joseph 4.68
+# [009] A Smarter Way to Learn Python (2017)
+#       Myers, Mark 4.66
+# """
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/229/test_best_programming_books.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/229/test_best_programming_books.py
@@ -1,88 +1,88 @@
-_______ p__
-
-____ best_programming_books _______ Book, display_books, load_data
-
-
-?p__.f.. s.._"session"
-___ dummy_book
-    title "Python Testing with pytest"
-    author "Okken, Brian"
-    year 2017
-    rank 1
-    rating 5
-    r.. Book(title, author, year, rank, rating)
-
-
-?p__.f.. s.._"session"
-___ python_books
-    data load_data()
-    __ isi.. ? l..
-        r.. data
-    r.. l.. ?
-
-
-___ test_book_class_incorrectly
-    w__ p__.r.. T..
-        Book()
-
-
-___ test_book_class(dummy_book
-    ... dummy_book.title __ "Python Testing with pytest"
-    ... dummy_book.author __ "Okken, Brian"
-    ... dummy_book.year __ 2017
-    ... dummy_book.rank __ 1
-    ... dummy_book.rating __ 5
-
-
-___ test_book_class_str(dummy_book
-    a.. s..(dummy_book)
-    e.. ("[001] Python Testing with pytest (2017)"
-                "\n      Okken, Brian 5.0")
-    ... a.. __ e..
-
-
-___ test_load_data(python_books
-    ... l..(python_books) __ 36
-    ... python_books[0].author __ "Bader, Dan"
-    ... python_books[-1].title __ "Python for Tweens and Teens"
-    ... python_books[10].rating __ 4.66
-
-
-?p__.m__.p.(
-    "index, expected",
-    [
-        (0, "[001] Python Tricks (2017)"),
-        (1, "      Bader, Dan 4.74"),
-        (2, "[002] Mastering Deep Learning Fundamentals with Python (2019)"),
-        (3, "      Wilson, Richard 4.7"),
-        (4, "[006] Python Programming (2019)"),
-        (5, "      Fedden, Antony Mc 4.68"),
-        (6, "[007] Python Programming (2019)"),
-        (7, "      Mining, Joseph 4.68"),
-        (8, "[009] A Smarter Way to Learn Python (2017)"),
-        (9, "      Myers, Mark 4.66"),
-        (10, "[010] Python Crash Course (2019)"),
-        (11, "      Robert, Antonio 4.66"),
-        (12, "[011] PYTHON PROGRAMMING (2019)"),
-        (13, "      Campbell, Clive 4.66"),
-        (14, "[012] Python Programming (2019)"),
-        (15, "      Harvard, Chris 4.66"),
-        (16, "[013] Python Programming (2019)"),
-        (17, "      Samelson, Steven 4.66"),
-        (18, "[014] Python Programming (2019)"),
-        (19, "      James, Thomas 4.65"),
-    ],
-)
-___ test_display_books(python_books, index, e.., capfd
-    display_books(python_books, year=2017)
-    output ?.r .. 0 .s..
-    ... output[index] __ e..
-
-
-?p__.m__.p.(
-    "limit, expected", [(40, 72), (53, 72), (69, 72), (100, 72), (1000, 72)]
-)
-___ test_display_books_plus(python_books, limit, e.., capfd
-    display_books(python_books, limit=limit)
-    output ?.r .. 0 .s..
-    ... l.. ?  __ e..
+# _______ p__
+#
+# ____ ? _______ ? ? ?
+#
+#
+# ?p__.f.. s.._"session"
+# ___ dummy_book
+#     t.. "Python Testing with pytest"
+#     a.. "Okken, Brian"
+#     y.. 2017
+#     r.. 1
+#     r.. 5
+#     r.. ? ? ? ? ? ?
+#
+#
+# ?p__.f.. s.._"session"
+# ___ python_books
+#     data ?
+#     __ isi.. ? l..
+#         r.. ?
+#     r.. l.. ?
+#
+#
+# ___ test_book_class_incorrectly
+#     w__ p__.r.. T..
+#         B..
+#
+#
+# ___ test_book_class dummy_book
+#     ... ?.t.. __ "Python Testing with pytest"
+#     ... ?.a.. __ "Okken, Brian"
+#     ... ?.y.. __ 2017
+#     ... ?.r.. __ 1
+#     ... ?.r.. __ 5
+#
+#
+# ___ test_book_class_str dummy_book
+#     a.. s.. ?
+#     e.. ("[001] Python Testing with pytest (2017)"
+#                 "\n      Okken, Brian 5.0")
+#     ... a.. __ e..
+#
+#
+# ___ test_load_data python_books
+#     ... l.. ? __ 36
+#     ... ? 0 .a.. __ "Bader, Dan"
+#     ... ? -1 .t.. __ "Python for Tweens and Teens"
+#     ... ? 10 .r.. __ 4.66
+#
+#
+# ?p__.m__.p.
+#     "index, expected",
+#     [
+#         (0, "[001] Python Tricks (2017)"),
+#         (1, "      Bader, Dan 4.74"),
+#         (2, "[002] Mastering Deep Learning Fundamentals with Python (2019)"),
+#         (3, "      Wilson, Richard 4.7"),
+#         (4, "[006] Python Programming (2019)"),
+#         (5, "      Fedden, Antony Mc 4.68"),
+#         (6, "[007] Python Programming (2019)"),
+#         (7, "      Mining, Joseph 4.68"),
+#         (8, "[009] A Smarter Way to Learn Python (2017)"),
+#         (9, "      Myers, Mark 4.66"),
+#         (10, "[010] Python Crash Course (2019)"),
+#         (11, "      Robert, Antonio 4.66"),
+#         (12, "[011] PYTHON PROGRAMMING (2019)"),
+#         (13, "      Campbell, Clive 4.66"),
+#         (14, "[012] Python Programming (2019)"),
+#         (15, "      Harvard, Chris 4.66"),
+#         (16, "[013] Python Programming (2019)"),
+#         (17, "      Samelson, Steven 4.66"),
+#         (18, "[014] Python Programming (2019)"),
+#         (19, "      James, Thomas 4.65"),
+#
+#
+# ___ test_display_books python_books index e.. capfd
+#     ? ? y.._2017
+#     output ?.r .. 0 .s..
+#     ... ? ? __ e..
+#
+#
+# ?p__.m__.p.
+#     "limit, expected", [(40, 72), (53, 72), (69, 72), (100, 72), (1000, 72)]
+#
+# ___ test_display_books_plus python_books limit e.. capfd
+#     ? ? l.._?
+#     output ?.r .. 0 .s..
+#     ... l.. ?  __ e..
