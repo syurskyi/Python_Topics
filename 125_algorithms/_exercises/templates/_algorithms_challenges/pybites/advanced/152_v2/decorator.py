2a2db6358d2b794193403bbbd1cb49923b0d7b50
syurskyi <sergejyurskyj@yahoo.com>
2022-06-12 16:39:36 -0700

    Committed algorithms advanced template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/152_v2/decorator.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/152_v2/decorator.py
@@ -1,28 +1,28 @@
-DEFAULT_TEXT ('Subscribe to our blog (sidebar) to periodically get '
-                'new PyBites Code Challenges (PCCs) in your inbox')
-DOT '.'
-
-
-___ strip_range(start, end
-    """Decorator that replaces characters of a text by dots, from 'start'
-       (inclusive) to 'end' (exclusive) = like range.
-
-        So applying this decorator on a function like this and 'text'
-        being 'Hello world' it would convert it into 'Hel.. world' when
-        applied like this:
-
-        @strip_range(3, 5)
-        def gen_output(text):
-            return text
-    """
-
-    ___ wrap(func
-        ___ wrapped(text, $  $$
-            _start m..(start, 0)
-            _end m..(l..(text), end) __ end > 0 ____ 0
-            result (text[:_start], text[_start:_end], text[_end:])
-            r.. func _*{result[0]}{DOT * l..(result[1])}{result[2]}')
-
-        r.. wrapped
-
-    r.. wrap
+# DEFAULT_TEXT ('Subscribe to our blog (sidebar) to periodically get '
+#                 'new PyBites Code Challenges (PCCs) in your inbox')
+# DOT '.'
+#
+#
+# ___ strip_range start end
+#     """Decorator that replaces characters of a text by dots, from 'start'
+#        (inclusive) to 'end' (exclusive) = like range.
+#
+#         So applying this decorator on a function like this and 'text'
+#         being 'Hello world' it would convert it into 'Hel.. world' when
+#         applied like this:
+#
+#         @strip_range(3, 5)
+#         def gen_output(text):
+#             return text
+#     """
+#
+#     ___ wrap func
+#         ___ wrapped text $  $$
+#             _start m..? 0
+#             _end m..(l.. ? e.. __ e.. > 0 ____ 0
+#             result t.. |_? ? _?|_? ? _?|
+#             r.. f.. _* ? 0 D.. * l.. ? 1 ? 2
+#
+#         r.. ?
+#
+#     r.. ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/152_v3/decorator.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/152_v3/decorator.py
@@ -1,33 +1,33 @@
-____ f.. _______ w..
-
-
-DEFAULT_TEXT ('Subscribe to our blog (sidebar) to periodically get '
-                'new PyBites Code Challenges (PCCs) in your inbox')
-DOT '.'
-
-
-___ strip_range(start, end
-    """Decorator that replaces characters of a text by dots, from 'start'
-       (inclusive) to 'end' (exclusive) = like range.
-
-        So applying this decorator on a function like this and 'text'
-        being 'Hello world' it would convert it into 'Hel.. world' when
-        applied like this:
-
-        @strip_range(3, 5)
-        def gen_output(text):
-            return text
-    """
-    ___ decorator_replace(fun
-        ??(fun)
-        ___ wrapper_replace $ $$
-            output l..(fun $,  $$
-            this_end end __ end < l.. ?  ____ l.. ?
-            this_end this_end __ this_end > 0 ____ 0
-            this_start start __ start >_ 0 ____ 0
-
-            ___ k __ r..(this_start, this_end
-                output[k] '.'
-            r.. ''.j.. ?
-        r.. wrapper_replace
-    r.. decorator_replace
+# ____ f.. _______ w..
+#
+#
+# DEFAULT_TEXT ('Subscribe to our blog (sidebar) to periodically get '
+#                 'new PyBites Code Challenges (PCCs) in your inbox')
+# DOT '.'
+#
+#
+# ___ strip_range start end
+#     """Decorator that replaces characters of a text by dots, from 'start'
+#        (inclusive) to 'end' (exclusive) = like range.
+#
+#         So applying this decorator on a function like this and 'text'
+#         being 'Hello world' it would convert it into 'Hel.. world' when
+#         applied like this:
+#
+#         @strip_range(3, 5)
+#         def gen_output(text):
+#             return text
+#     """
+#     ___ decorator_replace fun
+#         ?? ?
+#         ___ wrapper_replace $ $$
+#             output l.. f.. $,  $$
+#             this_end en __ end < l.. ?  ____ l.. ?
+#             t.. ? __ ? > 0 ____ 0
+#             t.. s.. __ ? >_ 0 ____ 0
+#
+#             ___ k __ r.. ? ?
+#                 o.. ? '.'
+#             r.. ''.j.. ?
+#         r.. ?
+#     r.. ?
