742d9fcaa22b442cd086de22e94dd4d25d72386a
syurskyi <sergejyurskyj@yahoo.com>
2022-06-12 15:59:17 -0700

    Committed algorithms advanced template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/240/account.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/240/account.py
@@ -1,49 +1,49 @@
-# source: https://dbader.org/blog/python-dunder-methods
-____ f.. _______ total_ordering
-
-
-@total_ordering
-c_ Account
-    'A simple account class'
-
-    ___ - , owner, amount=0
-        'This is the constructor that lets us create objects from this class'
-        owner owner
-        amount amount
-        _transactions    # list
-
-    ___  -r
-        r.. 'Account({!r}, {!r})'.f..(owner, amount)
-
-    ___ -s
-        r.. 'Account of {} with starting amount: {}'.f..(owner,
-                                                               amount)
-
-    ___ add_transaction  amount
-        __ n.. isi..(amount, i..
-            r.. V...('please use int for amount')
-        _?.a..(amount)
-
-    $
-    ___ balance
-        r.. amount + s.. _?
-
-    ___ -l
-        r.. l.. _?
-
-    ___ -g  position
-        r.. _transactions[position]
-
-    ___ -e  other
-        r.. b.. __ ?.b..
-
-    ___ -l  other
-        r.. b.. < ?.b..
-
-    ___ -a  other
-        owner '{}&{}'.f..(owner, other.owner)
-        start_amount amount + other.amount
-        acc Account(owner, start_amount)
-        ___ t __ l..(self) + l..(other
-            acc.add_transaction(t)
-        r.. acc
+# # source: https://dbader.org/blog/python-dunder-methods
+# ____ f.. _______ total_ordering
+#
+#
+# ??
+# c_ Account
+#     'A simple account class'
+#
+#     ___ -  owner amount=0
+#         'This is the constructor that lets us create objects from this class'
+#         ? ?
+#         ? ?
+#         _transactions    # list
+#
+#     ___  -r
+#         r.. Account @@@, @@@ .f.. ? ?
+#
+#     ___ -s
+#         r.. Account of @ with starting amount: @.f.. ? ?
+#
+#
+#     ___ add_transaction  amount
+#         __ n.. isi.. ? i..
+#             r.. V... 'please use int for amount')
+#         _?.a.. ?
+#
+#     $
+#     ___ balance
+#         r.. ? + s.. _?
+#
+#     ___ -l
+#         r.. l.. _?
+#
+#     ___ -g  position
+#         r.. _transactions ?
+#
+#     ___ -e  other
+#         r.. b.. __ ?.b..
+#
+#     ___ -l  other
+#         r.. b.. < ?.b..
+#
+#     ___ -a  other
+#         owner @&@.f.. ? ?.o..
+#         start_amount a.. + ?.a..
+#         acc A.. ? ?
+#         ___ t __ l.. ____ + l.. ?
+#             ?.a.. ?
+#         r.. ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/240/test_account.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/240/test_account.py
@@ -1,76 +1,76 @@
-_______ p__
-____ ? _______ ?
-
-
-# write your pytest functions below, they need to start with test_
-?p__.f..
-___ einstein
-    r.. Account('Einstein', 100)
-
-
-?p__.f..
-___ socrates
-    r.. Account('Socrates', 0)
-
-
-___ add_transactions(acct, values
-    ___ v __ values:
-        acct.add_transaction(v)
-
-
-___ test_account_create(einstein
-    ... isi..(einstein, Account)
-    ... einstein.balance __ 100
-    ... s..(einstein) __ 'Account of Einstein with starting amount: 100'
-    ... r.. (einstein) __ "Account('Einstein', 100)"
-    hector Account('Hector')
-    ... r.. (hector) __ "Account('Hector', 0)"
-
-
-___ test_account_transaction(einstein
-    ... einstein.balance __ 100
-    einstein.add_transaction(50)
-    ... einstein.balance __ 150
-    einstein.add_transaction(-75)
-    ... einstein.balance __ 75
-    ... l..(einstein) __ 2
-
-
-___ test_account_bad_transaction(socrates
-    ... socrates.balance __ 0
-    w__ p__.r.. V...) __ exp:
-        socrates.add_transaction(3.14)
-    ... 'please use int for amount' __ s..(exp.v..)
-    ... socrates.balance __ 0
-
-
-___ test_account_comparisons(einstein, socrates
-    ... einstein > socrates
-    add_transactions(socrates, [10, 20, 30])
-    ... socrates.balance __ 60
-    ... socrates[1] __ 20
-    socrates.add_transaction(40)
-    ... einstein __ socrates
-    ... n.. (einstein < socrates)
-
-
-___ test_account_merge_accounts(einstein, socrates
-    ... einstein.balance __ 100 a.. socrates.balance __ 0
-    add_transactions(einstein, [50, -75])
-    add_transactions(socrates, [10, 20, 30])
-    ... einstein.balance __ 75 a.. socrates.balance __ 60
-    pythagoras einstein + socrates
-    ... pythagoras.balance __ 135
-    ... s..(pythagoras) __ 'Account of Einstein&Socrates with starting amount: 100'
-    ... l..(pythagoras) __ 5
-    ... pythagoras[1] __ -75
-
-
-___ test_account_bad_merge_accounts(einstein, socrates
-    add_transactions(einstein, [50, -75])
-    add_transactions(socrates, [10, 20, 30])
-    kelvin Account('Kelvin', 20)
-    ... kelvin.balance __ 20
-    armstrong kelvin + einstein + socrates
-    ... armstrong.balance __ 155
-    ... s..(armstrong) __ 'Account of Kelvin&Einstein&Socrates with starting amount: 120'
+# _______ p__
+# ____ ? _______ ?
+#
+#
+# # write your pytest functions below, they need to start with test_
+# ?p__.f..
+# ___ einstein
+#     r.. ? 'Einstein', 100
+#
+#
+# ?p__.f..
+# ___ socrates
+#     r.. ? 'Socrates', 0
+#
+#
+# ___ add_transactions acct, values
+#     ___ v __ ?
+#         ?.a.. ?
+#
+#
+# ___ test_account_create einstein
+#     ... isi.. ? A..
+#     ... ?.b.. __ 100
+#     ... s.. ? __ 'Account of Einstein with starting amount: 100'
+#     ... r..  ? __ "Account('Einstein', 100)"
+#     hector ? 'Hector')
+#     ... r.. ? __ "Account('Hector', 0)"
+#
+#
+# ___ test_account_transaction einstein
+#     ... ? .b.. __ 100
+#     ? .a.. 50
+#     ... ? .b.. __ 150
+#     ? .a.. -75
+#     ... ? .b.. __ 75
+#     ... l..(? ) __ 2
+#
+#
+# ___ test_account_bad_transaction socrates
+#     ... ?.b.. __ 0
+#     w__ p__.r.. V... __ exp
+#         ?.a.. 3.14
+#     ... 'please use int for amount' __ s.. ?.v..
+#     ... ?.b.. __ 0
+#
+#
+# ___ test_account_comparisons(einstein, socrates
+#     ... ?  > ?
+#     ? ?, [10, 20, 30
+#     ... ?.b.. __ 60
+#     ... ? 1 __ 20
+#     ?.a.. 40
+#     ... ?  __ ?
+#     ... n.. ?  < ?
+#
+#
+# ___ test_account_merge_accounts einstein socrates
+#     ... ? .b.. __ 100 a.. ?.b.. __ 0
+#     ? ? , [50, -75
+#     ? ?, [10, 20, 30
+#     ... ? .b.. __ 75 a.. ?.b.. __ 60
+#     pythagoras ?  + ?
+#     ... ?.b.. __ 135
+#     ... s.. ? __ 'Account of Einstein&Socrates with starting amount: 100'
+#     ... l.. ? __ 5
+#     ... ? 1 __ -75
+#
+#
+# ___ test_account_bad_merge_accounts einstein, socrates
+#     ?(? , [50, -75
+#     ?(?, [10, 20, 30
+#     kelvin ? 'Kelvin', 20
+#     ... ?.b.. __ 20
+#     armstrong ? + ?  + ?
+#     ... ?.b.. __ 155
+#     ... s.. ? __ 'Account of Kelvin&Einstein&Socrates with starting amount: 120'
