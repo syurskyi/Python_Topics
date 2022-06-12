0c9dc53dada35638dddea96562db0ab05ee580da
syurskyi <sergejyurskyj@yahoo.com>
2022-06-12 16:04:31 -0700

    Committed algorithms advanced template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/237/belts.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/237/belts.py
@@ -1,54 +1,54 @@
-_______ j__
-_______ __
-____ p.. _______ P..
-_______ d__ __ dt
-____ d__.p.. _______ p..
-
-SCORES [10, 50, 100, 175, 250, 400, 600, 800, 1000]
-BELTS ('white yellow orange green blue brown black '
-         'paneled red').s..
-TMP P.. __.g.. "TMP", "/tmp"
-
-
-___ get_belts(data: s..) __ d..:
-    """Parsed the passed in json data:
-       {"date":"5/1/2019","score":1},
-       {"date":"9/13/2018","score":3},
-       {"date":"10/25/2019","score":1},
-
-       Loop through the scores in chronological order,
-       determining when belts were achieved (use SCORES
-       and BELTS).
-
-       Return a dict with keys = belts, and values =
-       readable dates, example entry:
-       'yellow': 'January 25, 2018'
-    """
-    w__ o.. data _ __ f:
-        dates j__.l.. f)
-
-    dates.s..(k.._l.... x: p..(x 'date'
-    
-    
-    belts    # dict
-    score_index 0
-    score_marker SCORES[score_index]
-    points 0
-    ___ date __ dates:
-        points += date 'score'
-        __ points >_ score_marker:
-
-            month,day,year m.. i..,date 'date' .s..('/'
-            date dt.date(year,month,day)
-            date_string date.s..('%B _d, _Y')
-            belts[BELTS[score_index]] date_string
-            score_index += 1
-            __ score_index __ l..(SCORES
-                _____
-            score_marker SCORES[score_index]
-
-
-
-    r.. belts
-
-
+# _______ j__
+# _______ __
+# ____ p.. _______ P..
+# _______ d__ __ dt
+# ____ d__.p.. _______ p..
+#
+# SCORES [10, 50, 100, 175, 250, 400, 600, 800, 1000]
+# BELTS ('white yellow orange green blue brown black '
+#          'paneled red').s..
+# TMP P.. __.g.. "TMP", "/tmp"
+#
+#
+# ___ get_belts data s.. __ d..
+#     """Parsed the passed in json data:
+#        {"date":"5/1/2019","score":1},
+#        {"date":"9/13/2018","score":3},
+#        {"date":"10/25/2019","score":1},
+#
+#        Loop through the scores in chronological order,
+#        determining when belts were achieved (use SCORES
+#        and BELTS).
+#
+#        Return a dict with keys = belts, and values =
+#        readable dates, example entry:
+#        'yellow': 'January 25, 2018'
+#     """
+#     w__ o.. ? _ __ f
+#         dates j__.l.. ?
+#
+#     ?.s.. k.._l.... x| p..? date
+#
+#
+#     belts    # dict
+#     score_index 0
+#     score_marker S.. ?
+#     points 0
+#     ___ date __ ?
+#         ? +_ ? score
+#         __ ? >_ s..
+#
+#             month day year m.. i.. d.. date .s..('/'
+#             date __.d.. ? ? ?
+#             date_string d__.s.. _B _d _Y
+#             b.. B.. ? ?
+#             s.. +_ 1
+#             __ s.. __ l.. S..
+#                 _____
+#             s.. S.. s..
+#
+#
+#
+#     r.. ?
+#
+#
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/237/test_belts.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/237/test_belts.py
@@ -1,43 +1,43 @@
-____ u__.r.. _______ u..
-
-____ belts _______ get_belts, TMP
-
-
-___ get_data(file_no=1, tmp=TMP
-    file_name f'bite_scores{file_no}.json'
-    file_path TMP / file_name
-    remote 'https://bites-data.s3.us-east-2.amazonaws.com/'
-    __ n.. file_path.exists
-        u.. _*{remote}{file_name}',
-                    file_path)
-    r.. file_path
-
-
-___ test_data_set1
-    data get_data(1)
-    a.. get_belts(data)
-    # py 3.7 we should be able to rely on ordering
-    e.. {
-        'white': 'January 23, 2018',
-        'yellow': 'June 20, 2018',
-        'orange': 'October 07, 2018',
-        'green': 'April 08, 2019'
-    }
-    ... a.. __ e..
-
-
-___ test_data_set2
-    data get_data(2)
-    a.. get_belts(data)
-    e.. {
-        'white': 'January 06, 2018',
-        'yellow': 'January 25, 2018',
-        'orange': 'February 08, 2018',
-        'green': 'March 15, 2018',
-        'blue': 'April 29, 2018',
-        'brown': 'July 13, 2018',
-        'black': 'October 31, 2018',
-        'paneled': 'March 01, 2019',
-        'red': 'June 26, 2019'
-    }
-    ... a.. __ e..
+# ____ u__.r.. _______ u..
+#
+# ____ ? _______ ? T..
+#
+#
+# ___ get_data file_no=1, tmp_?
+#     file_name _* bite_scores ? .json
+#     file_path T.. / ?
+#     remote 'https://bites-data.s3.us-east-2.amazonaws.com/'
+#     __ n.. ?.e..
+#         u.. _* ? f..
+#                     f..
+#     r.. ?
+#
+#
+# ___ test_data_set1
+#     data ? 1
+#     a.. g.. ?
+#     # py 3.7 we should be able to rely on ordering
+#     e..
+#         'white': 'January 23, 2018',
+#         'yellow': 'June 20, 2018',
+#         'orange': 'October 07, 2018',
+#         'green': 'April 08, 2019'
+#
+#     ... a.. __ e..
+#
+#
+# ___ test_data_set2
+#     data ? 2
+#     a.. g.. ?
+#     e..
+#         'white': 'January 06, 2018',
+#         'yellow': 'January 25, 2018',
+#         'orange': 'February 08, 2018',
+#         'green': 'March 15, 2018',
+#         'blue': 'April 29, 2018',
+#         'brown': 'July 13, 2018',
+#         'black': 'October 31, 2018',
+#         'paneled': 'March 01, 2019',
+#         'red': 'June 26, 2019'
+#
+#     ... a.. __ e..
