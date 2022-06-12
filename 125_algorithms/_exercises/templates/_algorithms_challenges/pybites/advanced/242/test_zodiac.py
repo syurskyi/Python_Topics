b93b7aab9156733c136a8787f11e96d1213958c5
syurskyi <sergejyurskyj@yahoo.com>
2022-06-12 15:45:28 -0700

    Committed algorithms advanced template

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/242/test_zodiac.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/242/test_zodiac.py
@@ -1,79 +1,79 @@
-____ d__ _______ d__
-_______ j__
-_______ __
-____ p.. _______ P..
-____ u__.r.. _______ u..
-
-_______ p__
-
-____ zodiac _______ (get_signs, get_sign_with_most_famous_people,
-                    signs_are_mutually_compatible, get_sign_by_date, Sign)
-
-# original source: https://zodiacal.herokuapp.com/api
-URL "https://bites-data.s3.us-east-2.amazonaws.com/zodiac.json"
-TMP __.g.. TMP  /tmp
-PATH P..(TMP, "zodiac.json")
-
-
-?p__.f.. s.._='module'
-___ signs
-    __ n.. PATH.exists
-        u..(URL, PATH)
-    w__ o.. PATH) __ f:
-        data j__.l.. (f.read
-    r.. get_signs(data)
-
-
-# write your pytest code here ...
-___ test_named_tuple(signs
-    ... l..(Sign._fields) __ l..('name compatibility famous_people sun_dates'.s..(' '
-    ... r.. (signs 0.s.. 'Sign(')
-
-
-___ test_get_signs(signs
-    ... l..(signs) __ 12
-
-
-___ test_get_sign_with_most_famouse_people(signs
-    ... get_sign_with_most_famous_people(signs) __ ('Scorpio', 35)
-
-
-?p__.m__.p. "sgn1, sgn2, result", [
-    ('Aries', 'Aries', F..),
-    ('Aries', 'Leo', T..),
-    ('Aries', 'Capricorn', F..),
-    ('Aries', 'Aquarius', T..)
-])
-___ test_signs_are_mutually_compatible(signs, sgn1, sgn2, result
-    ... signs_are_mutually_compatible(signs, sgn1, sgn2) __ result
-
-
-?p__.m__.p. "dt, result", [
-    ([3, 21], 'Aries'),
-    ([4, 19], 'Aries'),
-    ([4, 20], 'Taurus'),
-    ([5, 20], 'Taurus'),
-    ([5, 21], 'Gemini'),
-    ([6, 20], 'Gemini'),
-    ([6, 21], 'Cancer'),
-    ([7, 22], 'Cancer'),
-    ([7, 23], 'Leo'),
-    ([8, 22], 'Leo'),
-    ([8, 23], 'Virgo'),
-    ([9, 22], 'Virgo'),
-    ([9, 23], 'Libra'),
-    ([10, 22], 'Libra'),
-    ([10, 23], 'Scorpio'),
-    ([11, 21], 'Scorpio'),
-    ([11, 22], 'Sagittarius'),
-    ([12, 21], 'Sagittarius'),
-    ([12, 22], 'Capricorn'),  # should be 'Capricorn' but there's a bug in the code!!
-    ([1, 19], 'Capricorn'),  # should be 'Capricorn' but there's a bug in the code!!
-    ([1, 20], 'Aquarius'),
-    ([2, 18], 'Aquarius'),
-    ([2, 19], 'Pisces'),
-    ([3, 20], 'Pisces')
-])
-___ test_get_sign_by_date(signs, dt, result
-    m, d dt
-    ... get_sign_by_date(signs, d__ y.._2000,  m.._m,  d.._d __ result
+# ____ d__ _______ d__
+# _______ j__
+# _______ __
+# ____ p.. _______ P..
+# ____ u__.r.. _______ u..
+#
+# _______ p__
+#
+# ____ ? _______ (?, ?,
+#                     ?, ?, ?
+#
+# # original source: https://zodiacal.herokuapp.com/api
+# URL "https://bites-data.s3.us-east-2.amazonaws.com/zodiac.json"
+# TMP __.g.. TMP  /tmp
+# PATH P.. ? zodiac.json
+#
+#
+# ?p__.f.. s.._= module
+# ___ signs
+#     __ n.. ?.e..
+#         u.. ? ?
+#     w__ o.. P.. __ f
+#         data j__.l.. ?.r..
+#     r.. ? ?
+#
+#
+# # write your pytest code here ...
+# ___ test_named_tuple signs
+#     ... l.. S__._f.. __ l.. 'name compatibility famous_people sun_dates'.s..(' '
+#     ... r.. ? 0.s.. Sign(')
+#
+#
+# ___ test_get_signs signs
+#     ... l.. ? __ 12
+#
+#
+# ___ test_get_sign_with_most_famouse_people signs
+#     ... ? ? __ Scorpio 35
+#
+#
+# ?p__.m__.p. "sgn1, sgn2, result", [
+#     ('Aries', 'Aries', F..),
+#     ('Aries', 'Leo', T..),
+#     ('Aries', 'Capricorn', F..),
+#     ('Aries', 'Aquarius', T..)
+#
+# ___ test_signs_are_mutually_compatible signs sgn1 sgn2 result
+#     ... ? ? ? ? __ ?
+#
+#
+# ?p__.m__.p. "dt, result", [
+#     ([3, 21], 'Aries'),
+#     ([4, 19], 'Aries'),
+#     ([4, 20], 'Taurus'),
+#     ([5, 20], 'Taurus'),
+#     ([5, 21], 'Gemini'),
+#     ([6, 20], 'Gemini'),
+#     ([6, 21], 'Cancer'),
+#     ([7, 22], 'Cancer'),
+#     ([7, 23], 'Leo'),
+#     ([8, 22], 'Leo'),
+#     ([8, 23], 'Virgo'),
+#     ([9, 22], 'Virgo'),
+#     ([9, 23], 'Libra'),
+#     ([10, 22], 'Libra'),
+#     ([10, 23], 'Scorpio'),
+#     ([11, 21], 'Scorpio'),
+#     ([11, 22], 'Sagittarius'),
+#     ([12, 21], 'Sagittarius'),
+#     ([12, 22], 'Capricorn'),  # should be 'Capricorn' but there's a bug in the code!!
+#     ([1, 19], 'Capricorn'),  # should be 'Capricorn' but there's a bug in the code!!
+#     ([1, 20], 'Aquarius'),
+#     ([2, 18], 'Aquarius'),
+#     ([2, 19], 'Pisces'),
+#     ([3, 20], 'Pisces')
+#
+# ___ test_get_sign_by_date signs dt result
+#     m, d __
+#     ... ? ? d__ y.._2000,  m.._m,  d.._d __ ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/242/zodiac.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/advanced/242/zodiac.py
@@ -1,55 +1,55 @@
-____ c.. _______ n..
-____ d__ _______ d__
-____ o.. _______ i__
-____ t___ _______ L..
-
-Sign n..('Sign', 'name compatibility famous_people sun_dates')
-
-
-___ get_signs(data: l..) __ L..[Sign]:
-    ret    # list
-    ___ sign __ data:
-        name sign 'name'
-        compatibility sign 'compatibility'
-        famous_people sign 'famous_people'
-        sun_dates sign 'sun_dates'
-        sign Sign(name, compatibility, famous_people, sun_dates)
-        ret.a..(
-            sign
-        )
-    r.. ret
-
-
-___ get_sign_with_most_famous_people(signs: l..
-    """Get the sign with the most famous people associated"""
-    famous_people [
-        (s.name, l..(s.famous_people ___ s __ signs
-    ]
-    r.. m..(famous_people, k.._i.. 1
-
-
-___ signs_are_mutually_compatible(signs: l.., sign1: s.., sign2: s..) __ b..
-    """Given 2 signs return if they are compatible (compatibility field)"""
-    ret F..
-    ___ sign __ signs:
-        __ sign.n.. __ sign1:
-            ret sign2 __ sign.compatibility
-        ____ sign.n.. __ sign2:
-            ret sign1 __ sign.compatibility
-    r.. ret
-
-
-___ get_sign_by_date(signs: l.., date: d__) __ s..
-    """Given a date return the right sign (sun_dates field)"""
-    year date.year
-    ___ sign __ signs:
-        start, end sign.sun_dates
-        start_dt d__.s..(start, '%B _d').r.. y.._year)
-        end_dt d__.s..(end, '%B _d').r.. y.._year)
-        __ end_dt < start_dt:
-            __ date <_ end_dt:
-                start_dt d__.s..(start, '%B _d').r.. y.._year-1)
-            ____
-                end_dt d__.s..(end, '%B _d').r.. y.._year+1)
-        __ start_dt <_ date <_ end_dt:
-            r.. sign.name
+# ____ c.. _______ n..
+# ____ d__ _______ d__
+# ____ o.. _______ i__
+# ____ t___ _______ L..
+#
+# Sign n..('Sign', 'name compatibility famous_people sun_dates')
+#
+#
+# ___ get_signs data l.. __ L.. ?
+#     ret    # list
+#     ___ sign __ ?
+#         name ? name
+#         compatibility ? compatibility
+#         famous_people ? famous_people
+#         sun_dates ? sun_dates
+#         ? ? ? ? ? ?
+#         ?.a..
+#             ?
+#
+#     r.. ?
+#
+#
+# ___ get_sign_with_most_famous_people signs l..
+#     """Get the sign with the most famous people associated"""
+#     f..
+#         s.n.. l.. ?.f.. ___ ? __ ?
+#
+#     r.. m.. ? k.._i.. 1
+#
+#
+# ___ signs_are_mutually_compatible signs l.., sign1 s.. sign2 s.. __ b..
+#     """Given 2 signs return if they are compatible (compatibility field)"""
+#     ret F..
+#     ___ sign __ ?
+#         __ ?.n.. __ ?1
+#             r.. ?2 __ ?.c..
+#         ____ ?.n.. __ sign2:
+#             r.. ?1 __ ?.c..
+#     r.. ?
+#
+#
+# ___ get_sign_by_date signs l.. date d__ __ s..
+#     """Given a date return the right sign (sun_dates field)"""
+#     year ?.y..
+#     ___ sign __ ?
+#         start, end ?.s..
+#         start_dt d__.s.. ? _B _d .r.. y.._?
+#         end_dt d__.s.. > _B _d .r.. y.._?
+#         __ ? < ?
+#             __ date <_ e..
+#                 s.. d__.s.. ? _B _d .r.. y.._?-1
+#             ____
+#                 e.. d__.s.. ? _B _d .r.. y.._?+1
+#         __ s.. <_ ? <_ e..
+#             r.. ?.n..
