81079d1110ae804b6359fb99c1fd8a13d4bdd68d
serge <sergejyurskyj@yahoo.com>
2022-10-02 10:50:53 -0700

    lambda exercises

--- 045_functions/007_lambda/_exercises/exercises/006_Classic functional designs_!cool!.py
+++ 045_functions/007_lambda/_exercises/exercises/006_Classic functional designs_!cool!.py
@@ -1,7 +1,7 @@
-# l.. ma. l_____ x ?.up.. |*cat *dog *cow
+print(list(map(lambda x: x.upper(), ['cat', 'dog', 'cow'])))
 # # ['CAT', 'DOG', 'COW']
-# l.. fi.. l_____ x 'o' __ ? |cat dog cow
+print(list(filter(lambda x: 'o' in x, ['cat', 'dog', 'cow'])))
 # # ['dog', 'cow']
-# f_ fun.. _______ r..
-# r.. l_____ acc x _*|a.. | |? * | cat dog cow
+from functools import reduce
+print(reduce(lambda acc, x: f'{acc} | {x}', ['cat', 'dog', 'cow']))
 # 'cat | dog | cow'
