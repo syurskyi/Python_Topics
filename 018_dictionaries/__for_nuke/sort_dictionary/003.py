4b0514cc8190f2673a00e95510d44b47e21227c9
serge <sergejyurskyj@yahoo.com>
2022-10-02 11:31:16 -0700

    sorted dictionary exercises

---
+++ 018_dictionaries/__for_nuke/sort_dictionary/003.py
@@ -0,0 +1,108 @@
+# # Displaying the Keys in sorted order
+#
+# # Function calling
+# ___ dictionairy
+#     # Declare hash function
+#     key_value  # dixt
+#
+#     # Initializing value
+#     key_value[2] = 56
+#     key_value[1] = 2
+#     key_value[5] = 12
+#     key_value[4] = 24
+#     key_value[6] = 18
+#     key_value[3] = 323
+#
+#     print("Task 1:-\n")
+#
+#     print("key_value" ?
+#
+#     # iterkeys() returns an iterator over the
+#     # dictionary’s keys.
+#     ___ i __ s.. ?.k..
+#         print ? e.._" "
+#
+#
+# dictionairy()
+#
+#
+# _____ c.. _____ O..
+#
+# d.. = {'ravi': '10', 'rajnish': '9',
+#         'sanjeev': '15', 'yash': '2', 'suraj': '32'}
+# dict1 = O.. s.. d...i..
+# print ?
+#
+# # Sorting the Keys and Values in Alphabetical Order using the Key
+#
+# # function calling
+# ___ dictionairy
+#     # Declaring the hash function
+#     key_value # dict
+#
+#     # Initialize value
+#     key_value[2] = 56
+#     key_value[1] = 2
+#     key_value[5] = 12
+#     key_value[4] = 24
+#     key_value[6] = 18
+#     key_value[3] = 323
+#
+#     print("key_value" ?
+#
+#     print("Task 2:-\nKeys and Values sorted in",
+#           "alphabetical order by the key  ")
+#
+#     # sorted(key_value) returns an iterator over the
+#     # Dictionary’s value sorted in keys.
+#     ___ i __ s..(key_value):
+#         print((i, key_value[i]), end=" ")
+#
+#
+# dictionairy()
+#
+#
+# # Example 4: Sorting the Keys and Values in alphabetical using the
+#
+# # Function calling
+# ___ dictionairy
+#     # Declaring hash function
+#     key_value # dict
+#
+#     # Initializing the value
+#     key_value[2] = 56
+#     key_value[1] = 2
+#     key_value[5] = 12
+#     key_value[4] = 24
+#     key_value[6] = 18
+#     key_value[3] = 323
+#
+#     print("key_value" ?
+#
+#     print("Task 3:-\nKeys and Values sorted",
+#           "in alphabetical order by the value")
+#
+#     # Note that it will sort in lexicographical order
+#     # For mathematical way, change it to float
+#     print(s.. ?.i.., k.._l... kv
+#     ? 1 ? 0
+#
+#
+#
+# dictionairy()
+#
+# # Sort Dictionary By Value in Python
+#
+# # Creates a sorted dictionary (sorted by key)
+# _____ c.. _____ O..
+#
+# d.. = {'ravi': 10, 'rajnish': 9,
+#         'sanjeev': 15, 'yash': 2, 'suraj': 32}
+# print(d..)
+#
+# sorted_value_index = np.a.. d...v..
+# dictionary_keys = l.. d...k..
+# sorted_dict = ? ? s..
+#     d...v.. ? ___ i __ r.. l.. ?
+#
+# print ?
