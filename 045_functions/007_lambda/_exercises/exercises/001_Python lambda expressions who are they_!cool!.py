d6617fe8ea74efd8b00acdec7a85d02ba532febf
serge <sergejyurskyj@yahoo.com>
2022-10-01 18:59:33 -0700

    lambda exercises

--- 045_functions/007_lambda/_exercises/exercises/001_Python lambda expressions who are they_!cool!.py
+++ 045_functions/007_lambda/_exercises/exercises/001_Python lambda expressions who are they_!cool!.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
-# # Лямдба-выражения позволяют нам превратить следующий код:
-#
-# colors = ["Goldenrod", "Purple", "Salmon", "Turquoise", "Cyan"]
-#
-# _ normalize_case string
-#     r_ ?.ca__f.
-#
-#
-# normalized_colors _ ma. ? c...
-#
-# # Вот в такой код:
-#
-#
-# colors _ ["Goldenrod", "Purple", "Salmon", "Turquoise", "Cyan"]
-#
-# normalized_colors _ ma. l_____ s ?.ca_f..  c...
+# Лямдба-выражения позволяют нам превратить следующий код:
+
+colors = ["Goldenrod", "Purple", "Salmon", "Turquoise", "Cyan"]
+
+def normalize_case(string):
+    return string.casefold()
+
+
+normalized_colors = map(normalize_case, colors)
+
+# Вот в такой код:
+
+
+colors = ["Goldenrod", "Purple", "Salmon", "Turquoise", "Cyan"]
+
+normalized_colors = map(lambda s: s.casefold(),  colors)
+print(list(normalized_colors))
