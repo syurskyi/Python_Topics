6f2f5d2af1f97f460a216a4656fbcecda806b153
serge <sergejyurskyj@yahoo.com>
2022-10-02 11:20:43 -0700

    sorted dictionary exercises

---
+++ 018_dictionaries/_exercises/sort_dictionary/001.py
@@ -0,0 +1,29 @@
+# # Create List of Dictionary Values
+#
+# markdict={"Tom":67, "Tina": 54, "Akbar": 87, "Kane": 43, "Divya":73}
+# marklist=l.. ?.i..
+# print ?
+#
+#
+# # sorted()
+#
+# markdict  {"Tom":67, "Tina": 54, "Akbar": 87, "Kane": 43, "Divya":73}
+# marklist  s.. ?.i.. k.._l... x ? 1
+# sortdict  d.. ?
+# print ?
+#
+# # Sort Dict using itemgetter()
+#
+# _____ o__
+#
+# markdict  {"Tom":67, "Tina": 54, "Akbar": 87, "Kane": 43, "Divya":73}
+# marklist s.. ?.i.., key=o__.i.. 1
+# sortdict=d.. ?
+# print ?
+#
+# # sorted()
+#
+# markdict  {"Tom":67, "Tina": 54, "Akbar": 87, "Kane": 43, "Divya":73}
+# marklist s..value, key ___ ? ? __ ?.i..
+# sortdict d.. k v ___ ? ? __ ?
+# print ?
---
+++ 018_dictionaries/_exercises/sort_dictionary/002.py
@@ -0,0 +1,46 @@
+# customers = ['Kaley Fernandez', 'Darius Rowland', 'Isaac Borthwick',  'Alexandria Kidd']
+# sorted_customers = s.. ?
+# print ?
+#
+# # Sort in Descending Order
+#
+# orders = {
+# 	'cappuccino': 54,
+# 	'latte': 56,
+# 	'espresso': 72,
+# 	'americano': 48,
+# 	'cortado': 41
+# }
+#
+# sort_orders = s.. ?.i.., k.._l... x ? 1 r.._T..
+#
+# ___ i __ ?
+# 	print ?0 ? 1
+#
+# # Sort in Ascending Order
+#
+# orders = {
+# 	'cappuccino': 54,
+# 	'latte': 56,
+# 	'espresso': 72,
+# 	'americano': 48,
+# 	'cortado': 41
+# }
+#
+# sort_orders = s.. ?.i.. k.._l... x ? 1
+#
+# ___ i __ ?
+# 	print ? 0 ? 1
+#
+# # List Comprehension
+#
+# orders = {
+# 	'cappuccino': 54,
+# 	'latte': 56,
+# 	'espresso': 72,
+# 	'americano': 48,
+# 	'cortado': 41
+# }
+#
+# print key value ___ ? ? __ s.. ?.i.. k.._l... x ? 1
+#
