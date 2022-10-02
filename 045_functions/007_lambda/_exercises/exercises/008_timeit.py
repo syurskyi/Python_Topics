c18e373ce099266ec49b45875b27fa444c839a8e
serge <sergejyurskyj@yahoo.com>
2022-10-02 10:54:52 -0700

    lambda exercises

--- 045_functions/007_lambda/_exercises/exercises/008_timeit.py
+++ 045_functions/007_lambda/_exercises/exercises/008_timeit.py
@@ -1,7 +1,7 @@
-# ____ ti00 ______ ti00
-# _____ *factorial(999)", "____ ma.. ______ fact..*; nu.._10
-# # 0.0013087529951008037
-#
-# ____ ma.. ______ fac..
-# _____ l_____ fac.. 999 nu.._10
-# # 0.0012704220062005334
+from timeit import timeit
+print(timeit("factorial(999)", "from math import factorial", number=10))
+# 0.0013087529951008037
+
+from math import factorial
+print(timeit(lambda: factorial(999), number=10))
+# 0.0012704220062005334
