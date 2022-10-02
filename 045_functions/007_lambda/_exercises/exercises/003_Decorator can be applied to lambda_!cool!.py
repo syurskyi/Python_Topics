dba65194dc84144bdfc4ac40794c99f1c2ba1b2b
serge <sergejyurskyj@yahoo.com>
2022-10-01 19:11:38 -0700

    lambda exercises

--- 045_functions/007_lambda/_exercises/exercises/003_Decorator can be applied to lambda_!cool!.py
+++ 045_functions/007_lambda/_exercises/exercises/003_Decorator can be applied to lambda_!cool!.py
@@ -1,44 +1,44 @@
 # -*- coding: utf-8 -*-
 
-# ___ some_decorator f
-#     ___ wraps $
-#         print _*Calling function * | ?. -n |**|
-#         r_ ? a___
-#
-#     r_ ?
-#
-# ??
-# ___ decorated_function x
-#     print _*With argument *|x|**|
-#
-# ? 2
-#
-# print('#' * 52 + '  ')
-# print('#' * 52 + '  ')
-# print('#' * 52 + '  ')
-#
-# # Defining a decorator
-# ___ trace f
-#     ___ wrap $ $$
-#         print _*[TRACE] func: ?. -n| args: a..| kwargs: k..|*|
-#         r_ _|$ $$
-#
-#     r_ ?
-#
-# # Applying decorator to a function
-# ??
-# ___ add_two x
-#     r_ ? + 2
-#
-# # Calling the decorated function
-# ? 3
-#
-# # Applying decorator to a lambda
-# print t00|l_____ x x ** 2 | 3
-#
-# print('#' * 52 + '  ')
-# print('#' * 52 + '  ')
-# print('#' * 52 + '  ')
-#
-#
-# li.. ma. t..|l_____ x ? * 2 |ra.. 3
+def some_decorator(f):
+    def wraps(*args):
+        print(f"Calling function ' {f.__name__}'")
+        return f(args)
+
+    return wraps
+
+@some_decorator
+def decorated_function(x):
+    print(f"With argument ' {x}'")
+
+decorated_function(2)
+
+print('#' * 52 + '  ')
+print('#' * 52 + '  ')
+print('#' * 52 + '  ')
+
+# Defining a decorator
+___ trace f
+    ___ wrap $ $$
+        print _*[TRACE] func: ?. -n| args: a..| kwargs: k..|*|
+        r_ _|$ $$
+
+    r_ ?
+
+# Applying decorator to a function
+??
+___ add_two x
+    r_ ? + 2
+
+# Calling the decorated function
+? 3
+
+# Applying decorator to a lambda
+print t00|l_____ x x ** 2 | 3
+
+print('#' * 52 + '  ')
+print('#' * 52 + '  ')
+print('#' * 52 + '  ')
+
+
+li.. ma. t..|l_____ x ? * 2 |ra.. 3
