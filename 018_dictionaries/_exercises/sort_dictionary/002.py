6f707f906ab011e7a1740c8ba972842ed27980ef
serge <sergejyurskyj@yahoo.com>
2022-10-02 11:41:21 -0700

    sorted dictionary exercises

--- 018_dictionaries/_exercises/sort_dictionary/002.py
+++ 018_dictionaries/_exercises/sort_dictionary/002.py
@@ -1,46 +1,46 @@
-# customers = ['Kaley Fernandez', 'Darius Rowland', 'Isaac Borthwick',  'Alexandria Kidd']
-# sorted_customers = s.. ?
-# print ?
-#
-# # Sort in Descending Order
-#
-# orders = {
-# 	'cappuccino': 54,
-# 	'latte': 56,
-# 	'espresso': 72,
-# 	'americano': 48,
-# 	'cortado': 41
-# }
-#
-# sort_orders = s.. ?.i.., k.._l... x ? 1 r.._T..
-#
-# ___ i __ ?
-# 	print ?0 ? 1
-#
-# # Sort in Ascending Order
-#
-# orders = {
-# 	'cappuccino': 54,
-# 	'latte': 56,
-# 	'espresso': 72,
-# 	'americano': 48,
-# 	'cortado': 41
-# }
-#
-# sort_orders = s.. ?.i.. k.._l... x ? 1
-#
-# ___ i __ ?
-# 	print ? 0 ? 1
-#
-# # List Comprehension
-#
-# orders = {
-# 	'cappuccino': 54,
-# 	'latte': 56,
-# 	'espresso': 72,
-# 	'americano': 48,
-# 	'cortado': 41
-# }
-#
-# print key value ___ ? ? __ s.. ?.i.. k.._l... x ? 1
-#
+customers = ['Kaley Fernandez', 'Darius Rowland', 'Isaac Borthwick',  'Alexandria Kidd']
+sorted_customers = sorted(customers)
+print(sorted_customers)
+
+# Sort in Descending Order
+
+orders = {
+	'cappuccino': 54,
+	'latte': 56,
+	'espresso': 72,
+	'americano': 48,
+	'cortado': 41
+}
+
+sort_orders = sorted(orders.items(), key=lambda x: x[1], reverse=True)
+
+for i in sort_orders:
+	print(i[0], i[1])
+
+# Sort in Ascending Order
+
+orders = {
+	'cappuccino': 54,
+	'latte': 56,
+	'espresso': 72,
+	'americano': 48,
+	'cortado': 41
+}
+
+sort_orders = sorted(orders.items(), key=lambda x: x[1])
+
+for i in sort_orders:
+	print(i[0], i[1])
+
+# List Comprehension
+
+orders = {
+	'cappuccino': 54,
+	'latte': 56,
+	'espresso': 72,
+	'americano': 48,
+	'cortado': 41
+}
+
+[print(key, value) for (key, value) in sorted(orders.items(), key=lambda x: x[1])]
+
