400c65d60185308fd817f70aee1eb9a08de21772
serge <sergejyurskyj@yahoo.com>
2022-10-02 11:08:48 -0700

    lambda exercises

--- 045_functions/007_lambda/_exercises/exercises/010_Example use with filter.py
+++ 045_functions/007_lambda/_exercises/exercises/010_Example use with filter.py
@@ -1,8 +1,8 @@
-# # Program to filter out only the even items from a list
-#
-# my_list _ [1, 5, 4, 6, 8, 11, 3, 12]
-#
-# new_list _ li.. f.. l_____ x |?%2 __ 0  ?   # need to be carefull with parentheses
-#
-# # Output: [4, 6, 8, 12]
-# print ?
+# Program to filter out only the even items from a list
+
+my_list = [1, 5, 4, 6, 8, 11, 3, 12]
+
+new_list = list(filter(lambda x: (x%2 == 0),  my_list))   # need to be carefull with parentheses
+
+# Output: [4, 6, 8, 12]
+print(new_list)
