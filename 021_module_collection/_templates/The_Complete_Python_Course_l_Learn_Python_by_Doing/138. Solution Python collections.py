11e1a8fde803407814a3f111abb385aad9ab0aa7
serge <sergejyurskyj@yahoo.com>
2022-10-18 17:25:54 -0700

    module collections template

--- 021_module_collection/_templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/138. Solution Python collections.py
+++ 021_module_collection/_templates/The_Complete_Python_Course_l_Learn_Python_by_Doing/138. Solution Python collections.py
@@ -1,27 +1,27 @@
-____ c... ____ d.., O.., n.., d..
-
-
-def task1() -> d..:
-    dd = d..(l__ 'Unknown')
-    dd['Alan'] = 'Manchester'
-    return dd
-
-
-def task2(arg_od: O..):
-    arg_od.popitem()
-    arg_od.popitem(False)
-    # remember to remove start and end before moving Bob and Dan, otherwise they will be removed instead
-    arg_od.move_to_end('Bob')
-    arg_od.move_to_end('Dan', False)
-
-
-def task3(name: str, club: str) -> n..:
-    Player = n..('Player', ['name', 'club'])
-    player = Player(name, club)
-    return player
-
-
-def task4(arg_deque: d..):
-    arg_deque.pop()  # remove last element
-    arg_deque.a..(arg_deque.popleft())  # remove first element and append it to last
-    arg_deque.appendleft('Zack')  # add Zack to start
+# ____ c... ____ d.., O.., n.., d..
+#
+#
+# ___ task1 __ d..
+#     dd = d.. l__ 'Unknown'
+#     ? 'Alan'  'Manchester'
+#     r_ ??
+#
+#
+# ___ task2 arg_od| O..
+#     ?.p..
+#     ?.p.. F..
+#     # remember to remove start and end before moving Bob and Dan, otherwise they will be removed instead
+#     ?.m.. Bob
+#     ?.m.. Dan F..
+#
+#
+# ___ task3 name| s.. club| s.. __ n..
+#     Player = n..('Player', ['name', 'club'])
+#     player = ? ? ?
+#     r_ ?
+#
+#
+# ___ task4 arg_deque| d..
+#     ?.pop  # remove last element
+#     ?.a.. ?.p..  # remove first element and append it to last
+#     ?.a.. Zack  # add Zack to start
