b59e9915dad72a88f94e79bf25992bfb120137b7
serge <sergejyurskyj@yahoo.com>
2022-11-15 18:16:47 -0800

    Committed dict comprehension exercise

--- 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/exercises/01_!cool!.py
+++ 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/exercises/01_!cool!.py
@@ -1,7 +1,7 @@
-# # Example 1
+# Example 1
 
 words = ['data', 'science', 'machine', 'learning'] #list comprehension
-# # i
+# i
 print([len(i) for i in words])
 # # [4, 7, 7, 8]
 # # #dictionary comprehension
@@ -18,7 +18,7 @@ print({i: len(i) for i in words})
 # # Both list and dictionary comprehensions accept if/else conditional statements.
 #
 words = ['data', 'science', 'machine', 'learning']#list comprehension
-# # i
+# i
 print([len(i) for i in words if len(i) > 5])
 # # [7, 7, 8]
 # #dictionary comprehension
@@ -29,88 +29,88 @@ print({i: len(i) for i in words if len(i) > 5})
 # # Example 3
 # # In this example, we will slightly increase the complexity of the conditional statement.
 #
-# # i
+# i
 words_dict = {i: len(i) if len(i) > 5 else 'short' for i in words}
 print(words_dict)
-# # {'data': 'short', 'science': 7, 'machine': 7, 'learning': 8}
+# {'data': 'short', 'science': 7, 'machine': 7, 'learning': 8}
 
-words_dict = {} # dict
+words_dict = {}  # dict
 for i in words:
-    if len(i) > 5:
+    if len(i) >5:
         words_dict[i] = len(i)
     else:
         words_dict[i] = 'short'
 print(words_dict)
-
+#
 # # Example 4
 # # We can iterate over two iterables in a dictionary comprehension.
-
-words = ['data', 'science', 'machine', 'learning']
-values = [5, 3, 1, 8]
+#
+# words = ['data', 'science', 'machine', 'learning']
+# values = [5, 3, 1, 8]
 # # i, j
-dict_a = {i: j for i, j in zip(words, values)}
-print(dict_a)
+# dict_a =  ? ? ___ ? ? __ z.. ? ?
+# print ?
 # # {'data': 5, 'science': 3, 'machine': 1, 'learning': 8}
 #
 # # Example 5
 # # We can also put a condition on the values when iterating over a list of tuples.
-
-words = ['data', 'science', 'machine', 'learning']
-values = [5, 3, 1, 8]
+#
+# words = ['data', 'science', 'machine', 'learning']
+# values = [5, 3, 1, 8]
 # # i, j
-dict_a = {i: j for i, j in zip(words, values) if j > 4}
-print(dict_a)
+# dict_a _  ? ? ___ ? ? __ z.. ? ? __ ? > 4
+# print ?
 # # {'data': 5, 'learning': 8}
 #
 # # Example 6
 # # We can also apply transformations on key-value pairs.
 #
 # # i, j
-dict_b = {i.upper(): j**2 for i, j in zip(words, values)}
-print(dict_b)
+# dict_b =  ?.u.. ?**2 ___ ? ? __ z.. ? ?
+# print ?
 # # {'DATA': 25, 'SCIENCE': 9, 'MACHINE': 1, 'LEARNING': 64}
 #
 # # Example 7
 # # We can access the key-value pairs in a dictionary by using the items method.
-
-print(dict_b.items())
+#
+# print ?.i..
 # # dict_items([('DATA', 25), ('SCIENCE', 9), ('MACHINE', 1), ('LEARNING', 64)])
 #
 # # We can use the items of an existing dictionary as iterable in a dictionary comprehension.
 # # It allows us to create dictionaries based on existing dictionaries and modify both keys and values.
 #
 # # i, j
-dict_c = {i.lower(): j % 2 for i, j in dict_b.items()}
-print(dict_c)
+# dict_c = ?.l.. ?%2 ___ ? ? __ _b.i..
+# print ?
 # # {'data': 1, 'science': 1, 'machine': 1, 'learning': 0}
 #
 # # Example 8
 # # The enumerate function of Python can be used to create an iterable of tuples based on a list.
 # # Each tuple contains the items in the list with incrementing integer values.
-
-names = ['John', 'Jane', 'Adam', 'Eva', 'Ashley']
-print(list(enumerate(names)))
+#
+# names = ['John', 'Jane', 'Adam', 'Eva', 'Ashley']
+# l.. e.. ?
 # # [(0, 'John'), (1, 'Jane'), (2, 'Adam'), (3, 'Eva'), (4, 'Ashley')]
 #
 # # We can use the enumerate function in a dictionary comprehension.
 #
 # # i, j
-dict_names = {i: len(j) for i,j in enumerate(names)}
-print(dict_names)
+# dict_names = i le_ ? ___ ? ? __ e.. ?
+# print ?
 # # {0: 4, 1: 4, 2: 4, 3: 3, 4: 6}
 #
 # # If you just want to create a dictionary based on a list of tuples without any modification on the values,
 # # you do not need to use a comprehension. The dict function will do the job.
-
-print(dict(enumerate(names)))
+#
+# di.. e.. ?
 # # {0: 'John', 1: 'Jane', 2: 'Adam', 3: 'Eva', 4: 'Ashley'}
 #
 # # Example 9
 # # This example contains a slightly more complicated conditionals than the previous ones.
 # # Consider we have the following dictionary and list.
-
-lst = ['data','science','artificial', 'intelligence']
-dct = {'data': 5, 'science': 3, 'machine': 1, 'learning': 8}
+#
+# lst = ['data','science','artificial', 'intelligence']
+# dct = {'data': 5, 'science': 3, 'machine': 1, 'learning': 8}
 #
 # # We want to create a new dictionary using the list and dictionary defined above.
 # # The keys of the new dictionary will be the elements in the list so we will iterate over the elements in list.
@@ -118,18 +118,18 @@ dct = {'data': 5, 'science': 3, 'machine': 1, 'learning': 8}
 # # Otherwise, the value will be the length of the key.
 #
 # #
-print({i: dct[i] if i in dct else len(i) for i in lst})
+# print i d.. ? __ ? __ ? ____ le_ ? ___ ? __ ?
 # # {'artificial': 10, 'data': 5, 'intelligence': 12, 'science': 3}
 #
 # # Example 10
 # # The keys of a dictionary must be immutable so tuples can be used as keys.
 # # Dictionary comprehensions allow for generating keys of tuples by implemented nested loops.
 #
-a = [1,2,3,4]
-b = [5,6,7]
+# a = [1,2,3,4]
+# b = [5,6,7]
 # # i j
-dct = {(i, j): i*j for i in a for j in b}
-print(dct)
+# dct _  ? ? ?*? ___ ? __ ? ___ ? __ ?
+# print(?
 #
 # # {(1, 5): 5,
 # #  (1, 6): 6,
@@ -150,6 +150,6 @@ print(dct)
 #
 # dct  # dict
 # # i, j
-for i in a:
-    for j in b:
-        dct[(i, j)] = i * j
+# ___ ? __ ?
+#     ___ ? __ ?
+#         ? ? ? _ ? * ?
--- 050_iterations_comprehension_generations/002_dictionary_comprehension/_exercises/exercises/family_dict.py
+++
@@ -1,7 +0,0 @@
-# my_family = {'sister': {'name': 'Krista', 'age': 42}, 'mother': {'name': 'Cathie', 'age': 70}}
-#
-# # key, value
-# ___ ? ? __ ?.i..
-#    family =  ?|*name +" is my "+?+" and is "+st_ ?|*age +" years old." ___ ? ? __ ?.i..
-#    print ?
-# # {'Krista is my sister and is 42 years old.', 'Cathie is my mother and is 70 years old.'}
