0a4d880709b21f1f83840d845cedd249e2b450da
serge <sergejyurskyj@yahoo.com>
2022-12-25 17:51:51 -0800

    Committed algorithms exercises

--- 125_algorithms/_exercises/exercises/_algorithms_challenges/pybites/beginner/5_v4/names.py
+++ 125_algorithms/_exercises/exercises/_algorithms_challenges/pybites/beginner/5_v4/names.py
@@ -1,41 +1,41 @@
-# ____ pprint _______ pprint
-#
-# NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
-#          'julian sequeira', 'sandra bullock', 'keanu reeves',
-#          'julbob pybites', 'bob belderbos', 'julian sequeira',
-#          'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
-#
-#
-# ___ dedup_and_title_case_names names
-#     """Should return a list of title cased names,
-#        each name appears only once"""
-#     r.. l.. ? x .t.. ___ ? __ r.. l.. ?
-#
-#
-# ___ sort_by_surname_desc names
-#     """Returns names list sorted desc by surname"""
-#     names ? ?
-#     r.. s.. ? k.._l.... x ?.s.. 1 r.._T..
-#
-#
-# ___ shortest_first_name names
-#     """Returns the shortest first name (str).
-#        You can assume there is only one shortest name.
-#     """
-#     names ? ?
-#     r.. s.. ? k.._l.... x l.. ?.s.. 0 0 .s.. 0
-#
-#
-# ___ test_dedup_and_title_case_names():
-#     names = dedup_and_title_case_names(NAMES)
-#     ... names.c.. 'Bob Belderbos') __ 1
-#     ... names.c.. 'julian sequeira') __ 0
-#     ... names.c.. 'Brad Pitt') __ 1
-#     ... l..(names) __ 10
-#     ... a..(n.t.. __ names ___ n __ NAMES)
-#
-#
-# # test_dedup_and_title_case_names()
-# # pprint(dedup_and_title_case_names(NAMES))
-# pprint(sort_by_surname_desc(NAMES))
-# pprint(shortest_first_name(NAMES))
+from pprint import pprint
+
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
+
+
+def dedup_and_title_case_names(names):
+    """Should return a list of title cased names,
+       each name appears only once"""
+    return list({names[x].title() for x in range(len(names))})
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+    return sorted(names, key=lambda x: x.split()[1], reverse=True)
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names = dedup_and_title_case_names(names)
+    return sorted(names, key=lambda x: len(x.split()[0]))[0].split()[0]
+
+
+def test_dedup_and_title_case_names():
+    names = dedup_and_title_case_names(NAMES)
+    assert names.count('Bob Belderbos') == 1
+    assert names.count('julian sequeira') == 0
+    assert names.count('Brad Pitt') == 1
+    assert len(names) == 10
+    assert all(n.title() in names for n in NAMES)
+
+
+# test_dedup_and_title_case_names()
+# pprint(dedup_and_title_case_names(NAMES))
+pprint(sort_by_surname_desc(NAMES))
+pprint(shortest_first_name(NAMES))
