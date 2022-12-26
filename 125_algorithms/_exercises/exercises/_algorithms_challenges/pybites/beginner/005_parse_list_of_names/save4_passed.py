d0d438484307a450bd55dde852a0f03bbe57eed8
serge <sergejyurskyj@yahoo.com>
2022-12-25 18:00:42 -0800

    Committed algorithms exercises

--- 125_algorithms/_exercises/exercises/_algorithms_challenges/pybites/beginner/005_parse_list_of_names/save4_passed.py
+++ 125_algorithms/_exercises/exercises/_algorithms_challenges/pybites/beginner/005_parse_list_of_names/save4_passed.py
@@ -1,42 +1,42 @@
-# NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
-#          'julian sequeira', 'sandra bullock', 'keanu reeves',
-#          'julbob pybites', 'bob belderbos', 'julian sequeira',
-#          'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
-#
-#
-# ___ dedup_and_title_case_names names
-#     """Should return a list of title cased names,
-#        each name appears only once"""
-#     non_dup_lst    # list
-#     ___ x __ names
-#         __ x n.. __ ?
-#             ?.a..?
-#     non_dup_lst  element.t.. ___ ? __ ?
-#     r.. ?
-#
-#
-# ___ sort_by_surname_desc names
-#     """Returns names list sorted desc by surname"""
-#     names ? ?
-#     ?.s.. k.._l.... x ?.s.. 1 r.._T..
-#     r.. ?
-#
-#
-# ___ shortest_first_name names
-#     """Returns the shortest first name (str).
-#        You can assume there is only one shortest name.
-#     """
-#     names ? ?
-#     first_name  x.s.. 0 ___ ? __ ?
-#     shortest_name  m.. ? k.._l..
-#     r.. ?
+NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
+         'julian sequeira', 'sandra bullock', 'keanu reeves',
+         'julbob pybites', 'bob belderbos', 'julian sequeira',
+         'al pacino', 'brad pitt', 'matt damon', 'brad pitt']
 
-# if __name__ == '__main__':
-#     names = dedup_and_title_case_names(NAMES)
-#     print(names)
-#     names = dedup_and_title_case_names(PY_CONTENT_CREATORS)
-#     print(names)
-#     sort_by_surname_desc = sort_by_surname_desc(NAMES)
-#     print(sort_by_surname_desc)
-#     shortest_first_name = shortest_first_name(NAMES)
-#     print(shortest_first_name)
+
+def dedup_and_title_case_names(names):
+    """Should return a list of title cased names,
+       each name appears only once"""
+    non_dup_lst = []    # list
+    for x in names:
+        if x not in non_dup_lst:
+            non_dup_lst.append(x)
+    non_dup_lst = [element.title() for element in non_dup_lst]
+    return non_dup_lst
+
+
+def sort_by_surname_desc(names):
+    """Returns names list sorted desc by surname"""
+    names = dedup_and_title_case_names(names)
+    names.sort(key=lambda x: x.split()[1], reverse=True)
+    return names
+
+
+def shortest_first_name(names):
+    """Returns the shortest first name (str).
+       You can assume there is only one shortest name.
+    """
+    names =dedup_and_title_case_names(names)
+    first_name = [x.split()[0] for x in names]
+    shortest_name = min(first_name, key=len)
+    return shortest_name
+
+if __name__ == '__main__':
+    names = dedup_and_title_case_names(NAMES)
+    print(names)
+    names = dedup_and_title_case_names(PY_CONTENT_CREATORS)
+    print(names)
+    sort_by_surname_desc = sort_by_surname_desc(NAMES)
+    print(sort_by_surname_desc)
+    shortest_first_name = shortest_first_name(NAMES)
+    print(shortest_first_name)
