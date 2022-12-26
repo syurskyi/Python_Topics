72e4e0e2a22410ee352342a0348afa97004d7e73
serge <sergejyurskyj@yahoo.com>
2022-12-25 17:42:41 -0800

    Committed algorithms exercises

--- 125_algorithms/_examples/_algorithms_challenges/pybites/beginner/5_v2/names.py
+++ 125_algorithms/_examples/_algorithms_challenges/pybites/beginner/5_v2/names.py
@@ -7,21 +7,15 @@ NAMES = ['arnold schwarzenegger', 'alec baldwin', 'bob belderbos',
 def dedup_and_title_case_names(names):
     """Should return a list of title cased names,
        each name appears only once"""
-    
 
     return list({name.title() for name in names})
-
-
 
 
 def sort_by_surname_desc(names):
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
 
-
     return sorted(names,key=lambda x: x.split()[1],reverse=True)
-
-
 
 
 def shortest_first_name(names):
