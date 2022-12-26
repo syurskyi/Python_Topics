9869be4b21627c5c0538f600b98915418612b680
serge <sergejyurskyj@yahoo.com>
2022-12-25 17:20:08 -0800

    Committed algorithms exercises

--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/1/solution.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/1/solution.py
@@ -1,4 +1,4 @@
 # ___ sum_numbers numbers_ N..
 #     __ ? __ N..
-#         numbers _ r.. 1 101
+#         n.. _ r.. 1 101
 #     r.. s.. ?
--- 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/1/summing.py
+++ 125_algorithms/_exercises/templates/_algorithms_challenges/pybites/beginner/1/summing.py
@@ -1,13 +1,13 @@
 # ___ sum_numbers numbers_ N..
 #     total  0
-#     __ numbers __ N..
+#     __ n.. __ N..
 #         ___ i __ r.. 1 101
-#             total _ ? + i
+#             t.. _ ? + i
 #     ____ l.. ? __ 0
 #         t.. _ 0
 #     ____ isi.. n.. li.. o. isi.. n.. tu..
 #         ___ i __ ?
-#             total _ ? + ?
+#             t.. _ ? + ?
 #     ____ n.. isi.. ? li.. o. n.. isi.. ? tu..
 #         count _ l.. ?
 #         ___ i __ r.. 0 ?
